f21f39297f750e9432eb257e4fdcef71569f5154
Stephan Raue <stephan@openelec.tv>
2010-08-15 15:56:23 +0200

    release: add simple batchfile and syslinux configuration to create an USB install stick
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

---
+++ config/release/create_installstick.bat
@@ -0,0 +1,23 @@
+@ECHO OFF
+CLS
+ECHO.
+ECHO              OpenELEC.tv USB Installer
+ECHO.
+ECHO.
+ECHO ******************************************************
+ECHO. 
+ECHO This will wipe any data off your chosen drive
+ECHO Please read the instructions and use very carefully...
+ECHO.
+ECHO ******************************************************
+ECHO.
+ECHO.
+ECHO Enter USB Drive letter
+ECHO eg. d:
+ECHO.
+SET /P DRIVE= -- 
+format %DRIVE% /V:INSTALL /Q /FS:FAT32
+3rdparty\syslinux\win32\syslinux.exe %DRIVE%
+copy target\* %DRIVE%
+copy sample.conf\syslinux_installer.cfg %DRIVE%\syslinux.cfg
+SET DRIVE=
