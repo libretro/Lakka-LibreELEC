71bd4c2fe6a7ee2cca3af01d42fa453969b74829
Stephan Raue <stephan@openelec.tv>
2010-10-13 03:07:49 +0200

    3rdparty: add initial atvboot contents
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

---
+++ config/release/3rdparty/atvboot/boot_linux.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+echo "this is a boot_linux.sh script"
+#
+#
+#mkdir -p /tmp/rootfs
+#mount /dev/sda4 /tmp/rootfs
+#kexec --load /tmp/rootfs/vmlinux --initrd=/tmp/rootfs/initrd/gz --command-line="root=/dev/sda4 initrd=initrd.gz"
+#kexec -e
