167496e839380af2ef4912e0403e4ff1b8df1c1b
Stephan Raue <stephan@openelec.tv>
2010-10-29 23:02:32 +0200

    release: rework of 'create_installstick.bat', many thanks to lbirds and ilya
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- config/release/create_installstick.bat
+++ config/release/create_installstick.bat
@@ -12,12 +12,68 @@ ECHO.
 ECHO ******************************************************
 ECHO.
 ECHO.
+ECHO. Are you running this USB Wizard for the first time 
+ECHO. on Windows 7 or Windows Vista? [Y/N]
+ECHO.
+ECHO.
+
+SET /P OS= --
+IF "%OS%"=="n" GOTO INSTALL 
+IF "%OS%"=="N" GOTO INSTALL 
+
+:NOTES
+CLS
+ECHO.
+ECHO ******************************************************
+ECHO. 
+ECHO.
+ECHO Due to UAC in Windows 7 / Windows Vista we need to run
+ECHO syslinux.exe as 'Administrator'
+ECHO.
+ECHO After pressing any key the folder containing syslinux.exe
+ECHO will pop up automatically.
+ECHO.
+ECHO 1. Right click on syslinux.exe
+ECHO 2. Click on 'Properties' 
+ECHO 3. Change to the 'Compatibility' tab
+ECHO 4. Check the 'Run this program as an administrator' checkbox
+ECHO.
+ECHO Click Ok and run 'create_installstick.bat' again,
+ECHO answering 'N' to the first prompt
+ECHO.
+ECHO.
+ECHO ******************************************************
+ECHO.
+pause
+cmd /k "start %CD%\3rdparty\syslinux\win32"
+GOTO END
+
+:INSTALL
+CLS
+ECHO.
+ECHO              OpenELEC.tv USB Installer
+ECHO.
+ECHO.
+ECHO ******************************************************
+ECHO. 
+ECHO This will wipe any data off your chosen drive
+ECHO Please read the instructions and use very carefully...
+ECHO.
+ECHO ******************************************************
+ECHO.
+ECHO.
 ECHO Enter USB Drive letter
 ECHO eg. d:
 ECHO.
+
 SET /P DRIVE= -- 
+if %DRIVE%!==! goto INSTALL
 format %DRIVE% /V:INSTALL /Q /FS:FAT32
 3rdparty\syslinux\win32\syslinux.exe -f -m -a %DRIVE%
 copy target\* %DRIVE%
 copy sample.conf\syslinux_installer.cfg %DRIVE%\syslinux.cfg
 SET DRIVE=
+GOTO END
+
+:END
+SET OS=
