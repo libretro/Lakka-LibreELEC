1a8b872b5fcec3eecd59753d29ce61e0c774bd7d
Stephan Raue <stephan@openelec.tv>
2011-12-01 18:05:06 +0100

    xbmc-wiimote: rebuild with xbmc.python API 2.0, source /etc/profile in scripts
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/addons/driver/xbmc-wiimote/source/default.py
+++ packages/addons/driver/xbmc-wiimote/source/default.py
@@ -18,6 +18,15 @@
 #  http://www.gnu.org/copyleft/gpl.html
 ################################################################################
 
-import xbmc, time, os, subprocess
+import os
+import sys
+import xbmcaddon
 
-os.system("wiimote.service")
+__scriptname__ = "WiiMote driver"
+__author__ = "OpenELEC"
+__url__ = "http://www.openelec.tv"
+__settings__   = xbmcaddon.Addon(id='driver.remote.xbmc-wiimote')
+__cwd__        = __settings__.getAddonInfo('path')
+__path__       = xbmc.translatePath( os.path.join( __cwd__, 'bin', "wiimote.service") )
+
+os.system(__path__)
