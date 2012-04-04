1ee131a057e2416547e13f1fc526944306e00785
Lukas Heiniger <lheiniger@gmail.com>
2012-04-04 20:49:59 +0200

    Better integration, fix issue #334
    - Changes the default configuration to improve tool integration
    - Fixes Issue #334
    - Adds a new option to keep XBMC awake while SABnzbd is downloading

--- packages/addons/service/downloadmanager/SABnzbd-Suite/source/default.py
+++ packages/addons/service/downloadmanager/SABnzbd-Suite/source/default.py
@@ -24,15 +24,66 @@ import xbmcaddon
 import time
 import subprocess
 import xbmc
+import urllib2
+import socket
+from configobj import ConfigObj
 
 __scriptname__ = "SABnzbd Suite"
-__author__ = "OpenELEC"
-__url__ = "http://www.openelec.tv"
+__author__     = "OpenELEC"
+__url__        = "http://www.openelec.tv"
 __settings__   = xbmcaddon.Addon(id='service.downloadmanager.SABnzbd-Suite')
 __cwd__        = __settings__.getAddonInfo('path')
-__path__       = xbmc.translatePath( os.path.join( __cwd__, 'bin', "SABnzbd-Suite.service") )
+__path__       = xbmc.translatePath( os.path.join( __cwd__, 'bin', "SABnzbd-Suite.py") )
 
-#make binary files executable in adson bin folder
-subprocess.Popen("chmod -R +x " + __cwd__ + "/bin/*" , shell=True, close_fds=True)
+checkInterval  = 120
+timeout        = 20
 
-subprocess.Popen(__path__, shell=True, close_fds=True)
+
+# Launch Suite
+subprocess.call(['python',__path__])
+
+
+# SABnzbd addresses and api key
+sabNzbdAddress    = '127.0.0.1:8081'
+sabNzbdConfigFile = '/storage/.xbmc/userdata/addon_data/service.downloadmanager.SABnzbd-Suite/sabnzbd.ini'
+sabConfiguration  = ConfigObj(sabNzbdConfigFile)
+sabNzbdApiKey     = sabConfiguration['misc']['api_key']
+sabNzbdUser       = sabConfiguration['misc']['username']
+sabNzbdPass       = sabConfiguration['misc']['password']
+sabNzbdQueue      = 'http://' + sabNzbdAddress + '/sabnzbd/api?mode=queue&output=xml&apikey=' + sabNzbdApiKey + '&ma_username=' + sabNzbdUser + '&ma_password=' + sabNzbdUser
+
+# start checking SABnzbd for activity and prevent sleeping if necessary
+socket.setdefaulttimeout(timeout)
+
+shouldKeepAwake = __settings__.getSetting('SABNZBD_KEEP_AWAKE')
+if shouldKeepAwake:
+    xbmc.log('SABnzbd-Suite: will prevent idle sleep/shutdown while downloading')
+
+while (not xbmc.abortRequested):
+    
+    # reread setting in case it has changed
+    shouldKeepAwake = __settings__.getSetting('SABNZBD_KEEP_AWAKE')
+    
+    # check if SABnzbd is downloading
+    sabIsActive = False
+    req = urllib2.Request(sabNzbdQueue)
+    try: handle = urllib2.urlopen(req)
+    except IOError, e:
+        xbmc.log('SABnzbd-Suite: could not determine SABnzbds status', level=xbmc.LOGERROR)
+    else:
+        queue = handle.read()
+        handle.close()
+        sabIsActive = (queue.find('<status>Downloading</status>') >= 0)
+    
+    # reset idle timer when we're close to idle sleep/shutdown
+    if (shouldKeepAwake and sabIsActive):
+        response = xbmc.executehttpapi("GetGUISetting(0;powermanagement.shutdowntime)").replace('<li>','')
+        shutdownTime = int(response) * 60
+        idleTime = xbmc.getGlobalIdleTime()
+        timeToShutdown = shutdownTime - idleTime
+        
+        if (sabIsActive and timeToShutdown <= checkInterval - timeout):
+            xbmc.log('SABnzbd-Suite: still downloading. Resetting XBMC idle timer.')
+            xbmc.executehttpapi("SendKey(0xF000)")
+        
+    xbmc.sleep(checkInterval * 1000)
