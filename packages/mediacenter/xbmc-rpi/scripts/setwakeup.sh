ca7ed1c6d4971d0c4935ec7c704f22544f08a1d0
Stephan Raue <stephan@openelec.tv>
2012-06-03 00:09:50 +0200

    xbmc-rpi: sync buildscripts with package 'xbmc-frodo'
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/xbmc-rpi/scripts/setwakeup.sh
+++ packages/mediacenter/xbmc-rpi/scripts/setwakeup.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
 #      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
@@ -18,10 +20,9 @@
 #  http://www.gnu.org/copyleft/gpl.html
 ################################################################################
 
-# starting XBMC
-#
-# runlevels: openelec
-
-progress "clean up log files"
-  rm -f /storage/.xbmc/userdata/addon_data/*/*.log 2>/dev/null
-  rm -rf /storage/.xbmc/userdata/addon_data/*/log/* 2>/dev/null
+if [ -f /sys/class/rtc/rtc0/wakealarm ]; then
+  logger -t setwakeup.sh "### Setting system wakeup time ###"
+  echo 0 > /sys/class/rtc/rtc0/wakealarm
+  echo $1 > /sys/class/rtc/rtc0/wakealarm
+  logger -t setwakeup.sh "### $(cat /proc/driver/rtc) ###"
+fi
