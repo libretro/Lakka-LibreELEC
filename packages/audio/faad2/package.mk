04c190e4cceccbfc1c55f0297a1e7b4b1bc0a809
Stephan Raue <stephan@openelec.tv>
2013-07-17 17:29:07 +0200

    faad2: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/audio/faad2/package.mk
+++ packages/audio/faad2/package.mk
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
 #      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
@@ -20,7 +18,30 @@
 #  http://www.gnu.org/copyleft/gpl.html
 ################################################################################
 
-. config/options $1
+PKG_NAME="faad2"
+PKG_VERSION="2.7"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://www.audiocoding.com/"
+PKG_URL="$SOURCEFORGE_SRC/faac/faad2-src/$PKG_NAME-$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_DEPENDS=""
+PKG_BUILD_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="audio"
+PKG_SHORTDESC="faad: An MPEG-4 AAC decoder"
+PKG_LONGDESC="The FAAD project includes the AAC decoder FAAD2. It supports several MPEG-4 object types (LC, Main, LTP, HE AAC, PS) and file formats (ADTS AAC, raw AAC, MP4), multichannel and gapless decoding as well as MP4 metadata tags. The codecs are compatible with standard-compliant audio applications using one or more of these profiles."
 
-mkdir -p $INSTALL/usr/lib
-  cp -P $PKG_BUILD/libfaad/.libs/*.so* $INSTALL/usr/lib
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="yes"
+
+# package specific configure options
+PKG_CONFIGURE_OPTS_TARGET="--without-xmms \
+                           --without-drm \
+                           --without-mpeg4ip \
+                           --with-gnu-ld"
+
+
+post_makeinstall_target() {
+  rm -rf $INSTALL/usr/bin
+}
