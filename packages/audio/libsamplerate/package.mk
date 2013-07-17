82b574a41cbd1c61c33e4d5585c1758e7069a9aa
Stephan Raue <stephan@openelec.tv>
2013-07-17 18:33:14 +0200

    libsamplerate: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/audio/libsamplerate/package.mk
+++ packages/audio/libsamplerate/package.mk
@@ -26,11 +26,22 @@ PKG_LICENSE="GPL"
 PKG_SITE="http://www.mega-nerd.com/SRC/"
 PKG_URL="http://www.mega-nerd.com/SRC/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS=""
-PKG_BUILD_DEPENDS="toolchain"
+PKG_BUILD_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
 PKG_SECTION="audio"
 PKG_SHORTDESC="libsamplerate: A Sample Rate Converter library for audio"
 PKG_LONGDESC="Libsamplerate is a Sample Rate Converter for audio. One example of where such a thing would be useful is converting audio from the CD sample rate of 44.1kHz to the 48kHz sample rate used by DAT players."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="no" # ToDo
+
+# package specific configure options
+PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
+                           --enable-static \
+                           --datadir=/usr/share \
+                           --disable-fftw \
+                           --disable-sndfile"
+
+post_makeinstall_target() {
+  rm -rf $INSTALL/usr/bin
+}
