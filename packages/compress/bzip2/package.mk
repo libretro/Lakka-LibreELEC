3f0ad928db780c71c561f1c0810e4ec6569b39b9
Stephan Raue <stephan@openelec.tv>
2013-07-17 19:04:03 +0200

    bzip2: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/compress/bzip2/package.mk
+++ packages/compress/bzip2/package.mk
@@ -26,11 +26,33 @@ PKG_LICENSE="GPL"
 PKG_SITE="http://www.bzip.org"
 PKG_URL="http://www.bzip.org/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS=""
-PKG_BUILD_DEPENDS="toolchain"
+PKG_BUILD_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
 PKG_SECTION="compress"
 PKG_SHORTDESC="bzip2 data compressor"
 PKG_LONGDESC="bzip2 is a freely available, patent free (see below), high-quality data compressor. It typically compresses files to within 10% to 15% of the best available techniques (the PPM family of statistical compressors), whilst being around twice as fast at compression and six times faster at decompression."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+
+pre_make_target() {
+  sed -e "s,ln -s (lib.*),ln -snf \$$1; ln -snf libbz2.so.$PKG_VERSION libbz2.so,g" -i Makefile-libbz2_so
+}
+
+make_target() {
+  make -f Makefile-libbz2_so CC=$TARGET_CC CFLAGS="$CFLAGS -fPIC -DPIC"
+}
+
+post_make_target() {
+  ln -snf libbz2.so.1.0 libbz2.so
+}
+
+makeinstall_target() {
+  mkdir -p $SYSROOT_PREFIX/usr/include
+    cp bzlib.h $SYSROOT_PREFIX/usr/include
+  mkdir -p $SYSROOT_PREFIX/usr/lib
+    cp -P libbz2.so* $SYSROOT_PREFIX/usr/lib
+
+  mkdir -p $INSTALL/usr/lib
+    cp -P libbz2.so* $INSTALL/usr/lib
+}
