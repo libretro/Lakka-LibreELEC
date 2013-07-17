2d4257ff4a471b012fd927d36a140573118e8b24
Stephan Raue <stephan@openelec.tv>
2013-07-17 17:29:23 +0200

    lame: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/audio/lame/package.mk
+++ packages/audio/lame/package.mk
@@ -26,11 +26,34 @@ PKG_LICENSE="LGPL"
 PKG_SITE="http://lame.sourceforge.net/"
 PKG_URL="$SOURCEFORGE_SRC/lame/lame/3.99/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS=""
-PKG_BUILD_DEPENDS="toolchain"
+PKG_BUILD_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
 PKG_SECTION="audio"
 PKG_SHORTDESC="lame: LAME Ain't an Mp3 Encoder"
 PKG_LONGDESC="LAME originally stood for LAME Ain't an Mp3 Encoder. The goal of the LAME project is to use the open source model to improve the psycho acoustics, noise shaping and speed of MP3. Another goal of the LAME project is to use these improvements for the basis of a patent free audio compression codec for the GNU project."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
+
+# package specific configure options
+PKG_CONFIGURE_OPTS_TARGET="--enable-nasm \
+                           --disable-rpath \
+                           --disable-cpml \
+                           --disable-gtktest \
+                           --disable-efence \
+                           --disable-analyzer-hooks \
+                           --enable-decoder \
+                           --disable-frontend \
+                           --disable-mp3x \
+                           --disable-mp3rtp \
+                           --disable-dynamic-frontends \
+                           --enable-expopt=no \
+                           --enable-debug=no \
+                           --with-gnu-ld \
+                           --with-fileio=lame \
+                           GTK_CONFIG=no"
+
+post_make_target() {
+  # dont build parallel
+    MAKEFLAGS=-j1
+}
