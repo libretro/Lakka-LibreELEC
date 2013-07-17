0fea584083ab33b5b06027214c466200e26dc468
Stephan Raue <stephan@openelec.tv>
2013-07-17 06:35:36 +0200

    fontsproto: convert ot new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/x11/proto/fontsproto/package.mk
+++ packages/x11/proto/fontsproto/package.mk
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
 #      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
@@ -20,15 +18,22 @@
 #  http://www.gnu.org/copyleft/gpl.html
 ################################################################################
 
-. config/options $1
+PKG_NAME="fontsproto"
+PKG_VERSION="2.1.2"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="OSS"
+PKG_SITE="http://www.X.org"
+PKG_URL="http://xorg.freedesktop.org/archive/individual/proto/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_DEPENDS=""
+PKG_BUILD_DEPENDS_TARGET="toolchain util-macros"
+PKG_PRIORITY="optional"
+PKG_SECTION="x11/proto"
+PKG_SHORTDESC="fontsproto: Fonts extension headers"
+PKG_LONGDESC="Fonts extension headers"
 
-cd $PKG_BUILD
-./configure --host=$TARGET_NAME \
-            --build=$HOST_NAME \
-            --prefix=/usr \
-            --sysconfdir=/etc \
-            --localstatedir=/var \
-            --without-xmlto
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="yes"
 
-make
-$MAKEINSTALL
+# package specific configure options
+PKG_CONFIGURE_OPTS_TARGET="--without-xmlto"
