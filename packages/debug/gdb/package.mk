98c9fde73603fcad26b026db3a54a76e89958f8e
Stephan Raue <stephan@openelec.tv>
2013-07-21 19:04:08 +0200

    gd: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/debug/gdb/package.mk
+++ packages/debug/gdb/package.mk
@@ -25,13 +25,29 @@ PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/software/gdb/"
 PKG_URL="http://ftp.gnu.org/gnu/gdb/$PKG_NAME-$PKG_VERSION.tar.bz2"
-#PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS="zlib ncurses expat"
-PKG_BUILD_DEPENDS="toolchain zlib ncurses expat"
+PKG_BUILD_DEPENDS_TARGET="toolchain zlib ncurses expat"
 PKG_PRIORITY="optional"
 PKG_SECTION="debug"
 PKG_SHORTDESC="gdb: The GNU Debugger"
 PKG_LONGDESC="The purpose of a debugger such as GDB is to allow you to see what is going on ``inside'' another program while it executes--or what another program was doing at the moment it crashed."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+
+CC_FOR_BUILD="$HOST_CC"
+CFLAGS_FOR_BUILD="$HOST_CFLAGS"
+
+PKG_CONFIGURE_OPTS_TARGET="bash_cv_have_mbstate_t=set \
+                           --disable-shared \
+                           --enable-static \
+                           --disable-nls \
+                           --disable-sim \
+                           --without-x \
+                           --disable-tui \
+                           --disable-libada \
+                           --disable-werror"
+
+post_makeinstall_target() {
+  rm -rf $INSTALL/usr/share/gdb/python
+}
