9869c043f5e34092af9655a78ba24adfd0025b49
Stephan Raue <stephan@openelec.tv>
2013-07-28 05:47:47 +0200

    bluez: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

---
+++ packages/network/bluez/package.mk
@@ -0,0 +1,89 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.tv; see the file COPYING.  If not, write to
+#  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
+#  http://www.gnu.org/copyleft/gpl.html
+################################################################################
+
+PKG_NAME="bluez"
+PKG_VERSION="5.7"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://www.bluez.org/"
+PKG_URL="http://www.kernel.org/pub/linux/bluetooth/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_DEPENDS="libusb-compat dbus glib"
+PKG_BUILD_DEPENDS_TARGET="toolchain libusb-compat dbus glib readline"
+PKG_PRIORITY="optional"
+PKG_SECTION="network"
+PKG_SHORTDESC="bluez: Bluetooth Tools and System Daemons for Linux."
+PKG_LONGDESC="Bluetooth Tools and System Daemons for Linux."
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="yes"
+
+if [ "$DEBUG" = "yes" ]; then
+  BLUEZ_CONFIG="--enable-debug"
+else
+  BLUEZ_CONFIG="--disable-debug"
+fi
+
+if [ "$DEVTOOLS" = "yes" ]; then
+  BLUEZ_CONFIG="$BLUEZ_CONFIG --enable-monitor --enable-test --enable-tools"
+else
+  BLUEZ_CONFIG="$BLUEZ_CONFIG --disable-monitor --disable-test --enable-tools"
+fi
+
+PKG_CONFIGURE_OPTS_TARGET="--disable-dependency-tracking \
+                           --disable-silent-rules \
+                           --disable-shared \
+                           --enable-static \
+                           --enable-library \
+                           --enable-usb \
+                           --disable-udev \
+                           --disable-cups \
+                           --disable-obex \
+                           --enable-client \
+                           --disable-systemd \
+                           --enable-datafiles \
+                           --disable-experimental \
+                           --with-gnu-ld \
+                           $BLUEZ_CONFIG \
+                           storagedir=/storage/.cache/bluetooth"
+
+pre_configure_target() {
+# bluez fails to build in subdirs
+  cd $ROOT/$PKG_BUILD
+    rm -rf .$TARGET_NAME
+}
+
+post_makeinstall_target() {
+  if [ ! "$DEVTOOLS" = "yes" ]; then
+    rm -rf $INSTALL/usr/bin/bccmd
+    rm -rf $INSTALL/usr/bin/ciptool
+    rm -rf $INSTALL/usr/bin/hciattach
+    rm -rf $INSTALL/usr/bin/hciconfig
+    rm -rf $INSTALL/usr/bin/hcidump
+    rm -rf $INSTALL/usr/bin/hcitool
+    rm -rf $INSTALL/usr/bin/l2ping
+    rm -rf $INSTALL/usr/bin/l2test
+    rm -rf $INSTALL/usr/bin/rctest
+    rm -rf $INSTALL/usr/bin/rfcomm
+    rm -rf $INSTALL/usr/bin/sdptool
+    rm -rf $INSTALL/usr/bin/btmon
+    rm -rf $INSTALL/usr/lib/bluez/test
+  fi
+}
