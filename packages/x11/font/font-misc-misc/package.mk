2f8d91382f51519d77d7c419458cb19192f05ca1
Stephan Raue <stephan@openelec.tv>
2013-08-01 14:02:47 +0200

    font-misc-misc: change to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/x11/font/font-misc-misc/package.mk
+++ packages/x11/font/font-misc-misc/package.mk
@@ -26,11 +26,40 @@ PKG_LICENSE="OSS"
 PKG_SITE="http://www.X.org"
 PKG_URL="http://xorg.freedesktop.org/archive/individual/font/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS="font-util font-cursor-misc"
-PKG_BUILD_DEPENDS="toolchain util-macros font-util font-cursor-misc"
+PKG_BUILD_DEPENDS_TARGET="toolchain util-macros font-util font-cursor-misc"
 PKG_PRIORITY="optional"
 PKG_SECTION="x11/font"
 PKG_SHORTDESC="font-misc-misc: A misc. public domain font"
 PKG_LONGDESC="A misc. public domain font."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
+
+PKG_CONFIGURE_OPTS_TARGET="--with-fontrootdir=/usr/share/fonts \
+                           --disable-silent-rules \
+                           --enable-iso8859-1 \
+                           --enable-iso8859-2 \
+                           --disable-iso8859-3 \
+                           --disable-iso8859-4 \
+                           --enable-iso8859-5 \
+                           --disable-iso8859-6 \
+                           --enable-iso8859-7 \
+                           --enable-iso8859-8 \
+                           --enable-iso8859-9 \
+                           --disable-iso8859-10 \
+                           --disable-iso8859-11 \
+                           --disable-iso8859-12 \
+                           --disable-iso8859-13 \
+                           --enable-iso8859-14 \
+                           --enable-iso8859-15 \
+                           --disable-iso8859-16 \
+                           --disable-koi8-r \
+                           --disable-jisx0201"
+
+PKG_MAKE_OPTS_TARGET="UTIL_DIR=$SYSROOT_PREFIX/usr/share/fonts/util/"
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/share/fonts/misc
+    cp 6x13-ISO8859-1.pcf.gz $INSTALL/usr/share/fonts/misc
+    mkfontdir $INSTALL/usr/share/fonts/misc
+}
