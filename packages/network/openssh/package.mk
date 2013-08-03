0b7d137ec1aedb746c8c25e10fdbb8435411f797
Stephan Raue <stephan@openelec.tv>
2013-08-03 22:09:14 +0200

    opnessh: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/network/openssh/package.mk
+++ packages/network/openssh/package.mk
@@ -26,12 +26,45 @@ PKG_LICENSE="OSS"
 PKG_SITE="http://www.openssh.com/"
 PKG_URL="ftp://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS="zlib openssl"
-PKG_BUILD_DEPENDS="toolchain zlib openssl"
+PKG_BUILD_DEPENDS_TARGET="toolchain zlib openssl"
 PKG_PRIORITY="optional"
 PKG_SECTION="network"
 PKG_SHORTDESC="openssh: An open re-implementation of the SSH package"
 PKG_LONGDESC="This is a Linux port of OpenBSD's excellent OpenSSH. OpenSSH is based on the last free version of Tatu Ylonen's SSH with all patent-encumbered algorithms removed, all known security bugs fixed, new features reintroduced, and many other clean-ups. SSH (Secure Shell) is a program to log into another computer over a network, to execute commands in a remote machine, and to move files from one machine to another. It provides strong authentication and secure communications over insecure channels. It is intended as a replacement for rlogin, rsh, rcp, and rdist."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
+PKG_CONFIGURE_OPTS_TARGET="--libexecdir=/usr/lib/openssh \
+                           --disable-strip \
+                           --disable-lastlog \
+                           --with-sandbox=no \
+                           --disable-utmp \
+                           --disable-utmpx \
+                           --disable-wtmp \
+                           --disable-wtmpx \
+                           --without-rpath \
+                           --with-ssl-engine \
+                           --without-pam"
+
+pre_configure_target() {
+  export LD="$TARGET_CC"
+  export LDFLAGS="$TARGET_CFLAGS $TARGET_LDFLAGS"
+}
+
+post_makeinstall_target() {
+  mkdir -p $INSTALL/etc
+    cp $PKG_DIR/config/ssh_config $INSTALL/etc
+    cp $PKG_DIR/config/sshd_config $INSTALL/etc
+
+  rm -rf $INSTALL/usr/lib/openssh/ssh-keysign
+  rm -rf $INSTALL/usr/lib/openssh/ssh-pkcs11-helper
+  if [ ! $SFTP_SERVER = "yes" ]; then
+    rm -rf $INSTALL/usr/lib/openssh/sftp-server
+  fi
+}
+
+post_install() {
+  add_user sshd x 74 74 "Privilege-separated SSH" "/var/empty/sshd" "/bin/sh"
+  add_group sshd 74
+}
