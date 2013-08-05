16d7b5a941cc14ca51a2f299f43d80dfaa0117b8
Stephan Raue <stephan@openelec.tv>
2013-08-05 10:41:52 +0200

    connman: fix package.mk script
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/network/connman/package.mk
+++ packages/network/connman/package.mk
@@ -90,7 +90,7 @@ PKG_MAKE_OPTS_TARGET="storagedir=/storage/.cache/connman \
 post_makeinstall_target() {
   rm -rf $INSTALL/lib/systemd
 
-  mkdir $INSTALL/usr/bin
+  mkdir -p $INSTALL/usr/bin
     cp -P client/connmanctl $INSTALL/usr/bin
 
   mkdir -p $INSTALL/etc
