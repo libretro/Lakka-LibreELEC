857d00971752ecaf93eb4ec722f42e8e7ecaf511
Stephan Raue <stephan@openelec.tv>
2013-08-07 02:16:12 +0200

    polkit: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/security/polkit/package.mk
+++ packages/security/polkit/package.mk
@@ -26,11 +26,31 @@ PKG_LICENSE="MIT"
 PKG_SITE="http://gitweb.freedesktop.org/?p=PolicyKit.git;a=summary"
 PKG_URL="http://hal.freedesktop.org/releases/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS="zlib glib expat"
-PKG_BUILD_DEPENDS="toolchain zlib sg3_utils glib expat"
+PKG_BUILD_DEPENDS_TARGET="toolchain zlib sg3_utils glib expat"
 PKG_PRIORITY="optional"
 PKG_SECTION="security"
 PKG_SHORTDESC="polkit: Authorization Toolkit"
 PKG_LONGDESC="PolicyKit is a toolkit for defining and handling authorizations. It is used for allowing unprivileged processes to speak to privileged processes."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
+
+PKG_CONFIGURE_OPTS_TARGET="--libexecdir=/usr/lib/polkit-1 \
+                           --disable-man-pages \
+                           --disable-gtk-doc \
+                           --disable-nls \
+                           --disable-introspection \
+                           --disable-systemd \
+                           --with-authfw=shadow \
+                           --with-os-type=redhat \
+                           --with-expat=$SYSROOT_PREFIX/usr"
+
+post_makeinstall_target() {
+  rm -rf $INSTALL/usr/bin/pk-example-frobnicate
+  rm -rf $INSTALL/usr/share/polkit-1/actions/org.freedesktop.policykit.examples.pkexec.policy
+}
+
+post_install() {
+  echo "chmod 4755 $INSTALL/usr/bin/pkexec" >> $FAKEROOT_SCRIPT
+  echo "chmod 4755 $INSTALL/usr/lib/polkit-1/polkit-agent-helper-1" >> $FAKEROOT_SCRIPT
+}
