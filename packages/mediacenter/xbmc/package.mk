baa27648973d4634bd8396210042ae3b16a83815
Stephan Raue <stephan@openelec.tv>
2013-08-19 16:50:37 +0200

    xbmc: update to xbmc-12.2-5ba69b6
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/xbmc/package.mk
+++ packages/mediacenter/xbmc/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="xbmc"
-PKG_VERSION="12.2-8e195d7"
+PKG_VERSION="12.2-5ba69b6"
 if [ "$XBMC" = "master" ]; then
   PKG_VERSION="13.alpha-df91888"
 elif [ "$XBMC" = "xbmc-aml" ]; then
