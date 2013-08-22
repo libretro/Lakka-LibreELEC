98dc9475a1795fba391932078b4514030fe1ee5c
Stephan Raue <stephan@openelec.tv>
2013-08-22 15:53:56 +0200

    libssh: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/network/libssh/package.mk
+++ packages/network/libssh/package.mk
@@ -26,11 +26,37 @@ PKG_LICENSE="OpenSource"
 PKG_SITE="http://www.libssh.org/"
 PKG_URL="http://www.libssh.org/files/0.5/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS="zlib libgcrypt"
-PKG_BUILD_DEPENDS="toolchain zlib libgcrypt"
+PKG_BUILD_DEPENDS_TARGET="toolchain zlib libgcrypt"
 PKG_PRIORITY="optional"
 PKG_SECTION="network"
 PKG_SHORTDESC="libssh: A working SSH implementation by means of a library"
 PKG_LONGDESC="The ssh library was designed to be used by programmers needing a working SSH implementation by the mean of a library. The complete control of the client is made by the programmer. With libssh, you can remotely execute programs, transfer files, use a secure and transparent tunnel for your remote programs. With its Secure FTP implementation, you can play with remote files easily, without third-party programs others than libcrypto (from openssl)."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+
+configure_target() {
+  cmake -DCMAKE_TOOLCHAIN_FILE=$CMAKE_CONF \
+        -DCMAKE_INSTALL_PREFIX=/usr \
+        -DWITH_STATIC_LIB=1 -DWITH_GCRYPT="ON" \
+        ..
+}
+
+makeinstall_target() {
+# install static library only
+  mkdir -p $SYSROOT_PREFIX/usr/lib
+    cp src/libssh.a $SYSROOT_PREFIX/usr/lib
+    cp src/threads/libssh_threads.a $SYSROOT_PREFIX/usr/lib
+
+  mkdir -p $SYSROOT_PREFIX/usr/lib/pkgconfig
+    cp libssh.pc $SYSROOT_PREFIX/usr/lib/pkgconfig
+    cp libssh_threads.pc $SYSROOT_PREFIX/usr/lib/pkgconfig
+
+  mkdir -p $SYSROOT_PREFIX/usr/include/libssh
+    cp ../include/libssh/callbacks.h $SYSROOT_PREFIX/usr/include/libssh
+    cp ../include/libssh/legacy.h $SYSROOT_PREFIX/usr/include/libssh
+    cp ../include/libssh/libssh.h $SYSROOT_PREFIX/usr/include/libssh
+    cp ../include/libssh/server.h $SYSROOT_PREFIX/usr/include/libssh
+    cp ../include/libssh/sftp.h $SYSROOT_PREFIX/usr/include/libssh
+    cp ../include/libssh/ssh2.h $SYSROOT_PREFIX/usr/include/libssh
+}
