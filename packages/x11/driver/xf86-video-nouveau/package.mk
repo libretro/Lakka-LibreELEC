f29273d82853b94beadb79049c6670deb5d6f476
Stephan Raue <stephan@openelec.tv>
2013-08-26 16:06:35 +0200

    xf86-video-nouveau: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/x11/driver/xf86-video-nouveau/package.mk
+++ packages/x11/driver/xf86-video-nouveau/package.mk
@@ -24,15 +24,20 @@ PKG_REV="1"
 PKG_ARCH="i386 x86_64"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.x.org/"
-# PKG_URL="http://xorg.freedesktop.org/archive/individual/driver/$PKG_NAME-$PKG_VERSION.tar.bz2"
-# PKG_URL="http://cgit.freedesktop.org/nouveau/xf86-video-nouveau/snapshot/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_URL="http://cgit.freedesktop.org/nouveau/xf86-video-nouveau/snapshot/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS="libXrandr libXrender libdrm libXext libpciaccess systemd Mesa glu"
-PKG_BUILD_DEPENDS="toolchain util-macros libXrandr libXrender libdrm libXext libpciaccess systemd Mesa glu xorg-server"
+PKG_BUILD_DEPENDS_TARGET="toolchain util-macros libXrandr libXrender libdrm libXext libpciaccess systemd Mesa glu xorg-server"
 PKG_PRIORITY="optional"
 PKG_SECTION="x11/driver"
 PKG_SHORTDESC="xf86-video-nouveau: Nouveau display driver (experimental)"
 PKG_LONGDESC="This driver for the X.Org X server (see xserver-xorg for a further description) provides support for NVIDIA Riva, TNT, GeForce, and Quadro cards. Although the nouveau project aims to provide full 3D support it is not yet complete, and these packages do not include any 3D support. Users requiring 3D support should use the non-free "nvidia" driver."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
+
+PKG_CONFIGURE_OPTS_TARGET="--with-xorg-module-dir=$XORG_PATH_MODULES"
+
+post_makeinstall_target() {
+  mkdir -p $INSTALL/etc/X11
+    cp $PKG_DIR/config/*.conf $INSTALL/etc/X11
+}
