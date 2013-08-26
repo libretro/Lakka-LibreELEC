e645044140da55f5afb9403946d0444418ac690e
Stephan Raue <stephan@openelec.tv>
2013-08-26 23:14:18 +0200

    kmod: convert to new package format, update to kmod-15
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/sysutils/kmod/package.mk
+++ packages/sysutils/kmod/package.mk
@@ -19,18 +19,46 @@
 ################################################################################
 
 PKG_NAME="kmod"
-PKG_VERSION="14"
+PKG_VERSION="15"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://git.profusion.mobi/cgit.cgi/kmod.git/"
 PKG_URL="http://ftp.kernel.org/pub/linux/utils/kernel/kmod/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS=""
-PKG_BUILD_DEPENDS="toolchain"
+PKG_BUILD_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
 PKG_SECTION="system"
 PKG_SHORTDESC="kmod offers the needed flexibility and fine grained control over insertion, removal, configuration and listing of kernel modules."
 PKG_LONGDESC="kmod offers the needed flexibility and fine grained control over insertion, removal, configuration and listing of kernel modules."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
+
+PKG_CONFIGURE_OPTS_TARGET="--enable-tools \
+                           --enable-logging \
+                           --disable-debug \
+                           --disable-gtk-doc \
+                           --disable-gtk-doc-html \
+                           --disable-gtk-doc-pdf \
+                           --disable-manpages \
+                           --with-gnu-ld \
+                           --without-xz \
+                           --without-zlib"
+
+post_makeinstall_target() {
+# make symlinks for compatibility
+  mkdir -p $INSTALL/sbin
+    ln -sf /usr/bin/kmod $INSTALL/sbin/lsmod
+    ln -sf /usr/bin/kmod $INSTALL/sbin/insmod
+    ln -sf /usr/bin/kmod $INSTALL/sbin/rmmod
+    ln -sf /usr/bin/kmod $INSTALL/sbin/modinfo
+    ln -sf /usr/bin/kmod $INSTALL/sbin/modprobe
+
+# add system modprobe.d dir
+  mkdir -p $INSTALL/etc/modprobe.d
+    cp $PKG_DIR/modprobe.d/* $INSTALL/etc/modprobe.d
+
+# add user modprobe.d dir
+  mkdir -p $INSTALL/usr/config/modprobe.d
+}
