30bde4e48c6f19cdfd200ff08fd1d4a907f852f4
Stephan Raue <stephan@openelec.tv>
2013-09-08 17:15:51 +0200

    libX11: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/x11/lib/libX11/package.mk
+++ packages/x11/lib/libX11/package.mk
@@ -26,11 +26,37 @@ PKG_LICENSE="OSS"
 PKG_SITE="http://www.x.org/"
 PKG_URL="http://xorg.freedesktop.org/archive/individual/lib/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS=""
-PKG_BUILD_DEPENDS="toolchain xproto:host xproto util-macros xextproto xcmiscproto bigreqsproto kbproto inputproto xtrans libXau libxcb util-macros"
+PKG_BUILD_DEPENDS_TARGET="toolchain xproto:host xproto util-macros xextproto xcmiscproto bigreqsproto kbproto inputproto xtrans libXau libxcb util-macros"
 PKG_PRIORITY="optional"
 PKG_SECTION="x11/lib"
 PKG_SHORTDESC="libx11: The X11 library"
 PKG_LONGDESC="LibX11 is the main X11 library containing all the client-side code to access the X11 windowing system."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
+
+PKG_CONFIGURE_OPTS_TARGET="--disable-secure-rpc \
+                           --enable-loadable-i18n \
+                           --enable-xthreads \
+                           --disable-xcms \
+                           --enable-xlocale \
+                           --disable-xlocaledir \
+                           --enable-xkb \
+                           --with-keysymdefdir=$SYSROOT_PREFIX/usr/include/X11 \
+                           --disable-xf86bigfont \
+                           --enable-malloc0returnsnull \
+                           --disable-specs \
+                           --without-xmlto \
+                           --without-fop \
+                           --enable-composecache \
+                           --disable-lint-library \
+                           --disable-ipv6 \
+                           --without-launchd \
+                           --without-lint"
+
+pre_configure_target() {
+  export CC_FOR_BUILD="$HOST_CC"
+  export CPPFLAGS_FOR_BUILD="$HOST_CPPFLAGS"
+  export CFLAGS_FOR_BUILD="$HOST_CFLAGS"
+  export LDFLAGS_FOR_BUILD="$HOST_LDFLAGS"
+}
