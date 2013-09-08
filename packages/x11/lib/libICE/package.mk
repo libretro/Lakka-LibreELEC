c87aab4fae4197bdad634369e316adb353b61f58
Stephan Raue <stephan@openelec.tv>
2013-09-08 17:02:43 +0200

    libICE: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/x11/lib/libICE/package.mk
+++ packages/x11/lib/libICE/package.mk
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
 #      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
@@ -20,19 +18,24 @@
 #  http://www.gnu.org/copyleft/gpl.html
 ################################################################################
 
-. config/options $1
+PKG_NAME="libICE"
+PKG_VERSION="1.0.8"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="OSS"
+PKG_SITE="http://www.X.org"
+PKG_URL="http://xorg.freedesktop.org/archive/individual/lib/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_DEPENDS=""
+PKG_BUILD_DEPENDS_TARGET="toolchain util-macros xtrans"
+PKG_PRIORITY="optional"
+PKG_SECTION="x11/lib"
+PKG_SHORTDESC="libICE: X Inter-Client Exchange (ICE) protocol library"
+PKG_LONGDESC="X Inter-Client Exchange (ICE) protocol library."
 
-cd $PKG_BUILD
-./configure --host=$TARGET_NAME \
-            --build=$HOST_NAME \
-            --prefix=/usr \
-            --sysconfdir=/etc \
-            --localstatedir=/var \
-            --enable-static \
-            --disable-shared \
-            --disable-ipv6 \
-            --without-xmlto
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="yes"
 
-make
-
-$MAKEINSTALL
+PKG_CONFIGURE_OPTS_TARGET="--enable-static \
+                           --disable-shared \
+                           --disable-ipv6 \
+                           --without-xmlto"
