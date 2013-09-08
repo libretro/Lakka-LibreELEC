bf9162c54818339eaada0723267b049e9597b105
Stephan Raue <stephan@openelec.tv>
2013-09-08 19:00:41 +0200

    pixman: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/x11/lib/pixman/package.mk
+++ packages/x11/lib/pixman/package.mk
@@ -26,11 +26,39 @@ PKG_LICENSE="OSS"
 PKG_SITE="http://www.x.org/"
 PKG_URL="http://xorg.freedesktop.org/archive/individual/lib/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS=""
-PKG_BUILD_DEPENDS="toolchain util-macros"
+PKG_BUILD_DEPENDS_TARGET="toolchain util-macros"
 PKG_PRIORITY="optional"
 PKG_SECTION="x11/lib"
 PKG_SHORTDESC="pixman: Pixel manipulation library"
 PKG_LONGDESC="Pixman is a generic library for manipulating pixel regions, contains low-level pixel manipulation routines and is used by both xorg and cairo."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
+
+if [ "$TARGET_ARCH" = arm ]; then
+  if [ "$TARGET_FPU" = neon -o "$TARGET_FPU" = neon-fp16 ]; then
+    PIXMAN_NEON="--enable-arm-neon"
+  else
+    PIXMAN_NEON="--disable-arm-neon"
+  fi
+  PIXMAN_CONFIG="--disable-mmx --disable-sse2 --disable-vmx --enable-arm-simd $PIXMAN_NEON --disable-arm-iwmmxt"
+elif [ "$TARGET_ARCH" = i386 -o "$TARGET_ARCH" = x86_64  ]; then
+  PIXMAN_CONFIG="--enable-mmx --enable-sse2 --disable-vmx --disable-arm-simd --disable-arm-neon"
+fi
+
+PKG_CONFIGURE_OPTS_TARGET="--disable-openmp \
+                           --disable-loongson-mmi \
+                           $PIXMAN_CONFIG \
+                           --disable-mips-dspr2 \
+                           --enable-gcc-inline-asm \
+                           --disable-timers \
+                           --disable-gtk \
+                           --disable-libpng \
+                           --with-gnu-ld"
+
+post_makeinstall_target() {
+  cp $SYSROOT_PREFIX/usr/lib/pkgconfig/pixman-1.pc \
+     $SYSROOT_PREFIX/usr/lib/pkgconfig/pixman.pc
+  cp -rf $SYSROOT_PREFIX/usr/include/pixman-1 \
+     $SYSROOT_PREFIX/usr/include/pixman
+}
