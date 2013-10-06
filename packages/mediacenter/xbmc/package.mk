54e6dd2290681453fc038ac893d82f4462163eab
Stefan Saraev <stefan@saraev.ca>
2013-10-06 15:25:13 +0300

    xbmc (Gotham): update to xbmc-13.alpha-3723806

--- packages/mediacenter/xbmc/package.mk
+++ packages/mediacenter/xbmc/package.mk
@@ -21,7 +21,7 @@
 PKG_NAME="xbmc"
 PKG_VERSION="12.2-49f61b4"
 if [ "$XBMC" = "master" ]; then
-  PKG_VERSION="13.alpha-9df3bc9"
+  PKG_VERSION="13.alpha-3723806"
 elif [ "$XBMC" = "xbmc-aml" ]; then
   PKG_VERSION="aml-frodo-d9119f2"
 fi
