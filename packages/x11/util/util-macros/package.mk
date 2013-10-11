52bf6019b215c0e18aa0beb68fa3bcb07a0a813d
Stefan Saraev <stefan@saraev.ca>
2013-10-11 20:15:26 +0300

    util-macros: convert to new package format.

--- packages/x11/util/util-macros/package.mk
+++ packages/x11/util/util-macros/package.mk
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
 #      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
@@ -20,14 +18,23 @@
 #  http://www.gnu.org/copyleft/gpl.html
 ################################################################################
 
-. config/options $1
+PKG_NAME="util-macros"
+PKG_VERSION="1.17.1"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="OSS"
+PKG_SITE="http://www.X.org"
+PKG_URL="http://xorg.freedesktop.org/archive/individual/util/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_DEPENDS=""
+PKG_BUILD_DEPENDS="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="x11/util"
+PKG_SHORTDESC="util-macros: X.org autoconf utilities"
+PKG_LONGDESC="X.org autoconf utilities such as M4 macros."
+PKG_IS_ADDON="no"
 
-cd $PKG_BUILD
-./configure --host=$TARGET_NAME \
-            --build=$HOST_NAME \
-            --prefix=/usr \
-            --sysconfdir=/etc \
-            --localstatedir=/var \
+PKG_AUTORECONF="yes"
 
-make
-$MAKEINSTALL
+post_makeinstall_target() {
+  rm -rf $INSTALL/usr
+}
