dea21ef7510f8bb9e367858e3a88f49f7ae53b58
Stefan Saraev <stefan@saraev.ca>
2013-10-11 19:47:04 +0300

    yaml: convert to new package format.

--- packages/web/yaml/package.mk
+++ packages/web/yaml/package.mk
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
 #      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
@@ -20,7 +18,22 @@
 #  http://www.gnu.org/copyleft/gpl.html
 ################################################################################
 
-. config/options $1
+PKG_NAME="yaml"
+PKG_VERSION="0.1.4"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="OSS"
+PKG_SITE="http://pyyaml.org"
+PKG_URL="http://pyyaml.org/download/libyaml/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_DEPENDS=""
+PKG_BUILD_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="web"
+PKG_SHORTDESC="yaml: a next generation YAML parser and emitter."
+PKG_LONGDESC="yaml is the next generation YAML parser and emitter."
 
-mkdir -p $INSTALL/usr/lib
-  cp $PKG_BUILD/src/.libs/*.so* $INSTALL/usr/lib
+PKG_IS_ADDON="no"
+
+PKG_AUTORECONF="yes"
+
+PKG_CONFIGURE_OPTS_TARGET="--disable-static --enable-shared"
