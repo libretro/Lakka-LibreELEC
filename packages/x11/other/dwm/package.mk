812c058c486e5eafbf3955cccc153e402c0d8d17
Stefan Saraev <stefan@saraev.ca>
2013-10-12 16:46:09 +0300

    new package: 'dwm'
    TESTING. a ratpoison replacement. set WINDOWMANAGER="dwm" to test it

---
+++ packages/x11/other/dwm/package.mk
@@ -0,0 +1,48 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.tv; see the file COPYING.  If not, write to
+#  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
+#  http://www.gnu.org/copyleft/gpl.html
+################################################################################
+
+PKG_NAME="dwm"
+PKG_VERSION="6.0"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="OSS"
+PKG_SITE="http://dwm.suckless.org/"
+PKG_URL="http://dl.suckless.org/dwm/dwm-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS="libX11 libXinerama"
+PKG_BUILD_DEPENDS_TARGET="toolchain libX11 libXinerama"
+PKG_PRIORITY="optional"
+PKG_SECTION="x11/other"
+PKG_SHORTDESC="dwm: dynamic window manager for X"
+PKG_LONGDESC="dwm is a dynamic window manager for X. It manages windows in tiled, monocle and floating layouts. All of the layouts can be applied dynamically, optimising the environment for the application in use and the task performed"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+make_target() {
+  $MAKE LDFLAGS="$LDFLAGS -lX11 -lXinerama -lXft -lfontconfig" CFLAGS="$CFLAGS -DXINERAMA -std=c99" CC="$CC"
+}
+
+configure_target() {
+  :
+}
+
+post_install() {
+  enable_service windowmanager.service
+}
