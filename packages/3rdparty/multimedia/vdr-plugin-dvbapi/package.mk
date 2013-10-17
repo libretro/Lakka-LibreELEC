1b657e5ad1bc2c21908624bec57d9ab047982ee4
Henk Wiedig <henk@wiedig.de>
2013-10-17 16:25:45 +0200

    vdr-plugin-dvbapi: convert to new package format.

--- packages/3rdparty/multimedia/vdr-plugin-dvbapi/package.mk
+++ packages/3rdparty/multimedia/vdr-plugin-dvbapi/package.mk
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
 #      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
@@ -20,22 +18,43 @@
 #  http://www.gnu.org/copyleft/gpl.html
 ################################################################################
 
-. config/options $1
+PKG_NAME="vdr-plugin-dvbapi"
+PKG_VERSION="2be5e15"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/manio/vdr-plugin-dvbapi"
+PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_DEPENDS_TARGET="vdr openssl"
+PKG_BUILD_DEPENDS_TARGET="toolchain vdr openssl libdvbcsa"
+PKG_PRIORITY="optional"
+PKG_SECTION="multimedia"
+PKG_SHORTDESC="TV"
+PKG_LONGDESC="TV"
 
-VDR_DIR=`basename $BUILD/vdr-[0-9]*`
+PKG_IS_ADDON="no"
 
-CFLAGS="$CFLAGS -fPIC"
-CXXFLAGS="$CXXFLAGS -fPIC"
-LDFLAGS="$LDFLAGS -fPIC"
+PKG_AUTORECONF="no"
 
-# dont build parallel
+VDR_DIR=$(basename $BUILD/vdr-[0-9]*)
+PKG_MAKE_OPTS_TARGET="VDRDIR=$ROOT/$BUILD/$VDR_DIR \
+                      VDRSRC=$ROOT/$BUILD/$VDR_DIR \
+                      LIBDIR=\".\" \
+                      LOCALEDIR=\"./locale\" \
+                      LIBDVBCSA=1"
+
+pre_configure_target() {
+  CFLAGS="$CFLAGS -fPIC"
+  CXXFLAGS="$CXXFLAGS -fPIC"
+  LDFLAGS="$LDFLAGS -fPIC"
+  CSAFLAGS="$CFLAGS -Wall -fomit-frame-pointer -fexpensive-optimizations -funroll-loops"
+}
+
+pre_make_target() {
+  # dont build parallel
   MAKEFLAGS=-j1
+}
 
-cd $PKG_BUILD
-  make VDRDIR="../$VDR_DIR" \
-       VDRSRC="../$VDR_DIR" \
-       LIBDIR="." \
-       LOCALEDIR="./locale" \
-       LIBDVBCSA=1 \
-       CSAFLAGS="$CFLAGS -Wall -fomit-frame-pointer -fexpensive-optimizations -funroll-loops"
-
+makeinstall_target() {
+  : # installation not needed, done by create-addon script
+}
