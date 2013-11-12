4945364a87c127930e9553219a07c6536a1b4c93
Stephan Raue <stephan@openelec.tv>
2013-11-12 21:38:50 +0100

    util-linux: update to util-linux-2.24
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/sysutils/util-linux/package.mk
+++ packages/sysutils/util-linux/package.mk
@@ -19,11 +19,11 @@
 ################################################################################
 
 PKG_NAME="util-linux"
-PKG_VERSION="2.23.2"
+PKG_VERSION="2.24"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_URL="http://www.kernel.org/pub/linux/utils/util-linux/v2.23/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_URL="http://www.kernel.org/pub/linux/utils/util-linux/v2.24/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS=""
 PKG_BUILD_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
