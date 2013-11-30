ec23ba9501288c47373ca5818c49d8e1722680ec
Stefan Saraev <stefan@saraev.ca>
2013-11-30 16:53:14 +0200

    bluez: update to bluez-5.11

--- packages/network/bluez/package.mk
+++ packages/network/bluez/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="bluez"
-PKG_VERSION="5.10"
+PKG_VERSION="5.11"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
