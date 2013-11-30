0be5e72e5719e281ccd63a40e431622ea051e998
Stefan Saraev <stefan@saraev.ca>
2013-11-30 12:43:23 +0200

    xbmc: update to xbmc-13.alpha-f1e1102

--- packages/mediacenter/xbmc/package.mk
+++ packages/mediacenter/xbmc/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="xbmc"
-PKG_VERSION="13.alpha-81a9c0c"
+PKG_VERSION="13.alpha-f1e1102"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
