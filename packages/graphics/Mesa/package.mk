b2e1dd195bd62f2277a2e79891067fd05abf9aa0
Stephan Raue <stephan@openelec.tv>
2013-12-13 15:36:50 +0100

    Mesa: update to Mesa-10.0.1
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/graphics/Mesa/package.mk
+++ packages/graphics/Mesa/package.mk
@@ -19,12 +19,12 @@
 ################################################################################
 
 PKG_NAME="Mesa"
-PKG_VERSION="10.0.0"
+PKG_VERSION="10.0.1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
-PKG_URL="ftp://freedesktop.org/pub/mesa/10.0/MesaLib-$PKG_VERSION.tar.bz2"
+PKG_URL="ftp://freedesktop.org/pub/mesa/$PKG_VERSION/MesaLib-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS="libXdamage libdrm expat libXext libXfixes libX11"
 PKG_BUILD_DEPENDS_TARGET="toolchain Python-host makedepend:host libxml2-host expat glproto dri2proto libdrm libXext libXdamage libXfixes libXxf86vm libxcb libX11"
 PKG_PRIORITY="optional"
