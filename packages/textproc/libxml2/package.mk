148fb46640ae628de33733019026c44d2eb127cd
Stefan Saraev <stefan@saraev.ca>
2013-12-14 20:35:24 +0200

    libxml2: convert to new package format

--- packages/textproc/libxml2/package.mk
+++ packages/textproc/libxml2/package.mk
@@ -18,19 +18,45 @@
 #  http://www.gnu.org/copyleft/gpl.html
 ################################################################################
 
-PKG_NAME="libxml2-host"
-PKG_VERSION=""
+PKG_NAME="libxml2"
+PKG_VERSION="2.9.1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
 PKG_SITE="http://xmlsoft.org"
-PKG_URL=""
-PKG_DEPENDS=""
-PKG_BUILD_DEPENDS="toolchain zlib:host Python-host"
+PKG_URL="ftp://xmlsoft.org/libxml2/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_DEPENDS="zlib"
+PKG_BUILD_DEPENDS_TARGET="toolchain zlib"
 PKG_PRIORITY="optional"
 PKG_SECTION="textproc"
 PKG_SHORTDESC="libxml: XML parser library for Gnome"
 PKG_LONGDESC="The libxml package contains an XML library, which allows you to manipulate XML files. XML (eXtensible Markup Language) is a data format for structured document interchange via the Web."
 PKG_IS_ADDON="no"
 
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
+
+PKG_CONFIGURE_OPTS_HOST="--prefix=$ROOT/$TOOLCHAIN \
+             --disable-silent-rules \
+             --disable-ipv6 \
+             --with-python \
+             --with-zlib \
+             --without-lzma"
+
+PKG_CONFIGURE_OPTS_TARGET="--enable-static \
+             --enable-shared \
+             --disable-silent-rules \
+             --enable-ipv6 \
+             --without-python \
+             --with-zlib \
+             --with-sysroot=$SYSROOT_PREFIX \
+             --without-lzma"
+
+post_makeinstall_target() {
+  $SED "s:\(['= ]\)/usr:\\1$SYSROOT_PREFIX/usr:g" \
+    $SYSROOT_PREFIX/usr/bin/xml2-config
+
+  mv $SYSROOT_PREFIX/usr/bin/xml2-config $ROOT/$TOOLCHAIN/bin
+
+  rm -rf $INSTALL/usr/bin
+  rm -rf $INSTALL/usr/lib/xml2Conf.sh
+}
