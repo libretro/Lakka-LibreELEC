bc459cf9cb400500d2822ab244e8a5ab8eb0510a
Stephan Raue <stephan@openelec.tv>
2013-12-17 00:51:11 +0100

    v86d: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/sysutils/v86d/package.mk
+++ packages/sysutils/v86d/package.mk
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
 #      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
@@ -20,11 +18,27 @@
 #  http://www.gnu.org/copyleft/gpl.html
 ################################################################################
 
-. config/options $1
+PKG_NAME="v86d"
+PKG_VERSION="0.1.10"
+PKG_REV="1"
+PKG_ARCH="i386 x86_64"
+PKG_LICENSE="GPL"
+PKG_SITE="http://dev.gentoo.org/~spock/projects/uvesafb/"
+PKG_URL="http://dev.gentoo.org/~spock/projects/uvesafb/archive/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_DEPENDS=""
+PKG_BUILD_DEPENDS_INIT="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="system"
+PKG_SHORTDESC="v86d: A userspace helper that runs x86 code in an emulated environment."
+PKG_LONGDESC="v86d is the userspace helper that runs x86 code in an emulated environment. uvesafb will not work without v86d. v86d currently supports the x86 and amd64 (x86-64) architectures."
 
-$SCRIPTS/unpack linux
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
 
-cd $PKG_BUILD
-./configure --with-x86emu
+INIT_CONFIGURE_OPTS="--with-x86emu"
 
-make
+pre_configure_init() {
+# v86d fails to build in subdirs
+  cd $ROOT/$PKG_BUILD
+    rm -rf .$TARGET_NAME-init
+}
