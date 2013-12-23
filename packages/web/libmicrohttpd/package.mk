86ebf0e96d042617337519013881c93b613ae1be
Stephan Raue <stephan@openelec.tv>
2013-12-23 22:26:27 +0100

    libmicrohttpd: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/web/libmicrohttpd/package.mk
+++ packages/web/libmicrohttpd/package.mk
@@ -20,15 +20,25 @@ PKG_NAME="libmicrohttpd"
 PKG_VERSION="0.9.33"
 PKG_REV="1"
 PKG_ARCH="any"
-PKG_LICENSE="GPL"
+PKG_LICENSE="LGPL"
 PKG_SITE="http://www.gnu.org/software/libmicrohttpd/"
 PKG_URL="ftp://ftp.gnu.org/gnu/libmicrohttpd/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS=""
-PKG_BUILD_DEPENDS="toolchain"
+PKG_DEPENDS="libgcrypt"
+PKG_BUILD_DEPENDS="toolchain libgcrypt"
 PKG_PRIORITY="optional"
 PKG_SECTION="web"
 PKG_SHORTDESC="libmicrohttpd: a small webserver C library"
 PKG_LONGDESC="GNU libmicrohttpd is a small C library that is supposed to make it easy to run an HTTP server as part of another application."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
+
+PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
+                           --enable-static \
+                           --disable-curl \
+                           --disable-https \
+                           --with-libgcrypt-prefix=$SYSROOT_PREFIX/usr"
+
+post_makeinstall_target() {
+  rm -rf $INSTALL/usr/bin
+}
