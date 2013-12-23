5942809d6c59890f45041ba34138f5dede98924c
Stephan Raue <stephan@openelec.tv>
2013-12-23 22:14:41 +0100

    nettle: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/security/nettle/package.mk
+++ packages/security/nettle/package.mk
@@ -24,11 +24,22 @@ PKG_LICENSE="GPL"
 PKG_SITE="http://www.lysator.liu.se/~nisse/nettle"
 PKG_URL="http://www.lysator.liu.se/~nisse/archive/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS="gmp"
-PKG_BUILD_DEPENDS="toolchain gmp"
+PKG_BUILD_DEPENDS_TARGET="toolchain gmp"
 PKG_PRIORITY="optional"
 PKG_SECTION="security"
 PKG_SHORTDESC="nettle: a cryptographic library"
 PKG_LONGDESC="Nettle is a cryptographic library that is designed to fit easily in more or less any context: In crypto toolkits for object-oriented languages (C++, Python, Pike, ...), in applications like LSH or GNUPG, or even in kernel space. In most contexts, you need more than the basic cryptographic algorithms, you also need some way to keep track of available algorithms, their properties and variants. You often have some algorithm selection process, often dictated by a protocol you want to implement."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
+
+PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-openssl"
+
+pre_configure_target() {
+# dont build parallel
+  MAKEFLAGS=-j1
+}
+
+post_makeinstall_target() {
+  rm -rf $INSTALL/usr/bin
+}
