33bf44231f20ac8f8e9512b109089ea2b3fb8574
Stephan Raue <stephan@openelec.tv>
2013-12-23 22:14:08 +0100

    curl: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

---
+++ packages/web/curl/package.mk
@@ -0,0 +1,99 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
+#
+#  OpenELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  OpenELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="curl"
+# curl-7.32.0 breaks XBMC ftp sources support, please test before upgrade curl!
+PKG_VERSION="7.31.0"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="MIT"
+PKG_SITE="http://curl.haxx.se"
+PKG_URL="http://curl.haxx.se/download/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_DEPENDS="zlib gnutls rtmpdump"
+PKG_BUILD_DEPENDS_TARGET="toolchain zlib gnutls rtmpdump"
+PKG_PRIORITY="optional"
+PKG_SECTION="web"
+PKG_SHORTDESC="curl: Client and library for (HTTP, HTTPS, FTP, ...) transfers"
+PKG_LONGDESC="Curl is a client to get documents/files from or send documents to a server, using any of the supported protocols (HTTP, HTTPS, FTP, FTPS, GOPHER, DICT, TELNET, LDAP or FILE). The command is designed to work without user interaction or any kind of interactivity."
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="yes"
+
+PKG_CONFIGURE_OPTS_TARGET="ac_cv_lib_rtmp_RTMP_Init=yes \
+                           ac_cv_header_librtmp_rtmp_h=yes \
+                           --disable-debug \
+                           --enable-optimize \
+                           --enable-warnings \
+                           --disable-curldebug \
+                           --disable-ares \
+                           --enable-largefile \
+                           --enable-http \
+                           --enable-ftp \
+                           --enable-file \
+                           --disable-ldap \
+                           --disable-ldaps \
+                           --disable-rtsp \
+                           --enable-proxy \
+                           --disable-dict \
+                           --disable-telnet \
+                           --disable-tftp \
+                           --disable-pop3 \
+                           --disable-imap \
+                           --disable-smtp \
+                           --disable-gophper \
+                           --disable-manual \
+                           --enable-libgcc \
+                           --enable-ipv6 \
+                           --enable-versioned-symbols \
+                           --enable-nonblocking \
+                           --enable-threaded-resolver \
+                           --enable-verbose \
+                           --disable-sspi \
+                           --enable-crypto-auth \
+                           --enable-cookies \
+                           --enable-hidden-symbols \
+                           --disable-soname-bump \
+                           --with-gnu-ld \
+                           --without-krb4 \
+                           --without-spnego \
+                           --without-gssapi \
+                           --with-zlib \
+                           --without-egd-socket \
+                           --enable-thread \
+                           --with-random=/dev/urandom \
+                           --with-gnutls=$SYSROOT_PREFIX/usr \
+                           --without-ssl \
+                           --without-polarssl \
+                           --without-nss \
+                           --with-ca-bundle=$SSL_CERTIFICATES/cacert.pem \
+                           --without-ca-path \
+                           --without-libssh2 \
+                           --with-librtmp=$SYSROOT_PREFIX/usr \
+                           --without-libidn"
+
+pre_configure_target() {
+# link against librt because of undefined reference to 'clock_gettime'
+  export LIBS="-lrt -lm -lrtmp"
+}
+
+post_makeinstall_target() {
+  rm -rf $INSTALL/usr/bin/curl-config
+
+  sed -e "s:\(['= ]\)/usr:\\1$SYSROOT_PREFIX/usr:g" -i $SYSROOT_PREFIX/usr/bin/curl-config
+  mv $SYSROOT_PREFIX/usr/bin/curl-config $ROOT/$TOOLCHAIN/bin
+}
