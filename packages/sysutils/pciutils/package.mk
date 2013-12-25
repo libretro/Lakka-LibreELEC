39a7ad9776e0358ccf8a144c635a8a828e99303b
Stephan Raue <stephan@openelec.tv>
2013-12-25 20:27:06 +0100

    pciutils: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/sysutils/pciutils/package.mk
+++ packages/sysutils/pciutils/package.mk
@@ -24,11 +24,45 @@ PKG_LICENSE="GPL"
 PKG_SITE="http://mj.ucw.cz/pciutils.shtml"
 PKG_URL="http://www.kernel.org/pub/software/utils/pciutils/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS=""
-PKG_BUILD_DEPENDS="toolchain"
+PKG_BUILD_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
 PKG_SECTION="system"
 PKG_SHORTDESC="pciutils: Linux PCI Utilities"
 PKG_LONGDESC="This package contains various utilities for inspecting and setting of devices connected to the PCI bus and the PCI vendor/product ID database."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+
+PKG_MAKE_OPTS="PREFIX=/usr SHARED=yes STRIP= IDSDIR=/usr/share"
+
+pre_make_target() {
+# pciutils fails building with LTO support
+  strip_lto
+}
+
+make_target() {
+  make OPT="$CFLAGS" \
+       CROSS_COMPILE=${TARGET_PREFIX} \
+       HOST=$TARGET_ARCH-linux \
+       $PKG_MAKE_OPTS \
+       ZLIB=no \
+       DNS=no
+}
+
+makeinstall_target() {
+  make $PKG_MAKE_OPTS DESTDIR=$SYSROOT_PREFIX install
+  make $PKG_MAKE_OPTS DESTDIR=$SYSROOT_PREFIX install-lib
+  make $PKG_MAKE_OPTS DESTDIR=$INSTALL install-lib
+  if [ "$TARGET_ARCH" = i386 -o "$TARGET_ARCH" = x86_64 ]; then
+    make $PKG_MAKE_OPTS DESTDIR=$INSTALL install
+  fi
+}
+
+post_makeinstall_target() {
+  rm -rf $INSTALL/usr/bin/setpci
+  rm -rf $INSTALL/usr/bin/update-pciids
+  rm -rf $INSTALL/usr/share
+
+  mkdir -p $INSTALL/usr/share
+    cp $PKG_DIR/config/pci.ids $INSTALL/usr/share
+}
