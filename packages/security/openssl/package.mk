e88c3fed803313d4e8886280854f29702e169f2c
Stephan Raue <stephan@openelec.tv>
2013-12-25 15:30:02 +0100

    ca-certification: merge with package 'openssl'
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/security/openssl/package.mk
+++ packages/security/openssl/package.mk
@@ -23,7 +23,7 @@ PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.openssl.org/"
 PKG_URL="http://www.openssl.org/source/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS="zlib ca-certification"
+PKG_DEPENDS="zlib"
 PKG_BUILD_DEPENDS_TARGET="toolchain zlib"
 PKG_PRIORITY="optional"
 PKG_SECTION="security"
@@ -99,4 +99,10 @@ post_makeinstall_target() {
   rm -rf $INSTALL/etc/pki/tls/misc
   rm -rf $INSTALL/usr/bin/c_rehash
   $STRIP $INSTALL/usr/bin/openssl
+
+# ca-certification: provides a tool to download and create ca-bundle.crt
+# download url: http://curl.haxx.se
+# create new cert: perl ./mk-ca-bundle.pl
+  mkdir -p $INSTALL/$SSL_CERTIFICATES
+    cp $PKG_DIR/cert/ca-bundle.crt $INSTALL/$SSL_CERTIFICATES/cacert.pem
 }
