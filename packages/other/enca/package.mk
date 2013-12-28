c6940a40d8a792b8cf2023a0c899cd612fc8fc0f
Stephan Raue <stephan@openelec.tv>
2013-12-28 21:54:27 +0100

    enca: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/other/enca/package.mk
+++ packages/other/enca/package.mk
@@ -24,11 +24,39 @@ PKG_LICENSE="GPL"
 PKG_SITE="http://freshmeat.net/projects/enca/"
 PKG_URL="http://dl.cihar.com/enca/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS=""
-PKG_BUILD_DEPENDS="toolchain"
+PKG_BUILD_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
 PKG_SECTION="other"
 PKG_SHORTDESC="enca: detects the encoding of text files, on the basis of knowledge of their language."
 PKG_LONGDESC="Enca detects the encoding of text files, on the basis of knowledge of their language. It can also convert them to other encodings, allowing you to recode files without knowing their current encoding. It supports most of Central and East European languages, and a few Unicode variants, independently on language."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
+
+export CFLAGS="$CFLAGS -fPIC -DPIC"
+
+PKG_MAKEINSTALL_OPTS_TARGET="-C lib"
+PKG_CONFIGURE_OPTS_TARGET="ac_cv_file__dev_random=yes \
+                           ac_cv_file__dev_urandom=no \
+                           ac_cv_file__dev_srandom=no \
+                           ac_cv_file__dev_arandom=no \
+                           CPPFLAGS="-I$SYSROOT_PREFIX/usr/include" \
+                           --disable-shared \
+                           --enable-static \
+                           --disable-external \
+                           --without-librecode \
+                           --disable-rpath \
+                           --with-gnu-ld"
+
+pre_make_target() {
+  make CC="$HOST_CC" \
+       CPPFLAGS="$HOST_CPPFLAGS" \
+       CFLAGS="$HOST_CFLAGS" \
+       LDFLAGS="$HOST_LDFLAGS" \
+       -C tools
+}
+
+post_makeinstall_target() {
+  mkdir -p $SYSROOT_PREFIX/usr/lib/pkgconfig
+    cp enca.pc $SYSROOT_PREFIX/usr/lib/pkgconfig
+}
