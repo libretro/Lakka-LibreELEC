7961429af96d82b8ea039e4bd1be3f675834c0a6
Stephan Raue <stephan@openelec.tv>
2013-12-28 01:26:28 +0100

    libcap: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/devel/libcap/package.mk
+++ packages/devel/libcap/package.mk
@@ -25,14 +25,36 @@ PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE=""
-#PKG_URL="http://www.kernel.org/pub/linux/libs/security/linux-privs/libcap2/$PKG_NAME-$PKG_VERSION.tar.bz2"
-PKG_URL="http://www.mirrorservice.org/sites/ftp.kernel.org/pub/linux/libs/security/linux-privs/libcap2/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_URL="http://www.kernel.org/pub/linux/libs/security/linux-privs/libcap2/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS=""
 PKG_BUILD_DEPENDS="toolchain attr"
 PKG_PRIORITY="optional"
 PKG_SECTION="devel"
 PKG_SHORTDESC="libcap: A library for getting and setting POSIX.1e capabilities"
 PKG_LONGDESC="As of Linux 2.2.0, the power of the superuser has been partitioned into a set of discrete capabilities (in other places, these capabilities are know as privileges). The contents of the libcap package are a library and a number of simple programs that are intended to show how an application/daemon can be protected (with wrappers) or rewritten to take advantage of this fine grained approach to constraining the danger to your system from programs running as 'root'."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+
+make_target() {
+  make CC=$TARGET_CC \
+       AR=$TARGET_AR \
+       RANLIB=$TARGET_RANLIB \
+       CFLAGS="$TARGET_CFLAGS" \
+       BUILD_CC=$HOST_CC \
+       BUILD_CFLAGS="$HOST_CFLAGS -I$ROOT/$PKG_BUILD/libcap/include" \
+       PAM_CAP=no \
+       lib=/usr/lib \
+       -C libcap libcap.pc libcap.a
+}
+
+makeinstall_target() {
+  mkdir -p $SYSROOT_PREFIX/usr/lib
+    cp libcap/libcap.a $SYSROOT_PREFIX/usr/lib
+
+  mkdir -p $SYSROOT_PREFIX/usr/lib/pkgconfig
+    cp libcap/libcap.pc $SYSROOT_PREFIX/usr/lib/pkgconfig
+
+  mkdir -p $SYSROOT_PREFIX/usr/include/sys
+    cp libcap/include/sys/capability.h $SYSROOT_PREFIX/usr/include/sys
+}
