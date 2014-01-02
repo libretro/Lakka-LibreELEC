d4975750e7bb4845416553da734d5fb576a53d9a
Stephan Raue <stephan@openelec.tv>
2014-01-02 22:14:42 +0100

    xf86-video-virtualbox: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/x11/driver/xf86-video-virtualbox/package.mk
+++ packages/x11/driver/xf86-video-virtualbox/package.mk
@@ -22,13 +22,35 @@ PKG_REV="1"
 PKG_ARCH="i386 x86_64"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.virtualbox.org"
-PKG_URL="${DISTRO_SRC}/${PKG_NAME}-${PKG_VERSION}-${TARGET_ARCH}.tar.bz2"
-PKG_DEPENDS="libXcomposite libXdamage libXfixes libXext libX11"
-PKG_BUILD_DEPENDS="toolchain libXcomposite libXdamage libXfixes libXext libX11 libxcb libXau"
+PKG_DEPENDS_TARGET="libXcomposite libXdamage libXfixes libXext libX11"
+PKG_BUILD_DEPENDS_TARGET="toolchain libXcomposite libXdamage libXfixes libXext libX11 libxcb libXau"
 PKG_PRIORITY="optional"
 PKG_SECTION="x11/driver"
 PKG_SHORTDESC="xf86-video-virtualbox: The Xorg driver for virtualbox video"
 PKG_LONGDESC="xf86-video-virtualbox: The Xorg driver for virtualbox video"
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+
+unpack() {
+  mkdir -p $PKG_BUILD
+    tar jxpf $SOURCES/$PKG_NAME/$PKG_NAME-$PKG_VERSION-$TARGET_ARCH.tar.bz2 -C $PKG_BUILD
+}
+
+make_target() {
+  : # nothing todo
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/$XORG_PATH_MODULES/drivers
+    cp -P lib/VBoxGuestAdditions/vboxvideo_drv_114.so $INSTALL/$XORG_PATH_MODULES/drivers/vboxvideo_drv.so
+
+  mkdir -p $INSTALL/usr/lib/dri
+    cp -P lib/VBoxOGL.so $INSTALL/usr/lib/dri/vboxvideo_dri.so
+
+  mkdir -p $INSTALL/usr/lib
+    cp -aP lib/* $INSTALL/usr/lib
+
+  mkdir -p $INSTALL/etc/X11
+    cp $PKG_DIR/config/*.conf $INSTALL/etc/X11
+}
