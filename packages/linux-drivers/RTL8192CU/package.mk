2334df39d220e4974ec1f39f4557c164f5ec973c
Stephan Raue <stephan@openelec.tv>
2014-01-04 21:41:29 +0100

    RTL8192CU: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/linux-drivers/RTL8192CU/package.mk
+++ packages/linux-drivers/RTL8192CU/package.mk
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
 #      Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
@@ -18,7 +16,37 @@
 #  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-. config/options $1
+PKG_NAME="RTL8192CU"
+PKG_VERSION="v3.4.4_4749.20121105"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://www.realtek.com.tw/downloads/downloadsView.aspx?Langid=1&PFid=48&Level=5&Conn=4&ProdID=274&DownTypeID=3&GetDown=false&Downloads=true"
+PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_DEPENDS_TARGET=""
+PKG_BUILD_DEPENDS_TARGET="toolchain linux"
+PKG_NEED_UNPACK="$LINUX_DEPENDS"
+PKG_PRIORITY="optional"
+PKG_SECTION="driver"
+PKG_SHORTDESC="Realtek RTL81xxCU Linux 3.x driver"
+PKG_LONGDESC="Realtek RTL81xxCU Linux 3.x driver"
 
-mkdir -p $INSTALL/lib/modules/`kernel_version`/RTL8188CU
-  cp $PKG_BUILD/*.ko $INSTALL/lib/modules/`kernel_version`/RTL8188CU
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+pre_make_target() {
+  unset LDFLAGS
+}
+
+make_target() {
+  make V=1 \
+       ARCH=$TARGET_ARCH \
+       KSRC=$(kernel_path) \
+       CROSS_COMPILE=$TARGET_PREFIX \
+       CONFIG_POWER_SAVING=n
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/lib/modules/`kernel_version`/$PKG_NAME
+    cp *.ko $INSTALL/lib/modules/`kernel_version`/$PKG_NAME
+}
