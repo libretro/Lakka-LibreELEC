88051e83d00d51c71cdc58fc380e2ef8d83dabce
Stephan Raue <stephan@openelec.tv>
2014-01-04 21:41:29 +0100

    bcm_sta: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/linux-drivers/bcm_sta/package.mk
+++ packages/linux-drivers/bcm_sta/package.mk
@@ -16,6 +16,9 @@
 #  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
+# Downloadlocation:
+# http://www.broadcom.com/support/802.11/linux_sta.php
+
 PKG_NAME="bcm_sta"
 PKG_VERSION="6.20.155.1"
 PKG_REV="1"
@@ -23,16 +26,29 @@ PKG_ARCH="i386 x86_64"
 PKG_LICENSE="nonfree"
 PKG_SITE="http://www.broadcom.com/"
 PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS=""
-PKG_BUILD_DEPENDS="toolchain linux"
+PKG_DEPENDS_TARGET=""
+PKG_BUILD_DEPENDS_TARGET="toolchain linux"
 PKG_NEED_UNPACK="$LINUX_DEPENDS"
 PKG_PRIORITY="optional"
 PKG_SECTION="driver"
 PKG_SHORTDESC="bcm_sta: Broadcom's BCM4311-, BCM4312-, BCM4313-, BCM4321-, BCM4322-, BCM43224-, and BCM43225-based WLAN driver"
 PKG_LONGDESC="These packages contain Broadcom's IEEE 802.11a/b/g/n hybrid Linux® device driver for use with Broadcom's BCM4311-, BCM4312-, BCM4313-, BCM4321-, BCM4322-, BCM43224-, and BCM43225-based hardware. There are different tars for 32-bit and 64-bit x86 CPU architectures. Make sure that you download the appropriate tar because the hybrid binary file must be of the appropriate architecture type. The hybrid binary file is agnostic to the specific version of the Linux kernel because it is designed to perform all interactions with the operating system through operating-system-specific files and an operating system abstraction layer file. All Linux operating-system-specific code is provided in source form, making it possible to retarget to different kernel versions and fix operating system related issues."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-# Downloadlocation:
-# http://www.broadcom.com/support/802.11/linux_sta.php
+if [ "$TARGET_ARCH" = "i386" ]; then
+  DRIVER_ARCH="x86-32"
+elif [ "$TARGET_ARCH" = "x86_64" ]; then
+  DRIVER_ARCH="x86-64"
+fi
+
+make_target() {
+  cd $DRIVER_ARCH
+    KBUILD_NOPEDANTIC=1 make V=1 CC=$CC -C $(kernel_path) M=`pwd` BINARCH=$TARGET_ARCH
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/lib/modules/`kernel_version`/bcm_sta
+    cp *.ko $INSTALL/lib/modules/`kernel_version`/$PKG_NAME
+}
