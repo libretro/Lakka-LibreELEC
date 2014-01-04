d7eee97cf4cdf793bf4ed4febec1d5e227de1eff
Stefan Saraev <stefan@saraev.ca>
2014-01-04 18:53:12 +0200

    new package: 'wireless-regdb'

---
+++ packages/network/wireless-regdb/package.mk
@@ -0,0 +1,42 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
+#
+#  OpenELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  OpenELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="wireless-regdb"
+PKG_VERSION="2013.11.27"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://wireless.kernel.org/en/developers/Regulatory"
+PKG_URL="https://www.kernel.org/pub/software/network/$PKG_NAME/$PKG_NAME-2013.11.27.tar.gz"
+PKG_DEPENDS=""
+PKG_BUILD_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="network"
+PKG_SHORTDESC="wireless-regdb: regulatory database"
+PKG_LONGDESC="wireless-regdb is a regulatory database"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+make_target() {
+  : # nothing to do
+}
+
+makeinstall_target() {
+  : # nothing to do
+}
