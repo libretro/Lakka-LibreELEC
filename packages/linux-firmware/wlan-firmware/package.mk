b024a0ce0e3fdae41b7ad93e03d1704360098604
Stephan Raue <stephan@openelec.tv>
2014-01-05 11:02:55 +0100

    wlan-firmware: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/linux-firmware/wlan-firmware/package.mk
+++ packages/linux-firmware/wlan-firmware/package.mk
@@ -23,12 +23,20 @@ PKG_ARCH="any"
 PKG_LICENSE="Free-to-use"
 PKG_SITE="https://github.com/OpenELEC/wlan-firmware"
 PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS=""
-PKG_BUILD_DEPENDS="toolchain"
+PKG_DEPENDS_TARGET=""
+PKG_BUILD_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
 PKG_SECTION="firmware"
 PKG_SHORTDESC="wlan-firmware: firmwares for various WLAN drivers"
 PKG_LONGDESC="wlan-firmware: firmwares for various WLAN drivers"
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+
+make_target() {
+  : # nothing todo
+}
+
+makeinstall_target() {
+  DESTDIR=$INSTALL ./install
+}
