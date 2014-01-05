dc9b54d055159a47b937c7ee82bde44b46d3fa71
Stephan Raue <stephan@openelec.tv>
2014-01-05 16:03:40 +0100

    binutils: convert to new package format, move package
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/devel/binutils/package.mk
+++ packages/devel/binutils/package.mk
@@ -22,16 +22,54 @@ PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/software/binutils/binutils.html"
-#PKG_URL="http://www.mirrorservice.org/sites/ftp.kernel.org/pub/linux/devel/binutils/$PKG_NAME-$PKG_VERSION.tar.bz2"
-#PKG_URL="ftp://ftp.pgpi.com/linux/kernel/pub/linux/devel/binutils/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_URL="http://ftp.gnu.org/gnu/binutils/$PKG_NAME-$PKG_VERSION.tar.gz"
-#PKG_URL="ftp://ftp.kernel.org/pub/linux/devel/binutils/$PKG_NAME-$PKG_VERSION.tar.bz2"
-PKG_DEPENDS=""
-PKG_BUILD_DEPENDS="ccache bison:host flex linux:host gmp:host mpfr cloog ppl:host"
+PKG_DEPENDS_HOST=""
+PKG_BUILD_DEPENDS_HOST="ccache bison:host flex linux:host gmp:host mpfr cloog ppl:host"
 PKG_PRIORITY="optional"
 PKG_SECTION="toolchain/devel"
 PKG_SHORTDESC="binutils: A GNU collection of binary utilities"
 PKG_LONGDESC="The GNU binutils are utilities of use when dealing with object files. the packages includes ld - the GNU linker, as - the GNU assembler, addr2line - converts addresses into filenames and line numbers, ar - a utility for creating, modifying and extracting from archives, c++filt - filter to demangle encoded C++ symbols, gprof - displays profiling information, nlmconv - converts object code into an NLM, nm - lists symbols from object files, objcopy - Copys and translates object files, objdump - displays information from object files, ranlib - generates an index to the contents of an archive, readelf - displays information from any ELF format object file, size - lists the section sizes of an object or archive file, strings - lists printable strings from files, strip - discards symbols as well as windres - a compiler for Windows resource files."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+
+PKG_CONFIGURE_OPTS_HOST="--target=$TARGET_NAME \
+                         --with-sysroot=$SYSROOT_PREFIX \
+                         --with-lib-path=$SYSROOT_PREFIX/lib:$SYSROOT_PREFIX/usr/lib \
+                         --with-gmp=$ROOT/$TOOLCHAIN \
+                         --with-mpfr=$ROOT/$TOOLCHAIN \
+                         --with-ppl=$ROOT/$TOOLCHAIN \
+                         --with-cloog=$ROOT/$TOOLCHAIN \
+                         --disable-werror \
+                         --disable-multilib \
+                         --disable-libada \
+                         --disable-libssp \
+                         --disable-ppl-version-check \
+                         --enable-cloog-backend=isl \
+                         --enable-version-specific-runtime-libs \
+                         --enable-plugins \
+                         --enable-gold \
+                         --enable-ld=default \
+                         --enable-lto \
+                         --disable-nls"
+
+if [ "$TARGET_ARCH" = "x86_64" ]; then
+  PKG_CONFIGURE_OPTS_HOST="$PKG_CONFIGURE_OPTS_HOST --enable-64-bit-bfd"
+fi
+
+pre_configure_host() {
+  unset CPPFLAGS
+  unset CFLAGS
+  unset CXXFLAGS
+  unset LDFLAGS
+}
+
+make_host() {
+  make configure-host
+  make
+}
+
+makeinstall_host() {
+  cp -v ../include/libiberty.h $SYSROOT_PREFIX/usr/include
+  make install
+}
