bc19a0c01e16b07ff44b2b7197827f8a6697aa4e
Stephan Raue <stephan@openelec.tv>
2014-01-08 18:33:56 +0100

    cmake: convert to new package format, move package
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/devel/cmake/package.mk
+++ packages/devel/cmake/package.mk
@@ -23,12 +23,69 @@ PKG_ARCH="any"
 PKG_LICENSE="BSD"
 PKG_SITE="http://www.cmake.org/"
 PKG_URL="http://www.cmake.org/files/v2.8/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS=""
-PKG_BUILD_DEPENDS="ccache:host"
+PKG_DEPENDS_HOST=""
+PKG_BUILD_DEPENDS_HOST="ccache:host"
 PKG_PRIORITY="optional"
 PKG_SECTION="toolchain/devel"
 PKG_SHORTDESC="cmake: A cross-platform, open-source make system"
 PKG_LONGDESC="CMake is used to control the software compilation process using simple platform and compiler independent configuration files. CMake generates native makefiles and workspaces that can be used in the compiler environment of your choice. CMake is quite sophisticated: it is possible to support complex environments requiring system configuration, preprocessor generation, code generation, and template instantiation."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+
+configure_host() {
+  ../configure --no-qt-gui
+}
+
+makeinstall_host() {
+  mkdir -p $ROOT/$TOOLCHAIN/bin
+    cp -P bin/cmake $ROOT/$TOOLCHAIN/bin
+    cp -P bin/cpack $ROOT/$TOOLCHAIN/bin
+    cp -P bin/ctest $ROOT/$TOOLCHAIN/bin
+
+  mkdir -p $ROOT/$TOOLCHAIN/etc
+
+  cat >$ROOT/$TOOLCHAIN/etc/cmake-$TARGET_NAME.conf <<EOF
+# this one is important
+SET(CMAKE_SYSTEM_NAME Linux)
+#this one not so much
+SET(CMAKE_SYSTEM_VERSION 1)
+
+# specify the cross compiler
+SET(CMAKE_C_COMPILER   $TARGET_CC)
+SET(CMAKE_CXX_COMPILER $TARGET_CXX)
+
+# where is the target environment 
+SET(CMAKE_FIND_ROOT_PATH  $SYSROOT_PREFIX)
+
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+EOF
+
+  cat >$ROOT/$TOOLCHAIN/etc/cmake-$HOST_NAME.conf <<EOF
+# this one is important
+SET(CMAKE_SYSTEM_NAME Linux)
+#this one not so much
+SET(CMAKE_SYSTEM_VERSION 1)
+
+# specify the host compiler
+SET(CMAKE_C_COMPILER   $HOST_CC)
+SET(CMAKE_CXX_COMPILER $HOST_CXX)
+
+# where is the target environment 
+SET(CMAKE_FIND_ROOT_PATH  $ROOT/$TOOLCHAIN)
+
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM BOTH)
+
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
+
+EOF
+}
