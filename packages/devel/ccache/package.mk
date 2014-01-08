d766cefd3aedcef17fddc2e3cc7189e20e7464e3
Stephan Raue <stephan@openelec.tv>
2014-01-08 12:46:11 +0100

    ccache: convert to new package format, move package
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/devel/ccache/package.mk
+++ packages/devel/ccache/package.mk
@@ -23,12 +23,34 @@ PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://ccache.samba.org/"
 PKG_URL="http://samba.org/ftp/ccache/$PKG_NAME-$PKG_VERSION.tar.bz2"
-PKG_DEPENDS=""
-PKG_BUILD_DEPENDS=""
+PKG_DEPENDS_HOST=""
+PKG_BUILD_DEPENDS_HOST=""
 PKG_PRIORITY="optional"
-PKG_SECTION="toolchain/devel"
+PKG_SECTION="devel"
 PKG_SHORTDESC="ccache: A fast compiler cache"
 PKG_LONGDESC="Ccache is a compiler cache. It speeds up re-compilation of C/C++ code by caching previous compiles and detecting when the same compile is being done again."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+
+export CC=$LOCAL_CC
+
+makeinstall_host() {
+  mkdir -p $ROOT/$TOOLCHAIN/bin
+    cp -f ccache $ROOT/$TOOLCHAIN/bin
+
+# setup ccache
+  $ROOT/$TOOLCHAIN/bin/ccache --max-size=$CCACHE_CACHE_SIZE
+
+  cat > $HOST_CC <<EOF
+#!/bin/sh
+$ROOT/$TOOLCHAIN/bin/ccache $LOCAL_CC "\$@"
+EOF
+  chmod +x $HOST_CC
+
+  cat > $HOST_CXX <<EOF
+#!/bin/sh
+$ROOT/$TOOLCHAIN/bin/ccache $LOCAL_CXX "\$@"
+EOF
+  chmod +x $HOST_CXX
+}
