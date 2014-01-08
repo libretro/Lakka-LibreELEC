7c1aa0f12a3e6e7d4ac86a61847ffe3f2622ef2b
Stephan Raue <stephan@openelec.tv>
2014-01-08 21:14:30 +0100

    scons: convert to new package format, move package
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/devel/scons/package.mk
+++ packages/devel/scons/package.mk
@@ -29,6 +29,14 @@ PKG_PRIORITY="optional"
 PKG_SECTION="python/devel"
 PKG_SHORTDESC="SCons: an Open Source software construction tool—that is, a next-generation build tool."
 PKG_LONGDESC="SCons is an Open Source software construction tool—that is, a next-generation build tool."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+
+make_host() {
+ : # nothing todo
+}
+
+makeinstall_host() {
+  python setup.py install --prefix=$ROOT/$TOOLCHAIN
+}
