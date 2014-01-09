b3fd5fec54bc3af83455ad45be65758a446f234b
Stephan Raue <stephan@openelec.tv>
2014-01-09 23:50:28 +0100

    intltool: convert to new package format, move package
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/devel/intltool/package.mk
+++ packages/devel/intltool/package.mk
@@ -23,12 +23,17 @@ PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnome.org"
 PKG_URL="http://launchpad.net/intltool/trunk/$PKG_VERSION/+download/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS=""
-PKG_BUILD_DEPENDS="ccache:host"
+PKG_DEPENDS_HOST=""
+PKG_BUILD_DEPENDS_HOST="ccache:host"
 PKG_PRIORITY="optional"
-PKG_SECTION="toolchain/devel"
+PKG_SECTION="devel"
 PKG_SHORTDESC="intltool: Gnome international tools"
 PKG_LONGDESC="The Gnome international tools help to handle translation strings from various source files (.xml.in, .glade, .desktop.in, .server.in, .oaf.in)."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+
+post_makeinstall_host() {
+  mkdir -p  $SYSROOT_PREFIX/usr/share/aclocal
+    cp ../intltool.m4 $SYSROOT_PREFIX/usr/share/aclocal
+}
