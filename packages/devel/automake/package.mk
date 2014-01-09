014d1849f87d9b246c90809bb5c10cd9215e0a48
Stephan Raue <stephan@openelec.tv>
2014-01-09 23:47:33 +0100

    automake: convert to new package format, move package
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/devel/automake/package.mk
+++ packages/devel/automake/package.mk
@@ -23,12 +23,19 @@ PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://sources.redhat.com/automake/"
 PKG_URL="http://ftp.gnu.org/gnu/automake/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS=""
-PKG_BUILD_DEPENDS="ccache:host autoconf"
+PKG_DEPENDS_HOST=""
+PKG_BUILD_DEPENDS_HOST="ccache:host autoconf:host"
 PKG_PRIORITY="optional"
 PKG_SECTION="toolchain/devel"
 PKG_SHORTDESC="automake: A GNU tool for automatically creating Makefiles"
 PKG_LONGDESC="This is Automake, a Makefile generator. It was inspired by the 4.4BSD make and include files, but aims to be portable and to conform to the GNU standards for Makefile variables and targets. Automake is a Perl script. The input files are called Makefile.am. The output files are called Makefile.in; they are intended for use with Autoconf. Automake requires certain things to be done in your configure.in."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+
+PKG_CONFIGURE_OPTS_HOST="--target=$TARGET_NAME --disable-silent-rules"
+
+post_makeinstall_host() {
+  make prefix=$SYSROOT_PREFIX/usr install
+  cp -P $PKG_DIR/files/*.m4 $SYSROOT_PREFIX/usr/share/aclocal
+}
