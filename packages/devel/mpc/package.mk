02ad10307006284d6baf1e815a69d8405d00c59c
Stephan Raue <stephan@openelec.tv>
2014-01-09 17:48:20 +0100

    mpc: convert to new package format, move package
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/devel/mpc/package.mk
+++ packages/devel/mpc/package.mk
@@ -23,12 +23,18 @@ PKG_ARCH="any"
 PKG_LICENSE="LGPL"
 PKG_SITE="http://www.multiprecision.org"
 PKG_URL="http://www.multiprecision.org/mpc/download/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS=""
-PKG_BUILD_DEPENDS="ccache:host gmp:host mpfr"
+PKG_DEPENDS_HOST=""
+PKG_BUILD_DEPENDS_HOST="ccache:host gmp:host mpfr"
 PKG_PRIORITY="optional"
-PKG_SECTION="toolchain/math"
+PKG_SECTION="devel"
 PKG_SHORTDESC="mpc: A C library for the arithmetic of high precision complex numbers"
 PKG_LONGDESC="pc is a C library for the arithmetic of complex numbers with arbitrarily high precision and correct rounding of the result. It is built upon and follows the same principles as Mpfr."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+
+PKG_CONFIGURE_OPTS_HOST="--target=$TARGET_NAME \
+                         --enable-shared \
+                         --disable-static \
+                         --with-gmp=$ROOT/$TOOLCHAIN \
+                         --with-mpfr=$ROOT/$TOOLCHAIN"
