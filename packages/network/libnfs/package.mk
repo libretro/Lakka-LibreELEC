f1cf1ae3648c45a76b0bac2b39d45b3fd880a72e
Stephan Raue <stephan@openelec.tv>
2014-01-11 14:07:28 +0100

    libnfs: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/network/libnfs/package.mk
+++ packages/network/libnfs/package.mk
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
 #      Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
@@ -18,8 +16,28 @@
 #  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-. config/options $1
+PKG_NAME="libnfs"
+PKG_VERSION="1.8.0"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/sahlberg/libnfs"
+PKG_URL="https://sites.google.com/site/libnfstarballs/li/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET=""
+PKG_BUILD_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="network"
+PKG_SHORTDESC="libnfs: a client library for accessing NFS shares over a network."
+PKG_LONGDESC="LIBNFS is a client library for accessing NFS shares over a network."
 
-mkdir -p $INSTALL/usr/lib
-  cp -P $PKG_BUILD/lib/.libs/*.so* $INSTALL/usr/lib/
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="yes"
 
+PKG_CONFIGURE_OPTS_TARGET="--disable-examples --disable-tirpc"
+
+pre_configure_target() {
+# dont build parallel
+  MAKEFLAGS=-j1
+
+  export CFLAGS="$CFLAGS -D_FILE_OFFSET_BITS=64 -I$ROOT/$PKG_BUILD/mount -I$ROOT/$PKG_BUILD/nfs"
+}
