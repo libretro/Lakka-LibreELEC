3aee0704311dc488623573f928609381e2b6ad0e
Stefan Saraev <stefan@saraev.ca>
2014-01-11 16:29:10 +0200

    libmpeg2: convert to new package format

--- packages/multimedia/libmpeg2/package.mk
+++ packages/multimedia/libmpeg2/package.mk
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
 #      Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
@@ -18,17 +16,26 @@
 #  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-. config/options $1
+PKG_NAME="libmpeg2"
+PKG_VERSION="0.5.1"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://libmpeg2.sourceforge.net/"
+PKG_URL="http://libmpeg2.sourceforge.net/files/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET=""
+PKG_BUILD_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="multimedia"
+PKG_SHORTDESC="libmpeg2: The MPEG Library - version 2"
+PKG_LONGDESC="The MPEG Library is a collection of C routines to decode MPEG-1 and MPEG-2 movies and dither them in a variety of colour schemes."
+PKG_IS_ADDON="no"
 
-cd $PKG_BUILD
-./configure --host=$TARGET_NAME \
-            --build=$HOST_NAME \
-            --prefix=/usr \
-            --disable-static \
-            --enable-shared \
-            --disable-sdl \
-            --without-x
+PKG_AUTORECONF="yes"
 
-make
+PKG_CONFIGURE_OPTS_TARGET="--disable-sdl --without-x"
 
-$MAKEINSTALL
+post_makeinstall_target() {
+  rm -rf $INSTALL/usr/bin
+}
+
