b9b520cd63df8ff01671436c78c14a831728cdc2
Stephan Raue <stephan@openelec.tv>
2014-01-12 03:17:30 +0100

    libplist: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/devel/libplist/package.mk
+++ packages/devel/libplist/package.mk
@@ -23,12 +23,30 @@ PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://matt.colyer.name/projects/iphone-linux/"
 PKG_URL="http://www.libimobiledevice.org/downloads/$PKG_NAME-$PKG_VERSION.tar.bz2"
-PKG_DEPENDS="libxml2 glib"
-PKG_BUILD_DEPENDS="toolchain libxml2 glib"
+PKG_DEPENDS_TARGET="libxml2 glib"
+PKG_BUILD_DEPENDS_TARGET="toolchain libxml2 glib"
 PKG_PRIORITY="optional"
 PKG_SECTION="devel"
 PKG_SHORTDESC="libplist: a library for manipulating Apple Binary and XML Property Lists"
 PKG_LONGDESC="libplist is a library for manipulating Apple Binary and XML Property Lists"
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+
+pre_configure_target() {
+# dont build parallel
+  MAKEFLAGS=-j1
+}
+
+configure_target() {
+  cmake -DCMAKE_TOOLCHAIN_FILE=$CMAKE_CONF \
+        -DCMAKE_INSTALL_PREFIX=/usr \
+        -DENABLE_PYTHON="OFF" \
+        -DENABLE_CYTHON="OFF" \
+        -DENABLE_SWIG="OFF" \
+        ..
+}
+
+post_makeinstall_target() {
+  rm -rf $INSTALL/usr/bin
+}
