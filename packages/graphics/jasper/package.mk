df65f1899f893f9b56409857d5f30a010f1d5636
Stephan Raue <stephan@openelec.tv>
2014-01-17 14:45:17 +0100

    jasper: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/graphics/jasper/package.mk
+++ packages/graphics/jasper/package.mk
@@ -23,12 +23,22 @@ PKG_ARCH="any"
 PKG_LICENSE="OpenSource"
 PKG_SITE="http://www.ece.uvic.ca/~mdadams/jasper/"
 PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.bz2"
-PKG_DEPENDS=""
-PKG_BUILD_DEPENDS="toolchain libjpeg-turbo"
+PKG_DEPENDS_TARGET=""
+PKG_BUILD_DEPENDS_TARGET="toolchain libjpeg-turbo"
 PKG_PRIORITY="optional"
 PKG_SECTION="graphics"
 PKG_SHORTDESC="jasper: JPEG-2000 Part-1 standard (i.e., ISO/IEC 15444-1) implementation"
 PKG_LONGDESC="This distribution contains the public release of the an open-source implementation of the ISO/IEC 15444-1 also known as JPEG-2000 standard for image compression."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
+
+PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-static"
+
+pre_configure_target() {
+  export CFLAGS="$CFLAGS -fPIC -DPIC"
+}
+
+post_makeinstall_target() {
+  rm -rf $INSTALL/usr/bin
+}
