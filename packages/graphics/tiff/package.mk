a32c3cbb3468bd17c95a6525be2cfdc7105fdbe6
Stephan Raue <stephan@openelec.tv>
2014-01-17 14:56:10 +0100

    tiff: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/graphics/tiff/package.mk
+++ packages/graphics/tiff/package.mk
@@ -24,12 +24,29 @@ PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.remotesensing.org/libtiff/"
 PKG_URL="http://download.osgeo.org/libtiff/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS="zlib"
-PKG_BUILD_DEPENDS="toolchain libjpeg-turbo zlib"
+PKG_DEPENDS_TARGET="zlib"
+PKG_BUILD_DEPENDS_TARGET="toolchain libjpeg-turbo zlib"
 PKG_PRIORITY="optional"
 PKG_SECTION="graphics"
 PKG_SHORTDESC="libtiff: A library for reading and writing TIFF files"
 PKG_LONGDESC="libtiff is a library for reading and writing data files encoded with the Tag Image File format, Revision 6.0 (or revision 5.0 or revision 4.0). This file format is suit- able for archiving multi-color and monochromatic image data."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
+
+PKG_CONFIGURE_OPTS_TARGET="--enable-static \
+                           --disable-shared \
+                           --disable-mdi \
+                           --enable-cxx \
+                           --with-jpeg-lib-dir=$SYSROOT_PREFIX/usr/lib \
+                           --with-jpeg-include-dir=$SYSROOT_PREFIX/usr/include \
+                           --without-x"
+
+pre_configure_target() {
+  export CFLAGS="$CFLAGS -fPIC -DPIC"
+  export CXXFLAGS="$CXXFLAGS -fPIC -DPIC"
+}
+
+post_makeinstall_target() {
+  rm -rf $INSTALL/usr/bin
+}
