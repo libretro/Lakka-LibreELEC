b24ead1fc74d69ccb60f86bc59f6bfb59daa2511
Stefan Saraev <stefan@saraev.ca>
2014-01-18 17:58:55 +0200

    bluez: update to bluez-5.13

--- packages/network/bluez/package.mk
+++ packages/network/bluez/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="bluez"
-PKG_VERSION="5.12"
+PKG_VERSION="5.13"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
