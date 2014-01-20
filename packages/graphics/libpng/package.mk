d4259ac6e902c61c330051283a7d5069f2e75e55
Stephan Raue <stephan@openelec.tv>
2014-01-20 22:57:14 +0100

    libpng: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/graphics/libpng/package.mk
+++ packages/graphics/libpng/package.mk
@@ -23,12 +23,37 @@ PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.libpng.org/"
 PKG_URL="$SOURCEFORGE_SRC/libpng/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS="zlib"
-PKG_BUILD_DEPENDS="toolchain zlib"
+PKG_DEPENDS_HOST=""
+PKG_DEPENDS_TARGET="zlib"
+PKG_BUILD_DEPENDS_HOST="toolchain zlib:host"
+PKG_BUILD_DEPENDS_TARGET="toolchain zlib"
 PKG_PRIORITY="optional"
 PKG_SECTION="graphics"
 PKG_SHORTDESC="libpng: Portable Network Graphics (PNG) Reference Library"
 PKG_LONGDESC="PNG (Portable Network Graphics) is an extensible file format for the lossless, portable, well-compressed storage of raster images. PNG provides a patent-free replacement for GIF and can also replace many common uses of TIFF. Indexed-color, grayscale, and truecolor images are supported, plus an optional alpha channel. Sample depths range from 1 to 16 bits."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
+
+PKG_CONFIGURE_OPTS_TARGET="ac_cv_lib_z_zlibVersion=yes \
+                           --enable-static \
+                           --disable-shared"
+
+pre_configure_host() {
+  export CPPFLAGS="$CPPFLAGS -I$ROOT/$TOOLCHAIN/include"
+}
+
+pre_configure_target() {
+  export CFLAGS="$CFLAGS -fPIC -DPIC"
+  export CPPFLAGS="$CPPFLAGS -I$SYSROOT_PREFIX/usr/include"
+}
+
+post_makeinstall_target() {
+  sed -e "s:\([\"'= ]\)/usr:\\1$SYSROOT_PREFIX/usr:g" \
+      -e "s:libs=\"-lpng15\":libs=\"-lpng15 -lz\":g" \
+      -i $SYSROOT_PREFIX/usr/bin/libpng*-config
+
+  cp -P $SYSROOT_PREFIX/usr/bin/libpng*-config $ROOT/$TOOLCHAIN/bin
+
+  rm -rf $INSTALL/usr/bin
+}
