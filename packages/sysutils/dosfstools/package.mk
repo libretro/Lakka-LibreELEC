dcd03a89f8d27a56a17608e9fc5492e80ec2309d
Stephan Raue <stephan@openelec.tv>
2014-01-20 17:50:51 +0100

    dosfstools: update to dosfstools-3.0.25
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/sysutils/dosfstools/package.mk
+++ packages/sysutils/dosfstools/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="dosfstools"
-PKG_VERSION="3.0.24"
+PKG_VERSION="3.0.25"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
