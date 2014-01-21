23b851c9bc626bc059004d5eb7a8c1fbc8e8f8cd
Stephan Raue <stephan@openelec.tv>
2014-01-21 23:58:14 +0100

    dvbhdhomerun: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/linux-drivers/dvbhdhomerun/package.mk
+++ packages/linux-drivers/dvbhdhomerun/package.mk
@@ -25,12 +25,41 @@ PKG_SITE="http://sourceforge.net/projects/dvbhdhomerun/"
 PKG_URL="${DISTRO_SRC}/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 #PKG_URL="$SOURCEFORGE_SRC/project/dvbhdhomerun/${PKG_NAME}_${PKG_VERSION}.tar.gz"
 #PKG_SOURCE_DIR="${PKG_NAME}_${PKG_VERSION}"
-PKG_DEPENDS=""
+PKG_DEPENDS="libhdhomerun"
 PKG_BUILD_DEPENDS="toolchain linux libhdhomerun"
 PKG_NEED_UNPACK="$LINUX_DEPENDS"
 PKG_PRIORITY="optional"
 PKG_SECTION="driver/dvb"
 PKG_SHORTDESC="A linux DVB driver for the HDHomeRun TV tuner (http://www.silicondust.com)."
 PKG_LONGDESC="A linux DVB driver for the HDHomeRun TV tuner (http://www.silicondust.com)."
+
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+
+configure_target() {
+  ( cd kernel
+    LDFLAGS="" make dvb_hdhomerun KERNEL_DIR=$(kernel_path)
+    fix_module_depends dvb_hdhomerun_core.ko "dvb_core"
+  )
+
+# absolute path
+  LIBHDHOMERUN_PATH=$(ls -d $ROOT/$BUILD/libhdhomerun-*/)
+  sed -i "s|SET(LIBHDHOMERUN_PATH .*)|SET(LIBHDHOMERUN_PATH $LIBHDHOMERUN_PATH)|g" userhdhomerun/CMakeLists.txt
+  sed -i "s|/etc/dvbhdhomerun|/tmp/dvbhdhomerun|g" userhdhomerun/hdhomerun_tuner.cpp
+  sed -i "s|/etc/dvbhdhomerun|/tmp/dvbhdhomerun|g" userhdhomerun/hdhomerun_controller.cpp
+
+  mkdir -p .$TARGET_NAME && cd .$TARGET_NAME
+  cmake -DCMAKE_TOOLCHAIN_FILE=$CMAKE_CONF \
+        -DCMAKE_INSTALL_PREFIX=/usr \
+        -DLIBHDHOMERUN_PATH=$(ls -d $ROOT/$BUILD/libhdhomerun-*/) \
+        ../userhdhomerun
+}
+
+makeinstall_target() {
+  cd $ROOT/$PKG_BUILD
+    mkdir -p $INSTALL/lib/modules/$(get_module_dir)/hdhomerun
+      cp kernel/*.ko $INSTALL/lib/modules/$(get_module_dir)/hdhomerun/
+
+    mkdir -p $INSTALL/usr/bin
+      cp -PR .$TARGET_NAME/userhdhomerun $INSTALL/usr/bin
+}
