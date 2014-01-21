8b7c9b835803a14037241a1b8cd69e8e7c898ac9
Stephan Raue <stephan@openelec.tv>
2014-01-21 16:55:13 +0100

    pycrypto: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/python/security/pycrypto/package.mk
+++ packages/python/security/pycrypto/package.mk
@@ -23,12 +23,36 @@ PKG_ARCH="any"
 PKG_LICENSE="LGPL"
 PKG_SITE="https://www.dlitz.net/software/pycrypto/"
 PKG_URL="http://pypi.python.org/packages/source/p/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS="Python distutilscross:host"
-PKG_BUILD_DEPENDS="toolchain Python distutilscross:host"
+PKG_DEPENDS_TARGET="Python distutilscross:host"
+PKG_BUILD_DEPENDS_TARGET="toolchain Python distutilscross:host"
 PKG_PRIORITY="optional"
 PKG_SECTION="python/security"
 PKG_SHORTDESC="PyCrypto - The Python Cryptography Toolkit"
 PKG_LONGDESC="The Python Cryptography Toolkit describes a package containing various cryptographic modules for the Python programming language"
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+
+PKG_CONFIGURE_OPTS_TARGET="ac_cv_func_realloc_0_nonnull=yes \
+                           ac_cv_func_malloc_0_nonnull=yes \
+                           --without-gmp --without-mpir"
+
+pre_configure_target() {
+  cd $ROOT/$PKG_BUILD
+  rm -rf .$TARGET_NAME
+
+  export PYTHONXCPREFIX="$SYSROOT_PREFIX/usr"
+  export LDFLAGS="$LDFLAGS -L$SYSROOT_PREFIX/usr/lib -L$SYSROOT_PREFIX/lib"
+}
+
+make_target() {
+  python setup.py build --cross-compile
+}
+
+makeinstall_target() {
+  python setup.py install --root=$INSTALL --prefix=/usr
+}
+
+post_makeinstall_target() {
+  find $INSTALL/usr/lib -name "*.py" -exec rm -rf "{}" ";"
+}
