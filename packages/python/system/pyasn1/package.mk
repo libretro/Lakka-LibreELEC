21b49bf325632058efb4faa0b35c66311c5ac850
Stephan Raue <stephan@openelec.tv>
2014-01-21 17:31:30 +0100

    pyasn1: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/python/system/pyasn1/package.mk
+++ packages/python/system/pyasn1/package.mk
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
 #      Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
@@ -18,6 +16,35 @@
 #  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-. config/options $1
+PKG_NAME="pyasn1"
+PKG_VERSION="0.0.13b"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="BSD"
+PKG_SITE="http://sourceforge.net/projects/pyasn1/"
+PKG_URL="$SOURCEFORGE_SRC/pyasn1/pyasn1-devel/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="Python"
+PKG_BUILD_DEPENDS_TARGET="toolchain Python distutilscross:host"
+PKG_PRIORITY="optional"
+PKG_SECTION="python/system"
+PKG_SHORTDESC="pyasn1: ASN.1 types and codecs"
+PKG_LONGDESC="pyasn1: ASN.1 types and codecs."
 
-cp -PR $PKG_BUILD/.install/* $INSTALL
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+pre_make_target() {
+  export PYTHONXCPREFIX="$SYSROOT_PREFIX/usr"
+}
+
+make_target() {
+  python setup.py build --cross-compile
+}
+
+makeinstall_target() {
+  python setup.py install --root=$INSTALL --prefix=/usr
+}
+
+post_makeinstall_target() {
+  find $INSTALL/usr/lib -name "*.py" -exec rm -rf "{}" ";"
+}
