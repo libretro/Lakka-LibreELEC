235b4d838d448eea6194c67752bd7eaa9285cdb0
Stephan Raue <stephan@openelec.tv>
2014-01-21 16:28:03 +0100

    pygobject: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/python/devel/pygobject/package.mk
+++ packages/python/devel/pygobject/package.mk
@@ -23,12 +23,22 @@ PKG_ARCH="any"
 PKG_LICENSE="LGPL"
 PKG_SITE="http://www.pygtk.org/"
 PKG_URL="http://ftp.gnome.org/pub/GNOME/sources/pygobject/2.28/$PKG_NAME-$PKG_VERSION.tar.bz2"
-PKG_DEPENDS="Python glib libffi"
-PKG_BUILD_DEPENDS="toolchain Python glib libffi"
+PKG_DEPENDS_TARGET="Python glib libffi"
+PKG_BUILD_DEPENDS_TARGET="toolchain Python glib libffi"
 PKG_PRIORITY="optional"
 PKG_SECTION="python/devel"
 PKG_SHORTDESC="pygobject: The Python bindings for GObject"
 PKG_LONGDESC="PyGObject provides a convenient wrapper for the GObject+ library for use in Python programs, and takes care of many of the boring details such as managing memory and type casting. When combined with PyGTK, PyORBit and gnome-python, it can be used to write full featured Gnome applications."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
+
+PKG_CONFIGURE_OPTS_TARGET="--enable-thread --disable-introspection"
+
+post_makeinstall_target() {
+  find $INSTALL/usr/lib -name *.py -exec rm -rf "{}" ";"
+  find $INSTALL/usr/lib -name *.pyc -exec rm -rf "{}" ";"
+
+  rm -rf $INSTALL/usr/bin
+  rm -rf $INSTALL/usr/share/pygobject
+}
