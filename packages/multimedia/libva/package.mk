8320e10c796fb738b8943e602524d09635ae50ce
Stephan Raue <stephan@openelec.tv>
2014-01-21 19:12:13 +0100

    libva: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/multimedia/libva/package.mk
+++ packages/multimedia/libva/package.mk
@@ -23,20 +23,44 @@ PKG_ARCH="i386 x86_64"
 PKG_LICENSE="GPL"
 PKG_SITE="http://freedesktop.org/wiki/Software/vaapi"
 PKG_URL="http://cgit.freedesktop.org/vaapi/libva/snapshot/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS="libX11 libXext libXfixes libdrm Mesa glu"
-PKG_BUILD_DEPENDS="toolchain libX11 libXext libXfixes libdrm Mesa glu"
+PKG_DEPENDS_TARGET="libX11 libXext libXfixes libdrm Mesa glu"
+PKG_BUILD_DEPENDS_TARGET="toolchain libX11 libXext libXfixes libdrm Mesa glu"
 PKG_PRIORITY="optional"
 PKG_SECTION="multimedia"
 PKG_SHORTDESC="libva: The main motivation for VAAPI (Video Acceleration API) is to enable hardware accelerated video decode/encode at various entry-points (VLD, IDCT, Motion Compensation etc.) for the prevailing coding standards today (MPEG-2, MPEG-4 ASP/H.263, MPEG-4 AVC/H.264, and VC-1/VMW3)."
 PKG_LONGDESC="The main motivation for VAAPI (Video Acceleration API) is to enable hardware accelerated video decode/encode at various entry-points (VLD, IDCT, Motion Compensation etc.) for the prevailing coding standards today (MPEG-2, MPEG-4 ASP/H.263, MPEG-4 AVC/H.264, and VC-1/VMW3). Extending XvMC was considered, but due to its original design for MPEG-2 MotionComp only, it made more sense to design an interface from scratch that can fully expose the video decode capabilities in today's GPUs."
-PKG_IS_ADDON="no"
 
-PKG_AUTORECONF="no"
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="yes"
+
+PKG_CONFIGURE_OPTS_TARGET="--disable-silent-rules \
+                           --disable-docs \
+                           --enable-drm \
+                           --enable-x11 \
+                           --enable-glx \
+                           --disable-egl \
+                           --disable-wayland \
+                           --disable-dummy-driver \
+                           --with-drivers-path=/usr/lib/va"
 
 get_graphicdrivers
 
 for drv in $GRAPHIC_DRIVERS; do
   if [ "$drv" = i965 ]; then
-    PKG_DEPENDS="$PKG_DEPENDS libva-driver-intel"
+    PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET libva-driver-intel"
   fi
 done
+
+pre_configure_target() {
+# todo: libva fails to build in subdirs
+  cd $ROOT/$PKG_BUILD
+  rm -rf .$TARGET_NAME
+}
+
+post_makeinstall_target() {
+  rm -rf $INSTALL/usr/bin
+  if [ "$DEVTOOLS" = yes ]; then
+    mkdir -p $INSTALL/usr/bin
+      cp test/vainfo/.libs/vainfo $INSTALL/usr/bin
+  fi
+}
