c9f63fbd39c439f6add7590462594e57b0293d23
Stephan Raue <stephan@openelec.tv>
2014-01-21 19:01:30 +0100

    openal-soft: convert to new package format
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/multimedia/openal-soft/package.mk
+++ packages/multimedia/openal-soft/package.mk
@@ -23,12 +23,20 @@ PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.openal.org/"
 PKG_URL="http://kcat.strangesoft.net/openal-releases/$PKG_NAME-$PKG_VERSION.tar.bz2"
-PKG_DEPENDS="alsa-lib"
-PKG_BUILD_DEPENDS="toolchain alsa-lib"
+PKG_DEPENDS_TARGET="alsa-lib"
+PKG_BUILD_DEPENDS_TARGET="toolchain alsa-lib"
 PKG_PRIORITY="optional"
 PKG_SECTION="multimedia"
 PKG_SHORTDESC="openal: Open Audio Library"
 PKG_LONGDESC="OpenAL, the Open Audio Library, is a joint effort to create an open, vendor- neutral, cross-platform API for interactive, primarily spatialized audio. OpenAL's primary audience are application developers and desktop users that rely on portable standards like OpenGL, for games and other multimedia applications. OpenAL is already supported by a number of hardware vendors and developers."
-PKG_IS_ADDON="no"
 
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+
+configure_target() {
+  cmake -DCMAKE_TOOLCHAIN_FILE=$CMAKE_CONF \
+        -DCMAKE_INSTALL_PREFIX=/usr \
+        -DEXAMPLES="off" \
+        -DUTILS="off" \
+        ..
+}
