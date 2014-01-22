f1fbb09e26699e15a7d31c2c9b279c0f963fa506
Stefan Saraev <stefan@saraev.ca>
2014-01-22 21:24:54 +0200

    xbmc: update to xbmc-13.alpha-910ac62

--- packages/mediacenter/xbmc-theme-Confluence/package.mk
+++ packages/mediacenter/xbmc-theme-Confluence/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="xbmc-theme-Confluence"
-PKG_VERSION="13.alpha-63e652f"
+PKG_VERSION="13.alpha-910ac62"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
