3f543a4f88b27e16894e0214e3839447fd356de7
Stephan Raue <stephan@openelec.tv>
2014-01-30 14:38:55 +0100

    service.openelec.settings: rework PKG_DEPENDS, replace and merge PKG_BUILD_DEPENDS_* with PKG_DEPENDS_*
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/service.openelec.settings/package.mk
+++ packages/mediacenter/service.openelec.settings/package.mk
@@ -24,8 +24,7 @@ PKG_LICENSE="prop."
 PKG_SITE="http://www.openelec.tv"
 PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.zip"
 PKG_SOURCE_DIR="$PKG_NAME"
-PKG_DEPENDS="connman"
-PKG_BUILD_DEPENDS_TARGET="toolchain Python"
+PKG_DEPENDS_TARGET="toolchain Python connman"
 PKG_PRIORITY="optional"
 PKG_SECTION=""
 PKG_SHORTDESC="service.openelec.settings: Settings dialog for OpenELEC"
@@ -34,10 +33,10 @@ PKG_LONGDESC="service.openelec.settings: is a settings dialog for OpenELEC"
 PKG_IS_ADDON="yes"
 PKG_AUTORECONF="no"
 
-if [ "$DISPLAYSERVER" = "xorg-server" ]; then
-  PKG_DEPENDS="$PKG_DEPENDS setxkbmap"
+if [ "$DISPLAYSERVER" = "x11" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS setxkbmap"
 else
-  PKG_DEPENDS="$PKG_DEPENDS bkeymaps"
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS bkeymaps"
 fi
 
 make_target() {
