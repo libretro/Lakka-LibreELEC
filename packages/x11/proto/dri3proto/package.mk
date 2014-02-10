f1b02ead4003be24983dc71df52dd5e2a572a2c5
Stephan Raue <stephan@openelec.tv>
2014-02-10 03:43:59 +0100

    new package: add package 'dri3proto'
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

---
+++ packages/x11/proto/dri3proto/package.mk
@@ -0,0 +1,36 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
+#
+#  OpenELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  OpenELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="dri3proto"
+PKG_VERSION="1.0"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="OSS"
+PKG_SITE="http://www.X.org"
+PKG_URL="http://xorg.freedesktop.org/archive/individual/proto/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_DEPENDS_TARGET="toolchain util-macros"
+PKG_PRIORITY="optional"
+PKG_SECTION="x11/proto"
+PKG_SHORTDESC="dri3proto: X.Org DRI3 protocol headers"
+PKG_LONGDESC="X.Org DRI3 protocol headers."
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="yes"
+
+# package specific configure options
+PKG_CONFIGURE_OPTS_TARGET="--without-xmlto"
