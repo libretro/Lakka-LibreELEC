8534bcf0a44105c7b66ec73249201f3822fd3ecb
Stefan Saraev <stefan@saraev.ca>
2014-02-21 21:51:09 +0200

    vdr-plugin-dvbapi: update to vdr-plugin-dvbapi-400879b

--- packages/3rdparty/multimedia/vdr-plugin-dvbapi/package.mk
+++ packages/3rdparty/multimedia/vdr-plugin-dvbapi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="vdr-plugin-dvbapi"
-PKG_VERSION="4d9de95"
+PKG_VERSION="400879b"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
