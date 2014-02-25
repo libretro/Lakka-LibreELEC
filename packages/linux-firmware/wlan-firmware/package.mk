c4d10046521e5387a308a59cecdc29108548fab3
Stefan Saraev <stefan@saraev.ca>
2014-02-25 14:41:02 +0200

    wlan-firmware: update to wlan-firmware-0.0.16
    closes #2953

--- packages/linux-firmware/wlan-firmware/package.mk
+++ packages/linux-firmware/wlan-firmware/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="wlan-firmware"
-PKG_VERSION="0.0.15"
+PKG_VERSION="0.0.16"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Free-to-use"
