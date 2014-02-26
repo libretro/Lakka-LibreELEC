62582a2ff67d4a7fab1cd9e43e99b8623ef8ce31
Stefan Saraev <stefan@saraev.ca>
2014-02-26 19:57:21 +0200

    dbus: /var/run is now /run. --with-system-socket not needed anymore

--- packages/sysutils/dbus/package.mk
+++ packages/sysutils/dbus/package.mk
@@ -52,8 +52,7 @@ PKG_CONFIGURE_OPTS_TARGET="export ac_cv_have_abstract_sockets=yes \
                            --enable-inotify \
                            --with-xml=expat \
                            --without-x \
-                           --with-dbus-user=dbus \
-                           --with-system-socket=/run/dbus/system_bus_socket"
+                           --with-dbus-user=dbus"
 
 PKG_CONFIGURE_OPTS_HOST="--disable-verbose-mode \
                          --disable-asserts \
