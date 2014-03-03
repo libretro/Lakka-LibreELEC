015bd676dc3c6ee3e5eb59bf28bd161c26ce2e3a
Stephan Raue <stephan@openelec.tv>
2014-03-04 00:25:17 +0100

    xbmc: update to xbmc-13-128426f
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/xbmc-theme-Confluence/package.mk
+++ packages/mediacenter/xbmc-theme-Confluence/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="xbmc-theme-Confluence"
-PKG_VERSION="13.alpha-4b23e1e"
+PKG_VERSION="13-128426f"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
