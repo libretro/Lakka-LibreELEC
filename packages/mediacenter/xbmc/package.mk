ac0fc4a0f29c5cdad15e9b35fc3dae310b33f073
Stefan Saraev <stefan@saraev.ca>
2014-03-03 20:38:14 +0200

    xbmc: update to xbmc-13.alpha-4b23e1e

--- packages/mediacenter/xbmc/package.mk
+++ packages/mediacenter/xbmc/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="xbmc"
-PKG_VERSION="13.alpha-5cd779b"
+PKG_VERSION="13.alpha-4b23e1e"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
