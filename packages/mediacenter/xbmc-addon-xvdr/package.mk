f4ed2f2661dd389a19fd0f7dab2611122b8707d1
Stefan Saraev <stefan@saraev.ca>
2014-03-11 21:13:02 +0200

    xbmc-addon-xvdr: update to xbmc-addon-xvdr-77a27f8

--- packages/mediacenter/xbmc-addon-xvdr/package.mk
+++ packages/mediacenter/xbmc-addon-xvdr/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="xbmc-addon-xvdr"
-PKG_VERSION="df75826"
+PKG_VERSION="77a27f8"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
