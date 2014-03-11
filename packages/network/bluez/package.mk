10b90c5c60c52cfe662aa89c06227ca467a5b6ef
Stephan Raue <stephan@openelec.tv>
2014-03-12 00:04:54 +0100

    bluez: update to bluez-5.16
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/network/bluez/package.mk
+++ packages/network/bluez/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="bluez"
-PKG_VERSION="5.15"
+PKG_VERSION="5.16"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
