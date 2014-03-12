3ac2dbf11b239a2fa11bc814bcf7f51cabed0ae3
Stephan Raue <stephan@openelec.tv>
2014-03-12 16:27:11 +0100

    samba: update to samba-3.6.23
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/network/samba/package.mk
+++ packages/network/samba/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="samba"
-PKG_VERSION="3.6.22"
+PKG_VERSION="3.6.23"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
