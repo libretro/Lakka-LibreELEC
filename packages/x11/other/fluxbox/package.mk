d213be30380817623cb3ed54ac0c3a0d5ef7b28b
Stefan Saraev <stefan@saraev.ca>
2014-03-16 20:17:27 +0200

    fluxbox: tweak config. start by default

--- packages/x11/other/fluxbox/package.mk
+++ packages/x11/other/fluxbox/package.mk
@@ -45,6 +45,10 @@ PKG_CONFIGURE_OPTS_TARGET="ac_cv_func_realloc_0_nonnull=yes \
                            --disable-nls \
                            --disable-imlib2"
                            
+post_install() {
+  enable_service windowmanager.service
+}
+
 post_makeinstall_target() {
   rm -rf $INSTALL/usr/bin/fbrun
   rm -rf $INSTALL/usr/bin/fbsetbg
