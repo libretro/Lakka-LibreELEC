ae1cb224141c8e7b179619285333fc0dc741b0ac
Stephan Raue <stephan@openelec.tv>
2014-03-26 01:38:02 +0100

    systemd: update to systemd-212
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/sysutils/systemd/package.mk
+++ packages/sysutils/systemd/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="systemd"
-PKG_VERSION="211"
+PKG_VERSION="212"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
