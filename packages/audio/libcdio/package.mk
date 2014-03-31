d7aff8dd86d8511ed51d7e7dcaf36d53ce8b9638
Stephan Raue <stephan@openelec.tv>
2014-04-01 01:41:25 +0200

    libcdio: update to libcdio-0.92
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/audio/libcdio/package.mk
+++ packages/audio/libcdio/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libcdio"
-PKG_VERSION="0.90"
+PKG_VERSION="0.92"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
