a7260999cfa95068f7b165c3a542c12fc9fdd340
Stephan Raue <stephan@openelec.tv>
2014-04-01 01:54:44 +0200

    libvorbis: update to libvorbis-1.3.4
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/audio/libvorbis/package.mk
+++ packages/audio/libvorbis/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libvorbis"
-PKG_VERSION="1.3.3"
+PKG_VERSION="1.3.4"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="BSD"
