f3af98f5b247ce04be96078734fdf7e88b2aa644
Stephan Raue <stephan@openelec.tv>
2014-04-02 00:06:17 +0200

    freetype: update to freetype-2.5.3
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/print/freetype/package.mk
+++ packages/print/freetype/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="freetype"
-PKG_VERSION="2.5.2"
+PKG_VERSION="2.5.3"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
