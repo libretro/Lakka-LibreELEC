6968334aa8c35ab372d12f032b9e5839762d9d84
Stephan Raue <stephan@openelec.tv>
2014-04-01 03:57:40 +0200

    glew: update to glew-1.10.0
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/graphics/glew/package.mk
+++ packages/graphics/glew/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="glew"
-PKG_VERSION="1.9.0"
+PKG_VERSION="1.10.0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="BSD"
