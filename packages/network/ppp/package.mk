02dba6ef7741e9faaa5af3bec354b2837b48abf7
Stephan Raue <stephan@openelec.tv>
2014-04-01 21:31:59 +0200

    ppp: update to ppp-2.4.6
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/network/ppp/package.mk
+++ packages/network/ppp/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="ppp"
-PKG_VERSION="2.4.5"
+PKG_VERSION="2.4.6"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
