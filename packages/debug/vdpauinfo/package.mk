5a2259579cb8f849b653bda3086f916af8bbe944
Stephan Raue <stephan@openelec.tv>
2014-04-01 03:46:54 +0200

    vdpauinfo: update to vdpauinfo-0.1
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/debug/vdpauinfo/package.mk
+++ packages/debug/vdpauinfo/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="vdpauinfo"
-PKG_VERSION="0.0.6"
+PKG_VERSION="0.1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
