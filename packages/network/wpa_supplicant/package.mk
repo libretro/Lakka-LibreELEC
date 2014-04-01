49b7dc35e5c6cb608ffe8f5953538043c03eb20d
Stephan Raue <stephan@openelec.tv>
2014-04-01 21:32:23 +0200

    wpa_supplicant: update to wpa_supplicant-2.1
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/network/wpa_supplicant/package.mk
+++ packages/network/wpa_supplicant/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="wpa_supplicant"
-PKG_VERSION="2.0"
+PKG_VERSION="2.1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
