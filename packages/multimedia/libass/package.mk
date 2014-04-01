43a41f66f61cc5e8d03cb5e7301a255dec3e3d01
Stephan Raue <stephan@openelec.tv>
2014-04-01 16:14:39 +0200

    libass: update to libass-0.11.1
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/multimedia/libass/package.mk
+++ packages/multimedia/libass/package.mk
@@ -17,13 +17,13 @@
 ################################################################################
 
 PKG_NAME="libass"
-PKG_VERSION="0.10.2"
+PKG_VERSION="0.11.1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="BSD"
 PKG_SITE="http://code.google.com/p/libass/"
-PKG_URL="http://libass.googlecode.com/files/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain freetype fontconfig enca fribidi"
+PKG_URL="https://github.com/libass/libass/releases/download/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_DEPENDS_TARGET="toolchain freetype fontconfig enca fribidi yasm"
 PKG_PRIORITY="optional"
 PKG_SECTION="multimedia"
 PKG_SHORTDESC="libass: a portable subtitle renderer for the ASS/SSA (Advanced Substation Alpha/Substation Alpha) subtitle format."
@@ -33,8 +33,14 @@ PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-test \
-            --enable-enca \
-            --enable-fontconfig \
-            --disable-harfbuzz \
-            --disable-silent-rules \
-            --with-gnu-ld"
+                           --enable-enca \
+                           --enable-fontconfig \
+                           --disable-harfbuzz \
+                           --disable-silent-rules \
+                           --with-gnu-ld"
+
+if [ $TARGET_ARCH = "i386" -o $TARGET_ARCH = "x86_64" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET yasm"
+  PKG_CONFIGURE_OPTS_TARGET="$PKG_CONFIGURE_OPTS_TARGET --enable-asm"
+fi
+
