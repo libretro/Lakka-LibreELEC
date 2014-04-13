9f410ca67380324ba2659c8e9d4161bd854a1398
Stephan Raue <stephan@openelec.tv>
2014-04-13 19:57:58 +0200

    libfslparser: update to libfslparser-3.10.9-1.0.0
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/multimedia/libfslparser/package.mk
+++ packages/multimedia/libfslparser/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libfslparser"
-PKG_VERSION="3.5.7-1.0.0"
+PKG_VERSION="3.10.9-1.0.0"
 PKG_REV="1"
 PKG_ARCH="arm"
 PKG_LICENSE="other"
