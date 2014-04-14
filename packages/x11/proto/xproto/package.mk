51a767330df5e808d3e97480546cb45d46e86954
Stephan Raue <stephan@openelec.tv>
2014-04-14 22:10:46 +0200

    xproto: update to xproto-7.0.26
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/x11/proto/xproto/package.mk
+++ packages/x11/proto/xproto/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="xproto"
-PKG_VERSION="7.0.25"
+PKG_VERSION="7.0.26"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
