49aa69bf989059a585145642011f4faa42148ae5
Stephan Raue <stephan@openelec.tv>
2014-04-16 12:51:22 +0200

    xbmc: update to xbmc-13-0a5b020
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/xbmc/package.mk
+++ packages/mediacenter/xbmc/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="xbmc"
-PKG_VERSION="13-7f24c56"
+PKG_VERSION="13-0a5b020"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
