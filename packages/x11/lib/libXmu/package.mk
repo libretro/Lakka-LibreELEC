c2e1cd2b21fbb4008a2c52479e2b2cd4c09e8c18
Stephan Raue <stephan@openelec.tv>
2014-04-23 12:35:52 +0200

    libXmu: build with '-fPIC -DPIC'
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/x11/lib/libXmu/package.mk
+++ packages/x11/lib/libXmu/package.mk
@@ -33,3 +33,7 @@ PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared --with-gnu-ld --without-xmlto"
+
+pre_configure_target() {
+  CFLAGS="$CFLAGS -fPIC -DPIC"
+}
