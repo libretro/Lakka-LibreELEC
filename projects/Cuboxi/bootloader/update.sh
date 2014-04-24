053038a98fc16052d62859c8d75d76e61ac2ba3b
Stephan Raue <stephan@openelec.tv>
2014-04-24 19:50:57 +0200

    projects/Cuboxi: add bootloader update script
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

---
+++ projects/Cuboxi/bootloader/update.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
+#
+#  OpenELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  OpenELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+[ -z "$BOOT_ROOT" ] && BOOT_ROOT="/flash"
+[ -z "$SYSTEM_ROOT" ] && SYSTEM_ROOT=""
+
+# mount $BOOT_ROOT r/w
+  mount -o remount,rw $BOOT_ROOT
+
+# update Device Tree Blobs
+  for all_dtb in /flash/*.dtb; do
+    dtb=$(basename $all_dtb)
+    if [ -f $SYSTEM_ROOT/usr/share/bootloader/$dtb ]; then
+      echo "*** updating Device Tree Blob: $dtb ..."
+      cp -p $SYSTEM_ROOT/usr/share/bootloader/$dtb $BOOT_ROOT
+    fi
+  done
+
+# update bootloader files
+  # todo
+
+# mount $BOOT_ROOT r/o
+  sync
+  mount -o remount,ro $BOOT_ROOT
