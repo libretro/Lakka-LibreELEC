bddee101f92a3dc9860b37ab02cca4b54d6facea
Stefan Saraev <stefan@saraev.ca>
2014-04-24 20:09:10 +0300

    wpa_supplicant: build with lto

--- packages/network/wpa_supplicant/package.mk
+++ packages/network/wpa_supplicant/package.mk
@@ -36,9 +36,6 @@ PKG_MAKE_OPTS_TARGET="-C wpa_supplicant V=1 LIBDIR=/usr/lib BINDIR=/usr/bin"
 PKG_MAKEINSTALL_OPTS_TARGET="-C wpa_supplicant V=1 LIBDIR=/usr/lib BINDIR=/usr/bin"
 
 configure_target() {
-# wpa_supplicant fails to build with LTO
-  strip_lto
-
   LDFLAGS="$LDFLAGS -lpthread -lm"
 
   cp $PKG_DIR/config/makefile.config wpa_supplicant/.config
