9073a6031aceed113c221e50e41bb126ae526fa6
Stephan Raue <stephan@openelec.tv>
2014-04-30 13:38:20 +0200

    xf86-input-evdev: update to xf86-input-evdev-2.8.3
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/x11/driver/xf86-input-evdev/package.mk
+++ packages/x11/driver/xf86-input-evdev/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="xf86-input-evdev"
-PKG_VERSION="2.8.2"
+PKG_VERSION="2.8.3"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
