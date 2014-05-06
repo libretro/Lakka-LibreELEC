6da5749e5ca058df60d999b065c05d62b5bd6975
Stephan Raue <stephan@openelec.tv>
2014-05-06 19:03:13 +0200

    Mesa: update to Mesa-10.1.2
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/graphics/Mesa/package.mk
+++ packages/graphics/Mesa/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="Mesa"
-PKG_VERSION="10.1.1"
+PKG_VERSION="10.1.2"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
