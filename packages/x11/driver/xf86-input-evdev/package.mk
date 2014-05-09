c874256199beb714b8f5c82362a0b71a20390967
Stephan Raue <stephan@openelec.tv>
2014-05-09 11:12:22 +0200

    xf86-input-evdev: update to xf86-input-evdev-2.8.4
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/x11/driver/xf86-input-evdev/package.mk
+++ packages/x11/driver/xf86-input-evdev/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="xf86-input-evdev"
-PKG_VERSION="2.8.3"
+PKG_VERSION="2.8.4"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
