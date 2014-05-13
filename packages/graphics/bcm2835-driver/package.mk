c85ab72399c36354a66eae917310d44ec3f844e5
Stephan Raue <stephan@openelec.tv>
2014-05-13 02:10:47 +0200

    bcm2835-driver: update to bcm2835-driver-c0319ac
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/graphics/bcm2835-driver/package.mk
+++ packages/graphics/bcm2835-driver/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="bcm2835-driver"
-PKG_VERSION="680ac93"
+PKG_VERSION="c0319ac"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="nonfree"
