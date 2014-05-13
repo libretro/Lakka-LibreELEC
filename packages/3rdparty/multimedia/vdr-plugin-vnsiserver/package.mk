b8d09005b9fa778a0afea045274d9bc3ef5d5788
Stephan Raue <stephan@openelec.tv>
2014-05-13 12:00:32 +0200

    vdr-plugin-vnsiserver: update to vdr-plugin-vnsiserver-031f69b
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/3rdparty/multimedia/vdr-plugin-vnsiserver/package.mk
+++ packages/3rdparty/multimedia/vdr-plugin-vnsiserver/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="vdr-plugin-vnsiserver"
-PKG_VERSION="a3f7ac5"
+PKG_VERSION="031f69b"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
