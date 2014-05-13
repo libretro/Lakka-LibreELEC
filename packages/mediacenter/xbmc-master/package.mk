daa90579c05401449827e850fe9b1178b7c7ae28
Stephan Raue <stephan@openelec.tv>
2014-05-13 02:27:16 +0200

    xbmc-master: update to xbmc-master-14-af2d1b5
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/xbmc-master/package.mk
+++ packages/mediacenter/xbmc-master/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="xbmc-master"
-PKG_VERSION="14-54a2dc7"
+PKG_VERSION="14-af2d1b5"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
