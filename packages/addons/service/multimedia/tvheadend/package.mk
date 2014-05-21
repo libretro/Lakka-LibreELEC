bc14b3df1c7ae27e53da34dc115cd36a1a4f1825
Stefan Saraev <stefan@saraev.ca>
2014-05-21 20:48:37 +0300

    tvheadend: update to tvheadend-3.9.681

--- packages/addons/service/multimedia/tvheadend/package.mk
+++ packages/addons/service/multimedia/tvheadend/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="tvheadend"
-PKG_VERSION="3.9.467"
+PKG_VERSION="3.9.681"
 PKG_REV="2"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
