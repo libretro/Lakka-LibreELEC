0de5d41965d5a3510739537e56f52e29c7982daa
Stephan Raue <stephan@openelec.tv>
2014-06-07 13:52:04 +0200

    libICE: update to libICE-1.0.9
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/x11/lib/libICE/package.mk
+++ packages/x11/lib/libICE/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libICE"
-PKG_VERSION="1.0.8"
+PKG_VERSION="1.0.9"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
