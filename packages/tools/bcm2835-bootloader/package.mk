9b897f17a11ee7f5859e80ea2bf10640d81a4ae5
Stephan Raue <stephan@openelec.tv>
2014-06-14 00:42:28 +0200

    bcm2835-bootloader: update to bcm2835-bootloader-e45a4a2
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/tools/bcm2835-bootloader/package.mk
+++ packages/tools/bcm2835-bootloader/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="bcm2835-bootloader"
-PKG_VERSION="2990b78"
+PKG_VERSION="e45a4a2"
 PKG_REV="1"
 PKG_ARCH="arm"
 PKG_LICENSE="nonfree"
