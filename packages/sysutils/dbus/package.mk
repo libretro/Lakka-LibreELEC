091816fd11394d2c16de5a273ff5ba87d5bfb847
Stephan Raue <stephan@openelec.tv>
2014-06-18 09:58:30 +0200

    dbus: update to dbus-1.8.4
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/sysutils/dbus/package.mk
+++ packages/sysutils/dbus/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="dbus"
-PKG_VERSION="1.8.0"
+PKG_VERSION="1.8.4"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
