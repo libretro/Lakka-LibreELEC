2e1810cd86d1767bf2d79036d5b9e0d0a5c10d10
Stephan Raue <stephan@openelec.tv>
2014-06-19 20:51:35 +0200

    kmod: update to kmod-18
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/sysutils/kmod/package.mk
+++ packages/sysutils/kmod/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kmod"
-PKG_VERSION="17"
+PKG_VERSION="18"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
