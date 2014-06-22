aa5243b4c9f1c7d25ceb49b91c7acd944f85b8e1
Stephan Raue <stephan@openelec.tv>
2014-06-22 14:53:41 +0200

    alsa-lib: update to alsa-lib-1.0.28
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/audio/alsa/alsa-lib/package.mk
+++ packages/audio/alsa/alsa-lib/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="alsa-lib"
-PKG_VERSION="1.0.27.2"
+PKG_VERSION="1.0.28"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
