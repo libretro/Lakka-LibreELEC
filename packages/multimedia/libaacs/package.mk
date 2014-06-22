64a70c43c0f9c920a9f88bd8ccf6fab38a5efaab
Stephan Raue <stephan@openelec.tv>
2014-06-22 14:55:43 +0200

    libaacs: update to libaacs-0.7.1
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/multimedia/libaacs/package.mk
+++ packages/multimedia/libaacs/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libaacs"
-PKG_VERSION="0.7.0"
+PKG_VERSION="0.7.1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
