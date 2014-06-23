53fce5110b69ec1e64d6bdb32b8f30214ea5a200
Stephan Raue <stephan@openelec.tv>
2014-06-23 03:26:48 +0200

    cloog: update to cloog-0.18.2
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/devel/cloog/package.mk
+++ packages/devel/cloog/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="cloog"
-PKG_VERSION="0.18.0"
+PKG_VERSION="0.18.2"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="LGPL"
