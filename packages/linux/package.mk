f021f424013e6492a017bb95009fc93acc51bc17
Stephan Raue <stephan@openelec.tv>
2014-06-27 06:04:42 +0200

    linux: update to linux-3.15.2
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -23,7 +23,7 @@ case "$LINUX" in
     PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
     ;;
   *)
-    PKG_VERSION="3.15.1"
+    PKG_VERSION="3.15.2"
     PKG_URL="http://www.kernel.org/pub/linux/kernel/v3.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     ;;
 esac
