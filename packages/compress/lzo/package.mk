5894a65157e5cb195f6c41ac5d383518295b5e6c
Stephan Raue <stephan@openelec.tv>
2014-06-29 20:42:46 +0200

    lzo: update to lzo-2.08
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/compress/lzo/package.mk
+++ packages/compress/lzo/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="lzo"
-PKG_VERSION="2.07"
+PKG_VERSION="2.08"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
