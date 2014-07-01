07f2d2f96d6a49349b6403c12586a08b4d334ac1
Stephan Raue <stephan@openelec.tv>
2014-07-01 18:20:40 +0200

    simplejson: update to simplejson-3.5.3
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/python/system/simplejson/package.mk
+++ packages/python/system/simplejson/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="simplejson"
-PKG_VERSION="3.4.0"
+PKG_VERSION="3.5.3"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
