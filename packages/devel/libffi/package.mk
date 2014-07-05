b0f1f53130f634da5b431448ff747fb2fca5285e
Stephan Raue <stephan@openelec.tv>
2014-07-05 16:27:33 +0200

    libffi: update to libffi-3.1
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/devel/libffi/package.mk
+++ packages/devel/libffi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libffi"
-PKG_VERSION="3.0.11"
+PKG_VERSION="3.1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
