ce2a1a4ff2b000e51a6ec8a38219e21a2ca37976
Stephan Raue <stephan@openelec.tv>
2014-07-08 16:20:36 +0200

    fontsproto: update to fontsproto-2.1.3
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/x11/proto/fontsproto/package.mk
+++ packages/x11/proto/fontsproto/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="fontsproto"
-PKG_VERSION="2.1.2"
+PKG_VERSION="2.1.3"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
