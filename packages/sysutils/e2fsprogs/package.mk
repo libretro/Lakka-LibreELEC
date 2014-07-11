af97a8491e4aa366ae86abb61f1ee7970a577752
Stephan Raue <stephan@openelec.tv>
2014-07-11 06:42:23 +0200

    e2fsprogs: update to e2fsprogs-1.42.11
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/sysutils/e2fsprogs/package.mk
+++ packages/sysutils/e2fsprogs/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="e2fsprogs"
-PKG_VERSION="1.42.10"
+PKG_VERSION="1.42.11"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
