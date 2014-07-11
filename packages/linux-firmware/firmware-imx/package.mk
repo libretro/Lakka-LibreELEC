a83ad2b5b99e67e73e0b8b364b8c1ad28e08e385
Stephan Raue <stephan@openelec.tv>
2014-07-11 05:29:25 +0200

    firmware-imx: update to firmware-imx-3.10.17-1.0.0
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/linux-firmware/firmware-imx/package.mk
+++ packages/linux-firmware/firmware-imx/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="firmware-imx"
-PKG_VERSION="3.5.7-1.0.0"
+PKG_VERSION="3.10.17-1.0.0"
 PKG_REV="1"
 PKG_ARCH="arm"
 PKG_LICENSE="other"
