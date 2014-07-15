fb04b04ae4b3c2cd64b22a8b8f51f59438b163dd
Stephan Raue <stephan@openelec.tv>
2014-07-15 07:55:53 +0200

    nano: disable utf8 support to fix buildissue if libncursesw is installed on hostsystem
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/tools/nano/package.mk
+++ packages/tools/nano/package.mk
@@ -32,6 +32,8 @@ PKG_LONGDESC="GNU nano (Nano's ANOther editor, or Not ANOther editor) is an enha
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
+PKG_CONFIGURE_OPTS_TARGET="--disable-utf8"
+
 export CFLAGS="$CFLAGS -I$SYSROOT_PREFIX/usr/include/ncurses"
 export LDFLAGS=`echo $LDFLAGS | sed -e "s|-Wl,--as-needed||"`
 export LIBS="$LIBS -lz"
