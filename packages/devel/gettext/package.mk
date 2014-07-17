6ae3a84a94f2c02eedef2df0845bb83df3cb2fb3
Stephan Raue <stephan@openelec.tv>
2014-07-17 15:33:56 +0200

    gettext: update to gettext-0.19.2
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/devel/gettext/package.mk
+++ packages/devel/gettext/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="gettext"
-PKG_VERSION="0.19.1"
+PKG_VERSION="0.19.2"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
