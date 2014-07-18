0194afbf0670696a7fdc56dc4c531029c5a3d51c
Stephan Raue <stephan@openelec.tv>
2014-07-19 00:43:36 +0200

    libXi: update to libXi-1.7.4
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/x11/lib/libXi/package.mk
+++ packages/x11/lib/libXi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libXi"
-PKG_VERSION="1.7.3"
+PKG_VERSION="1.7.4"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
