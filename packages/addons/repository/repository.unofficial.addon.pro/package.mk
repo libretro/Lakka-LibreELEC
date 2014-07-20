8a11e519f5485a7b788ece5eda246c72480b9400
Stefan Saraev <stefan@saraev.ca>
2014-07-20 16:00:19 +0300

    repository.unofficial.addon.pro: bump

--- packages/addons/repository/repository.unofficial.addon.pro/package.mk
+++ packages/addons/repository/repository.unofficial.addon.pro/package.mk
@@ -19,8 +19,8 @@
 ################################################################################
 
 PKG_NAME="repository.unofficial.addon.pro"
-PKG_VERSION="4.1.2"
-PKG_REV="2"
+PKG_VERSION="4.3.0"
+PKG_REV="0"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://unofficial.addon.pro"
