5a42b44125f7bdb07d573b53838cc94f5d94357f
Stephan Raue <stephan@openelec.tv>
2014-07-25 07:05:36 +0200

    libXext: update to libXext-1.3.3
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/x11/lib/libXext/package.mk
+++ packages/x11/lib/libXext/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libXext"
-PKG_VERSION="1.3.2"
+PKG_VERSION="1.3.3"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
