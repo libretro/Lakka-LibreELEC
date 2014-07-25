570c418b8bb9cccaea5e6a7618d0f0d0112b8cd9
Stephan Raue <stephan@openelec.tv>
2014-07-25 07:05:07 +0200

    util-linux: update to util-linux-2.25
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/sysutils/util-linux/package.mk
+++ packages/sysutils/util-linux/package.mk
@@ -17,11 +17,11 @@
 ################################################################################
 
 PKG_NAME="util-linux"
-PKG_VERSION="2.24.2"
+PKG_VERSION="2.25"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_URL="http://www.kernel.org/pub/linux/utils/util-linux/v2.24/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_URL="http://www.kernel.org/pub/linux/utils/util-linux/v2.25/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_DEPENDS_INIT="toolchain"
 PKG_PRIORITY="optional"
