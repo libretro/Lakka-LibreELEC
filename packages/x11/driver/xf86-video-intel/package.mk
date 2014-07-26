fd7f8219a6033210d47086d9662fc26802d1072d
Stephan Raue <stephan@openelec.tv>
2014-07-26 08:21:34 +0200

    xf86-video-intel: add some default configure options, build with GLAMOR and tear-free support, disable building of tool and DRI1 support
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/x11/driver/xf86-video-intel/package.mk
+++ packages/x11/driver/xf86-video-intel/package.mk
@@ -32,16 +32,27 @@ PKG_LONGDESC="The Xorg driver for Intel i810, i815, 830M, 845G, 852GM, 855GM, 86
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
-PKG_CONFIGURE_OPTS_TARGET="--enable-udev \
+PKG_CONFIGURE_OPTS_TARGET="--disable-backlight \
+                           --disable-backlight-helper \
+                           --disable-gen4asm \
+                           --enable-udev \
+                           --disable-tools \
                            --enable-dri \
+                           --disable-dri1 \
+                           --enable-dri2 \
+                           --enable-dri3 \
                            --enable-kms --enable-kms-only \
                            --disable-ums --disable-ums-only \
                            --enable-sna \
                            --enable-uxa \
                            --disable-xvmc \
-                           --disable-glamor \
+                           --enable-glamor \
                            --disable-xaa \
                            --disable-dga \
+                           --enable-tear-free \
+                           --disable-rendernode \
+                           --disable-create2 \
+                           --disable-rotation \
                            --disable-async-swap \
                            --with-xorg-module-dir=$XORG_PATH_MODULES"
 
