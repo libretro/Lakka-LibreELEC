e7853e76bbc2919a50544fa281b362476d6b2bda
Stephan Raue <stephan@openelec.tv>
2014-07-26 07:53:24 +0200

    libdrm: update to libdrm-2.4.55
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/graphics/libdrm/package.mk
+++ packages/graphics/libdrm/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libdrm"
-PKG_VERSION="2.4.54"
+PKG_VERSION="2.4.55"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
