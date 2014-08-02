555b81f5ffecfca6acb1450d4ad7e2992e524f39
Stefan Saraev <stefan@saraev.ca>
2014-08-02 15:35:11 +0300

    swig:host: build without pcre

--- packages/devel/swig/package.mk
+++ packages/devel/swig/package.mk
@@ -23,7 +23,7 @@ PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.swig.org"
 PKG_URL="$SOURCEFORGE_SRC/swig/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_HOST="ccache:host pcre:host"
+PKG_DEPENDS_HOST="ccache:host"
 PKG_PRIORITY="optional"
 PKG_SECTION="devel"
 PKG_SHORTDESC="SWIG: a software development tool that connects programs written in C and C++ with a variety of high-level programming languages."
@@ -34,6 +34,7 @@ PKG_AUTORECONF="no"
 
 PKG_CONFIGURE_OPTS_HOST="--with-pcre-prefix=$ROOT/$TOOLCHAIN \
                          --with-boost=no \
+                         --without-pcre \
                          --without-x \
                          --without-tcl \
                          --without-python \
