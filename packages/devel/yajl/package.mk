d0da3b8a54527583f685a313fd204ad6c58105e7
Stephan Raue <stephan@openelec.tv>
2014-08-04 23:14:30 +0200

    yajl: update to yajl-2.1.0
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/devel/yajl/package.mk
+++ packages/devel/yajl/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="yajl"
-PKG_VERSION="2.0.4"
+PKG_VERSION="2.1.0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="ISC"
