428a655c2793f46203fa8477ff91e612cec11cf7
Stephan Raue <stephan@openelec.tv>
2014-08-15 13:01:16 +0200

    libressl: update to libressl-2.0.5
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/security/libressl/package.mk
+++ packages/security/libressl/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libressl"
-PKG_VERSION="2.0.4"
+PKG_VERSION="2.0.5"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="BSD"
