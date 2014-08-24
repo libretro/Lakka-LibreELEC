2b49d2a9fc9b5d640bb98ca821cb0c27b6c04dc4
Stephan Raue <stephan@openelec.tv>
2014-08-24 18:33:11 +0300

    flex:host: build static

--- packages/devel/flex/package.mk
+++ packages/devel/flex/package.mk
@@ -32,7 +32,7 @@ PKG_LONGDESC="flex is a tool for generating programs that perform pattern-matchi
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-PKG_CONFIGURE_OPTS_HOST="--disable-rpath --with-gnu-ld"
+PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared --disable-rpath --with-gnu-ld"
 
 post_makeinstall_host() {
   cat > $ROOT/$TOOLCHAIN/bin/lex << "EOF"
