e9ef9515f23eb28ce4c23e710c18fff9fa6880fd
Stephan Raue <stephan@openelec.tv>
2014-09-03 22:37:06 +0200

    util-linux: update to util-linux-2.25.1
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/sysutils/util-linux/package.mk
+++ packages/sysutils/util-linux/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="util-linux"
-PKG_VERSION="2.25"
+PKG_VERSION="2.25.1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
