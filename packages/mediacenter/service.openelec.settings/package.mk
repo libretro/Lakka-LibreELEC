94ea0f9411db123feead06d5c43b926474a4efb6
Stephan Raue <stephan@openelec.tv>
2014-09-10 15:08:04 +0200

    service.openelec.settings: update to service.openelec.settings-0.4.3
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/service.openelec.settings/package.mk
+++ packages/mediacenter/service.openelec.settings/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="service.openelec.settings"
-PKG_VERSION="0.4.2"
+PKG_VERSION="0.4.3"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
