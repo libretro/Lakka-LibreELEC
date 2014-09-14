25f97cd8b98ba0f750afe39043d527bcde9a9048
Stephan Raue <stephan@openelec.tv>
2014-09-14 18:06:43 +0200

    xbmc: update to xbmc-14-68ef36a
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/xbmc/package.mk
+++ packages/mediacenter/xbmc/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="xbmc"
-PKG_VERSION="14-f78c550"
+PKG_VERSION="14-68ef36a"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
