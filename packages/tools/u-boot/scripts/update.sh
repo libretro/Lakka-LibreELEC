ecf607f07c982bdd1d6e379e2669f54d0ebe766b
Stephan Raue <stephan@openelec.tv>
2014-09-14 16:19:13 +0200

    u-boot: fix broken 'SPL' update in update script
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/tools/u-boot/scripts/update.sh
+++ packages/tools/u-boot/scripts/update.sh
@@ -18,9 +18,19 @@
 #  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-[ -z "$BOOT_ROOT" ] && BOOT_ROOT="/flash"
-[ -z "$BOOT_DISK" ] && BOOT_DISK=$(df "$BOOT_ROOT" |tail -1 |awk {' print $1 '})
 [ -z "$SYSTEM_ROOT" ] && SYSTEM_ROOT=""
+[ -z "$BOOT_ROOT" ] && BOOT_ROOT="/flash"
+[ -z "$BOOT_PART" ] && BOOT_PART=$(df "$BOOT_ROOT" | tail -1 | awk {' print $1 '})
+if [ -z "$BOOT_DISK" ]; then
+  case $BOOT_PART in
+    /dev/sd[a-z][0-9]*)
+      BOOT_DISK=$(echo $BOOT_PART | sed -e "s,[0-9]*,,g")
+      ;;
+    /dev/mmcblk*)
+      BOOT_DISK=$(echo $BOOT_PART | sed -e "s,p[0-9]*,,g")
+      ;;
+  esac
+fi
 
 # mount $BOOT_ROOT r/w
   mount -o remount,rw $BOOT_ROOT
