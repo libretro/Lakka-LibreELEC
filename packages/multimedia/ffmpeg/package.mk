ae1af6435529d98a082b68cd69cf2b3dd7d7077d
Stephan Raue <stephan@openelec.tv>
2014-09-18 11:39:23 +0200

    ffmpeg: update to ffmpeg-2.4
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/multimedia/ffmpeg/package.mk
+++ packages/multimedia/ffmpeg/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="ffmpeg"
-PKG_VERSION="2.3.3"
+PKG_VERSION="2.4"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="LGPL"
@@ -218,7 +218,6 @@ configure_target() {
               $FFMPEG_CPU \
               $FFMPEG_FPU \
               --enable-yasm \
-              --disable-sram \
               --disable-symver
 }
 
