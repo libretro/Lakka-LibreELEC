3da492c73d931e3edd6592976bfbd51fa57a7c44
Stephan Raue <stephan@openelec.tv>
2014-09-21 14:41:30 +0200

    xz: update to xz-5.0.7
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/compress/xz/package.mk
+++ packages/compress/xz/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="xz"
-PKG_VERSION="5.0.5"
+PKG_VERSION="5.0.7"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
