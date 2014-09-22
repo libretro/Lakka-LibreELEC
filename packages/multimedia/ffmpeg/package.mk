3a19737ed8aa2d5daeb7f69b70db29a06a73f49b
Stephan Raue <stephan@openelec.tv>
2014-09-22 19:28:37 +0200

    ffmpeg: update to ffmpeg-2.4.1
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/multimedia/ffmpeg/package.mk
+++ packages/multimedia/ffmpeg/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="ffmpeg"
-PKG_VERSION="2.4"
+PKG_VERSION="2.4.1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="LGPL"
