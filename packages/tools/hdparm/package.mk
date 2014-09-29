744ae4ec925e2bf31d263e3b098757e97d472366
Stephan Raue <stephan@openelec.tv>
2014-09-29 15:53:36 +0200

    hdparm: update to hdparm-9.45
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/tools/hdparm/package.mk
+++ packages/tools/hdparm/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="hdparm"
-PKG_VERSION="9.43"
+PKG_VERSION="9.45"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="BSD"
