aca2f2eaca665be3ee425335367fb4cc95dfce8e
Stephan Raue <stephan@openelec.tv>
2014-09-30 02:40:28 +0200

    wlan-firmware: update to wlan-firmware-0.0.24
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/linux-firmware/wlan-firmware/package.mk
+++ packages/linux-firmware/wlan-firmware/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="wlan-firmware"
-PKG_VERSION="0.0.23"
+PKG_VERSION="0.0.24"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Free-to-use"
