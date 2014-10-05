ba5862809a70dbfb1b687e0720572d04d3a09ee8
Stephan Raue <stephan@openelec.tv>
2014-10-05 16:06:49 +0200

    xbmc: build agains shared ffmpeg
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/xbmc/package.mk
+++ packages/mediacenter/xbmc/package.mk
@@ -347,6 +347,7 @@ PKG_CONFIGURE_OPTS_TARGET="gl_cv_func_gettimeofday_clobber=no \
                            $XBMC_OPTICAL \
                            $XBMC_BLURAY \
                            --enable-texturepacker \
+                           --with-ffmpeg=shared \
                            $XBMC_CODEC \
                            $XBMC_PLAYER"
 
