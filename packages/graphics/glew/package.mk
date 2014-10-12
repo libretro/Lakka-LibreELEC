175498fd4ef3a656c66169c19e67c0060734b329
Stephan Raue <stephan@openelec.tv>
2014-10-12 16:32:04 +0200

    glew: use $OPENGL instead hardcoded 'Mesa' as GL provider
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/graphics/glew/package.mk
+++ packages/graphics/glew/package.mk
@@ -32,8 +32,8 @@ PKG_LONGDESC="The OpenGL Extension Wrangler Library (GLEW) is a cross-platform C
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-if [ "$OPENGL" = "Mesa" ]; then
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET Mesa glu"
+if [ ! "$OPENGL" = "no" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OPENGL glu"
 fi
 
 make_target() {
