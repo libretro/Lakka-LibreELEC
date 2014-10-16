4a82afe95ba6adf1c315aeb2e666ea4e3abd92aa
Stephan Raue <stephan@openelec.tv>
2014-10-16 14:30:28 +0200

    linux: fix build if /lib/firmeware is not avaible before creating the symlink
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -213,7 +213,8 @@ makeinstall_init() {
 }
 
 post_install() {
-  ln -sf /storage/.config/firmware/ $INSTALL/lib/firmware/updates
+  mkdir -p $INSTALL/lib/firmware/
+    ln -sf /storage/.config/firmware/ $INSTALL/lib/firmware/updates
 
   enable_service cpufreq-threshold.service
 }
