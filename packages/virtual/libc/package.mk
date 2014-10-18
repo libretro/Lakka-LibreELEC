9a7856efe75eb6dedb7ae60cb322bcdd481a6ae5
Stephan Raue <stephan@openelec.tv>
2014-10-18 22:48:45 +0200

    [xbmc-rebrand] libc: rename all variables '$XBMC*' to '$KODI*'
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/virtual/libc/package.mk
+++ packages/virtual/libc/package.mk
@@ -35,7 +35,7 @@ PKG_LONGDESC="Meta package for installing various tools and libs needed for libc
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-if [ "$XBMCPLAYER_DRIVER" = "bcm2835-driver" ]; then
+if [ "$KODIPLAYER_DRIVER" = "bcm2835-driver" ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET arm-mem"
   PKG_DEPENDS_INIT="$PKG_DEPENDS_INIT arm-mem:init"
 fi
