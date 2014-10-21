f775d294696b369e21e08853231246cdeb339947
Stephan Raue <stephan@openelec.tv>
2014-10-21 13:12:59 +0200

    kodi-pvr-addons: update to kodi-pvr-addons-067befe
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/kodi-pvr-addons/package.mk
+++ packages/mediacenter/kodi-pvr-addons/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi-pvr-addons"
-PKG_VERSION="259d150"
+PKG_VERSION="067befe"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
