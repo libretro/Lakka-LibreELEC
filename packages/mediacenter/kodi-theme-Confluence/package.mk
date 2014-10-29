73355a696826ced8b278958a71d29eb805b25564
Stephan Raue <stephan@openelec.tv>
2014-10-29 01:16:14 +0100

    kodi: update to kodi-14-af6d342, update FM support patch, update RPi support patch
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/kodi-theme-Confluence/package.mk
+++ packages/mediacenter/kodi-theme-Confluence/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi-theme-Confluence"
-PKG_VERSION="14-34923de"
+PKG_VERSION="14-af6d342"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
