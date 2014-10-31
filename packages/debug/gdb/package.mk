0612c11688ccef542164d617830ba2b693b1e426
Stephan Raue <stephan@openelec.tv>
2014-10-31 11:04:25 +0100

    gdb: update to gdb-7.8.1
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/debug/gdb/package.mk
+++ packages/debug/gdb/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="gdb"
-PKG_VERSION="7.8"
+PKG_VERSION="7.8.1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
