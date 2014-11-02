a0f38a4ced3a931b2ca6d9fc66fb87f472afec21
Stephan Raue <stephan@openelec.tv>
2014-11-02 12:27:12 +0100

    kodi: update to kodi-14-a693300
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/kodi-theme-Confluence/package.mk
+++ packages/mediacenter/kodi-theme-Confluence/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi-theme-Confluence"
-PKG_VERSION="14-ba267af"
+PKG_VERSION="14-a693300"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
