baa14a87e52ffdbf6fcfba72ea5d0bc5ca4e2f7b
vpeter4 <peter.vicman@gmail.com>
2014-11-15 14:38:05 +0100

    sundtek-mediatv: allow running scripts from addon settings

--- packages/addons/driver/sundtek-mediatv/source/resources/lib/xmlpp.py
+++ packages/addons/driver/sundtek-mediatv/source/resources/lib/xmlpp.py
@@ -37,36 +37,39 @@ def _usage(this_file):
     return """SYNOPSIS: pretty print an XML document
 USAGE: python %s <filename> \n""" % this_file
 
-def _pprint_line(indent_level, line, width=100, output=_sys.stdout):
+def _pprint_line(indent_level, line, width=100, output=_sys.stdout, ignore_contents = False):
     if line.strip():
         start = ""
         number_chars = 0
         for l in range(indent_level):
             start = start + " "
             number_chars = number_chars + 1
-        try:
-            elem_start = _re.findall("(\<\W{0,1}\w+:\w+) ?", line)[0]
-            elem_finished = _re.findall("([?|\]\]/]*\>)", line)[0] 
-            #should not have *
-            attrs = _re.findall("(\S*?\=\".*?\")", line)
-            output.write(start + elem_start)
-            number_chars = len(start + elem_start)
-            for attr in attrs:
-                if (attrs.index(attr) + 1) == len(attrs):
-                    number_chars = number_chars + len(elem_finished)
-                if (number_chars + len(attr) + 1) > width:
-                    output.write("\n")
-                    for i in range(len(start + elem_start) + 1):
+        if not ignore_contents:
+            try:
+                elem_start = _re.findall("(\<\W{0,1}\w+:\w+) ?", line)[0]
+                elem_finished = _re.findall("([?|\]\]/|\-\-]*\>)", line)[0] 
+                #should not have *
+                attrs = _re.findall("(\S*?\=\".*?\")", line)
+                output.write(start + elem_start)
+                number_chars = len(start + elem_start)
+                for attr in attrs:
+                    if (attrs.index(attr) + 1) == len(attrs):
+                        number_chars = number_chars + len(elem_finished)
+                    if (number_chars + len(attr) + 1) > width:
+                        output.write("\n")
+                        for i in range(len(start + elem_start) + 1):
+                            output.write(" ")
+                        number_chars = len(start + elem_start) + 1 
+                    else:
                         output.write(" ")
-                    number_chars = len(start + elem_start) + 1 
-                else:
-                    output.write(" ")
-                    number_chars = number_chars + 1
-                output.write(attr)
-                number_chars = number_chars + len(attr)
-            output.write(elem_finished + "\n")
-        except IndexError:
-            #give up pretty print this line
+                        number_chars = number_chars + 1
+                    output.write(attr)
+                    number_chars = number_chars + len(attr)
+                output.write(elem_finished + "\n")
+            except IndexError:
+                #give up pretty print this line
+                output.write(start + line + "\n")
+        else:
             output.write(start + line + "\n")
                 
 
@@ -80,7 +83,8 @@ def _get_next_elem(data):
     start_pos = data.find("<")
     end_pos = data.find(">") + 1
     retval = data[start_pos:end_pos]
-    stopper = retval.rfind("/") 
+    stopper = retval.rfind("/")
+    ignore_contents = False
     if stopper < retval.rfind("\""):
         stopper = -1
     single = (stopper > -1 and ((retval.find(">") - stopper) < (stopper - retval.find("<"))))
@@ -89,11 +93,19 @@ def _get_next_elem(data):
     ignore_question =  retval.find("<?") > -1
 
     if ignore_excl:
+        ignore_contents = True
         cdata = retval.find("<![CDATA[") > -1
         if cdata:
             end_pos = data.find("]]>")
             if end_pos > -1:
                 end_pos = end_pos + len("]]>")
+                stopper = end_pos
+        else:
+            end_pos = data.find("-->")
+            if end_pos > -1:
+                end_pos = end_pos + len("-->")
+                stopper = end_pos
+        retval = data[start_pos:end_pos]
 
     elif ignore_question:
         end_pos = data.find("?>") + len("?>")
@@ -101,11 +113,12 @@ def _get_next_elem(data):
     
     no_indent = ignore or single
 
-    #print retval, end_pos, start_pos, stopper > -1, no_indent
+    
     return start_pos, \
            end_pos, \
            stopper > -1, \
-           no_indent
+           no_indent, \
+           ignore_contents
 
 def get_pprint(xml, indent=4, width=80):
     """Returns the pretty printed xml """
@@ -116,6 +129,8 @@ def get_pprint(xml, indent=4, width=80):
             self.output += string
     out = out()
     pprint(xml, output=out, indent=indent, width=width)
+    
+
 
     return out.output
 
@@ -126,7 +141,7 @@ def pprint(xml, output=_sys.stdout, indent=4, width=80):
     Use indent to select indentation level. Default is 4   """
     data = xml
     indent_level = 0
-    start_pos, end_pos, is_stop, no_indent  = _get_next_elem(data)
+    start_pos, end_pos, is_stop, no_indent, ignore_contents  = _get_next_elem(data)
     while ((start_pos > -1 and end_pos > -1)):
         _pprint_elem_content(indent_level, data[:start_pos].strip(), 
                              output=output)
@@ -136,7 +151,8 @@ def pprint(xml, output=_sys.stdout, indent=4, width=80):
         _pprint_line(indent_level, 
                      data[:end_pos - start_pos], 
                      width=width,
-                     output=output)
+                     output=output,
+                     ignore_contents=ignore_contents)
         data = data[end_pos - start_pos:]
         if not is_stop and not no_indent :
             indent_level = indent_level + indent
@@ -144,7 +160,7 @@ def pprint(xml, output=_sys.stdout, indent=4, width=80):
         if not data:
             break
         else:
-            start_pos, end_pos, is_stop, no_indent  = _get_next_elem(data)
+            start_pos, end_pos, is_stop, no_indent, ignore_contents  = _get_next_elem(data)
     
 
 if __name__ == "__main__":
