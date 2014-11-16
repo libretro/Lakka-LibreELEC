db094872c72c4f52961c124f988ebd58857850bf
Stephan Raue <stephan@openelec.tv>
2014-11-16 12:15:14 +0100

    kodi: update to kodi-14-2cd9438
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="14-19893f8"
+PKG_VERSION="14-2cd9438"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
