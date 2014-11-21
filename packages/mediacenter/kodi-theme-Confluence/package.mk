16ac2784e58114c10195f17d6eea7ea1097543a3
Stephan Raue <stephan@openelec.tv>
2014-11-21 06:20:59 +0100

    kodi: update to kodi-14-6ba8072
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/kodi-theme-Confluence/package.mk
+++ packages/mediacenter/kodi-theme-Confluence/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi-theme-Confluence"
-PKG_VERSION="14-88a9a44"
+PKG_VERSION="14-6ba8072"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
