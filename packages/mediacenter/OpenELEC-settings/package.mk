488d243912b6f1b215d363fced021ac4cae15724
Stephan Raue <stephan@openelec.tv>
2014-11-23 21:48:56 +0100

    OpenELEC-settings: update to OpenELEC-settings-0.5.5
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/OpenELEC-settings/package.mk
+++ packages/mediacenter/OpenELEC-settings/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="OpenELEC-settings"
-PKG_VERSION="0.5.4"
+PKG_VERSION="0.5.5"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
