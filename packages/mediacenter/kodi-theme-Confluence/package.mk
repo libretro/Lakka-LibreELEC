ec9fd7270e446e618f404a3f93f2627c42a6307b
Stephan Raue <stephan@openelec.tv>
2014-11-23 19:49:01 +0100

    kodi: update to kodi-14-ec361ca
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/kodi-theme-Confluence/package.mk
+++ packages/mediacenter/kodi-theme-Confluence/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi-theme-Confluence"
-PKG_VERSION="14-23c389a"
+PKG_VERSION="14-ec361ca"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
