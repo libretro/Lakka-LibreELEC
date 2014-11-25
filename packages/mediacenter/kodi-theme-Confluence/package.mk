d9ed9c4e4164f7109c8e52e6e13785a3c2738b41
Stephan Raue <stephan@openelec.tv>
2014-11-25 20:45:36 +0100

    kodi: update to kodi-14-4465fbf
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/kodi-theme-Confluence/package.mk
+++ packages/mediacenter/kodi-theme-Confluence/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi-theme-Confluence"
-PKG_VERSION="14-ec361ca"
+PKG_VERSION="14-4465fbf"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
