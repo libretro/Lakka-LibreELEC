7302467c1933eccd0f938fa4ec851ec4157799fc
Stephan Raue <stephan@openelec.tv>
2014-12-02 05:56:24 +0100

    kodi-pvr-addons: update to kodi-pvr-addons-14d9828
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/kodi-pvr-addons/package.mk
+++ packages/mediacenter/kodi-pvr-addons/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi-pvr-addons"
-PKG_VERSION="06842b9"
+PKG_VERSION="14d9828"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
