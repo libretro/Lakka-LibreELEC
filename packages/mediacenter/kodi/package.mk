371b375ee11729260b08cb952c86ac919fea36ea
Stephan Raue <stephan@openelec.tv>
2014-12-02 05:55:57 +0100

    kodi: update to kodi-14-ffad6b8
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="14-bcfc032"
+PKG_VERSION="14-ffad6b8"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
