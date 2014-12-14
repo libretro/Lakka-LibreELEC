c673d2abd2af9d2bca11169be82ec3a8bbbd0f0e
Stephan Raue <stephan@openelec.tv>
2014-12-14 20:13:15 +0100

    kodi: update to kodi-14-88f9fb0
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="14-f4576be"
+PKG_VERSION="14-88f9fb0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
