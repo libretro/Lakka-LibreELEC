6a6bfb0c6dc32ba1623629ddce82af9acd5daddc
Stephan Raue <stephan@openelec.tv>
2014-12-17 00:03:11 +0100

    ethtool: update to ethtool-3.18
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/network/ethtool/package.mk
+++ packages/network/ethtool/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="ethtool"
-PKG_VERSION="3.13"
+PKG_VERSION="3.18"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
