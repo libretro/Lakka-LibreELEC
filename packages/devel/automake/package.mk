ca01bb8738d4c9cc8344d4257e267561edcd586a
Stephan Raue <stephan@openelec.tv>
2015-01-10 22:30:29 +0100

    automake: update to automake-1.15
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/devel/automake/package.mk
+++ packages/devel/automake/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="automake"
-PKG_VERSION="1.14.1"
+PKG_VERSION="1.15"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
