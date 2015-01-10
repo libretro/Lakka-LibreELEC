82af386707d266a2bdfb6e5fc2afeed5831e2804
Stephan Raue <stephan@openelec.tv>
2015-01-10 20:43:29 +0100

    libjpeg-turbo: update to libjpeg-turbo-1.4.0
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/graphics/libjpeg-turbo/package.mk
+++ packages/graphics/libjpeg-turbo/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libjpeg-turbo"
-PKG_VERSION="1.3.0"
+PKG_VERSION="1.4.0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
