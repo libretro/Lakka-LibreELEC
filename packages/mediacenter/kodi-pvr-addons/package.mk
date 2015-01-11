3df7bc2ce0aa16c7901b317550f2c8d4c7c418d2
Stefan Saraev <stefan@saraev.ca>
2015-01-11 20:54:01 +0200

    kodi-pvr-addons: update to kodi-pvr-addons-78397af
    closes #3775

--- packages/mediacenter/kodi-pvr-addons/package.mk
+++ packages/mediacenter/kodi-pvr-addons/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi-pvr-addons"
-PKG_VERSION="9f63d1b"
+PKG_VERSION="78397af"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
