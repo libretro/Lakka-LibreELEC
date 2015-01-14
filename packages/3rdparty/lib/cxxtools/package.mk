4743a0a6ed1d193bcc487ff8e1c9f60e836732c6
Gerald Dachs <gda@dachsweb.de>
2015-01-14 23:29:12 +0100

    cxxtools: update to cxxtools-2.2.1

--- packages/3rdparty/lib/cxxtools/package.mk
+++ packages/3rdparty/lib/cxxtools/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="cxxtools"
-PKG_VERSION="2.2"
+PKG_VERSION="2.2.1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL-2"
