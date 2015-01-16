476b061ddef713175c58f18a68f29e6f801c5d28
Stephan Raue <stephan@openelec.tv>
2015-01-16 21:42:48 +0100

    linux: update to linux-3.18.3
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -27,7 +27,7 @@ case "$LINUX" in
     PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
     ;;
   *)
-    PKG_VERSION="3.18.2"
+    PKG_VERSION="3.18.3"
     PKG_URL="http://www.kernel.org/pub/linux/kernel/v3.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     ;;
 esac
