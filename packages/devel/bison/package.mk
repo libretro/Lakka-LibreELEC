c071cae5f7459bf9d60999ebeca28c2937729b64
Stephan Raue <stephan@openelec.tv>
2015-01-17 20:31:05 +0100

    bison: update to bison-3.0.3
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/devel/bison/package.mk
+++ packages/devel/bison/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="bison"
-PKG_VERSION="3.0.2"
+PKG_VERSION="3.0.3"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
