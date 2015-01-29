22a51021b65e611fc7b38f085f78d70622f3e2c3
Stephan Raue <stephan@openelec.tv>
2015-01-29 20:15:32 +0100

    kodi-audioencoder-flac: update to kodi-audioencoder-flac-62c2cc8
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/kodi-audioencoder-flac/package.mk
+++ packages/mediacenter/kodi-audioencoder-flac/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi-audioencoder-flac"
-PKG_VERSION="a960eba"
+PKG_VERSION="62c2cc8"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
