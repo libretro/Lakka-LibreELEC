3ce50468816fd132805187134b195f257ae4e109
Stefan Saraev <stefan@saraev.ca>
2015-01-31 18:51:19 +0200

    vdr-plugin-vnsiserver: update to vdr-plugin-vnsiserver-b887bc8

--- packages/multimedia/vdr-plugin-vnsiserver/package.mk
+++ packages/multimedia/vdr-plugin-vnsiserver/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="vdr-plugin-vnsiserver"
-PKG_VERSION="a48edf1"
+PKG_VERSION="b887bc8"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
