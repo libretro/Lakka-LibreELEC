78a22118049ab7fddf535621a3cc9fe3ea645096
Stefan Saraev <stefan@saraev.ca>
2015-02-11 20:15:18 +0200

    vdr-plugin-vnsiserver: update to f2175ba

--- packages/multimedia/vdr-plugin-vnsiserver/package.mk
+++ packages/multimedia/vdr-plugin-vnsiserver/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="vdr-plugin-vnsiserver"
-PKG_VERSION="b887bc8"
+PKG_VERSION="f2175ba"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
