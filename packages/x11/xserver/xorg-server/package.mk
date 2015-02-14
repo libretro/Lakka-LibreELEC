82d1800c0570257d04dbafd3849086646f95435d
Stephan Raue <stephan@openelec.tv>
2015-02-14 10:44:25 +0100

    xorg-server: update to xorg-server-1.16.4
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/x11/xserver/xorg-server/package.mk
+++ packages/x11/xserver/xorg-server/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="xorg-server"
-PKG_VERSION="1.16.3"
+PKG_VERSION="1.16.4"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
