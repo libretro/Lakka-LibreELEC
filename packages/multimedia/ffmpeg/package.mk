2fb0892a26ecfd7a6b4f602e4a7559bbe10b46e8
Stephan Raue <stephan@openelec.tv>
2015-02-15 13:08:34 +0100

    ffmpeg: update to ffmpeg-2.5.4
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/multimedia/ffmpeg/package.mk
+++ packages/multimedia/ffmpeg/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="ffmpeg"
-PKG_VERSION="2.4.6"
+PKG_VERSION="2.5.4"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="LGPL"
