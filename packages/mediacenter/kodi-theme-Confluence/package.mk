80de5bd6133fcda21223153ee9c3c693ffa8ee3b
Stephan Raue <stephan@openelec.tv>
2015-02-23 19:49:55 +0100

    kodi: update to kodi-14-085163e
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/kodi-theme-Confluence/package.mk
+++ packages/mediacenter/kodi-theme-Confluence/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi-theme-Confluence"
-PKG_VERSION="14-b5dbdb5"
+PKG_VERSION="14-085163e"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
