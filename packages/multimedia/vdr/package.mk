f195d299130a4430479909155b55918a78399bed
Stefan Saraev <stefan@saraev.ca>
2015-03-01 17:25:06 +0200

    vdr: update to vdr-2.2.0

--- packages/multimedia/vdr/package.mk
+++ packages/multimedia/vdr/package.mk
@@ -18,12 +18,12 @@
 ################################################################################
 
 PKG_NAME="vdr"
-PKG_VERSION="2.1.10"
+PKG_VERSION="2.2.0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.tvdr.de"
-PKG_URL="ftp://ftp.tvdr.de/vdr/Developer/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_URL="ftp://ftp.tvdr.de/vdr/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain fontconfig freetype libcap libjpeg-turbo bzip2"
 PKG_PRIORITY="optional"
 PKG_SECTION="multimedia"
