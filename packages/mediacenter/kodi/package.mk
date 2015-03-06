3c9ad0311d90d50136977157a83b2ec76245bfa2
Stephan Raue <stephan@openelec.tv>
2015-03-06 23:33:19 +0100

    kodi: update to kodi-14-4606179
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="14-085163e"
+PKG_VERSION="14-4606179"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
