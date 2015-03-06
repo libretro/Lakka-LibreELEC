33c46d6acbf4d8f8754e3ee3373ab0848f5f382e
Stephan Raue <stephan@openelec.tv>
2015-03-06 19:20:34 +0100

    plymouth-lite: check PROJECT dir first for splash
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/tools/plymouth-lite/package.mk
+++ packages/tools/plymouth-lite/package.mk
@@ -47,18 +47,18 @@ makeinstall_init() {
     cp ply-image $INSTALL/bin
 
   mkdir -p $INSTALL/splash
-    if [ -f $DISTRO_DIR/$DISTRO/splash/splash.conf ]; then
-      cp $DISTRO_DIR/$DISTRO/splash/splash.conf $INSTALL/splash
-      cp $DISTRO_DIR/$DISTRO/splash/*.png $INSTALL/splash
-    elif [ -f $DISTRO_DIR/$DISTRO/splash/splash-1024.png \
-           -o -f $DISTRO_DIR/$DISTRO/splash/splash-full.png ]; then
-      cp $DISTRO_DIR/$DISTRO/splash/splash-*.png $INSTALL/splash
-    elif [ -f $PROJECT_DIR/$PROJECT/splash/splash.conf ]; then
+    if [ -f $PROJECT_DIR/$PROJECT/splash/splash.conf ]; then
       cp $PROJECT_DIR/$PROJECT/splash/splash.conf $INSTALL/splash
       cp $PROJECT_DIR/$PROJECT/splash/*.png $INSTALL/splash
     elif [ -f $PROJECT_DIR/$PROJECT/splash/splash-1024.png \
            -o -f $PROJECT_DIR/$PROJECT/splash/splash-full.png ]; then
       cp $PROJECT_DIR/$PROJECT/splash/splash-*.png $INSTALL/splash
+    elif [ -f $DISTRO_DIR/$DISTRO/splash/splash.conf ]; then
+      cp $DISTRO_DIR/$DISTRO/splash/splash.conf $INSTALL/splash
+      cp $DISTRO_DIR/$DISTRO/splash/*.png $INSTALL/splash
+    elif [ -f $DISTRO_DIR/$DISTRO/splash/splash-1024.png \
+           -o -f $DISTRO_DIR/$DISTRO/splash/splash-full.png ]; then
+      cp $DISTRO_DIR/$DISTRO/splash/splash-*.png $INSTALL/splash
     else
       cp $PKG_DIR/splash/splash-*.png $INSTALL/splash
     fi
