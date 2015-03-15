0850b6d8542eefe7ddb58632508d167967497e9f
Stephan Raue <stephan@openelec.tv>
2015-03-15 21:04:47 +0100

    libXxf86vm: update to libXxf86vm-1.1.4
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/x11/lib/libXxf86vm/package.mk
+++ packages/x11/lib/libXxf86vm/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libXxf86vm"
-PKG_VERSION="1.1.3"
+PKG_VERSION="1.1.4"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
