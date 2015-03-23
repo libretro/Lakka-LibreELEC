629fe55cc5409abfbf971b8d64e032bba99be423
Stephan Raue <stephan@openelec.tv>
2015-03-23 15:04:30 +0100

    wireless-regdb: update to wireless-regdb-2015.03.13
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/network/wireless-regdb/package.mk
+++ packages/network/wireless-regdb/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="wireless-regdb"
-PKG_VERSION="2014.11.18"
+PKG_VERSION="2015.03.13"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
