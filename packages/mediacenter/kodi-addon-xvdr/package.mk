52753dae34d928d67d79c35cf20fb258ed3771ec
Stefan Saraev <stefan@saraev.ca>
2015-03-30 00:19:53 +0300

    kodi-addon-xvdr: update to kodi-addon-xvdr-c5eff57
    @sraue next time pls dont cherry-pick my stuff to stables without testing.
    while the 'broken' xvdr was 'fine' on master. it's NOT fine on 5.0.
    If I wanted it there, I would push myself (if it was a crashandburn fix) or
    open a PR..
    
    now you've got shitstorm of existing xvdr users crash-looping after
    upgrade to 5.0.7.. enjoy.
    
    ref #4055

--- packages/mediacenter/kodi-addon-xvdr/package.mk
+++ packages/mediacenter/kodi-addon-xvdr/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi-addon-xvdr"
-PKG_VERSION="39d66cd"
+PKG_VERSION="c5eff57"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
