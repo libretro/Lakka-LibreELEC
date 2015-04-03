1fa8db4f97d95a9e0d5252528bf9e6a9be2dcf47
Stefan Saraev <stefan@saraev.ca>
2015-04-03 15:19:33 +0300

    vdr-plugin-vnsiserver: update to 36e2b61

--- packages/multimedia/vdr-plugin-vnsiserver/package.mk
+++ packages/multimedia/vdr-plugin-vnsiserver/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="vdr-plugin-vnsiserver"
-PKG_VERSION="9529e6d"
+PKG_VERSION="36e2b61"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
