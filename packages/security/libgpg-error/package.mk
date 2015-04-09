9df5e4f92a113c7d2fdc2cff8a1757f70103299f
Stephan Raue <stephan@openelec.tv>
2015-04-09 10:31:21 +0200

    libgpg-error: update to libgpg-error-1.18
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/security/libgpg-error/package.mk
+++ packages/security/libgpg-error/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libgpg-error"
-PKG_VERSION="1.12"
+PKG_VERSION="1.18"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
@@ -33,6 +33,30 @@ PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
 PKG_CONFIGURE_OPTS_TARGET="CC_FOR_BUILD=$HOST_CC --enable-static --disable-shared --disable-nls --disable-rpath --with-gnu-ld"
+
+pre_configure_target() {
+# inspired by openembedded
+  case ${TARGET_ARCH} in
+    aarch64)
+      GPGERROR_TUPLE=aarch64-unknown-linux-gnu
+      GPGERROR_TARGET=linux-gnueabi
+      ;;
+    arm)
+      GPGERROR_TUPLE=arm-unknown-linux-gnueabi
+      GPGERROR_TARGET=linux-gnueabi
+      ;;
+    i386)
+      GPGERROR_TUPLE=i486-pc-linux-gnu
+      GPGERROR_TARGET=linux-gnu
+      ;;
+    x86_64)
+      GPGERROR_TUPLE=x86_64-pc-linux-gnu
+      GPGERROR_TARGET=linux-gnu
+      ;;
+  esac
+
+  cp $ROOT/$PKG_BUILD/src/syscfg/lock-obj-pub.$GPGERROR_TUPLE.h $ROOT/$PKG_BUILD/src/syscfg/lock-obj-pub.$GPGERROR_TARGET.h
+}
 
 post_makeinstall_target() {
   rm -rf $INSTALL/usr/bin
