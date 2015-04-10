10ff5e93ab85b77b7aff8c287e7c9f972e9abebd
Stephan Raue <stephan@openelec.tv>
2015-04-10 09:25:06 +0200

    pciutils: update to pciutils-3.3.1
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/sysutils/pciutils/package.mk
+++ packages/sysutils/pciutils/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="pciutils"
-PKG_VERSION="3.3.0"
+PKG_VERSION="3.3.1"
 PKG_REV="1"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL"
