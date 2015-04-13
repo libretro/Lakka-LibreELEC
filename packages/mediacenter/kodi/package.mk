cb7991aba5ac0250019e11525850ecf89c88f759
Stefan Saraev <stefan@saraev.ca>
2015-04-13 17:16:41 +0300

    kodi: bump to 15.0-beta1-cc252ce. drop rbpi patches

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="15.0-beta1-23ef019"
+PKG_VERSION="15.0-beta1-cc252ce"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
