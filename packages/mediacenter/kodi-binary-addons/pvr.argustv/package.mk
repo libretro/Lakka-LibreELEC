72de05f88afbf11f18d441f9dcaaf7c470048742
Stefan Saraev <stefan@saraev.ca>
2015-04-13 17:15:59 +0300

    pvr.argustv: bump to 96ee875

--- packages/mediacenter/kodi-binary-addons/pvr.argustv/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.argustv/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="pvr.argustv"
-PKG_VERSION="e04d562"
+PKG_VERSION="96ee875"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
