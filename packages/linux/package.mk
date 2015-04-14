789b3d9b0ddbb0b7855f6c6899028bd36bbfbed5
Stephan Raue <stephan@openelec.tv>
2015-04-14 13:45:35 +0200

    linux: remove linux-3.19 support
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -26,10 +26,6 @@ case "$LINUX" in
     PKG_VERSION="cuboxi-3.14-dc5edb8"
     PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
     ;;
-  3.19)
-    PKG_VERSION="3.19.3"
-    PKG_URL="http://www.kernel.org/pub/linux/kernel/v3.x/$PKG_NAME-$PKG_VERSION.tar.xz"
-    ;;
   *)
     PKG_VERSION="4.0"
     PKG_URL="http://www.kernel.org/pub/linux/kernel/v4.x/$PKG_NAME-$PKG_VERSION.tar.xz"
