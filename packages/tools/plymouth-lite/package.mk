acf36ec4cb560afa61d02a94c41b54c8a67b5725
Stefan Saraev <stefan@saraev.ca>
2015-04-15 20:19:16 +0300

    Revert "plymouth-lite: link with math library"
    This reverts commit bdde001075b23808cee9e12c7e8f7c31500108ec.
    
    splash loading was broken at least on RPi/2 and wetek play

--- packages/tools/plymouth-lite/package.mk
+++ packages/tools/plymouth-lite/package.mk
@@ -40,8 +40,6 @@ pre_configure_init() {
   # plymouth-lite dont support to build in subdirs
   cd $ROOT/$PKG_BUILD
     rm -rf .$TARGET_NAME-init
-    
-  LDFLAGS="$LDFLAGS -lm"
 }
 
 makeinstall_init() {
