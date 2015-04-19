0f6656a50f4eeb42d1fb89bac059797b481879c7
Stefan Saraev <stefan@saraev.ca>
2015-04-19 18:08:03 +0300

    ... and clean up settings / re-add fallbacktimeservers to main.conf

--- packages/network/connman/package.mk
+++ packages/network/connman/package.mk
@@ -88,6 +88,7 @@ post_makeinstall_target() {
     sed -i $INSTALL/etc/connman/main.conf \
         -e "s|^# BackgroundScanning.*|BackgroundScanning = true|g" \
         -e "s|^# FallbackNameservers.*|FallbackNameservers = 8.8.8.8,8.8.4.4|g" \
+        -e "s|^# FallbackTimeservers.*|FallbackTimeservers = 0.pool.ntp.org,1.pool.ntp.org,2.pool.ntp.org,3.pool.ntp.org|g" \
         -e "s|^# PreferredTechnologies.*|PreferredTechnologies = ethernet,wifi,cellular|g" \
         -e "s|^# TetheringTechnologies.*|TetheringTechnologies = wifi|g" \
         -e "s|^# AllowHostnameUpdates.*|AllowHostnameUpdates = false|g" \
