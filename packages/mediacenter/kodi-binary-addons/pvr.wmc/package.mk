8ab11bfad1858c342ccb5b80a4a500bd8fca139a
Stefan Saraev <stefan@saraev.ca>
2015-04-21 19:24:02 +0300

    pvr.wmc: update to pvr.wmc-37b4b29

--- packages/mediacenter/kodi-binary-addons/pvr.wmc/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.wmc/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="pvr.wmc"
-PKG_VERSION="cf28e4c"
+PKG_VERSION="37b4b29"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
