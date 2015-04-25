b0cf3e56537902bb527f5b7f7efac82daad29c6d
Stefan Saraev <stefan@saraev.ca>
2015-04-25 19:08:00 +0300

    vdr-iptv: update to 2.2.1

--- packages/multimedia/vdr-iptv/package.mk
+++ packages/multimedia/vdr-iptv/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="vdr-iptv"
-PKG_VERSION="2.2.0"
+PKG_VERSION="2.2.1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
