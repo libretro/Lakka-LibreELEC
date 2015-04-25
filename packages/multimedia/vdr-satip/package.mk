02df206fde0fd882bf3243d80121ec94b35a9ac0
Stefan Saraev <stefan@saraev.ca>
2015-04-25 19:08:24 +0300

    vdr-satip: update to 2.2.1

--- packages/multimedia/vdr-satip/package.mk
+++ packages/multimedia/vdr-satip/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="vdr-satip"
-PKG_VERSION="2.2.0"
+PKG_VERSION="2.2.1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
