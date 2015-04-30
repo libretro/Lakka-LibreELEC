31bc4c7dfad6b6ec1f5f26bcda2eb8dfe245fdcd
Stephan Raue <stephan@openelec.tv>
2015-04-30 10:08:23 +0200

    linux: update to linux-4.0.1
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -27,7 +27,7 @@ case "$LINUX" in
     PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
     ;;
   *)
-    PKG_VERSION="4.0"
+    PKG_VERSION="4.0.1"
     PKG_URL="http://www.kernel.org/pub/linux/kernel/v4.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     ;;
 esac
