0a1c3cf66c175765613456de2edf28e811131c1e
Stephan Raue <stephan@openelec.tv>
2015-05-02 12:57:13 +0200

    libfontenc: update to libfontenc-1.1.3
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/x11/lib/libfontenc/package.mk
+++ packages/x11/lib/libfontenc/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libfontenc"
-PKG_VERSION="1.1.2"
+PKG_VERSION="1.1.3"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
