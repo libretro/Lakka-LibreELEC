4986022b9f406b6298811c9019ddf811868ae935
Stephan Raue <stephan@openelec.tv>
2015-05-10 22:09:25 +0200

    sqlite: update to sqlite-autoconf-3081001
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/databases/sqlite/package.mk
+++ packages/databases/sqlite/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="sqlite"
-PKG_VERSION="autoconf-3081000"
+PKG_VERSION="autoconf-3081001"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="PublicDomain"
