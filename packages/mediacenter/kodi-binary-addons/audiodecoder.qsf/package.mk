f01c71276f4d789facc304323436f505f02cb940
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2015-05-12 15:27:36 +0200

    bump audiodecoder.qsf

--- packages/mediacenter/kodi-binary-addons/audiodecoder.qsf/package.mk
+++ packages/mediacenter/kodi-binary-addons/audiodecoder.qsf/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="audiodecoder.qsf"
-PKG_VERSION="294fd39"
+PKG_VERSION="6aaf860"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
