6f00a01a24b28dc37b286dbd756fdb91f060ba3a
Stephan Raue <stephan@openelec.tv>
2015-05-12 10:03:32 +0200

    mesa: update to mesa-10.5.5
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="mesa"
-PKG_VERSION="10.5.4"
+PKG_VERSION="10.5.5"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
