509e3844fe0535fe5bc82353e29103531cab0c83
vpeter4 <peter.vicman@gmail.com>
2015-05-17 17:23:20 +0200

    add imx6 mfgtool2 package

---
+++ packages/tools/imx6-mfgtool2-tbs-matrix/package.mk
@@ -0,0 +1,41 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
+#
+#  OpenELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  OpenELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="imx6-mfgtool2-tbs-matrix"
+PKG_VERSION="1.0"
+PKG_REV="1"
+PKG_ARCH="arm"
+PKG_LICENSE="GPL"
+PKG_SITE="http://sourceforge.net/projects/matrixtv/"
+PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.zip"
+PKG_SOURCE_DIR="$PKG_NAME"
+PKG_DEPENDS_TARGET=""
+PKG_PRIORITY="optional"
+PKG_SECTION="tools"
+PKG_SHORTDESC="MfgTool2 flash tool"
+PKG_LONGDESC="Freescales manufacturing tool for flashing image to TBS Matrix system"
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+make_target() {
+  : # nothing
+}
+
+makeinstall_target() {
+  : # install from image script
+}
