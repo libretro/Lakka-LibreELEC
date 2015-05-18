04e3b4e0f34e614eaaf2a6564be7cf890f835729
Stephan Raue <stephan@openelec.tv>
2015-05-18 14:15:45 +0200

    randrproto: update to randrproto-1.5.0
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/x11/proto/randrproto/package.mk
+++ packages/x11/proto/randrproto/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="randrproto"
-PKG_VERSION="1.4.1"
+PKG_VERSION="1.5.0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
