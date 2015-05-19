9eb3b6d0a964ddcb696b57534065b7a93c341bda
Stephan Raue <stephan@openelec.tv>
2015-05-19 09:27:07 +0200

    linux: update to linux-4.0.4
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -40,7 +40,7 @@ case "$LINUX" in
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET imx6-status-led imx6-soc-fan"
     ;;
   *)
-    PKG_VERSION="4.0.3"
+    PKG_VERSION="4.0.4"
     PKG_URL="http://www.kernel.org/pub/linux/kernel/v4.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     ;;
 esac
