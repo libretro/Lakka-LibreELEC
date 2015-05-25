0bc39e16f811d08ae23c3df14fd00984ff5a2d37
Stefan Saraev <stefan@saraev.ca>
2015-05-25 12:32:10 +0300

    vdr-plugin-vnsiserver: update to vdr-plugin-vnsiserver-a7b0670

--- packages/multimedia/vdr-plugin-vnsiserver/package.mk
+++ packages/multimedia/vdr-plugin-vnsiserver/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="vdr-plugin-vnsiserver"
-PKG_VERSION="36e2b61"
+PKG_VERSION="a7b0670"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
