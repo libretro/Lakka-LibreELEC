08f603651fddee75111209d2a8227f48ade98fc2
Stefan Saraev <stefan@saraev.ca>
2015-05-25 19:18:25 +0300

    vdr-addon: bump

--- packages/addons/service/multimedia/vdr-addon/package.mk
+++ packages/addons/service/multimedia/vdr-addon/package.mk
@@ -19,7 +19,7 @@
 
 PKG_NAME="vdr-addon"
 PKG_VERSION="6.0"
-PKG_REV="0"
+PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.openelec.tv"
@@ -33,7 +33,7 @@ PKG_AUTORECONF="no"
 PKG_IS_ADDON="yes"
 PKG_ADDON_TYPE="xbmc.service"
 PKG_ADDON_PROVIDES=""
-PKG_ADDON_REQUIRES="pvr.vdr.vnsi:0.0.0"
+PKG_ADDON_REQUIRES="pvr.vdr.vnsi:0.0.0 script.config.vdr:0.0.0"
 PKG_ADDON_NAME="VDR PVR Backend"
 PKG_ADDON_REPOVERSION="6.0"
 
