28555d82047d7b5a9b7f18510cd134f4cd13c982
Alex Deryskyba <alex@codesnake.com>
2015-05-31 15:43:57 +0200

    packages/linux-firmware: Split brcmfmac_sdio-firmware package into two separate packages for iMX and Amlogic based devices

--- packages/linux-firmware/brcmfmac_sdio-firmware-aml/package.mk
+++ packages/linux-firmware/brcmfmac_sdio-firmware-aml/package.mk
@@ -16,7 +16,7 @@
 #  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-PKG_NAME="brcmfmac_sdio-firmware"
+PKG_NAME="brcmfmac_sdio-firmware-aml"
 PKG_VERSION="0.1"
 PKG_REV="1"
 PKG_ARCH="any"
@@ -26,8 +26,8 @@ PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
 PKG_SECTION="firmware"
-PKG_SHORTDESC="brcmfmac_sdio-firmware: firmware for brcm bluetooth devices"
-PKG_LONGDESC="Firmware for Broadcom Bluetooth devices and brcm-patchram-plus that downloads a patchram files in the HCD format to the Bluetooth based silicon and combo chips and other utility functions."
+PKG_SHORTDESC="brcmfmac_sdio-firmware: firmware for brcm bluetooth chips used in some Amlogic based devices"
+PKG_LONGDESC="Firmware for Broadcom Bluetooth devices used in some Amlogic based devices, and brcm-patchram-plus that downloads a patchram files in the HCD format to the Bluetooth based silicon and combo chips and other utility functions."
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
