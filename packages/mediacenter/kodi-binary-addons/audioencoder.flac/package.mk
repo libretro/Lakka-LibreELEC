a1593e2268ea6b813febb4fb74dcea547fd11732
Stephan Raue <stephan@openelec.tv>
2015-06-05 09:52:57 +0200

    audioencoder.flac: update to audioencoder.flac-a5e2d12
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/kodi-binary-addons/audioencoder.flac/package.mk
+++ packages/mediacenter/kodi-binary-addons/audioencoder.flac/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="audioencoder.flac"
-PKG_VERSION="84acb14"
+PKG_VERSION="a5e2d12"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
