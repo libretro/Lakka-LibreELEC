13595f9263151666306698a677cdce8b619591b4
Stephan Raue <stephan@openelec.tv>
2015-06-09 14:25:29 +0200

    fuse: update to fuse-2.9.4
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/sysutils/fuse/package.mk
+++ packages/sysutils/fuse/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="fuse"
-PKG_VERSION="2.9.3"
+PKG_VERSION="2.9.4"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
