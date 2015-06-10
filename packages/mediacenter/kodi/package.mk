753c9935ceb33d2f02c515e57eb67a995b8a15a9
Stephan Raue <stephan@openelec.tv>
2015-06-10 11:39:23 +0200

    kodi: update to kodi-15.0-beta2-658e700
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="15.0-beta2-81a115b"
+PKG_VERSION="15.0-beta2-658e700"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
