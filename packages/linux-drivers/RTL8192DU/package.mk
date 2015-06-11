fa9f3c28693b08a928edc4c0e83f758b79b7f262
Stephan Raue <stephan@openelec.tv>
2015-06-11 10:00:23 +0200

    RTL8192DU: update to RTL8192DU-7498302, this fixes #4187
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/linux-drivers/RTL8192DU/package.mk
+++ packages/linux-drivers/RTL8192DU/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="RTL8192DU"
-PKG_VERSION="085e206"
+PKG_VERSION="7498302"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
