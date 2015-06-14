e9b462cda4288ca121083b4e16e0394b189ccf97
Stephan Raue <stephan@openelec.tv>
2015-06-14 20:33:31 +0200

    audiodecoder.timidity: update to audiodecoder.timidity-da5eb9a
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/kodi-binary-addons/audiodecoder.timidity/package.mk
+++ packages/mediacenter/kodi-binary-addons/audiodecoder.timidity/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="audiodecoder.timidity"
-PKG_VERSION="7f079c1"
+PKG_VERSION="da5eb9a"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
