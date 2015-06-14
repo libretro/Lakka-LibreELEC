6da47babe398ea972f9f8445a133946a9b9e9ae8
Stephan Raue <stephan@openelec.tv>
2015-06-14 20:36:42 +0200

    pvr.vdr.vnsi: update to pvr.vdr.vnsi-033cc50
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/kodi-binary-addons/pvr.vdr.vnsi/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.vdr.vnsi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="pvr.vdr.vnsi"
-PKG_VERSION="244b893"
+PKG_VERSION="033cc50"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
