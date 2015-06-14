f3b07050bca6b99f2731639fd3fca71b3d38402c
Stephan Raue <stephan@openelec.tv>
2015-06-14 20:32:25 +0200

    audiodecoder.snesapu: update to audiodecoder.snesapu-399d1d3
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/kodi-binary-addons/audiodecoder.snesapu/package.mk
+++ packages/mediacenter/kodi-binary-addons/audiodecoder.snesapu/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="audiodecoder.snesapu"
-PKG_VERSION="2bd1e34"
+PKG_VERSION="399d1d3"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
