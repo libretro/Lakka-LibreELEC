40dc855eaf88f12cb446951c8a63ab4e18a40e4f
Stephan Raue <stephan@openelec.tv>
2015-06-14 20:35:45 +0200

    pvr.stalker: update to pvr.stalker-a8c3e45
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/kodi-binary-addons/pvr.stalker/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.stalker/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="pvr.stalker"
-PKG_VERSION="2d65b21"
+PKG_VERSION="a8c3e45"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
