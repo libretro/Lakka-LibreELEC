02d7ed594923080d94cdf252ec28c5a89b677677
Stephan Raue <stephan@openelec.tv>
2015-06-14 20:30:57 +0200

    audiodecoder.nosefart: update to audiodecoder.nosefart-936313f
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/kodi-binary-addons/audiodecoder.nosefart/package.mk
+++ packages/mediacenter/kodi-binary-addons/audiodecoder.nosefart/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="audiodecoder.nosefart"
-PKG_VERSION="a49ba6e"
+PKG_VERSION="936313f"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
