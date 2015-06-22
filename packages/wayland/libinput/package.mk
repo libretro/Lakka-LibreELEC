a719434fd349ae0bd1af4c9cd07d38667713a400
Stephan Raue <stephan@openelec.tv>
2015-06-22 16:04:05 +0200

    libinput: update to libinput-0.18.0
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/wayland/libinput/package.mk
+++ packages/wayland/libinput/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libinput"
-PKG_VERSION="0.17.0"
+PKG_VERSION="0.18.0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
