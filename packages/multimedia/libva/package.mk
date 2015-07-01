96e5cfe3110337962056936d9b1797d2bc131f95
Stephan Raue <stephan@openelec.tv>
2015-07-01 20:15:46 +0200

    libva: update to libva-1.6.0
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/multimedia/libva/package.mk
+++ packages/multimedia/libva/package.mk
@@ -17,12 +17,12 @@
 ################################################################################
 
 PKG_NAME="libva"
-PKG_VERSION="1.6.0.pre1"
+PKG_VERSION="1.6.0"
 PKG_REV="1"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL"
 PKG_SITE="http://freedesktop.org/wiki/Software/vaapi"
-PKG_URL="http://www.freedesktop.org/software/vaapi/testing/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_URL="http://www.freedesktop.org/software/vaapi/releases/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain libX11 libXext libXfixes libdrm mesa glu"
 PKG_PRIORITY="optional"
 PKG_SECTION="multimedia"
