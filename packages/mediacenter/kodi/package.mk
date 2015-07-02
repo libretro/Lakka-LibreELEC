85590a4a71514f4e813be0f0c9c2ced00f0d311f
Stephan Raue <stephan@openelec.tv>
2015-07-03 00:03:03 +0200

    kodi: update to kodi-15.0-rc1-9ff25f8
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="15.0-rc2-58ccb8b"
+PKG_VERSION="15.0-rc1-9ff25f8"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
