66269ed5c769200e75c048ba5c98319518f9705f
MilhouseVH <milhouseVH.github@nmacleod.com>
2015-07-06 21:16:02 +0100

    Bump libshairplay in anticipation of Kodi PR7093

--- packages/network/libshairplay/package.mk
+++ packages/network/libshairplay/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libshairplay"
-PKG_VERSION="6e521c7"
+PKG_VERSION="0f41ade"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
