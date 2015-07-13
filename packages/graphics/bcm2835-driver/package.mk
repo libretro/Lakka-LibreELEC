196021b48337785da919bd81abb8c66a778ae68c
Stephan Raue <stephan@openelec.tv>
2015-07-13 09:42:40 +0200

    bcm2835-driver: update to bcm2835-driver-47d51d6
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/graphics/bcm2835-driver/package.mk
+++ packages/graphics/bcm2835-driver/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="bcm2835-driver"
-PKG_VERSION="89881b5"
+PKG_VERSION="47d51d6"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="nonfree"
