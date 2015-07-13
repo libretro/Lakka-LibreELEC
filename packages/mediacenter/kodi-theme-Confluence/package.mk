4d44f9c0d5b117b71c1711a331d634ba94a67a50
Stephan Raue <stephan@openelec.tv>
2015-07-13 09:43:19 +0200

    kodi: update to kodi-15.0-rc2-0aa930b
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/kodi-theme-Confluence/package.mk
+++ packages/mediacenter/kodi-theme-Confluence/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi-theme-Confluence"
-PKG_VERSION="15.0-rc1-9ff25f8"
+PKG_VERSION="15.0-rc2-0aa930b"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
