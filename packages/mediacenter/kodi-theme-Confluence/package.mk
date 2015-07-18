0635203b10e5b0e544dbbfbd7ef473e3e72a271b
Stefan Saraev <stefan@saraev.ca>
2015-07-18 12:19:04 +0300

    kodi: update to kodi-15.0-rc3-284f66a

--- packages/mediacenter/kodi-theme-Confluence/package.mk
+++ packages/mediacenter/kodi-theme-Confluence/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi-theme-Confluence"
-PKG_VERSION="15.0-rc2-0aa930b"
+PKG_VERSION="15.0-rc3-284f66a"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
