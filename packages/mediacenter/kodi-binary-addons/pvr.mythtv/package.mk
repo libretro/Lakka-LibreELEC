40fa430c0cb667d0fba92e98b257c1df27c3f790
Stefan Saraev <stefan@saraev.ca>
2015-07-19 22:07:53 +0300

    pvr.mythtv: update to pvr.mythtv-538e24d

--- packages/mediacenter/kodi-binary-addons/pvr.mythtv/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.mythtv/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="pvr.mythtv"
-PKG_VERSION="75fee97"
+PKG_VERSION="538e24d"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
