5fd79560ba6c0f7b9e3b19175d8758b7e7eae78c
Stephan Raue <stephan@openelec.tv>
2015-07-24 09:52:04 +0200

    binutils: update to binutils-2.25.1
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/devel/binutils/package.mk
+++ packages/devel/binutils/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="binutils"
-PKG_VERSION="2.25"
+PKG_VERSION="2.25.1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
