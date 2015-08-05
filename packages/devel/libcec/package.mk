067fb8464a035c7c96bc524c711e2218fba1b850
Stephan Raue <stephan@openelec.tv>
2015-08-05 14:22:43 +0200

    libcec: update to libcec-3.0.1
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/devel/libcec/package.mk
+++ packages/devel/libcec/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libcec"
-PKG_VERSION="3.0.0"
+PKG_VERSION="3.0.1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
