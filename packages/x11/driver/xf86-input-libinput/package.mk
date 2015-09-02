4aff2c22d6acbe38d6dac264a58761cc4c20116b
Stephan Raue <stephan@openelec.tv>
2015-09-02 09:37:42 +0200

    xf86-input-libinput: update to xf86-input-libinput-0.14.0
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/x11/driver/xf86-input-libinput/package.mk
+++ packages/x11/driver/xf86-input-libinput/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="xf86-input-libinput"
-PKG_VERSION="0.13.0"
+PKG_VERSION="0.14.0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
