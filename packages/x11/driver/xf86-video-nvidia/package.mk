b0ebdc4b40e7c72bd9346749babcc29e677f139c
Stephan Raue <stephan@openelec.tv>
2015-09-03 14:49:47 +0200

    xf86-video-nvidia: update to xf86-video-nvidia-352.41
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/x11/driver/xf86-video-nvidia/package.mk
+++ packages/x11/driver/xf86-video-nvidia/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="xf86-video-nvidia"
-PKG_VERSION="352.30"
+PKG_VERSION="352.41"
 PKG_REV="1"
 PKG_ARCH="x86_64"
 PKG_LICENSE="nonfree"
