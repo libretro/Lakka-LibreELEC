40000d6c6703b2433ec5002af2895740856297b0
Stephan Raue <stephan@openelec.tv>
2015-09-03 14:51:46 +0200

    libvdpau: update to libvdpau-1.1.1
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/multimedia/libvdpau/package.mk
+++ packages/multimedia/libvdpau/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libvdpau"
-PKG_VERSION="1.1"
+PKG_VERSION="1.1.1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
