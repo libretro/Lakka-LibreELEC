2e46e15b3f5dd954551c1e3c5a9acdaa6184889d
qods <hlwwffnh@sharklasers.com>
2015-09-17 22:51:19 +0200

    libcdio: update to libcdio-0.93, unpdate links and licence

--- packages/audio/libcdio/package.mk
+++ packages/audio/libcdio/package.mk
@@ -17,12 +17,12 @@
 ################################################################################
 
 PKG_NAME="libcdio"
-PKG_VERSION="0.92"
+PKG_VERSION="0.93"
 PKG_REV="1"
 PKG_ARCH="any"
-PKG_LICENSE="GPL"
-PKG_SITE="http://www.gnu.org/software/libcdio/"
-PKG_URL="http://ftp.gnu.org/gnu/libcdio/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://www.gnu.org/software/libcdio/"
+PKG_URL="https://ftp.gnu.org/gnu/libcdio/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
 PKG_SECTION="audio"
