d70062793766aea9bbe6b08ed905a6916f9d9d48
qods <hlwwffnh@sharklasers.com>
2015-09-22 01:36:25 +0200

    libusb: update to libusb-1.0.20, update license and link

--- packages/sysutils/libusb/package.mk
+++ packages/sysutils/libusb/package.mk
@@ -17,11 +17,11 @@
 ################################################################################
 
 PKG_NAME="libusb"
-PKG_VERSION="1.0.19"
+PKG_VERSION="1.0.20"
 PKG_REV="1"
 PKG_ARCH="any"
-PKG_LICENSE="GPL"
-PKG_SITE="http://libusb.sourceforge.net/"
+PKG_LICENSE="LGPLv2.1"
+PKG_SITE="http://libusb.info/"
 PKG_URL="$SOURCEFORGE_SRC/libusb/files/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain systemd"
 PKG_PRIORITY="optional"
