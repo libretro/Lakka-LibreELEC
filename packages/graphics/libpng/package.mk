cd56447dee02bab6ffa2e61a272e2f90f91d2c30
Stephan Raue <stephan@openelec.tv>
2015-10-03 20:53:55 +0200

    libpng: update to libpng-1.6.18
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/graphics/libpng/package.mk
+++ packages/graphics/libpng/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libpng"
-PKG_VERSION="1.6.17"
+PKG_VERSION="1.6.18"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
