4bfb721783a5c76a7458a28b367fc8abd82e7b29
Stephan Raue <stephan@openelec.tv>
2015-10-04 13:04:57 +0200

    imx-vpu: update to imx-vpu-5.4.31
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/multimedia/imx-vpu/package.mk
+++ packages/multimedia/imx-vpu/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="imx-vpu"
-PKG_VERSION="5.4.28"
+PKG_VERSION="5.4.31"
 PKG_REV="1"
 PKG_ARCH="arm"
 PKG_LICENSE="other"
