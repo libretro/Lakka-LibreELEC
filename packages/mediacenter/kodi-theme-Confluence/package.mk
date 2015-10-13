b37493f278922d0cb8fb7c46614f8a170fc98728
Stephan Raue <stephan@openelec.tv>
2015-10-13 19:10:36 +0200

    kodi: update to kodi-15.2-rc3-9efd358
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/kodi-theme-Confluence/package.mk
+++ packages/mediacenter/kodi-theme-Confluence/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi-theme-Confluence"
-PKG_VERSION="15.2-rc3-cf72616"
+PKG_VERSION="15.2-rc3-9efd358"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
