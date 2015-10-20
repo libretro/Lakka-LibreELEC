e24932e6326a8c949911a06ab7dee917b91f6544
Stefan Saraev <stefan@saraev.ca>
2015-10-20 01:23:10 -0700

    (jarvis) new package: asplib

---
+++ packages/audio/asplib/package.mk
@@ -0,0 +1,40 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
+#
+#  OpenELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  OpenELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="asplib"
+PKG_VERSION="39fe0a7"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/AchimTuran/asplib"
+PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="audio"
+PKG_SHORTDESC="asplib: Achim's Signal Processing LIBrary"
+PKG_LONGDESC="asplib is a small and lightweight C++ library for digital signal processing."
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+configure_target() {
+  cmake -DCMAKE_TOOLCHAIN_FILE=$CMAKE_CONF \
+    -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_SHARED_LIBS=0 \
+    -DCMAKE_CXX_FLAGS="$CXXFLAGS -DTARGET_LINUX" \
+    ..
+}
