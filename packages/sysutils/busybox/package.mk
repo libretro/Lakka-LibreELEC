2813f9d9acc054d7643a63f477628a96ec309757
Stephan Raue <stephan@openelec.tv>
2015-10-24 22:41:14 +0200

    busybox: update to busybox-1.24.1
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/sysutils/busybox/package.mk
+++ packages/sysutils/busybox/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="busybox"
-PKG_VERSION="1.24.0"
+PKG_VERSION="1.24.1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
