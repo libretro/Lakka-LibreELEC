257e39416b82874feffbb8f04aac484c59f7da38
Stephan Raue <stephan@openelec.tv>
2015-11-01 03:50:01 +0100

    libbluray: update to libbluray-0.9.0
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/multimedia/libbluray/package.mk
+++ packages/multimedia/libbluray/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libbluray"
-PKG_VERSION="0.8.1"
+PKG_VERSION="0.9.0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
