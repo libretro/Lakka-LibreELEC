OpenELEC/OpenELEC.tv

(jarvis) bump binary addons

packages/mediacenter/kodi-binary-addons/pvr.nextpvr/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="pvr.nextpvr"
-PKG_VERSION="3a205e4"
+PKG_VERSION="ef36c94"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"

packages/mediacenter/kodi-binary-addons/visualization.goom/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="visualization.goom"
-PKG_VERSION="fb5933b"
+PKG_VERSION="a559815"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"

libretro/Lakka-LibreELEC

Signed-off-by: Stephan Raue <stephan@openelec.tv>

packages/mediacenter/kodi-binary-addons/pvr.nextpvr/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="pvr.nextpvr"
-PKG_VERSION="3a205e4"
+PKG_VERSION="b902321"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"

packages/mediacenter/kodi-binary-addons/visualization.goom/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="visualization.goom"
-PKG_VERSION="fb5933b"
+PKG_VERSION="f8dea02"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"