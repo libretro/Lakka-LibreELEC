40300a7a85dd10c5c0d1875abaa407e802ed2dca
Stephan Raue <stephan@openelec.tv>
2015-11-17 13:57:25 +0100

    libevdev: update to util-linux-2.27.1
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/sysutils/util-linux/package.mk
+++ packages/sysutils/util-linux/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="util-linux"
-PKG_VERSION="2.27"
+PKG_VERSION="2.27.1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
