10bdc708bfc37cada6f8835e715103622069edd8
Stephan Raue <stephan@openelec.tv>
2015-11-17 01:07:39 +0100

    libtirpc: update to libtirpc-1.0.1
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/network/libtirpc/package.mk
+++ packages/network/libtirpc/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libtirpc"
-PKG_VERSION="0.3.2"
+PKG_VERSION="1.0.1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
