b35878f5ff1e13c1c0ac973c0a1e49ebba32274f
Stephan Raue <stephan@openelec.tv>
2015-11-17 13:56:34 +0100

    fuse-exfat: update to fuse-exfat-1.2.2
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/sysutils/fuse-exfat/package.mk
+++ packages/sysutils/fuse-exfat/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="fuse-exfat"
-PKG_VERSION="1.2.0"
+PKG_VERSION="1.2.2"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2+"
