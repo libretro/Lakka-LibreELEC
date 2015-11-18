e147d69cd554f57023cd3551d925d4bd1026dc31
Stephan Raue <stephan@openelec.tv>
2015-11-18 22:15:24 +0100

    libirman: update to libirman-0.5.0
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/devel/libirman/package.mk
+++ packages/devel/libirman/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libirman"
-PKG_VERSION="0.4.5"
+PKG_VERSION="0.5.0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
