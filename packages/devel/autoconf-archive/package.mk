dfd78310bd058e9e898affbf7adeb45fb42cf058
Stephan Raue <stephan@openelec.tv>
2015-11-18 22:05:54 +0100

    autoconf-archive: update to autoconf-archive-2015.09.25
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/devel/autoconf-archive/package.mk
+++ packages/devel/autoconf-archive/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="autoconf-archive"
-PKG_VERSION="2015.02.24"
+PKG_VERSION="2015.09.25"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
