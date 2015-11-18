f574a01b9ebba5f475c9be491f2de32f69132677
Stephan Raue <stephan@openelec.tv>
2015-11-18 22:12:09 +0100

    elfutils: update to elfutils-0.164
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/devel/elfutils/package.mk
+++ packages/devel/elfutils/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="elfutils"
-PKG_VERSION="0.161"
+PKG_VERSION="0.164"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
