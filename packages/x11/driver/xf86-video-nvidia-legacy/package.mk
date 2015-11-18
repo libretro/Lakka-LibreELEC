ba6d61c207717775f3d8a665b6262f3271a575dc
Stephan Raue <stephan@openelec.tv>
2015-11-18 10:06:19 +0100

    xf86-video-nvidia-legacy: update to xf86-video-nvidia-legacy-304.131
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/x11/driver/xf86-video-nvidia-legacy/package.mk
+++ packages/x11/driver/xf86-video-nvidia-legacy/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="xf86-video-nvidia-legacy"
-PKG_VERSION="304.128"
+PKG_VERSION="304.131"
 PKG_REV="1"
 PKG_ARCH="x86_64"
 PKG_LICENSE="nonfree"
