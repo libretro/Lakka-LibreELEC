49ad93c077c30ecad3d1fb601ddc5f9ca97dc426
Stephan Raue <stephan@openelec.tv>
2015-11-18 12:25:03 +0100

    Pillow: update to Pillow-3.0.0
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/python/graphics/Pillow/package.mk
+++ packages/python/graphics/Pillow/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="Pillow"
-PKG_VERSION="2.7.0"
+PKG_VERSION="3.0.0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="BSD"
