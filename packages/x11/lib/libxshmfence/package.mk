83598586949a36d9b42e8070326c73caf72f6837
Stephan Raue <stephan@openelec.tv>
2015-11-18 14:24:55 +0100

    libxshmfence: update to libxshmfence-1.2
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/x11/lib/libxshmfence/package.mk
+++ packages/x11/lib/libxshmfence/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libxshmfence"
-PKG_VERSION="1.1"
+PKG_VERSION="1.2"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
