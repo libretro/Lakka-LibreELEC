1b55e53853ba6e8da91b9ada87946a6cb9b75c76
Stephan Raue <stephan@openelec.tv>
2015-11-19 09:25:01 +0100

    fakeroot: add buildnote
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/devel/fakeroot/package.mk
+++ packages/devel/fakeroot/package.mk
@@ -16,6 +16,10 @@
 #  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
+# fakeroot-1.20.2 depends on libcap:host, which depends on attr:host
+# there are reported buildproblems with attr:host, which should be replicated
+# use fakeroot-1.18.4 instead until attr:host builds 
+
 PKG_NAME="fakeroot"
 PKG_VERSION="1.18.4"
 PKG_REV="1"
