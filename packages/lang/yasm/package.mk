16bfdd91a0691bbb777ab0bc7c3cab1b36306bd8
Stephan Raue <stephan@openelec.tv>
2015-11-19 14:39:03 +0100

    yasm: update to yasm-1.3.0
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/lang/yasm/package.mk
+++ packages/lang/yasm/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="yasm"
-PKG_VERSION="1.2.0"
+PKG_VERSION="1.3.0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="BSD"
