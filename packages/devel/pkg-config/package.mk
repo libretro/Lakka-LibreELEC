57e5f0e7df067867fe1733e20cc6584ccbf8bcd8
Stephan Raue <stephan@openelec.tv>
2015-11-19 09:39:46 +0100

    pkg-config: update to pkg-config-0.29
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/devel/pkg-config/package.mk
+++ packages/devel/pkg-config/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="pkg-config"
-PKG_VERSION="0.28"
+PKG_VERSION="0.29"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
@@ -38,5 +38,5 @@ PKG_CONFIGURE_OPTS_HOST="--disable-silent-rules \
 
 post_makeinstall_host() {
   mkdir -p $SYSROOT_PREFIX/usr/share/aclocal
-    cp ../pkg.m4 $SYSROOT_PREFIX/usr/share/aclocal
+    cp pkg.m4 $SYSROOT_PREFIX/usr/share/aclocal
 }
