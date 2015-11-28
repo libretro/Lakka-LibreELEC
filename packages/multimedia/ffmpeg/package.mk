2a06ca0cf9dca364a1ce8eea059d2c33d00e20fa
fritsch <peter.fruehberger@gmail.com>
2015-11-28 15:31:23 +0100

    ffmpeg: update to ffmpeg-2.8.3

--- packages/multimedia/ffmpeg/package.mk
+++ packages/multimedia/ffmpeg/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="ffmpeg"
-PKG_VERSION="2.8.2"
+PKG_VERSION="2.8.3"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="LGPLv2.1+"
