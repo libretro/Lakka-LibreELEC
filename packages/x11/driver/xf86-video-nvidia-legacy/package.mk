bae6dcc1cd8e344debe1488ac831b2783d297ca2
Lukas Rusak <lorusak@gmail.com>
2015-12-09 15:19:02 -0800

    xf86-video-nvidia-legacy: package to allow multiple nvidia drivers

--- packages/x11/driver/xf86-video-nvidia-legacy/package.mk
+++ packages/x11/driver/xf86-video-nvidia-legacy/package.mk
@@ -45,16 +45,18 @@ make_target() {
 
   cd kernel
     make module CC=$CC SYSSRC=$(kernel_path) SYSOUT=$(kernel_path)
+    $STRIP --strip-debug nvidia.ko
   cd ..
 }
 
 makeinstall_target() {
   mkdir -p $INSTALL/$XORG_PATH_MODULES/drivers
-    cp -P nvidia_drv.so $INSTALL/$XORG_PATH_MODULES/drivers
+    cp -P nvidia_drv.so $INSTALL/$XORG_PATH_MODULES/drivers/nvidia-legacy_drv.so
+    ln -sf /var/lib/nvidia_drv.so $INSTALL/$XORG_PATH_MODULES/drivers/nvidia_drv.so
 
   mkdir -p $INSTALL/$XORG_PATH_MODULES/extensions
   # rename to not conflicting with Mesa libGL.so
-    cp -P libglx.so* $INSTALL/$XORG_PATH_MODULES/extensions/libglx_nvidia.so
+    cp -P libglx.so* $INSTALL/$XORG_PATH_MODULES/extensions/libglx_nvidia-legacy.so
 
   mkdir -p $INSTALL/etc/X11
     cp $PKG_DIR/config/*.conf $INSTALL/etc/X11
@@ -62,19 +64,25 @@ makeinstall_target() {
   mkdir -p $INSTALL/usr/lib
     cp -P libnvidia-glcore.so.$PKG_VERSION $INSTALL/usr/lib
     cp -P libnvidia-ml.so.$PKG_VERSION $INSTALL/usr/lib
-      ln -sf libnvidia-ml.so.$PKG_VERSION $INSTALL/usr/lib/libnvidia-ml.so.1
+      ln -sf /var/lib/libnvidia-ml.so.1 $INSTALL/usr/lib/libnvidia-ml.so.1
     cp -P tls/libnvidia-tls.so.$PKG_VERSION $INSTALL/usr/lib
   # rename to not conflicting with Mesa libGL.so
-    cp -P libGL.so* $INSTALL/usr/lib/libGL_nvidia.so.1
+    cp -P libGL.so* $INSTALL/usr/lib/libGL_nvidia-legacy.so.1
 
   mkdir -p $INSTALL/lib/modules/$(get_module_dir)/nvidia
-    cp kernel/nvidia.ko $INSTALL/lib/modules/$(get_module_dir)/nvidia
+    ln -sf /var/lib/nvidia.ko $INSTALL/lib/modules/$(get_module_dir)/nvidia/nvidia.ko
+
+  mkdir -p $INSTALL/usr/lib/nvidia-legacy
+    cp kernel/nvidia.ko $INSTALL/usr/lib/nvidia-legacy
 
   mkdir -p $INSTALL/usr/bin
-    cp nvidia-smi $INSTALL/usr/bin
-    cp nvidia-xconfig $INSTALL/usr/bin
+    ln -s /var/lib/nvidia-smi $INSTALL/usr/bin/nvidia-smi
+    cp nvidia-smi $INSTALL/usr/bin/nvidia-legacy-smi
+    ln -s /var/lib/nvidia-xconfig $INSTALL/usr/bin/nvidia-xconfig
+    cp nvidia-xconfig $INSTALL/usr/bin/nvidia-legacy-xconfig
 
   mkdir -p $INSTALL/usr/lib/vdpau
-    cp libvdpau_nvidia.so* $INSTALL/usr/lib/vdpau/libvdpau_nvidia.so.1
-    ln -sf libvdpau_nvidia.so.1 $INSTALL/usr/lib/vdpau/libvdpau_nvidia.so
+    cp libvdpau_nvidia.so* $INSTALL/usr/lib/vdpau/libvdpau_nvidia-legacy.so.1
+    ln -sf /var/lib/libvdpau_nvidia.so $INSTALL/usr/lib/vdpau/libvdpau_nvidia.so
+    ln -sf /var/lib/libvdpau_nvidia.so.1 $INSTALL/usr/lib/vdpau/libvdpau_nvidia.so.1
 }
