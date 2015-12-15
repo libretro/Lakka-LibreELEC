2d39a5761f48d0389d6a995f6f921ae6666ba5ae
Lukas Rusak <lorusak@gmail.com>
2015-12-15 08:46:57 -0800

    mesa: update to 11.0.7

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="mesa"
-PKG_VERSION="11.0.6"
+PKG_VERSION="11.0.7"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
