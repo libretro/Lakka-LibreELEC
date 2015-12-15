80ce77194b63bdc864244d178d30a61c10519404
Alex Deryskyba <alex@codesnake.com>
2015-12-15 10:45:51 +0100

    packages/linux-drivers/wetekdvb: Update WeTek proprietary DVB driver to 20151215

--- packages/linux-drivers/wetekdvb/package.mk
+++ packages/linux-drivers/wetekdvb/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="wetekdvb"
-PKG_VERSION="20151007"
+PKG_VERSION="20151215"
 PKG_REV="1"
 PKG_ARCH="arm"
 PKG_LICENSE="nonfree"
