1bdb12cf18ce4716b109d6deb3834e9de2ddfbf7
Stephan Raue <stephan@openelec.tv>
2015-12-21 08:06:25 +0100

    iptables: update to iptables-1.6.0
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/network/iptables/package.mk
+++ packages/network/iptables/package.mk
@@ -17,13 +17,13 @@
 ################################################################################
 
 PKG_NAME="iptables"
-PKG_VERSION="1.4.21"
+PKG_VERSION="1.6.0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.netfilter.org/"
 PKG_URL="http://www.netfilter.org/projects/iptables/files/$PKG_NAME-$PKG_VERSION.tar.bz2"
-PKG_DEPENDS_TARGET="toolchain linux"
+PKG_DEPENDS_TARGET="toolchain linux libmnl libnftnl"
 PKG_PRIORITY="optional"
 PKG_SECTION="network"
 PKG_SHORTDESC="iptables: IP packet filter administration"
