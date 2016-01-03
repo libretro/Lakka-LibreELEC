0f37e9075a6c2392279a1e86c2ea0695ebb04084
Stephan Raue <stephan@openelec.tv>
2016-01-03 14:30:34 +0100

    iwlwifi-firmware: update to iwlwifi-firmware-0.0.5
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/linux-firmware/iwlwifi-firmware/package.mk
+++ packages/linux-firmware/iwlwifi-firmware/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="iwlwifi-firmware"
-PKG_VERSION="0.0.4"
+PKG_VERSION="0.0.5"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Free-to-use"
