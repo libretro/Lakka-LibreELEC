0e0eabf27b0504ec150dcd7da4a29a87cb5c75f7
CvH <namerp@googlemail.com>
2016-01-11 10:43:11 +0100

    vdr-plugin-xmltv2vdr: update to vdr-plugin-xmltv2vdr-b48e0bec

--- packages/multimedia/vdr-plugin-xmltv2vdr/package.mk
+++ packages/multimedia/vdr-plugin-xmltv2vdr/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="vdr-plugin-xmltv2vdr"
-PKG_VERSION="30903cc0"
+PKG_VERSION="b48e0bec"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
