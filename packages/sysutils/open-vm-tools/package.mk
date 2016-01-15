faabe3f5a62807dc8353d7c300532cf73a33c255
Lukas Rusak <lorusak@gmail.com>
2016-01-15 23:32:59 +0100

    open-vm-tools: initial import

---
+++ packages/sysutils/open-vm-tools/package.mk
@@ -0,0 +1,74 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
+#      Copyright (C) 2011 Anthony Nash (nash.ant@gmail.com)
+#
+#  OpenELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  OpenELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="open-vm-tools"
+PKG_VERSION="stable-10.0.7"
+PKG_REV="1"
+PKG_ARCH="x86_64"
+PKG_LICENSE="GPL"
+PKG_SITE="http://open-vm-tools.sourceforge.net"
+PKG_URL="https://github.com/vmware/open-vm-tools/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain glib:host glib libdnet"
+PKG_PRIORITY="optional"
+PKG_SECTION="virtualization"
+PKG_SHORTDESC="open-vm-tools: open source implementation of VMware Tools"
+PKG_LONGDESC="open-vm-tools: open source implementation of VMware Tools"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="yes"
+
+OPENVMTOOLS_KERNEL_VER=$(basename $(ls -d $ROOT/$BUILD/linux-[0-9]*)| sed 's|linux-||g')
+
+PKG_CONFIGURE_OPTS_TARGET="--disable-docs \
+                           --disable-tests \
+                           --disable-deploypkg \
+                           --without-pam \
+                           --without-gtk2 \
+                           --without-gtkmm \
+                           --without-ssl \
+                           --without-x \
+                           --without-xerces \
+                           --without-icu \
+                           --without-procps \
+                           --without-kernel-modules \
+                           --with-sysroot=$SYSROOT_PREFIX"
+
+unpack() {
+  # ugly
+  $SCRIPTS/extract $PKG_NAME "$PKG_VERSION.tar.gz" $BUILD
+  mv $PKG_BUILD/$PKG_NAME/* $PKG_BUILD/
+  rm -r $PKG_BUILD/$PKG_NAME
+}
+
+pre_configure_target() {
+   export LIBS="-ldnet"
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib
+    cp -PR libvmtools/.libs/libvmtools.so* $INSTALL/usr/lib
+
+  mkdir -p $INSTALL/usr/bin
+    cp -PR services/vmtoolsd/.libs/vmtoolsd $INSTALL/usr/bin
+    cp -PR checkvm/.libs/vmware-checkvm $INSTALL/usr/bin
+}
+
+post_install() {
+  enable_service open-vm-tools.service
+}
