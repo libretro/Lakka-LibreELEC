61c7f1d580ca249379f6f2bcf7d6ac609badfcbf
Peter Vicman <peter.vicman@gmail.com>
2016-01-22 13:24:38 +0100

    v4l-utils: build without jpeg support

--- packages/sysutils/v4l-utils/package.mk
+++ packages/sysutils/v4l-utils/package.mk
@@ -34,6 +34,7 @@ PKG_LONGDESC="Linux V4L2 and DVB API utilities and v4l libraries (libv4l)."
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
+PKG_CONFIGURE_OPTS_TARGET="--without-jpeg"
 PKG_MAKEINSTALL_OPTS_TARGET="PREFIX=/usr -C utils/keytable"
 
 make_target() {
