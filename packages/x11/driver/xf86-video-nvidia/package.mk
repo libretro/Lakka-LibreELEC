818015f6ce56eaef9109a8c71f7f055293aac57d
MilhouseVH <milhouseVH.github@nmacleod.com>
2016-01-30 16:08:54 +0000

    xf86-video-nvidia: update to xf86-video-nvidia-352.79

--- packages/x11/driver/xf86-video-nvidia/package.mk
+++ packages/x11/driver/xf86-video-nvidia/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="xf86-video-nvidia"
-PKG_VERSION="352.63"
+PKG_VERSION="352.79"
 PKG_REV="1"
 PKG_ARCH="x86_64"
 PKG_LICENSE="nonfree"
