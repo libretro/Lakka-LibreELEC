cf411f8d4746f16d3cd09c07992c6075e76b3ed3
Lukas Rusak <lorusak@gmail.com>
2016-02-03 11:44:21 +0100

    libnfs: update to 1.10

--- packages/network/libnfs/package.mk
+++ packages/network/libnfs/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libnfs"
-PKG_VERSION="1.9.8"
+PKG_VERSION="1.10.0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
