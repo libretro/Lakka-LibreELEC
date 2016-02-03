b419c6e046a0a6e0eff8caf8c5257abf4d4a01d6
Lukas Rusak <lorusak@gmail.com>
2016-02-03 11:53:38 +0100

    libressl: update to 2.2.6

--- packages/security/libressl/package.mk
+++ packages/security/libressl/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libressl"
-PKG_VERSION="2.2.4"
+PKG_VERSION="2.2.6"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="BSD"
