41a4d5628f02ff42dd0a16ed4ff8631db459fca2
CvH <namerp@googlemail.com>
2016-02-05 21:04:25 +0100

    vdr-plugin-restfulapi: update to vdr-plugin-restfulapi-0.2.6.0
    - they messed up the release version -> change of dl url was needed

--- packages/multimedia/vdr-plugin-restfulapi/package.mk
+++ packages/multimedia/vdr-plugin-restfulapi/package.mk
@@ -17,12 +17,12 @@
 ################################################################################
 
 PKG_NAME="vdr-plugin-restfulapi"
-PKG_VERSION="0.2.5.6"
+PKG_VERSION="0.2.6.0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/yavdr/vdr-plugin-restfulapi"
-PKG_URL="https://github.com/yavdr/${PKG_NAME}/releases/download/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/yavdr/${PKG_NAME}/releases/download/2.6.0/${PKG_NAME}-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain vdr cxxtools vdr-wirbelscan"
 PKG_PRIORITY="optional"
 PKG_SECTION="multimedia"
