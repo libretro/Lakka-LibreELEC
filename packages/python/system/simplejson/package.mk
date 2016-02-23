bd38c3adb5a95218de0418012d63c070c67ca5b7
Lukas Rusak <lorusak@gmail.com>
2016-02-23 08:24:55 +0100

    simplejson: update to 3.8.2

--- packages/python/system/simplejson/package.mk
+++ packages/python/system/simplejson/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="simplejson"
-PKG_VERSION="3.8.1"
+PKG_VERSION="3.8.2"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
