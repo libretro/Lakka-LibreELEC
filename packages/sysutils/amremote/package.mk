28d366294b311583acfc630821196d5c99bd75a9
Alex Deryskyba <alex@codesnake.com>
2016-02-25 16:14:59 +0100

    packages/sysutils/amremote: Update license header

--- packages/sysutils/amremote/package.mk
+++ packages/sysutils/amremote/package.mk
@@ -1,6 +1,6 @@
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2014 Alex Deryskyba (alex@codesnake.com)
+#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 #
 #  OpenELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
