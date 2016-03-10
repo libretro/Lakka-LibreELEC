1106d29c5ed202edcf4999d57bbeb3ac9d629f5c
Stephan Raue <stephan@openelec.tv>
2016-03-10 12:33:46 +0100

    pygobject: add patch to setup proper python includes
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/python/devel/pygobject/package.mk
+++ packages/python/devel/pygobject/package.mk
@@ -34,6 +34,10 @@ PKG_AUTORECONF="yes"
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-thread --disable-introspection"
 
+pre_configure_target() {
+  export PYTHON_INCLUDES="$($SYSROOT_PREFIX/usr/bin/python2-config --includes)"
+}
+
 post_makeinstall_target() {
   find $INSTALL/usr/lib -name "*.py" -exec rm -rf "{}" ";"
   find $INSTALL/usr/lib -name "*.pyc" -exec rm -rf "{}" ";"
