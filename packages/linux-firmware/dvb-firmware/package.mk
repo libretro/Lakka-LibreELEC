6ba24a983acc3f5bd329cf7489621005c20022be
CvH <namerp@googlemail.com>
2016-03-16 12:02:26 +0100

    dvb-firmware: update to 1.0.0

--- packages/linux-firmware/dvb-firmware/package.mk
+++ packages/linux-firmware/dvb-firmware/package.mk
@@ -17,12 +17,12 @@
 ################################################################################
 
 PKG_NAME="dvb-firmware"
-PKG_VERSION="0.0.51"
+PKG_VERSION="1.0.0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Free-to-use"
-PKG_SITE="https://github.com/OpenELEC/dvb-firmware"
-PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_SITE="https://github.com/LibreELEC/dvb-firmware"
+PKG_URL="https://github.com/LibreELEC/dvb-firmware/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
 PKG_SECTION="firmware"
