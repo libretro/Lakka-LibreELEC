10b0fa5d82d16fbd069050f04fbe9494a260c327
Stephan Raue <stephan@openelec.tv>
2016-03-28 12:31:09 +0200

    libxml2: add support for host build
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/textproc/libxml2/package.mk
+++ packages/textproc/libxml2/package.mk
@@ -23,6 +23,7 @@ PKG_ARCH="any"
 PKG_LICENSE="MIT"
 PKG_SITE="http://xmlsoft.org"
 PKG_URL="ftp://xmlsoft.org/libxml2/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_HOST="toolchain zlib:host"
 PKG_DEPENDS_TARGET="toolchain zlib"
 PKG_PRIORITY="optional"
 PKG_SECTION="textproc"
@@ -32,15 +33,18 @@ PKG_IS_ADDON="no"
 
 PKG_AUTORECONF="yes"
 
-PKG_CONFIGURE_OPTS_TARGET="ac_cv_header_ansidecl_h=no \
+PKG_CONFIGURE_OPTS_ALL="ac_cv_header_ansidecl_h=no \
              --enable-static \
              --enable-shared \
              --disable-silent-rules \
              --enable-ipv6 \
              --without-python \
-             --with-zlib=$SYSROOT_PREFIX/usr \
-             --with-sysroot=$SYSROOT_PREFIX \
+             --with-zlib=$ROOT/$TOOLCHAIN \
              --without-lzma"
+
+PKG_CONFIGURE_OPTS_HOST="$PKG_CONFIGURE_OPTS_ALL --with-zlib=$ROOT/$TOOLCHAIN"
+
+PKG_CONFIGURE_OPTS_TARGET="$PKG_CONFIGURE_OPTS_ALL --with-zlib=$SYSROOT_PREFIX/usr --with-sysroot=$SYSROOT_PREFIX"
 
 post_makeinstall_target() {
   $SED "s:\(['= ]\)/usr:\\1$SYSROOT_PREFIX/usr:g" $SYSROOT_PREFIX/usr/bin/xml2-config
