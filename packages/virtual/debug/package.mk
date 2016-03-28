c57700c72e57f2ff0068989067772d93c587da7a
Stephan Raue <stephan@openelec.tv>
2016-03-28 11:49:13 +0200

    debug: VDPAU support depends on X11 support
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/virtual/debug/package.mk
+++ packages/virtual/debug/package.mk
@@ -35,7 +35,7 @@ PKG_AUTORECONF="no"
 # configure GPU drivers and dependencies:
   get_graphicdrivers
 
-if [ "$VDPAU_SUPPORT" = "yes" ]; then
+if [ "$VDPAU_SUPPORT" = "yes" -a "$DISPLAYSERVER" = "x11" ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET vdpauinfo"
 fi
 
