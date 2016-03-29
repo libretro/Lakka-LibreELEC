81473b0a8477b103a265aaf3b49f98942ef6c867
Stephan Raue <stephan@openelec.tv>
2016-03-30 00:18:28 +0200

    mtdev: build statically
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/wayland/mtdev/package.mk
+++ packages/wayland/mtdev/package.mk
@@ -32,3 +32,4 @@ PKG_LONGDESC="The mtdev is a stand-alone library which transforms all variants o
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
+PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared"
