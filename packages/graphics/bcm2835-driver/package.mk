f7d20ddc475d850426e9440d648d909e93d45ab5
MilhouseVH <milhouseVH.github@nmacleod.com>
2016-04-04 02:50:40 +0100

    RBP: Update firmware to be in sync with kernel

--- packages/graphics/bcm2835-driver/package.mk
+++ packages/graphics/bcm2835-driver/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="bcm2835-driver"
-PKG_VERSION="c230b2b"
+PKG_VERSION="046effa"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="nonfree"
