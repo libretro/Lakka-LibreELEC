4cf3ade150659ac4ea267738766da9b4005dffcc
Lukas Rusak <lorusak@gmail.com>
2016-04-06 11:57:49 -0700

    harfbuzz: dependency of chromium

---
+++ packages/addons/addon-depends/harfbuzz/package.mk
@@ -0,0 +1,38 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.tv; see the file COPYING.  If not, write to
+#  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
+#  http://www.gnu.org/copyleft/gpl.html
+################################################################################
+
+PKG_NAME="harfbuzz"
+PKG_VERSION="1.2.5"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://www.freedesktop.org/wiki/Software/HarfBuzz"
+PKG_URL="http://www.freedesktop.org/software/harfbuzz/release/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_DEPENDS_TARGET="toolchain glib freetype cairo"
+PKG_PRIORITY="optional"
+PKG_SECTION="x11/toolkits"
+PKG_SHORTDESC="harfbuzz: an OpenType text shaping engine."
+PKG_LONGDESC="HarfBuzz is an OpenType text shaping engine."
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="yes"
+
+PKG_MAINTAINER="none"
+
+PKG_CONFIGURE_OPTS_TARGET="--with-icu=no"
