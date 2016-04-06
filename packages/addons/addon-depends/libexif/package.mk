1c0e3810df98813353f6e95784db952b77c15964
Lukas Rusak <lorusak@gmail.com>
2016-04-06 11:59:14 -0700

    libexif: dependency of chromium

---
+++ packages/addons/addon-depends/libexif/package.mk
@@ -0,0 +1,33 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
+#
+#  OpenELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  OpenELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="libexif"
+PKG_VERSION="0.6.21"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="LGPL"
+PKG_SITE="http://sourceforge.net/projects/libexif"
+PKG_URL="http://downloads.sf.net/sourceforge/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="lib"
+PKG_SHORTDESC="A library to parse an EXIF file and read the data from those tags"
+PKG_LONGDESC="A library to parse an EXIF file and read the data from those tags"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="yes"
