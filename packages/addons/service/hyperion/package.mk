06b6807488a2768c149b3ec747af89bf52c08135
Lukas Rusak <lorusak@gmail.com>
2016-04-07 21:03:42 +0300

    hyperion: enable protobuf/v4l2 and clean up

--- packages/addons/service/hyperion/package.mk
+++ packages/addons/service/hyperion/package.mk
@@ -22,7 +22,7 @@ PKG_REV="100"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/tvdzwan/hyperion"
 PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain Python libusb qt"
+PKG_DEPENDS_TARGET="toolchain Python libusb qt protobuf"
 PKG_SECTION="service"
 PKG_SHORTDESC="Hyperion: an AmbiLight controller"
 PKG_LONGDESC="Hyperion($PKG_VERSION) is an modern opensource AmbiLight implementation."
@@ -59,10 +59,10 @@ configure_target() {
         $DISPMANX_SUPPORT \
         $FB_SUPPORT \
         -DENABLE_OSX=0 \
-        -DENABLE_PROTOBUF=0 \
+        -DENABLE_PROTOBUF=1 \
         -DENABLE_SPIDEV=1 \
         -DENABLE_TINKERFORGE=0 \
-        -DENABLE_V4L2=0 \
+        -DENABLE_V4L2=1 \
         -DENABLE_WS2812BPWM=0 \
         -DENABLE_WS281XPWM=1 \
         $X11_SUPPORT \
@@ -78,16 +78,26 @@ makeinstall_target() {
 
 addon() {
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
-  cp -P $PKG_BUILD/.$TARGET_NAME/bin/hyperiond $ADDON_BUILD/$PKG_ADDON_ID/bin
-  cp -P $PKG_BUILD/.$TARGET_NAME/bin/hyperion-remote $ADDON_BUILD/$PKG_ADDON_ID/bin
+    cp $PKG_BUILD/.$TARGET_NAME/bin/hyperiond $ADDON_BUILD/$PKG_ADDON_ID/bin
+    cp $PKG_BUILD/.$TARGET_NAME/bin/hyperion-remote $ADDON_BUILD/$PKG_ADDON_ID/bin
+    cp $PKG_BUILD/.$TARGET_NAME/bin/hyperion-v4l2 $ADDON_BUILD/$PKG_ADDON_ID/bin
+
+  if [ "$KODIPLAYER_DRIVER" = "libamcodec" ]; then
+    cp $PKG_BUILD/.$TARGET_NAME/bin/hyperion-aml $ADDON_BUILD/$PKG_ADDON_ID/bin
+    cp $PKG_BUILD/.$TARGET_NAME/bin/hyperion-framebuffer $ADDON_BUILD/$PKG_ADDON_ID/bin
+  elif [ "$KODIPLAYER_DRIVER" = "bcm2835-driver" ]; then
+    cp $PKG_BUILD/.$TARGET_NAME/bin/hyperion-dispmanx $ADDON_BUILD/$PKG_ADDON_ID/bin
+  elif [ "$DISPLAYSERVER" = "x11" ]; then
+    cp $PKG_BUILD/.$TARGET_NAME/bin/hyperion-x11 $ADDON_BUILD/$PKG_ADDON_ID/bin
+  fi
 
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/config
-  cp -P $PKG_BUILD/config/hyperion.config.json $ADDON_BUILD/$PKG_ADDON_ID/config/hyperion.config.json.sample
-  sed -i -e "s,/opt/hyperion/effects,/storage/.kodi/addons/service.hyperion/effects,g" \
-    $ADDON_BUILD/$PKG_ADDON_ID/config/hyperion.config.json.sample
+    cp -P $PKG_BUILD/config/hyperion.config.json $ADDON_BUILD/$PKG_ADDON_ID/config/hyperion.config.json.sample
+    sed -i -e "s,/opt/hyperion/effects,/storage/.kodi/addons/service.hyperion/effects,g" \
+      $ADDON_BUILD/$PKG_ADDON_ID/config/hyperion.config.json.sample
 
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/effects
-  cp -PR $PKG_BUILD/effects/* $ADDON_BUILD/$PKG_ADDON_ID/effects
+    cp -PR $PKG_BUILD/effects/* $ADDON_BUILD/$PKG_ADDON_ID/effects
 
   debug_strip $ADDON_BUILD/$PKG_ADDON_ID/bin
 }
