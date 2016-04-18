1db1941eae720d8daea926cbdd762b12e7345760
zalaare <zalaare@gmail.com>
2016-04-18 20:07:01 +0000

    virtual: initial virtual package "virtual" for project Virtual dependencies

---
+++ packages/virtual/virtual/package.mk
@@ -0,0 +1,33 @@
+################################################################################
+#      This file is part of LibreELEC - http://www.libreelec.tv
+#      Copyright (C) 2016-     Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="virtual"
+PKG_VERSION=""
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://www.libreelec.tv"
+PKG_URL=""
+PKG_DEPENDS_TARGET="qemu:host"
+PKG_PRIORITY="optional"
+PKG_SECTION="virtual"
+PKG_SHORTDESC="virtual: Meta package to install Virtual project extra deps"
+PKG_LONGDESC="virtual is a Meta package to install Virtual project extra dependencies"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
