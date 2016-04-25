41e6173800b1cb9bc820506363cc1bd51fc3f16d
Lukas Rusak <lorusak@gmail.com>
2016-04-25 10:37:14 -0700

    hid_mapper: add package

---
+++ packages/addons/addon-depends/system-tools-depends/hid_mapper/package.mk
@@ -0,0 +1,36 @@
+################################################################################
+#      This file is part of LibreELEC - http://www.libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="hid_mapper"
+PKG_VERSION="0"
+PKG_REV="0"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE=""
+PKG_URL="http://www.coldsource.net/hid_mapper.tar.gz"
+PKG_SOURCE_DIR="${PKG_NAME}_beta"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="tools"
+PKG_SHORTDESC="generic HID mapper"
+PKG_LONGDESC="generic HID mapper"
+PKG_AUTORECONF="no"
+
+makeinstall_target() {
+  : # nope
+}
