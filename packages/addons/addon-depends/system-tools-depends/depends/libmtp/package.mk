285e221c5d392f0d118a138426ef145b071adc4c
Lukas Rusak <lorusak@gmail.com>
2016-04-25 10:37:14 -0700

    libmtp: add package

---
+++ packages/addons/addon-depends/system-tools-depends/depends/libmtp/package.mk
@@ -0,0 +1,36 @@
+################################################################################
+#      This file is part of LibreELEC - http://www.libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="libmtp"
+PKG_VERSION="1.1.11"
+PKG_REV="3"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://libmtp.sourceforge.net/"
+PKG_URL="$SOURCEFORGE_SRC/project/$PKG_NAME/$PKG_NAME/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain libusb"
+PKG_SECTION="libs"
+PKG_PRIORITY="optional"
+PKG_SHORTDESC="MTP library"
+PKG_LONGDESC="libmtp is an Initiator implementation of the Media Transfer Protocol (MTP) in the form of a library suitable primarily for POSIX compliant operating systems. We implement MTP Basic, the stuff proposed for standardization."
+PKG_AUTORECONF="yes"
+
+PKG_CONFIGURE_OPTS_TARGET="ac_cv_func_malloc_0_nonnull=yes \
+                           --disable-shared \
+                           --enable-static \
+                           --disable-mtpz"
