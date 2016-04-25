e2dd9ca73714b0c853e19c178dc1baccb641530a
Lukas Rusak <lorusak@gmail.com>
2016-04-25 10:29:25 -0700

    libstatgrab: add package

---
+++ packages/addons/addon-depends/network-tools-depends/depends/libstatgrab/package.mk
@@ -0,0 +1,38 @@
+################################################################################
+#      This file is part of LibreELEC - http://www.libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="libstatgrab"
+PKG_VERSION="0.91"
+PKG_REV="1"
+PKG_SITE="http://www.i-scream.org/libstatgrab/"
+PKG_URL="http://ftp.i-scream.org/pub/i-scream/libstatgrab/libstatgrab-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY=optional
+PKG_SECTION=libs
+PKG_SHORTDESC="provides cross platform access to statistics about the system on which it's run"
+PKG_LONGDESC="libstatgrab is a library that provides cross platform access to statistics about the system on which it's run. It's written in C and presents a selection of useful interfaces which can be used to access key system statistics. The current list of statistics includes CPU usage, memory utilisation, disk usage, process counts, network traffic, disk I/O, and more."
+PKG_AUTORECONF="yes"
+
+PKG_CONFIGURE_OPTS_TARGET="ac_cv_func_malloc_0_nonnull=yes \
+                           --enable-static \
+                           --disable-shared \
+                           --disable-saidar \
+                           --disable-examples \
+                           --disable-manpages \
+                           --disable-setuid-binaries \
+                           --disable-setgid-binaries"
