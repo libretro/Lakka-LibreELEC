840b3da570f273cd62354914cafdf5e2df6659a6
Lukas Rusak <lorusak@gmail.com>
2016-04-25 10:37:14 -0700

    dtach: add package

---
+++ packages/addons/addon-depends/system-tools-depends/dtach/package.mk
@@ -0,0 +1,37 @@
+################################################################################
+#      This file is part of LibreELEC - http://www.libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="dtach"
+PKG_VERSION="0.8"
+PKG_REV="0"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://dtach.sourceforge.net"
+PKG_URL="http://download.sourceforge.net/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="tools"
+PKG_SHORTDESC="dtach - A program that emulates the detach feature of screen"
+PKG_LONGDESC="dtach - A program that emulates the detach feature of screen"
+PKG_AUTORECONF="no"
+
+PKG_CONFIGURE_OPTS_TARGET="--disable-nls"
+
+makeinstall_target() {
+  : # nop
+}
