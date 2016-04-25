5730d629f06493d3b1c4f8cf1ec63a1f43000e68
Lukas Rusak <lorusak@gmail.com>
2016-04-25 10:37:14 -0700

    getscancodes: add package

---
+++ packages/addons/addon-depends/system-tools-depends/getscancodes/package.mk
@@ -0,0 +1,36 @@
+################################################################################
+#      This file is part of LibreELEC - http://www.libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="getscancodes"
+PKG_VERSION="1.0"
+PKG_REV="0"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://keytouch.sourceforge.net"
+PKG_URL="$SOURCEFORGE_SRC/keytouch/getscancodes-${PKG_VERSION}.tar.gz"
+PKG_SOURCE_DIR="$PKG_NAME"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="tools"
+PKG_SHORTDESC="Shows the scancode of the pressed or released key"
+PKG_LONGDESC="Shows the scancode of the pressed or released key"
+PKG_AUTORECONF="no"
+
+makeinstall_target() {
+  : # nop
+}
