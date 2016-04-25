4c0472425b0b8537ce9b648506f65d90a8230082
Lukas Rusak <lorusak@gmail.com>
2016-04-25 10:37:14 -0700

    evtest: add package

---
+++ packages/addons/addon-depends/system-tools-depends/evtest/package.mk
@@ -0,0 +1,35 @@
+################################################################################
+#      This file is part of LibreELEC - http://www.libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="evtest"
+PKG_VERSION="1.33"
+PKG_REV="0"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://cgit.freedesktop.org/evtest/"
+PKG_URL="http://cgit.freedesktop.org/evtest/snapshot/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_DEPENDS_TARGET="toolchain libxml2"
+PKG_PRIORITY="optional"
+PKG_SECTION="debug/tools"
+PKG_SHORTDESC="evtest: Simple tool for input event debugging."
+PKG_LONGDESC="evtest is a simple tool for input event debugging."
+PKG_AUTORECONF="yes"
+
+makeinstall_target() {
+  : # nop
+}
