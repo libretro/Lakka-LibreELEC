4fbfd0a87033cf9838f17baf9f257df16f308d91
Lukas Rusak <lorusak@gmail.com>
2016-04-25 10:29:25 -0700

    libpcap: add package

---
+++ packages/addons/addon-depends/network-tools-depends/depends/libpcap/package.mk
@@ -0,0 +1,51 @@
+################################################################################
+#      This file is part of LibreELEC - http://www.libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="libpcap"
+PKG_VERSION="1.7.4"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://www.tcpdump.org/"
+PKG_URL="http://www.tcpdump.org/release/libpcap-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="devel"
+PKG_SHORTDESC="system interface for user-level packet capture"
+PKG_LONGDESC="libpcap (Packet CAPture) provides a portable framework for low-level network monitoring. Applications include network statistics collection, security monitoring, network debugging, etc."
+PKG_AUTORECONF="yes"
+
+PKG_CONFIGURE_OPTS_TARGET="LIBS=-lpthread \
+                           ac_cv_header_libusb_1_0_libusb_h=no \
+                           --disable-shared \
+                           --with-pcap=linux \
+                           --disable-bluetooth \
+                           --disable-can \
+                           --without-libnl \
+                           --disable-dbus \
+                           --disable-canusb"
+
+pre_configure_target() {
+# When cross-compiling, configure can't set linux version
+# forcing it
+  sed -i -e 's/ac_cv_linux_vers=unknown/ac_cv_linux_vers=2/' ../configure
+}
+
+post_makeinstall_target() {
+  rm -rf $INSTALL/usr/bin
+}
