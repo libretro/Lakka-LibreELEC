73bf6353f580570a60104cc66f9e15b34cc4f92e
Stephan Raue <stephan@openelec.tv>
2016-04-29 18:16:09 +0200

    libsndfile: update to libsndfile-1.0.26
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/audio/libsndfile/package.mk
+++ packages/audio/libsndfile/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libsndfile"
-PKG_VERSION="1.0.25"
+PKG_VERSION="1.0.26"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="LGPL"
