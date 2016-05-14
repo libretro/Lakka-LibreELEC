23a5535fec5ec0749183756b82d06eb3c7da865d
Anton Voyl <awiouy@gmail.com>
2016-05-14 20:33:15 +0200

    mpg123: create addon

---
+++ packages/addons/addon-depends/multimedia-tools-depends/mpg123/package.mk
@@ -0,0 +1,35 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="mpg123"
+PKG_VERSION="1.23.4"
+PKG_REV="0"
+PKG_ARCH="any"
+PKG_LICENSE="LGPLv2"
+PKG_SITE="http://www.mpg123.org/"
+PKG_URL="http://netcologne.dl.sourceforge.net/project/$PKG_NAME/$PKG_NAME/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_DEPENDS_TARGET="toolchain alsa-lib SDL2"
+PKG_SECTION="tools"
+PKG_PRIORITY="optional"
+PKG_SHORTDESC="mpg123"
+PKG_LONGDESC="A fast MP3 Player"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="yes"
+
+PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-static"
