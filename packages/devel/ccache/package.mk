ab48b2da116d2540bfebbce119ae0e16271d706f
Jonas Karlman <jonas@kwiboo.se>
2016-05-15 11:55:43 +0200

    ccache: update to 3.2.5

--- packages/devel/ccache/package.mk
+++ packages/devel/ccache/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="ccache"
-PKG_VERSION="3.2.3"
+PKG_VERSION="3.2.5"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
