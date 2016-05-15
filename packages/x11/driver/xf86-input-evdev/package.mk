643eb02a75f5c6308fe25cb2c6d5f8470958bfbe
Jonas Karlman <jonas@kwiboo.se>
2016-05-15 11:55:43 +0200

    xf86-input-evdev: update to 2.10.2

--- packages/x11/driver/xf86-input-evdev/package.mk
+++ packages/x11/driver/xf86-input-evdev/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="xf86-input-evdev"
-PKG_VERSION="2.10.1"
+PKG_VERSION="2.10.2"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
