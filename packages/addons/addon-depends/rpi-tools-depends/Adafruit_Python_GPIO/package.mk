306beb4dfb7bd435645d97d4830600d442509b5a
Sascha Kuehndel (InuSasha) <dev@inusasha.de>
2016-05-15 13:05:10 +0200

    RPi-Tools: add same Adafruit library for LED backpacks

---
+++ packages/addons/addon-depends/rpi-tools-depends/Adafruit_Python_GPIO/package.mk
@@ -0,0 +1,39 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="Adafruit_Python_GPIO"
+PKG_VERSION="fdf0fca"
+PKG_REV="1"
+PKG_ARCH="arm"
+PKG_LICENSE="MIT"
+PKG_SITE="https://github.com/adafruit/${PKG_NAME}"
+PKG_URL="https://github.com/adafruit/${PKG_NAME}/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain Python distutilscross:host"
+PKG_PRIORITY="optional"
+PKG_SECTION="python"
+PKG_SHORTDESC="Adafruit Python GPIO Library"
+PKG_LONGDESC="Library to provide a cross-platform GPIO interface on the Raspberry Pi and Beaglebone Black using the RPi.GPIO and Adafruit_BBIO librarie"
+PKG_AUTORECONF="no"
+
+make_target() {
+  : # nop
+}
+
+makeinstall_target() {
+  : # nop
+}
---
+++ packages/addons/addon-depends/rpi-tools-depends/Adafruit_Python_PureIO/package.mk
@@ -0,0 +1,39 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="Adafruit_Python_PureIO"
+PKG_VERSION="a29d95a"
+PKG_REV="1"
+PKG_ARCH="arm"
+PKG_LICENSE="MIT"
+PKG_SITE="https://github.com/adafruit/${PKG_NAME}"
+PKG_URL="https://github.com/adafruit/${PKG_NAME}/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain Python distutilscross:host"
+PKG_PRIORITY="optional"
+PKG_SECTION="python"
+PKG_SHORTDESC="Pure python access to Linux IO including I2C and SPI."
+PKG_LONGDESC="Pure python access to Linux IO including I2C and SPI. Drop in replacement for smbus and spidev modules."
+PKG_AUTORECONF="no"
+
+make_target() {
+  : # nop
+}
+
+makeinstall_target() {
+  : # nop
+}
