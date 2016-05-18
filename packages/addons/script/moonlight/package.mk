c0491f5a4c070d24b762a25a9456249f3d0fbc4a
Lukas Rusak <lorusak@gmail.com>
2016-05-18 14:29:49 -0700

    script.moonlight: initial add-on

---
+++ packages/addons/script/moonlight/package.mk
@@ -0,0 +1,78 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="moonlight"
+PKG_VERSION="c371ff8"
+PKG_REV="0"
+PKG_ARCH="arm aarch64"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/dead/moonlight-openelec-rpi2"
+PKG_URL="https://github.com/dead/moonlight-openelec-rpi2/archive/$PKG_VERSION.tar.gz"
+PKG_SOURCE_DIR="moonlight-openelec-rpi2-$PKG_VERSION*"
+PKG_DEPENDS_TARGET="toolchain moonlight-embedded"
+PKG_PRIORITY="optional"
+PKG_SECTION="script"
+PKG_SHORTDESC="Moonlight-embedded Addon for LibreELEC"
+PKG_LONGDESC="Moonlight-embedded Addon for LibreELEC"
+PKG_AUTORECONF="no"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_REPOVERSION="8.0"
+PKG_ADDON_TYPE="xbmc.service.pluginsource"
+PKG_ADDON_PROVIDES="executable"
+PKG_ADDON_NAME="Moonlight"
+PKG_MAINTAINER="dead (gustavobenn@hotmail.com)"
+
+make_target() {
+  :
+}
+
+makeinstall_target() {
+  :
+}
+
+addon() {
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID
+    cp -PR $ROOT/$PKG_BUILD/script.moonlight/* $ADDON_BUILD/$PKG_ADDON_ID
+
+    # let's use our addon.xml instead
+    rm $ADDON_BUILD/$PKG_ADDON_ID/addon.xml
+
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
+    cp -P $(get_build_dir moonlight-embedded)/.$TARGET_NAME/moonlight $ADDON_BUILD/$PKG_ADDON_ID/bin
+
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/lib
+    cp -P $(get_build_dir moonlight-embedded)/.$TARGET_NAME/libgamestream/libgamestream.so* $ADDON_BUILD/$PKG_ADDON_ID/lib
+    cp -P $(get_build_dir moonlight-embedded)/.$TARGET_NAME/libgamestream/libmoonlight-common.so* $ADDON_BUILD/$PKG_ADDON_ID/lib
+
+    if [ "$KODIPLAYER_DRIVER" = "bcm2835-driver" ]; then
+      cp -P $(get_build_dir moonlight-embedded)/.$TARGET_NAME/libmoonlight-pi.so $ADDON_BUILD/$PKG_ADDON_ID/lib
+    elif [ "$KODIPLAYER_DRIVER" = "libfslvpuwrap" ]; then
+      cp -P $(get_build_dir moonlight-embedded)/.$TARGET_NAME/libmoonlight-imx.so $ADDON_BUILD/$PKG_ADDON_ID/lib
+    elif [ "$KODIPLAYER_DRIVER" = "libamcodec" ]; then
+      cp -P $(get_build_dir moonlight-embedded)/.$TARGET_NAME/libmoonlight-aml.so $ADDON_BUILD/$PKG_ADDON_ID/lib
+    fi
+
+    cp -P $(get_build_dir libevdev)/.install_pkg/usr/lib/libevdev.so* $ADDON_BUILD/$PKG_ADDON_ID/lib
+
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/etc
+    cp -P $(get_build_dir moonlight-embedded)/moonlight.conf $ADDON_BUILD/$PKG_ADDON_ID/etc
+
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/share/moonlight
+    cp -PR $(get_build_dir moonlight-embedded)/mappings $ADDON_BUILD/$PKG_ADDON_ID/share/moonlight
+}
