f522ac76adbfac31feacbaa7cfa476f95482bd4f
Lukas Rusak <lorusak@gmail.com>
2016-05-20 13:58:41 -0700

    SDL2: update to 2.0.4

--- packages/multimedia/SDL2/package.mk
+++ packages/multimedia/SDL2/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="SDL2"
-PKG_VERSION="2.0.3"
+PKG_VERSION="2.0.4"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
