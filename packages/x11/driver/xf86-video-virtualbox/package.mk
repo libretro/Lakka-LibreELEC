c4d968235a0c1496452168f7661f0fd5f5b6a824
Lukas Rusak <lorusak@gmail.com>
2016-06-24 12:04:45 -0700

    xf86-video-virtualbox: update to 5.0.22

--- packages/x11/driver/xf86-video-virtualbox/package.mk
+++ packages/x11/driver/xf86-video-virtualbox/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="xf86-video-virtualbox"
-PKG_VERSION="5.0.20"
+PKG_VERSION="5.0.22"
 PKG_REV="1"
 PKG_ARCH="x86_64"
 PKG_LICENSE="OSS"
