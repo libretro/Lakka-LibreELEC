5edf016b70784a072ff8adfddd1d7403112e02bb
Alex Deryskyba <alex@codesnake.com>
2016-06-28 12:16:39 +0200

    packages/sysutils/amremote: Enable package for aarch64 architecture

--- packages/sysutils/amremote/package.mk
+++ packages/sysutils/amremote/package.mk
@@ -19,7 +19,7 @@
 PKG_NAME="amremote"
 PKG_VERSION="d4f6458"
 PKG_REV="1"
-PKG_ARCH="arm"
+PKG_ARCH="arm aarch64"
 PKG_LICENSE="other"
 PKG_SITE="http://www.amlogic.com"
 PKG_URL="https://github.com/codesnake/amremote/archive/$PKG_VERSION.tar.gz"
