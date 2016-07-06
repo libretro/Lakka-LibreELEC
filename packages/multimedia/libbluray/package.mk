0c39d7b3353bc97861054d241039cc40e89b4577
MilhouseVH <milhouseVH.github@nmacleod.com>
2016-07-06 14:35:03 +0100

    libbluray: update to 0.9.3

--- packages/multimedia/libbluray/package.mk
+++ packages/multimedia/libbluray/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libbluray"
-PKG_VERSION="0.9.2"
+PKG_VERSION="0.9.3"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
