5d05b2a8d23d5a5b8f1b5f70cf15ee1b61c2adfa
jc <awiouy@gmail.com>
2016-07-17 23:53:49 +0200

    webgrabplus: initial addon

---
+++ packages/addons/service/webgrabplus/package.mk
@@ -0,0 +1,33 @@
+PKG_NAME="webgrabplus"
+PKG_VERSION="56.27pre-20160721"
+PKG_REV="100"
+PKG_ARCH="any"
+PKG_LICENSE="prop."
+PKG_SITE="http://www.webgrabplus.com/"
+PKG_URL="https://github.com/awiouy/webgrabplus/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="service"
+PKG_SHORTDESC="WebGrab+Plus: a multi-site incremental xmltv epg grabber"
+PKG_LONGDESC="WebGrab+Plus ($PKG_VERSION) collects tv-program guide data from selected tvguide sites for your favourite channels."
+PKG_AUTORECONF="no"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_NAME="WebGrab+Plus"
+PKG_ADDON_TYPE="xbmc.service"
+PKG_ADDON_REPOVERSION="8.0"
+PKG_ADDON_REQUIRES="tools.mono:0.0.0"
+PKG_MAINTAINER="Anton Voyl (awiouy)"
+
+make_target() {
+  : # nop
+}
+
+makeinstall_target() {
+  : # nop
+}
+
+addon() {
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID
+  cp -PR $PKG_BUILD/* $ADDON_BUILD/$PKG_ADDON_ID
+}
---
+++ packages/addons/service/webgrabplus/source/default.py
@@ -0,0 +1,17 @@
+import subprocess
+import xbmc
+import xbmcaddon
+
+
+class Monitor(xbmc.Monitor):
+
+   def __init__(self, *args, **kwargs):
+      xbmc.Monitor.__init__(self)
+      self.id = xbmcaddon.Addon().getAddonInfo('id')
+
+   def onSettingsChanged(self):
+      subprocess.call(['systemctl', 'restart', self.id])
+
+
+if __name__ == "__main__":
+   Monitor().waitForAbort()
