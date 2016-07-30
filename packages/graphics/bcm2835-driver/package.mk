b8790e870ea02a5b7d144c4706ce31402c6b2c5e
MilhouseVH <milhouseVH.github@nmacleod.com>
2016-07-30 19:40:55 +0100

    RBP: Update to firmware b8ef00f

--- packages/graphics/bcm2835-driver/package.mk
+++ packages/graphics/bcm2835-driver/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="bcm2835-driver"
-PKG_VERSION="1ba9c9a"
+PKG_VERSION="b8ef00f"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="nonfree"
