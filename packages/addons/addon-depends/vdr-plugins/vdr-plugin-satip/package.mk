3d31b7b41f9a29edf7b7059f240d66b29f0b4f22
cvh <namerp@googlemail.com>
2016-08-19 11:09:38 +0200

    vdr-addon: update plugins

--- packages/addons/addon-depends/vdr-plugins/vdr-plugin-satip/package.mk
+++ packages/addons/addon-depends/vdr-plugins/vdr-plugin-satip/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="vdr-plugin-satip"
-PKG_VERSION="7815821"
+PKG_VERSION="db0c18b"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
