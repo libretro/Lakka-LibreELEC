cc056d8c340588dd16e8cc74fcda3e90bc9c4fa4
Jean-André Santoni <jean.andre.santoni@gmail.com>
2016-08-30 14:47:10 +0200

    Fix RetroArch compilation for WeTek Play

--- packages/libretro/retroarch/package.mk
+++ packages/libretro/retroarch/package.mk
@@ -54,7 +54,7 @@ elif [ "$OPENGLES" == "bcm2835-driver" ]; then
   RETROARCH_GL="--enable-gles --disable-kms --disable-x11"
   CFLAGS="$CFLAGS -I$SYSROOT_PREFIX/usr/include/interface/vcos/pthreads \
                   -I$SYSROOT_PREFIX/usr/include/interface/vmcs_host/linux"
-elif [ "$OPENGLES" == "sunxi-mali" ] || [ "$OPENGLES" == "odroidc1-mali" ] || [ "$OPENGLES" == "odroidxu3-mali" ] || [ "$OPENGLES" == "opengl-meson6" ] || [ "$OPENGLES" == "opengl-meson8" ]; then
+elif [ "$OPENGLES" == "sunxi-mali" ] || [ "$OPENGLES" == "odroidc1-mali" ] || [ "$OPENGLES" == "odroidxu3-mali" ] || [ "$OPENGLES" == "opengl-meson" ] || [ "$OPENGLES" == "opengl-meson8" ]; then
   RETROARCH_GL="--enable-gles --disable-kms --disable-x11 --enable-mali_fbdev"
 elif [ "$OPENGLES" == "gpu-viv-bin-mx6q" ]; then
   RETROARCH_GL="--enable-gles --disable-kms --disable-x11 --enable-vivante_fbdev"
