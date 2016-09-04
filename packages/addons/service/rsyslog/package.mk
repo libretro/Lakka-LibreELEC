dfa26885cffe6a877b867581088008bbf44debef
jc <awiouy@gmail.com>
2016-09-04 19:49:34 +0200

    rsyslog: initial addon

---
+++ packages/addons/service/rsyslog/package.mk
@@ -0,0 +1,59 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="rsyslog"
+PKG_VERSION="8.21.0"
+PKG_REV="100"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/rsyslog"
+PKG_URL="http://www.rsyslog.com/files/download/rsyslog/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain util-linux libestr libfastjson liblognorm librelp zlib libgcrypt liblogging"
+PKG_SECTION="service"
+PKG_PRIORITY="optional"
+PKG_SHORTDESC="Rsyslog: a rocket-fast system for log processing."
+PKG_LONGDESC="Rsyslog ($PKG_VERSION) offers high-performance, great security features and a modular design."
+PKG_AUTORECONF="yes"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_NAME="Rsyslog"
+PKG_ADDON_TYPE="xbmc.service"
+PKG_ADDON_REPOVERSION="8.0"
+
+PKG_CONFIGURE_OPTS_TARGET="--enable-imfile \
+                           --enable-imjournal \
+                           ac_cv_func_malloc_0_nonnull=yes \
+                           ac_cv_func_realloc_0_nonnull=yes"
+
+export LIBGCRYPT_CONFIG="$SYSROOT_PREFIX/usr/bin/libgcrypt-config"
+
+makeinstall_target() {
+  :
+}
+
+addon() {
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
+  cp $PKG_BUILD/.$TARGET_NAME/tools/rsyslogd \
+     $ADDON_BUILD/$PKG_ADDON_ID/bin/
+
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/lib/rsyslog
+  for l in $(find $PKG_BUILD/.$TARGET_NAME -name *.so)
+  do
+    cp $l $ADDON_BUILD/$PKG_ADDON_ID/lib/rsyslog/
+  done
+}
