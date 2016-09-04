a8d52514bd3fe5c8681e3df32b8e8e49e94e92d5
Lukas Rusak <lorusak@gmail.com>
2016-09-03 17:50:15 -0700

    mesa: adjust llvm-config path and use static llvm

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -37,8 +37,8 @@ PKG_AUTORECONF="yes"
 
 if [ "$LLVM_SUPPORT" = "yes" ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET elfutils llvm"
-  export LLVM_CONFIG="$SYSROOT_PREFIX/usr/bin/llvm-config-host"
-  MESA_GALLIUM_LLVM="--enable-gallium-llvm --enable-llvm-shared-libs"
+  export LLVM_CONFIG="$SYSROOT_PREFIX/usr/bin/llvm-config"
+  MESA_GALLIUM_LLVM="--enable-gallium-llvm --disable-llvm-shared-libs"
 else
   MESA_GALLIUM_LLVM="--disable-gallium-llvm"
 fi
