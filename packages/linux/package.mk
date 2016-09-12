816a3e98d8d78d2c61fcfe03a1460e71c159a96c
Jonas Karlman <jonas@kwiboo.se>
2016-09-13 00:04:28 +0200

    projects/WeTek_Hub: enable aocec/libcec support

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -40,7 +40,7 @@ case "$LINUX" in
     PKG_SOURCE_DIR="$PKG_NAME-amlogic-$PKG_VERSION*"
     ;;
   amlogic-3.14)
-    PKG_VERSION="7618021"
+    PKG_VERSION="c733661"
     PKG_URL="https://github.com/LibreELEC/linux-amlogic/archive/$PKG_VERSION.tar.gz"
     PKG_SOURCE_DIR="$PKG_NAME-amlogic-$PKG_VERSION*"
     ;;
