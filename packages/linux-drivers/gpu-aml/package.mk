e4ae963a61b7cf2b8b9113805454f8858fd8253d
Stefan Saraev <stefan@saraev.ca>
2016-09-22 17:53:20 +0300

    gpu-aml: simplify build. strip

--- packages/linux-drivers/gpu-aml/package.mk
+++ packages/linux-drivers/gpu-aml/package.mk
@@ -34,15 +34,12 @@ PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
 make_target() {
-  cd mali
-  LDFLAGS="" make V=1 \
-    -C $(kernel_path) M=$ROOT/$PKG_BUILD/mali \
-    ARCH=$TARGET_KERNEL_ARCH \
-    CROSS_COMPILE=$TARGET_PREFIX \
+  LDFLAGS="" make -C $(kernel_path) M=$ROOT/$PKG_BUILD/mali \
     CONFIG_MALI400=m CONFIG_MALI450=m
 }
 
 makeinstall_target() {
-  mkdir -p $INSTALL/lib/modules/$(get_module_dir)/mali
-  cp *.ko $INSTALL/lib/modules/$(get_module_dir)/mali
+  LDFLAGS="" make -C $(kernel_path) M=$ROOT/$PKG_BUILD/mali \
+    INSTALL_MOD_PATH=$INSTALL INSTALL_MOD_STRIP=1 DEPMOD=: \
+  modules_install
 }
