d0c506e52bd3cc65438cdc8f3fbf21baad9df9cf
MilhouseVH <milhouseVH.github@nmacleod.com>
2016-09-23 17:07:36 +0100

    RBP: Bump firmware to version that includes libelftoolchain.so now that we install it...

--- packages/graphics/bcm2835-driver/package.mk
+++ packages/graphics/bcm2835-driver/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="bcm2835-driver"
-PKG_VERSION="83dc067"
+PKG_VERSION="ad8608c"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="nonfree"
--- packages/tools/bcm2835-bootloader/package.mk
+++ packages/tools/bcm2835-bootloader/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="bcm2835-bootloader"
-PKG_VERSION="83dc067"
+PKG_VERSION="ad8608c"
 PKG_REV="1"
 PKG_ARCH="arm"
 PKG_LICENSE="nonfree"
