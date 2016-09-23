3b037295ef8e6c48fd71f33097b355e2760c4eb2
Stephan Raue <stephan@openelec.tv>
2016-09-23 14:29:49 -0700

    p8-platform: simplyfing cmake configuration
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/p8-platform/package.mk
+++ packages/mediacenter/p8-platform/package.mk
@@ -33,16 +33,11 @@ PKG_LONGDESC="Platform support library used by libCEC and binary add-ons for Kod
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-configure_target() {
-  cmake -DCMAKE_TOOLCHAIN_FILE=$CMAKE_CONF \
-        -DCMAKE_INSTALL_PREFIX=/usr \
-        -DCMAKE_INSTALL_LIBDIR=/usr/lib \
-        -DCMAKE_INSTALL_LIBDIR_NOARCH=/usr/lib \
-        -DCMAKE_INSTALL_PREFIX_TOOLCHAIN=$SYSROOT_PREFIX/usr \
-        -DCMAKE_PREFIX_PATH=$SYSROOT_PREFIX/usr \
-        -DBUILD_SHARED_LIBS=0 \
-        ..
-}
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_INSTALL_LIBDIR=/usr/lib \
+                       -DCMAKE_INSTALL_LIBDIR_NOARCH=/usr/lib \
+                       -DCMAKE_INSTALL_PREFIX_TOOLCHAIN=$SYSROOT_PREFIX/usr \
+                       -DCMAKE_PREFIX_PATH=$SYSROOT_PREFIX/usr \
+                       -DBUILD_SHARED_LIBS=0"
 
 post_makeinstall_target() {
   rm -rf $INSTALL/usr
