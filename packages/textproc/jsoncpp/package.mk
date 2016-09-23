dbedd752a265595d7aacb4e40ee4faba9e60d72f
Stephan Raue <stephan@openelec.tv>
2016-09-23 14:29:49 -0700

    jsoncpp: simplyfing cmake configuration
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/textproc/jsoncpp/package.mk
+++ packages/textproc/jsoncpp/package.mk
@@ -39,9 +39,3 @@ pre_configure_target() {
 pre_build_target() {
   cp $PKG_DIR/config/CMakeLists.txt $ROOT/$PKG_BUILD
 }
-
-configure_target() {
-  cmake -DCMAKE_TOOLCHAIN_FILE=$CMAKE_CONF \
-        -DCMAKE_INSTALL_PREFIX=/usr \
-        ..
-}
