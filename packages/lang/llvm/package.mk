b027a49b7cdc69a8981b68b8048e9656d8ea2036
Jean-André Santoni <jean.andre.santoni@gmail.com>
2016-10-15 23:16:46 +0200

    LLVM Fix

--- packages/lang/llvm/package.mk
+++ packages/lang/llvm/package.mk
@@ -19,7 +19,7 @@
 PKG_NAME="llvm"
 PKG_VERSION="3.9.0"
 PKG_REV="1"
-PKG_ARCH="any"
+PKG_ARCH="x86_64"
 PKG_LICENSE="GPL"
 PKG_SITE="http://llvm.org/"
 PKG_URL="http://llvm.org/releases/$PKG_VERSION/${PKG_NAME}-${PKG_VERSION}.src.tar.xz"
@@ -32,8 +32,7 @@ PKG_LONGDESC="Low-Level Virtual Machine (LLVM) is a compiler infrastructure desi
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-PKG_CMAKE_OPTS_HOST="-DCMAKE_BUILD_TYPE=Release \
-                     -DLLVM_INCLUDE_TOOLS=ON \
+PKG_CMAKE_OPTS_HOST="-DLLVM_INCLUDE_TOOLS=ON \
                      -DLLVM_BUILD_TOOLS=OFF \
                      -DLLVM_BUILD_UTILS=OFF \
                      -DLLVM_BUILD_EXAMPLES=OFF \
@@ -49,22 +48,23 @@ PKG_CMAKE_OPTS_HOST="-DCMAKE_BUILD_TYPE=Release \
                      -DLLVM_ENABLE_TERMINFO=OFF \
                      -DLLVM_ENABLE_ASSERTIONS=OFF \
                      -DLLVM_ENABLE_WERROR=OFF \
-                     -DLLVM_ENABLE_ZLIB=OFF"
+                     -DLLVM_ENABLE_ZLIB=OFF \
+                     -DLLVM_OPTIMIZED_TABLEGEN=ON"
 
 make_host() {
-  make llvm-config
+  make llvm-config llvm-tblgen
 }
 
 makeinstall_host() {
   cp -a bin/llvm-config $SYSROOT_PREFIX/usr/bin/llvm-config-host
+  cp -a bin/llvm-tblgen $ROOT/$TOOLCHAIN/bin
 }
 
-PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release \
-                       -DCMAKE_C_FLAGS="$CFLAGS" \
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_C_FLAGS="$CFLAGS" \
                        -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
                        -DLLVM_INCLUDE_TOOLS=ON \
                        -DLLVM_BUILD_TOOLS=OFF \
-                       -DLLVM_BUILD_UTILS=OFF \
+                       -DLLVM_BUILD_UTILS=ON \
                        -DLLVM_BUILD_EXAMPLES=OFF \
                        -DLLVM_INCLUDE_EXAMPLES=OFF \
                        -DLLVM_BUILD_TESTS=OFF \
@@ -81,7 +81,9 @@ PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release \
                        -DLLVM_TARGET_ARCH="$TARGET_ARCH" \
                        -DLLVM_ENABLE_ZLIB=ON \
                        -DLLVM_BUILD_LLVM_DYLIB=ON \
-                       -DLLVM_LINK_LLVM_DYLIB=ON"
+                       -DLLVM_LINK_LLVM_DYLIB=ON \
+                       -DLLVM_OPTIMIZED_TABLEGEN=ON \
+                       -DLLVM_TABLEGEN=$ROOT/$TOOLCHAIN/bin/llvm-tblgen"
 
 post_makeinstall_target() {
   rm -rf $INSTALL/usr/bin
