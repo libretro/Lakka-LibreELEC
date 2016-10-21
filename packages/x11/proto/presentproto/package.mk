a70d2aa5d69f057e9c1d756d047a3f7b3588b5a0
awiouy <awiouy@gmail.com>
2016-10-21 18:08:49 +0200

    presentproto: disable autoreconf

--- packages/x11/proto/presentproto/package.mk
+++ packages/x11/proto/presentproto/package.mk
@@ -29,7 +29,7 @@ PKG_SHORTDESC="presentproto: Present extension headers"
 PKG_LONGDESC="The Present extension provides a way for applications to update their window contents from a pixmap in a well defined fashion, synchronizing with the display refresh and potentially using a more efficient mechanism than copying the contents of the source pixmap."
 
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
 # package specific configure options
 PKG_CONFIGURE_OPTS_TARGET="--without-xmlto"
