941f8d40bbe9606b062bedec4b35cdcb7fc58143
awiouy <awiouy@gmail.com>
2016-10-21 17:44:39 +0200

    dbus-python: disable autoreconf

--- packages/python/system/dbus-python/package.mk
+++ packages/python/system/dbus-python/package.mk
@@ -29,7 +29,7 @@ PKG_SHORTDESC="dbus-python: A message bus system"
 PKG_LONGDESC="D-BUS is a message bus, used for sending messages between applications. Conceptually, it fits somewhere in between raw sockets and CORBA in terms of complexity. D-BUS supports broadcast messages, asynchronous messages (thus decreasing latency), authentication, and more. It is designed to be low-overhead; messages are sent using a binary protocol, not using XML. D-BUS also supports a method call mapping for its messages, but it is not required; this makes using the system quite simple."
 
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
 pre_configure_target() {
   export PYTHON_INCLUDES="$($SYSROOT_PREFIX/usr/bin/python2-config --includes)"
