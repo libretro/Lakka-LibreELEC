0fe32e7d78fdee48a8da4c797ea0ff0a63948e3a
awiouy <awiouy@gmail.com>
2016-10-21 17:47:45 +0200

    kmod: disable autoreconf

--- packages/sysutils/kmod/package.mk
+++ packages/sysutils/kmod/package.mk
@@ -29,7 +29,7 @@ PKG_SHORTDESC="kmod offers the needed flexibility and fine grained control over 
 PKG_LONGDESC="kmod offers the needed flexibility and fine grained control over insertion, removal, configuration and listing of kernel modules."
 
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
 PKG_CONFIGURE_OPTS_HOST="--enable-tools \
                          --disable-logging \
