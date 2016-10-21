552464990ff3627249fcc10d17cefeb1a6498133
awiouy <awiouy@gmail.com>
2016-10-21 18:02:37 +0200

    font-cursor-misc: disable autoreconf

--- packages/x11/font/font-cursor-misc/package.mk
+++ packages/x11/font/font-cursor-misc/package.mk
@@ -29,7 +29,7 @@ PKG_SHORTDESC="font-cursor-misc: X11 cursor fonts"
 PKG_LONGDESC="X11 cursor fonts."
 
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
 PKG_CONFIGURE_OPTS_TARGET="--with-fontrootdir=/usr/share/fonts"
 
