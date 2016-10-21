24f7d21f90a78d3fe7401d0018f3825a2bd6ef09
awiouy <awiouy@gmail.com>
2016-10-21 17:40:50 +0200

    rpcbind: disable autoreconf

--- packages/network/rpcbind/package.mk
+++ packages/network/rpcbind/package.mk
@@ -29,7 +29,7 @@ PKG_SHORTDESC="rpcbind: a server that converts RPC program numbers into universa
 PKG_LONGDESC="The rpcbind utility is a server that converts RPC program numbers into universal addresses."
 
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-warmstarts \
                            --disable-libwrap \
