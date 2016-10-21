d5e41936e05887c9eb703c685fd484442b4ed95c
awiouy <awiouy@gmail.com>
2016-10-21 17:33:12 +0200

    mesa: disable autoreconf

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -29,7 +29,7 @@ PKG_SHORTDESC="mesa: 3-D graphics library with OpenGL API"
 PKG_LONGDESC="Mesa is a 3-D graphics library with an API which is very similar to that of OpenGL*. To the extent that Mesa utilizes the OpenGL command syntax or state machine, it is being used with authorization from Silicon Graphics, Inc. However, the author makes no claim that Mesa is in any way a compatible replacement for OpenGL or associated with Silicon Graphics, Inc. Those who want a licensed implementation of OpenGL should contact a licensed vendor. While Mesa is not a licensed OpenGL implementation, it is currently being tested with the OpenGL conformance tests. For the current conformance status see the CONFORM file included in the Mesa distribution."
 
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
 # configure GPU drivers and dependencies:
   get_graphicdrivers
