a5645234f5baedeb1def34068fa7893bd30a2a9a
awiouy <awiouy@gmail.com>
2016-10-21 18:09:36 +0200

    xproto: disable autoreconf

--- packages/x11/proto/xproto/package.mk
+++ packages/x11/proto/xproto/package.mk
@@ -30,7 +30,7 @@ PKG_SHORTDESC="xproto: KB extension headers"
 PKG_LONGDESC="X11 extension headers"
 
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
 # package specific configure options
 PKG_CONFIGURE_OPTS_TARGET="--without-xmlto"
