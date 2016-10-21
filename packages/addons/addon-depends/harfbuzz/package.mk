59aef8148bf950d7c2137451b27fb5e935777717
awiouy <awiouy@gmail.com>
2016-10-21 16:12:03 +0200

    harfbuzz: disable autoreconf

--- packages/addons/addon-depends/harfbuzz/package.mk
+++ packages/addons/addon-depends/harfbuzz/package.mk
@@ -30,7 +30,7 @@ PKG_SECTION="x11/toolkits"
 PKG_SHORTDESC="harfbuzz: an OpenType text shaping engine."
 PKG_LONGDESC="HarfBuzz is an OpenType text shaping engine."
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
 PKG_MAINTAINER="none"
 
