3d13fd83efa26eda5c109f2426f2798fd63997e5
awiouy <awiouy@gmail.com>
2016-10-21 18:09:29 +0200

    xf86vidmodeproto: disable autoreconf

--- packages/x11/proto/xf86vidmodeproto/package.mk
+++ packages/x11/proto/xf86vidmodeproto/package.mk
@@ -29,7 +29,7 @@ PKG_SHORTDESC="xf86vidmodeproto: XF86VidMode extension headers"
 PKG_LONGDESC="XF86VidMode extension headers"
 
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
 # package specific configure options
 PKG_CONFIGURE_OPTS_TARGET="--without-xmlto"
