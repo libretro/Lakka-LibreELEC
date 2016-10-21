e44c5bdd4b4b3fc1ad2f812799179881ad4497e3
awiouy <awiouy@gmail.com>
2016-10-21 18:08:09 +0200

    bigreqsproto: disable autoreconf

--- packages/x11/proto/bigreqsproto/package.mk
+++ packages/x11/proto/bigreqsproto/package.mk
@@ -29,7 +29,7 @@ PKG_SHORTDESC="bigreqsproto: BigReqs extension headers"
 PKG_LONGDESC="BigReqs extension headers"
 
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
 # package specific configure options
 PKG_CONFIGURE_OPTS_TARGET="--without-xmlto"
