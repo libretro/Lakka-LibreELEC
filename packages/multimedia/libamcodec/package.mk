b2cf6df670077700f5aed8cc4a1cae864c924099
Lukas Rusak <lorusak@gmail.com>
2016-10-22 09:57:36 -0700

    libamcodec: requires alsa-lib

--- packages/multimedia/libamcodec/package.mk
+++ packages/multimedia/libamcodec/package.mk
@@ -31,7 +31,7 @@ case $TARGET_ARCH in
     PKG_URL="http://amlinux.ru/source/$PKG_NAME-$PKG_VERSION.tar.gz"
     ;;
 esac
-PKG_DEPENDS_TARGET="toolchain"
+PKG_DEPENDS_TARGET="toolchain alsa-lib"
 PKG_SECTION="multimedia"
 PKG_SHORTDESC="libamcodec: Interface library for Amlogic media codecs"
 PKG_LONGDESC="libamplayer: Interface library for Amlogic media codecs"
