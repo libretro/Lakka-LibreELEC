0265682f5e1dec243d899f7f106bdd881bc97e97
MilhouseVH <milhouseVH.github@nmacleod.com>
2016-10-23 01:54:40 +0100

    RBP: Uodate to firmware ec5a517

--- packages/tools/bcm2835-bootloader/package.mk
+++ packages/tools/bcm2835-bootloader/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="bcm2835-bootloader"
-PKG_VERSION="597c662"
+PKG_VERSION="ec5a517"
 PKG_REV="1"
 PKG_ARCH="arm"
 PKG_LICENSE="nonfree"
