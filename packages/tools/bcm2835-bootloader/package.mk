60c621b33d93279c68cc8e48f7c63ab3ecb9dde4
MilhouseVH <milhouseVH.github@nmacleod.com>
2016-11-01 17:54:40 +0000

    RBP: Update to firmware c53b9e5

--- packages/tools/bcm2835-bootloader/package.mk
+++ packages/tools/bcm2835-bootloader/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="bcm2835-bootloader"
-PKG_VERSION="ec5a517"
+PKG_VERSION="c53b9e5"
 PKG_REV="1"
 PKG_ARCH="arm"
 PKG_LICENSE="nonfree"
