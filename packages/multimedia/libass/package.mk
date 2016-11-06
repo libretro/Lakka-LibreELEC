a99d8579d12f7bfbd75ac771b54b74223e335692
MilhouseVH <milhouseVH.github@nmacleod.com>
2016-11-06 14:34:57 +0000

    libass: update to 0.13.4

--- packages/multimedia/libass/package.mk
+++ packages/multimedia/libass/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libass"
-PKG_VERSION="0.13.2"
+PKG_VERSION="0.13.4"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="BSD"
