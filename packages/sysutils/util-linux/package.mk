f7baa8b0e7886b37e27aaa16a5c21b3cd08849f1
Stephan Raue <stephan@openelec.tv>
2016-11-16 00:13:28 +0000

    util-linux: update to util-linux-2.28.1
    Signed-off-by: Stephan Raue <stephan@openelec.tv>
    (cherry picked from commit 0e486f8261328d7c9906a316054ecaa260fa4e9c)

--- packages/sysutils/util-linux/package.mk
+++ packages/sysutils/util-linux/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="util-linux"
-PKG_VERSION="2.28"
+PKG_VERSION="2.28.1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
