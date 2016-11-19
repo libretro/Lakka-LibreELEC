80131787fae3fc7d6842f17c02d87f8c231b60e3
Lukas Rusak <lorusak@gmail.com>
2016-11-19 11:26:11 -0800

    lftp: update to 4.7.4

--- packages/addons/addon-depends/network-tools-depends/lftp/package.mk
+++ packages/addons/addon-depends/network-tools-depends/lftp/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="lftp"
-PKG_VERSION="4.6.5"
+PKG_VERSION="4.7.4"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
@@ -29,13 +29,11 @@ PKG_SHORTDESC="ftp client"
 PKG_LONGDESC="LFTP is a sophisticated ftp/http client, and a file transfer program supporting a number of network protocols"
 PKG_AUTORECONF="no"
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-nls --with-gnu-ld --without-gnutls --with-openssl"
+PKG_CONFIGURE_OPTS_TARGET="--disable-nls \
+                           --without-gnutls \
+                           --with-openssl \
+                           --with-readline=$SYSROOT_PREFIX/usr"
 
 makeinstall_target() {
   : # nop
-}
-
-pre_configure_target() {
-  # fails to build with gcc 4.9 + lto
-  strip_lto
 }
