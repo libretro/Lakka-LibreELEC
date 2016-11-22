812e8d0c82aed11677bece00aa5041fbfc2de7bd
chewitt <github@chrishewitt.net>
2016-11-22 02:27:22 +0000

    lftp: fix build after gcc changes

--- packages/addons/addon-depends/network-tools-depends/lftp/package.mk
+++ packages/addons/addon-depends/network-tools-depends/lftp/package.mk
@@ -32,7 +32,8 @@ PKG_AUTORECONF="no"
 PKG_CONFIGURE_OPTS_TARGET="--disable-nls \
                            --without-gnutls \
                            --with-openssl \
-                           --with-readline=$SYSROOT_PREFIX/usr"
+                           --with-readline=$SYSROOT_PREFIX/usr \
+                           --with-zlib=$SYSROOT_PREFIX/usr"
 
 makeinstall_target() {
   : # nop
