d627fb8d3087b49a6dd33240ce140cfa5c5bfcb6
MilhouseVH <milhouseVH.github@nmacleod.com>
2016-11-27 05:02:57 +0000

    linux: update to linux-4.8.11

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -56,7 +56,7 @@ case "$LINUX" in
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET imx6-status-led imx6-soc-fan irqbalanced"
     ;;
   *)
-    PKG_VERSION="4.8.10"
+    PKG_VERSION="4.8.11"
     PKG_URL="http://www.kernel.org/pub/linux/kernel/v4.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     ;;
 esac
