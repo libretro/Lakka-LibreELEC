32dbdb29bccd17993b211eafb41b852b0507a494
Alex Deryskyba <alex@codesnake.com>
2016-11-27 21:08:00 +0100

    packages/linux-drivers/wetekdvb: Update WeTek proprietary DVB modules to wetekdvb-20161127

--- packages/linux-drivers/wetekdvb/package.mk
+++ packages/linux-drivers/wetekdvb/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="wetekdvb"
-PKG_VERSION="20160930"
+PKG_VERSION="20161127"
 PKG_REV="1"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="nonfree"
