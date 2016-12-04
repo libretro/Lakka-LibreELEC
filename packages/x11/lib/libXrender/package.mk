6d5e4feb753d7b99de66752fd098d9d6fdfa8453
Lukas Rusak <lorusak@gmail.com>
2016-12-04 11:22:10 -0800

    libXrender: update to 0.9.10

--- packages/x11/lib/libXrender/package.mk
+++ packages/x11/lib/libXrender/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libXrender"
-PKG_VERSION="0.9.9"
+PKG_VERSION="0.9.10"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
