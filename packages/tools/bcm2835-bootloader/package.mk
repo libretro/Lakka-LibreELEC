a20a31281d1a88efa15bb84b310c5255c2276138
MilhouseVH <milhouseVH.github@nmacleod.com>
2016-12-09 21:39:41 +0000

    RBP: Update firmware to 6d45dcf

--- packages/tools/bcm2835-bootloader/package.mk
+++ packages/tools/bcm2835-bootloader/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="bcm2835-bootloader"
-PKG_VERSION="6ce98b9"
+PKG_VERSION="6d45dcf"
 PKG_REV="1"
 PKG_ARCH="arm"
 PKG_LICENSE="nonfree"
