5759404c2c52292e53dbced818879b9e4a59f319
Lukas Rusak <lorusak@gmail.com>
2016-12-16 10:44:51 -0800

    kodi: update to 17.0-beta7-7581c4a

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="17.0-beta6-654f1b7"
+PKG_VERSION="17.0-beta7-7581c4a"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
