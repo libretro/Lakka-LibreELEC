85565c314198ff32d8da6c353ce00c8a496c37ed
MilhouseVH <milhouseVH.github@nmacleod.com>
2016-12-21 13:19:06 +0000

    mysql: update to mysql-5.7.17

--- packages/databases/mysql/package.mk
+++ packages/databases/mysql/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="mysql"
-PKG_VERSION="5.7.15"
+PKG_VERSION="5.7.17"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="LGPL"
