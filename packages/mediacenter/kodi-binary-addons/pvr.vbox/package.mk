aa0e79fa2a82781c90d5ad59ba266a4538f408aa
chewitt <github@chrishewitt.net>
2016-12-21 14:17:07 +0000

    kodi: update binary add-ons

--- packages/mediacenter/kodi-binary-addons/pvr.vbox/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.vbox/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="pvr.vbox"
-PKG_VERSION="c1a141b"
+PKG_VERSION="0afccb5"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
