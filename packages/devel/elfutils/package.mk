a6ac497c5e4d9b6bdc67ea7b52e6037733294367
MilhouseVH <milhouseVH.github@nmacleod.com>
2016-12-21 13:19:06 +0000

    elfutils: update to elfutils-0.167

--- packages/devel/elfutils/package.mk
+++ packages/devel/elfutils/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="elfutils"
-PKG_VERSION="0.166"
+PKG_VERSION="0.167"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
