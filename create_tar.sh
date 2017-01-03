0c7cf8b4719af209b6cef53fb699622afc7710ce
Jérôme Benoit <jerome.benoit@piment-noir.org>
2017-01-03 16:58:27 +0100

    create_tar.sh: remove a leftover reference to OpenELEC
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- create_tar.sh
+++ create_tar.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-git archive --format=tar --prefix=OpenELEC-source-$1/ tags/$1 | bzip2 > OpenELEC-source-$1.tar.bz2
+git archive --format=tar --prefix=LibreELEC-source-$1/ tags/$1 | bzip2 > LibreELEC-source-$1.tar.bz2
