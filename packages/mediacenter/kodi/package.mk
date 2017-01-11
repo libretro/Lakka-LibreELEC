ccb805c26376846cbe9481eda6293b8b64638235
Lukas Rusak <lorusak@gmail.com>
2017-01-11 12:21:54 -0800

    kodi: update to 4d93228

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="5c40b5c"
+PKG_VERSION="4d93228"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
