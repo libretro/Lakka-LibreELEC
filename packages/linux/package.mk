aeddaf0082d1cc32c688f9bd0bd38cd861abd2f3
Radostan Riedel <raybuntu@googlemail.com>
2017-01-15 18:24:09 +0100

    linux: Update hashes for Amlogic 3.10/3.14 kernel

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -30,13 +30,13 @@ PKG_SHORTDESC="linux26: The Linux kernel 2.6 precompiled kernel binary image and
 PKG_LONGDESC="This package contains a precompiled kernel image and the modules."
 case "$LINUX" in
   amlogic-3.10)
-    PKG_VERSION="0268c8e"
+    PKG_VERSION="1261cae"
     PKG_URL="https://github.com/LibreELEC/linux-amlogic/archive/$PKG_VERSION.tar.gz"
     PKG_SOURCE_DIR="$PKG_NAME-amlogic-$PKG_VERSION*"
     PKG_PATCH_DIRS="amlogic-3.10"
     ;;
   amlogic-3.14)
-    PKG_VERSION="6eaacba"
+    PKG_VERSION="33aa3be"
     PKG_URL="https://github.com/LibreELEC/linux-amlogic/archive/$PKG_VERSION.tar.gz"
     PKG_SOURCE_DIR="$PKG_NAME-amlogic-$PKG_VERSION*"
     PKG_PATCH_DIRS="amlogic-3.14"
