f750d5230bb15495f3eb8e13d361acddc795bb04
Ntemis <ierokipides.dem@gmail.com>
2017-01-24 02:04:23 +0200

    Add S802 SOC to Lakka-LE

---
+++ projects/S802/packages/linux-firmware/rtl8723bs_bt/package.mk
@@ -0,0 +1,33 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+#
+#  OpenELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  OpenELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="rtl8723bs_bt"
+PKG_VERSION="07cda47"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/lwfinger/rtl8723bs_bt"
+PKG_URL="https://github.com/lwfinger/rtl8723bs_bt/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="firmware"
+PKG_SHORTDESC="rtl8723bs_bt"
+PKG_LONGDESC="rtl8723bs_bt"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
