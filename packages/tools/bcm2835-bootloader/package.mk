7eacfa564ea7df22b3f678ddae73fa7fe3ac2a31
MilhouseVH <milhouseVH.github@nmacleod.com>
2017-01-25 20:41:34 +0000

    RBP: update to firmware debe2d2

--- packages/tools/bcm2835-bootloader/package.mk
+++ packages/tools/bcm2835-bootloader/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="bcm2835-bootloader"
-PKG_VERSION="6df93de"
+PKG_VERSION="debe2d2"
 PKG_ARCH="arm"
 PKG_LICENSE="nonfree"
 PKG_SITE="http://www.broadcom.com"
