867c87d15f6188c2257362dbc6a708e82b2805df
Radostan Riedel <raybuntu@googlemail.com>
2017-01-27 14:14:26 +0100

    kodi: bump to e80803a

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="4d93228"
+PKG_VERSION="e80803a"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
