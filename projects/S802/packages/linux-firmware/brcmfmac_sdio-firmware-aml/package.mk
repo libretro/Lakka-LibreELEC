eb5361e4ff6649206e4160d950ee7a46b8166c38
Ntemis <ierokipides.dem@gmail.com>
2017-01-30 01:01:44 +0200

    S8xx:Fix Broadcom Bluetooth and unify with S905

--- projects/S802/packages/linux-firmware/brcmfmac_sdio-firmware-aml/package.mk
+++ projects/S802/packages/linux-firmware/brcmfmac_sdio-firmware-aml/package.mk
@@ -1,19 +1,20 @@
 ################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+#      This file is part of LibreELEC - https://LibreELEC.tv
+#      Copyright (C) 2016 Team LibreELEC
+#      Copyright (C) 2016 kszaq
 #
-#  OpenELEC is free software: you can redistribute it and/or modify
+#  LibreELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 2 of the License, or
 #  (at your option) any later version.
 #
-#  OpenELEC is distributed in the hope that it will be useful,
+#  LibreELEC is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
 PKG_NAME="brcmfmac_sdio-firmware-aml"
@@ -21,14 +22,27 @@ PKG_VERSION="0.1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/OpenELEC/OpenELEC.tv"
-PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_SITE="https://github.com/kszaq/LibreELEC.tv"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
 PKG_SECTION="firmware"
-PKG_SHORTDESC="brcmfmac_sdio-firmware: firmware for brcm bluetooth chips used in some Amlogic based devices"
-PKG_LONGDESC="Firmware for Broadcom Bluetooth devices used in some Amlogic based devices, and brcm-patchram-plus that downloads a patchram files in the HCD format to the Bluetooth based silicon and combo chips and other utility functions."
+PKG_SHORTDESC="brcmfmac_sdio-firmware-aml: firmware for brcm bluetooth chips used in some Amlogic based devices"
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
+make_target() {
+  : # nop
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/firmware/brcm
+
+  cp -P $PKG_DIR/firmware/*.hcd $INSTALL/usr/lib/firmware/brcm
+  cd $INSTALL/usr/lib/firmware/brcm
+  for f in *.hcd; do
+    ln -sr $f $(grep --text -o 'BCM\S*' $f).hcd 2>/dev/null || true
+    ln -sr $f $(grep --text -o 'BCM\S*' $f | cut -c4-).hcd 2>/dev/null || true
+    ln -sr $f $(echo $f | sed -r 's/[^.]*/\U&/') 2>/dev/null || true
+  done
+}
