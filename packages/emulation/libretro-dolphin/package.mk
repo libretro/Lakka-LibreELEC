06b31f1d70869c0110c07b0aaeb3a4104959b99b
Lukas Rusak <lorusak@gmail.com>
2017-02-01 15:44:19 -0800

    libretro-dolphin: add package

---
+++ packages/emulation/libretro-dolphin/package.mk
@@ -0,0 +1,52 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="libretro-dolphin"
+PKG_VERSION="7d55df4"
+PKG_ARCH="none"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/libretro/dolphin"
+PKG_URL="https://github.com/libretro/dolphin/archive/$PKG_VERSION.tar.gz"
+PKG_SOURCE_DIR="dolphin-$PKG_VERSION*"
+PKG_DEPENDS_TARGET="toolchain kodi-platform enet"
+PKG_SECTION="emulation"
+PKG_SHORTDESC="Dolphin is a GameCube / Wii emulator, allowing you to play games for these two platforms on PC"
+PKG_LONGDESC="Dolphin is a GameCube / Wii emulator, allowing you to play games for these two platforms on PC"
+PKG_AUTORECONF="no"
+PKG_IS_ADDON="no"
+
+PKG_LIBNAME="dolphin_libretro.so"
+PKG_LIBPATH="libretro/$PKG_LIBNAME"
+PKG_LIBVAR="DOLPHIN_LIB"
+
+pre_configure_target() {
+  # fails to build in subdirs
+  cd $ROOT/$PKG_BUILD
+  rm -rf .$TARGET_NAME
+}
+
+make_target() {
+  make -C libretro/
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib
+  cp $PKG_LIBPATH $INSTALL/usr/lib/$PKG_LIBNAME
+  echo "set($PKG_LIBVAR $INSTALL/usr/lib/$PKG_LIBNAME)" > $SYSROOT_PREFIX/usr/$PKG_NAME-config.cmake
+}
+
