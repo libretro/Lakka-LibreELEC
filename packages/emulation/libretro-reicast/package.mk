687690d44f7b619d99ed80ee9d6d201f67ee7d73
Lukas Rusak <lorusak@gmail.com>
2017-02-01 15:44:28 -0800

    libretro-reicast: add package

---
+++ packages/emulation/libretro-reicast/package.mk
@@ -0,0 +1,62 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="libretro-reicast"
+PKG_VERSION="3601150"
+PKG_ARCH="any"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/libretro/reicast-emulator"
+PKG_URL="https://github.com/libretro/reicast-emulator/archive/$PKG_VERSION.tar.gz"
+PKG_SOURCE_DIR="reicast-emulator-$PKG_VERSION*"
+PKG_DEPENDS_TARGET="toolchain kodi-platform"
+PKG_SECTION="emulation"
+PKG_SHORTDESC="Reicast is a multiplatform Sega Dreamcast emulator"
+PKG_LONGDESC="Reicast is a multiplatform Sega Dreamcast emulator"
+PKG_AUTORECONF="no"
+PKG_IS_ADDON="no"
+
+PKG_LIBNAME="reicast_libretro.so"
+PKG_LIBPATH="$PKG_LIBNAME"
+PKG_LIBVAR="REICAST_LIB"
+
+make_target() {
+  case $PROJECT in
+    RPi)
+      make platform=armv6-hardfloat-arm1176jzf-s
+      ;;
+    RPi2)
+      make platform=rpi2
+      ;;
+    imx6)
+      make platform=armv7-neon-hardfloat-cortex-a9
+      ;;
+    WeTek_Play)
+      make platform=armv7-neon-hardfloat-cortex-a9
+      ;;
+    Generic)
+      make
+      ;;
+  esac
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib
+  cp $PKG_LIBPATH $INSTALL/usr/lib/$PKG_LIBNAME
+  echo "set($PKG_LIBVAR $INSTALL/usr/lib/$PKG_LIBNAME)" > $SYSROOT_PREFIX/usr/$PKG_NAME-config.cmake
+}
+
