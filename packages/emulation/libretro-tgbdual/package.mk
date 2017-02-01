23bd1232511730a197ae3f36d4a21612f1403e40
Lukas Rusak <lorusak@gmail.com>
2017-02-01 15:44:31 -0800

    libretro-tgbdual: add package

---
+++ packages/emulation/libretro-tgbdual/package.mk
@@ -0,0 +1,46 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="libretro-tgbdual"
+PKG_VERSION="6f8bfe5"
+PKG_ARCH="any"
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://github.com/libretro/tgbdual-libretro"
+PKG_URL="https://github.com/libretro/tgbdual-libretro/archive/$PKG_VERSION.tar.gz"
+PKG_SOURCE_DIR="tgbdual-libretro-$PKG_VERSION*"
+PKG_DEPENDS_TARGET="toolchain kodi-platform"
+PKG_SECTION="emulation"
+PKG_SHORTDESC="game.libretro.tgbdual: tgbdual for Kodi"
+PKG_LONGDESC="game.libretro.tgbdual: tgbdual for Kodi"
+PKG_AUTORECONF="no"
+PKG_IS_ADDON="no"
+
+PKG_LIBNAME="tgbdual_libretro.so"
+PKG_LIBPATH="$PKG_LIBNAME"
+PKG_LIBVAR="TGBDUAL_LIB"
+
+make_target() {
+  make
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib
+  cp $PKG_LIBPATH $INSTALL/usr/lib/$PKG_LIBNAME
+  echo "set($PKG_LIBVAR $INSTALL/usr/lib/$PKG_LIBNAME)" > $SYSROOT_PREFIX/usr/$PKG_NAME-config.cmake
+}
+
