8b36cc8aba405a06f6741622e65ac4e87fe6ea99
Lukas Rusak <lorusak@gmail.com>
2017-02-01 13:39:10 -0800

    linuxconsoletools: add package

---
+++ packages/addons/addon-depends/game-tools/linuxconsoletools/package.mk
@@ -0,0 +1,37 @@
+################################################################################
+#      This file is part of LibreELEC - http://www.libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="linuxconsoletools"
+PKG_VERSION="1.5.1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://sourceforge.net/projects/linuxconsole/"
+PKG_URL="http://prdownloads.sourceforge.net/linuxconsole/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="system"
+PKG_SHORTDESC="Set of utilities for joysticks and serial devices"
+PKG_LONGDESC="Set of utilities for joysticks and serial devices"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+PKG_MAKE_OPTS_TARGET="SYSTEMD_SUPPORT=0"
+
+makeinstall_target() {
+  :
+}
