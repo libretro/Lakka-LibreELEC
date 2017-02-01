1b6c985c34e678eb59f523eaa6030b1346afbab9
Lukas Rusak <lorusak@gmail.com>
2017-02-01 15:44:22 -0800

    libretro-mame: add package

---
+++ packages/emulation/libretro-mame/package.mk
@@ -0,0 +1,71 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="libretro-mame"
+PKG_VERSION="769dd8d"
+PKG_ARCH="x86_64"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/libretro/mame"
+PKG_URL="https://github.com/libretro/mame/archive/$PKG_VERSION.tar.gz"
+PKG_SOURCE_DIR="mame-$PKG_VERSION*"
+PKG_DEPENDS_TARGET="toolchain kodi-platform"
+PKG_SECTION="emulation"
+PKG_SHORTDESC="game.libretro.mame: MAME for Kodi"
+PKG_LONGDESC="game.libretro.mame: MAME for Kodi"
+PKG_AUTORECONF="no"
+PKG_IS_ADDON="no"
+
+PKG_LIBNAME="mame_libretro.so"
+PKG_LIBPATH="$PKG_LIBNAME"
+PKG_LIBVAR="MAME_LIB"
+
+pre_make_target() {
+  strip_lto
+  strip_gold
+
+  export OVERRIDE_CC="$CC"
+  export OVERRIDE_CXX="$CXX"
+  export OVERRIDE_LD="$CXX"
+}
+
+make_target() {
+  case $PROJECT in
+    RPi)
+      make platform=armv6-hardfloat-arm1176jzf-s
+      ;;
+    RPi2)
+      make platform=armv7-neon-hardfloat-cortex-a7
+      ;;
+    imx6)
+      make platform=armv7-neon-hardfloat-cortex-a9
+      ;;
+    WeTek_Play)
+      make platform=armv7-neon-hardfloat-cortex-a9
+      ;;
+    Generic)
+      make -f Makefile.libretro
+      ;;
+  esac
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib
+  cp $PKG_LIBPATH $INSTALL/usr/lib/$PKG_LIBNAME
+  echo "set($PKG_LIBVAR $INSTALL/usr/lib/$PKG_LIBNAME)" > $SYSROOT_PREFIX/usr/$PKG_NAME-config.cmake
+}
+
