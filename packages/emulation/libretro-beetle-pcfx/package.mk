3bf7cc4e1e13d097a4115cbc0eadbd7196fcb885
Lukas Rusak <lorusak@gmail.com>
2017-02-01 15:44:16 -0800

    libretro-beetle-pcfx: add package

---
+++ packages/emulation/libretro-beetle-pcfx/package.mk
@@ -0,0 +1,65 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="libretro-beetle-pcfx"
+PKG_VERSION="89d16cd"
+PKG_ARCH="any"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/libretro/beetle-pcfx-libretro"
+PKG_URL="https://github.com/libretro/beetle-pcfx-libretro/archive/$PKG_VERSION.tar.gz"
+PKG_SOURCE_DIR="beetle-pcfx-libretro-$PKG_VERSION*"
+PKG_DEPENDS_TARGET="toolchain kodi-platform"
+PKG_SECTION="emulation"
+PKG_SHORTDESC="Standalone port of Mednafen PCFX to libretro"
+PKG_LONGDESC="Standalone port of Mednafen PCFX to libretro"
+PKG_AUTORECONF="no"
+PKG_IS_ADDON="no"
+
+PKG_LIBNAME="mednafen_pcfx_libretro.so"
+PKG_LIBPATH="$PKG_LIBNAME"
+PKG_LIBVAR="BEETLE_PCFX_LIB"
+
+make_target() {
+  case $PROJECT in
+    RPi)
+      make platform=armv6-hardfloat
+      ;;
+    RPi2)
+      make platform=armv7-neon-hardfloat
+      ;;
+    imx6)
+      make platform=armv7-cortexa9-neon-hardfloat
+      ;;
+    WeTek_Play)
+      make platform=armv7-cortexa9-neon-hardfloat
+      ;;
+    Odroid_C2|WeTek_Hub|WeTek_Play_2)
+      make platform=aarch64
+      ;;
+    Generic)
+      make
+      ;;
+  esac
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib
+  cp $PKG_LIBPATH $INSTALL/usr/lib/$PKG_LIBNAME
+  echo "set($PKG_LIBVAR $INSTALL/usr/lib/$PKG_LIBNAME)" > $SYSROOT_PREFIX/usr/$PKG_NAME-config.cmake
+}
+
