e4bc0bb3ab12e4701959f41ea929bd1b72be6e8e
Lukas Rusak <lorusak@gmail.com>
2017-02-01 15:44:31 -0800

    libretro-snes9x2010: add package

---
+++ packages/emulation/libretro-snes9x2010/package.mk
@@ -0,0 +1,42 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="libretro-snes9x2010"
+PKG_VERSION="74953d8"
+PKG_ARCH="any"
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://github.com/libretro/snes9x2010"
+PKG_URL="https://github.com/libretro/snes9x2010/archive/$PKG_VERSION.tar.gz"
+PKG_SOURCE_DIR="snes9x2010-$PKG_VERSION*"
+PKG_DEPENDS_TARGET="toolchain kodi-platform"
+PKG_SECTION="emulation"
+PKG_SHORTDESC="snes9x2010 for Kodi"
+PKG_LONGDESC="snes9x2010 for Kodi"
+PKG_AUTORECONF="no"
+PKG_IS_ADDON="no"
+
+PKG_LIBNAME="snes9x2010_libretro.so"
+PKG_LIBPATH="$PKG_LIBNAME"
+PKG_LIBVAR="SNES9X2010_LIB"
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib
+  cp $PKG_LIBPATH $INSTALL/usr/lib/$PKG_LIBNAME
+  echo "set($PKG_LIBVAR $INSTALL/usr/lib/$PKG_LIBNAME)" > $SYSROOT_PREFIX/usr/$PKG_NAME-config.cmake
+}
+
