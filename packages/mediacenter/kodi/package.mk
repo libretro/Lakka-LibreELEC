c0622e06c23281a32deaa562bf32a3218ebed982
MilhouseVH <milhouseVH.github@nmacleod.com>
2017-02-02 08:56:21 +0000

    kodi: update to 18.0 alpha1-d829753

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="1f12c2f"
+PKG_VERSION="d829753"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
