07e8421ff5f7c0f7a210cacbe8df8c60ba6d33aa
MilhouseVH <milhouseVH.github@nmacleod.com>
2017-02-04 18:59:10 +0000

    RBP: update to firmware 475a89a

--- packages/graphics/bcm2835-driver/package.mk
+++ packages/graphics/bcm2835-driver/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="bcm2835-driver"
-PKG_VERSION="debe2d2"
+PKG_VERSION="475a89a"
 PKG_ARCH="any"
 PKG_LICENSE="nonfree"
 PKG_SITE="http://www.broadcom.com"
