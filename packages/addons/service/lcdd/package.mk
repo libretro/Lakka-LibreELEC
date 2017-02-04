ac32f51d3d022f94b520731256e64a283d3fbe63
awiouy <awiouy@gmail.com>
2017-02-04 10:23:32 +0100

    lcdd: update to 0.5.8

--- packages/addons/service/lcdd/package.mk
+++ packages/addons/service/lcdd/package.mk
@@ -17,13 +17,13 @@
 ################################################################################
 
 PKG_NAME="lcdd"
-PKG_VERSION="c05a7de"
-PKG_REV="102"
+PKG_VERSION="0.5.8"
+PKG_REV="103"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://lcdproc.org/"
-PKG_URL="https://github.com/lcdproc/lcdproc/archive/$PKG_VERSION.tar.gz"
-PKG_SOURCE_DIR="lcdproc-$PKG_VERSION*"
+PKG_URL="https://github.com/lcdproc/lcdproc/archive/lcdproc-$PKG_VERSION.tar.gz"
+PKG_SOURCE_DIR="lcdproc-lcdproc-$PKG_VERSION*"
 PKG_DEPENDS_TARGET="toolchain freetype libftdi1 libhid libugpio libusb netbsd-curses serdisplib"
 PKG_SECTION="service"
 PKG_SHORTDESC="LCDproc: Software to display system information from your Linux/*BSD box on a LCD"
