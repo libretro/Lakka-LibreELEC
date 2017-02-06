d5bc9d07dd281b2b1e536b8798e5880ca4ce42a4
Lukas Rusak <lorusak@gmail.com>
2017-02-05 22:05:13 -0800

    libretro-hatari: update to e99678b and cleanup

--- packages/emulation/libretro-hatari/package.mk
+++ packages/emulation/libretro-hatari/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libretro-hatari"
-PKG_VERSION="4930236"
+PKG_VERSION="e99678b"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/hatari"
@@ -29,6 +29,7 @@ PKG_SHORTDESC="game.libretro.hatari: hatari for Kodi"
 PKG_LONGDESC="game.libretro.hatari: hatari for Kodi"
 PKG_AUTORECONF="no"
 PKG_IS_ADDON="no"
+PKG_USE_CMAKE="no"
 
 PKG_LIBNAME="hatari_libretro.so"
 PKG_LIBPATH="$PKG_LIBNAME"
@@ -44,8 +45,8 @@ make_target() {
 }
 
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/lib
-  cp $PKG_LIBPATH $INSTALL/usr/lib/$PKG_LIBNAME
-  echo "set($PKG_LIBVAR $INSTALL/usr/lib/$PKG_LIBNAME)" > $SYSROOT_PREFIX/usr/$PKG_NAME-config.cmake
+  mkdir -p $SYSROOT_PREFIX/usr/lib/cmake/$PKG_NAME
+  cp $PKG_LIBPATH $SYSROOT_PREFIX/usr/lib/$PKG_LIBNAME
+  echo "set($PKG_LIBVAR $SYSROOT_PREFIX/usr/lib/$PKG_LIBNAME)" > $SYSROOT_PREFIX/usr/lib/cmake/$PKG_NAME/$PKG_NAME-config.cmake
 }
 
