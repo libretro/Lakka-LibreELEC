157c8069554c13486702be55a6ab4e3a25db4574
Lukas Rusak <lorusak@gmail.com>
2017-02-05 22:11:57 -0800

    game.libretro.4do: update to 38e0fe5

--- packages/mediacenter/kodi-binary-addons/game.libretro.4do/package.mk
+++ packages/mediacenter/kodi-binary-addons/game.libretro.4do/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="game.libretro.4do"
-PKG_VERSION="da1924b"
+PKG_VERSION="38e0fe5"
 PKG_REV="100"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
