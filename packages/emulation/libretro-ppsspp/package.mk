ac497e80b351a61cbe668619775db7c69187a12f
Lukas Rusak <lorusak@gmail.com>
2017-02-05 22:05:15 -0800

    libretro-ppsspp: update to 5f7bcf7 and cleanup

--- packages/emulation/libretro-ppsspp/package.mk
+++ packages/emulation/libretro-ppsspp/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libretro-ppsspp"
-PKG_VERSION="e1bb9da"
+PKG_VERSION="5f7bcf7"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/libretro-ppsspp"
@@ -28,6 +28,7 @@ PKG_SHORTDESC="A PSP emulator for Android, Windows, Mac, Linux and Blackberry 10
 PKG_LONGDESC="A PSP emulator for Android, Windows, Mac, Linux and Blackberry 10, written in C++."
 PKG_AUTORECONF="no"
 PKG_IS_ADDON="no"
+PKG_USE_CMAKE="no"
 
 PKG_LIBNAME="ppsspp_libretro.so"
 PKG_LIBPATH="libretro/$PKG_LIBNAME"
@@ -66,8 +67,8 @@ make_target() {
 }
 
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/lib
-  cp $PKG_LIBPATH $INSTALL/usr/lib/$PKG_LIBNAME
-  echo "set($PKG_LIBVAR $INSTALL/usr/lib/$PKG_LIBNAME)" > $SYSROOT_PREFIX/usr/$PKG_NAME-config.cmake
+  mkdir -p $SYSROOT_PREFIX/usr/lib/cmake/$PKG_NAME
+  cp $PKG_LIBPATH $SYSROOT_PREFIX/usr/lib/$PKG_LIBNAME
+  echo "set($PKG_LIBVAR $SYSROOT_PREFIX/usr/lib/$PKG_LIBNAME)" > $SYSROOT_PREFIX/usr/lib/cmake/$PKG_NAME/$PKG_NAME-config.cmake
 }
 
