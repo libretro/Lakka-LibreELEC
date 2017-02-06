4cb89319097789b68530c35987cfcd163afd0f5b
Lukas Rusak <lorusak@gmail.com>
2017-02-05 22:11:59 -0800

    game.libretro.bsnes-mercury-accuracy: update to f4433c3

--- packages/mediacenter/kodi-binary-addons/game.libretro.bsnes-mercury-accuracy/package.mk
+++ packages/mediacenter/kodi-binary-addons/game.libretro.bsnes-mercury-accuracy/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="game.libretro.bsnes-mercury-accuracy"
-PKG_VERSION="56abee8"
+PKG_VERSION="f4433c3"
 PKG_REV="100"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
