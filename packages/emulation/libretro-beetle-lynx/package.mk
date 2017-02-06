f27897e5c7c08d332462b2ff80d0300e30a2d801
Lukas Rusak <lorusak@gmail.com>
2017-02-05 22:05:06 -0800

    libretro-beetle-lynx: update to 8ea8650 and cleanup

--- packages/emulation/libretro-beetle-lynx/package.mk
+++ packages/emulation/libretro-beetle-lynx/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libretro-beetle-lynx"
-PKG_VERSION="76fd67f"
+PKG_VERSION="8ea8650"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/beetle-lynx-libretro"
@@ -29,18 +29,19 @@ PKG_SHORTDESC="Standalone port of Mednafen Lynx to libretro, itself a fork of Ha
 PKG_LONGDESC="Standalone port of Mednafen Lynx to libretro, itself a fork of Handy"
 PKG_AUTORECONF="no"
 PKG_IS_ADDON="no"
+PKG_USE_CMAKE="no"
 
 PKG_LIBNAME="mednafen_lynx_libretro.so"
 PKG_LIBPATH="$PKG_LIBNAME"
-PKG_LIBVAR="BEETLE_LYNX_LIB"
+PKG_LIBVAR="BEETLE-LYNX_LIB"
 
 make_target() {
   make
 }
 
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/lib
-  cp $PKG_LIBPATH $INSTALL/usr/lib/$PKG_LIBNAME
-  echo "set($PKG_LIBVAR $INSTALL/usr/lib/$PKG_LIBNAME)" > $SYSROOT_PREFIX/usr/$PKG_NAME-config.cmake
+  mkdir -p $SYSROOT_PREFIX/usr/lib/cmake/$PKG_NAME
+  cp $PKG_LIBPATH $SYSROOT_PREFIX/usr/lib/$PKG_LIBNAME
+  echo "set($PKG_LIBVAR $SYSROOT_PREFIX/usr/lib/$PKG_LIBNAME)" > $SYSROOT_PREFIX/usr/lib/cmake/$PKG_NAME/$PKG_NAME-config.cmake
 }
 
