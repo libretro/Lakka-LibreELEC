189a8d13722c49bcdaa831f3cd08af6116512288
Lukas Rusak <lorusak@gmail.com>
2017-02-05 22:11:59 -0800

    game.libretro.bsnes-mercury-performance: update to c32bd32

--- packages/mediacenter/kodi-binary-addons/game.libretro.bsnes-mercury-performance/package.mk
+++ packages/mediacenter/kodi-binary-addons/game.libretro.bsnes-mercury-performance/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="game.libretro.bsnes-mercury-performance"
-PKG_VERSION="0cf2426"
+PKG_VERSION="c32bd32"
 PKG_REV="100"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
