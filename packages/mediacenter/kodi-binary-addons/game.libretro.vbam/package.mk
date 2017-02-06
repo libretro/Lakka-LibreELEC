e408ce040fdb1024081d785b18a3360a1b6e2ffd
Lukas Rusak <lorusak@gmail.com>
2017-02-05 22:15:23 -0800

    game.libretro.vbam: update to 53f0e6a

--- packages/mediacenter/kodi-binary-addons/game.libretro.vbam/package.mk
+++ packages/mediacenter/kodi-binary-addons/game.libretro.vbam/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="game.libretro.vbam"
-PKG_VERSION="2441d35"
+PKG_VERSION="53f0e6a"
 PKG_REV="100"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
