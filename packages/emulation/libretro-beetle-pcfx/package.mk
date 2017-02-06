c299c3357a310345fbbd1bf8fc3fc24efd3cf971
Lukas Rusak <lorusak@gmail.com>
2017-02-05 22:05:07 -0800

    libretro-beetle-pcfx: update to f53c58a and cleanup

--- packages/emulation/libretro-beetle-pcfx/package.mk
+++ packages/emulation/libretro-beetle-pcfx/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libretro-beetle-pcfx"
-PKG_VERSION="89d16cd"
+PKG_VERSION="f53c58a"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/beetle-pcfx-libretro"
@@ -29,10 +29,11 @@ PKG_SHORTDESC="Standalone port of Mednafen PCFX to libretro"
 PKG_LONGDESC="Standalone port of Mednafen PCFX to libretro"
 PKG_AUTORECONF="no"
 PKG_IS_ADDON="no"
+PKG_USE_CMAKE="no"
 
 PKG_LIBNAME="mednafen_pcfx_libretro.so"
 PKG_LIBPATH="$PKG_LIBNAME"
-PKG_LIBVAR="BEETLE_PCFX_LIB"
+PKG_LIBVAR="BEETLE-PCFX_LIB"
 
 make_target() {
   case $PROJECT in
@@ -58,8 +59,8 @@ make_target() {
 }
 
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/lib
-  cp $PKG_LIBPATH $INSTALL/usr/lib/$PKG_LIBNAME
-  echo "set($PKG_LIBVAR $INSTALL/usr/lib/$PKG_LIBNAME)" > $SYSROOT_PREFIX/usr/$PKG_NAME-config.cmake
+  mkdir -p $SYSROOT_PREFIX/usr/lib/cmake/$PKG_NAME
+  cp $PKG_LIBPATH $SYSROOT_PREFIX/usr/lib/$PKG_LIBNAME
+  echo "set($PKG_LIBVAR $SYSROOT_PREFIX/usr/lib/$PKG_LIBNAME)" > $SYSROOT_PREFIX/usr/lib/cmake/$PKG_NAME/$PKG_NAME-config.cmake
 }
 
