ffc6e22639d5c07ecd95ad54b976df70d18b4ee7
Lukas Rusak <lorusak@gmail.com>
2017-02-05 22:15:23 -0800

    game.libretro.snes9x: update to 957307e

--- packages/mediacenter/kodi-binary-addons/game.libretro.snes9x/package.mk
+++ packages/mediacenter/kodi-binary-addons/game.libretro.snes9x/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="game.libretro.snes9x"
-PKG_VERSION="230865b"
+PKG_VERSION="957307e"
 PKG_REV="100"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
