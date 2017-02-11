7a16d36127e5206d76908f6eff76aee5f6022564
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-02-11 14:51:18 +0100

    Update pcsx_rearmed

--- packages/libretro/pcsx_rearmed/package.mk
+++ packages/libretro/pcsx_rearmed/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="pcsx_rearmed"
-PKG_VERSION="731139f"
+PKG_VERSION="76996fc"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
