a59bfb3e3a7540d92042b4d39ad7e213f4b1bcec
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-02-13 00:56:19 +0100

    Update XU3 kernel

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -30,7 +30,7 @@ PKG_SHORTDESC="linux26: The Linux kernel 2.6 precompiled kernel binary image and
 PKG_LONGDESC="This package contains a precompiled kernel image and the modules."
 case "$LINUX" in
   linux-odroidxu3)
-    PKG_VERSION="e337a20"
+    PKG_VERSION="4603743"
     PKG_URL="$LAKKA_MIRROR/$PKG_NAME-$PKG_VERSION.tar.xz"
     ;;
   linux-odroidc-3.10.y)
