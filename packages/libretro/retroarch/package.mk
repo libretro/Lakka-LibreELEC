a732f4ab1790eef69a95615c56bb89df930b3f11
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-02-21 15:40:54 +0100

    Update RA. Move Menu Settings at the top level.

--- packages/libretro/retroarch/package.mk
+++ packages/libretro/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="8d188f"
+PKG_VERSION="b648f45"
 PKG_REV="3"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
