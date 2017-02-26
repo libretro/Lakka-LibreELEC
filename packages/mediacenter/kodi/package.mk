ad8a1547fb66c66a9068516a88bc6ae9d14ddeca
chewitt <github@chrishewitt.net>
2017-02-26 12:15:18 +0000

    kodi: bump to 17.1-RC1

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="a10c504"
+PKG_VERSION="da07d20"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
