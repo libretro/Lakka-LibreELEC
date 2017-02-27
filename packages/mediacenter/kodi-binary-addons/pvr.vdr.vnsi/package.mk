7e967b1f6649d901d0b55769e95dd279fa917e51
chewitt <github@chrishewitt.net>
2017-02-27 01:34:20 +0000

    kodi: binary add-on updates

--- packages/mediacenter/kodi-binary-addons/pvr.vdr.vnsi/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.vdr.vnsi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="pvr.vdr.vnsi"
-PKG_VERSION="dcdc216"
+PKG_VERSION="9659c8c"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
