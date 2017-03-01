0a2030deaa3500da8bc89ce86124ae6910bf06c2
awiouy <awiouy@gmail.com>
2017-03-01 02:12:27 +0100

    imagemagick: update to 7.0.5-0

--- packages/addons/addon-depends/emby-depends/imagemagick/package.mk
+++ packages/addons/addon-depends/emby-depends/imagemagick/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="imagemagick"
-PKG_VERSION="6.9.6-7"
+PKG_VERSION="7.0.5-0"
 PKG_ARCH="any"
 PKG_LICENSE="http://www.imagemagick.org/script/license.php"
 PKG_SITE="http://www.imagemagick.org/"
