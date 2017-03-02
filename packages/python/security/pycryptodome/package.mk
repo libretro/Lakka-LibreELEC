a5c8f44d5233b30bc63d57447ec3cb239394fe9a
MilhouseVH <milhouseVH.github@nmacleod.com>
2017-03-02 01:08:35 +0000

    pycryptodome: add package

---
+++ packages/python/security/pycryptodome/package.mk
@@ -0,0 +1,55 @@
+################################################################################
+#      This file is part of LibreELEC - http://www.libreelec.tv
+#      Copyright (C) 2017 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="pycryptodome"
+PKG_VERSION="3.4.5"
+PKG_ARCH="any"
+PKG_LICENSE="BSD"
+PKG_SITE="https://pypi.python.org/pypi/pycryptodome"
+PKG_URL="https://files.pythonhosted.org/packages/source/p/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain Python distutilscross:host"
+PKG_SECTION="python/security"
+PKG_SHORTDESC="Cryptographic library for Python"
+PKG_LONGDESC="PyCryptodome is a self-contained Python package of low-level cryptographic primitives."
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+pre_configure_target() {
+  cd $PKG_BUILD
+  rm -rf .$TARGET_NAME
+
+  export PYTHONXCPREFIX="$SYSROOT_PREFIX/usr"
+  export LDSHARED="$CC -shared"
+}
+
+make_target() {
+  python setup.py build --cross-compile
+}
+
+makeinstall_target() {
+  python setup.py install --root=$INSTALL --prefix=/usr
+
+  # Remove SelfTest bloat
+  find $INSTALL -type d -name SelfTest -exec rm -fr "{}" \; 2>/dev/null || true
+  find $INSTALL -name SOURCES.txt -exec sed -i "/\/SelfTest\//d;" "{}" \;
+}
+
+post_makeinstall_target() {
+  find $INSTALL/usr/lib -name "*.py" -exec rm -rf "{}" ";"
+}
