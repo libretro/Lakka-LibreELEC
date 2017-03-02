4ff8eb54830608df1d67a63ac15f6d7c76156e48
MilhouseVH <milhouseVH.github@nmacleod.com>
2017-03-02 17:20:16 +0000

    RBP: update firmware to f5446ba

--- packages/tools/bcm2835-bootloader/package.mk
+++ packages/tools/bcm2835-bootloader/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="bcm2835-bootloader"
-PKG_VERSION="a5d4376"
+PKG_VERSION="f5446ba"
 PKG_ARCH="arm"
 PKG_LICENSE="nonfree"
 PKG_SITE="http://www.broadcom.com"
