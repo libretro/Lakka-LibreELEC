3c81ce4ab982c986b70fc372f53984839de61ff8
Ntemis <ierokipides.dem@gmail.com>
2017-03-05 20:05:46 +0200

    S8xx:Switch DEVICE to SYSTEM build images

--- build_all.sh
+++ build_all.sh
@@ -39,15 +39,16 @@ DISTRO=Lakka PROJECT=Gamegirl ARCH=arm make image -j8
 >&2 echo "Virtual.x86_64"
 DISTRO=Lakka PROJECT=Virtual ARCH=x86_64 make image -j8
 >&2 echo "S802.arm"
-DISTRO=Lakka PROJECT=S802 DEVICE=S82 ARCH=arm make amlpkg -j8
-DISTRO=Lakka PROJECT=S802 DEVICE=M8 ARCH=arm make amlpkg -j8
-DISTRO=Lakka PROJECT=S802 DEVICE=T8 ARCH=arm make amlpkg -j8
-DISTRO=Lakka PROJECT=S802 DEVICE=MXIII-1G ARCH=arm make amlpkg -j8
+DISTRO=Lakka PROJECT=S802 SYSTEM=S82 ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=S802 SYSTEM=M8 ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=S802 SYSTEM=T8 ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=S802 SYSTEM=MXIII-1G ARCH=arm make image -j8
 >&2 echo "S805.arm"
-DISTRO=Lakka PROJECT=S805 DEVICE=HD18Q ARCH=arm make amlpkg -j8
-DISTRO=Lakka PROJECT=S805 DEVICE=M201C ARCH=arm make amlpkg -j8
-DISTRO=Lakka PROJECT=S805 DEVICE=MK808B-Plus ARCH=arm make amlpkg -j8
-DISTRO=Lakka PROJECT=S805 DEVICE=MXQ ARCH=arm make amlpkg -j8
+DISTRO=Lakka PROJECT=S805 SYSTEM=MXQ ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=S805 SYSTEM=HD18Q ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=S805 SYSTEM=M201C ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=S805 SYSTEM=M201D ARCH=arm make image -j8
+DISTRO=Lakka PROJECT=S805 SYSTEM=MK808B-Plus ARCH=arm make image -j8
 >&2 echo "S905.arm"
 DISTRO=Lakka PROJECT=S905 ARCH=arm make image -j8
 >&2 echo "H3.op2.arm"
