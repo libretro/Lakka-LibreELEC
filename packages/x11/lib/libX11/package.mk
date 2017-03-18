4a269494eb9c491c5558e169cd124a352e5ec1c2
Stephan Raue <stephan@openelec.tv>
2017-03-18 10:09:25 -0700

    libX11: update to libX11-1.6.5
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/x11/lib/libX11/package.mk
+++ packages/x11/lib/libX11/package.mk
@@ -1,6 +1,6 @@
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+#      Copyright (C) 2009-2017 Stephan Raue (stephan@openelec.tv)
 #
 #  OpenELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libX11"
-PKG_VERSION="1.6.4"
+PKG_VERSION="1.6.5"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.x.org/"
