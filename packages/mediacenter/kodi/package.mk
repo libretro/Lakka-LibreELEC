c3a7fcffaee3759eceedc171e4c66bd40d58d88c
MilhouseVH <milhouseVH.github@nmacleod.com>
2017-03-20 22:12:27 +0000

    kodi: update to kodi-17.1

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="da07d20"
+PKG_VERSION="fc1619b"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
