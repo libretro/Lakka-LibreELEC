8966e58d32db3990464709faa22779ae2998baa1
chewitt <github@chrishewitt.net>
2017-03-23 08:13:40 +0000

    iwlwifi: udpate to ba301aa

--- packages/linux-firmware/iwlwifi-firmware/package.mk
+++ packages/linux-firmware/iwlwifi-firmware/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="iwlwifi-firmware"
-PKG_VERSION="cb23d96"
+PKG_VERSION="ba301aa"
 PKG_ARCH="any"
 PKG_LICENSE="Free-to-use"
 PKG_SITE="https://github.com/LibreELEC/iwlwifi-firmware"
