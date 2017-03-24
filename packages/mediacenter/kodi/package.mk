2bacb8c13d391c82d7c6f5c7ee27062c1d0cb9a2
MilhouseVH <milhouseVH.github@nmacleod.com>
2017-03-24 22:24:59 +0000

    kodi: update to latest kodi 18a1-78cce57

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="93e9d69"
+PKG_VERSION="78cce57"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
