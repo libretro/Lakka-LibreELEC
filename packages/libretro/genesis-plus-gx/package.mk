8fb2109e28cf32358911e22cfb71ce9a376111aa
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-03-27 08:31:42 -0500

    Update genesis plus gx, add cheat support

--- packages/libretro/genesis-plus-gx/package.mk
+++ packages/libretro/genesis-plus-gx/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="genesis-plus-gx"
-PKG_VERSION="a8ac6ad"
+PKG_VERSION="f64e755"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Non-commercial"
