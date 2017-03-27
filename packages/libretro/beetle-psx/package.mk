df3aa0e70fd1004bb33ff4e055d654fea5ddb8a8
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-03-27 08:29:40 -0500

    Update beetle-psx, bugfixes and SABR support

--- packages/libretro/beetle-psx/package.mk
+++ packages/libretro/beetle-psx/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="beetle-psx"
-PKG_VERSION="e3aae3d"
+PKG_VERSION="3ac1ccc"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
