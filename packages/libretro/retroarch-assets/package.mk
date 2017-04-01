45b1ea470dbf95a87a9654d88985791d592fb648
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-04-01 10:17:28 -0500

    Udpate assets, add korean glyphs, and Commodore 64 icons

--- packages/libretro/retroarch-assets/package.mk
+++ packages/libretro/retroarch-assets/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch-assets"
-PKG_VERSION="5c08c2e"
+PKG_VERSION="eceae2e"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
