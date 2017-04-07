53ca0ea21450a9aaa3b08dfbcb6a01cbd93c49f0
Matthias Reichl <hias@horus.com>
2017-04-07 15:27:13 +0200

    lirc: use upstream method to configure and run lirc
    Add helper scripts for lircd and lircd-uinput so that we can
    override lirc_options.conf and lircd.conf via files in
    /storage/.config
    
    Use separate systemd units for lircd and lircd-uinput, like in
    upstream, and just add a ConditionPathExists to enable/disable
    the services.
    
    The lirc socket is managed by systemd via lircd.socket, like
    in upstream.
    
    Remove udev file for enabling lirc, this is now handled via
    systemd only.

--- packages/sysutils/lirc/package.mk
+++ packages/sysutils/lirc/package.mk
@@ -59,10 +59,17 @@ post_makeinstall_target() {
 
   mkdir -p $INSTALL/usr/lib/libreelec
     cp $PKG_DIR/scripts/lircd_helper $INSTALL/usr/lib/libreelec
+    cp $PKG_DIR/scripts/lircd_uinput_helper $INSTALL/usr/lib/libreelec
 
   mkdir -p $INSTALL/usr/lib/udev
     cp $PKG_DIR/scripts/lircd_wakeup_enable $INSTALL/usr/lib/udev
 
   mkdir -p $INSTALL/usr/share/services
     cp -P $PKG_DIR/default.d/*.conf $INSTALL/usr/share/services
+}
+
+post_install() {
+  enable_service lircd.socket
+  enable_service lircd.service
+  enable_service lircd-uinput.service
 }
