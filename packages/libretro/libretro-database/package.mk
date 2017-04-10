3c65c60903a408a301d87d59872058da44cf7589
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-04-10 08:15:59 -0500

    Update libretro-database

--- packages/libretro/libretro-database/package.mk
+++ packages/libretro/libretro-database/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="libretro-database"
-PKG_VERSION="85067ce"
+PKG_VERSION="c23d54e"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
