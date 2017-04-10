0d897a4317083d302901c05a0029d65cea7c6012
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-04-10 08:15:31 -0500

    Update tyrquake

--- packages/libretro/tyrquake/package.mk
+++ packages/libretro/tyrquake/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="tyrquake"
-PKG_VERSION="56e49b9"
+PKG_VERSION="383a657"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
