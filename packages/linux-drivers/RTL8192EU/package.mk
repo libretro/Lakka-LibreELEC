93a40145828a650da60eab3ca18d9eac22957e11
Carlos Garcés <CGarces@users.noreply.github.com>
2017-04-17 08:50:41 +0200

    RTL8192EU: update to a7fd035

--- packages/linux-drivers/RTL8192EU/package.mk
+++ packages/linux-drivers/RTL8192EU/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="RTL8192EU"
-PKG_VERSION="0c80da7"
+PKG_VERSION="a7fd035"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/Mange/rtl8192eu-linux-driver"
