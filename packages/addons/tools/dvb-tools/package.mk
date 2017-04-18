309710e64b4dadec72bf73fa2d12bc9884597c54
cvh <namerp@googlemail.com>
2017-04-18 23:58:50 +0200

    dvb-tools: update addon

--- packages/addons/tools/dvb-tools/package.mk
+++ packages/addons/tools/dvb-tools/package.mk
@@ -1,6 +1,6 @@
 ################################################################################
-#      This file is part of LibreELEC - http://www.libreelec.tv
-#      Copyright (C) 2016 Team LibreELEC
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016-present Team LibreELEC
 #
 #  LibreELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -18,7 +18,7 @@
 
 PKG_NAME="dvb-tools"
 PKG_VERSION=""
-PKG_REV="101"
+PKG_REV="102"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE=""
