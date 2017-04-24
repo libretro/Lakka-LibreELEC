d05cc48ea9286478a224016087e88d31e87503e3
MilhouseVH <milhouseVH.github@nmacleod.com>
2017-04-24 19:22:33 +0100

    vfs.sacd: new kodi-binary-addon

---
+++ packages/mediacenter/kodi-binary-addons/vfs.sacd/package.mk
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="vfs.sacd"
+PKG_VERSION="ade78f22e220ffd91d1bfa573be7b2c3be9ca763"
+PKG_SHA256="ecd33002fa2b119c7bb6819c3cdbf1f7b93841dd819fb70b1f0a9fa9dce93755"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://www.kodi.tv"
+PKG_URL="https://github.com/xbmc/vfs.sacd/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform"
+PKG_SECTION=""
+PKG_SHORTDESC="vfs.sacd"
+PKG_LONGDESC="vfs.sacd"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="kodi.vfs"
