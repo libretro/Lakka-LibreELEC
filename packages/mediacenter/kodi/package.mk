63f64179a24a67782924683762613ad68e8394ee
chewitt <github@chrishewitt.net>
2017-04-25 07:05:05 +0100

    kodi: bump to 91a9066 (25-04-2017)

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="78cce57"
+PKG_VERSION="91a9066"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
