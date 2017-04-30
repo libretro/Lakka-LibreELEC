0f167402987b3221db1d10711bf1fa5cd8e57bfd
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-04-30 10:56:18 -0500

    Checkout the PKG_VERSION and manage the submodules directly in scripts/unpack

--- packages/libretro/picodrive/package.mk
+++ packages/libretro/picodrive/package.mk
@@ -34,15 +34,6 @@ PKG_LONGDESC="This is yet another Megadrive / Genesis / Sega CD / Mega CD / 32X 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-post_unpack() {
-  rm -rf $BUILD/$PKG_NAME-$PKG_VERSION/
-  git clone https://github.com/libretro/picodrive.git $BUILD/$PKG_NAME-$PKG_VERSION/
-  cd $BUILD/$PKG_NAME-$PKG_VERSION/
-  git checkout $PKG_VERSION
-  git submodule update --init
-  cd $ROOT
-}
-
 pre_configure_target() {
   strip_gold
 }
