bf03888292c741358842712bac7f804b9ba50c7a
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-05-02 16:29:44 -0500

    Dolphin package

---
+++ packages/libretro/dolphin/package.mk
@@ -0,0 +1,46 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.tv; see the file COPYING.  If not, write to
+#  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
+#  http://www.gnu.org/copyleft/gpl.html
+################################################################################
+
+PKG_NAME="dolphin"
+PKG_VERSION="a34037a"
+PKG_REV="1"
+PKG_ARCH="x86_64"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/libretro/dolphin"
+PKG_URL="https://github.com/libretro/dolphin/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="libretro"
+PKG_SHORTDESC="Dolphin is a GameCube / Wii emulator, allowing you to play games for these two platforms on PC with improvements."
+PKG_LONGDESC="Dolphin is a GameCube / Wii emulator, allowing you to play games for these two platforms on PC with improvements."
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+PKG_USE_CMAKE="no"
+
+make_target() {
+  cd $ROOT/$PKG_BUILD
+  make -C Source/Core/DolphinLibretro
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/libretro
+  cp Source/Core/DolphinLibretro/dolphin_libretro.so $INSTALL/usr/lib/libretro/
+}
