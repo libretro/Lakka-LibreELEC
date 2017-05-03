afe7e8a5b95f581442b2d715182fbea105148e21
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-05-03 10:13:21 -0500

    Update dolphin

--- packages/libretro/dolphin/package.mk
+++ packages/libretro/dolphin/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="dolphin"
-PKG_VERSION="a34037a"
+PKG_VERSION="169059e"
 PKG_REV="1"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPLv2"
