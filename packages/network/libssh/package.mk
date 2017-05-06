540925cbcf67df89aa831ac72013341bf47ba4df
MilhouseVH <milhouseVH.github@nmacleod.com>
2017-05-06 21:16:56 +0100

    libssh: update to libssh-0.7.5

--- packages/network/libssh/package.mk
+++ packages/network/libssh/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libssh"
-PKG_VERSION="0.7.3"
+PKG_VERSION="0.7.5"
 PKG_ARCH="any"
 PKG_LICENSE="OpenSource"
 PKG_SITE="http://www.libssh.org/"
