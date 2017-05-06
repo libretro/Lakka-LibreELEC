1c7b7ad56be9f0889a8535a85e0cf3a1a129cdcb
MilhouseVH <milhouseVH.github@nmacleod.com>
2017-05-06 21:16:56 +0100

    openvpn: update to openvpn-2.4.1

--- packages/network/openvpn/package.mk
+++ packages/network/openvpn/package.mk
@@ -18,7 +18,7 @@
 ################################################################################
 
 PKG_NAME="openvpn"
-PKG_VERSION="2.4.0"
+PKG_VERSION="2.4.1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://openvpn.net"
