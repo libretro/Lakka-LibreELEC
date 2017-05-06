548799dcb2c4ca270408be5d7fcf23a37253191e
MilhouseVH <milhouseVH.github@nmacleod.com>
2017-05-06 21:16:56 +0100

    xkbcomp: update to xkbcomp-1.4.0

--- packages/x11/app/xkbcomp/package.mk
+++ packages/x11/app/xkbcomp/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="xkbcomp"
-PKG_VERSION="1.3.1"
+PKG_VERSION="1.4.0"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.X.org"
