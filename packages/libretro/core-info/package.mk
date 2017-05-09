b82165b5d5dad256ae9dad32cd50d1a2b1185fee
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-05-09 10:30:32 -0500

    Update core infos

--- packages/libretro/core-info/package.mk
+++ packages/libretro/core-info/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="core-info"
-PKG_VERSION="098c835"
+PKG_VERSION="9d6c0a0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
