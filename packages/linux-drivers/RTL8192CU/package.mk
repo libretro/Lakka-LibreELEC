9f2aecc347970a530ff43786dd0473ac6632e97f
MilhouseVH <milhouseVH.github@nmacleod.com>
2017-05-12 21:53:50 +0100

    RTL8192CU: update to RTL8192CU-76b54cd

--- packages/linux-drivers/RTL8192CU/package.mk
+++ packages/linux-drivers/RTL8192CU/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="RTL8192CU"
-PKG_VERSION="372d4b0"
+PKG_VERSION="76b54cd"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/pvaret/rtl8192cu-fixes"
