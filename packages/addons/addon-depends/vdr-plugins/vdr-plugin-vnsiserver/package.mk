6d00a85d8094e876bb4fb2584f687c31e677f685
cvh <namerp@googlemail.com>
2017-05-14 01:03:47 +0200

    vdr-addon: update addon

--- packages/addons/addon-depends/vdr-plugins/vdr-plugin-vnsiserver/package.mk
+++ packages/addons/addon-depends/vdr-plugins/vdr-plugin-vnsiserver/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="vdr-plugin-vnsiserver"
-PKG_VERSION="d6847c3"
+PKG_VERSION="615a077"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/FernetMenta/vdr-plugin-vnsiserver"
