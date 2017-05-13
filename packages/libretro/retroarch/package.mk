aa5cefcb3139d7b61a50bed1e78166d1222dfb49
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-05-13 07:30:45 -0500

    Update RetroArch, display Lakka Version in System Information. Fixes #120

--- packages/libretro/retroarch/package.mk
+++ packages/libretro/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="e7061a8"
+PKG_VERSION="ce9439e"
 PKG_REV="4"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
