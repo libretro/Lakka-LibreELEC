2612b796d5ef4465c30a9a7050d72c12e9f84d76
Peter Vicman <peter.vicman@gmail.com>
2017-05-21 19:27:34 +0200

    imx6/options: unify linux version strings and simplify it's usage [backport of #1633] Variable LINUX_VERSION now took value '3.14-sr' to build imx6 project with 3.14 kernel from solidrun.

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -130,15 +130,6 @@ post_patch() {
     sed -i -e "s|^CONFIG_ISCSI_IBFT_FIND=.*$|# CONFIG_ISCSI_IBFT_FIND is not set|" $PKG_BUILD/.config
     sed -i -e "s|^CONFIG_ISCSI_IBFT=.*$|# CONFIG_ISCSI_IBFT is not set|" $PKG_BUILD/.config
   fi
-
-  # enable different libcec version for imx6 project with kernel 4.4
-  # using customized kernel driver
-  if [ "$PROJECT" = "imx6" ]; then
-    if [ "$LIBCEC_TYPE" = "xbian" -a "$LINUX" = "imx6-4.4-xbian" ]; then
-      sed -i -e "s|# CONFIG_MXC_HDMI_CEC is not set|CONFIG_MXC_HDMI_CEC=y|" $PKG_BUILD/.config
-      sed -i -e "s|CONFIG_MXC_HDMI_CEC_SR=y||" $PKG_BUILD/.config
-    fi
-  fi
 }
 
 makeinstall_host() {
