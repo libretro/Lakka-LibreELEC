ba18b00209f4021288a40478c1869184f7ce091b
chewitt <github@chrishewitt.net>
2017-05-24 05:33:05 +0100

    kodi: use correct 17.2 githash

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="4f53fb5"
+PKG_VERSION="a2a4632"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
