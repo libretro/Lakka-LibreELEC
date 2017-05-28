8714dd695f8de5ca8c89837de8bd7f0faac8e43a
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-05-28 16:58:01 -0500

    Fix imx6 build

--- packages/libretro/ppsspp/package.mk
+++ packages/libretro/ppsspp/package.mk
@@ -41,7 +41,7 @@ pre_configure_target() {
 
 make_target() {
   cd $ROOT/$PKG_BUILD/libretro
-  if [ "$OPENGLES" == "gpu-viv-bin-mx6q" ]; then
+  if [ "$OPENGLES" == "gpu-viv-bin-mx6q" -o "$OPENGLES" == "imx-gpu-viv" ]; then
     CFLAGS="$CFLAGS -DLINUX -DEGL_API_FB"
     CXXFLAGS="$CXXFLAGS -DLINUX -DEGL_API_FB"
   fi
