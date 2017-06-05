0b6b0e950a5c771d28f64c627bc3b340059961ae
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-06-05 17:41:10 -0500

    Package netcat

---
+++ packages/network/netcat/package.mk
@@ -0,0 +1,32 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+#
+#  OpenELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  OpenELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="netcat"
+PKG_VERSION="0.7.1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://netcat.sourceforge.net/"
+PKG_URL="http://downloads.sourceforge.net/sourceforge/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="network"
+PKG_SHORTDESC="GNU rewrite of netcat, the network piping application"
+PKG_LONGDESC="GNU rewrite of netcat, the network piping application"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
