1cc2669705b03fb2eb926acc7078d9c2ce1e746e
MilhouseVH <milhouseVH.github@nmacleod.com>
2017-06-07 21:21:12 +0100

    Revert "xf86-video-nvidia: disable nvidia-drm to build with 4.11.y"
    This reverts commit 7b950e0c4a64c109ec11ccc8c4c0339a65ab9721.
    
    Fixed by https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=d557d1b58b3546bab2c5bc2d624c5709840e6b10 in 4.11.1

--- packages/x11/driver/xf86-video-nvidia/package.mk
+++ packages/x11/driver/xf86-video-nvidia/package.mk
@@ -44,9 +44,6 @@ make_target() {
   unset LDFLAGS
 
   cd kernel
-    # Don't build nvidia-drm with kernel 4.11.0 due to incompatible license
-    export NV_EXCLUDE_KERNEL_MODULES=nvidia-drm
-
     make module CC=$CC SYSSRC=$(kernel_path) SYSOUT=$(kernel_path)
     $STRIP --strip-debug nvidia.ko
   cd ..
