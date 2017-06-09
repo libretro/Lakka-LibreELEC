16688751818f9afd75334de925497a97c0d472d7
awiouy <awiouy@gmail.com>
2017-06-09 21:28:12 +0200

    tigervnc: update to 1.8.0

--- packages/addons/service/tigervnc/package.mk
+++ packages/addons/service/tigervnc/package.mk
@@ -17,13 +17,13 @@
 ################################################################################
 
 PKG_NAME="tigervnc"
-PKG_VERSION="1.7.1"
-PKG_REV="102"
+PKG_VERSION="1.8.0"
+PKG_REV="103"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPLv2"
 PKG_SITE="http://www.tigervnc.org"
 PKG_URL="https://github.com/TigerVNC/tigervnc/archive/v$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain cmake:host libX11 libXext libXtst zlib libjpeg-turbo"
+PKG_DEPENDS_TARGET="toolchain cmake:host libX11 libXdamage libXext libXtst zlib libjpeg-turbo"
 PKG_SECTION="service"
 PKG_SHORTDESC="$PKG_ADDON_NAME server"
 PKG_LONGDESC="$PKG_ADDON_NAME ($PKG_VERSION) is a high-performance, platform-neutral implementation of Virtual Network Computing, a client/server application that allows users to launch and interact with graphical applications on remote machines"
@@ -32,7 +32,7 @@ PKG_IS_ADDON="yes"
 PKG_ADDON_NAME="TigerVNC"
 PKG_ADDON_TYPE="xbmc.service"
 
-PKG_CMAKE_OPTS_TARGET="-DBUILD_VIEWER=off"
+PKG_CMAKE_OPTS_TARGET="-DBUILD_VIEWER=off -Wno-dev"
 
 makeinstall_target() {
   : # nothing to do
