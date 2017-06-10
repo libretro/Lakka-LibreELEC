d587eded76c4d181b99eb0ff21f44438dac51b0d
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-06-10 16:24:48 -0500

    Update bsnes

--- packages/libretro/bsnes/package.mk
+++ packages/libretro/bsnes/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="bsnes"
-PKG_VERSION="fcc7244"
+PKG_VERSION="9535055"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
