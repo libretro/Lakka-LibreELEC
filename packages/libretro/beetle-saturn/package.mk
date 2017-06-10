da528b4d2ebaf703ca995417723d05e0d4756952
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-06-10 16:23:35 -0500

    Update beetle-saturn

--- packages/libretro/beetle-saturn/package.mk
+++ packages/libretro/beetle-saturn/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="beetle-saturn"
-PKG_VERSION="e299962"
+PKG_VERSION="ddced9b"
 PKG_REV="1"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPLv2"
