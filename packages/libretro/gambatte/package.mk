2f34df05bd5cecda6e2c1398ddbb5e71a0354580
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-06-10 16:26:35 -0500

    Update gambatte

--- packages/libretro/gambatte/package.mk
+++ packages/libretro/gambatte/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="gambatte"
-PKG_VERSION="fafc291"
+PKG_VERSION="675a838"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
