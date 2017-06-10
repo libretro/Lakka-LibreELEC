9c61fc905e0278db12a9e009a02bab0708d28ddf
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-06-10 16:31:45 -0500

    Update sameboy

--- packages/libretro/sameboy/package.mk
+++ packages/libretro/sameboy/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="sameboy"
-PKG_VERSION="81aec22"
+PKG_VERSION="6f47553"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
