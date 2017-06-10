b98b576430884d12da155cba123ba69a948a705d
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-06-10 16:26:20 -0500

    Update fceumm

--- packages/libretro/fceumm/package.mk
+++ packages/libretro/fceumm/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="fceumm"
-PKG_VERSION="fc569b7"
+PKG_VERSION="45f773a"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
