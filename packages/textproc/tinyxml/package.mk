777e8247461a435bf8a84e46e4b41e3af19bf6e1
Lukas Rusak <lorusak@gmail.com>
2017-06-14 22:36:01 -0700

    tinyxml: add -fPIC to CFLAGS

--- packages/textproc/tinyxml/package.mk
+++ packages/textproc/tinyxml/package.mk
@@ -30,7 +30,7 @@ PKG_LONGDESC="TinyXML is a simple, small, C++ XML parser that can be easily inte
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
-PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared"
+PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared --with-pic"
 
 pre_configure_target() {
   export CFLAGS="$CFLAGS -fPIC"
