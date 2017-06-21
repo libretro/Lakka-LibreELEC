d9cef7ab3ce36b99f4edb77d67f2a6a199a3a62c
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-06-21 11:45:39 -0500

    Update vecx

--- packages/libretro/vecx/package.mk
+++ packages/libretro/vecx/package.mk
@@ -19,12 +19,12 @@
 ################################################################################
 
 PKG_NAME="vecx"
-PKG_VERSION="0c75655"
+PKG_VERSION="30d39a0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2|LGPLv2.1"
 PKG_SITE="https://github.com/libretro/libretro-vecx"
-PKG_URL="$LAKKA_MIRROR/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_URL="https://github.com/libretro/libretro-vecx/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
 PKG_SECTION="libretro"
@@ -33,6 +33,10 @@ PKG_LONGDESC="libretro adaptation of vecx"
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+
+post_unpack() {
+  mv $BUILD/libretro-vecx-$PKG_VERSION* $BUILD/$PKG_NAME-$PKG_VERSION
+}
 
 make_target() {
   make -f Makefile.libretro
