91da2f852bf6fa6cdcdf602f1f2d68f89c75d2e3
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-06-21 11:36:55 -0500

    Update openlara

--- packages/libretro/openlara/package.mk
+++ packages/libretro/openlara/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="openlara"
-PKG_VERSION="aff0267"
+PKG_VERSION="fc17672"
 PKG_REV="1"
 PKG_ARCH="i386 x86_64"
 PKG_LICENSE="BSD"
