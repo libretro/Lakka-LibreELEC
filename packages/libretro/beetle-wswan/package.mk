3bc271a92e42eaaea5c258acd9cf0d8a243de07e
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-06-21 17:36:54 -0500

    Update beetle-wswan

--- packages/libretro/beetle-wswan/package.mk
+++ packages/libretro/beetle-wswan/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="beetle-wswan"
-PKG_VERSION="45c4614"
+PKG_VERSION="59f31b2"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
