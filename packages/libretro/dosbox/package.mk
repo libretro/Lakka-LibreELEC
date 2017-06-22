06c75d6f63c7e997ac87d9f536ad0658207bb46e
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-06-22 04:36:29 -0500

    Update dosbox

--- packages/libretro/dosbox/package.mk
+++ packages/libretro/dosbox/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="dosbox"
-PKG_VERSION="c18e2d9"
+PKG_VERSION="ce1bb80"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
