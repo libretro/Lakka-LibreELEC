3ef66522bf20c6f6d5f8c96fcd90487e5103c7cf
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-06-22 04:50:30 -0500

    Update scummvm

--- packages/libretro/scummvm/package.mk
+++ packages/libretro/scummvm/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="scummvm"
-PKG_VERSION="de8d7e5"
+PKG_VERSION="3254301"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
