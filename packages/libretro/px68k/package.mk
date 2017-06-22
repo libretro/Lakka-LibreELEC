4d4367557ef6a0d58511d7ab2fa2561766cf4602
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-06-22 04:50:43 -0500

    Update px68k

--- packages/libretro/px68k/package.mk
+++ packages/libretro/px68k/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="px68k"
-PKG_VERSION="4bfd564"
+PKG_VERSION="31c50c1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Unknown"
