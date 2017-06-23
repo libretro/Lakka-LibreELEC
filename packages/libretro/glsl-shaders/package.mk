7ee7010b86157b4fb01c70e9877e492e8b570ba6
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-06-23 15:19:02 -0500

    Update shaders

--- packages/libretro/glsl-shaders/package.mk
+++ packages/libretro/glsl-shaders/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="glsl-shaders"
-PKG_VERSION="302ac3d"
+PKG_VERSION="c610f1c"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
