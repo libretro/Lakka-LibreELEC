17a208a98f5486c0782b3cde2d5ce853ac91af4a
MilhouseVH <milhouseVH.github@nmacleod.com>
2017-06-30 13:36:15 +0100

    linux: update to linux-4.11.7

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -59,7 +59,7 @@ case "$LINUX" in
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET imx6-status-led imx6-soc-fan irqbalanced"
     ;;
   *)
-    PKG_VERSION="4.11.6"
+    PKG_VERSION="4.11.7"
     PKG_URL="http://www.kernel.org/pub/linux/kernel/v4.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;
