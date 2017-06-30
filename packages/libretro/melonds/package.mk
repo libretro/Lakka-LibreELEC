ee23797eb3e99e8ffd32ce1cf73ba2be1c9f9511
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-06-30 04:27:44 -0500

    Update melonds

--- packages/libretro/melonds/package.mk
+++ packages/libretro/melonds/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="melonds"
-PKG_VERSION="8460f2c"
+PKG_VERSION="72cd382"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
