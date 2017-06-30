cc7384806d686ddd1517138fca4015054718eb24
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-06-30 04:33:28 -0500

    Update gw

--- packages/libretro/gw-libretro/package.mk
+++ packages/libretro/gw-libretro/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="gw-libretro"
-PKG_VERSION="9b031bb"
+PKG_VERSION="7cafbf4"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
