3dd7c0f03acb41d35badf279da5bca0772d743ce
cvh <namerp@googlemail.com>
2017-07-06 22:59:07 +0200

    file: update to 5.31

--- packages/addons/addon-depends/system-tools-depends/file/package.mk
+++ packages/addons/addon-depends/system-tools-depends/file/package.mk
@@ -1,6 +1,6 @@
 ################################################################################
-#      This file is part of LibreELEC - http://www.libreelec.tv
-#      Copyright (C) 2016 Team LibreELEC
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016-present Team LibreELEC
 #
 #  LibreELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -17,17 +17,17 @@
 ################################################################################
 
 PKG_NAME="file"
-PKG_VERSION="5.29"
+PKG_VERSION="70c5f15"
 PKG_ARCH="any"
 PKG_LICENSE="BSD"
 PKG_SITE="http://www.darwinsys.com/file/"
-PKG_URL="ftp://ftp.astron.com/pub/file/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/file/file/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST="ccache:host"
 PKG_DEPENDS_TARGET="toolchain zlib file:host"
 PKG_SECTION="tools"
 PKG_SHORTDESC="file: File type identification utility"
 PKG_LONGDESC="These are the sources to Darwin's file(1) utility and master magic(4) file, now maintained by Christos Zoulas. The file(1) utility is used to determine the types of various files."
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
 PKG_CONFIGURE_OPTS_HOST="--enable-fsect-man5 --enable-static --disable-shared"
 PKG_CONFIGURE_OPTS_TARGET="--enable-fsect-man5 --enable-static --disable-shared"
