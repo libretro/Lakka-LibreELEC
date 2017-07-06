5e53dd9c38fc0dc530bacc0df2d5b1c487cf6768
cvh <namerp@googlemail.com>
2017-07-06 22:59:02 +0200

    difftools: update to 3.6

--- packages/addons/addon-depends/system-tools-depends/diffutils/package.mk
+++ packages/addons/addon-depends/system-tools-depends/diffutils/package.mk
@@ -1,6 +1,6 @@
 ################################################################################
-#      This file is part of LibreELEC - http://www.libreelec.tv
-#      Copyright (C) 2016 Team LibreELEC
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016-present Team LibreELEC
 #
 #  LibreELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="diffutils"
-PKG_VERSION="3.5"
+PKG_VERSION="3.6"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/software/diffutils/"
