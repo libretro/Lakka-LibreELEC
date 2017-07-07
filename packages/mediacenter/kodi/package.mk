05e30fd300b1fc10e01c775729ec0242191211f9
MilhouseVH <milhouseVH.github@nmacleod.com>
2017-07-07 17:37:22 +0100

    kodi: update to kodi-734cda1

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="423072a"
+PKG_VERSION="734cda1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
