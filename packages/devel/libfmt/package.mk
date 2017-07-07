52297bcd92522d0fdcbfa70bcac8a423c34c2ec2
MilhouseVH <milhouseVH.github@nmacleod.com>
2017-07-07 17:37:22 +0100

    libfmt: update to fmt-3.0.2

--- packages/devel/libfmt/package.mk
+++ packages/devel/libfmt/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libfmt"
-PKG_VERSION="3.0.1"
+PKG_VERSION="3.0.2"
 PKG_ARCH="any"
 PKG_LICENSE="BSD"
 PKG_SITE="https://github.com/fmtlib/fmt"
