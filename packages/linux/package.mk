4018e98ac359c2d00b84be21078047eb12235b05
MilhouseVH <milhouseVH.github@nmacleod.com>
2017-07-14 21:30:56 +0100

    linux: update to linux-4.12-rc1

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -59,8 +59,8 @@ case "$LINUX" in
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET imx6-status-led imx6-soc-fan irqbalanced"
     ;;
   *)
-    PKG_VERSION="4.11.10"
-    PKG_URL="http://www.kernel.org/pub/linux/kernel/v4.x/$PKG_NAME-$PKG_VERSION.tar.xz"
+    PKG_VERSION="4.12-rc1"
+    PKG_URL="https://git.kernel.org/torvalds/t/$PKG_NAME-$PKG_VERSION.tar.gz"
     PKG_PATCH_DIRS="default"
     ;;
 esac
