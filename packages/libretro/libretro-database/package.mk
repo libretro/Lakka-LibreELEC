f6c72c02c55f91a91000e06e39b315cfe784c74c
Rob Loach <robloach@gmail.com>
2017-07-20 13:06:04 -0400

    libretro-database: update to 4c3af88

--- packages/libretro/libretro-database/package.mk
+++ packages/libretro/libretro-database/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="libretro-database"
-PKG_VERSION="330693a"
+PKG_VERSION="4c3af88"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
