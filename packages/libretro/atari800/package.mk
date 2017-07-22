ff8534a38306d10232046778a6b873a1706dc8e9
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-07-22 03:42:51 -0500

    Update atari800

--- packages/libretro/atari800/package.mk
+++ packages/libretro/atari800/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="atari800"
-PKG_VERSION="b943747"
+PKG_VERSION="61f1908"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
