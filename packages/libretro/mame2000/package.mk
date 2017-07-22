3a4cdc4ed782803e86e46734c71de0c303439d25
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-07-22 03:36:40 -0500

    Update mame2000

--- packages/libretro/mame2000/package.mk
+++ packages/libretro/mame2000/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="mame2000"
-PKG_VERSION="594c480"
+PKG_VERSION="db8c92f"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MAME"
