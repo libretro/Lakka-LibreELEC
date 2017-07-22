59a44c41de510dc270116f84e73edc2171227303
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-07-21 19:42:14 -0500

    Update prboom

--- packages/libretro/prboom/package.mk
+++ packages/libretro/prboom/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="prboom"
-PKG_VERSION="4c690eb"
+PKG_VERSION="1b5c4e4"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
