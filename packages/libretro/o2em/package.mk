f99624ebbc5854c2919d306c27d77acd4149f487
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-07-21 19:43:49 -0500

    Update o2em

--- packages/libretro/o2em/package.mk
+++ packages/libretro/o2em/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="o2em"
-PKG_VERSION="1d62d4e"
+PKG_VERSION="86b050a"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Artistic License"
