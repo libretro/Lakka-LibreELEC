5e56bf62bcd00f6b8195ad802becc7a022349650
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-07-22 03:40:32 -0500

    Update craft

--- packages/libretro/craft/package.mk
+++ packages/libretro/craft/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="craft"
-PKG_VERSION="a1e173b"
+PKG_VERSION="a6176bd"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
