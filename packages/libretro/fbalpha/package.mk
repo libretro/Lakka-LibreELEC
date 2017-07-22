98debaf2dd929fb05f4ce63a59a57d724af6b76e
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-07-22 03:39:40 -0500

    Update fbaplha

--- packages/libretro/fbalpha/package.mk
+++ packages/libretro/fbalpha/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="fbalpha"
-PKG_VERSION="2acda85"
+PKG_VERSION="ed1e706"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Non-commercial"
