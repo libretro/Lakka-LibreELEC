1c60277e95c10210052173a6e879f9eb51a66cbf
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-07-21 19:26:39 -0500

    Update psp1

--- packages/libretro/psp1/package.mk
+++ packages/libretro/psp1/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="psp1"
-PKG_VERSION="5fafaba"
+PKG_VERSION="edf1cb7"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
