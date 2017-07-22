033042898e3229026a17b37a26171140289f6821
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-07-21 19:47:30 -0500

    Update mgba

--- packages/libretro/mgba/package.mk
+++ packages/libretro/mgba/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="mgba"
-PKG_VERSION="15fad78"
+PKG_VERSION="34474ff"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MPLv2.0"
