072425f8ecfab002a43ae1d13a69506231e49aed
MilhouseVH <MilhouseVH@users.noreply.github.com>
2017-08-02 06:04:19 +0100

    iwlwifi-firmware: update to iwlwifi-firmware-53913d7 (#1852)

--- packages/linux-firmware/iwlwifi-firmware/package.mk
+++ packages/linux-firmware/iwlwifi-firmware/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="iwlwifi-firmware"
-PKG_VERSION="6a717a9"
-PKG_SHA256="7a75da0b9421a2717ab9b243ace02aac566af37a837f47a9f8890cca0d159969"
+PKG_VERSION="53913d7"
+PKG_SHA256="0581b8aab276ef6db17dab74d5852357ba799a9269681142611385504239dcf4"
 PKG_ARCH="any"
 PKG_LICENSE="Free-to-use"
 PKG_SITE="https://github.com/LibreELEC/iwlwifi-firmware"
