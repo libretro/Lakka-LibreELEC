930813e7e5c7c640a1ca249a6cc25356ac1a42be
awiouy <awiouy@gmail.com>
2017-08-15 14:23:37 +0200

    mono: update to 5.2.0.215

--- packages/addons/tools/mono/package.mk
+++ packages/addons/tools/mono/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="mono"
-PKG_VERSION="5.0.1.1"
-PKG_REV="105"
+PKG_VERSION="5.2.0.215"
+PKG_REV="106"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
 PKG_SITE="http://www.mono-project.com"
