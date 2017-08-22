72609f4e8df1fea22f048987b3bf476316e3b562
chewitt <github@chrishewitt.net>
2017-08-22 09:36:14 +0100

    kodi: update to 17.4 final

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="3aa19b0"
+PKG_VERSION="7fc6da0"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
