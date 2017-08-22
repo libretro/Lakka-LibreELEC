2de087dab98938b5b10cd1d21608f476c6c3c88b
chewitt <github@chrishewitt.net>
2017-08-22 09:54:34 +0100

    kodi: updates to binary-addons

--- packages/mediacenter/kodi-binary-addons/pvr.iptvsimple/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.iptvsimple/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="pvr.iptvsimple"
-PKG_VERSION="d782816"
+PKG_VERSION="2a649d7"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
