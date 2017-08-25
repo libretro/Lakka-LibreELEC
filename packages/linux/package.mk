59ede795d3ad1ff02d3034311f7bad0e7b3261d0
MilhouseVH <milhouseVH.github@nmacleod.com>
2017-08-25 19:58:01 +0100

    linux: update to linux-4.12.5

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -63,8 +63,8 @@ case "$LINUX" in
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET imx6-status-led imx6-soc-fan irqbalanced"
     ;;
   *)
-    PKG_VERSION="4.12.4"
-    PKG_SHA256="f470d75f1f5a375f6882958daf96e7f8552fff28702b70abf3744e1711ef5ac9"
+    PKG_VERSION="4.12.5"
+    PKG_SHA256="715148e5f3640349a3dbb8d0b19527cf221b675f0014577b79ab8db0da8a63a0"
     PKG_URL="http://www.kernel.org/pub/linux/kernel/v4.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;
