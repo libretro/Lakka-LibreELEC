9667591332047b34b5722738c2d78f4e7bf7f939
awiouy <awiouy@gmail.com>
2017-08-29 21:24:39 +0200

    webgrabplus: update to 2.1.5_beta

--- packages/addons/service/webgrabplus/source/default.py
+++ packages/addons/service/webgrabplus/source/default.py
@@ -1,3 +1,21 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016-present Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
 import subprocess
 import xbmc
 import xbmcaddon
@@ -13,5 +31,5 @@ class Monitor(xbmc.Monitor):
       subprocess.call(['systemctl', 'restart', self.id])
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
    Monitor().waitForAbort()
