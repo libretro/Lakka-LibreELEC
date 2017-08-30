40366d3bc1cfa0d4ffb9b609bfc3ecdf04302e84
MilhouseVH <milhouseVH.github@nmacleod.com>
2017-08-30 11:05:02 +0100

    linux: update to linux-4.12.10

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -63,8 +63,8 @@ case "$LINUX" in
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET imx6-status-led imx6-soc-fan irqbalanced"
     ;;
   *)
-    PKG_VERSION="4.12.9"
-    PKG_SHA256="690439175c9dfbb90159bdc4b24cf10011675392264764f2031eb19ce0a1649c"
+    PKG_VERSION="4.12.10"
+    PKG_SHA256="c0fbf7b7fdd518a6e731fc7a6f0d9ade0840f553007dd414a223eb4a83a7a684"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v4.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;
