eb9eda6db128fb2721ee486210f3c0a23489275f
MilhouseVH <milhouseVH.github@nmacleod.com>
2017-09-06 17:09:57 +0100

    pvr.zattoo: initial package

---
+++ packages/mediacenter/kodi-binary-addons/pvr.zattoo/package.mk
@@ -0,0 +1,34 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2017-present Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="pvr.zattoo"
+PKG_VERSION="a24879b"
+PKG_SHA256="bbe5c2513a2ae67eff0da19810fb70e5bc880d0510ee8e25e4d8a44027aaccad"
+PKG_REV="2"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://www.kodi.tv"
+PKG_URL="https://github.com/rbuehlma/pvr.zattoo/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform"
+PKG_SECTION=""
+PKG_SHORTDESC="pvr.zattoo"
+PKG_LONGDESC="pvr.zattoo"
+PKG_AUTORECONF="no"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="xbmc.pvrclient"
