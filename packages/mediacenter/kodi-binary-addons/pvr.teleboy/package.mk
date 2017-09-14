6accd1441b48046c90025575044257543e0c70b1
MilhouseVH <milhouseVH.github@nmacleod.com>
2017-09-14 22:04:15 +0100

    pvr.teleboy: new pvr addon

---
+++ packages/mediacenter/kodi-binary-addons/pvr.teleboy/package.mk
@@ -0,0 +1,34 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2017-present Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="pvr.teleboy"
+PKG_VERSION="a0c218b"
+PKG_SHA256="eabe85ec76c140c9703598266c59d1b16197dc4e3461c7c7e4d13f61051a4439"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://www.kodi.tv"
+PKG_URL="https://github.com/rbuehlma/pvr.teleboy/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform"
+PKG_SECTION=""
+PKG_SHORTDESC="pvr.teleboy"
+PKG_LONGDESC="pvr.teleboy"
+PKG_AUTORECONF="no"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="xbmc.pvrclient"
