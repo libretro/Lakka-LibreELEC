50c0095032e7539b077d4126030393ed5b3b3a93
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-09-16 08:15:23 -0500

    Attempt to fix citra

--- packages/libretro/citra/package.mk
+++ packages/libretro/citra/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="citra"
-PKG_VERSION="a0a4d69"
+PKG_VERSION="c892166"
 PKG_REV="1"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPLv2+"
