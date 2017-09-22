03359258bff62a021acd9acbe2870714d2d44a5f
Ntemis <ierokipides.dem@gmail.com>
2017-09-22 21:29:25 +0300

    S8X2: Fix initramfs package for LE-8.2

--- projects/S8X2/packages/initramfs/package.mk
+++ projects/S8X2/packages/initramfs/package.mk
@@ -42,17 +42,27 @@ if [ "$INITRAMFS_PARTED_SUPPORT" = yes ]; then
 fi
 
 post_install() {
-  ( cd $ROOT/$BUILD/initramfs
+  ( cd $BUILD/initramfs
+    if [ "$TARGET_ARCH" = "x86_64" -o "$TARGET_ARCH" = "powerpc64" ]; then
+      ln -sf /usr/lib $BUILD/initramfs/lib64
+      mkdir -p $BUILD/initramfs/usr
+      ln -sf /usr/lib $BUILD/initramfs/usr/lib64
+    fi
+
     for MOD in `find ./usr/lib/modules/ -type f -name *.ko`; do
       $STRIP --strip-debug $MOD
     done
 
-    ln -sf /usr/lib $ROOT/$BUILD/initramfs/lib
-    ln -sf /usr/bin $ROOT/$BUILD/initramfs/bin
-    ln -sf /usr/sbin $ROOT/$BUILD/initramfs/sbin
+    ln -sf /usr/lib $BUILD/initramfs/lib
+    ln -sf /usr/bin $BUILD/initramfs/bin
+    ln -sf /usr/sbin $BUILD/initramfs/sbin
 
-    mkdir -p $ROOT/$BUILD/image/
+    rm $BUILD/initramfs/splash/splash-768.png
+    rm $BUILD/initramfs/splash/splash-1200.png
+    rm $BUILD/initramfs/splash/splash-2160.png
+
+    mkdir -p $BUILD/image/
     fakeroot -- sh -c \
-      "mkdir -p dev; mknod -m 600 dev/console c 5 1; find . | cpio -H newc -ov -R 0:0 | gzip -n -9 -f > $ROOT/$BUILD/image/initramfs.cpio"
+      "mkdir -p dev; mknod -m 600 dev/console c 5 1; find . | cpio -H newc -ov -R 0:0 | gzip -n -9 -f > $BUILD/image/initramfs.cpio"
   )
 }
