ab113f426a9835ffcba412277e1c0089d6b92311
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-09-26 12:59:12 -0500

    u-boot fix

--- packages/tools/u-boot/package.mk
+++ packages/tools/u-boot/package.mk
@@ -91,7 +91,7 @@ make_target() {
       CROSS_COMPILE=aarch64-elf- ARCH=arm CFLAGS="" LDFLAGS="" make $UBOOT_TARGET
       CROSS_COMPILE=aarch64-elf- ARCH=arm CFLAGS="" LDFLAGS="" make HOSTCC="$HOST_CC" HOSTSTRIP="true"
     elif [ "$PROJECT" = "OdroidC1" -o "$PROJECT" = "a20" -o "$PROJECT" = "a10" -o "$PROJECT" = "Bananapi" ]; then
-      export PATH=$ROOT/$TOOLCHAIN/lib/gcc-linaro-arm-eabi/bin/:$PATH
+      export PATH=$TOOLCHAIN/lib/gcc-linaro-arm-eabi/bin/:$PATH
       make CROSS_COMPILE="arm-eabi-" ARCH=arm mrproper
       make CROSS_COMPILE="arm-eabi-" ARCH=arm $UBOOT_TARGET
       make CROSS_COMPILE="arm-eabi-" ARCH=arm HOSTCC="$HOST_CC" HOSTSTRIP="true"
