a35d6c5c24e32a8dd9a9f0b5f6c99fc9ebe41b3c
Ntemis <ierokipides.dem@gmail.com>
2017-10-11 23:01:01 +0300

    Allwinner: Add gpu driver and mali support for mainline kernel

---
+++ packages/graphics/allwinner-mali/package.mk
@@ -0,0 +1,65 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="allwinner-mali"
+PKG_VERSION="3d7f4d4"
+PKG_SHA256="ef5f0f2c0545d1a20d283b87aa447f452e353150cdffadc7f405559e42626cb8"
+PKG_ARCH="arm"
+PKG_LICENSE="nonfree"
+PKG_SITE="https://github.com/linux-sunxi/sunxi-mali"
+PKG_URL="https://github.com/mosajjal/r6p2/archive/$PKG_VERSION.tar.gz"
+PKG_SOURCE_DIR="r6p2-$PKG_VERSION*"
+PKG_DEPENDS_TARGET="toolchain libdrm wayland"
+PKG_SECTION="graphics"
+PKG_SHORTDESC="Allwinner Mali-400/450 support libraries"
+PKG_LONGDESC="Allwinner Mali-400/450 support libraries"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+make_target() {
+ : # nothing todo
+}
+
+makeinstall_target() {
+  mkdir -p $SYSROOT_PREFIX/usr/include/
+    cp -av fbdev/include/* $SYSROOT_PREFIX/usr/include
+
+  mkdir -p $SYSROOT_PREFIX/usr/lib/pkgconfig
+    cp -PRv $PKG_DIR/pkgconfig/*.pc $SYSROOT_PREFIX/usr/lib/pkgconfig
+
+  MALI="libwayland_for_mali/h3/lib_wayland/libMali.so"
+
+  mkdir -p $SYSROOT_PREFIX/usr/lib/
+    cp -v $MALI $SYSROOT_PREFIX/usr/lib
+
+  mkdir -p $INSTALL/usr/lib
+    cp -v $MALI $INSTALL/usr/lib
+
+    for lib in libEGL.so \
+               libEGL.so.1 \
+               libEGL.so.1.4 \
+               libGLESv2.so \
+               libGLESv2.so.2 \
+               libGLESv2.so.2.0 \
+               libgbm.so \
+               libgbm.so.1; do
+      ln -sfv libMali.so $INSTALL/usr/lib/${lib}
+      ln -sfv libMali.so $SYSROOT_PREFIX/usr/lib/${lib}
+    done
+}
---
+++ packages/linux-drivers/allwinner-gpu/package.mk
@@ -0,0 +1,59 @@
+################################################################################
+#      This file is part of LibreELEC - https://LibreELEC.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="allwinner-gpu"
+PKG_VERSION="r6p2-01rel0"
+PKG_SHA256="bb49d23ab3d9fbeb701a127e6f28cff1c963bba05786f98d76edff1df0fe6c52"
+PKG_ARCH="arm aarch64"
+PKG_LICENSE="GPL"
+PKG_SITE="https://developer.arm.com/products/software/mali-drivers/utgard-kernel"
+PKG_URL="https://developer.arm.com/-/media/Files/downloads/mali-drivers/kernel/mali-utgard-gpu/DX910-SW-99002-$PKG_VERSION.tgz"
+PKG_SOURCE_DIR="DX910-SW-99002-$PKG_VERSION"
+PKG_DEPENDS_TARGET="toolchain linux"
+PKG_NEED_UNPACK="$LINUX_DEPENDS"
+PKG_SECTION="driver"
+PKG_SHORTDESC="allwinner-gpu: Linux drivers for Mali GPUs found in Allwinner SoCs"
+PKG_LONGDESC="allwinner-gpu: Linux drivers for Mali GPUs found in Allwinner SoCs"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+DRIVER_DIR=$PKG_BUILD/src/devicedrv/mali/
+
+pre_patch() {
+  # move source dir to allow patching
+  mv $PKG_BUILD/driver/src $PKG_BUILD/
+  rm -rf $PKG_BUILD/driver
+}
+
+make_target() {
+  USING_UMP=0 \
+  BUILD=$BUILD \
+  USING_PROFILING=0 \
+  MALI_PLATFORM=sunxi \
+  USING_DVFS=1 \
+  USING_DEVFREQ=0 \
+  KDIR=$(kernel_path) \
+  CROSS_COMPILE=$TARGET_PREFIX \
+    make -C $DRIVER_DIR
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/modules/$(get_module_dir)/$PKG_NAME
+    cp $DRIVER_DIR/*.ko $INSTALL/usr/lib/modules/$(get_module_dir)/$PKG_NAME
+}
