09287310871a71a158cfab78d742ea986dee773f
Ntemis <ierokipides.dem@gmail.com>
2017-10-15 21:42:16 +0300

    Allwinner: Add serial console service config Update dtc: - install libfdt for host - Fix cardcoded python version

--- packages/tools/dtc/package.mk
+++ packages/tools/dtc/package.mk
@@ -17,26 +17,38 @@
 ################################################################################
 
 PKG_NAME="dtc"
-PKG_VERSION="1.4.4"
+PKG_VERSION="1.4.5"
+PKG_SHA256="cfb9394690ebec1e4f942ee0c3b863b660eb0c4ef85bab19429f30c3469a3415"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://git.kernel.org/pub/scm/utils/dtc/dtc.git/"
 PKG_URL="https://git.kernel.org/pub/scm/utils/dtc/dtc.git/snapshot/$PKG_VERSION.tar.gz"
 PKG_SOURCE_DIR="$PKG_VERSION"
+PKG_DEPENDS_HOST="Python2:host swig:host"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="tools"
 PKG_SHORTDESC="The Device Tree Compiler"
 PKG_LONGDESC="The Device Tree Compiler"
 PKG_AUTORECONF="no"
 
-PKG_MAKE_OPTS_TARGET="dtc"
+PKG_MAKE_OPTS_TARGET="dtc libfdt"
 
 makeinstall_target() {
   mkdir -p $INSTALL/usr/bin
     cp -P $PKG_BUILD/dtc $INSTALL/usr/bin
 }
 
+PKG_MAKE_OPTS_HOST="dtc libfdt"
+
 makeinstall_host() {
   mkdir -p $TOOLCHAIN/bin
     cp -P $PKG_BUILD/dtc $TOOLCHAIN/bin
+    cp -P $PKG_BUILD/libfdt/libfdt.so $TOOLCHAIN/lib
+}
+
+post_makeinstall_host() {
+  python ./pylibfdt/setup.py build_ext --inplace
+  python ./pylibfdt/setup.py install --prefix=$TOOLCHAIN
+
+  touch $TOOLCHAIN/lib/$PKG_PYTHON_VERSION/site-packages/pylibfdt/__init__.py
 }
