aad85f5764eb79eb2f9b0d1238bb082de4ef97db
Ntemis <ierokipides.dem@gmail.com>
2017-10-18 20:26:12 +0300

    Allwinner: Update kernel to 4.13.8

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -97,7 +97,7 @@ case "$LINUX" in
     PKG_PATCH_DIRS="rockchip-4.4"
     ;;
   allwinner)
-    PKG_VERSION="4.13.7"
+    PKG_VERSION="4.13.8"
     PKG_URL="http://www.kernel.org/pub/linux/kernel/v4.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     ;;
   *)
