0837f0880d711a9a2db2f142c92538920360a237
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-10-18 03:10:12 -0500

    Fix mrboom build

--- packages/libretro/mrboom/package.mk
+++ packages/libretro/mrboom/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="mrboom"
-PKG_VERSION="9359a0f"
+PKG_VERSION="ff215d6"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
