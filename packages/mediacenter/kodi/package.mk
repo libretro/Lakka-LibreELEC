0d59f48407121a431b0dd297527002dd450aa560
MilhouseVH <milhouseVH.github@nmacleod.com>
2017-10-23 10:07:42 +0100

    kodi: update to kodi-5bd45ab

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="7fc6da0"
+PKG_VERSION="5bd45ab"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
