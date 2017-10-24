9e6020c9af736e583535e49b21b283d96abb272f
MilhouseVH <milhouseVH.github@nmacleod.com>
2017-10-24 02:51:11 +0100

    libva-utils: update to libva-utils-2.0.0

--- packages/debug/libva-utils/package.mk
+++ packages/debug/libva-utils/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="libva-utils"
-PKG_VERSION="1.8.3"
-PKG_SHA256="c59de4fb6f1021c435b3f49e2410760692324ee5bb464c716d674fcb626a7e03"
+PKG_VERSION="2.0.0"
+PKG_SHA256="a921df31311d8f49d2e392a5fc2a068d79f89aeb588309fbff24365310dbc5f6"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/01org/libva-utils"
