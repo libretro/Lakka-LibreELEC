3b495a1c8c79c7bb309f628bbce6349bd2c86b2e
cvh <namerp@googlemail.com>
2017-10-25 11:23:45 +0200

    x265: initial package

---
+++ packages/addons/addon-depends/ffmpegx-depends/x265/package.mk
@@ -0,0 +1,34 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2017-present Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="x265"
+PKG_VERSION="2.5"
+PKG_SHA256="a5607edead00e9ba90ca222c4a6e93c1f4db76e9f04a4905a96f21fbdb7d626d"
+PKG_ARCH="x86_64"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/videolan/x265"
+PKG_URL="https://github.com/videolan/x265/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="multimedia"
+PKG_LONGDESC="x265 is a H.265/HEVC video encoder application library"
+PKG_AUTORECONF="no"
+
+pre_configure_target() {
+  LDFLAGS="$LDFLAGS -ldl"
+  cmake -G "Unix Makefiles" ./source
+}
