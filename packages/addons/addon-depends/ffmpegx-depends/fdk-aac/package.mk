e4cabb0244d303dfb3d4a05e6080ddc192999b48
cvh <namerp@googlemail.com>
2017-10-25 11:23:49 +0200

    fdk-aac: initial package

---
+++ packages/addons/addon-depends/ffmpegx-depends/fdk-aac/package.mk
@@ -0,0 +1,29 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2017-present Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="fdk-aac"
+PKG_VERSION="0.1.5"
+PKG_SHA256="ff53d1d01cacc29c071e23192dfefa93bdbeaf775fc5d296259b4859d0306b79"
+PKG_ARCH="any"
+PKG_LICENSE="other"
+PKG_SITE="https://sourceforge.net/projects/opencore-amr/"
+PKG_URL="https://github.com/mstorsjo/fdk-aac/archive/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="multimedia"
+PKG_LONGDESC="A standalone library of the Fraunhofer FDK AAC code from Android."
+PKG_AUTORECONF="yes"
