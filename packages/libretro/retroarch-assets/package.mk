17779ce0e8873609d5a0a84ca355c5d791054bba
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-10-27 03:34:16 -0500

    (RetroArch) Add Gamecube scanning

--- packages/libretro/retroarch-assets/package.mk
+++ packages/libretro/retroarch-assets/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch-assets"
-PKG_VERSION="3acffa4"
+PKG_VERSION="b81ef53"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
