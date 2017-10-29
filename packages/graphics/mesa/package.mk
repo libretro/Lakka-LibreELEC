fb391f927d2805716492e3fe2277336c5e6bb98b
Emil Velikov <emil.l.velikov@gmail.com>
2017-10-29 12:28:53 +0000

    mesa: drop EGL drom MESA_EGL_PLATFORMS
    The toggle is used for more than EGL - EGL, Vulkan as well a the video
    codec drivers.

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -36,12 +36,12 @@ if [ "$DISPLAYSERVER" = "x11" ]; then
   export X11_INCLUDES=
   MESA_DRI="--enable-dri --enable-dri3"
   MESA_GLX="--enable-glx --enable-driglx-direct --enable-glx-tls"
-  MESA_EGL_PLATFORMS="--with-platforms=x11,drm"
+  MESA_PLATFORMS="--with-platforms=x11,drm"
 else
   MESA_DRI="--enable-dri --disable-dri3"
   # The glx in glx-tls is a misnomer - there's nothing glx in it.
   MESA_GLX="--disable-glx --disable-driglx-direct --enable-glx-tls"
-  MESA_EGL_PLATFORMS="--with-platforms=drm"
+  MESA_PLATFORMS="--with-platforms=drm"
 fi
 
 # configure GPU drivers and dependencies:
@@ -83,6 +83,7 @@ PKG_CONFIGURE_OPTS_TARGET="CC_FOR_BUILD=$HOST_CC \
                            --enable-texture-float \
                            --enable-asm \
                            --disable-selinux \
+                           $MESA_PLATFORMS \
                            --enable-opengl \
                            $MESA_GLES \
                            $MESA_DRI \
@@ -90,7 +91,6 @@ PKG_CONFIGURE_OPTS_TARGET="CC_FOR_BUILD=$HOST_CC \
                            --disable-osmesa \
                            --disable-gallium-osmesa \
                            --enable-egl \
-                           $MESA_EGL_PLATFORMS \
                            $XA_CONFIG \
                            --enable-gbm \
                            --disable-nine \
