532ee416f7636f2c10a366e857809b41d7bf19a4
Jonas Karlman <jonas@kwiboo.se>
2017-11-01 22:57:41 +0100

    rkbin: new package

---
+++ packages/tools/rkbin/package.mk
@@ -0,0 +1,30 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2017-present Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="rkbin"
+PKG_VERSION="878533b"
+PKG_SHA256="75348fec13992e0e07c9efa3453d08523db078ccbe5bbc700aca7dd3f368f97a"
+PKG_ARCH="arm aarch64"
+PKG_LICENSE="nonfree"
+PKG_SITE="https://github.com/rockchip-linux/rkbin"
+PKG_URL="https://github.com/rockchip-linux/rkbin/archive/$PKG_VERSION.tar.gz"
+PKG_SOURCE_DIR="rkbin-$PKG_VERSION*"
+PKG_SECTION="tools"
+PKG_SHORTDESC="rkbin: Rockchip Firmware and Tool Binaries"
+PKG_LONGDESC="rkbin: Rockchip Firmware and Tool Binaries"
+PKG_TOOLCHAIN="manual"
