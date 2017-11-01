877fceee50b9ddb00b047bb18d848953b483a9c2
Jonas Karlman <jonas@kwiboo.se>
2017-11-01 22:57:41 +0100

    Rockchip: new project

---
+++ projects/Rockchip/README.md
@@ -0,0 +1,16 @@
+# Rockchip
+
+This project is for Rockchip SoC devices
+
+## Devices
+
+**My single-board computer is not listed, will it be added in the future?**<br />
+If your single-board computer uses a current generation SoC listed on http://opensource.rock-chips.com/wiki_Main_Page the odds are in your favor.
+
+**My Android device is not listed, will it be added in the future?**<br />
+You may have luck if your device vendor is open source friendly, otherwise keep using Android for best support.
+
+## Links
+
+* https://github.com/rockchip-linux
+* http://opensource.rock-chips.com
---
+++ projects/Rockchip/bootloader/canupdate.sh
@@ -0,0 +1,24 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2017-present Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+# Allow upgrades between arm and aarch64
+if [ "$1" = "@PROJECT@.arm" -o "$1" = "@PROJECT@.aarch64" ]; then
+  exit 0
+else
+  exit 1
+fi
