c99bd9735c9b7d516c5e9647cd894bd663fe05b2
MilhouseVH <milhouseVH.github@nmacleod.com>
2017-11-03 16:32:57 +0000

    linux: update to linux-4.13.7

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -66,8 +66,8 @@ case "$LINUX" in
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET imx6-status-led imx6-soc-fan irqbalanced"
     ;;
   *)
-    PKG_VERSION="4.13.6"
-    PKG_SHA256="47d419ceae1d353dad565b858301ac9670c747deb37936c8a935915b3bdd6050"
+    PKG_VERSION="4.13.7"
+    PKG_SHA256="6393be5f698c24c02829aa9b08f4626ce4bbd082dcdbd54d6448018e0795729b"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v4.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;
