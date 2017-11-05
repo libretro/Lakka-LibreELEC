1805f0cfb0e04bb0d4c3b4254f5f8e92bdfac846
Ntemis <ierokipides.dem@gmail.com>
2017-11-06 00:03:26 +0200

    Update snes9x

--- packages/libretro/snes9x/package.mk
+++ packages/libretro/snes9x/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="snes9x"
-PKG_VERSION="ec2d6b9"
+PKG_VERSION="4d7ab13"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Non-commercial"
