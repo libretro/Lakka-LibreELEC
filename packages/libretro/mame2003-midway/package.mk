1cc39229de95c03e0c9cc5875da7b04f0f53c17b
Ntemis <ierokipides.dem@gmail.com>
2017-11-05 23:44:25 +0200

    New Libretro core: Mame-2003-Midway

---
+++ packages/libretro/mame2003-midway/package.mk
@@ -0,0 +1,49 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.tv; see the file COPYING.  If not, write to
+#  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
+#  http://www.gnu.org/copyleft/gpl.html
+################################################################################
+
+PKG_NAME="mame2003-midway"
+PKG_VERSION="06f76b1"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="MAME"
+PKG_SITE="https://github.com/libretro/mame2003_midway"
+PKG_URL="https://github.com/libretro/mame2003_midway/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="libretro"
+PKG_SHORTDESC="MAME - Multiple Arcade Machine Emulator"
+PKG_LONGDESC="MAME - Multiple Arcade Machine Emulator"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+post_unpack() {
+  mv $BUILD/mame2003_midway-$PKG_VERSION* $BUILD/$PKG_NAME-$PKG_VERSION
+}
+
+make_target() {
+  strip_lto
+  make ARCH="" CC="$CC" NATIVE_CC="$CC" LD="$CC" -j 1
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/libretro
+  cp mame2003_midway_libretro.so $INSTALL/usr/lib/libretro/
+}
