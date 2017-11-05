055f8f5f886409f3bc88f6315b43aecaa0e26dcb
Ntemis <ierokipides.dem@gmail.com>
2017-11-05 23:49:56 +0200

    Update Mrboom to v3.8 stable

--- packages/libretro/mrboom/package.mk
+++ packages/libretro/mrboom/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="mrboom"
-PKG_VERSION="ff215d6"
+PKG_VERSION="44daf18"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
