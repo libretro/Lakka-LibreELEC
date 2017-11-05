966adf2a3e42666d81d08faaeb22db5fdb211481
Ntemis <ierokipides.dem@gmail.com>
2017-11-06 00:02:16 +0200

    Update mgba

--- packages/libretro/mgba/package.mk
+++ packages/libretro/mgba/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="mgba"
-PKG_VERSION="abf5a18"
+PKG_VERSION="68dca5b"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MPLv2.0"
