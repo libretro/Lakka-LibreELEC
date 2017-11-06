08bcc9f88b0e766999320e3a5a8fd854934aef2a
Ntemis <ierokipides.dem@gmail.com>
2017-11-06 16:24:59 +0200

    Update reicast

--- packages/libretro/reicast/package.mk
+++ packages/libretro/reicast/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="reicast"
-PKG_VERSION="1f71ce7"
+PKG_VERSION="74c4ddf"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
