5e3672e29a7746f4914ee8b42c985177dd71c942
Ntemis <ierokipides.dem@gmail.com>
2017-11-06 16:10:21 +0200

    Update genesis-plus-gx

--- packages/libretro/genesis-plus-gx/package.mk
+++ packages/libretro/genesis-plus-gx/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="genesis-plus-gx"
-PKG_VERSION="2c8af63"
+PKG_VERSION="7104058"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Non-commercial"
