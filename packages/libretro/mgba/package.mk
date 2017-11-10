77eb774a8e3a4c655af8472856dc146da0f194db
ntemis <ierokipides.dem@gmail.com>
2017-11-11 00:56:18 +0200

    mgba: update to 0.6.2 stable Bugfixes: - GBA Video: Force align 256-color tiles - GBA DMA: ROM reads are forced to increment - GB Video: Only trigger STAT write IRQs when screen is on (fixes mgba.io/i/912)

--- packages/libretro/mgba/package.mk
+++ packages/libretro/mgba/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="mgba"
-PKG_VERSION="68dca5b"
+PKG_VERSION="49c9254"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MPLv2.0"
