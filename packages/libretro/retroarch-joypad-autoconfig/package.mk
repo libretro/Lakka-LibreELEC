06e1cab2fb2f44fa31a6dc78a1290948f5f3547f
Ntemis <ierokipides.dem@gmail.com>
2017-11-12 23:54:48 +0200

    Update retroarch joypad autoconfig package Adds support for Radica Gamester Arcade Stick

--- packages/libretro/retroarch-joypad-autoconfig/package.mk
+++ packages/libretro/retroarch-joypad-autoconfig/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch-joypad-autoconfig"
-PKG_VERSION="75d560e"
+PKG_VERSION="5f74331"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
