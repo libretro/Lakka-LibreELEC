2572da02b0968d230c021ae31f4eb809384e7848
Ntemis <ierokipides.dem@gmail.com>
2017-11-13 00:53:01 +0200

    Update retroarch joypad autoconfig package second update to fix Gamester_Reflex_Arcade_Stick. By adding digital directional config fixes navigational issues in retroarch menu

--- packages/libretro/retroarch-joypad-autoconfig/package.mk
+++ packages/libretro/retroarch-joypad-autoconfig/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch-joypad-autoconfig"
-PKG_VERSION="5f74331"
+PKG_VERSION="9dda99d"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
