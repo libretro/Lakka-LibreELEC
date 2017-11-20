996285f8c1f7b17f47fa8c8f300b9c5b8cd044af
Ntemis <ierokipides.dem@gmail.com>
2017-11-20 19:30:48 +0200

    Beetle-psx: Fix my derp

--- packages/libretro/beetle-psx/package.mk
+++ packages/libretro/beetle-psx/package.mk
@@ -39,7 +39,7 @@ post_unpack() {
 }
 
 make_target() {
-  if [ "$OPENGLES" == "mali-rockchip" -o [ "$DEVICE" = "TinkerBoard" ] || [ "$DEVICE" = "MiQi" ]; then
+  if [ "$OPENGLES" == "mali-rockchip" -o  "$DEVICE" == "TinkerBoard" -o "$DEVICE" == "MiQi" ]; then
   make HAVE_VULKAN=1
   else
   make HAVE_OPENGL=1
