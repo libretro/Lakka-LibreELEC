1f6b4166ccdb4d5267bdd88e352e9679096933c8
Ntemis <ierokipides.dem@gmail.com>
2017-11-21 01:05:33 +0200

    Last minute fixes to mame2003 and retroarch

--- packages/libretro/mame2003/package.mk
+++ packages/libretro/mame2003/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="mame2003"
-PKG_VERSION="276f613"
+PKG_VERSION="c00120a"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MAME"
--- packages/libretro/retroarch/package.mk
+++ packages/libretro/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="7ff25fe"
+PKG_VERSION="f27574b"
 PKG_REV="9"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
