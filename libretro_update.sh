083d3c4d189c725ad80d9fc1ccff6e4b342082a9
ToKe79 <tomas.kelemen@gmail.com>
2017-12-08 22:21:34 +0100

    Merge with original update script

--- libretro_update.sh
+++ libretro_update.sh
@@ -1,7 +1,59 @@
 #!/bin/bash
-FILES=packages/libretro/*/package.mk
-for f in $FILES
+LR_PKG_PATH="packages/libretro"
+usage()
+{
+  echo ""
+  echo "$0 <--all|--used>"
+  echo ""
+  echo "Updates PKG_VERSION in package.mk of libretro packages to latest."
+  echo ""
+  echo "Parameters:"
+  echo " -a --all   Update all libretro packages"
+  echo " -u --used  Update only libretro packages used by Lakka"
+  echo ""
+}
+
+[ $# -gt 1 ] && { usage ; echo "Error: too many parameters given!" ; exit 1 ; }
+
+[ "$1" = "" ] && { usage ; exit ; }
+
+case $1 in
+  -a | --all )
+    # Get list of all libretro packages
+    echo "Checking all libretro packages:"
+    PACKAGES_ALL=`ls $LR_PKG_PATH`
+    ;;
+  -u | --used )
+    echo "Checking only libretro packages used by Lakka:"
+    # Get list of cores, which are used with Lakka:
+    OPTIONS_FILE="distributions/Lakka/options"
+    [ -f "$OPTIONS_FILE" ] && source "$OPTIONS_FILE" || { echo "$OPTIONS_FILE: not found! Aborting." ; exit 1 ; }
+    [ -z "$LIBRETRO_CORES" ] && { echo "LIBRETRO_CORES: empty. Aborting!" ; exit 1 ; }
+    # List of core retroarch packages
+    RA_PACKAGES="retroarch retroarch-assets retroarch-joypad-autoconfig retroarch-overlays libretro-database core-info glsl-shaders"
+    # List of cores used by specific projects not included in LIBRETRO_CORES.
+    # This needs to be updated manually in case packages/lakka/*/package.mk
+    # PKG_DEPENDS_TARGET includes a libretro core/package not included above.
+    # PKG_DEPENDS_TARGET often includes also non-libretro packages, hence
+    # not pulled from individual project-package files.
+    ADDITIONAL_PACKAGES="beetle-bsnes bsnes beetle-psx bsnes-mercury"
+    # List of all libretro packages to update:
+    PACKAGES_ALL="$RA_PACKAGES $ADDITIONAL_PACKAGES $LIBRETRO_CORES"
+    ;;
+  * )
+    usage
+    echo "Unknown parameter: $1"
+    exit 1
+    ;;
+esac
+declare -i i=0
+for p in $PACKAGES_ALL
 do
+  f=$LR_PKG_PATH/$p/package.mk
+  if [ ! -f "$f" ] ; then
+    echo "$f: not found! Skipping."
+    continue
+  fi
   PKG_VERSION=`cat $f | sed -En "s/PKG_VERSION=\"(.*)\"/\1/p"`
   PKG_SITE=`cat $f | sed -En "s/PKG_SITE=\"(.*)\"/\1/p"`
   PKG_NAME=`cat $f | sed -En "s/PKG_NAME=\"(.*)\"/\1/p"`
@@ -14,9 +66,11 @@ do
   fi
   UPS_VERSION=`git ls-remote $PKG_SITE | grep HEAD | awk '{ print substr($1,1,7) }'`
   if [ "$UPS_VERSION" == "$PKG_VERSION" ]; then
-    echo "$PKG_NAME is up to date (local: $PKG_VERSION / remote: $UPS_VERSION)"
+    echo "$PKG_NAME is up to date ($UPS_VERSION)"
   else
-  echo "$PKG_NAME is outdated (local: $PKG_VERSION / remote: $UPS_VERSION)"
+    i+=1
+    echo "$PKG_NAME updated from $PKG_VERSION to $UPS_VERSION"
     sed -i "s/$PKG_VERSION/$UPS_VERSION/" $f
   fi
 done
+echo "$i package(s) updated."
