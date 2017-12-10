297633968d515a15e3686d5bcd4558e72b5b2b75
Ntemis <ierokipides.dem@gmail.com>
2017-12-11 01:33:13 +0200

    Libretro new core: GearBoy - Game Boy / Gameboy Color emulator

---
+++ packages/libretro/gearboy/package.mk
@@ -0,0 +1,28 @@
+PKG_NAME="gearboy"
+PKG_VERSION="aaabaca"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/libretro/Gearboy"
+PKG_URL="https://github.com/libretro/Gearboy/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="libretro"
+PKG_SHORTDESC="Game Boy / Gameboy Color emulator for iOS, Mac, Raspberry Pi, Windows and Linux"
+PKG_LONGDESC="Game Boy / Gameboy Color emulator for iOS, Mac, Raspberry Pi, Windows and Linux"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+post_unpack() {
+  mv $BUILD/Gearboy-$PKG_VERSION* $BUILD/$PKG_NAME-$PKG_VERSION
+}
+
+make_target() {
+  make -C platforms/libretro/
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/libretro
+  cp platforms/libretro/gearboy_libretro.so $INSTALL/usr/lib/libretro/
+}
