b807b84ad02d013d98cda7acba31641276d6f03c
cvh <namerp@googlemail.com>
2017-12-10 18:50:34 +0100

    media_tree: initial package

---
+++ packages/linux-driver-addons/dvb/depends/media_tree/package.mk
@@ -0,0 +1,42 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2017-present Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="media_tree"
+PKG_VERSION="2017-11-14-f2ecc3d0787e"
+PKG_SHA256="54aaa4cb2ab34804f42410dd461e587ecae36bd808c6e4accb3bcdae8c04579b"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://git.linuxtv.org/media_tree.git"
+PKG_URL="http://linuxtv.org/downloads/drivers/linux-media-${PKG_VERSION}.tar.bz2"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="driver"
+PKG_LONGDESC="Source of Linux Kernel media_tree subsystem to build with media_build."
+PKG_TOOLCHAIN="manual"
+
+unpack() {
+  mkdir -p $PKG_BUILD/
+  tar -xf $SOURCES/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.bz2 -C $PKG_BUILD/
+
+  # hack/workaround for borked upstream kernel/media_build
+  # without removing atomisp there a lot additional includes that 
+  # slowdown build process after modpost from 3min to 6min
+  # even if atomisp is disabled via kernel.conf
+  rm -rf $PKG_BUILD/drivers/staging/media/atomisp
+  sed -i 's|^.*drivers/staging/media/atomisp.*$||' \
+    $PKG_BUILD/drivers/staging/media/Kconfig
+}
