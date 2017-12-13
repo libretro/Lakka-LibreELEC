134552fe41dde16b5163f21f8f05f32717cf49ff
Rob Loach <robloach@gmail.com>
2017-12-13 11:51:53 -0500

    chailove: Update to 0.12.0
    ### Added
    - `SoundData.getVolume()`
    - `SoundData.setVolume()`
    
    ### Changed
    - Updated C++ namespace from `Modules` to `love`
    - [Method chaining](https://en.wikipedia.org/wiki/Method_chaining) for most functions
        ```
        graphics.love.point(100,100).point(50, 200)
        ```
    - Moved `Types` namespace to `love::Types`

--- packages/libretro/chailove/package.mk
+++ packages/libretro/chailove/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="chailove"
-PKG_VERSION="996a1c9"
+PKG_VERSION="8b1c70d"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
