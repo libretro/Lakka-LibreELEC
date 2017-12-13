96bbec2e79121a937690c28ff944f05be725e41b
Jonas Karlman <jonas@kwiboo.se>
2017-12-13 18:52:02 +0100

    linux: update rockchip-4.4 to bbea8b14

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -47,8 +47,8 @@ case "$LINUX" in
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET aml-dtbtools:host"
     ;;
   rockchip-4.4)
-    PKG_VERSION="a1af913e"
-    PKG_SHA256="09460e2fd0b8cdcd081ec7f7f465e4329f39e4f0ddc630d58f7a81af46074805"
+    PKG_VERSION="bbea8b14"
+    PKG_SHA256="6485f7ecbe4eac424b43df774fdce8c62b97ac7d9d406740d9fafb9f6a84e8af"
     PKG_URL="https://github.com/rockchip-linux/kernel/archive/$PKG_VERSION.tar.gz"
     PKG_SOURCE_DIR="kernel-$PKG_VERSION*"
     PKG_PATCH_DIRS="rockchip-4.4"
--- projects/Rockchip/README.md
+++ projects/Rockchip/README.md
@@ -18,3 +18,11 @@ You may have luck if your device vendor is open source friendly, otherwise keep 
 
 * https://github.com/rockchip-linux
 * http://opensource.rock-chips.com
+
+## Useful debug commands
+
+* `cat /sys/kernel/debug/dri/0/summary`
+* `cat /sys/kernel/debug/dw-hdmi/status`
+* `cat /sys/kernel/debug/clk/clk_summary`
+* `hexdump -C /sys/class/drm/card0-HDMI-A-1/edid`
+* `edid-decode /sys/class/drm/card0-HDMI-A-1/edid`
