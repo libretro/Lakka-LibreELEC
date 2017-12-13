751db59e45875f46c966254358d0897dc95a68e2
Rob Loach <robloach@gmail.com>
2017-12-13 12:11:37 -0500

    libretro-database: Update to the latest database
    This updates to the latest libretro-database, with a recent RDB update.

--- packages/libretro/libretro-database/package.mk
+++ packages/libretro/libretro-database/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="libretro-database"
-PKG_VERSION="4667ad6"
+PKG_VERSION="7bceb69"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
