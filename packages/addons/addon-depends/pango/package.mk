54307b296e67dc99ae420b4a7d49ce8e3f28c0ef
Lionel CHAZALLON <LongChair@hotmail.com>
2017-12-16 10:17:50 -0800

    pango: don't assume we have X11
    Allows X11 / Wayland support depending on DISPLAYSERVER

--- packages/addons/addon-depends/pango/package.mk
+++ packages/addons/addon-depends/pango/package.mk
@@ -25,7 +25,7 @@ PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.pango.org/"
 PKG_URL="http://ftp.gnome.org/pub/gnome/sources/pango/1.40/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain glib cairo freetype fontconfig libX11 libXft harfbuzz"
+PKG_DEPENDS_TARGET="toolchain glib freetype fontconfig harfbuzz"
 PKG_SECTION="x11/toolkits"
 PKG_SHORTDESC="pango: Library for layout and rendering of internationalized text"
 PKG_LONGDESC="The goal of the Pango project is to provide an open-source framework for the layout and rendering of internationalized text. Pango is an offshoot of the GTK+ and GNOME projects, and the initial focus is operation in those environments, however there is nothing fundamentally GTK+ or GNOME specific about Pango. Pango uses Unicode for all of its encoding, and will eventually support output in all the worlds major languages."
@@ -34,5 +34,13 @@ PKG_TOOLCHAIN="autotools"
 PKG_CONFIGURE_OPTS_TARGET="--disable-doc-cross-references \
                            --disable-gtk-doc \
                            --disable-man \
-                           --enable-debug=no \
-                           --with-xft"
+                           --enable-debug=no"
+
+if [ "$DISPLAYSERVER" = "x11" ] ; then
+  PKG_DEPENDS_TARGET+=" cairo libX11 libXft"
+  PKG_CONFIGURE_OPTS_TARGET+=" --with-xft"
+fi
+
+if [ "$DISPLAYSERVER" = "weston" ] ; then
+  PKG_DEPENDS_TARGET+=" wayland"
+fi
