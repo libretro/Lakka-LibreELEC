4ff5240148515814a2c0e350663b3a66f9256652
kszaq <kszaquitto@gmail.com>
2018-01-06 04:33:24 +0000

    linux-drivers/amlogic: RTL*-aml: use default MAC address instead of random in case no valid MAC is present

--- packages/linux-drivers/amlogic/RTL8189ES-aml/package.mk
+++ packages/linux-drivers/amlogic/RTL8189ES-aml/package.mk
@@ -35,6 +35,7 @@ PKG_TOOLCHAIN="manual"
 post_unpack() {
   sed -i 's/-DCONFIG_CONCURRENT_MODE//g; s/^CONFIG_POWER_SAVING.*$/CONFIG_POWER_SAVING = n/g; s/^CONFIG_RTW_DEBUG.*/CONFIG_RTW_DEBUG = n/g' $PKG_BUILD/*/Makefile
   sed -i 's/^#define CONFIG_DEBUG.*//g' $PKG_BUILD/*/include/autoconf.h
+  sed -i 's/#define DEFAULT_RANDOM_MACADDR.*1/#define DEFAULT_RANDOM_MACADDR 0/g' $PKG_BUILD/*/core/rtw_ieee80211.c
 }
 
 pre_make_target() {
