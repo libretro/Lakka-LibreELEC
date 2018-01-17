3d97c1298c4caeb603a5d9b5742d81a47dfc8660
MilhouseVH <milhouseVH.github@nmacleod.com>
2018-01-17 02:03:18 +0000

    RPi: update firmware to 5e55ff0

--- packages/graphics/bcm2835-driver/package.mk
+++ packages/graphics/bcm2835-driver/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="bcm2835-driver"
-PKG_VERSION="008700b"
-PKG_SHA256="840f94f383f1c252f840906a190b58ad2fb174716f2cfed5f83b4ad47a4c1f74"
+PKG_VERSION="5e55ff0"
+PKG_SHA256="05b2d417e79e0629003d349d280a4b28f79aa6a312a0d53b05ec5b868b1e405b"
 PKG_ARCH="any"
 PKG_LICENSE="nonfree"
 PKG_SITE="http://www.broadcom.com"
