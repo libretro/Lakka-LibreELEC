d67415076cb86d901983cda0ff4049373990e745
gouchi <gouchi@free.fr>
2018-01-20 14:14:19 -0600

    Package Game Music Emu core

---
+++ packages/libretro/gme/package.mk
@@ -0,0 +1,45 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.tv; see the file COPYING.  If not, write to
+#  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
+#  http://www.gnu.org/copyleft/gpl.html
+################################################################################
+
+PKG_NAME="gme"
+PKG_VERSION="0876813"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://github.com/libretro/libretro-gme"
+PKG_URL="https://github.com/libretro/libretro-gme/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="libretro"
+PKG_SHORTDESC="Port of blargg's Game_Music_Emu library."
+PKG_LONGDESC="Port of blargg's Game_Music_Emu library."
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+post_unpack() {
+  mv $BUILD/libretro-gme-$PKG_VERSION* $BUILD/$PKG_NAME-$PKG_VERSION
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/libretro
+  cp gme_libretro.so $INSTALL/usr/lib/libretro/
+}
+
