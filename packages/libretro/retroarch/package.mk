583afe6afb7f786863fa2961929a71ddf20d0549
Jean-André Santoni <jean.andre.santoni@gmail.com>
2018-01-22 19:48:38 -0600

    Update RetroArch. Fixes GL cores on RPi

--- packages/libretro/retroarch/package.mk
+++ packages/libretro/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="dbd2faf"
+PKG_VERSION="4ba100f"
 PKG_REV="9"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
