559feac92120ed6b727e2c669dd7a92fa9c989fc
MilhouseVH <milhouseVH.github@nmacleod.com>
2018-01-25 19:00:49 +0000

    linux: update to linux-4.14.15

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -47,8 +47,8 @@ case "$LINUX" in
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET aml-dtbtools:host"
     ;;
   *)
-    PKG_VERSION="4.14.14"
-    PKG_SHA256="8b96362eb55ae152555980e7193fe2585b487176fb936cc69b8947d7dd32044a"
+    PKG_VERSION="4.14.15"
+    PKG_SHA256="ffc393a0c66f80375eacd3fb177b92e5c9daa07de0dcf947e925e049352e6142"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v4.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;
