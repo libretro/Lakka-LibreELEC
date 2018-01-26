8ba501a28853d3124176ba673c567b7f0de58265
MilhouseVH <milhouseVH.github@nmacleod.com>
2018-01-26 03:24:58 +0000

    mesa: update to mesa-17.3.3

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="mesa"
-PKG_VERSION="17.3.2"
-PKG_SHA256="e2844a13f2d6f8f24bee65804a51c42d8dc6ae9c36cff7ee61d0940e796d64c6"
+PKG_VERSION="17.3.3"
+PKG_SHA256="41bac5de0ef6adc1f41a1ec0f80c19e361298ce02fa81b5f9ba4fdca33a9379b"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
