b7967dfcb77aab400a45998f465e25a1677ddeeb
MilhouseVH <milhouseVH.github@nmacleod.com>
2018-01-28 04:57:52 +0000

    Revert "add -J flag to limit race condition during build step"
    This reverts commit 8af0df0bf6a39fd07d2f32b393da97cee7a44077.

--- packages/sysutils/busybox/package.mk
+++ packages/sysutils/busybox/package.mk
@@ -40,7 +40,6 @@ PKG_MAKE_OPTS_INIT="ARCH=$TARGET_ARCH \
                     HOSTCC=$HOST_CC \
                     CROSS_COMPILE=$TARGET_PREFIX \
                     KBUILD_VERBOSE=1 \
-                    MAKEFLAGS="-j1" \
                     install"
 
 # nano text editor
