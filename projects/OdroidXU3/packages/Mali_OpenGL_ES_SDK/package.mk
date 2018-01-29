c671c0bbec267efe8cfc291d3bb25bb5885c6b9a
Ntemis <ierokipides.dem@gmail.com>
2018-01-29 16:29:51 +0200

    XU[34]: Add new mali packages for gbm/kms support

--- projects/OdroidXU3/packages/Mali_OpenGL_ES_SDK/package.mk
+++ projects/OdroidXU3/packages/Mali_OpenGL_ES_SDK/package.mk
@@ -18,20 +18,20 @@
 #  http://www.gnu.org/copyleft/gpl.html
 ################################################################################
 
-PKG_NAME="odroidxu3-mali-headers"
-PKG_VERSION="01"
-PKG_REV="1"
+PKG_NAME="Mali_OpenGL_ES_SDK"
+PKG_VERSION="v2.4.4"
+PKG_REVISION="ed0c8beb-298d-43bc-8d1a-0c99ed94eee6"
 PKG_ARCH="any"
 PKG_LICENSE="nonfree"
-PKG_SITE="https://github.com/linux-sunxi/sunxi-mali"
-PKG_URL="$LAKKA_MIRROR/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_BUILD_DEPENDS_TARGET="toolchain"
-PKG_DEPENDS_TARGET=""
+PKG_TOOLCHAIN="manual"
+PKG_SITE="https://developer.arm.com/products/software/mali-sdks/opengl-es/downloads"
+PKG_URL="https://developer.arm.com/-/media/Files/downloads/mali-sdk/v2.4.4/Mali_OpenGL_ES_SDK_v2.4.4.ef7d5a_Linux_x64.tar.gz"
+PKG_SOURCE_DIR="Mali_OpenGL_ES_SDK_v2.4.4"
 PKG_PRIORITY="optional"
 PKG_SECTION="graphics"
-PKG_SHORTDESC="MALI headers for XU3."
-PKG_LONGDESC="MALI headers for XU3."
-
+PKG_SHORTDESC="MALI headers for XU[34]."
+PKG_LONGDESC="MALI headers for XU[34]."
+PKG_USE_CMAKE="no"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
@@ -41,5 +41,9 @@ make_target() {
 
 makeinstall_target() {
   mkdir -p $SYSROOT_PREFIX/usr/include
-    cp -PR usr/include/* $SYSROOT_PREFIX/usr/include
+    cp -PR $PKG_BUILD/inc/* $SYSROOT_PREFIX/usr/include
+    cp -PR $PKG_BUILD/simple_framework/inc/mali/* $SYSROOT_PREFIX/usr/include
+
+    mkdir -p $SYSROOT_PREFIX/usr/lib/pkgconfig
+    cp -PRv $PKG_DIR/pkgconfig/*.pc $SYSROOT_PREFIX/usr/lib/pkgconfig
 }
