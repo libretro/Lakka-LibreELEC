5e4f6e3b7bb55d7b48882ec5752146177bb85207
Demetris Ierokipides <ierokipides.dem@gmail.com>
2018-02-01 14:15:01 +0200

    XU[34]: Add new mali packages for gbm/kms support (#358)
    * XU[34]: Add new mali packages for gbm/kms support
    
    * XU[34]: Add libdrm to mali depends and switch retroarch to work with kms/drm
    
    * XU[34]: Fix boot.ini
    
    * XU[34]: Update kernel add modules config
    
    * XU[34] XU3 compatibility restore
    
    * XU[34]: And there will be Retroarch
    
    * XU[34]:Add Systemd serial console service
    
    * XU[34]: Fix sound and update gfx libheaders

--- packages/libretro/retroarch/package.mk
+++ packages/libretro/retroarch/package.mk
@@ -54,7 +54,7 @@ elif [ "$OPENGLES" == "bcm2835-driver" ]; then
   RETROARCH_GL="--enable-opengles --disable-kms --disable-x11"
   CFLAGS="$CFLAGS -I$SYSROOT_PREFIX/usr/include/interface/vcos/pthreads \
                   -I$SYSROOT_PREFIX/usr/include/interface/vmcs_host/linux"
-elif [ "$OPENGLES" == "odroidc1-mali" ] || [ "$OPENGLES" == "odroidxu3-mali" ] || [ "$OPENGLES" == "opengl-meson" ] || [ "$OPENGLES" == "opengl-meson8" ] || [ "$OPENGLES" == "opengl-meson-t82x" ]; then
+elif [ "$OPENGLES" == "odroidc1-mali" ] || [ "$OPENGLES" == "opengl-meson" ] || [ "$OPENGLES" == "opengl-meson8" ] || [ "$OPENGLES" == "opengl-meson-t82x" ]; then
   RETROARCH_GL="--enable-opengles --disable-kms --disable-x11 --enable-mali_fbdev"
 elif [ "$OPENGLES" == "gpu-viv-bin-mx6q" ] || [ "$OPENGLES" == "imx-gpu-viv" ]; then
   RETROARCH_GL="--enable-opengles --disable-kms --disable-x11 --enable-vivante_fbdev"
@@ -63,7 +63,7 @@ elif [ "$OPENGLES" == "mali-rockchip" ]; then
   RETROARCH_GL="--enable-opengles --enable-kms --disable-x11 --disable-wayland"
 elif [ "$OPENGLES" == "allwinner-fb-mali" ]; then
    RETROARCH_GL="--enable-opengles --disable-kms --disable-x11 --enable-mali_fbdev"
-elif [ "$OPENGLES" == "allwinner-mali" ]; then
+elif [ "$OPENGLES" == "allwinner-mali" ] || [ "$OPENGLES" == "odroidxu3-mali" ]; then
    RETROARCH_GL="--enable-opengles --enable-kms --disable-x11"
 fi
 
