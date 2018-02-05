032cf5a44b4bbb763d627f0b5322fb6b09a93088
Matthias Reichl <hias@horus.com>
2018-02-05 11:34:42 +0100

    elfutils: install libdwfl, libebl and version.h to sysroot
    Needed by perf to parse and display debug symbols.
    
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/devel/elfutils/package.mk
+++ packages/devel/elfutils/package.mk
@@ -45,14 +45,21 @@ make_target() {
   make V=1 -C libelf libelf.a
   make V=1 -C libebl libebl.a
   make V=1 -C libdwfl libdwfl.a
+  make V=1 -C libdwelf libdwelf.a
   make V=1 -C libdw libdw.a
 }
 
 makeinstall_target() {
   make DESTDIR="$SYSROOT_PREFIX" -C libelf install-includeHEADERS install-pkgincludeHEADERS
+  make DESTDIR="$SYSROOT_PREFIX" -C libdwfl install-pkgincludeHEADERS
   make DESTDIR="$SYSROOT_PREFIX" -C libdw install-includeHEADERS install-pkgincludeHEADERS
 
   mkdir -p $SYSROOT_PREFIX/usr/lib
     cp libelf/libelf.a $SYSROOT_PREFIX/usr/lib
+    cp libebl/libebl.a $SYSROOT_PREFIX/usr/lib
+    cp libdwfl/libdwfl.a $SYSROOT_PREFIX/usr/lib
     cp libdw/libdw.a $SYSROOT_PREFIX/usr/lib
+
+  mkdir -p $SYSROOT_PREFIX/usr/include/elfutils
+    cp version.h $SYSROOT_PREFIX/usr/include/elfutils
 }
