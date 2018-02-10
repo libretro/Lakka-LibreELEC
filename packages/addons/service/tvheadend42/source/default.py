6d998df283f5928aaf729b6b8cdd7a33bd059a62
cvh <namerp@googlemail.com>
2018-02-10 11:02:49 +0100

    tvheadend42: rework and update

--- packages/addons/service/tvheadend42/source/default.py
+++ packages/addons/service/tvheadend42/source/default.py
@@ -1,6 +1,6 @@
 ################################################################################
 #      This file is part of LibreELEC - https://libreelec.tv
-#      Copyright (C) 2016 Team LibreELEC
+#      Copyright (C) 2016-present Team LibreELEC
 #
 #  LibreELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
