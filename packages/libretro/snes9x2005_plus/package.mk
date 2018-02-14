de3e5b8ba9e20afe00d1b27fffda3e191b798742
Tomáš Kelemen <tomas.kelemen@gmail.com>
2018-02-14 15:40:28 +0100

    new core: snes9x2005_plus (#386)
    * new core: snes9x2005_plus
    
    * snes9x2005_plus: add to LIBRETRO_CORES
    
    * snes9x2005_plus: add make flags, remove patch, fix makeinstall_target()

---
+++ packages/libretro/snes9x2005_plus/package.mk
@@ -0,0 +1,42 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.tv; see the file COPYING.  If not, write to
+#  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
+#  http://www.gnu.org/copyleft/gpl.html
+################################################################################
+
+PKG_NAME="snes9x2005_plus"
+PKG_VERSION="e2ad3df"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="Non-commercial"
+PKG_SITE="https://github.com/libretro/snes9x2005"
+PKG_GIT_URL="$PKG_SITE"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="libretro"
+PKG_SHORTDESC="Snes9x 2005 Plus."
+PKG_LONGDESC="Snes9x 2005 Plus. Port of SNES9x 1.43 for libretro (was previously called CAT SFC) with enabled BLARRG APU."
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+PKG_MAKE_OPTS_TARGET="USE_BLARGG_APU=1"
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/libretro
+  cp snes9x2005_plus_libretro.so $INSTALL/usr/lib/libretro/
+}
