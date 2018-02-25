dface8a19d512faa0898405e4251559ccdf0e18a
cvh <namerp@googlemail.com>
2018-02-25 12:05:07 +0100

    ffmpegx: update and add x11grab

--- packages/addons/addon-depends/ffmpegx/package.mk
+++ packages/addons/addon-depends/ffmpegx/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="ffmpegx"
-PKG_VERSION="3.4"
-PKG_SHA256="2865712ead48b9e5939c1eb23c409c8f1d60e49bc6434ca489271ea677c81adc"
+PKG_VERSION="3.4.2"
+PKG_SHA256="d079c68dc19a0223239a152ffc2b67ef1e9d3144e4d2c2563380dc59dccf33e5"
 PKG_ARCH="any"
 PKG_LICENSE="LGPLv2.1+"
 PKG_SITE="https://ffmpeg.org"
@@ -121,6 +121,16 @@ pre_configure_target() {
     --enable-libvorbis \
     --enable-encoder=libvorbis"
 
+# X11 grab for screen recording
+  if [ "$DISPLAYSERVER" = "x11" ]; then
+    PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET libxcb"
+    PKG_FFMPEG_LIBS="$PKG_FFMPEG_LIBS -lX11"
+    PKG_FFMPEG_X11_GRAB="\
+    --enable-libxcb \
+    --enable-libxcb-shm \
+    --enable-libxcb-xfixes \
+    --enable-libxcb-shape"
+  fi
 }
 
 configure_target() {
@@ -151,6 +161,7 @@ configure_target() {
     --enable-avresample \
     --disable-lzma \
     --disable-alsa \
+    $PKG_FFMPEG_X11_GRAB \
     \
     `#Toolchain options` \
     --arch="$TARGET_ARCH" \
