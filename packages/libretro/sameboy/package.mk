4815305c707e57115586d943aac6a72569ff2082
Tomáš Kelemen <tomas.kelemen@gmail.com>
2018-02-26 11:21:02 +0100

    sameboy: PKG_BUILD is not needed (#404)

--- packages/libretro/sameboy/package.mk
+++ packages/libretro/sameboy/package.mk
@@ -35,10 +35,10 @@ PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
 make_target() {
-  make -C $PKG_BUILD/libretro
+  make -C libretro
 }
 
 makeinstall_target() {
   mkdir -p $INSTALL/usr/lib/libretro
-  cp $PKG_BUILD/libretro/sameboy_libretro.so $INSTALL/usr/lib/libretro/
+  cp libretro/sameboy_libretro.so $INSTALL/usr/lib/libretro/
 }
