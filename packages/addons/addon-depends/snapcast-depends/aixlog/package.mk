d6446e7ba6157ff590bd19510b470582f832c9d5
awiouy <awiouy@gmail.com>
2018-03-03 19:59:22 +0100

    aixlog: initial package

---
+++ packages/addons/addon-depends/snapcast-depends/aixlog/package.mk
@@ -0,0 +1,29 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2018-present Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="aixlog"
+PKG_VERSION="1.1.0"
+PKG_SHA256="a32f1e899b2ba4631847f1c89d6f967ef81eeef2fa8efa9a13ffea870e7f06f2"
+PKG_ARCH="any"
+PKG_LICENSE="MIT"
+PKG_SITE="https://github.com/badaix/aixlog"
+PKG_URL="https://github.com/badaix/aixlog/archive/v$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="tools"
+PKG_LONGDESC="Header-only C++ logging library"
+PKG_TOOLCHAIN="manual"
