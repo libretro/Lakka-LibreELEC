82ceb2e5ab5a5e1d59e97b22b8c8e0aa1a197336
awiouy <awiouy@gmail.com>
2018-03-03 19:59:42 +0100

    asio: initial package

---
+++ packages/addons/addon-depends/snapcast-depends/asio/package.mk
@@ -0,0 +1,29 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2018-present Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="asio"
+PKG_VERSION="9229964"
+PKG_SHA256="799ca6607cb873bdc3253b0f8a058cb15af9c9fc2c74fd1732f6fe7726d7c200"
+PKG_ARCH="any"
+PKG_LICENSE="BSL"
+PKG_SITE="http://think-async.com/Asio"
+PKG_URL="https://github.com/chriskohlhoff/asio/archive/$PKG_VERSION.zip"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="tools"
+PKG_LONGDESC="Asio C++ Library"
+PKG_TOOLCHAIN="manual"
