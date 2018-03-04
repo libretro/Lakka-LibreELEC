266d6be5fac3ad02d8aeecb14be5fa421ca1cc7b
MilhouseVH <milhouseVH.github@nmacleod.com>
2018-03-04 14:52:29 +0000

    Drop Amlogic config, don't install RPi sdio files

--- packages/linux-firmware/kernel-firmware/package.mk
+++ packages/linux-firmware/kernel-firmware/package.mk
@@ -62,6 +62,10 @@ makeinstall_target() {
     done < ${fwlist}
   done
 
+  # The following files are RPi specific and installed by brcmfmac_sdio-firmware-rpi instead
+  rm -fr $FW_TARGET_DIR/brcm/brcmfmac43430*-sdio.bin
+  rm -fr $FW_TARGET_DIR/brcm/brcmfmac43455*-sdio.bin
+
   # Cleanup - which may be project or device specific
   find_file_path scripts/cleanup.sh && ${FOUND_PATH} ${FW_TARGET_DIR} || true
 }
--- projects/RPi/packages/kernel-firmware/scripts/cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-FW_TARGET_DIR=$1
-
-# The following files will be installed by brcmfmac_sdio-firmware-rpi instead
-rm -fr $FW_TARGET_DIR/brcm/brcmfmac43430*-sdio.bin
-rm -fr $FW_TARGET_DIR/brcm/brcmfmac43455*-sdio.bin
-
-exit 0
