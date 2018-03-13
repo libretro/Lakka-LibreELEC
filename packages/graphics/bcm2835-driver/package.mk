eee50c694cbd61621a4a2d132d03c87e38c23174
MilhouseVH <milhouseVH.github@nmacleod.com>
2018-03-13 19:46:54 +0000

    RPi: update firmware to 3347884

--- packages/graphics/bcm2835-driver/package.mk
+++ packages/graphics/bcm2835-driver/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="bcm2835-driver"
-PKG_VERSION="6e3c5f8"
+PKG_VERSION="3347884"
 PKG_ARCH="any"
 PKG_LICENSE="nonfree"
 PKG_SITE="http://www.broadcom.com"
