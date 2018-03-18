7b0b3d4c5a5054b0a14faf4194565a3c7961d3b0
Jean-André Santoni <jean.andre.santoni@gmail.com>
2018-03-18 02:54:41 +0100

    Revert ppsspp to a version that work

--- packages/libretro/ppsspp/package.mk
+++ packages/libretro/ppsspp/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="ppsspp"
-PKG_VERSION="7f30ab1"
+PKG_VERSION="dd73f91"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
