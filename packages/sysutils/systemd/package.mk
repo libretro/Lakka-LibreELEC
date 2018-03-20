d38cadaf8c08c497189d9878fa93ed7e1a5c81f5
Sascha Kuehndel (InuSasha) <dev@inusasha.de>
2018-03-20 22:11:25 +0100

    meson: update to 0.45 - generate cross-file per package, needed since https://github.com/mesonbuild/meson/pull/1807 to use changed build-flags

--- packages/sysutils/systemd/package.mk
+++ packages/sysutils/systemd/package.mk
@@ -106,10 +106,6 @@ PKG_MESON_OPTS_TARGET="--libdir=/usr/lib \
 pre_configure_target() {
   export CFLAGS="$CFLAGS -fno-schedule-insns -fno-schedule-insns2"
   export LC_ALL=en_US.UTF-8
-
-  # meson needs a host compiler and it's detected through the environment. meh.
-  export CC="$HOST_CC"
-  export CXX="$HOST_CXX"
 }
 
 post_makeinstall_target() {
