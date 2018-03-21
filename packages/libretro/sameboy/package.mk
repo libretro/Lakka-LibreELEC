c90e96b2ea9d05dd66fa29ce7de2e907fdf8eda1
Tomáš Kelemen <tomas.kelemen@gmail.com>
2018-03-21 17:38:46 +0100

    sameboy: bump version (#425)

--- packages/libretro/sameboy/package.mk
+++ packages/libretro/sameboy/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="sameboy"
-PKG_VERSION="a5e6868"
+PKG_VERSION="666ea63"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
