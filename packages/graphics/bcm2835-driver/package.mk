b37646d107bf540028968ee9aa209442cd7c4e37
MilhouseVH <milhouseVH.github@nmacleod.com>
2018-03-24 21:21:12 +0000

    RPi: update to firmware 36f9913 [WIP]

--- packages/graphics/bcm2835-driver/package.mk
+++ packages/graphics/bcm2835-driver/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="bcm2835-driver"
-PKG_VERSION="3347884"
+PKG_VERSION="36f9913"
 PKG_ARCH="any"
 PKG_LICENSE="nonfree"
 PKG_SITE="http://www.broadcom.com"
--- packages/tools/bcm2835-bootloader/package.mk
+++ packages/tools/bcm2835-bootloader/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="bcm2835-bootloader"
-PKG_VERSION="3347884"
+PKG_VERSION="36f9913"
 PKG_ARCH="arm"
 PKG_LICENSE="nonfree"
 PKG_SITE="http://www.broadcom.com"
