af3f01ae4bca99f609c8968b6db8be157cf18ac2
Jean-André Santoni <jean.andre.santoni@gmail.com>
2018-04-05 17:51:24 +0700

    Update RA to 1.7.2 (#440)

--- packages/libretro/retroarch/package.mk
+++ packages/libretro/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="29a057a"
+PKG_VERSION="ab00d58"
 PKG_REV="11"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
