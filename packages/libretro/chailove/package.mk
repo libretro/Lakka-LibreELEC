39a505b2844dc90456d694ca1c44c2402565c915
Rob Loach <robloach@gmail.com>
2018-04-27 08:08:12 -0400

    chailove: Update to 0.20.1 (#454)
    ChaiLove 0.20.1 comes with a few updates since 0.18.0...
    
    - No Game demo screen
    - Version mismatch checking
    - `love.data.hash()`
    - `love.system.getUsername()`
    - Better memory usage
    - Fixed compilation warnings

--- packages/libretro/chailove/package.mk
+++ packages/libretro/chailove/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="chailove"
-PKG_VERSION="d0e4a2c"
+PKG_VERSION="e19269c"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
