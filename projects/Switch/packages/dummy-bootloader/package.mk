68fbc5b13de3ae3ae0f719afadc720f237a7c511
natinusala <natinusala@gmail.com>
2018-05-07 21:25:08 +0200

    Added Switch project (needs extra work to boot)

---
+++ projects/Switch/packages/dummy-bootloader/package.mk
@@ -0,0 +1,23 @@
+PKG_NAME="dummy-bootloader"
+PKG_VERSION="dummy"
+PKG_ARCH="any"
+
+make_init() {
+	:
+}
+
+make_target() {
+	echo "Nope"
+}
+
+makeinstall_target() {
+	echo "Nope"
+}
+
+make_host() {
+	echo "Nope"
+}
+
+makeinstall_host() {
+	echo "Nope"
+}
