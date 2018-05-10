bcbf56e7cb5511f06517a86f03400cc9d3d42819
natinusala <natinusala@gmail.com>
2018-05-10 23:49:43 +0200

    Added tegra video driver

---
+++ projects/Switch/packages/xf86-video-nouveau/package.mk
@@ -0,0 +1,11 @@
+PKG_NAME="xf86-video-nouveau"
+PKG_VERSION="1.0.15"
+PKG_ARCH="any"
+PKG_SECTION="x11/driver"
+PKG_LICENSE="OSS"
+PKG_SITE="http://www.X.org"
+PKG_DEPENDS_TARGET="toolchain util-macros"
+PKG_URL="http://xorg.freedesktop.org/archive/individual/driver/$PKG_NAME-$PKG_VERSION.tar.bz2"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="yes"
