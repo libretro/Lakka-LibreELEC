224f80b1cc7c7adbdd72c2c25e0b4e3ca09784c6
cvh <namerp@googlemail.com>
2018-05-11 23:22:42 +0200

    file: update to 5.33

--- packages/addons/addon-depends/system-tools-depends/file/package.mk
+++ packages/addons/addon-depends/system-tools-depends/file/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="file"
-PKG_VERSION="70c5f15"
-PKG_SHA256="499a851b0aac92e261c069ab5581e8376ede9527bf36351f1d8d9dcd051819fc"
+PKG_VERSION="2198460"
+PKG_SHA256="d825eeee9e0a20b79dbcd00c51022b69fe4468354d8ccc03f441eb349ad8bde0"
 PKG_ARCH="any"
 PKG_LICENSE="BSD"
 PKG_SITE="http://www.darwinsys.com/file/"
@@ -26,13 +26,12 @@ PKG_URL="https://github.com/file/file/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST="ccache:host"
 PKG_DEPENDS_TARGET="toolchain zlib file:host"
 PKG_SECTION="tools"
-PKG_SHORTDESC="file: File type identification utility"
-PKG_LONGDESC="These are the sources to Darwin's file(1) utility and master magic(4) file, now maintained by Christos Zoulas. The file(1) utility is used to determine the types of various files."
+PKG_LONGDESC="The file utility is used to determine the types of various files."
 PKG_TOOLCHAIN="autotools"
 
 PKG_CONFIGURE_OPTS_HOST="--enable-fsect-man5 --enable-static --disable-shared"
 PKG_CONFIGURE_OPTS_TARGET="--enable-fsect-man5 --enable-static --disable-shared"
 
 makeinstall_target() {
-  : # meh
+  :
 }
