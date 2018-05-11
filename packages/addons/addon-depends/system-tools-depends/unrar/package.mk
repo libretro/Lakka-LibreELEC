164f1bbd22bfa0c1d5dcd2ccf6e5320e5b669a8b
cvh <namerp@googlemail.com>
2018-05-11 23:22:42 +0200

    unrar: update to 5.6.3

--- packages/addons/addon-depends/system-tools-depends/unrar/package.mk
+++ packages/addons/addon-depends/system-tools-depends/unrar/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="unrar"
-PKG_VERSION="5.5.5"
-PKG_SHA256="a4553839cb2f025d0d9c5633816a83a723e3938209f17620c8c15da06ed061ef"
+PKG_VERSION="5.6.3"
+PKG_SHA256="c590e70a745d840ae9b9f05ba6c449438838c8280d76ce796a26b3fcd0a1972e"
 PKG_ARCH="any"
 PKG_LICENSE="free"
 PKG_SITE="http://www.rarlab.com"
@@ -26,8 +26,7 @@ PKG_URL="http://www.rarlab.com/rar/unrarsrc-$PKG_VERSION.tar.gz"
 PKG_SOURCE_DIR="${PKG_NAME}"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="tools"
-PKG_SHORTDESC="unrar: Extract, test and view RAR archives"
-PKG_LONGDESC="Unrar is a package to handle files compressed in the RAR format. Due to strange licensing issues this package can only view, test and extract files in a given archive, but not pack files. But since we have far more advanced open-source compression utils it should be enough to extract the content when you get a RAR archive."
+PKG_LONGDESC="unrar extract, test and view RAR archives"
 PKG_TOOLCHAIN="manual"
 
 make_target() {
