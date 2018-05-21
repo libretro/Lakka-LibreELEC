21ff8045841d41aa63f8a1eed034d78f592f0190
awiouy <awiouy@gmail.com>
2018-05-21 15:25:15 +0200

    snapserver: update to 0.15.0

--- packages/addons/service/snapserver/package.mk
+++ packages/addons/service/snapserver/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="snapserver"
-PKG_VERSION="0.13.0"
-PKG_REV="101"
+PKG_VERSION="0.15.0"
+PKG_REV="102"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
 PKG_DEPENDS_TARGET="toolchain shairport-sync snapcast"
