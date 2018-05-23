4aa28f292beaddda539bd7ac490e100cea6f00ae
CvH <namerp@googlemail.com>
2018-05-23 21:58:01 +0200

    speex: update to 1.2.0

--- packages/audio/speex/package.mk
+++ packages/audio/speex/package.mk
@@ -1,30 +1,29 @@
 ################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016-present Team LibreELEC
 #      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 #
-#  OpenELEC is free software: you can redistribute it and/or modify
+#  LibreELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 2 of the License, or
 #  (at your option) any later version.
 #
-#  OpenELEC is distributed in the hope that it will be useful,
+#  LibreELEC is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
 PKG_NAME="speex"
-PKG_VERSION="1.2rc2"
-PKG_SHA256="caa27c7247ff15c8521c2ae0ea21987c9e9710a8f2d3448e8b79da9806bce891"
+PKG_VERSION="1.2.0"
+PKG_SHA256="eaae8af0ac742dc7d542c9439ac72f1f385ce838392dc849cae4536af9210094"
 PKG_ARCH="any"
 PKG_LICENSE="BSD"
-PKG_SITE="http://downloads.us.xiph.org/releases/speex"
-#PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_URL="$PKG_SITE/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SITE="https://speex.org"
+PKG_URL="http://downloads.us.xiph.org/releases/speex/speex-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="audio"
-PKG_SHORTDESC="Speex / OPUS audio codec"
-PKG_LONGDESC="Speex / OPUS audio codec"
+PKG_LONGDESC="An Open Source Software patent-free audio compression format designed for speech."
