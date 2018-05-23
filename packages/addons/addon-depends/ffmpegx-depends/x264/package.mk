b3788a9d254062b08c00f01d9b8c7e9fc880aa4a
CvH <namerp@googlemail.com>
2018-05-23 21:58:01 +0200

    x264: update to 20180516

--- packages/addons/addon-depends/ffmpegx-depends/x264/package.mk
+++ packages/addons/addon-depends/ffmpegx-depends/x264/package.mk
@@ -17,15 +17,14 @@
 ################################################################################
 
 PKG_NAME="x264"
-PKG_VERSION="snapshot-20180220-2245"
-PKG_SHA256="80090285b40983776793168a3828dfd8125caca06cb6949511d64946d6166882"
+PKG_VERSION="snapshot-20180517-2245"
+PKG_SHA256="55c6558ca6458f92939a53f669a4e51b104c2275f22984393e36fb670528bcbf"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.videolan.org/developers/x264.html"
 PKG_URL="https://download.videolan.org/x264/snapshots/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="multimedia"
-PKG_SHORTDESC="x264"
 PKG_LONGDESC="x264"
 
 pre_configure_target() {
