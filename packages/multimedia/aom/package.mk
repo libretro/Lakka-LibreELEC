53247d7715b2f630a6ffa2fd0d1bfdfd4306f2c6
CvH <namerp@googlemail.com>
2018-05-23 21:58:01 +0200

    aom: initial package

---
+++ packages/multimedia/aom/package.mk
@@ -0,0 +1,32 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2018-present Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="aom"
+PKG_VERSION="a586982"
+PKG_SHA256="078c0cc780fa9fcd3d67456722339a8eaa5591a484e1daae9fc2c3c34810ec46"
+PKG_ARCH="any"
+PKG_LICENSE="BSD"
+PKG_SITE="https://www.webmproject.org"
+PKG_URL="http://repo.or.cz/aom.git/snapshot/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="multimedia"
+PKG_LONGDESC="AV1 Codec Library"
+
+PKG_CMAKE_OPTS_TARGET="-DENABLE_CCACHE=1 \
+                       -DENABLE_DOCS=0 \
+                       -DENABLE_TOOLS=0"
