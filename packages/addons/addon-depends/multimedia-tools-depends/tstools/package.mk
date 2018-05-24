db07346665c562f336f969d583b328d8b489a516
CvH <namerp@googlemail.com>
2018-05-24 18:25:38 +0200

    tstools: update to db1f79f

--- packages/addons/addon-depends/multimedia-tools-depends/tstools/package.mk
+++ packages/addons/addon-depends/multimedia-tools-depends/tstools/package.mk
@@ -1,33 +1,31 @@
 ################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016-present Team LibreELEC
 #      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
 #
-#  This Program is free software; you can redistribute it and/or modify
+#  LibreELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2, or (at your option)
-#  any later version.
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
 #
-#  This Program is distributed in the hope that it will be useful,
+#  LibreELEC is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.tv; see the file COPYING.  If not, write to
-#  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
-#  http://www.gnu.org/copyleft/gpl.html
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
 PKG_NAME="tstools"
-PKG_VERSION="1.11"
-PKG_SHA256="4e207ad7298ec421c6710e3024147b486320d792cec2dbd34efb7e6d9e96230a"
+PKG_VERSION="db1f79f"
+PKG_SHA256="f204229016c9deafcc75fe602c390339878312126134edbfcebf239e093dc4ff"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE="http://tstools.berlios.de/"
-PKG_URL="$SOURCEFORGE_SRC/project/tstools.berlios/tstools-1_11.tgz"
+PKG_SITE="https://github.com/kynesim/tstools"
+PKG_URL="https://github.com/kynesim/tstools/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="tools"
-PKG_SHORTDESC="command line tools for working with MPEG data"
 PKG_LONGDESC="This is a set of cross-platform command line tools for working with MPEG data."
 PKG_BUILD_FLAGS="-parallel"
 
@@ -36,5 +34,5 @@ make_target() {
 }
 
 makeinstall_target() {
-  : # nop
+  :
 }
