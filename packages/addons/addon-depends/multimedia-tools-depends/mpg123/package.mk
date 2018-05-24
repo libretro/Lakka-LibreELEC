aaca1d40bf89be57bedf0adca68fe196ceaffc10
CvH <namerp@googlemail.com>
2018-05-24 18:25:38 +0200

    mpg123: update to 1.25.10

--- packages/addons/addon-depends/multimedia-tools-depends/mpg123/package.mk
+++ packages/addons/addon-depends/multimedia-tools-depends/mpg123/package.mk
@@ -17,15 +17,15 @@
 ################################################################################
 
 PKG_NAME="mpg123"
-PKG_VERSION="1.23.6"
-PKG_SHA256="4073d9c60a43872f6f5a3a322f5ea21ab7f0869d2ed25e79c3eb8521fa3c32d4"
+PKG_VERSION="1.25.10"
+PKG_SHA256="6c1337aee2e4bf993299851c70b7db11faec785303cfca3a5c3eb5f329ba7023"
 PKG_ARCH="any"
 PKG_LICENSE="LGPLv2"
 PKG_SITE="http://www.mpg123.org/"
-PKG_URL="http://netcologne.dl.sourceforge.net/project/$PKG_NAME/$PKG_NAME/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_URL="http://downloads.sourceforge.net/sourceforge/mpg123/mpg123-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain alsa-lib SDL2"
 PKG_SECTION="tools"
-PKG_SHORTDESC="mpg123"
-PKG_LONGDESC="A fast MP3 Player"
+PKG_LONGDESC="A console based real time MPEG Audio Player for Layer 1, 2 and 3."
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-static"
+PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
+                           --enable-static"
