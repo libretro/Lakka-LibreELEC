536d25907ac3fb7c802d6e34abfc1d67d3da1480
CvH <namerp@googlemail.com>
2018-05-24 18:25:38 +0200

    soxr: update to 0.1.3

--- packages/audio/soxr/package.mk
+++ packages/audio/soxr/package.mk
@@ -1,36 +1,35 @@
 ################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016-present Team LibreELEC
 #      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 #
-#  OpenELEC is free software: you can redistribute it and/or modify
+#  LibreELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 2 of the License, or
 #  (at your option) any later version.
 #
-#  OpenELEC is distributed in the hope that it will be useful,
+#  LibreELEC is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
 PKG_NAME="soxr"
-PKG_VERSION="0.1.2"
-PKG_SHA256="54e6f434f1c491388cd92f0e3c47f1ade082cc24327bdc43762f7d1eefe0c275"
+PKG_VERSION="0.1.3"
+PKG_SHA256="b111c15fdc8c029989330ff559184198c161100a59312f5dc19ddeb9b5a15889"
 PKG_ARCH="any"
 PKG_LICENSE="LGPL"
-PKG_SITE="http://sourceforge.net/p/soxr/wiki/Home/"
-PKG_URL="$SOURCEFORGE_SRC/soxr/$PKG_NAME-$PKG_VERSION-Source.tar.xz"
+PKG_SITE="https://sourceforge.net/projects/soxr/"
+PKG_URL="$SOURCEFORGE_SRC/soxr/soxr-$PKG_VERSION-Source.tar.xz"
 PKG_SOURCE_DIR="$PKG_NAME-$PKG_VERSION-Source"
 PKG_DEPENDS_TARGET="toolchain cmake:host"
 PKG_SECTION="audio"
-PKG_SHORTDESC="soxr: a library which performs one-dimensional sample-rate conversion."
-PKG_LONGDESC="The SoX Resampler library performs one-dimensional sample-rate conversion. it may be used, for example, to resample PCM-encoded audio."
-PKG_BUILD_FLAGS="+pic"
+PKG_LONGDESC="The SoX Resampler library performs one-dimensional sample-rate conversion. It may be used to resample PCM-encoded audio."
+PKG_BUILD_FLAGS="-lto +pic"
 
-PKG_CMAKE_OPTS_TARGET="-DHAVE_WORDS_BIGENDIAN_EXITCODE=1 \
-                       -DBUILD_TESTS=0 \
-                       -DBUILD_EXAMPLES=1 \
-                       -DBUILD_SHARED_LIBS=OFF"
+PKG_CMAKE_OPTS_TARGET="-DBUILD_EXAMPLES=OFF \
+                       -DBUILD_SHARED_LIBS=OFF \
+                       -DBUILD_TESTS=OFF"
