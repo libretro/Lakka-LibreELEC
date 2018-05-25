fd71b45c20f51a16729cec32516f018b27373c01
Demetris Ierokipides <ierokipides.dem@gmail.com>
2018-05-25 19:45:02 +0300

    Update Retroarch & Cores to be on par with V1.7.3 stable (#468)
    * Update Retroarch & Cores to be on par with V1.7.3 stable
    https://github.com/libretro/RetroArch/releases
    
    * chailove: Update to 0.22.2

--- packages/libretro/mame2003-plus/package.mk
+++ packages/libretro/mame2003-plus/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="mame2003-plus"
-PKG_VERSION="270c968"
+PKG_VERSION="ac22277"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MAME"
