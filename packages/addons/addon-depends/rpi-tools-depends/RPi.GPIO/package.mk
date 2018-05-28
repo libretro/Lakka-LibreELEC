49c64d392b182572dff594bd81ae95ac95aedee9
MilhouseVH <milhouseVH.github@nmacleod.com>
2018-05-28 02:36:34 +0100

    RPi.GPIO: update to RPi.GPIO-0.6.3

--- packages/addons/addon-depends/rpi-tools-depends/RPi.GPIO/package.mk
+++ packages/addons/addon-depends/rpi-tools-depends/RPi.GPIO/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="RPi.GPIO"
-PKG_VERSION="0.6.2"
-PKG_SHA256="82acff0ef6bbe3cdf6f4dbdd73d96add5294bb94baf7f51c1d901861af3c2392"
+PKG_VERSION="0.6.3"
+PKG_SHA256="a5fc0eb5e401963b6c0a03650da6b42c4005f02d962b81241d96c98d0a578516"
 PKG_ARCH="arm"
 PKG_LICENSE="MIT"
 PKG_SITE="http://sourceforge.net/p/raspberry-gpio-python/"
