c8dc0bae320381024224ba4c48a6cee6a49ccbd6
Matthias Reichl <hias@horus.com>
2018-06-01 12:24:39 +0200

    fluidsynth: enable pic
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/audio/fluidsynth/package.mk
+++ packages/audio/fluidsynth/package.mk
@@ -27,6 +27,7 @@ PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="audio"
 PKG_SHORTDESC="FluidSynth: A SoundFont Synthesizer."
 PKG_LONGDESC="FluidSynth renders midi music files as raw audio data, for playing or conversion. "
+PKG_BUILD_FLAGS="+pic"
 
 PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=0 \
                        -Denable-readline=0 \
