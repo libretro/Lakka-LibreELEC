5f5ba7f2d642eab9557787a0207b1dcce6b800e3
Sascha Kuehndel (InuSasha) <dev@inusasha.de>
2018-06-09 17:13:38 +0200

    openssh/rpcbind: use internal defines, not the from obsolete glibc-rpc

--- packages/network/rpcbind/package.mk
+++ packages/network/rpcbind/package.mk
@@ -1,19 +1,20 @@
 ################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2018-present Team LibreELEC
 #      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 #
-#  OpenELEC is free software: you can redistribute it and/or modify
+#  LibreELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 2 of the License, or
 #  (at your option) any later version.
 #
-#  OpenELEC is distributed in the hope that it will be useful,
+#  LibreELEC is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
 PKG_NAME="rpcbind"
@@ -28,7 +29,8 @@ PKG_SECTION="network"
 PKG_SHORTDESC="rpcbind: a server that converts RPC program numbers into universal addresses."
 PKG_LONGDESC="The rpcbind utility is a server that converts RPC program numbers into universal addresses."
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-warmstarts \
+PKG_CONFIGURE_OPTS_TARGET="ac_cv_header_rpcsvc_mount_h=no \
+                           --disable-warmstarts \
                            --disable-libwrap \
                            --with-statedir=/tmp \
                            --with-rpcuser=root"
