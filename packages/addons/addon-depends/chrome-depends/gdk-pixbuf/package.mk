c13958f9523d5490d7803bae1ead92c1f45b8434
CvH <namerp@googlemail.com>
2018-06-21 10:56:16 +0200

    gdk-pixbuf: update to 2.36.12

---
+++ packages/addons/addon-depends/chrome-depends/gdk-pixbuf/package.mk
@@ -0,0 +1,33 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016-present Team LibreELEC
+#      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="gdk-pixbuf"
+PKG_VERSION="2.36.12"
+PKG_SHA256="fff85cf48223ab60e3c3c8318e2087131b590fd6f1737e42cb3759a3b427a334"
+PKG_ARCH="any"
+PKG_LICENSE="OSS"
+PKG_SITE="http://www.gtk.org/"
+PKG_URL="https://ftp.gnome.org/pub/gnome/sources/gdk-pixbuf/${PKG_VERSION:0:4}/gdk-pixbuf-$PKG_VERSION.tar.xz"
+PKG_DEPENDS_TARGET="toolchain glib libjpeg-turbo libpng jasper shared-mime-info tiff"
+PKG_LONGDESC="GdkPixbuf is a a GNOME library for image loading and manipulation."
+
+PKG_MESON_OPTS_TARGET="-Ddocs=false \
+                       -Dgir=false \
+                       -Dman=false \
+                       -Drelocatable=false"
