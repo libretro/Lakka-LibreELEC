6ee402080c9584bb668d5119750544b456931fdc
CvH <namerp@googlemail.com>
2018-06-21 10:56:16 +0200

    icu: update to 61.1

--- packages/addons/addon-depends/chrome-depends/icu/package.mk
+++ packages/addons/addon-depends/chrome-depends/icu/package.mk
@@ -17,24 +17,31 @@
 ################################################################################
 
 PKG_NAME="icu"
-PKG_VERSION="57.1"
-PKG_SHA256="ff8c67cb65949b1e7808f2359f2b80f722697048e90e7cfc382ec1fe229e9581"
+PKG_VERSION="61.1"
+PKG_SHA256="d007f89ae8a2543a53525c74359b65b36412fa84b3349f1400be6dcf409fafef"
 PKG_ARCH="any"
 PKG_LICENSE="Custom"
 PKG_SITE="http://www.icu-project.org"
-PKG_URL="http://download.icu-project.org/files/${PKG_NAME}4c/${PKG_VERSION}/${PKG_NAME}4c-${PKG_VERSION//./_}-src.tgz"
+PKG_URL="http://download.icu-project.org/files/icu4c/${PKG_VERSION}/icu4c-${PKG_VERSION//./_}-src.tgz"
 PKG_SOURCE_DIR="icu"
 PKG_DEPENDS_TARGET="toolchain icu:host"
-PKG_SECTION="textproc"
-PKG_SHORTDESC="International Components for Unicode library"
 PKG_LONGDESC="International Components for Unicode library"
-PKG_BUILD_FLAGS="+pic"
 
-PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared"
+PKG_ICU_OPTS="--disable-extras \
+              --disable-icuio \
+              --disable-layout \
+              --disable-renaming \
+              --disable-samples \
+              --disable-tests \
+              --disable-tools"
 
-PKG_CONFIGURE_OPTS_TARGET="--enable-static \
-                           --disable-shared \
-                           --with-cross-build=$PKG_BUILD/.$HOST_NAME"
+
+PKG_CONFIGURE_OPTS_HOST="--enable-static \
+                         --disable-shared \
+                         $PKG_ICU_OPTS"
+
+PKG_CONFIGURE_OPTS_TARGET="--with-cross-build=$PKG_BUILD/.$HOST_NAME \
+                         $PKG_ICU_OPTS"
 
 PKG_CONFIGURE_SCRIPT="source/configure"
 
