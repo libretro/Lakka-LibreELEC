34a0a6957079ff70df6a88c4b2af5c09ea9d4d13
CvH <namerp@googlemail.com>
2018-06-21 10:56:16 +0200

    unclutter: move to chrome-deps

--- packages/addons/addon-depends/chrome-depends/unclutter/package.mk
+++ packages/addons/addon-depends/chrome-depends/unclutter/package.mk
@@ -22,11 +22,9 @@ PKG_SHA256="3a53575fe2a75a34bc9a2b0ad92ee0f8a7dbedc05d8783f191c500060a40a9bd"
 PKG_ARCH="any"
 PKG_LICENSE="Public Domain"
 PKG_SITE="https://sourceforge.net/projects/unclutter/"
-PKG_URL="http://jaist.dl.sourceforge.net/project/unclutter/unclutter/source_$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_URL="https://sourceforge.net/projects/unclutter/unclutter/source_$PKG_VERSION/unclutter-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain libX11"
-PKG_SECTION="x11"
-PKG_SHORTDESC="Unclutter: Hide X11 Cursor"
-PKG_LONGDESC="Unclutter runs in the background of an X11 session and after a specified period of inactivity hides the cursor from display. When the cursor is moved its display is restored. Users may specify specific windows to be ignored by unclutter."
+PKG_LONGDESC="Unclutter runs in the background of an X11 session and hides the X11 Cursor."
 
 make_target() {
   rm -f Makefile
