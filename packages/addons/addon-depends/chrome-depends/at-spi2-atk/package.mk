57208bcc19dc2038f88fbb266ec63bcd6665adab
CvH <namerp@googlemail.com>
2018-06-21 10:56:16 +0200

    at-spi2-atk: initial package

---
+++ packages/addons/addon-depends/chrome-depends/at-spi2-atk/package.mk
@@ -0,0 +1,27 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2018-present Team LibreELEC
+#      Copyright (C) 2017 Escalade
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="at-spi2-atk"
+PKG_VERSION="2.26.2"
+PKG_ARCH="any"
+PKG_LICENSE="OSS"
+PKG_SITE="http://www.gnome.org/"
+PKG_URL="https://ftp.gnome.org/pub/gnome/sources/at-spi2-atk/${PKG_VERSION:0:4}/at-spi2-atk-$PKG_VERSION.tar.xz"
+PKG_DEPENDS_TARGET="toolchain at-spi2-core atk libX11 libxml2"
+PKG_SHORTDESC="A GTK+ module that bridges ATK to D-Bus at-spi"
