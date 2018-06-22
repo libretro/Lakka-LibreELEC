618dde699ef1016bee340f840280332e91e136a5
Jean-André Santoni <jean.andre.santoni@gmail.com>
2018-06-22 19:31:42 +0200

    Add a few settings in RA

--- packages/libretro/retroarch/package.mk
+++ packages/libretro/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="91744d9"
+PKG_VERSION="6868670"
 PKG_REV="11"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
