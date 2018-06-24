8a68ff8e56acfdc8ff87ef35421cc8706026505d
CvH <namerp@googlemail.com>
2018-06-24 10:58:08 +0200

    xcb-proto: remove unused options

--- packages/x11/proto/xcb-proto/package.mk
+++ packages/x11/proto/xcb-proto/package.mk
@@ -27,6 +27,3 @@ PKG_DEPENDS_TARGET="toolchain util-macros Python2:host"
 PKG_SECTION="x11/proto"
 PKG_SHORTDESC="xcb-proto: X C-language Bindings protocol headers"
 PKG_LONGDESC="X C-language Bindings protocol headers."
-
-# package specific configure options
-PKG_CONFIGURE_OPTS_TARGET="--without-xmlto"
