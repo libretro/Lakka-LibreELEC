3bfcd2607faf94f4ba7b085f2390d7df844a81bf
CvH <namerp@googlemail.com>
2018-07-04 14:17:23 +0200

    SDL2: enable fpic

--- packages/multimedia/SDL2/package.mk
+++ packages/multimedia/SDL2/package.mk
@@ -28,7 +28,7 @@ PKG_SECTION="multimedia"
 PKG_SHORTDESC="SDL2: A cross-platform Graphic API"
 PKG_LONGDESC="Simple DirectMedia Layer is a cross-platform multimedia library designed to provide fast access to the graphics framebuffer and audio device. It is used by MPEG playback software, emulators, and many popular games, including the award winning Linux port of 'Civilization: Call To Power.' Simple DirectMedia Layer supports Linux, Win32, BeOS, MacOS, Solaris, IRIX, and FreeBSD."
 PKG_TOOLCHAIN="configure"
-PKG_BUILD_FLAGS="-parallel"
+PKG_BUILD_FLAGS="-parallel +pic"
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-shared --enable-static \
                            --enable-libc \
