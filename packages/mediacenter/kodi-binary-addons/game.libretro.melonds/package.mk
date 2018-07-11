3221b625d2e073737f62a5cb046b6bb0336523bd
CvH <namerp@googlemail.com>
2018-07-11 21:41:39 +0200

    game.libretro.melonds: initial package

---
+++ packages/mediacenter/kodi-binary-addons/game.libretro.melonds/package.mk
@@ -0,0 +1,32 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2018-present Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="game.libretro.melonds"
+PKG_VERSION="8263fa7"
+PKG_SHA256="15df7d44cc990cbea20e9003338f8621f2961e8c5033acda6346495e5361c299"
+PKG_REV="100"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/kodi-game/game.libretro.melonds"
+PKG_URL="https://github.com/kodi-game/game.libretro.melonds/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform libretro-melonds"
+PKG_SECTION=""
+PKG_LONGDESC="game.libretro.melonds: melonDS emulator for Kodi"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="kodi.gameclient"
