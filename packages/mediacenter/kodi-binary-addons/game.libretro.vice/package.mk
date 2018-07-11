7a88b9319ce703b28f89b844187d85f5c52390ba
CvH <namerp@googlemail.com>
2018-07-11 21:41:39 +0200

    game.libretro.vice: initial package

---
+++ packages/mediacenter/kodi-binary-addons/game.libretro.vice/package.mk
@@ -0,0 +1,32 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2018-present Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="game.libretro.vice"
+PKG_VERSION="ae89cda"
+PKG_SHA256="57fa261c6552b7ebc96e54ff50c14ce89a2fd1137a2780f87205e27a82a745a8"
+PKG_REV="100"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/kodi-game/game.libretro.vice"
+PKG_URL="https://github.com/kodi-game/game.libretro.vice/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform libretro-vice"
+PKG_SECTION=""
+PKG_LONGDESC="game.libretro.vice: vice emulator for Kodi"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="kodi.gameclient"
