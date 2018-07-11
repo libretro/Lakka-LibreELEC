cae64256b6b86abab2594d67e8c8c9d5e637784f
CvH <namerp@googlemail.com>
2018-07-11 21:41:39 +0200

    game.libretro.fsuae: initial package

---
+++ packages/mediacenter/kodi-binary-addons/game.libretro.fsuae/package.mk
@@ -0,0 +1,31 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2018-present Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="game.libretro.fsuae"
+PKG_VERSION="aaae96a"
+PKG_SHA256="f2dd360ab5504f38ab3e75985fcf1bf9c7eeaf30dc37c7fb0eefd93850dd09e8"
+PKG_REV="100"
+PKG_ARCH="x86_64"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/kodi-game/game.libretro.fsuae"
+PKG_URL="https://github.com/kodi-game/game.libretro.fsuae/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform libretro-fsuae"
+PKG_LONGDESC="game.libretro.fsuae: fsuae emulator for Kodi"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="kodi.gameclient"
