3ecb6b8bb5207f7e4e97279ece3c2e65a7ffa015
CvH <namerp@googlemail.com>
2018-07-11 21:41:39 +0200

    game.libretro.pokemini: initial package

---
+++ packages/mediacenter/kodi-binary-addons/game.libretro.pokemini/package.mk
@@ -0,0 +1,32 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2018-present Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="game.libretro.pokemini"
+PKG_VERSION="84a1066"
+PKG_SHA256="f8ba6e469fde8f354181e7802d89fbe99aad377315cf116760dda4a0a335ed32"
+PKG_REV="100"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/kodi-game/game.libretro.pokemini"
+PKG_URL="https://github.com/kodi-game/game.libretro.pokemini/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform libretro-pokemini"
+PKG_SECTION=""
+PKG_LONGDESC="game.libretro.pokemini: PokeMini emulator for Kodi"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="kodi.gameclient"
