dc668dd0e231e8faebf6231539d4cc5be89d77c7
CvH <namerp@googlemail.com>
2018-07-11 21:41:39 +0200

    libretro-mame: fixed build

--- packages/emulation/libretro-mame/package.mk
+++ packages/emulation/libretro-mame/package.mk
@@ -19,23 +19,35 @@
 PKG_NAME="libretro-mame"
 PKG_VERSION="3f5b1456"
 PKG_SHA256="6814be8a249bef7bd847ba51ef5f10c94ca3b8d996b772d5ef4ee7304bd31b3f"
-PKG_ARCH="x86_64"
+PKG_ARCH="x86_64 arm"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/mame"
 PKG_URL="https://github.com/libretro/mame/archive/$PKG_VERSION.tar.gz"
 PKG_SOURCE_DIR="mame-$PKG_VERSION*"
 PKG_DEPENDS_TARGET="toolchain kodi-platform"
 PKG_SECTION="emulation"
-PKG_SHORTDESC="game.libretro.mame: MAME for Kodi"
 PKG_LONGDESC="game.libretro.mame: MAME for Kodi"
-PKG_BUILD_FLAGS="-gold"
+PKG_BUILD_FLAGS="-gold -lto"
+PKG_TOOLCHAIN="make"
 
-PKG_LIBNAME="mame_libretro.so"
+PKG_LIBNAME="mamearcade_libretro.so"
 PKG_LIBPATH="$PKG_LIBNAME"
 PKG_LIBVAR="MAME_LIB"
 
 make_target() {
-  make -f Makefile.libretro
+  PTR64="0"
+  NOASM="0"
+
+  if [ "$ARCH" == "arm" ]; then
+    NOASM="1"
+  elif [ "$ARCH" == "x86_64" ]; then
+    PTR64="1"
+  fi
+
+  make REGENIE=1 VERBOSE=1 NOWERROR=1 PYTHON_EXECUTABLE=python2 CONFIG=libretro \
+       LIBRETRO_OS="unix" ARCH="" PROJECT="" LIBRETRO_CPU="$ARCH" DISTRO="debian-stable" \
+       CC="$CC" CXX="$CXX" LD="$LD" CROSS_BUILD="" PTR64="$PTR64" TARGET="mame" \
+       SUBTARGET="arcade" PLATFORM="$ARCH" RETRO=1 OSD="retro"
 }
 
 makeinstall_target() {
