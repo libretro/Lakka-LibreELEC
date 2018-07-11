094b0b6b4c570284d0c26f5cf678c9e9428b01b6
CvH <namerp@googlemail.com>
2018-07-11 21:41:39 +0200

    game.libretro.vram-test: initial package

---
+++ packages/mediacenter/kodi-binary-addons/game.libretro.vram-test/package.mk
@@ -0,0 +1,32 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2018-present Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="game.libretro.vram-test"
+PKG_VERSION="0c29c7b"
+PKG_SHA256="ece792fb35a7bf1cbf9c07c335149a5f05d92181289612b121fd529204e16278"
+PKG_REV="100"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/kodi-game/game.libretro.vram-test"
+PKG_URL="https://github.com/kodi-game/game.libretro.vram-test/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform libretro-vram-test"
+PKG_SECTION=""
+PKG_LONGDESC="game.libretro.vram-test: vram-test emulator for Kodi"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="kodi.gameclient"
