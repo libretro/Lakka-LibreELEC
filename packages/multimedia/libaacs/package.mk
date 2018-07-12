c5a0437bb01ad28007d8caa1f65f88ae37cf7f18
MilhouseVH <milhouseVH.github@nmacleod.com>
2018-07-12 07:24:08 +0100

    libaacs: stop using ftp server with duff cert, bump to 0.9.0

--- packages/multimedia/libaacs/package.mk
+++ packages/multimedia/libaacs/package.mk
@@ -17,13 +17,12 @@
 ################################################################################
 
 PKG_NAME="libaacs"
-PKG_VERSION="9da2b68"
-PKG_SHA256="a0596728724f6a8b5bc43f6dde3f8e87f5ffa9876c268518bd4332e029558b13"
+PKG_VERSION="0.9.0"
+PKG_SHA256="47e0bdc9c9f0f6146ed7b4cc78ed1527a04a537012cf540cf5211e06a248bace"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.videolan.org/developers/libaacs.html"
-PKG_URL="ftp://ftp.videolan.org/pub/videolan/libaacs/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.bz2"
-PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_URL="http://download.videolan.org/pub/videolan/libaacs/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain libgcrypt"
 PKG_SECTION="multimedia"
 PKG_SHORTDESC="libaacs: a research project to implement the Advanced Access Content System specification."
