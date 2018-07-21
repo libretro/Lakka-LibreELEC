d7f2091b31bbd67bcda2c73da7903146d72ff9fa
CvH <namerp@googlemail.com>
2018-07-21 16:54:38 +0200

    pvr.sledovanitv.cz: initial package

---
+++ packages/mediacenter/kodi-binary-addons/pvr.sledovanitv.cz/package.mk
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="pvr.sledovanitv.cz"
+PKG_VERSION="22d60d2"
+PKG_SHA256="b6b9f326d6250a96d6ad91e1842e25a1061827961473ed2d554009c5476330ae"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://kodi.tv"
+PKG_URL="https://github.com/palinek/pvr.sledovanitv.cz/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform jsoncpp"
+PKG_SECTION=""
+PKG_SHORTDESC="pvr.sledovanitv.cz"
+PKG_LONGDESC="pvr.sledovanitv.cz"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="xbmc.pvrclient"
