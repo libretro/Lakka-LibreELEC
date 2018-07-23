89f6831fed090e6918baaf8bf451061cf65e3406
natinusala <natinusala@gmail.com>
2018-07-23 14:34:46 +0200

    Added workaround build script for Switch

---
+++ build_switch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+export DISTRO="Lakka"
+export PROJECT="Switch"
+export ARCH="aarch64"
+
+build_target() {
+  cd "$PROJECT_DIR" && make image $MAKE_OPTS
+}
+
+LOGFILE="$(mktemp)"
+build_target 2> >(tee "$LOGFILE" >&2)
+RC=${PIPESTATUS[0]}
+if [[ $RC != 0 ]]; then
+  if grep -qF 'fatal error: asm/barrier.h: No such file or directory' "$LOGFILE"; then
+    echo "*** asm/barrier.h bug detected; deleting the linux build artifact ***"
+    find "$PROJECT_DIR" -maxdepth 2 -type d -path "$PROJECT_DIR/build.$DISTRO-$PROJECT.$ARCH-*-devel/linux-*" -print -exec rm -rf {} \;
+    build_target
+    RC=$?
+  fi
+fi
+
+exit $RC
+
