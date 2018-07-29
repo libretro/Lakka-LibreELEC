551d314b9c417794df0ffcb5ce83620345264a3c
CvH <namerp@googlemail.com>
2018-07-29 11:14:06 +0200

    keyutils: add fpic

--- packages/sysutils/keyutils/package.mk
+++ packages/sysutils/keyutils/package.mk
@@ -12,6 +12,7 @@ PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="system"
 PKG_SHORTDESC="keyutils: Linux Key Management Utilities"
 PKG_LONGDESC="Keyutils is a set of utilities for managing the key retention facility in the kernel."
+PKG_BUILD_FLAGS="+pic"
 
 PKG_MAKE_OPTS_TARGET="NO_ARLIB=0 NO_SOLIB=1 BINDIR=/usr/bin SBINDIR=/usr/sbin LIBDIR=/usr/lib USRLIBDIR=/usr/lib"
 PKG_MAKEINSTALL_OPTS_TARGET="$PKG_MAKE_OPTS_TARGET"
