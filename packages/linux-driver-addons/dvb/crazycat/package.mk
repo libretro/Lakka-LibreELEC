94b74574415e0d97f456d61d02bc1cdb1bae9031
CvH <namerp@googlemail.com>
2018-08-01 14:09:21 +0200

    crazycat: change to embedded addon

--- packages/linux-driver-addons/dvb/crazycat/package.mk
+++ packages/linux-driver-addons/dvb/crazycat/package.mk
@@ -13,7 +13,7 @@ PKG_NEED_UNPACK="$LINUX_DEPENDS"
 PKG_SECTION="driver.dvb"
 PKG_LONGDESC="DVB driver for TBS cards with CrazyCats additions."
 
-PKG_IS_ADDON="yes"
+PKG_IS_ADDON="embedded"
 PKG_IS_KERNEL_PKG="yes"
 PKG_ADDON_IS_STANDALONE="yes"
 PKG_ADDON_NAME="DVB drivers for TBS (CrazyCat)"
