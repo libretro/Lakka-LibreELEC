8c3a7e6e3cea81788f13d6d40e3f2628940e7e58
gouchi <gouchi@free.fr>
2018-08-11 06:47:56 +0200

    mesen : add mesen libretro core (#517)

---
+++ packages/libretro/mesen/package.mk
@@ -0,0 +1,50 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.tv; see the file COPYING.  If not, write to
+#  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
+#  http://www.gnu.org/copyleft/gpl.html
+################################################################################
+
+PKG_NAME="mesen"
+PKG_VERSION="1aef0c3"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://github.com/SourMesen/Mesen"
+PKG_URL="https://github.com/SourMesen/Mesen/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="libretro"
+PKG_SHORTDESC="Mesen is a cross-platform (Windows & Linux) NES/Famicom emulator built in C++ and C#"
+PKG_LONGDESC="Mesen is a cross-platform (Windows & Linux) NES/Famicom emulator built in C++ and C#"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+post_unpack() {
+  mv $BUILD/Mesen-$PKG_VERSION* $BUILD/$PKG_NAME-$PKG_VERSION
+}
+
+make_target() {
+  make -C Libretro/
+}
+
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/libretro
+  cp Libretro/mesen_libretro.so $INSTALL/usr/lib/libretro/
+}
+
