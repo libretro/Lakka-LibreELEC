4c7c97605d3fd848d96e63a1a656dfad7600ef35
MilhouseVH <milhouseVH.github@nmacleod.com>
2018-08-13 20:21:11 +0100

    busybox: update to busybox-1.29.2

--- packages/sysutils/busybox/package.mk
+++ packages/sysutils/busybox/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="busybox"
-PKG_VERSION="1.28.1"
-PKG_SHA256="98fe1d3c311156c597cd5cfa7673bb377dc552b6fa20b5d3834579da3b13652e"
+PKG_VERSION="1.29.2"
+PKG_SHA256="67d2fa6e147a45875fe972de62d907ef866fe784c495c363bf34756c444a5d61"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.busybox.net"
