4bbfdea84d33d797fe941d6ef58d1f4f85f0a144
MilhouseVH <milhouseVH.github@nmacleod.com>
2018-08-14 15:19:12 +0100

    xorg-server: update to xorg-server-1.20.1

--- packages/x11/xserver/xorg-server/package.mk
+++ packages/x11/xserver/xorg-server/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="xorg-server"
-PKG_VERSION="1.20.0"
-PKG_SHA256="9d967d185f05709274ee0c4f861a4672463986e550ca05725ce27974f550d3e6"
+PKG_VERSION="1.20.1"
+PKG_SHA256="59c99fe86fe75b8164c6567bfc6e982aecc2e4a51e6fbac1b842d5d00549e918"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.X.org"
