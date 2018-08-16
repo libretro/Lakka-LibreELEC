afc62c331e0a3bbf97609deb5afdda4aba3f9c3d
natinusala <natinusala@gmail.com>
2018-08-16 17:17:35 +0200

    New core : dosbox-svn

--- packages/libretro/dosbox-svn/package.mk
+++ packages/libretro/dosbox-svn/package.mk
@@ -18,28 +18,41 @@
 #  http://www.gnu.org/copyleft/gpl.html
 ################################################################################
 
-PKG_NAME="core-info"
-PKG_VERSION="da2d678"
+PKG_NAME="dosbox-svn"
+PKG_VERSION="b3856be"
 PKG_REV="1"
 PKG_ARCH="any"
-PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/libretro/libretro-super"
-PKG_GIT_URL="$PKG_SITE"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/fr500/dosbox-svn"
+PKG_URL="https://github.com/fr500/dosbox-svn/archive/$PKG_VERSION.zip"
+PKG_DEPENDS_TARGET="toolchain SDL SDL_net"
 PKG_PRIORITY="optional"
 PKG_SECTION="libretro"
-PKG_SHORTDESC="Info files for libretro cores"
-PKG_LONGDESC="Super repo for other libretro projects. Fetches, builds and installs."
+PKG_SHORTDESC="Upstream port of DOSBox to libretro"
+PKG_LONGDESC="Upstream port of DOSBox to libretro"
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
+pre_configure_target() {
+  strip_lto
+}
+
 make_target() {
-  :
+  if [ "$ARCH" = "aarch64" ]; then
+    make -C libretro target=arm64 WITH_EMBEDDED_SDL=0
+  elif [ "$ARCH" = "arm" ]; then
+    make -C libretro target=arm WITH_EMBEDDED_SDL=0
+  elif [ "$ARCH" = "x86_64" ]; then
+    make -C libretro target=x86_64 WITH_EMBEDDED_SDL=0
+  elif [ "$ARCH" = "i386" ]; then 
+    make -C libretro target=x86 WITH_EMBEDDED_SDL=0
+  else
+    make -C libretro WITH_EMBEDDED_SDL=0
+  fi
 }
 
 makeinstall_target() {
   mkdir -p $INSTALL/usr/lib/libretro
-  cp dist/info/*.info $INSTALL/usr/lib/libretro/
+  cp $PKG_BUILD/libretro/dosbox_svn_libretro.so $INSTALL/usr/lib/libretro
 }
-
