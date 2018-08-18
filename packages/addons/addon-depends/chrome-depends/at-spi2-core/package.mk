2fcbceb5d8667a1a401dfdbed2d99b0629c9763f
MilhouseVH <milhouseVH.github@nmacleod.com>
2018-08-18 09:20:06 +0100

    various: more missing checksums

--- packages/addons/addon-depends/chrome-depends/at-spi2-core/package.mk
+++ packages/addons/addon-depends/chrome-depends/at-spi2-core/package.mk
@@ -4,6 +4,7 @@
 
 PKG_NAME="at-spi2-core"
 PKG_VERSION="2.28.0"
+PKG_SHA256="42a2487ab11ce43c288e73b2668ef8b1ab40a0e2b4f94e80fca04ad27b6f1c87"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.gnome.org/"
--- packages/addons/addon-depends/chrome-depends/libxss/package.mk
+++ packages/addons/addon-depends/chrome-depends/libxss/package.mk
@@ -3,6 +3,7 @@
 
 PKG_NAME="libxss"
 PKG_VERSION="1.2.2"
+PKG_SHA256="8ff1efa7341c7f34bcf9b17c89648d6325ddaae22e3904e091794e0b4426ce1d"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.X.org"
