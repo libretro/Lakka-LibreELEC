756d5e9a51e56effc403352446059af57402914c
CvH <namerp@googlemail.com>
2018-08-21 11:02:53 +0200

    dvb-firmware: update to 1.3.0

--- packages/linux-firmware/dvb-firmware/package.mk
+++ packages/linux-firmware/dvb-firmware/package.mk
@@ -3,18 +3,19 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="dvb-firmware"
-PKG_VERSION="1.2.2"
-PKG_SHA256="2e39a0a85db54de7f38bd8e522e48445257f4edefe61d1e264079b0c45bc1fcc"
+PKG_VERSION="1.3.0"
+PKG_SHA256="0a0e64a759b4597d2c31209db0cc6458b658799e93dcdd0ddb58220fc6cbf393"
 PKG_ARCH="any"
 PKG_LICENSE="Free-to-use"
 PKG_SITE="https://github.com/LibreELEC/dvb-firmware"
 PKG_URL="https://github.com/LibreELEC/dvb-firmware/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="firmware"
-PKG_SHORTDESC="dvb-firmware: firmwares for various DVB drivers"
 PKG_LONGDESC="dvb-firmware: firmwares for various DVB drivers"
 PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
-  DESTDIR=$INSTALL/$(get_kernel_overlay_dir) ./install
+  PKG_FW_DIR="$INSTALL/$(get_kernel_overlay_dir)/lib/firmware"
+  mkdir -p "$PKG_FW_DIR"
+    cp -a "$PKG_BUILD/firmware/"* "$PKG_FW_DIR"
 }
