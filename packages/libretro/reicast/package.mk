cd92fe94eecbe4644a17ba28496d09025c59b43f
Jean-André Santoni <jean.andre.santoni@gmail.com>
2018-08-23 15:02:41 +0700

    Update reicast, fix compilation for ARM (#523)
    * Update reicast, fix compilation for ARM
    
    * Fix x86_64
    
    * Fix i386

--- packages/libretro/reicast/package.mk
+++ packages/libretro/reicast/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="reicast"
-PKG_VERSION="03d3cc3"
+PKG_VERSION="689e21a"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
@@ -36,9 +36,9 @@ PKG_AUTORECONF="no"
 
 make_target() {
   if [ "$ARCH" == "arm" ]; then
-    make platform=rpi FORCE_GLES=1
+    make platform=rpi FORCE_GLES=1 HAVE_OPENMP=0
   else
-    make AS=${AS} CC_AS=${AS}
+    make AS=${AS} CC_AS=${AS} ARCH=${ARCH} HAVE_OPENMP=0
   fi
 }
 
