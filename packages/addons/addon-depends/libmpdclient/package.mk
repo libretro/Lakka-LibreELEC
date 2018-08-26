8429a54ba0c0686b0c3713bade33d3a5429e7bc1
CvH <namerp@googlemail.com>
2018-08-26 22:42:29 +0200

    libmpdclient: update to 2.14

--- packages/addons/addon-depends/libmpdclient/package.mk
+++ packages/addons/addon-depends/libmpdclient/package.mk
@@ -2,13 +2,11 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libmpdclient"
-PKG_VERSION="2.13"
-PKG_SHA256="e1583fbcae89ad543a6cc44caeaf16b31e709831f5b7eac4413f78c54a21db95"
+PKG_VERSION="2.14"
+PKG_SHA256="0a84e2791bfe3077cf22ee1784c805d5bb550803dffe56a39aa3690a38061372"
 PKG_ARCH="any"
 PKG_LICENSE="BSD-3c"
 PKG_SITE="https://www.musicpd.org"
-PKG_URL="https://github.com/MusicPlayerDaemon/libmpdclient/archive/v${PKG_VERSION}.tar.gz"
+PKG_URL="https://www.musicpd.org/download/libmpdclient/2/libmpdclient-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_SECTION="service"
-PKG_SHORTDESC="is a C library which implements the Music Player Daemon protocol"
 PKG_LONGDESC="is a C library which implements the Music Player Daemon protocol"
