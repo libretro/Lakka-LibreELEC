6ca28e5556ac5786fe7580f9c1e8e83fb3a93e2e
CvH <namerp@googlemail.com>
2018-08-26 17:25:32 +0200

    dvb-latest: initial package

---
+++ packages/linux-driver-addons/dvb/dvb-latest/package.mk
@@ -0,0 +1,40 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="dvb-latest"
+PKG_VERSION="baf45935ffad914f33faf751ad9f4d0dd276c021"
+PKG_SHA256="591f0c4c039f4b2453982187447232fa2e72a48dfd915ed307ae11c7a6427031"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://git.linuxtv.org/media_build.git"
+PKG_URL="https://git.linuxtv.org/media_build.git/snapshot/${PKG_VERSION}.tar.gz"
+PKG_SOURCE_DIR="${PKG_VERSION}"
+PKG_DEPENDS_TARGET="toolchain linux media_tree"
+PKG_NEED_UNPACK="$LINUX_DEPENDS media_tree"
+PKG_SECTION="driver.dvb"
+PKG_LONGDESC="DVB drivers from the latest kernel (media_build)"
+
+PKG_IS_ADDON="embedded"
+PKG_IS_KERNEL_PKG="yes"
+PKG_ADDON_IS_STANDALONE="yes"
+PKG_ADDON_NAME="DVB drivers from the latest kernel"
+PKG_ADDON_TYPE="xbmc.service"
+PKG_ADDON_VERSION="${ADDON_VERSION}.${PKG_REV}"
+
+pre_make_target() {
+  export KERNEL_VER=$(get_module_dir)
+  export LDFLAGS=""
+}
+
+make_target() {
+  cp -RP $(get_build_dir media_tree)/* $PKG_BUILD/linux
+
+  # make staging config (all + experimental)
+  kernel_make VER=$KERNEL_VER SRCDIR=$(kernel_path) stagingconfig
+
+  kernel_make VER=$KERNEL_VER SRCDIR=$(kernel_path)
+}
+
+makeinstall_target() {
+  install_driver_addon_files "$PKG_BUILD/v4l/"
+}
