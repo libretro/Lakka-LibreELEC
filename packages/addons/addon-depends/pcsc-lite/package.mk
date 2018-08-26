84c412c9b1233cb3f982197a01229f277233999c
CvH <namerp@googlemail.com>
2018-08-26 19:02:02 +0200

    pcsc-lite: update to 1.8.23

--- packages/addons/addon-depends/pcsc-lite/package.mk
+++ packages/addons/addon-depends/pcsc-lite/package.mk
@@ -2,15 +2,13 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pcsc-lite"
-PKG_VERSION="1.8.22"
-PKG_SHA256="6a358f61ed3b66a7f6e1f4e794a94c7be4c81b7a58ec360c33791e8d7d9bd405"
+PKG_VERSION="1.8.23"
+PKG_SHA256="5a27262586eff39cfd5c19aadc8891dd71c0818d3d629539bd631b958be689c9"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE="http://pcsclite.alioth.debian.org/pcsclite.html"
-PKG_URL="https://alioth.debian.org/frs/download.php/latestfile/39/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
+PKG_SITE="https://pcsclite.apdu.fr"
+PKG_URL="https://pcsclite.apdu.fr/files/pcsc-lite-${PKG_VERSION}.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain libusb"
-PKG_SECTION="service/system"
-PKG_SHORTDESC="Middleware to access a smart card using SCard API (PC/SC)"
 PKG_LONGDESC="Middleware to access a smart card using SCard API (PC/SC)"
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
