968e288066748d897df5f9df79a0f280a47d8d9f
MilhouseVH <milhouseVH.github@nmacleod.com>
2018-08-30 19:36:52 +0100

    RTL8192DU: update to RTL8192DU-31a276d

--- packages/linux-drivers/RTL8192DU/package.mk
+++ packages/linux-drivers/RTL8192DU/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="RTL8192DU"
-PKG_VERSION="a9deaec"
-PKG_SHA256="af71aa79c0382f056f2c7091b45a3c2d79b9c2848eb6bb1ffed22b6f0a63cbfd"
+PKG_VERSION="31a276df70c7cdbb534cf29de31cfc2d59c71eb8"
+PKG_SHA256="9dd97f3fbfc31714d8b2cba4966051371f9c0515cff78da13d6e2c98506ac888"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/lwfinger/rtl8192du"
 PKG_URL="https://github.com/lwfinger/rtl8192du/archive/$PKG_VERSION.tar.gz"
