8939013f2159723951370718ceeb6dd950d93b6a
CvH <namerp@googlemail.com>
2018-09-10 21:58:05 +0200

    qemu: disable gcrypt

--- packages/tools/qemu/package.mk
+++ packages/tools/qemu/package.mk
@@ -9,22 +9,21 @@ PKG_LICENSE="GPL"
 PKG_SITE="http://wiki.qemu.org"
 PKG_URL="https://download.qemu.org/qemu-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_HOST="toolchain glib:host pixman:host Python2:host zlib:host"
-PKG_SECTION="tools"
-PKG_SHORTDESC="QEMU is a generic and open source machine emulator and virtualizer."
 PKG_LONGDESC="QEMU is a generic and open source machine emulator and virtualizer."
 
-HOST_CONFIGURE_OPTS="--prefix=$TOOLCHAIN \
-  --bindir=$TOOLCHAIN/bin \
-  --sbindir=$TOOLCHAIN/sbin \
-  --sysconfdir=$TOOLCHAIN/etc \
-  --libexecdir=$TOOLCHAIN/lib \
-  --localstatedir=$TOOLCHAIN/var \
+HOST_CONFIGURE_OPTS="--bindir=$TOOLCHAIN/bin \
   --extra-cflags=-I$TOOLCHAIN/include \
   --extra-ldflags=-L$TOOLCHAIN/lib \
+  --libexecdir=$TOOLCHAIN/lib \
+  --localstatedir=$TOOLCHAIN/var \
+  --prefix=$TOOLCHAIN \
+  --sbindir=$TOOLCHAIN/sbin \
   --static \
-  --disable-vnc \
-  --disable-werror \
+  --sysconfdir=$TOOLCHAIN/etc \
   --disable-blobs \
+  --disable-docs \
+  --disable-gcrypt \
   --disable-system \
   --disable-user \
-  --disable-docs"
+  --disable-vnc \
+  --disable-werror"
