a6506aa9913257d7088321b3d3bc4d946a8288b4
CvH <namerp@googlemail.com>
2018-09-11 12:52:14 +0200

    liblognorm: update to 2.0.5

--- packages/addons/addon-depends/rsyslog-depends/liblognorm/package.mk
+++ packages/addons/addon-depends/rsyslog-depends/liblognorm/package.mk
@@ -2,17 +2,17 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="liblognorm"
-PKG_VERSION="2.0.1"
-PKG_SHA256="6a7fda0da2791a87c808fbfde1af20e6463e3ff73496aae7756e3440f3bc5b75"
+PKG_VERSION="2.0.5"
+PKG_SHA256="dd779b6992de37995555e1d54caf0716a694765efc65480eed2c713105ab46fe"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE="http://www.liblognorm.com/"
-PKG_URL="http://www.liblognorm.com/files/download/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SITE="http://www.liblognorm.com"
+PKG_URL="https://github.com/rsyslog/liblognorm/archive/v$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_SECTION="rsyslog"
-PKG_SHORTDESC="liblognorm"
-PKG_LONGDESC="liblognorm"
+PKG_TOOLCHAIN="autotools"
+PKG_LONGDESC="a fast samples-based log normalization library"
 
-PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared \
+PKG_CONFIGURE_OPTS_TARGET="--enable-static \
+                           --disable-shared \
                            ac_cv_func_malloc_0_nonnull=yes \
                            ac_cv_func_realloc_0_nonnull=yes"
