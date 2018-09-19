e6698bc26825cc8730a616b94144bc3d0ad05c81
Matthias Reichl <hias@horus.com>
2018-09-19 21:30:14 +0200

    game.libretro.mame2003: switch to upstream repo
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/mediacenter/kodi-binary-addons/game.libretro.mame2003/package.mk
+++ packages/mediacenter/kodi-binary-addons/game.libretro.mame2003/package.mk
@@ -2,15 +2,15 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="game.libretro.mame2003"
-PKG_VERSION="7e63b8e"
-PKG_SHA256="96191832c294c0f63d20ae2ff975fb5f0db73520e25daa1ce44f8dcf06fadc59"
-PKG_REV="101"
-PKG_ARCH="none"
+PKG_VERSION="12f436d323c17349152d417cf7b7467347b29ebe"
+PKG_SHA256="0156bc26bd6d752f1726d0ee06f3c6f213c46437d8267a321f332ceebefc2bd8"
+PKG_REV="100"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/kodi-game/game.libretro.mame"
+PKG_SITE="https://github.com/kodi-game/game.libretro.mame2003"
+PKG_URL="https://github.com/kodi-game/game.libretro.mame2003/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain kodi-platform libretro-mame2003"
 PKG_SECTION=""
-PKG_LONGDESC="game.libretro.mame2003: MAME emulator for Kodi"
+PKG_LONGDESC="game.libretro.mame2003: MAME 2003 emulator for Kodi"
 
 PKG_IS_ADDON="yes"
 PKG_ADDON_TYPE="kodi.gameclient"
