bbd9cee0f88f9062c7d9ef732c6a0371a4c65393
Matthias Reichl <hias@horus.com>
2018-09-19 21:31:06 +0200

    game.libretro.mame2000: switch to upstream repo
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/mediacenter/kodi-binary-addons/game.libretro.mame2000/package.mk
+++ packages/mediacenter/kodi-binary-addons/game.libretro.mame2000/package.mk
@@ -2,15 +2,15 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="game.libretro.mame2000"
-PKG_VERSION="7e63b8e"
-PKG_SHA256="96191832c294c0f63d20ae2ff975fb5f0db73520e25daa1ce44f8dcf06fadc59"
-PKG_REV="101"
-PKG_ARCH="none"
+PKG_VERSION="c20f41ffb3c66f3b1cd8b568951df4ed7fb9fc70"
+PKG_SHA256="6b59f5e4249afedf589002c63e40e2c46d7c6f25541128d33166b14849b64de4"
+PKG_REV="100"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/kodi-game/game.libretro.mame"
+PKG_SITE="https://github.com/kodi-game/game.libretro.mame2000"
+PKG_URL="https://github.com/kodi-game/game.libretro.mame2000/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain kodi-platform libretro-mame2000"
 PKG_SECTION=""
-PKG_LONGDESC="game.libretro.mame2000: MAME emulator for Kodi"
+PKG_LONGDESC="game.libretro.mame2000: MAME 2000 emulator for Kodi"
 
 PKG_IS_ADDON="yes"
 PKG_ADDON_TYPE="kodi.gameclient"
