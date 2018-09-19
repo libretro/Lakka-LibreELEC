8c53b2da2149f75e21663d8cc5994927b9cfb8e3
Matthias Reichl <hias@horus.com>
2018-09-19 23:12:40 +0200

    game.libretro.mame2010: switch to upstream repo
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/mediacenter/kodi-binary-addons/game.libretro.mame2010/package.mk
+++ packages/mediacenter/kodi-binary-addons/game.libretro.mame2010/package.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="game.libretro.mame2010"
-PKG_VERSION="7e63b8e"
-PKG_SHA256="96191832c294c0f63d20ae2ff975fb5f0db73520e25daa1ce44f8dcf06fadc59"
-PKG_REV="101"
-PKG_ARCH="none"
+PKG_VERSION="3f8772c912520d3331782ca38216e7e5d6c23845"
+PKG_SHA256="b7c72ff30ce4a7538f6fbb81945627f9e056fdc1e76447c1e3c13d9f0d1e6968"
+PKG_REV="100"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/kodi-game/game.libretro.mame"
+PKG_SITE="https://github.com/kodi-game/game.libretro.mame2010"
+PKG_URL="https://github.com/kodi-game/game.libretro.mame2010/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain kodi-platform libretro-mame2010"
 PKG_SECTION=""
 PKG_LONGDESC="game.libretro.mame2010: MAME emulator for Kodi"
