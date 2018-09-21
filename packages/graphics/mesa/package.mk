6aa3b52e3b3d8cc863dfaa07d632b2dfc8c23265
MilhouseVH <milhouseVH.github@nmacleod.com>
2018-09-21 14:35:45 +0100

    mesa: update to mesa-18.2.1

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa"
-PKG_VERSION="18.2.0"
-PKG_SHA256="22452bdffff8e11bf4284278155a9f77cb28d6d73a12c507f1490732d0d9ddce"
+PKG_VERSION="18.2.1"
+PKG_SHA256="9636dc6f3d188abdcca02da97cedd73640d9035224efd5db724187d062c81056"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
