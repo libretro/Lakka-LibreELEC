8ab5c1e1497bf902716d3590bd37adac786e560e
Rob Loach <robloach@gmail.com>
2018-09-26 07:40:50 -0400

    chailove: Update to ChaiLove 0.27.0

--- packages/libretro/chailove/package.mk
+++ packages/libretro/chailove/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="chailove"
-PKG_VERSION="268d940"
+PKG_VERSION="6ba89d2"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
