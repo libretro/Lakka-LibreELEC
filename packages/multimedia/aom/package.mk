9d6d4029b54212538ff36edaf01d6dbac5bdd11f
CvH <namerp@googlemail.com>
2018-10-02 17:26:44 +0200

    aom: update to 51b282d

--- packages/multimedia/aom/package.mk
+++ packages/multimedia/aom/package.mk
@@ -2,14 +2,12 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="aom"
-PKG_VERSION="96ee0eb"
-PKG_SHA256="37c8d930cc105ccad4987c65751400fc42819b77fd487c9ef19cadee0c95a2d8"
-PKG_ARCH="any"
+PKG_VERSION="51b282d13d5ee58dadf45164fc49a6e034333478"
+PKG_SHA256="15a2f7960da99f865bd4da1e60b110e2220115851d60a1bb042465ff99c0bc83"
 PKG_LICENSE="BSD"
 PKG_SITE="https://www.webmproject.org"
 PKG_URL="http://repo.or.cz/aom.git/snapshot/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_SECTION="multimedia"
 PKG_LONGDESC="AV1 Codec Library"
 
 PKG_CMAKE_OPTS_TARGET="-DENABLE_CCACHE=1 \
