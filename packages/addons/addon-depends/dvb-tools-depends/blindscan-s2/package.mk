1edbc53f7f695552b4b79c23174b05679ddb2093
CvH <namerp@googlemail.com>
2018-10-02 17:43:40 +0200

    blindscan-s2: update to 192eaf0

--- packages/addons/addon-depends/dvb-tools-depends/blindscan-s2/package.mk
+++ packages/addons/addon-depends/dvb-tools-depends/blindscan-s2/package.mk
@@ -2,15 +2,12 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="blindscan-s2"
-PKG_VERSION="94cb626"
-PKG_SHA256="cea70a7c4f3091530fd3cdf6e79e1c7386928678ef89fa5d937e5092c7cf9522"
-PKG_ARCH="any"
+PKG_VERSION="192eaf0b3eccfb9721b3d49b9010b5c1447776e0"
+PKG_SHA256="c9e5bd07672765b30dac758844e2eb87ab68d5dc6bc731e9df6394159acddcbb"
 PKG_LICENSE="GPL"
 PKG_SITE="https://bitbucket.org/majortom/blindscan-s2"
-PKG_URL="https://github.com/persianpros/blindscan-s2/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/PLi-metas/blindscan-s2/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_SECTION="tools"
-PKG_SHORTDESC="blindscan-s2 is a program to blindscan digital satellite signals"
 PKG_LONGDESC="blindscan-s2 is a program to blindscan digital satellite signals"
 
 makeinstall_target() {
