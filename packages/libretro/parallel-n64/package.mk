4ad977d94d498f012a09594de8605f36d9389a4b
natinusala <natinusala@gmail.com>
2018-10-02 20:23:37 +0200

    parallel-n64: support 32bit arm platforms with opengl

--- packages/libretro/parallel-n64/package.mk
+++ packages/libretro/parallel-n64/package.mk
@@ -51,8 +51,13 @@ make_target() {
     LDFLAGS="$LDFLAGS -lpthread"
     make WITH_DYNAREC=$DYNAREC HAVE_PARALLEL=1 HAVE_OPENGL=0
   elif [[ "$TARGET_FPU" =~ "neon" ]]; then
+    if [ "$OPENGLES_SUPPORT" = "yes" ]; then
+      P64GLES="-gles"
+    else
+      P64GLES=""
+    fi
     CFLAGS="$CFLAGS -DGL_BGRA_EXT=0x80E1" # Fix build for platforms where GL_BGRA_EXT is not defined
-    make platform=armv-gles-neon
+    make platform=armv$P64GLES-neon
   elif [ "$PROJECT" ==  "Rockchip" -a "$ARCH" == "aarch64" ]; then
     LDFLAGS="$LDFLAGS -lpthread"
     make FORCE_GLES=1 HAVE_PARALLEL=1
