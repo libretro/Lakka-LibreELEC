52c2c9e078d37cf793d961bab3955df739ec1efa
CvH <namerp@googlemail.com>
2018-10-02 17:43:40 +0200

    dvbsnoop: update to badf61f

--- packages/addons/addon-depends/dvb-tools-depends/dvbsnoop/package.mk
+++ packages/addons/addon-depends/dvb-tools-depends/dvbsnoop/package.mk
@@ -2,15 +2,12 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="dvbsnoop"
-PKG_VERSION="c1ec72f"
-PKG_SHA256="a277434fa78a31493d53a74e2a4f5bbb77e9cc396ec230c64f03e85dc6338e7f"
-PKG_ARCH="any"
+PKG_VERSION="badf61fcdab1177c1162747be06d035a2b671e9b"
+PKG_SHA256="7f0f5d9ca15c5caae3ca249d95a5fc30cececd16f63e00a1404e0d2368ce56fa"
 PKG_LICENSE="GPL"
 PKG_SITE="http://dvbsnoop.sourceforge.net/"
 PKG_URL="https://github.com/persianpros/dvbsnoop/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_SECTION="tools"
-PKG_SHORTDESC="dvbsnoop is a DVB/MPEG stream analyzer program"
 PKG_LONGDESC="dvbsnoop is a DVB/MPEG stream analyzer program"
 PKG_TOOLCHAIN="autotools"
 
