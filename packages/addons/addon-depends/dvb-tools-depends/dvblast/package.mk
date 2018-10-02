211e0149067c6e63b8a0e5601d92dd7884fe869a
CvH <namerp@googlemail.com>
2018-10-02 17:43:40 +0200

    dvblast: update to acf3844

--- packages/addons/addon-depends/dvb-tools-depends/dvblast/package.mk
+++ packages/addons/addon-depends/dvb-tools-depends/dvblast/package.mk
@@ -3,15 +3,12 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="dvblast"
-PKG_VERSION="77cfaa8"
-PKG_SHA256="b78eaec73addb328384bf8acb93a1b6a6334f4fa47914f98b91b4cd4fc00b639"
-PKG_ARCH="any"
+PKG_VERSION="acf3844377351ea2949ce1c48dbfde589491013d"
+PKG_SHA256="007d2c93876892bd09c2a520bdce0b04f6469187123bbf24cdcf0d5effec5389"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.videolan.org/projects/dvblast.html"
 PKG_URL="http://repo.or.cz/dvblast.git/snapshot/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain bitstream libev"
-PKG_SECTION="tools"
-PKG_SHORTDESC="DVBlast is a simple and powerful MPEG-2/TS demux and streaming application"
 PKG_LONGDESC="DVBlast is a simple and powerful MPEG-2/TS demux and streaming application"
 
 pre_configure_target() {
