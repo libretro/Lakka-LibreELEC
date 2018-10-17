2adce8b3f8e7900ea3afecfda4c47d3270232b51
CvH <namerp@googlemail.com>
2018-10-17 20:44:27 +0200

    libssh: update to 0.8.4

--- packages/network/libssh/package.mk
+++ packages/network/libssh/package.mk
@@ -1,16 +1,20 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libssh"
-PKG_VERSION="0.8.1"
-PKG_SHA256="d17f1267b4a5e46c0fbe66d39a3e702b8cefe788928f2eb6e339a18bb00b1924"
+PKG_VERSION="0.8.4"
+PKG_SHA256="6bb07713021a8586ba2120b2c36c468dc9ac8096d043f9b1726639aa4275b81b"
 PKG_LICENSE="LGPL"
 PKG_SITE="http://www.libssh.org/"
-PKG_URL="https://www.libssh.org/files/0.8/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_URL="https://www.libssh.org/files/${PKG_VERSION%.*}/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain zlib openssl"
-PKG_LONGDESC="A working SSH implementation by means of a library."
+PKG_LONGDESC="Library for accessing ssh client services through C libraries."
 
-PKG_CMAKE_OPTS_TARGET="-DWITH_STATIC_LIB=1 -DWITH_SERVER=OFF -DWITH_GCRYPT=OFF -DWITH_GSSAPI=OFF"
+PKG_CMAKE_OPTS_TARGET="-DWITH_STATIC_LIB=1 \
+                       -DWITH_SERVER=OFF \
+                       -DWITH_GCRYPT=OFF \
+                       -DWITH_GSSAPI=OFF"
 
 makeinstall_target() {
 # install static library only
