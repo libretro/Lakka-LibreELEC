ad3119922c11d5378563bd73b69e299af3ffddc4
CvH <namerp@googlemail.com>
2018-10-22 15:06:12 +0200

    cleanup: remove PKG_PRIORITY

--- packages/addons/addon-depends/python-libusb1/package.mk
+++ packages/addons/addon-depends/python-libusb1/package.mk
@@ -8,7 +8,6 @@ PKG_LICENSE="LGPL"
 PKG_SITE="https://github.com/vpelletier/python-libusb1"
 PKG_URL="https://github.com/vpelletier/$PKG_NAME/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain Python2 distutilscross:host"
-PKG_PRIORITY="optional"
 PKG_LONGDESC="A pure-python wrapper for libusb-1.0."
 PKG_TOOLCHAIN="manual"
 
