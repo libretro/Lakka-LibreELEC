06accc27a642a6a177d05b0fc79abce80a64be45
CvH <namerp@googlemail.com>
2018-10-27 19:19:56 +0200

    libretro-ppsspp: update to 1.7.0

--- packages/emulation/libretro-ppsspp/package.mk
+++ packages/emulation/libretro-ppsspp/package.mk
@@ -2,25 +2,38 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libretro-ppsspp"
-PKG_VERSION="bd678d4f4be7a3aad3d5dc9f0698be97e1baf755"
-PKG_SHA256="fd7af56fc7a7136d635a296e4c7151da4df4259d565b2f4ccf59e57475da8352"
-PKG_ARCH="x86_64"
+PKG_VERSION="caa506bf2a253a99850a4248a1cb5a399f32467a"
+PKG_SHA256="d59b4d044b761a73e744ab71e207e5b3bdbac819ed2201b79ed4455606ac0719"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/hrydgard/ppsspp"
 PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain glew kodi-platform SDL2 zlib"
-PKG_LONGDESC="A PSP emulator for Android, Windows, Mac, Linux and Blackberry 10, written in C++."
+PKG_DEPENDS_TARGET="toolchain SDL2"
+PKG_LONGDESC="A PSP emulator written in C++."
 PKG_TOOLCHAIN="cmake-make"
 
 PKG_LIBNAME="ppsspp_libretro.so"
 PKG_LIBPATH="lib/$PKG_LIBNAME"
 PKG_LIBVAR="PPSSPP_LIB"
 
-PKG_CMAKE_OPTS_TARGET="-DLIBRETRO=ON"
+if [ "$PROJECT" = "Amlogic" ] || [ "$PROJECT" = "RPi" ]; then
+  case $DEVICE in
+     KVIM|RPi2|S905|Odroid_C2)
+      PKG_ARCH_ARM="-DARMV7=ON \
+                -DUSING_FBDEV=ON \
+                -DUSING_EGL=ON \
+                -DUSING_GLES2=ON \
+                -DUSING_X11_VULKAN=OFF"
+     ;;
+  esac
+fi
 
 pre_configure_target() {
   LDFLAGS="$LDFLAGS -lpthread"
 }
+
+PKG_CMAKE_OPTS_TARGET="-DLIBRETRO=ON \
+                       -DUSE_SYSTEM_FFMPEG=ON \
+                       $PKG_ARCH_ARM"
 
 pre_make_target() {
   find . -name flags.make -exec sed -i "s:isystem :I:g" \{} \;
