a0b399b3219d4a7dace2c47a779781d61ae58246
MilhouseVH <milhouseVH.github@nmacleod.com>
2018-10-30 22:30:00 +0000

    bcmstat: update to bcmstat-0.5.1

--- packages/tools/bcmstat/package.mk
+++ packages/tools/bcmstat/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="bcmstat"
-PKG_VERSION="0.5.0"
-PKG_SHA256="d18cc53adb7cb4b51137fec533d51b95e4c37b836a8afe20e0d8f02137136cb7"
+PKG_VERSION="0.5.1"
+PKG_SHA256="88b3b189170e7d0eee83a233565d9ef0fb323489dc1161ef2392fca50e9637d6"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/MilhouseVH/bcmstat"
 PKG_URL="https://github.com/MilhouseVH/$PKG_NAME/archive/$PKG_VERSION.tar.gz"
