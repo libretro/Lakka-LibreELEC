e6439069b0d22e504925abb60f9fe03e7f3e6063
Matthias Reichl <hias@horus.com>
2018-11-04 11:56:29 +0100

    expat: use official release download URL
    See https://github.com/libexpat/libexpat/releases
    
    This also allows us to drop PKG_SOURCE_DIR and the post-unpack hack
    
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/textproc/expat/package.mk
+++ packages/textproc/expat/package.mk
@@ -3,18 +3,13 @@
 
 PKG_NAME="expat"
 PKG_VERSION="2.2.5"
-PKG_SHA256="b3781742738611eaa737543ee94264dd511c52a3ba7e53111f7d705f6bff65a8"
+PKG_SHA256="d9dc32efba7e74f788fcc4f212a43216fc37cf5f23f4c2339664d473353aedf6"
 PKG_LICENSE="OSS"
 PKG_SITE="http://expat.sourceforge.net/"
-PKG_URL="https://github.com/libexpat/libexpat/archive/R_${PKG_VERSION//./_}.tar.gz"
-PKG_SOURCE_DIR="libexpat-*/expat"
+PKG_URL="https://github.com/libexpat/libexpat/releases/download/R_${PKG_VERSION//./_}/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Expat is an XML parser library written in C."
 
 PKG_CMAKE_OPTS_TARGET="-DBUILD_doc=OFF -DBUILD_tools=OFF -DBUILD_examples=OFF -DBUILD_tests=OFF -DBUILD_shared=ON"
 PKG_CMAKE_OPTS_HOST="-DBUILD_doc=OFF -DBUILD_tools=OFF -DBUILD_examples=OFF -DBUILD_tests=OFF -DBUILD_shared=ON"
 
-# cleanup
-post_unpack() {
-  rm -fr $BUILD/libexpat-R_${PKG_VERSION//./_}
-}
