88a363867dc96467f96a8e48e788ccb1dc5c2ef4
MilhouseVH <milhouseVH.github@nmacleod.com>
2018-11-06 11:22:03 +0000

    libnftnl: update to libnftnl-1.1.2

--- packages/network/libnftnl/package.mk
+++ packages/network/libnftnl/package.mk
@@ -1,9 +1,10 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libnftnl"
-PKG_VERSION="1.0.7"
-PKG_SHA256="9bb66ecbc64b8508249402f0093829f44177770ad99f6042b86b3a467d963982"
+PKG_VERSION="1.1.2"
+PKG_SHA256="a5c7b7a6c13c9c5898b13fcb1126fefce2015d5a96d7c354b19aaa40b6aece5d"
 PKG_LICENSE="GPL"
 PKG_SITE="http://netfilter.org/projects/libnftnl"
 PKG_URL="http://netfilter.org/projects/libnftnl/files/$PKG_NAME-$PKG_VERSION.tar.bz2"
