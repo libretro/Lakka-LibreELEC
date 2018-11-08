a4fde412bf89e39fc1c012090abab5fd816f01ff
MilhouseVH <milhouseVH.github@nmacleod.com>
2018-11-08 12:46:55 +0000

    samba: update to samba-4.9.2

--- packages/network/samba/package.mk
+++ packages/network/samba/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="samba"
-PKG_VERSION="4.9.1"
-PKG_SHA256="33118cbe83a87be085eba1aae6e597878b02d6ac9b2da67454ed33cf3e9853f2"
+PKG_VERSION="4.9.2"
+PKG_SHA256="349c17b7bf1bf667167843470533da89ff1b2ca4a768b529aaacf5197af1efa2"
 PKG_LICENSE="GPLv3+"
 PKG_SITE="https://www.samba.org"
 PKG_URL="https://download.samba.org/pub/samba/stable/$PKG_NAME-$PKG_VERSION.tar.gz"
