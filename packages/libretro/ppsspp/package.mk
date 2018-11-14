32a370402e6a13469c52bec6009a957e400f026c
Jean-André Santoni <jean.andre.santoni@gmail.com>
2018-11-14 13:33:41 +0100

    Update and fix ppsspp

--- packages/libretro/ppsspp/package.mk
+++ packages/libretro/ppsspp/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="ppsspp"
-PKG_VERSION="9a610c8"
+PKG_VERSION="79d16f7"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
