c7457adaeabbbced3b76b00c284008c1aa007d5c
zalaare <zalaare@gmail.com>
2018-11-23 11:51:04 -0500

    nasm: initial package

---
+++ packages/lang/nasm/package.mk
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="nasm"
+PKG_VERSION="2.14"
+PKG_SHA256="97c615dbf02ef80e4e2b6c385f7e28368d51efc214daa98e600ca4572500eec0"
+PKG_LICENSE="BSD"
+PKG_SITE="https://www.nasm.us/"
+PKG_URL="http://www.nasm.us/pub/nasm/releasebuilds/${PKG_VERSION}/nasm-${PKG_VERSION}.tar.xz"
+PKG_DEPENDS_HOST="ccache:host"
+PKG_LONGDESC="The Netwide Assembler, NASM, is an 80x86 and x86-64 assembler designed for portability and modularity."
