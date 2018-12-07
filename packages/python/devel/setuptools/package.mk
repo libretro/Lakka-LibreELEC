9227995e55afa2e3813c2274ca901f9b4894ebcf
MilhouseVH <milhouseVH.github@nmacleod.com>
2018-12-07 17:21:14 +0000

    setuptools: add Python3 support, needed by Mako

--- packages/python/devel/setuptools/package.mk
+++ packages/python/devel/setuptools/package.mk
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="setuptools"
 PKG_VERSION="39.2.0"
@@ -7,14 +8,16 @@ PKG_SHA256="ca8119dd5c2764a7d290518817de0b880d23d790913fcd797c02ad2aa39b8c41"
 PKG_LICENSE="OSS"
 PKG_SITE="https://pypi.org/project/setuptools"
 PKG_URL="https://github.com/pypa/setuptools/archive/v$PKG_VERSION.tar.gz"
-PKG_DEPENDS_HOST="Python2:host"
+PKG_DEPENDS_HOST="Python2:host Python3:host"
 PKG_LONGDESC="Replaces Setuptools as the standard method for working with Python module distributions."
 PKG_TOOLCHAIN="manual"
 
 make_host() {
-  python bootstrap.py
+  python2 bootstrap.py
+  python3 bootstrap.py
 }
 
 makeinstall_host() {
-  python setup.py install --prefix=$TOOLCHAIN
+  python2 setup.py install --prefix=$TOOLCHAIN
+  python3 setup.py install --prefix=$TOOLCHAIN
 }
