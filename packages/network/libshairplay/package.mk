965b4b5b0f15217c0f72775cb85e25d5d4e71b8c
CvH <namerp@googlemail.com>
2018-12-21 12:13:30 +0100

    libshairplay: update to 096b61a

--- packages/network/libshairplay/package.mk
+++ packages/network/libshairplay/package.mk
@@ -1,15 +1,20 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libshairplay"
-PKG_VERSION="ce80e00"
-PKG_SHA256="49ac0e0470ec6367f720c7b79a09165138f57b60f5949a2d9c38be5823d13294"
-PKG_LICENSE="GPL"
+PKG_VERSION="096b61ad14c90169f438e690d096e3fcf87e504e"
+PKG_SHA256="7e2b013ffe75ea2f13fb12b1aa38b8e2e8b1899ac292d57f05d7b352a3a181cf"
+PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/juhovh/shairplay"
 PKG_URL="https://github.com/juhovh/shairplay/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="Emulates an AirPort Express to streaming music from iTunes and compatible iPods."
+PKG_DEPENDS_TARGET="toolchain avahi"
+PKG_LONGDESC="Apple airplay and raop protocol server"
 PKG_TOOLCHAIN="autotools"
+
+pre_configure_target() {
+  CFLAGS="$CFLAGS -I$(get_build_dir avahi)/avahi-compat-libdns_sd"
+}
 
 post_makeinstall_target() {
   mkdir -p $INSTALL/etc/shairplay
