83ebe4f0ac4546ea3b4bf8d773c8217f3faf601d
Jean-André Santoni <jean.andre.santoni@gmail.com>
2018-12-25 15:58:48 +0100

    Update mesen

--- packages/libretro/mesen/package.mk
+++ packages/libretro/mesen/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="mesen"
-PKG_VERSION="080d253"
+PKG_VERSION="ae0822e"
 PKG_REV="1"
 PKG_ARCH="i386 x86_64 arm"
 PKG_LICENSE="GPLv3"
