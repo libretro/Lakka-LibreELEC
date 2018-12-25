c665da910c82ba5d728924237a05db8d7a39d6e2
Jean-André Santoni <jean.andre.santoni@gmail.com>
2018-12-25 17:07:18 +0100

    Update chailove

--- packages/libretro/chailove/package.mk
+++ packages/libretro/chailove/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="chailove"
-PKG_VERSION="73baade"
+PKG_VERSION="67e775e"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
