81d3bc1a3fc7101260b658c9dd9baa110e5485a9
vpeter4 <peter.vicman@gmail.com>
2018-12-26 18:28:35 +0100

    mariadb: initial addon

---
+++ packages/addons/service/mariadb/package.mk
@@ -0,0 +1,97 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="mariadb"
+PKG_VERSION="10.3.11"
+PKG_REV="100"
+PKG_SHA256="211655b794c9d5397ba3be6c90737eac02e882f296268299239db47ba328f1b2"
+PKG_LICENSE="GPL2"
+PKG_SITE="https://mariadb.org"
+PKG_URL="https://downloads.mariadb.org/interstitial/${PKG_NAME}-${PKG_VERSION}/source/${PKG_NAME}-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_HOST="toolchain ncurses:host"
+PKG_DEPENDS_TARGET="toolchain binutils bzip2 libaio libxml2 lzo ncurses openssl systemd zlib mariadb:host"
+PKG_SHORTDESC="MariaDB is a community-developed fork of the MySQL."
+PKG_LONGDESC="MariaDB (${PKG_VERSION}) is a fast SQL database server and a drop-in replacement for MySQL."
+PKG_TOOLCHAIN="cmake"
+PKG_BUILD_FLAGS="-gold"
+
+PKG_IS_ADDON="yes"
+PKG_SECTION="service"
+PKG_ADDON_NAME="MariaDB SQL database server"
+PKG_ADDON_TYPE="xbmc.service"
+
+configure_package() {
+  PKG_CMAKE_OPTS_HOST=" \
+    -DCMAKE_INSTALL_MESSAGE=NEVER \
+    -DSTACK_DIRECTION=-1 \
+    -DHAVE_IB_GCC_ATOMIC_BUILTINS=1 \
+    -DCMAKE_CROSSCOMPILING=OFF \
+    import_executables"
+
+  PKG_CMAKE_OPTS_TARGET=" \
+    -DCMAKE_INSTALL_MESSAGE=NEVER \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_CONFIG=mysql_release \
+    -DFEATURE_SET=classic \
+    -DSTACK_DIRECTION=1 \
+    -DDISABLE_LIBMYSQLCLIENT_SYMBOL_VERSIONING=ON \
+    -DCMAKE_CROSSCOMPILING=ON \
+    -DIMPORT_EXECUTABLES=${PKG_BUILD}/.${HOST_NAME}/import_executables.cmake \
+    -DWITHOUT_AWS_KEY_MANAGEMENT=ON \
+    -DWITH_EXTRA_CHARSETS=complex \
+    -DWITH_SSL=system \
+    -DWITH_SSL=${SYSROOT_PREFIX}/usr \
+    -DWITH_JEMALLOC=OFF \
+    -DWITH_PCRE=bundled \
+    -DWITH_ZLIB=bundled \
+    -DWITH_EDITLINE=bundled \
+    -DWITH_LIBEVENT=bundled \
+    -DCONNECT_WITH_LIBXML2=bundled \
+    -DSKIP_TESTS=ON \
+    -DWITH_DEBUG=OFF \
+    -DWITH_UNIT_TESTS=OFF \
+    -DENABLE_DTRACE=OFF \
+    -DSECURITY_HARDENED=OFF \
+    -DWITH_EMBEDDED_SERVER=OFF \
+    -DWITHOUT_SERVER=OFF \
+    -DPLUGIN_AUTH_SOCKET=STATIC \
+    -DDISABLE_SHARED=NO \
+    -DENABLED_PROFILING=OFF \
+    -DENABLE_STATIC_LIBS=OFF \
+    -DMYSQL_UNIX_ADDR=/var/run/mysqld/mysqld.sock \
+    -DWITH_SAFEMALLOC=OFF \
+    -DWITHOUT_AUTH_EXAMPLES=ON"
+}
+
+make_host() {
+  ninja ${NINJA_OPTS} import_executables
+}
+
+makeinstall_host() {
+  :
+}
+
+makeinstall_target() {
+  # use only for addon
+  DESTDIR=${PKG_BUILD}/.install_addon ninja ${NINJA_OPTS} install
+  rm -rf "${PKG_BUILD}/.install_addon/usr/mysql-test"
+}
+
+addon() {
+  local ADDON="${ADDON_BUILD}/${PKG_ADDON_ID}"
+  local MARIADB="${PKG_BUILD}/.install_addon/usr"
+
+  mkdir -p ${ADDON}/bin
+  mkdir -p ${ADDON}/config
+
+  cp ${MARIADB}/bin/mysql \
+     ${MARIADB}/bin/mysqld \
+     ${MARIADB}/bin/mysqladmin \
+     ${MARIADB}/bin/mysql_secure_installation \
+     ${MARIADB}/bin/my_print_defaults \
+     ${MARIADB}/bin/resolveip \
+     ${MARIADB}/scripts/mysql_install_db \
+     ${ADDON}/bin
+
+  cp -PR ${MARIADB}/share ${ADDON}
+}
