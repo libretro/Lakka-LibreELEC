f3989a87c1518abe474af1dc4f9039fdc6ea0d7e
mglae <mglmail@arcor.de>
2018-12-27 15:53:05 +0100

    efibootmgr: update to 17 and update efivars

--- packages/addons/addon-depends/system-tools-depends/efibootmgr/package.mk
+++ packages/addons/addon-depends/system-tools-depends/efibootmgr/package.mk
@@ -2,18 +2,22 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="efibootmgr"
-PKG_VERSION="95f7a63"
-PKG_SHA256="a6f936508c5b50b6fb5693dd2f0db911da298da0f72ffc0e2e74b09b22592fd1"
+PKG_VERSION="99b578501643377e0b1994b2a068b790d189d5ad"
+PKG_SHA256="04bc45dc8a841985d78d8df87b3475eb6e1122f993fae975197bf3adbc6e3341"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/vathpela/efibootmgr"
-PKG_URL="https://github.com/vathpela/efibootmgr-devel/archive/$PKG_VERSION.tar.gz"
+PKG_SITE="https://github.com/rhboot/efibootmgr"
+PKG_URL="https://github.com/rhboot/efibootmgr/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain efivar pciutils zlib"
 PKG_LONGDESC="Tool to modify UEFI Firmware Boot Manager Variables."
 
-pre_make_target() {
-  export EXTRA_CFLAGS="$CFLAGS -I$SYSROOT_PREFIX/usr/include -I$SYSROOT_PREFIX/usr/include/efivar -fgnu89-inline"
+make_target() {
+  export CFLAGS="$CFLAGS -I$SYSROOT_PREFIX/usr/include -I$SYSROOT_PREFIX/usr/include/efivar -fgnu89-inline"
   export LDFLAGS="$LDFLAGS -L$SYSROOT_PREFIX/usr/lib -ludev -ldl"
+
+  make EFIDIR=BOOT EFI_LOADER=bootx64.efi PKG_CONFIG=true \
+    LDLIBS="-lefiboot -lefivar" \
+    efibootmgr
 }
 
 makeinstall_target() {
