880b2d5af4976a3dbe6681121e3c2ae008e7cc65
MilhouseVH <milhouseVH.github@nmacleod.com>
2018-12-29 16:00:12 +0000

    linux (RPi/Generic): update to linux-4.19.13

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -38,14 +38,14 @@ case "$LINUX" in
     PKG_SOURCE_NAME="linux-$LINUX-$PKG_VERSION.tar.gz"
     ;;
   raspberrypi)
-    PKG_VERSION="e75aca6e66f6091dd3b9c316750025c8e9684f16" # 4.19.12
-    PKG_SHA256="0f29fb52dc4d7fd705c9215e8a910a6e854b8014e697bdd7b6ce4854d7e342f4"
+    PKG_VERSION="738499ce347b4670d4c6e3f018477b1237073922" # 4.19.13
+    PKG_SHA256="d5677209bdd8ebf5a6406139151a89c4409d07abbcc1555cf19885af56efc00f"
     PKG_URL="https://github.com/raspberrypi/linux/archive/$PKG_VERSION.tar.gz"
     PKG_SOURCE_NAME="linux-$LINUX-$PKG_VERSION.tar.gz"
     ;;
   *)
-    PKG_VERSION="4.19.12"
-    PKG_SHA256="4d81ac539d62617f5b52f25971749d8c6d3a200deee76898bb99be8492999b77"
+    PKG_VERSION="4.19.13"
+    PKG_SHA256="f50a77fc40e13fc406791346fa91926394205874cd50246c7c2374006bacc0c2"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v4.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;
