28755d3fddb8644f8144d6f3a47abe024c1f9c8a
lsellens <lsellens@gmail.com>
2019-01-09 11:39:42 -0600

    libdvdcss is building even when disabled

--- packages/multimedia/libdvdread/package.mk
+++ packages/multimedia/libdvdread/package.mk
@@ -8,6 +8,10 @@ PKG_SHA256="2d9d6d185dd25a983d6dfc2a00207cafdc396a969c227d5edd84b6215b2fba89"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/xbmc/libdvdread"
 PKG_URL="https://github.com/xbmc/libdvdread/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain libdvdcss"
+PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="libdvdread is a library which provides a simple foundation for reading DVDs."
 PKG_TOOLCHAIN="manual"
+
+if [ "$KODI_DVDCSS_SUPPORT" = yes ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET libdvdcss"
+fi
