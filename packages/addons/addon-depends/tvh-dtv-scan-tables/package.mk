e4a129939fd25d582b0b527a56a1a5b7cdf5bcee
CvH <namerp@googlemail.com>
2019-01-09 11:38:56 +0100

    tvh-dtv-scan-tables: update to 68f86b4

--- packages/addons/addon-depends/tvh-dtv-scan-tables/package.mk
+++ packages/addons/addon-depends/tvh-dtv-scan-tables/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="tvh-dtv-scan-tables"
-PKG_VERSION="421c08b31b24e0cd5d8fb54e6f3278a1ef9d3666"
-PKG_SHA256="2377c0edec0997d320f121a8f7944b37935e0fd40249252d4098b7002df82693"
+PKG_VERSION="68f86b42272fc364cdd53d7cfeaa2a78d676dbfa"
+PKG_SHA256="2bb102094696bbaf440b9319a9e8a4c93e2ecbcf2d2c556fb719b1febf1a7d3b"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/tvheadend"
 PKG_URL="https://github.com/tvheadend/dtv-scan-tables/archive/$PKG_VERSION.tar.gz"
