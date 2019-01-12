ba50b7b19ce5aaf68da25a3905cae38ad1e77d5a
awiouy <awiouy@gmail.com>
2019-01-12 13:48:49 +0100

    emby4: initial addon

---
+++ packages/addons/service/emby4/package.mk
@@ -0,0 +1,35 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="emby4"
+PKG_VERSION="4.0.0.2"
+PKG_SHA256="cac3de0f4b75d421719676377bae80274338a493e79c0498c8772e7e51cd8edf"
+PKG_REV="100"
+PKG_ARCH="any"
+PKG_LICENSE="prop."
+PKG_SITE="http://emby.media"
+PKG_URL="https://github.com/MediaBrowser/Emby.Releases/releases/download/$PKG_VERSION/embyserver-netcore_$PKG_VERSION.zip"
+PKG_SOURCE_DIR="system"
+PKG_DEPENDS_TARGET="toolchain imagemagick"
+PKG_SECTION="service"
+PKG_SHORTDESC="Emby Server: a personal media server"
+PKG_LONGDESC="Emby Server ($PKG_VERSION) brings your home videos, music, and photos together, automatically converting and streaming your media on-the-fly to any device"
+PKG_TOOLCHAIN="manual"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_NAME="Emby Server 4"
+PKG_ADDON_TYPE="xbmc.service"
+PKG_ADDON_REQUIRES="tools.ffmpeg-tools:0.0.0 tools.dotnet-runtime:0.0.0"
+PKG_MAINTAINER="Anton Voyl (awiouy)"
+
+addon() {
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/emby
+  cp -r $PKG_BUILD/* \
+        -d $ADDON_BUILD/$PKG_ADDON_ID/emby
+
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/lib
+  cp -L $(get_build_dir imagemagick)/.install_pkg/usr/lib/libMagickCore-7.Q16HDRI.so.? \
+        $ADDON_BUILD/$PKG_ADDON_ID/lib/
+  cp -L $(get_build_dir imagemagick)/.install_pkg/usr/lib/libMagickWand-7.Q16HDRI.so \
+        $ADDON_BUILD/$PKG_ADDON_ID/lib/CORE_RL_Wand_.so
+}
---
+++ packages/addons/service/emby4/source/default.py
@@ -0,0 +1,46 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
+
+import json
+import subprocess
+import xbmc
+import xbmcaddon
+import xbmcgui
+
+
+def jsonrpc(request):
+    return json.loads(xbmc.executeJSONRPC(json.dumps(request)))
+
+
+def disable_conflicting(conficting,
+        message='{that} conflicts with {this} and has been disabled'):
+    is_enabled = {'jsonrpc': '2.0', 'method': 'Addons.GetAddonDetails', 'id': 1,
+                  'params': {'addonid': conficting, 'properties': ['enabled']}}
+    disable = {'jsonrpc': '2.0', 'method': 'Addons.SetAddonEnabled', 'id': 1,
+               'params': {'addonid': conficting, 'enabled': False}}
+    try:
+        if jsonrpc(is_enabled)['result']['addon']['enabled']:
+            this = xbmcaddon.Addon().getAddonInfo('name')
+            that = xbmcaddon.Addon(conficting).getAddonInfo('name')
+            jsonrpc(disable)
+            dialog = xbmcgui.Dialog()
+            dialog.ok(this, message.format(
+                this=this, that=that))
+            del dialog
+    except KeyError:
+        pass
+
+
+class Monitor(xbmc.Monitor):
+
+    def __init__(self, *args, **kwargs):
+        xbmc.Monitor.__init__(self)
+        self.id = xbmcaddon.Addon().getAddonInfo('id')
+
+    def onSettingsChanged(self):
+        subprocess.call(['systemctl', 'restart', self.id])
+
+
+if __name__ == '__main__':
+    disable_conflicting('service.emby')
+    Monitor().waitForAbort()
