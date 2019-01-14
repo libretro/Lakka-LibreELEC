636ee2740786533eeadb59bf11e6bca4e1c4d515
CvH <namerp@googlemail.com>
2019-01-14 02:26:15 +0100

    file: update to d1ff3af

--- packages/addons/addon-depends/system-tools-depends/file/package.mk
+++ packages/addons/addon-depends/system-tools-depends/file/package.mk
@@ -2,13 +2,13 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="file"
-PKG_VERSION="2198460"
-PKG_SHA256="d825eeee9e0a20b79dbcd00c51022b69fe4468354d8ccc03f441eb349ad8bde0"
+PKG_VERSION="d1ff3af7a2c6b38bdbdde7af26b59e3c50a48fff"
+PKG_SHA256="acc16f878576ad0bb29bbb7c724e29d2827f14ddb39fe94d76c3a859d4a3d0d9"
 PKG_LICENSE="BSD"
 PKG_SITE="http://www.darwinsys.com/file/"
 PKG_URL="https://github.com/file/file/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST="ccache:host"
-PKG_DEPENDS_TARGET="toolchain zlib file:host"
+PKG_DEPENDS_TARGET="toolchain file:host zlib"
 PKG_LONGDESC="The file utility is used to determine the types of various files."
 PKG_TOOLCHAIN="autotools"
 
