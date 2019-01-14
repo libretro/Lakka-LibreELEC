7c7adbacdab90f75880d09f3a8a5628c3d874bf6
CvH <namerp@googlemail.com>
2019-01-14 02:26:15 +0100

    jq: update to 1.6

--- packages/addons/addon-depends/system-tools-depends/jq/package.mk
+++ packages/addons/addon-depends/system-tools-depends/jq/package.mk
@@ -2,18 +2,20 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="jq"
-PKG_VERSION="1.5"
-PKG_SHA256="c4d2bfec6436341113419debf479d833692cc5cdab7eb0326b5a4d4fbe9f493c"
+PKG_VERSION="1.6"
+PKG_SHA256="5de8c8e29aaa3fb9cc6b47bb27299f271354ebb72514e3accadc7d38b5bbaa72"
 PKG_LICENSE="MIT"
 PKG_SITE="http://stedolan.github.io/jq/"
 PKG_URL="https://github.com/stedolan/jq/releases/download/$PKG_NAME-$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_DEPENDS_TARGET="toolchain oniguruma"
 PKG_LONGDESC="A like sed for JSON data."
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
                            --enable-static \
-                           --disable-maintainer-mode"
+                           --disable-docs \
+                           --disable-maintainer-mode \
+                           --disable-valgrind"
 
 makeinstall_target() {
-  : # nop
+  :
 }
