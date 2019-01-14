f61091f7282cef52d1db3b3207d40c1c7b26d601
CvH <namerp@googlemail.com>
2019-01-14 02:26:15 +0100

    mtpfs: fix version

--- packages/addons/addon-depends/system-tools-depends/mtpfs/package.mk
+++ packages/addons/addon-depends/system-tools-depends/mtpfs/package.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mtpfs"
-PKG_VERSION="fd3864d"
+PKG_VERSION="fd3864dd6f0e8183fa2598d4cf890401d3a1e09a"
 PKG_SHA256="f004136a82452d13362581277eb2496033aa13a6c3f35d0501327248f3120456"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.adebenham.com/mtpfs/"
 PKG_URL="https://github.com/cjd/mtpfs/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain fuse libmtp glib"
+PKG_DEPENDS_TARGET="toolchain fuse glib libmtp"
 PKG_LONGDESC="MTPfs is a FUSE filesystem that supports reading and writing from any MTP device."
 PKG_TOOLCHAIN="autotools"
 
