490c65f30fed09db079d976777f3cfc2ae160136
CvH <namerp@googlemail.com>
2019-01-15 12:16:14 +0100

    unrar: build additional shared

--- packages/addons/addon-depends/system-tools-depends/unrar/package.mk
+++ packages/addons/addon-depends/system-tools-depends/unrar/package.mk
@@ -2,14 +2,15 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="unrar"
-PKG_VERSION="5.6.3"
-PKG_SHA256="c590e70a745d840ae9b9f05ba6c449438838c8280d76ce796a26b3fcd0a1972e"
+PKG_VERSION="5.6.8"
+PKG_SHA256="a4cc0ac14a354827751912d2af4a0a09e2c2129df5766576fa7e151791dd3dff"
 PKG_LICENSE="free"
 PKG_SITE="http://www.rarlab.com"
 PKG_URL="http://www.rarlab.com/rar/unrarsrc-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="unrar extract, test and view RAR archives"
 PKG_TOOLCHAIN="manual"
+PKG_BUILD_FLAGS="+pic"
 
 make_target() {
   make CXX="$CXX" \
@@ -17,5 +18,17 @@ make_target() {
      RANLIB="$RANLIB" \
      AR="$AR" \
      STRIP="$STRIP" \
-     -f makefile
+     -f makefile unrar
+
+  make clean
+
+  make CXX="$CXX" \
+     CXXFLAGS="$TARGET_CXXFLAGS" \
+     RANLIB="$RANLIB" \
+     AR="$AR" \
+     -f makefile lib
+}
+
+post_make_target() {
+  rm -f libunrar.so
 }
