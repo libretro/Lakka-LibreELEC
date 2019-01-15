612db49d61487f459a03d694b6fe0aad64cbe7b5
Jonas Karlman <jonas@kwiboo.se>
2019-01-15 19:50:06 +0100

    libmali: new package

---
+++ packages/graphics/libmali/package.mk
@@ -0,0 +1,27 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="libmali"
+PKG_VERSION="3e06d6226c22d8d69cd5cb4935d040178b0737f6"
+PKG_SHA256="dfdcf2b9f55c3ba9a4e723834708edefb5d7bc7596c4cbde8448e1b0410796d7"
+PKG_ARCH="arm aarch64"
+PKG_LICENSE="nonfree"
+PKG_SITE="https://github.com/LibreELEC/libmali"
+PKG_URL="https://github.com/LibreELEC/libmali/archive/$PKG_VERSION.tar.gz"
+PKG_LONGDESC="OpenGL ES user-space binary for the ARM Mali GPU family"
+
+PKG_DEPENDS_TARGET="libdrm"
+
+if [ "$MALI_FAMILY" = "t720" ]; then
+  PKG_DEPENDS_TARGET+=" wayland"
+fi
+
+PKG_CMAKE_OPTS_TARGET="-DMALI_VARIANT=$MALI_FAMILY"
+
+if [ -n "$MALI_REVISION" ]; then
+  PKG_CMAKE_OPTS_TARGET+=" -DMALI_REVISION=$MALI_REVISION"
+fi
+
+if [ "$TARGET_ARCH" = "aarch64" ]; then
+  PKG_CMAKE_OPTS_TARGET+=" -DMALI_ARCH=aarch64-linux-gnu"
+fi
