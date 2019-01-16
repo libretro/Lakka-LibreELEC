f24737b24a127aad7c61d0661305f8e2c96e9842
awiouy <awiouy@gmail.com>
2019-01-16 17:02:43 +0100

    rustup.rs: update to 1.16.0

--- packages/addons/addon-depends/librespot-depends/rustup.rs/package.mk
+++ packages/addons/addon-depends/librespot-depends/rustup.rs/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="rustup.rs"
-PKG_VERSION="1.3.0"
-PKG_SHA256="c0ca06b70104fed8f1de5a6f5ecfd8478e8bc03f15add8d7896b86b3b15e81e3"
+PKG_VERSION="1.16.0"
+PKG_SHA256="8c4ffeda2088dbdd5ea2eac8acef5ddd57dfcfe1f06a503e3da790f93161e1a6"
 PKG_LICENSE="MIT"
 PKG_SITE="https://www.rust-lang.org"
 PKG_URL="https://github.com/rust-lang-nursery/rustup.rs/archive/$PKG_VERSION.tar.gz"
