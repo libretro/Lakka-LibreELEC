b7a4d914002df0a7c4adbe7f6ef4902cd31a3a64
CvH <namerp@googlemail.com>
2019-01-17 13:44:01 +0100

    rar2fs: add fuse depend

--- packages/addons/addon-depends/network-tools-depends/rar2fs/package.mk
+++ packages/addons/addon-depends/network-tools-depends/rar2fs/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="f7e84b813fe82c6a886313e13a85d4f0d229b21f343a3ce6f73325a4ac90cb5a"
 PKG_LICENSE="GPL3"
 PKG_SITE="https://github.com/hasse69/rar2fs"
 PKG_URL="https://github.com/hasse69/rar2fs/releases/download/v$PKG_VERSION/rar2fs-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain unrar"
+PKG_DEPENDS_TARGET="toolchain fuse unrar"
 PKG_LONGDESC="FUSE file system for reading RAR archives"
 
 PKG_CONFIGURE_OPTS_TARGET="--with-unrar=$(get_build_dir unrar) \
