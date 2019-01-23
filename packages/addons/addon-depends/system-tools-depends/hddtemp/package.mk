b341b7b0be29d996c7cb6a30a01450b4e7af59b3
Red Wil <redwil@gmail.com>
2019-01-23 16:34:47 +0000

    hddtemp: fix build: update urls, local hddtemp.db

--- packages/addons/addon-depends/system-tools-depends/hddtemp/package.mk
+++ packages/addons/addon-depends/system-tools-depends/hddtemp/package.mk
@@ -5,7 +5,7 @@ PKG_NAME="hddtemp"
 PKG_VERSION="e16aed6"
 PKG_SHA256="5d5af74ba7449b6e56a8f872a0e10d654a512ed65d62beaef1575b0c1826d9f3"
 PKG_LICENSE="GPL"
-PKG_SITE="http://www.guzu.net/linux/hddtemp.php"
+PKG_SITE="https://savannah.nongnu.org/projects/hddtemp"
 PKG_URL="https://github.com/guzu/hddtemp/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A utility that gives you the temperature of your hard drive by reading S.M.A.R.T.."
@@ -13,8 +13,7 @@ PKG_LONGDESC="A utility that gives you the temperature of your hard drive by rea
 PKG_CONFIGURE_OPTS_TARGET="--with-db-path=/storage/.kodi/addons/virtual.system-tools/data/hddtemp.db"
 
 post_unpack() {
-  cd $PKG_BUILD
-  wget -O hddtemp.db http://www.guzu.net/linux/hddtemp.db
+  cp $PKG_DIR/db/* $PKG_BUILD
 }
 
 makeinstall_target() {
