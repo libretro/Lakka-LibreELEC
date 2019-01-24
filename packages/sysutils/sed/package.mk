53d8985089d1f19b4b2923439e6f5bc44cdd643a
Ian Leonard <antonlacon@gmail.com>
2019-01-24 23:08:07 +0000

    sed: update to 4.7
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- packages/sysutils/sed/package.mk
+++ packages/sysutils/sed/package.mk
@@ -2,13 +2,12 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="sed"
-PKG_VERSION="4.2.2"
-PKG_SHA256="f048d1838da284c8bc9753e4506b85a1e0cc1ea8999d36f6995bcb9460cddbd7"
+PKG_VERSION="4.7"
+PKG_SHA256="2885768cd0a29ff8d58a6280a270ff161f6a3deb5690b2be6c49f46d4c67bd6a"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/software/sed/"
-PKG_URL="http://ftpmirror.gnu.org/sed/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_URL="http://ftpmirror.gnu.org/sed/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_HOST="ccache:host"
 PKG_LONGDESC="The sed (Stream EDitor) editor is a stream or batch (non-interactive) editor."
 
 PKG_CONFIGURE_OPTS_HOST="--disable-nls --disable-acl --without-selinux"
-PKG_MAKEINSTALL_OPTS_HOST="-C sed install"
