ce6c34fcd3ee50276a00faa442ca893aa933eda0
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-01-28 18:48:14 +0000

    kodi (Generic): update to kodi-18.0-Leia

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -24,8 +24,8 @@ case $KODI_VENDOR in
     PKG_SOURCE_NAME="kodi-$KODI_VENDOR-$PKG_VERSION.tar.gz"
     ;;
   *)
-    PKG_VERSION="18.0rc5.2-Leia"
-    PKG_SHA256="e8ec83659339491286bf7d9f65037f18c0a4e21d5479808474ef5ca98ff691bb"
+    PKG_VERSION="18.0-Leia"
+    PKG_SHA256="ac5d64d59c6f4811b41a869538506e56c342b530fac97ad9dc9715f3d480e633"
     PKG_URL="https://github.com/xbmc/xbmc/archive/$PKG_VERSION.tar.gz"
     PKG_SOURCE_NAME="kodi-$PKG_VERSION.tar.gz"
     ;;
