9d0f5d0a782853b4673d9da8c1e1ad3312723056
CvH <namerp@googlemail.com>
2019-01-28 12:46:43 +0100

    libid3tag: install pkgconfig file

--- packages/addons/addon-depends/libid3tag/package.mk
+++ packages/addons/addon-depends/libid3tag/package.mk
@@ -1,15 +1,19 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libid3tag"
 PKG_VERSION="0.15.1b"
 PKG_SHA256="63da4f6e7997278f8a3fef4c6a372d342f705051d1eeb6a46a86b03610e26151"
 PKG_LICENSE="GPL"
-PKG_SITE="http://www.mars.org/home/rob/proj/mpeg/"
-PKG_URL="$SOURCEFORGE_SRC/mad/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SITE="https://www.underbit.com/products/mad/"
+PKG_URL="ftp://ftp.mars.org/pub/mpeg/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain zlib"
 PKG_LONGDESC="A library for id3 tagging."
 
-PKG_MAINTAINER="Lukas Sabota (LTsmooth42@gmail.com)"
+PKG_CONFIGURE_OPTS_TARGET="--enable-static \
+                           --disable-shared"
 
-PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared"
+post_makeinstall_target(){
+ cp $PKG_BUILD/id3tag.pc $SYSROOT_PREFIX/usr/lib/pkgconfig
+}
