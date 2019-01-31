dc9b3e67b50cacc5a667937205812ec19458525a
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-01-31 23:25:13 +0000

    mesa: update to mesa-18.3.3

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa"
-PKG_VERSION="18.3.2"
-PKG_SHA256="e9760ff8f0a6700c395705181ee997cd4fcb6f492143f2d52965ca63bca7139f"
+PKG_VERSION="18.3.3"
+PKG_SHA256="10e20e8498f935863946c82581ee1f94cfa051f6e7e96a1ba9d804efbefb9c1f"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
 PKG_URL="https://github.com/mesa3d/mesa/archive/mesa-$PKG_VERSION.tar.gz"
