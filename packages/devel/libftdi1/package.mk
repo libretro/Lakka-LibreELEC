7bea6bdafc34b18c1fdd367363026fe1cd3b06ef
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-02-01 08:10:35 +0000

    libftdi1: update to libftdi1-1.4

--- packages/devel/libftdi1/package.mk
+++ packages/devel/libftdi1/package.mk
@@ -1,9 +1,10 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libftdi1"
-PKG_VERSION="1.3"
-PKG_SHA256="9a8c95c94bfbcf36584a0a58a6e2003d9b133213d9202b76aec76302ffaa81f4"
+PKG_VERSION="1.4"
+PKG_SHA256="ec36fb49080f834690c24008328a5ef42d3cf584ef4060f3a35aa4681cb31b74"
 PKG_LICENSE="LGPL"
 PKG_SITE="http://www.intra2net.com/en/developer/libftdi/"
 PKG_URL="http://www.intra2net.com/en/developer/libftdi/download/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
