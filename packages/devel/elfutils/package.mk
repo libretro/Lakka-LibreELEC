925bdf959247e830aebdeb105508d4c9415489de
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-02-01 08:07:08 +0000

    elfutils: update to elfutils-0.175

--- packages/devel/elfutils/package.mk
+++ packages/devel/elfutils/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="elfutils"
-PKG_VERSION="0.173"
-PKG_SHA256="b76d8c133f68dad46250f5c223482c8299d454a69430d9aa5c19123345a000ff"
+PKG_VERSION="0.175"
+PKG_SHA256="f7ef925541ee32c6d15ae5cb27da5f119e01a5ccdbe9fe57bf836730d7b7a65b"
 PKG_LICENSE="GPL"
 PKG_SITE="https://sourceware.org/elfutils/"
 PKG_URL="https://sourceware.org/elfutils/ftp/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.bz2"
