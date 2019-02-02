c05928eaa1eb1b08c48b16009d406d68fb978c0a
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-02-02 12:05:35 +0000

    gtk3: update to gtk3-3.24.4

--- packages/addons/addon-depends/chrome-depends/gtk3/package.mk
+++ packages/addons/addon-depends/chrome-depends/gtk3/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="gtk3"
-PKG_VERSION="3.22.30"
-PKG_SHA256="a1a4a5c12703d4e1ccda28333b87ff462741dc365131fbc94c218ae81d9a6567"
+PKG_VERSION="3.24.4"
+PKG_SHA256="d84f59ff02a87cc90c9df4a572a13eca4e3506e2bf511e2b9cbdb4526fa0cb9c"
 PKG_LICENSE="LGPL"
 PKG_SITE="http://www.gtk.org/"
 PKG_URL="https://ftp.gnome.org/pub/gnome/sources/gtk+/${PKG_VERSION:0:4}/gtk+-$PKG_VERSION.tar.xz"
@@ -25,8 +25,9 @@ PKG_CONFIGURE_OPTS_TARGET="--disable-cups \
 
 pre_configure_target() {
   LIBS="$LIBS -lXcursor"
-  export PKG_CONFIG_PATH="$(get_build_dir pango)/.$TARGET_NAME"
-  export CFLAGS="$CFLAGS -I$(get_build_dir pango) -L$(get_build_dir pango)/.$TARGET_NAME/pango"
+  export PKG_CONFIG_PATH="$(get_build_dir pango)/.$TARGET_NAME/meson-private:$(get_build_dir gdk-pixbuf)/.$TARGET_NAME/meson-private:$(get_build_dir shared-mime-info)/.$TARGET_NAME"
+  export CFLAGS="$CFLAGS -I$(get_build_dir pango) -I$(get_build_dir pango)/.$TARGET_NAME -L$(get_build_dir pango)/.$TARGET_NAME/pango"
+  export GLIB_COMPILE_RESOURCES=glib-compile-resources GLIB_MKENUMS=glib-mkenums GLIB_GENMARSHAL=glib-genmarshal
 }
 
 makeinstall_target() {
