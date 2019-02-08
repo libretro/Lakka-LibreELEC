52f921055a56bc9b5da1ead2c3f7d72de437d5e8
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-02-08 17:17:43 +0000

    font-bitstream-type1: add font-xfree86-type1 dependency to avoid install race condition
    Both packages update the same fonts.dir, but font-xfree86-type1 trashes it so build this first.

--- packages/x11/font/font-bitstream-type1/package.mk
+++ packages/x11/font/font-bitstream-type1/package.mk
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="font-bitstream-type1"
 PKG_VERSION="1.0.3"
@@ -7,7 +8,7 @@ PKG_SHA256="c6ea0569adad2c577f140328dc3302e729cb1b1ea90cd0025caf380625f8a688"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.X.org"
 PKG_URL="http://xorg.freedesktop.org/archive/individual/font/$PKG_NAME-$PKG_VERSION.tar.bz2"
-PKG_DEPENDS_TARGET="toolchain util-macros"
+PKG_DEPENDS_TARGET="toolchain util-macros font-xfree86-type1"
 PKG_LONGDESC="Bitstream font family."
 
 PKG_CONFIGURE_OPTS_TARGET="--with-fontrootdir=/usr/share/fonts"
