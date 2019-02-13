812739f7d68121682fde6e756b8ebcf92872b56d
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-02-13 15:10:54 +0000

    slice: fix slice addon build

--- packages/tools/led_tools/package.mk
+++ packages/tools/led_tools/package.mk
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2014 Gordon Hollingworth (gordon@fiveninjas.com)
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="led_tools"
 PKG_VERSION="0.1"
@@ -7,7 +8,7 @@ PKG_SHA256="0484b4a2da9d586accef87ba7dd18595baae1d602c1b1bd9e0a8565ab50419a2"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.fiveninjas.com"
 PKG_URL="http://updates.fiveninjas.com/src/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain zlib libpng slice-addon"
+PKG_DEPENDS_TARGET="toolchain zlib libpng slice"
 PKG_DEPENDS_HOST="toolchain"
 PKG_LONGDESC="LED tools, these are a set of tools to control the LEDs on Slice"
 
