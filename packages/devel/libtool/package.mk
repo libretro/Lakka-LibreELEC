da79708b57779acc60ac06f1c6fbfd3f1cc784ea
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-02-13 15:10:54 +0000

    libtool: fix dependency

--- packages/devel/libtool/package.mk
+++ packages/devel/libtool/package.mk
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libtool"
 PKG_VERSION="2.4.6"
@@ -7,7 +8,7 @@ PKG_SHA256="e3bd4d5d3d025a36c21dd6af7ea818a2afcd4dfc1ea5a17b39d7854bcd0c06e3"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/software/libtool/"
 PKG_URL="http://ftpmirror.gnu.org/libtool/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_HOST="ccache:host autoconf:host automake:host"
+PKG_DEPENDS_HOST="ccache:host autoconf:host automake:host intltool:host"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A generic library support script."
 PKG_TOOLCHAIN="autotools"
