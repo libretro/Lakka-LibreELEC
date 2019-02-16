d6b702f114662210254e54c50db609195ae59a73
CvH <namerp@googlemail.com>
2019-02-16 21:16:56 +0100

    libretro-mesen: initial package

---
+++ packages/emulation/libretro-mesen/package.mk
@@ -0,0 +1,26 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="libretro-mesen"
+PKG_VERSION="278ca034b2bbe8c3e931458a63d12a7559770f17"
+PKG_SHA256="1d5743a4ff7d4fbedd9b1a0063e51fad5f961274cf3f007bc1d4537450753d45"
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://github.com/SourMesen/Mesen/"
+PKG_URL="https://github.com/SourMesen/Mesen/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform"
+PKG_LONGDESC="game.libretro.mesen: mesen for Kodi"
+PKG_TOOLCHAIN="make"
+
+PKG_LIBNAME="mesen_libretro.so"
+PKG_LIBPATH="Libretro/$PKG_LIBNAME"
+PKG_LIBVAR="MESEN_LIB"
+
+make_target() {
+  make -C Libretro/
+}
+
+makeinstall_target() {
+  mkdir -p $SYSROOT_PREFIX/usr/lib/cmake/$PKG_NAME
+  cp $PKG_LIBPATH $SYSROOT_PREFIX/usr/lib/$PKG_LIBNAME
+  echo "set($PKG_LIBVAR $SYSROOT_PREFIX/usr/lib/$PKG_LIBNAME)" > $SYSROOT_PREFIX/usr/lib/cmake/$PKG_NAME/$PKG_NAME-config.cmake
+}
---
+++ packages/mediacenter/kodi-binary-addons/game.libretro.mesen/package.mk
@@ -0,0 +1,17 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="game.libretro.mesen"
+PKG_VERSION="62e31566e6459face42a8629cde4d7e9295390ce"
+PKG_SHA256="cc1f8b9b70bda8db1eceb0426b670ff2f61595f812c29606720ff14e83ba8e54"
+PKG_REV="0"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/kodi-game/game.libretro.mesen"
+PKG_URL="https://github.com/kodi-game/game.libretro.mesen/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform libretro-mesen"
+PKG_SECTION=""
+PKG_LONGDESC="game.libretro.mesen: mesen for Kodi"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="kodi.gameclient"
