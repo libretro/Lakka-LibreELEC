7747d0aaeaf439e07bf64c704d8ecb46c5eaa256
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-02-21 03:05:59 +0000

    qtbase: meh, fix itself up

--- packages/addons/addon-depends/qtbase/package.mk
+++ packages/addons/addon-depends/qtbase/package.mk
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Lukas Rusak (lrusak@libreelec.tv)
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="qtbase"
 PKG_VERSION="5.6.2"
@@ -11,8 +12,8 @@ PKG_DEPENDS_TARGET="pcre zlib"
 PKG_LONGDESC="A cross-platform application and UI framework."
 
 PKG_CONFIGURE_OPTS_TARGET="-prefix /usr
-                           -sysroot $SYSROOT_PREFIX
-                           -hostprefix $TOOLCHAIN
+                           -sysroot "${SYSROOT_PREFIX}"
+                           -hostprefix "${TOOLCHAIN}"
                            -device linux-libreelec-g++
                            -opensource -confirm-license
                            -release
@@ -52,31 +53,42 @@ PKG_CONFIGURE_OPTS_TARGET="-prefix /usr
 
 configure_target() {
   QMAKE_CONF_DIR="mkspecs/devices/linux-libreelec-g++"
-  QMAKE_CONF="${QMAKE_CONF_DIR}/qmake.conf"
 
   cd ..
-  mkdir -p $QMAKE_CONF_DIR
-  echo "MAKEFILE_GENERATOR       = UNIX" > $QMAKE_CONF
-  echo "CONFIG                  += incremental" >> $QMAKE_CONF
-  echo "QMAKE_INCREMENTAL_STYLE  = sublib" >> $QMAKE_CONF
-  echo "include(../../common/linux.conf)" >> $QMAKE_CONF
-  echo "include(../../common/gcc-base-unix.conf)" >> $QMAKE_CONF
-  echo "include(../../common/g++-unix.conf)" >> $QMAKE_CONF
-  echo "load(device_config)" >> $QMAKE_CONF
-  echo "QMAKE_CC                = $CC" >> $QMAKE_CONF
-  echo "QMAKE_CXX               = $CXX" >> $QMAKE_CONF
-  echo "QMAKE_LINK              = $CXX" >> $QMAKE_CONF
-  echo "QMAKE_LINK_SHLIB        = $CXX" >> $QMAKE_CONF
-  echo "QMAKE_AR                = $AR cqs" >> $QMAKE_CONF
-  echo "QMAKE_OBJCOPY           = $OBJCOPY" >> $QMAKE_CONF
-  echo "QMAKE_NM                = $NM -P" >> $QMAKE_CONF
-  echo "QMAKE_STRIP             = $STRIP" >> $QMAKE_CONF
-  echo "QMAKE_CFLAGS = $CFLAGS" >> $QMAKE_CONF
-  echo "QMAKE_CXXFLAGS = $CXXFLAGS" >> $QMAKE_CONF
-  echo "QMAKE_LFLAGS = $LDFLAGS" >> $QMAKE_CONF
-  echo "load(qt_config)" >> $QMAKE_CONF
-  echo '#include "../../linux-g++/qplatformdefs.h"' >> $QMAKE_CONF_DIR/qplatformdefs.h
+  mkdir -p ${QMAKE_CONF_DIR}
+
+  cat >"${QMAKE_CONF_DIR}/qmake.conf" <<EOF
+MAKEFILE_GENERATOR      = UNIX
+CONFIG                 += incremental
+QMAKE_INCREMENTAL_STYLE = sublib
+include(../../common/linux.conf)
+include(../../common/gcc-base-unix.conf)
+include(../../common/g++-unix.conf)
+load(device_config)
+QMAKE_CC         = ${CC}
+QMAKE_CXX        = ${CXX}
+QMAKE_LINK       = ${CXX}
+QMAKE_LINK_SHLIB = ${CXX}
+QMAKE_AR         = ${AR} cqs
+QMAKE_OBJCOPY    = ${OBJCOPY}
+QMAKE_NM         = ${NM} -P
+QMAKE_STRIP      = ${STRIP}
+QMAKE_CFLAGS     = ${CFLAGS}
+QMAKE_CXXFLAGS   = ${CXXFLAGS}
+QMAKE_LFLAGS     = ${LDFLAGS}
+load(qt_config)
+EOF
+
+  cat >"${QMAKE_CONF_DIR}/qplatformdefs.h" <<EOF
+#include "../../linux-g++/qplatformdefs.h"
+EOF
 
   unset CC CXX LD RANLIB AR AS CPPFLAGS CFLAGS LDFLAGS CXXFLAGS
-  ./configure $PKG_CONFIGURE_OPTS_TARGET
+  ./configure ${PKG_CONFIGURE_OPTS_TARGET}
+}
+
+post_makeinstall_target() {
+  # Qt installs directly to $SYSROOT_PREFIX so don't rely on scripts/build fixing this up
+  # PKG_ORIG_SYSROOT_PREFIX will be undefined when performing a legacy build
+  sed -e "s:\(['= ]\)/usr:\\1${PKG_ORIG_SYSROOT_PREFIX:-${SYSROOT_PREFIX}}/usr:g" -i "${PKG_ORIG_SYSROOT_PREFIX:-${SYSROOT_PREFIX}}/usr/lib"/libQt*.la
 }
