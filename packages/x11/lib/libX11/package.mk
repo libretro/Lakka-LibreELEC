68a8b60914637c470726bb8b8c1749e0bf39f4a9
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-02-28 06:35:32 +0000

    libX11: update to libX11-1.6.7

--- packages/x11/lib/libX11/package.mk
+++ packages/x11/lib/libX11/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libX11"
-PKG_VERSION="1.6.5"
-PKG_SHA256="4d3890db2ba225ba8c55ca63c6409c1ebb078a2806de59fb16342768ae63435d"
+PKG_VERSION="1.6.7"
+PKG_SHA256="910e9e30efba4ad3672ca277741c2728aebffa7bc526f04dcfa74df2e52a1348"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.x.org/"
 PKG_URL="http://xorg.freedesktop.org/archive/individual/lib/$PKG_NAME-$PKG_VERSION.tar.bz2"
