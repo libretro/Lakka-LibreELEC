eccee33b9415c9f03c1c69d74e41e6a8bf8a3050
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-02-28 06:44:53 +0000

    xkbcomp: update to xkbcomp-1.4.2

--- packages/x11/app/xkbcomp/package.mk
+++ packages/x11/app/xkbcomp/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="xkbcomp"
-PKG_VERSION="1.4.1"
-PKG_SHA256="748dc4cf58ac95684106bd9cf163ac6ab7de9a236faec02a6f4d4006d63a5736"
+PKG_VERSION="1.4.2"
+PKG_SHA256="6dd8bcb9be7e85bd7294abe261b8c7b0539d2fc93e41b80fb8bd013767ce8424"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.X.org"
 PKG_URL="http://xorg.freedesktop.org/archive/individual/app/$PKG_NAME-$PKG_VERSION.tar.bz2"
