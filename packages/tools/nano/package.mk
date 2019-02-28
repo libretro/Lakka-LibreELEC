45cd696e7855f68bfd516dcead8f5030a42b0bce
Ian Leonard <antonlacon@gmail.com>
2019-02-28 22:36:34 +0000

    nano: update to 3.2
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- packages/tools/nano/package.mk
+++ packages/tools/nano/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="nano"
-PKG_VERSION="3.0"
-PKG_SHA256="e0a5bca354514e64762c987c200a8758b05e7bcced3b00b3e48ea0a2d383c8a0"
+PKG_VERSION="3.2"
+PKG_SHA256="d12773af3589994b2e4982c5792b07c6240da5b86c5aef2103ab13b401fe6349"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.nano-editor.org/"
 PKG_URL="http://ftpmirror.gnu.org/nano/$PKG_NAME-$PKG_VERSION.tar.xz"
