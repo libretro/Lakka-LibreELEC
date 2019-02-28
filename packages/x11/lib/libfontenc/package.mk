656726331c58ef8fc0f877c68ebe59a3618db0fe
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-02-28 06:40:37 +0000

    libfontenc: update to libfontenc-1.1.4

--- packages/x11/lib/libfontenc/package.mk
+++ packages/x11/lib/libfontenc/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libfontenc"
-PKG_VERSION="1.1.3"
-PKG_SHA256="70588930e6fc9542ff38e0884778fbc6e6febf21adbab92fd8f524fe60aefd21"
+PKG_VERSION="1.1.4"
+PKG_SHA256="2cfcce810ddd48f2e5dc658d28c1808e86dcf303eaff16728b9aa3dbc0092079"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.X.org"
 PKG_URL="http://xorg.freedesktop.org/archive/individual/lib/$PKG_NAME-$PKG_VERSION.tar.bz2"
