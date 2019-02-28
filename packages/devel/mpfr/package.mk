1cde3762672b9bed2ac364dcfc2e12793da2088c
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-02-28 09:23:21 +0000

    mpfr: update to mpfr-4.0.2

--- packages/devel/mpfr/package.mk
+++ packages/devel/mpfr/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mpfr"
-PKG_VERSION="4.0.1"
-PKG_SHA256="67874a60826303ee2fb6affc6dc0ddd3e749e9bfcb4c8655e3953d0458a6e16e"
+PKG_VERSION="4.0.2"
+PKG_SHA256="1d3be708604eae0e42d578ba93b390c2a145f17743a744d8f3f8c2ad5855a38a"
 PKG_LICENSE="LGPL"
 PKG_SITE="http://www.mpfr.org/"
 PKG_URL="http://ftpmirror.gnu.org/mpfr/$PKG_NAME-$PKG_VERSION.tar.xz"
