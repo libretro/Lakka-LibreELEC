fd7f8fdbaa27bff739f2b3fa5378389a15a4bb41
Jean-André Santoni <jean.andre.santoni@gmail.com>
2019-03-10 17:52:55 +0100

    Update mupen64plus

--- packages/libretro/mupen64plus/package.mk
+++ packages/libretro/mupen64plus/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="mupen64plus"
-PKG_VERSION="0064cb4"
+PKG_VERSION="be81697"
 PKG_REV="1"
 PKG_ARCH="arm i386 x86_64"
 PKG_LICENSE="GPLv2"
