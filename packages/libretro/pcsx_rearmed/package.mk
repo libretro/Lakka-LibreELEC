d51e47b1ec0b3e30243bbab615e523b87975cdb2
Jean-André Santoni <jean.andre.santoni@gmail.com>
2019-03-12 16:17:59 +0100

    Revert pcsx_rearmed upgrade

--- packages/libretro/pcsx_rearmed/package.mk
+++ packages/libretro/pcsx_rearmed/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="pcsx_rearmed"
-PKG_VERSION="4f8453c"
+PKG_VERSION="28ea3e2"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
