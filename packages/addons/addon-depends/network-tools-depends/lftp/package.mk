db4c0fa053884a570f9e5fa789a7f84e48efae72
Ian Leonard <antonlacon@gmail.com>
2019-03-16 23:04:24 +0000

    lftp: add libidn2 as depend
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- packages/addons/addon-depends/network-tools-depends/lftp/package.mk
+++ packages/addons/addon-depends/network-tools-depends/lftp/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="4ebc271e9e5cea84a683375a0f7e91086e5dac90c5d51bb3f169f75386107a62"
 PKG_LICENSE="GPLv3"
 PKG_SITE="http://lftp.yar.ru/"
 PKG_URL="http://lftp.yar.ru/ftp/${PKG_NAME}-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_TARGET="toolchain readline openssl zlib"
+PKG_DEPENDS_TARGET="toolchain readline openssl zlib libidn2"
 PKG_LONGDESC="A sophisticated ftp/http client, and a file transfer program supporting a number of network protocols."
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-nls \
