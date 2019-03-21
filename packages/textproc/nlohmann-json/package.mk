add7e6d26efe260347409d4ea1819589dd30e281
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-03-21 13:51:53 +0000

    nlohmann-json: update to nlohmann-json-3.6.1

--- packages/textproc/nlohmann-json/package.mk
+++ packages/textproc/nlohmann-json/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="nlohmann-json"
-PKG_VERSION="3.5.0"
-PKG_SHA256="e0b1fc6cc6ca05706cce99118a87aca5248bd9db3113e703023d23f044995c1d"
+PKG_VERSION="3.6.1"
+PKG_SHA256="80c45b090e40bf3d7a7f2a6e9f36206d3ff710acfa8d8cc1f8c763bb3075e22e"
 PKG_LICENSE="MIT"
 PKG_SITE="https://nlohmann.github.io/json/"
 PKG_URL="https://github.com/nlohmann/json/archive/v$PKG_VERSION.tar.gz"
