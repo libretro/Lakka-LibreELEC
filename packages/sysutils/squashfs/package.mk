e691b3b6933f82c1fb2f75c0d9a8d85b533956b7
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-03-26 15:58:05 +0000

    packages: correct original copyright

--- packages/sysutils/squashfs/package.mk
+++ packages/sysutils/squashfs/package.mk
@@ -1,4 +1,5 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="squashfs"
