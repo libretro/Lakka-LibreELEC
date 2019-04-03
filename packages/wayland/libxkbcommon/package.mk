2c5f068be8981b1bb7aa04cbcb5232e689d354a7
5schatten <supervisedthinking@gmail.com>
2019-04-03 11:55:30 +0200

    libxkbcommon: updated to 0.8.4 / added meson opts

--- packages/wayland/libxkbcommon/package.mk
+++ packages/wayland/libxkbcommon/package.mk
@@ -1,17 +1,25 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libxkbcommon"
-PKG_VERSION="0.7.1"
-PKG_SHA256="ba59305d2e19e47c27ea065c2e0df96ebac6a3c6e97e28ae5620073b6084e68b"
-PKG_LICENSE="OSS"
+PKG_VERSION="0.8.4"
+PKG_SHA256="60ddcff932b7fd352752d51a5c4f04f3d0403230a584df9a2e0d5ed87c486c8b"
+PKG_LICENSE="MIT"
 PKG_SITE="http://xkbcommon.org"
 PKG_URL="http://xkbcommon.org/download/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain xkeyboard-config"
 PKG_LONGDESC="xkbcommon is a library to handle keyboard descriptions."
 
-if [ "$DISPLAYSERVER" = "x11" ]; then
-  PKG_CONFIGURE_OPTS_TARGET="--enable-x11"
+PKG_MESON_OPTS_TARGET="-Denable-docs=false"
+
+if [ "${DISPLAYSERVER}" = "x11" ]; then
+  PKG_MESON_OPTS_TARGET+=" -Denable-x11=true \
+                           -Denable-wayland=false"
+elif [ "${DISPLAYSERVER}" = "weston" ]; then
+  PKG_MESON_OPTS_TARGET+=" -Denable-x11=false \
+                           -Denable-wayland=true"
 else
-  PKG_CONFIGURE_OPTS_TARGET="--disable-x11"
+  PKG_MESON_OPTS_TARGET+=" -Denable-x11=false \
+                           -Denable-wayland=false"
 fi
