81052f530e50507aa2f03fd26b7f09cb5c96ede4
5schatten <supervisedthinking@gmail.com>
2019-04-07 10:30:12 +0200

    nano: updated to nano-4.0 / added syntax highlighting

--- packages/tools/nano/package.mk
+++ packages/tools/nano/package.mk
@@ -1,12 +1,13 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="nano"
-PKG_VERSION="3.2"
-PKG_SHA256="d12773af3589994b2e4982c5792b07c6240da5b86c5aef2103ab13b401fe6349"
+PKG_VERSION="4.0"
+PKG_SHA256="1e2fcfea35784624a7d86785768b772d58bb3995d1aec9176a27a113b1e9bac3"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.nano-editor.org/"
-PKG_URL="http://ftpmirror.gnu.org/nano/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_URL="http://ftpmirror.gnu.org/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain ncurses"
 PKG_LONGDESC="Nano is an enhanced clone of the Pico text editor."
 
@@ -16,5 +17,23 @@ PKG_CONFIGURE_OPTS_TARGET="--disable-utf8 \
                            --disable-wrapping"
 
 post_makeinstall_target() {
-  rm -rf $INSTALL/usr/share/nano
+  rm -rf ${INSTALL}/usr/share/nano
+
+  mkdir -p ${INSTALL}/etc
+  cp -a ${PKG_DIR}/config/* ${INSTALL}/etc/
+
+  mkdir -p ${INSTALL}/usr/share/nano 
+  for FILE_TYPES in \
+    css \
+    html \
+    java \
+    javascript \
+    json \
+    php \
+    python \
+    sh \
+    xml
+  do
+    cp -a ${PKG_BUILD}/syntax/${FILE_TYPES}.nanorc ${INSTALL}/usr/share/nano/
+  done
 }
