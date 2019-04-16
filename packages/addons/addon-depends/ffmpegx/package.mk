c15538b7c78254bf1f89846e4d46216f16856214
5schatten <supervisedthinking@gmail.com>
2019-04-16 23:23:41 +0200

    ffmpegx: removed RPi specific include CFLAGS for brcm2835 headers

--- packages/addons/addon-depends/ffmpegx/package.mk
+++ packages/addons/addon-depends/ffmpegx/package.mk
@@ -41,7 +41,6 @@ pre_configure_target() {
   LDFLAGS="$LDFLAGS -L$(get_build_dir gnutls)/.INSTALL_PKG/usr/lib"
 
   if [ "$KODIPLAYER_DRIVER" == "bcm2835-driver" ]; then
-    CFLAGS="-DRPI=1 -I$SYSROOT_PREFIX/usr/include/IL -I$SYSROOT_PREFIX/usr/include/interface/vcos/pthreads -I$SYSROOT_PREFIX/usr/include/interface/vmcs_host/linux $CFLAGS"
     PKG_FFMPEG_LIBS="-lbcm_host -ldl -lmmal -lmmal_core -lmmal_util -lvchiq_arm -lvcos -lvcsm"
   fi
 
