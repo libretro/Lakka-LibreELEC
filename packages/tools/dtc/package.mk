0695fb75e41487117ec9e3e844b3f69badea7e36
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-04-25 21:13:43 +0100

    dtc: drop host build - not required

--- packages/tools/dtc/package.mk
+++ packages/tools/dtc/package.mk
@@ -8,27 +8,10 @@ PKG_SHA256="14343cb204aaff386206ea27e39e93d6e35d9a797222e8426f95e57828ca6b94"
 PKG_LICENSE="GPL"
 PKG_SITE="https://git.kernel.org/pub/scm/utils/dtc/dtc.git/"
 PKG_URL="https://git.kernel.org/pub/scm/utils/dtc/dtc.git/snapshot/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_HOST="Python2:host swig:host"
-PKG_DEPENDS_TARGET="toolchain dtc:host"
+PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="The Device Tree Compiler"
 
-PKG_MAKE_OPTS_HOST="dtc libfdt"
 PKG_MAKE_OPTS_TARGET="dtc fdtput fdtget libfdt"
-
-makeinstall_host() {
-  mkdir -p $TOOLCHAIN/bin
-    cp -P $PKG_BUILD/dtc $TOOLCHAIN/bin
-    cp -P $PKG_BUILD/libfdt/libfdt.so $TOOLCHAIN/lib/
-}
-
-post_makeinstall_host() {
-  python ./pylibfdt/setup.py build_ext --inplace
-  exec_thread_safe python ./pylibfdt/setup.py install --prefix=$TOOLCHAIN
-}
-
-pre_make_target() {
-  make clean BIN=
-}
 
 makeinstall_target() {
   mkdir -p $INSTALL/usr/bin
