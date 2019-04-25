083e8cde8e70a7074401b2a20a57e77a6174f5f0
kszaq <kszaquitto@gmail.com>
2019-04-25 21:14:31 +0200

    packages: drop amlogic-specific packages

--- packages/addons/addon-depends/dvb-tools-depends/w_scan/package.mk
+++ packages/addons/addon-depends/dvb-tools-depends/w_scan/package.mk
@@ -11,14 +11,6 @@ PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A channel scan tool which generates ATSC, DVB-C, DVB-S/S2 and DVB-T channels.conf files."
 PKG_TOOLCHAIN="autotools"
 
-# aml 3.14 hack
-pre_configure_target() {
-  if [ "$LINUX" = "amlogic-3.14" -o "$LINUX" = "amlogic-3.10" ]; then
-    sed -i 's/DVB_HEADER=0/DVB_HEADER=1/g' $PKG_BUILD/configure*
-    sed -i 's/HAS_DVB_API5=0/HAS_DVB_API5=1/g' $PKG_BUILD/configure*
-  fi
-}
-
 makeinstall_target() {
   :
 }
--- packages/linux-driver-addons/dvb/crazycat_aml/source/default.py
+++
@@ -1,2 +0,0 @@
-# SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
