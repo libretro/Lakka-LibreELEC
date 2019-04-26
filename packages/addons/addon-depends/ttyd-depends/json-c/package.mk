e066418fcff38f698963497c2d5ee9f8ef9d40a7
CvH <namerp@googlemail.com>
2019-04-26 21:16:34 +0200

    json-c: initial package

---
+++ packages/addons/addon-depends/ttyd-depends/json-c/package.mk
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2010-2011 Roman Weber (roman@openelec.tv)
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="json-c"
+PKG_VERSION="3e81b4abe359c8128bb2b4127f4e8c8c057fb004"
+PKG_SHA256="85569b581e8ff0999de5a43a65332559df3aa386e757d76ac8a4915eb4fb783a"
+PKG_LICENSE="MIT"
+PKG_SITE="https://github.com/json-c/json-c"
+PKG_URL="https://github.com/json-c/json-c/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Implements a reference counting object model that allows you to easily construct JSON objects in C."
