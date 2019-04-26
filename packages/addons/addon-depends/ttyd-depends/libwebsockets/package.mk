22bf53e384e42f4b52147c684dddd20b174e2f32
CvH <namerp@googlemail.com>
2019-04-26 21:16:34 +0200

    libwebsockets: initial package

---
+++ packages/addons/addon-depends/ttyd-depends/libwebsockets/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="libwebsockets"
+PKG_VERSION="3.1.0"
+PKG_SHA256="db948be74c78fc13f1f1a55e76707d7baae3a1c8f62b625f639e8f2736298324"
+PKG_LICENSE="LGPL2+"
+PKG_SITE="https://libwebsockets.org"
+PKG_URL="https://github.com/warmcat/libwebsockets/archive/v$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain openssl json-c"
+PKG_LONGDESC="Library for implementing network protocols with a tiny footprint."
