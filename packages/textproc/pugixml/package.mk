961f7c0e300cfb15eeeea0c5048eb267ea1f4f78
5schatten <supervisedthinking@gmail.com>
2019-05-05 22:17:13 +0200

    pugixml: initial package v1.9

---
+++ packages/textproc/pugixml/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="pugixml"
+PKG_VERSION="1.9"
+PKG_SHA256="7ac060048bf00da4b3902cf411f0ba9f048cdc5580eed1f5a27ff51be4a38243"
+PKG_LICENSE="MIT"
+PKG_SITE="https://pugixml.org/"
+PKG_URL="https://github.com/zeux/pugixml/archive/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Light-weight, simple and fast XML parser for C++ with XPath support."
