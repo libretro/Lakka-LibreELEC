d015207763d73be5f4a1ff729e108cc01fdcacac
Matthias Reichl <hias@horus.com>
2019-05-11 10:42:23 +0200

    systemd: add version-tag build option
    Without this option systemd 241 and newer builds will run git describe
    and report the LibreELEC git tree info as version, eg in journal
    
    systemd 8.95.002-927-gb1cdc76 running in system mode.
    
    With version-tag set the version is reported like before
    
    systemd 242 running in system mode.
    
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/sysutils/systemd/package.mk
+++ packages/sysutils/systemd/package.mk
@@ -88,7 +88,8 @@ PKG_MESON_OPTS_TARGET="--libdir=/usr/lib \
                        -Dzshcompletiondir=no \
                        -Dkmod-path=/usr/bin/kmod \
                        -Dmount-path=/usr/bin/mount \
-                       -Dumount-path=/usr/bin/umount"
+                       -Dumount-path=/usr/bin/umount \
+                       -Dversion-tag=${PKG_VERSION}"
 
 pre_configure_target() {
   export CFLAGS="$CFLAGS -fno-schedule-insns -fno-schedule-insns2 -Wno-format-truncation"
