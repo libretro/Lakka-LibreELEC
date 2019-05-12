2b8f502334432a2d6168801429aa922e4621fc78
plaidman <jrtomsic@gmail.com>
2019-05-11 21:12:02 -0400

    max overdrive clocks, update retroarch

--- packages/libretro/retroarch/package.mk
+++ packages/libretro/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="177fd8b"
+PKG_VERSION="ef5d15e"
 PKG_REV="11"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
