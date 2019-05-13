57ecc366108cec6dbc879fe0b514869ab3bcf08c
plaidman <jrtomsic@gmail.com>
2019-05-12 22:08:02 -0400

    update RA to fix battery pack

--- packages/libretro/retroarch/package.mk
+++ packages/libretro/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="ef5d15e"
+PKG_VERSION="59d8d70"
 PKG_REV="11"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
