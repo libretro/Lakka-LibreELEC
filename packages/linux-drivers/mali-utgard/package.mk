a8f0af8f9ffb8eaafd58370b9f510f715984eec7
kszaq <kszaquitto@gmail.com>
2019-05-18 00:41:07 +0200

    gpu-sunxi: rename to mali-utgard, prepare for adding platforms

--- packages/linux-drivers/mali-utgard/package.mk
+++ packages/linux-drivers/mali-utgard/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
-PKG_NAME="gpu-sunxi"
+PKG_NAME="mali-utgard"
 PKG_VERSION="r9p0-01rel0"
 PKG_SHA256="7a67127341d17640c1fff5dad80258fb2a37c8a2121b81525fe2327e4532ce2b"
 PKG_ARCH="arm aarch64"
@@ -10,14 +10,20 @@ PKG_SITE="https://developer.arm.com/products/software/mali-drivers/utgard-kernel
 PKG_URL="https://developer.arm.com/-/media/Files/downloads/mali-drivers/kernel/mali-utgard-gpu/DX910-SW-99002-$PKG_VERSION.tgz"
 PKG_DEPENDS_TARGET="toolchain linux"
 PKG_NEED_UNPACK="$LINUX_DEPENDS"
-PKG_LONGDESC="gpu-sunxi: Linux drivers for Mali GPUs found in Allwinner SoCs"
+PKG_LONGDESC="mali-utgard: Linux drivers for Mali Utgard GPUs"
 PKG_TOOLCHAIN="manual"
 PKG_IS_KERNEL_PKG="yes"
 
+case $PROJECT in
+  Allwinner)
+    PKG_MALI_PLATFORM="sunxi"
+    ;;
+esac
+
 make_target() {
-  kernel_make -C $(kernel_path) M=$PKG_BUILD/driver/src/devicedrv/mali/ \
-    MALI_PLATFORM_FILES=platform/sunxi/sunxi.c \
-    EXTRA_CFLAGS="-DMALI_FAKE_PLATFORM_DEVICE=1 -DCONFIG_MALI_DMA_BUF_MAP_ON_ATTACH" \
+  kernel_make -C $(kernel_path) M=$PKG_BUILD/driver/src/devicedrv/mali \
+    MALI_PLATFORM_FILES="platform/${PKG_MALI_PLATFORM}/${PKG_MALI_PLATFORM}.c" \
+    EXTRA_CFLAGS="-DMALI_FAKE_PLATFORM_DEVICE=1 -DCONFIG_MALI_DMA_BUF_MAP_ON_ATTACH -DCONFIG_MALI400=1 -DCONFIG_MALI450=1 -DCONFIG_MALI470=1" \
     CONFIG_MALI400=m
 }
 
