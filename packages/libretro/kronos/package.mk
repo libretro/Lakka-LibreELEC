47595a4727e7fa0203d8fbecf9ee3cb6f2198a37
plaidman <jrtomsic@gmail.com>
2019-05-21 05:42:09 -0400

    kronos - update to remove patch file

--- packages/libretro/kronos/package.mk
+++ packages/libretro/kronos/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="kronos"
-PKG_VERSION="23b6d99"
+PKG_VERSION="ac74c14"
 PKG_GIT_BRANCH="kronos"
 PKG_REV="1"
 PKG_ARCH="any"
