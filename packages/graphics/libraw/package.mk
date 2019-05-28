4a0f61d371e6d9a7567afa382fddb28d601e980f
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-05-28 18:37:57 +0100

    libraw: update to libraw-0.19.2
    Demosaic support has been dropped, as the original code is out of date
    and abandoned. See README.demosaic-packs.

--- packages/graphics/libraw/package.mk
+++ packages/graphics/libraw/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libraw"
-PKG_VERSION="0.18.7"
-PKG_SHA256="87e347c261a8e87935d9a23afd750d27676b99f540e8552314d40db0ea315771"
+PKG_VERSION="0.19.2"
+PKG_SHA256="400d47969292291d297873a06fb0535ccce70728117463927ddd9452aa849644"
 PKG_LICENSE="LGPL"
 PKG_SITE="http://www.libraw.org/"
 PKG_URL="http://www.libraw.org/data/LibRaw-$PKG_VERSION.tar.gz"
@@ -17,6 +17,4 @@ PKG_CONFIGURE_OPTS_TARGET="--enable-static \
                            --enable-jpeg \
                            --disable-jasper \
                            --disable-lcms \
-                           --disable-examples \
-                           --disable-demosaic-pack-gpl2 \
-                           --disable-demosaic-pack-gpl3"
+                           --disable-examples"
