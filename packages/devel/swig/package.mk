7b3be161d0b5cb5f942555409707332d14d52582
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-05-28 18:37:57 +0100

    swig: update to swig-4.0.0

--- packages/devel/swig/package.mk
+++ packages/devel/swig/package.mk
@@ -1,16 +1,17 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="swig"
-PKG_VERSION="3.0.12"
-PKG_SHA256="7cf9f447ae7ed1c51722efc45e7f14418d15d7a1e143ac9f09a668999f4fc94d"
+PKG_VERSION="4.0.0"
+PKG_SHA256="e8a39cd6437e342cdcbd5af27a9bf11b62dc9efec9248065debcb8276fcbb925"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.swig.org"
 PKG_URL="$SOURCEFORGE_SRC/swig/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_HOST="ccache:host"
 PKG_LONGDESC="SWIG is a software development tool that connects programs written in C and C++ with a variety of high-level programming languages."
 
-PKG_CONFIGURE_OPTS_HOST="--program-suffix=3.0 \
+PKG_CONFIGURE_OPTS_HOST="--program-suffix=4.0 \
                          --with-pcre-prefix=$TOOLCHAIN \
                          --with-boost=no \
                          --without-pcre \
@@ -39,5 +40,5 @@ PKG_CONFIGURE_OPTS_HOST="--program-suffix=3.0 \
                          --without-d"
 
 post_makeinstall_host() {
-  ln -sf swig3.0 $TOOLCHAIN/bin/swig
+  ln -sf swig4.0 $TOOLCHAIN/bin/swig
 }
