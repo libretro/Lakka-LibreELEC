6291edb06b5a8cb11e473a9fb9ce2fe6ef3f38a4
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-05-28 18:37:57 +0100

    glib: update to glib-2.61.1

--- packages/devel/glib/package.mk
+++ packages/devel/glib/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="glib"
-PKG_VERSION="2.59.3"
-PKG_SHA256="dfefafbc37bbcfb8101f3f181f880e8b7a8bee48620c92869ec4ef1d3d648e5e"
+PKG_VERSION="2.61.1"
+PKG_SHA256="f8d827955f0d8e197ff5c2105dd6ac4f6b63d15cd021eb1de66534c92a762161"
 PKG_LICENSE="LGPL"
 PKG_SITE="http://www.gtk.org/"
 PKG_URL="http://ftp.gnome.org/pub/gnome/sources/glib/${PKG_VERSION%.*}/$PKG_NAME-$PKG_VERSION.tar.xz"
