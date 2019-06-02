a9e2056469e04cc1e2d511846b9aeaa7f844fa72
CvH <namerp@googlemail.com>
2019-06-02 22:29:06 +0200

    crazycat: update to 2019-05-12

--- packages/linux-driver-addons/dvb/crazycat/package.mk
+++ packages/linux-driver-addons/dvb/crazycat/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="crazycat"
-PKG_VERSION="c59ee3624b7f6bf5bdaeec7c73c69ef4938d3952"
-PKG_SHA256="00b9fade221327e1aed8f06ef0477f2761b57cc5fe6310c4536e361b17a595d1"
+PKG_VERSION="532599d255411a24f93b585a92b1b0c49e2012f7"
+PKG_SHA256="0e3addc3562057a77edefdde0052a78aec145c4dd5b737b53dd25ce389b95093"
 PKG_LICENSE="GPL"
 PKG_SITE="https://bitbucket.org/CrazyCat/media_build"
 PKG_URL="https://bitbucket.org/CrazyCat/media_build/get/$PKG_VERSION.tar.gz"
