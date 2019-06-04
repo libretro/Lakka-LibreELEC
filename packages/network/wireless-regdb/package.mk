f25a4c71595b52019be08814aa310746d98d20de
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-06-04 18:06:43 +0100

    wireless-regdb: update to wireless-regdb-2019.06.03

--- packages/network/wireless-regdb/package.mk
+++ packages/network/wireless-regdb/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="wireless-regdb"
-PKG_VERSION="2019.03.01"
-PKG_SHA256="3a4e6f7006599bc5764f86e1e86422710da13ad80e0242147b61c6855ebc915f"
+PKG_VERSION="2019.06.03"
+PKG_SHA256="cd917ed86b63ce8d93947979f1f18948f03a4ac0ad89ec25227b36ac00dc54bf"
 PKG_LICENSE="GPL"
 PKG_SITE="http://wireless.kernel.org/en/developers/Regulatory"
 PKG_URL="https://www.kernel.org/pub/software/network/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.xz"
