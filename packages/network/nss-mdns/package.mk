e6107599a7e40b38eea1cbea5f54e9a924663a64
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-06-12 22:25:54 +0100

    nss-mdns: update to nss-mdns-0.14.1

--- packages/network/nss-mdns/package.mk
+++ packages/network/nss-mdns/package.mk
@@ -1,12 +1,13 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="nss-mdns"
-PKG_VERSION="47edc38"
-PKG_SHA256="f02e8baeceea30e82a2ecdaa8cafdbcabfdaa33a766f6942e7dc8aa81948f7b6"
+PKG_VERSION="0.14.1"
+PKG_SHA256="4fe54bffd20e410fc41382dc6c4708cdfa3a65f50c3753f262dc4c78fd864a6e"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/lathiat/nss-mdns"
-PKG_URL="https://github.com/lathiat/nss-mdns/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/lathiat/nss-mdns/archive/v$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain avahi"
 PKG_LONGDESC="A plugin for nss to allow name resolution via Multicast DNS."
 PKG_TOOLCHAIN="autotools"
