8e9caeeefefcb1641e9413473ad774a0a290587b
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-06-20 17:34:51 +0100

    pvr.freebox: new package pvr.freebox-2.2.0-Matrix

---
+++ packages/mediacenter/kodi-binary-addons/pvr.freebox/package.mk
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="pvr.freebox"
+PKG_VERSION="2.2.0-Matrix"
+PKG_SHA256="4195031c9bc63077cf03ce445679e8ab9a6b508c6b28ec842c296ce925966631"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/aassif/pvr.freebox"
+PKG_URL="https://github.com/aassif/pvr.freebox/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform rapidjson openssl zlib"
+PKG_SECTION=""
+PKG_SHORTDESC="pvr.freebox"
+PKG_LONGDESC="pvr.freebox"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="xbmc.pvrclient"
