63c2b8d23bf77f37ce96a5131e579455b48bbba9
Matthias Reichl <hias@horus.com>
2019-06-25 22:54:32 +0200

    bcmstat: update to 0.5.2
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/tools/bcmstat/package.mk
+++ packages/tools/bcmstat/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="bcmstat"
-PKG_VERSION="0.5.1"
-PKG_SHA256="88b3b189170e7d0eee83a233565d9ef0fb323489dc1161ef2392fca50e9637d6"
+PKG_VERSION="0.5.2"
+PKG_SHA256="55dec681b8dddb3b4d98e339644fa68ff3d0f05d287b09037cbad4d450df7a3a"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/MilhouseVH/bcmstat"
 PKG_URL="https://github.com/MilhouseVH/$PKG_NAME/archive/$PKG_VERSION.tar.gz"
