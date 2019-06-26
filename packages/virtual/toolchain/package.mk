a4b0706441895bfcac469af816af692c3ef146d7
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-06-26 06:04:12 +0100

    toolchain: build pigz:host

--- packages/virtual/toolchain/package.mk
+++ packages/virtual/toolchain/package.mk
@@ -1,11 +1,12 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="toolchain"
 PKG_VERSION=""
 PKG_LICENSE="GPL"
 PKG_SITE="https://libreelec.tv"
 PKG_URL=""
-PKG_DEPENDS_TARGET="configtools:host make:host xz:host sed:host pkg-config:host autoconf:host automake:host intltool:host libtool:host autoconf-archive:host gcc:host bison:host flex:host cmake:host xmlstarlet:host p7zip:host ninja:host meson:host"
+PKG_DEPENDS_TARGET="configtools:host make:host xz:host sed:host pkg-config:host autoconf:host automake:host intltool:host libtool:host autoconf-archive:host gcc:host bison:host flex:host cmake:host xmlstarlet:host p7zip:host pigz:host ninja:host meson:host"
 PKG_SECTION="virtual"
 PKG_LONGDESC="LibreELEC.tv' toolchain to compile all packages"
