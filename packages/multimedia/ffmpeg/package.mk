4ba7e76c647397e36114f3128074ca6e0095cb26
Matthias Reichl <hias@horus.com>
2019-06-27 21:32:24 +0200

    ffmpeg: move RPi and RPi4 HEVC patches to separate patch dirs
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/multimedia/ffmpeg/package.mk
+++ packages/multimedia/ffmpeg/package.mk
@@ -59,6 +59,11 @@ fi
 
 if [ "$PROJECT" = "RPi" ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET bcm2835-driver"
+  if [ "$DEVICE" = "RPi4" ]; then
+   PKG_PATCH_DIRS+=" rpi4-hevc"
+  else
+   PKG_PATCH_DIRS+=" rpi-hevc"
+ fi
 fi
 
 if target_has_feature neon; then
