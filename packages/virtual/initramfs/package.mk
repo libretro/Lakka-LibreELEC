7aa93e98bbc4c0fd1fa517dc1743662e5b42e96e
Tomas Kelemen (vudiq) <vudiq@vudiq.sk>
2019-06-30 22:21:01 +0200

    Gamegirl options update

--- packages/virtual/initramfs/package.mk
+++ packages/virtual/initramfs/package.mk
@@ -19,6 +19,10 @@ if [ "$INITRAMFS_PARTED_SUPPORT" = yes ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET parted:init"
 fi
 
+if [ "$PROJECT" = "Gamegirl" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET gamegirl-screen:init"
+fi
+
 post_install() {
   ( cd $BUILD/initramfs
     if [ "$TARGET_ARCH" = "x86_64" ]; then
