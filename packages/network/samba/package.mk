fd1d9fbc846b13826ef37b54c3c802ee7ef8b246
Ntemis <ierokipides.dem@gmail.com>
2019-07-01 12:21:24 +0300

    A small bump in samba version to same series latest version

--- packages/network/samba/package.mk
+++ packages/network/samba/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="samba"
-PKG_VERSION="4.6.14"
+PKG_VERSION="4.6.16"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3+"
 PKG_SITE="https://www.samba.org"
