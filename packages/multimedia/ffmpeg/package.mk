c58d936966689df6ede61ed2a2ae80762428e8d7
Matthias Reichl <hias@horus.com>
2019-07-02 10:49:30 +0200

    ffmpeg: move RPi patch to separate patch dir
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/multimedia/ffmpeg/package.mk
+++ packages/multimedia/ffmpeg/package.mk
@@ -59,6 +59,7 @@ fi
 
 if [ "$KODIPLAYER_DRIVER" = "bcm2835-driver" ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET bcm2835-driver"
+  PKG_PATCH_DIRS+=" rpi-hevc"
 fi
 
 if target_has_feature neon; then
@@ -83,8 +84,6 @@ pre_configure_target() {
   if [ "$KODIPLAYER_DRIVER" = "bcm2835-driver" ]; then
     PKG_FFMPEG_LIBS="-lbcm_host -lvcos -lvchiq_arm -lmmal -lmmal_core -lmmal_util -lvcsm"
     PKG_FFMPEG_RPI="--enable-rpi"
-  else
-    PKG_FFMPEG_RPI="--disable-rpi"
   fi
 }
 
