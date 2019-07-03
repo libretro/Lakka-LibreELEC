e3decf49b95e68ba05d588040e29398fd9c3dde8
Lukas Rusak <lorusak@gmail.com>
2019-07-03 13:19:39 -0700

    firmware-imx: update to 7.9 and add sdma firmware

--- packages/linux-firmware/firmware-imx/package.mk
+++ packages/linux-firmware/firmware-imx/package.mk
@@ -3,12 +3,12 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="firmware-imx"
-PKG_VERSION="5.4"
-PKG_SHA256="c5bd4bff48cce9715a5d6d2c190ff3cd2262c7196f7facb9b0eda231c92cc223"
+PKG_VERSION="7.9"
+PKG_SHA256="30e22c3e24a8025d60c52ed5a479e30fad3ad72127c84a870e69ec34e46ea8c0"
 PKG_ARCH="arm"
 PKG_LICENSE="other"
 PKG_SITE="http://www.freescale.com"
-PKG_URL="http://www.nxp.com/lgfiles/NMG/MAD/YOCTO/${PKG_NAME}-${PKG_VERSION}.bin"
+PKG_URL="https://www.nxp.com/lgfiles/NMG/MAD/YOCTO/${PKG_NAME}-${PKG_VERSION}.bin"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="firmware-imx: Freescale IMX firmware such as for the VPU"
 PKG_TOOLCHAIN="manual"
@@ -19,6 +19,9 @@ unpack() {
 }
 
 makeinstall_target() {
+  mkdir -p $INSTALL/$(get_full_firmware_dir)/imx/sdma
+    cp -P firmware/sdma/sdma-imx6q.bin $INSTALL/$(get_full_firmware_dir)/imx/sdma
+
   mkdir -p $INSTALL/$(get_full_firmware_dir)/vpu
     cp -P firmware/vpu/vpu_fw_imx6d.bin $INSTALL/$(get_full_firmware_dir)/vpu
     cp -P firmware/vpu/vpu_fw_imx6q.bin $INSTALL/$(get_full_firmware_dir)/vpu
