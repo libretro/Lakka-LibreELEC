3a1ce070fbed53d8d91f9e2767b74de4b286c4e8
M4xw <m4x@m4xw.net>
2019-07-09 11:53:17 +0200

    [PPSSPP] Bump Package

--- packages/libretro/ppsspp/package.mk
+++ packages/libretro/ppsspp/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="ppsspp"
-PKG_VERSION="e5f1598"
+PKG_VERSION="179cafc"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
