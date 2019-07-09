e87f3cbacb887706ff8f94ff6003c9f5804512e7
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-07-09 18:51:38 +0100

    xf86-video-nvidia: update to xf86-video-nvidia-430.34

--- packages/x11/driver/xf86-video-nvidia/package.mk
+++ packages/x11/driver/xf86-video-nvidia/package.mk
@@ -6,8 +6,8 @@ PKG_NAME="xf86-video-nvidia"
 # Remember to run "python packages/x11/driver/xf86-video-nvidia/scripts/make_nvidia_udev.py" and commit changes to
 # "packages/x11/driver/xf86-video-nvidia/udev.d/96-nvidia.rules" whenever bumping version.
 # Host may require installation of python-lxml and python-requests packages.
-PKG_VERSION="430.26"
-PKG_SHA256="a7dfb1bdd7591f42bbe501b8a9a80349415bdb51723db3cd11b08ce2e2d4679c"
+PKG_VERSION="430.34"
+PKG_SHA256="248ed9d3dee62eba86d0e0c7737218666aae204a0c0bb9c557b114db75edc523"
 PKG_ARCH="x86_64"
 PKG_LICENSE="nonfree"
 PKG_SITE="http://www.nvidia.com/"
