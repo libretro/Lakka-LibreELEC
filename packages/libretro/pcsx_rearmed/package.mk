0c049ad9402ba7b053f5542a6f6299a8bbdf5484
M4xw <m4x@m4xw.net>
2019-07-09 12:00:03 +0200

    [PCSX] Bump Package

--- packages/libretro/pcsx_rearmed/package.mk
+++ packages/libretro/pcsx_rearmed/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="pcsx_rearmed"
-PKG_VERSION="3796032"
+PKG_VERSION="7375bf9"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
