22e633640c638d464c3fbd967a3e977abc1ffe94
Tomas Kelemen (vudiq) <vudiq@vudiq.sk>
2019-07-13 22:46:50 +0200

    Play!: new libretro core - Sony Playstation 2

---
+++ packages/libretro/play/package.mk
@@ -0,0 +1,64 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.tv; see the file COPYING.  If not, write to
+#  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
+#  http://www.gnu.org/copyleft/gpl.html
+################################################################################
+
+PKG_NAME="play"
+PKG_VERSION="95ce2c3"
+PKG_ARCH="any"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/libretro/Play-"
+PKG_URL="$PKG_SITE.git"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="libretro"
+PKG_SHORTDESC="Play! is an attempt to create a PlayStation 2 emulator for Windows, macOS, UNIX, Android & iOS platforms."
+PKG_LONGDESC="Play! is an attempt to create a PlayStation 2 emulator for Windows, macOS, UNIX, Android & iOS platforms."
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+PKG_TOOLCHAIN="cmake"
+
+if [ "$OPENGL_SUPPORT" = yes ]; then
+  PKG_DEPENDS_TARGET+=" $OPENGL"
+fi
+
+if [ "$OPENGLES_SUPPORT" = yes ]; then
+  PKG_DEPENDS_TARGET+=" $OPENGLES"
+fi
+
+PKG_CMAKE_OPTS_TARGET="-DBUILD_LIBRETRO_CORE=yes \
+                       -DBUILD_PLAY=off \
+                       -DBUILD_TESTS=no \
+                       -DENABLE_AMAZON_S3=no \
+                       -DCMAKE_BUILD_TYPE=Release \
+                       --target play_libretro"
+
+if [ "$OPENGL_SUPPORT" = no -a "$OPENGLES_SUPPORT" = yes ]; then
+  PKG_CMAKE_OPTS_TARGET="-DUSE_GLES=yes $PKG_CMAKE_OPTS_TARGET"
+fi
+
+pre_make_target() {
+  find $PKG_BUILD -name flags.make -exec sed -i "s:isystem :I:g" \{} \;
+  find $PKG_BUILD -name build.ninja -exec sed -i "s:isystem :I:g" \{} \;
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/libretro
+  cp $PKG_BUILD/.$TARGET_NAME/Source/ui_libretro/play_libretro.so $INSTALL/usr/lib/libretro/
+}
