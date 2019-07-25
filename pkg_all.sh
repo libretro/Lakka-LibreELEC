ab861110278ad3608dadef26f42ced064a54cd56
Tomáš Kelemen <vudiq@vudiq.sk>
2019-07-25 10:09:26 +0200

    pkg_all.sh: enhancements (#706)
    - added Generic_VK_nvidia to the list of targets
    - build_folder based on OS_VERSION/LIBREELEC_VERSION/IGNORE_VERSION
    - added output before/after activity
    - cleanup

--- pkg_all.sh
+++ pkg_all.sh
@@ -13,10 +13,12 @@ case $1 in
 	clean)
 		action=$1
 		script="./scripts/clean"
+		activity="Cleaning"
 		;;
 	build)
 		action=$1
 		script="./scripts/build"
+		activity="Compilation"
 		;;
 	*)
 		usage
@@ -29,6 +31,7 @@ esac
 targets="\
 	Generic|i386||| \
 	Generic|x86_64||| \
+	Generic_VK_nvidia|x86_64||| \
 	RPi|arm||| \
 	RPi2|arm||| \
 	Allwinner|arm||Bananapi| \
@@ -57,6 +60,14 @@ declare -i failed=0
 failed_targets=""
 skipped_targets=""
 distro="Lakka"
+source distributions/${distro}/options
+if [ -z "$OS_VERSION" -o -z "$LIBREELEC_VERSION" ]; then
+	echo "OS_VERSION or LIBREELEC_VERSION empty / not set in 'distributions/${distro}/options'!"
+	echo "  OS_VERSION=$OS_VERSION"
+	echo "  LIBREELEC_VERSION=$LIBREELEC_VERSION"
+	echo "Bailing out!"
+	exit 127
+fi
 
 for T in $targets ; do
 	IFS='|' read -r -a build <<< "$T"
@@ -66,21 +77,35 @@ for T in $targets ; do
 	system=${build[3]}
 	board=${build[4]}
 	target_name=${board:-${device:-${project}}}.${arch}
-	if [ ! -d build.${distro}-${target_name}* ] ; then
+	if [ "$IGNORE_VERSION" = "1" ]; then
+		build_folder=build.${distro}-${target_name}
+	else
+		if [ "$LIBREELEC_VERSION" = "devel" ]; then
+			build_folder=build.${distro}-${target_name}-${OS_VERSION}-${LIBREELEC_VERSION}
+		else
+			build_folder=build.${distro}-${target_name}-${LIBREELEC_VERSION}
+		fi
+	fi
+	echo "Processing package '$package' for '$target_name':"
+	if [ ! -d ${build_folder} ] ; then
 		skipped_targets="${skipped_targets}${target_name}\n"
+		echo "No build folder - skipping."
 	else
 		PROJECT=$project DEVICE=$device BOARD=$board ARCH=$arch $script $package
 		if [ $? -gt 0 ] ; then
 			failed+=1
 			failed_targets="${failed_targets}${target_name}\n"
+			echo "$activity of package '$package' failed for '$target_name'!"
+		else
+			echo "$activity of package '$package' succeeded for '$target_name'."
 		fi
 	fi
 done
-[ -n "$skipped_targets" ] && { echo -e "Following targets were skipped - could not find existing build folder:\n$skipped_targets\n\n" ; }
+[ -n "$skipped_targets" ] &&
+	echo -e "\nFollowing targets were skipped - could not find existing build folder:\n$skipped_targets\n" 
 if [ $failed -gt 0 ] ; then
 	echo -e "\nFailed to ${action} package '$package' on following targets:\n${failed_targets}" >&2
-	exit 127
 else
 	echo "Done."
-	exit 0
 fi
+exit $failed
