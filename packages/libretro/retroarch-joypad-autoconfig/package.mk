76e8c37dad60e62543913afa4590a8d337960013
Tomáš Kelemen <vudiq@vudiq.sk>
2019-07-27 09:41:15 +0200

    retroarch-joypad-autoconfig: update to latest (#728)
    includes udev config for 'Wireless Steam Controller'

--- packages/libretro/retroarch-joypad-autoconfig/package.mk
+++ packages/libretro/retroarch-joypad-autoconfig/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch-joypad-autoconfig"
-PKG_VERSION="6297c68"
+PKG_VERSION="a36201a"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
