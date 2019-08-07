d04402668963ca3373c1d717445b2a3b889977e7
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-08-08 00:29:57 +0100

    mesa: update to mesa-19.1.4

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa"
-PKG_VERSION="19.1.3"
-PKG_SHA256="6456c477919b256bd575478ea2f98a9261d4eab70dc4a18a22b9a5747956a3d3"
+PKG_VERSION="19.1.4"
+PKG_SHA256="d4856509db0fb63bfbb89027fc75fed55e5203e1a1be10f768bb6e1992cefc8d"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
 PKG_URL="https://github.com/mesa3d/mesa/archive/mesa-$PKG_VERSION.tar.gz"
