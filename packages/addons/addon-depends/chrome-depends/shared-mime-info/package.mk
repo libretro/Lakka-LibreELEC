2ad72df9573358b29f84f57444f52a36ce6f54d4
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-08-08 21:53:28 +0100

    shared-mime-info: fix target dependency

--- packages/addons/addon-depends/chrome-depends/shared-mime-info/package.mk
+++ packages/addons/addon-depends/chrome-depends/shared-mime-info/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="5c0133ec4e228e41bdf52f726d271a2d821499c2ab97afd3aa3d6cf43efcdc83"
 PKG_LICENSE="GPL2"
 PKG_SITE="https://freedesktop.org/wiki/Software/shared-mime-info/"
 PKG_URL="http://freedesktop.org/~hadess/shared-mime-info-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain libxml2"
+PKG_DEPENDS_TARGET="toolchain glib libxml2"
 PKG_LONGDESC="The shared-mime-info package contains the core database of common types."
 PKG_BUILD_FLAGS="-parallel"
 
