d64033342f4b6403ab15fd9845428c7743cbb7ed
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-08-08 21:53:28 +0100

    icu: fix host dependency

--- packages/addons/addon-depends/chrome-depends/icu/package.mk
+++ packages/addons/addon-depends/chrome-depends/icu/package.mk
@@ -7,6 +7,7 @@ PKG_SHA256="d007f89ae8a2543a53525c74359b65b36412fa84b3349f1400be6dcf409fafef"
 PKG_LICENSE="Custom"
 PKG_SITE="http://www.icu-project.org"
 PKG_URL="http://download.icu-project.org/files/icu4c/${PKG_VERSION}/icu4c-${PKG_VERSION//./_}-src.tgz"
+PKG_DEPENDS_HOST="gcc:host"
 PKG_DEPENDS_TARGET="toolchain icu:host"
 PKG_LONGDESC="International Components for Unicode library."
 
