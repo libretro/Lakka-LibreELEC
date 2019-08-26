426e7d5486fd495a3d525cd6c61205c98a61000e
5schatten <supervisedthinking@gmail.com>
2019-08-26 21:25:47 +0200

    libretro-mame2016: updated to git efc37c9 / fixed build for ARM / cleaned up / updated cross-compile patch

--- packages/emulation/libretro-mame2016/package.mk
+++ packages/emulation/libretro-mame2016/package.mk
@@ -2,38 +2,45 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libretro-mame2016"
-PKG_VERSION="e06d731644217f46bf5a7613222632d41a327f93"
-PKG_SHA256="60a8aaab5158868419f24cc0671f8bcba0a578aae46cae7b9482e2c332784553"
-PKG_ARCH="x86_64"
+PKG_VERSION="efc37c9968fe24af8d91d9ce6022e080a1fd90c3"
+PKG_SHA256="6033fad40c676b65d8892aa216c5a23d1043cc2776878244ee7ec39298a8cacd"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/mame2016-libretro"
-PKG_URL="https://github.com/libretro/mame2016-libretro/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="Late 2016 version of MAME (0.174) for libretro. Compatible with MAME 0.174 romsets"
+PKG_URL="https://github.com/libretro/mame2016-libretro/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain linux glibc alsa-lib"
+PKG_LONGDESC="Late 2016 version of MAME (0.174) for libretro. Compatible with MAME 0.174 romsets."
 PKG_TOOLCHAIN="make"
-PKG_BUILD_FLAGS="-gold"
 
 PKG_LIBNAME="mame2016_libretro.so"
-PKG_LIBPATH="$PKG_LIBNAME"
+PKG_LIBPATH="${PKG_LIBNAME}"
 PKG_LIBVAR="MAME2016_LIB"
 
 make_target() {
-  PTR64="1"
-  NOASM="0"
+  if [ "${ARCH}" = "arm" ]; then
+    PKG_NOASM="1"
+  else
+    PKG_NOASM="0"
+  fi
+
+  if [ "${ARCH}" = "x86_64" ]; then
+    PKG_PTR64="1"
+  else
+    PKG_PTR64="0"
+  fi
 
   make REGENIE=1 VERBOSE=1 NOWERROR=1 PYTHON_EXECUTABLE=python2 CONFIG=libretro \
-       LIBRETRO_OS="unix" ARCH="" PROJECT="" LIBRETRO_CPU="$ARCH" DISTRO="debian-stable" \
-       CROSS_BUILD="1" OVERRIDE_CC="$CC" OVERRIDE_CXX="$CXX" \
-       PTR64="$PTR64" TARGET="mame" \
-       SUBTARGET="arcade" PLATFORM="$ARCH" RETRO=1 OSD="retro"
+       LIBRETRO_OS="unix" ARCH="" PROJECT="" LIBRETRO_CPU="${ARCH}" DISTRO="debian-stable" \
+       CROSS_BUILD="1" OVERRIDE_CC="${CC}" OVERRIDE_CXX="${CXX}" \
+       PTR64="${PKG_PTR64}" NOASM="${PKG_NOASM}" TARGET="mame" \
+       SUBTARGET="arcade" PLATFORM="${ARCH}" RETRO=1 OSD="retro" GIT_VERSION=${PKG_VERSION:0:7}
 }
 
 post_make_target() {
-  mv $PKG_BUILD/mamearcade2016_libretro.so $PKG_BUILD/mame2016_libretro.so
+  mv ${PKG_BUILD}/mamearcade2016_libretro.so ${PKG_BUILD}/mame2016_libretro.so
 }
 
 makeinstall_target() {
-  mkdir -p $SYSROOT_PREFIX/usr/lib/cmake/$PKG_NAME
-  cp $PKG_LIBPATH $SYSROOT_PREFIX/usr/lib/$PKG_LIBNAME
-  echo "set($PKG_LIBVAR $SYSROOT_PREFIX/usr/lib/$PKG_LIBNAME)" > $SYSROOT_PREFIX/usr/lib/cmake/$PKG_NAME/$PKG_NAME-config.cmake
+  mkdir -p ${SYSROOT_PREFIX}/usr/lib/cmake/${PKG_NAME}
+  cp ${PKG_LIBPATH} ${SYSROOT_PREFIX}/usr/lib/${PKG_LIBNAME}
+  echo "set(${PKG_LIBVAR} ${SYSROOT_PREFIX}/usr/lib/${PKG_LIBNAME})" > ${SYSROOT_PREFIX}/usr/lib/cmake/${PKG_NAME}/${PKG_NAME}-config.cmake
 }
