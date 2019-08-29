a6c833d7de9cb5e759ff12195e0470f54abc06d2
Albert Malina <albert.malina@gmail.com>
2019-08-29 20:51:51 +0200

    retroarch: fix audio output for GPICase

--- packages/libretro/retroarch/package.mk
+++ packages/libretro/retroarch/package.mk
@@ -226,6 +226,11 @@ makeinstall_target() {
     sed -i -e "s/video_smooth = false/video_smooth = true/" $INSTALL/etc/retroarch.cfg
     sed -i -e "s/video_font_path =\/usr\/share\/retroarch-assets\/xmb\/monochrome\/font.ttf//" $INSTALL/etc/retroarch.cfg
   fi
+
+  # GPICase
+  if [ "$PROJECT" == "RPi" ] && [ "$BOARD" == "GPICase" ]; then
+    echo "audio_device = \"default:CARD=ALSA\"" >> $INSTALL/etc/retroarch.cfg
+  fi
 }
 
 post_install() {  
