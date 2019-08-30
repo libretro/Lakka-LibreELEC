eb0cff33fd61d3395c986a58e95cfee097eb0e56
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-08-30 18:18:07 +0100

    linux (Mainline): update to linux-5.2.11

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -29,8 +29,8 @@ case "$LINUX" in
     PKG_SOURCE_NAME="linux-$LINUX-$PKG_VERSION.tar.gz"
     ;;
   *)
-    PKG_VERSION="5.2.10"
-    PKG_SHA256="6096c4387c2a296de9136080942d11ae3f1bd28129b6952f7133c570e43bfc49"
+    PKG_VERSION="5.2.11"
+    PKG_SHA256="0c2a831f993dc8a8a8e1ca4186b467de72ff173c6f5855e2aab70f6f7fb033f9"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v5.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;
