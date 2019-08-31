221cf1402face3df343aed182bd11849814c8209
Albert Malina <albert.malina@gmail.com>
2019-08-31 09:12:00 +0200

    python-raspberry-gpio: add package

---
+++ packages/python/utils/python-raspberry-gpio/package.mk
@@ -0,0 +1,47 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+#
+#  OpenELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  OpenELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="python-raspberry-gpio"
+PKG_VERSION="0.7.0"
+PKG_ARCH="any"
+PKG_LICENSE="MIT"
+PKG_SITE="http://sourceforge.net/projects/raspberry-gpio-python"
+PKG_URL="https://files.pythonhosted.org/packages/source/R/RPi.GPIO/RPi.GPIO-${PKG_VERSION}.tar.gz"
+PKG_SOURCE_DIR="RPi.GPIO-${PKG_VERSION}"
+PKG_DEPENDS_TARGET="toolchain Python distutilscross:host"
+PKG_SECTION="python"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+pre_make_target() {
+  export PYTHONXCPREFIX="$SYSROOT_PREFIX/usr"
+  export LDSHARED="$CC -shared"
+}
+
+make_target() {
+  : nop
+}
+
+makeinstall_target() {
+  python setup.py install --root=$INSTALL --prefix=/usr
+}
+
+post_makeinstall_target() {
+  find $INSTALL/usr/lib -name "*.py" -exec rm -rf "{}" ";"
+}
