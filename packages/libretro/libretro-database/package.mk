0481ba89230e42ba317e7da14d7fb042eb0312dc
Nathan S <natinusala@gmail.com>
2019-09-01 14:47:38 +0200

    libretro-database: update
    Co-Authored-By: Rob Loach <robloach@gmail.com>

--- packages/libretro/libretro-database/package.mk
+++ packages/libretro/libretro-database/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="libretro-database"
-PKG_VERSION="5809b27"
+PKG_VERSION="2a8e537"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
