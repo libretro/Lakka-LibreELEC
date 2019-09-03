b35224fb38b89473bec15078984c4fc5bce11c75
5schatten <supervisedthinking@gmail.com>
2019-09-03 12:15:33 +0200

    containerd: updated to v1.2.8 / fixed displayed git commit

--- packages/addons/addon-depends/containerd/package.mk
+++ packages/addons/addon-depends/containerd/package.mk
@@ -3,14 +3,17 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="containerd"
-PKG_VERSION="1.2.7"
-PKG_SHA256="7179c709a0d187708a1eeddcbdecd7206b2c642dc4413bcdb049cd6b38d06801"
+PKG_VERSION="1.2.8"
+PKG_SHA256="6165ae2ad669d9ec6d317492d30a1511365bd31ad29efae757f19c1828bf75b3"
 PKG_LICENSE="APL"
 PKG_SITE="https://containerd.tools/"
 PKG_URL="https://github.com/containerd/containerd/archive/v$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain go:host"
 PKG_LONGDESC="A daemon to control runC, built for performance and density."
 PKG_TOOLCHAIN="manual"
+
+# Git commit of the matching release https://github.com/containerd/containerd/releases
+PKG_GIT_COMMIT="a4bc1d432a2c33aa2eed37f338dceabb93641310"
 
 pre_make_target() {
   case ${TARGET_ARCH} in
@@ -38,8 +41,8 @@ pre_make_target() {
   export CGO_ENABLED=1
   export CGO_NO_EMULATION=1
   export CGO_CFLAGS=${CFLAGS}
-  export CONTAINERD_VERSION=v${PKG_VERSION}
-  export CONTAINERD_REVISION=${PKG_VERSION}
+  export CONTAINERD_VERSION=${PKG_VERSION}
+  export CONTAINERD_REVISION=${PKG_GIT_COMMIT}
   export CONTAINERD_PKG=github.com/containerd/containerd
   export LDFLAGS="-w -extldflags -static -X ${CONTAINERD_PKG}/version.Version=${CONTAINERD_VERSION} -X ${CONTAINERD_PKG}/version.Revision=${CONTAINERD_REVISION} -X ${CONTAINERD_PKG}/version.Package=${CONTAINERD_PKG} -extld $CC"
   export GOLANG=${TOOLCHAIN}/lib/golang/bin/go
