e11aafe2ed57848ab714cf23e724c49a56f033bb
natinusala <natinusala@gmail.com>
2019-09-05 11:06:50 +0200

    easyrpg: add all optional dependencies

--- packages/multimedia/opusfile/package.mk
+++ packages/multimedia/opusfile/package.mk
@@ -18,31 +18,19 @@
 #  http://www.gnu.org/copyleft/gpl.html
 ################################################################################
 
-PKG_NAME="easyrpg"
-PKG_VERSION="00ad8ab"
-PKG_REV="2"
+PKG_NAME="opusfile"
+PKG_VERSION="58b229a"
+PKG_REV="1"
 PKG_ARCH="any"
-PKG_LICENSE="GPLv3"
-PKG_SITE="https://github.com/easyrpg/player"
+PKG_SITE="https://github.com/xiph/opusfile"
 PKG_GIT_URL="$PKG_SITE"
-PKG_DEPENDS_TARGET="toolchain zlib liblcf pixman libspeexdsp mpg123 libsndfile libvorbis opusfile wildmidi libxmp-lite"
+PKG_DEPENDS_TARGET="toolchain opus"
 PKG_PRIORITY="optional"
-PKG_SECTION="libretro"
-PKG_SHORTDESC="An unofficial libretro port of the EasyRPG/Player."
-PKG_LONGDESC="An unofficial libretro port of the EasyRPG/Player."
+PKG_SHORTDESC="Stand-alone decoder library for .opus streams"
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
-PKG_USE_CMAKE="yes"
 
-PKG_CMAKE_OPTS_TARGET="-DPLAYER_TARGET_PLATFORM=libretro -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release"
-
-pre_make_target() {
-  find $PKG_BUILD -name flags.make -exec sed -i "s:isystem :I:g" \{} \;
-  find $PKG_BUILD -name build.ninja -exec sed -i "s:isystem :I:g" \{} \;
-}
-
-makeinstall_target() {
-  mkdir -p $INSTALL/usr/lib/libretro
-  cp $PKG_BUILD/.$TARGET_NAME/easyrpg_libretro.so $INSTALL/usr/lib/libretro/
+pre_configure_target() {
+  ./autogen.sh
 }
