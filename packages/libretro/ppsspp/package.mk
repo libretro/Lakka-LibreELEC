d121796b927f290667ff4e67d1ca04323a2837b5
natinusala <natinusala@gmail.com>
2019-09-06 13:15:42 +0200

    ppsspp: update and fix, nevermind for the tag

--- packages/libretro/ppsspp/package.mk
+++ packages/libretro/ppsspp/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="ppsspp"
-PKG_VERSION="e66f019"
+PKG_VERSION="2439c3e"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
