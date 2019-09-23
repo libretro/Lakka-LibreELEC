c89fb3d5fb4f507d8a40c487b00ed48d64812953
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-09-24 00:26:15 +0100

    kernel-firmware: update to kernel-firmware-20190923

--- packages/linux-firmware/kernel-firmware/package.mk
+++ packages/linux-firmware/kernel-firmware/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="kernel-firmware"
-PKG_VERSION="20190717"
-PKG_SHA256="cdb28523dc6399922b8b322af034a1de536ff9ef000a6f992bbce82489fa978a"
+PKG_VERSION="20190923"
+PKG_SHA256="311d0d95ab18d1aa4764a76b4ddb30b3588aeea1772b00984ca627475ec72e81"
 PKG_LICENSE="other"
 PKG_SITE="https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/"
 PKG_URL="https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/snapshot/$PKG_VERSION.tar.gz"
