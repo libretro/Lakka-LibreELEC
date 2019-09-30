675d878cc5294e81c33efcf86baa5786e67ddbfc
Ian Leonard <antonlacon@gmail.com>
2019-09-30 04:33:40 +0000

    libgcrypt: update to 1.8.5
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- packages/security/libgcrypt/package.mk
+++ packages/security/libgcrypt/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libgcrypt"
-PKG_VERSION="1.8.4"
-PKG_SHA256="f638143a0672628fde0cad745e9b14deb85dffb175709cacc1f4fe24b93f2227"
+PKG_VERSION="1.8.5"
+PKG_SHA256="3b4a2a94cb637eff5bdebbcaf46f4d95c4f25206f459809339cdada0eb577ac3"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://www.gnupg.org/"
 PKG_URL="https://www.gnupg.org/ftp/gcrypt/libgcrypt/$PKG_NAME-$PKG_VERSION.tar.bz2"
