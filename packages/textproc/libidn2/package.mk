57243e6f1ad3308faf42ee6980046b1e78e9e086
Ian Leonard <antonlacon@gmail.com>
2019-09-30 03:31:09 +0000

    libidn2: update to 2.2.0
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- packages/textproc/libidn2/package.mk
+++ packages/textproc/libidn2/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libidn2"
-PKG_VERSION="2.1.1a"
-PKG_SHA256="57666bcf6ecf54230d7bac95c392379561954b57a673903aed4d3336b3048b72"
+PKG_VERSION="2.2.0"
+PKG_SHA256="fc734732b506d878753ec6606982bf7b936e868c25c30ddb0d83f7d7056381fe"
 PKG_LICENSE="LGPL3"
 PKG_SITE="https://www.gnu.org/software/libidn/"
 PKG_URL="http://ftpmirror.gnu.org/gnu/libidn/libidn2-$PKG_VERSION.tar.gz"
