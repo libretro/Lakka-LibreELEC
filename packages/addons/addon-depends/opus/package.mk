44d79450e4f8b008dda0f6951f033b25f281c4b4
Ian Leonard <antonlacon@gmail.com>
2019-10-02 00:40:26 +0000

    opus: update to 1.3.1
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- packages/addons/addon-depends/opus/package.mk
+++ packages/addons/addon-depends/opus/package.mk
@@ -2,13 +2,14 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="opus"
-PKG_VERSION="1.3"
-PKG_SHA256="4f3d69aefdf2dbaf9825408e452a8a414ffc60494c70633560700398820dc550"
+PKG_VERSION="1.3.1"
+PKG_SHA256="65b58e1e25b2a114157014736a3d9dfeaad8d41be1c8179866f144a2fb44ff9d"
 PKG_LICENSE="BSD"
 PKG_SITE="http://www.opus-codec.org"
 PKG_URL="https://archive.mozilla.org/pub/opus/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Codec designed for interactive speech and audio transmission over the Internet."
+PKG_TOOLCHAIN="configure"
 
 if [ "$TARGET_ARCH" = "arm" ]; then
   PKG_FIXED_POINT="--enable-fixed-point"
