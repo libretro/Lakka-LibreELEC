73f7a41c3446b6dea07d14f3170f355b2495d110
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-10-24 22:51:28 +0100

    dbus-python: update to dbus-python-1.2.12

--- packages/python/system/dbus-python/package.mk
+++ packages/python/system/dbus-python/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="dbus-python"
-PKG_VERSION="1.2.8"
-PKG_SHA256="abf12bbb765e300bf8e2a1b2f32f85949eab06998dbda127952c31cb63957b6f"
+PKG_VERSION="1.2.12"
+PKG_SHA256="cdd4de2c4f5e58f287b12013ed7b41dee81d503c8d0d2397c5bd2fb01badf260"
 PKG_LICENSE="GPL"
 PKG_SITE="https://freedesktop.org/wiki/Software/dbus"
 PKG_URL="https://dbus.freedesktop.org/releases/dbus-python/$PKG_NAME-$PKG_VERSION.tar.gz"
