214829e511387ffd0c95cc2f38edf1c9f3442bdf
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-10-24 22:51:28 +0100

    Python3: update to Python3-3.7.5; install target
    Remove bogus post_unpack() step as it no longer has any effect with
    repsect to pgen (or pgen2).

--- packages/lang/Python3/package.mk
+++ packages/lang/Python3/package.mk
@@ -3,18 +3,19 @@
 
 PKG_NAME="Python3"
 # When changing PKG_VERSION remember to sync PKG_PYTHON_VERSION!
-PKG_VERSION="3.7.3"
-PKG_SHA256="da60b54064d4cfcd9c26576f6df2690e62085123826cff2e667e72a91952d318"
+PKG_VERSION="3.7.5"
+PKG_SHA256="e85a76ea9f3d6c485ec1780fca4e500725a4a7bbc63c78ebc44170de9b619d94"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.python.org/"
 PKG_URL="http://www.python.org/ftp/python/$PKG_VERSION/${PKG_NAME::-1}-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_HOST="zlib:host bzip2:host libffi:host util-linux:host xz:host"
-PKG_DEPENDS_TARGET="toolchain sqlite expat zlib bzip2 openssl Python3:host readline ncurses"
+PKG_DEPENDS_TARGET="toolchain Python3:host sqlite expat zlib bzip2 openssl libffi readline ncurses"
 PKG_LONGDESC="Python3 is an interpreted object-oriented programming language."
-
-PKG_PYTHON_VERSION=python3.7
-
 PKG_TOOLCHAIN="autotools"
+
+PKG_PYTHON_VERSION="python3.7"
+
+PKG_PY_DISABLED_MODULES="_tkinter nis gdbm bsddb ossaudiodev"
 
 PKG_CONFIGURE_OPTS_HOST="ac_cv_prog_HAS_HG=/bin/false
                          ac_cv_prog_SVNVERSION=/bin/false
@@ -78,20 +79,21 @@ PKG_CONFIGURE_OPTS_TARGET="ac_cv_prog_HAS_HG=/bin/false
                            --with-doc-strings
                            --without-pymalloc
                            --without-ensurepip
-                           --with-threads
                            --enable-ipv6
 "
 
-post_unpack() {
-  # This is needed to make sure the Python build process doesn't try to
-  # regenerate those files with the pgen program. Otherwise, it builds
-  # pgen for the target, and tries to run it on the host.
-    touch $PKG_BUILD/Include/graminit.h
-    touch $PKG_BUILD/Python/graminit.c
+pre_configure_host() {
+  export DISABLED_EXTENSIONS="readline _curses _curses_panel $PKG_PY_DISABLED_MODULES"
+}
+
+post_make_host() {
+  # python distutils per default adds -L$LIBDIR when linking binary extensions
+  sed -e "s|^ 'LIBDIR':.*| 'LIBDIR': '/usr/lib',|g" -i $(find $PKG_BUILD/.$HOST_NAME -not -path '*/__pycache__/*' -name '_sysconfigdata__*.py')
 }
 
 post_makeinstall_host() {
-  rm -f $TOOLCHAIN/bin/python*-config
+  ln -sf $PKG_PYTHON_VERSION $TOOLCHAIN/bin/python
+
   rm -f $TOOLCHAIN/bin/smtpd.py*
   rm -f $TOOLCHAIN/bin/pyvenv
   rm -f $TOOLCHAIN/bin/pydoc*
@@ -101,7 +103,13 @@ post_makeinstall_host() {
   cp $PKG_BUILD/Tools/scripts/reindent.py $TOOLCHAIN/lib/$PKG_PYTHON_VERSION
 }
 
+pre_configure_target() {
+  export DISABLED_EXTENSIONS="$PKG_PY_DISABLED_MODULES"
+}
+
 post_makeinstall_target() {
+  ln -sf $PKG_PYTHON_VERSION $INSTALL/usr/bin/python
+
   rm -fr $PKG_BUILD/.$TARGET_NAME/build/temp.*
 
   PKG_INSTALL_PATH_LIB=$INSTALL/usr/lib/$PKG_PYTHON_VERSION
@@ -109,6 +117,8 @@ post_makeinstall_target() {
   for dir in config compiler sysconfigdata lib-dynload/sysconfigdata lib2to3 test; do
     rm -rf $PKG_INSTALL_PATH_LIB/$dir
   done
+
+  rm -rf $PKG_INSTALL_PATH_LIB/distutils/command/*.exe
 
   rm -rf $INSTALL/usr/bin/pyvenv
   rm -rf $INSTALL/usr/bin/python*-config
