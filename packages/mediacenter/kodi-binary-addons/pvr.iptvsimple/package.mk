9e1d2f213c760607550cddc596e8734e20f3c60f
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-11-04 15:14:47 +0000

    pvr.iptvsimple: update to pvr.iptvsimple-4.7.1-Matrix
    swap rapidxml for pugixml (PR297)

--- packages/mediacenter/kodi-binary-addons/pvr.iptvsimple/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.iptvsimple/package.mk
@@ -3,19 +3,17 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pvr.iptvsimple"
-PKG_VERSION="4.7.0-Matrix"
-PKG_SHA256="a45f1bd6dec0290725ab03e8222c5e587d256bbdd5660bb38a16136a5cd7d984"
+PKG_VERSION="4.7.1-Matrix"
+PKG_SHA256="1f27b3bd0e809080245889810bdae778485de7bfcfe97923a7194e8bff6d6872"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/kodi-pvr/pvr.iptvsimple"
 PKG_URL="https://github.com/kodi-pvr/pvr.iptvsimple/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain kodi-platform rapidxml zlib"
+PKG_DEPENDS_TARGET="toolchain kodi-platform pugixml zlib"
 PKG_SECTION=""
 PKG_SHORTDESC="pvr.iptvsimple"
 PKG_LONGDESC="pvr.iptvsimple"
 
 PKG_IS_ADDON="yes"
 PKG_ADDON_TYPE="xbmc.pvrclient"
-
-PKG_CMAKE_OPTS_TARGET="-DRAPIDXML_INCLUDE_DIRS=$(get_build_dir rapidxml)"
