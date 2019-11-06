4543ce351c4d1f15738ed7b8219830fc2255cc64
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-11-06 16:00:22 +0000

    RPi: update to firmware 8e688a4

--- packages/graphics/bcm2835-driver/package.mk
+++ packages/graphics/bcm2835-driver/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="bcm2835-driver"
-PKG_VERSION="b79618b5db4e4e7c02f9ad9d3ada51713825313e"
-PKG_SHA256="0b9c7e995d7726e8425fa5d7506ea45856cc0190b3a7a0d183d18475c07acd78"
+PKG_VERSION="8e688a44332268ee4f1e3d70d67b81e0567cbe71"
+PKG_SHA256="cccc552006cc4dfe698650a9a6d70c542f4e3ffcd02ac6403e14bf6394281179"
 PKG_LICENSE="nonfree"
 PKG_SITE="http://www.broadcom.com"
 PKG_URL="${DISTRO_SRC}/${PKG_NAME}-${PKG_VERSION}.tar.xz"
