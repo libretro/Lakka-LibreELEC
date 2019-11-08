1e691eb655d23f8e1ff20272a005841dd548f7ae
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-11-08 14:23:33 +0000

    rpi-eeprom: update to rpi-eeprom-9202943

--- packages/tools/rpi-eeprom/package.mk
+++ packages/tools/rpi-eeprom/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="rpi-eeprom"
-PKG_VERSION="b360b29ab64bb1cbf223d21aa6086d6541675471"
-PKG_SHA256="58196f74d9b9b62f22f966441a7822962ff28c6633578e4863ab356a9d6a8ba7"
+PKG_VERSION="9202943804f01fe8015cdaab692be8092d11719d"
+PKG_SHA256="9833abdfd47f1dd5c8bd4f993e9f103259d3aa35c065a04e25f1bde035ebdaca"
 PKG_ARCH="arm"
 PKG_LICENSE="BSD-3/custom"
 PKG_SITE="https://github.com/raspberrypi/rpi-eeprom"
@@ -38,7 +38,7 @@ makeinstall_target() {
     cp -PRv ${PKG_DIR}/source/rpi-eeprom-update ${INSTALL}/usr/bin
     cp -PRv ${PKG_BUILD}/rpi-eeprom-update ${INSTALL}/usr/bin/.rpi-eeprom-update.real
     cp -PRv ${PKG_BUILD}/rpi-eeprom-config ${INSTALL}/usr/bin
-    cp -PRv ${PKG_BUILD}/vl805 ${INSTALL}/usr/bin
+    cp -PRv ${PKG_BUILD}/firmware/vl805 ${INSTALL}/usr/bin
 
   mkdir -p ${INSTALL}/etc/default
     cp -PRv ${PKG_DIR}/config/* ${INSTALL}/etc/default
