91a5490cdec33acc7f6b6502bae92d8ca46f81c2
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-11-16 02:33:27 +0000

    intel-ucode: update to intel-ucode-20191115

--- packages/linux-firmware/intel-ucode/package.mk
+++ packages/linux-firmware/intel-ucode/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="intel-ucode"
-PKG_VERSION="20191113"
-PKG_SHA256="75e0d26a2f10678e158b0e6cdb235cf12d1662a36283b1fcdffbfc0f7ce0c171"
+PKG_VERSION="20191115"
+PKG_SHA256="14b26d27be70774948b8cb582e298f5317263b8b8bb1fe8e41260eae54f531dc"
 PKG_ARCH="x86_64"
 PKG_LICENSE="other"
 PKG_SITE="https://downloadcenter.intel.com/search?keyword=linux+microcode"
