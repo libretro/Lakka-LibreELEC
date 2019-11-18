86db64e287a4425e946db91b0fd7b2c91516f6ac
natinusala <natinusala@gmail.com>
2019-11-18 17:03:39 +0100

    New core: neocd

---
+++ packages/libretro/neocd/package.mk
@@ -0,0 +1,50 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.tv; see the file COPYING.  If not, write to
+#  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
+#  http://www.gnu.org/copyleft/gpl.html
+################################################################################
+
+PKG_NAME="neocd"
+PKG_VERSION="25f5fef"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/libretro/neocd_libretro"
+PKG_GIT_URL="$PKG_SITE"
+PKG_DEPENDS_TARGET="toolchain flac"
+PKG_PRIORITY="optional"
+PKG_SECTION="libretro"
+PKG_SHORTDESC="Neo Geo CD emulator for libretro"
+PKG_LONGDESC="Neo Geo CD emulator for libretro"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+PKG_USE_CMAKE="yes"
+
+PKG_CMAKE_SCRIPT="$PKG_BUILD/CMakeLists.txt"
+
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=${CXX} -DCMAKE_C_COMPILER=${CC}"
+
+pre_make_target() {
+  # build fix for cross-compiling Dolphin, from Dolphin forums
+  find $PKG_BUILD -name flags.make -exec sed -i "s:isystem :I:g" \{} \;
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/libretro
+  cp $PKG_BUILD/.$TARGET_NAME/libneocd_libretro.so $INSTALL/usr/lib/libretro/
+}
