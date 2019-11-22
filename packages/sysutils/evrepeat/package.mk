cfdb0315e9873a9c99fe63228ec00570e8c45d61
Matthias Reichl <hias@horus.com>
2019-11-22 14:52:17 +0100

    evrepeat: add new package to configure input device repeat settings
    Signed-off-by: Matthias Reichl <hias@horus.com>

---
+++ packages/sysutils/evrepeat/package.mk
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="evrepeat"
+PKG_VERSION="0.0.1"
+PKG_SHA256="f1d14a12d700b9650562f8ec8ca593e6033f2e18b3d1fd1f44af29875152547e"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/HiassofT/evrepeat"
+PKG_URL="https://github.com/HiassofT/evrepeat/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Tool to view and change linux input device repeat settings"
+
