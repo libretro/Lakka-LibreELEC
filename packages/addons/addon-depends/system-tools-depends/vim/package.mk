3dc0d27e2ebf4f54f9f2da5b7561f884d134e0af
bam80 <butirsky@gmail.com>
2019-12-04 01:50:52 +0300

    system-tools: Vim update

--- packages/addons/addon-depends/system-tools-depends/vim/package.mk
+++ packages/addons/addon-depends/system-tools-depends/vim/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="vim"
-PKG_VERSION="8.1.2384"
-PKG_SHA256="f17462971e577063552cd4dbbebeb84e77a9fb47cd40c6234969e7672aebcd59"
+PKG_VERSION="8.2.0023"
+PKG_SHA256="100c1226286e24421084b9301d945d0310c7b3c9d45a6da4741dc23c0be60050"
 PKG_LICENSE="VIM"
 PKG_SITE="http://www.vim.org/"
 PKG_URL="https://github.com/vim/vim/archive/v$PKG_VERSION.tar.gz"
