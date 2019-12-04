a6111a6d0ba427db21ce0fc9ce25be4c0e4794a2
Andre Heider <a.heider@gmail.com>
2019-12-04 10:24:21 +0100

    kodi: don't wipe themes, move them for the theme packages

--- packages/mediacenter/kodi-theme-Estuary/package.mk
+++ packages/mediacenter/kodi-theme-Estuary/package.mk
@@ -6,12 +6,11 @@ PKG_VERSION="1.0"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
 PKG_URL=""
-PKG_DEPENDS_TARGET="toolchain kodi"
-PKG_NEED_UNPACK="$(get_pkg_directory $MEDIACENTER)"
+PKG_DEPENDS_TARGET="kodi"
 PKG_LONGDESC="Kodi Mediacenter default theme."
 PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p $INSTALL/usr/share/kodi/addons/
-    cp -a $(get_build_dir kodi)/.$TARGET_NAME/addons/skin.estuary $INSTALL/usr/share/kodi/addons/
+    cp -a $(get_install_dir kodi)/.noinstall/skin.estuary $INSTALL/usr/share/kodi/addons/
 }
