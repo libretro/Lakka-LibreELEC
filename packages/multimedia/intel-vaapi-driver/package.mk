bc0eb5354147bcb6676b87fd3e1ac19bbb116d43
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-12-06 19:26:30 +0000

    intel-vaapi-driver: update to intel-vaapi-driver-2.4.0

--- packages/multimedia/intel-vaapi-driver/package.mk
+++ packages/multimedia/intel-vaapi-driver/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="intel-vaapi-driver"
-PKG_VERSION="2.3.0"
-PKG_SHA256="fcc3f09291e58fd316fd015d4e1329e7e03c38cffa4651bda725d500a66aa74e"
+PKG_VERSION="2.4.0"
+PKG_SHA256="58567dac882167021f031489062cbbab76bc646214be0ee44d5f724d960b3d76"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL"
 PKG_SITE="https://01.org/linuxmedia"
