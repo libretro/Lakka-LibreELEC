8b5054fc3e4842a86a80ddcf71cab4fc91163add
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-12-09 02:14:31 +0000

    linux (Generic): update to linux-5.5-rc1

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -29,9 +29,9 @@ case "$LINUX" in
     PKG_SOURCE_NAME="linux-$LINUX-$PKG_VERSION.tar.gz"
     ;;
   *)
-    PKG_VERSION="5.4.16"
-    PKG_SHA256="911541df2974782062f551ba0a818d7822ec69db4c41e590bcc7422398bada7a"
-    PKG_URL="https://www.kernel.org/pub/linux/kernel/v5.x/$PKG_NAME-$PKG_VERSION.tar.xz"
+    PKG_VERSION="5.5-rc1"
+    PKG_SHA256="b739f74a099f96128eecad8bad6346f0f41df7e526846e21b912c985c0175a34"
+    PKG_URL="https://git.kernel.org/torvalds/t/$PKG_NAME-$PKG_VERSION.tar.gz"
     PKG_PATCH_DIRS="default"
     ;;
 esac
