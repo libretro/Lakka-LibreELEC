f6b890601465300931f76dde1c4a9093d759661d
Andre Heider <a.heider@gmail.com>
2019-12-16 14:01:47 +0100

    tvheadend42: cleanup and use the "sysroot" build flag
    As with system-tools before.
    
    Use PKG_DEPENDS_CONFIG where appropriate.

--- packages/addons/addon-depends/argtable2/package.mk
+++ packages/addons/addon-depends/argtable2/package.mk
@@ -9,7 +9,7 @@ PKG_SITE="http://argtable.sourceforge.net/"
 PKG_URL="https://downloads.sourceforge.net/project/argtable/argtable/argtable-${PKG_VERSION}/argtable2-${PKG_VERSION:2:4}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Argtable is an open source ANSI C library that parses GNU-style command-line options."
+PKG_TOOLCHAIN="configure"
+PKG_BUILD_FLAGS="-sysroot"
 
-make_target() {
-  :
-}
+PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared"
