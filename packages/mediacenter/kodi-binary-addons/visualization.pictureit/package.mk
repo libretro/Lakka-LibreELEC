e8c6ad81646451910a45dfd28fa75358438375a2
Matthias Reichl <hias@horus.com>
2019-12-19 20:41:13 +0100

    kodi-binary-addons: update to latest Leia versions
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/mediacenter/kodi-binary-addons/visualization.pictureit/package.mk
+++ packages/mediacenter/kodi-binary-addons/visualization.pictureit/package.mk
@@ -3,9 +3,9 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="visualization.pictureit"
-PKG_VERSION="f08d0aa6d5f80cfa95a24adca48be15e333ca8e0"
-PKG_SHA256="f29112d232907b46a738e6971f03c75ec6a61b70be70de203db82b2252ae4f8d"
-PKG_REV="3"
+PKG_VERSION="4060888ba482e99e9c08753a385ecf164a9ac821"
+PKG_SHA256="dbd9d355b62042887bcaed69cb58033a71447e0ae3143ca2e319fd5cac7bd292"
+PKG_REV="4"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
