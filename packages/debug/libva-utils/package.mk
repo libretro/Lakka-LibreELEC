e46c85e268f6004896f813c876a8688fafb87355
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-12-25 11:33:26 +0000

    libva-utils: update to libva-utils-2.6.0

--- packages/debug/libva-utils/package.mk
+++ packages/debug/libva-utils/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libva-utils"
-PKG_VERSION="2.4.1"
-PKG_SHA256="4ab6faa6592ffe2d4817450f5453f3974b47441650e8f49e1a12497ce2a794ac"
+PKG_VERSION="2.6.0"
+PKG_SHA256="6ce7a534539c9ae554ace0bfa6689d871145cc172bb3f66ceb3fe2f65466fd90"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/01org/libva-utils"
 PKG_URL="https://github.com/intel/libva-utils/archive/$PKG_VERSION.tar.gz"
