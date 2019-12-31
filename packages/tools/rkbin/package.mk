e6b4bd115fe369dc9c22ae2f1529f2b0331f4f0a
Jonas Karlman <jonas@kwiboo.se>
2019-12-31 09:09:33 +0000

    rkbin: update to 4563e24

--- packages/tools/rkbin/package.mk
+++ packages/tools/rkbin/package.mk
@@ -2,9 +2,9 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="rkbin"
-# Version is: Kwiboo/tag:libreelec-ba436b9
-PKG_VERSION="ba436b9d616318a9437895457f6bbef1cc873e2b"
-PKG_SHA256="72c2ef6ec1fe79da7c701056662343b9e1df1cf20e5df10bafc4ec0619ef4578"
+# Version is: Kwiboo/tag:libreelec-4563e24
+PKG_VERSION="4563e249a3f47e7fcd47a4c3769b6c05683b6e9d"
+PKG_SHA256="0b3479117700bce9afea2110c1f027b626c76d99045802218b35a53606547d60"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="nonfree"
 PKG_SITE="https://github.com/rockchip-linux/rkbin"
