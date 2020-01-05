24916d2f87231cc4fceb2fc30115e6406e6c5164
Jonas Karlman <jonas@kwiboo.se>
2020-01-05 23:44:36 +0000

    grub: depend on gettext

--- packages/tools/grub/package.mk
+++ packages/tools/grub/package.mk
@@ -8,7 +8,7 @@ PKG_ARCH="x86_64"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://www.gnu.org/software/grub/index.html"
 PKG_URL="http://git.savannah.gnu.org/cgit/grub.git/snapshot/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain flex freetype:host gnulib:host"
+PKG_DEPENDS_TARGET="toolchain flex freetype:host gnulib:host gettext:host"
 PKG_LONGDESC="GRUB is a Multiboot boot loader."
 PKG_TOOLCHAIN="configure"
 
