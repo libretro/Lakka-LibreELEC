bcc544c5b02e30a74e80b7fb850d51284e2971be
MilhouseVH <milhouseVH.github@nmacleod.com>
2020-01-11 18:12:10 +0000

    inputstream.ffmpegdirect: new package

---
+++ packages/mediacenter/kodi-binary-addons/inputstream.ffmpegdirect/package.mk
@@ -0,0 +1,17 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="inputstream.ffmpegdirect"
+PKG_VERSION="1.7.0-Matrix"
+PKG_SHA256="33b0aaf963e29e79e402bea55df50a65dd6673624687550201b7988af0642902"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL2+"
+PKG_SITE="https://github.com/xbmc/inputstream.ffmpegdirect"
+PKG_URL="https://github.com/xbmc/inputstream.ffmpegdirect/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform bzip2 ffmpeg gmp gnutls libpng libxml2 nettle xz zlib zvbi"
+PKG_SECTION=""
+PKG_SHORTDESC="inputstream.ffmpegdirect"
+PKG_LONGDESC="InputStream Client for streams that can be opened by FFmpeg's libavformat such as plain TS, HLS and DASH (without DRM) streams."
+
+PKG_IS_ADDON="yes"
