351e23a527a9fb858f65fe1f3afa0e7f1e15d2c8
Andre Heider <a.heider@gmail.com>
2020-01-14 07:27:39 +0100

    libretro-mame: fix cross compilation
    Don't set PTR64=0 on 32bit archs, instead set ARCHITECTURE to an empty
    string. That way we don't need to patch out hardcoded -m32 arguments.
    
    While at it, disable the bgfx hw renderers, or that pile of build crap
    tries to include X11 headers.

--- packages/emulation/libretro-mame/package.mk
+++ packages/emulation/libretro-mame/package.mk
@@ -17,21 +17,18 @@ PKG_LIBNAME="mame_libretro.so"
 PKG_LIBPATH="$PKG_LIBNAME"
 PKG_LIBVAR="MAME_LIB"
 
-make_target() {
-  PTR64="0"
-  NOASM="0"
+pre_make_target() {
+  PKG_MAKE_OPTS_TARGET=" \
+    REGENIE=1 VERBOSE=1 NOWERROR=1 PYTHON_EXECUTABLE=python3 CONFIG=libretro \
+    LIBRETRO_OS="unix" ARCH="" PROJECT="" LIBRETRO_CPU="${ARCH}" DISTRO="debian-stable" \
+    CROSS_BUILD="1" OVERRIDE_CC="${CC}" OVERRIDE_CXX="${CXX}" \
+    TARGET="mame" SUBTARGET="arcade" PLATFORM="${ARCH}" RETRO=1 OSD="retro""
 
   if [ "$ARCH" = "arm" ]; then
-    NOASM="1"
+    PKG_MAKE_OPTS_TARGET+=" NOASM="1" ARCHITECTURE="""
   elif [ "$ARCH" = "x86_64" ]; then
-    PTR64="1"
+    PKG_MAKE_OPTS_TARGET+=" NOASM="0" PTR64="1""
   fi
-
-  make REGENIE=1 VERBOSE=1 NOWERROR=1 PYTHON_EXECUTABLE=python3 CONFIG=libretro \
-       LIBRETRO_OS="unix" ARCH="" PROJECT="" LIBRETRO_CPU="$ARCH" DISTRO="debian-stable" \
-       CROSS_BUILD="1" OVERRIDE_CC="$CC" OVERRIDE_CXX="$CXX" \
-       PTR64="$PTR64" TARGET="mame" \
-       SUBTARGET="arcade" PLATFORM="$ARCH" RETRO=1 OSD="retro"
 }
 
 post_make_target() {
