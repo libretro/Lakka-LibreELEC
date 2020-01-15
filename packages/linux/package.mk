33186418db1464556eb11c0c7c26484a52a5d7cd
MilhouseVH <milhouseVH.github@nmacleod.com>
2020-01-15 17:31:29 +0000

    linux (Generic/Allwinner): update to linux-5.4.12

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -29,8 +29,8 @@ case "$LINUX" in
     PKG_SOURCE_NAME="linux-$LINUX-$PKG_VERSION.tar.gz"
     ;;
   *)
-    PKG_VERSION="5.4.11"
-    PKG_SHA256="62bd36e5d5e1d8208750ccddd8e8aa3d109b29b5ac5344b5b1c47d0f6d55d72c"
+    PKG_VERSION="5.4.12"
+    PKG_SHA256="d9c3c7a2d52d25c24cbca0f89d55511422d3d207c573d42ec58f7f17c948d0fb"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v5.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;
