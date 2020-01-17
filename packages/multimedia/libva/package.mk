6054310e44ddf3770254bec24c6d505462dd318c
MilhouseVH <milhouseVH.github@nmacleod.com>
2020-01-17 14:49:47 +0000

    libva: update to libva-2.6.1

--- packages/multimedia/libva/package.mk
+++ packages/multimedia/libva/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libva"
-PKG_VERSION="2.6.0"
-PKG_SHA256="5566755739503ef53beafad7fd7e155e68039e9af78e051e27f1715a420a0952"
+PKG_VERSION="2.6.1"
+PKG_SHA256="965f581e096508ece26ea9bef8bd769e93f9d1fe6840e309cc925cfef35ad13f"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL"
 PKG_SITE="https://01.org/linuxmedia"
