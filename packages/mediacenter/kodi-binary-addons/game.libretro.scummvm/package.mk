d467d623139f4433697dda1f1d77b4f25bfeeaab
Sven Neumann <sven@svenfoo.org>
2020-01-19 12:19:26 +0100

    game.libretro.scummvm: bump package revision

--- packages/mediacenter/kodi-binary-addons/game.libretro.scummvm/package.mk
+++ packages/mediacenter/kodi-binary-addons/game.libretro.scummvm/package.mk
@@ -4,7 +4,7 @@
 PKG_NAME="game.libretro.scummvm"
 PKG_VERSION="2.0.0.6-Leia"
 PKG_SHA256="f71f7ba56ca4c7717afdd0626b0ce1d9da3ae199a32eceb22837f8658c7d4698"
-PKG_REV="1"
+PKG_REV="2"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/kodi-game/game.libretro.scummvm"
