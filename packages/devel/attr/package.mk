0bb2b2e7089999ce4e1a55a5cf27a6e18e331604
MilhouseVH <milhouseVH.github@nmacleod.com>
2020-01-25 21:00:28 +0000

    attr: update to attr-2.4.48

--- packages/devel/attr/package.mk
+++ packages/devel/attr/package.mk
@@ -1,13 +1,14 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2010-2011 Roman Weber (roman@openelec.tv)
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="attr"
-PKG_VERSION="2.4.47"
-PKG_SHA256="25772f653ac5b2e3ceeb89df50e4688891e21f723c460636548971652af0a859"
+PKG_VERSION="2.4.48"
+PKG_SHA256="5ead72b358ec709ed00bbf7a9eaef1654baad937c001c044fe8b74c57f5324e7"
 PKG_LICENSE="GPL"
-PKG_SITE=""
-PKG_URL="http://download.savannah.gnu.org/releases-noredirect/attr/$PKG_NAME-$PKG_VERSION.src.tar.gz"
+PKG_SITE="https://savannah.nongnu.org/projects/attr"
+PKG_URL="http://download.savannah.nongnu.org/releases/attr/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Extended Attributes Of Filesystem Objects."
 PKG_BUILD_FLAGS="+pic"
@@ -23,15 +24,9 @@ else
   PKG_CONFIGURE_OPTS_TARGET="$PKG_CONFIGURE_OPTS_TARGET DEBUG=-DNDEBUG"
 fi
 
-pre_configure_target() {
-# attr fails to build in subdirs
-  cd $PKG_BUILD
-    rm -rf .$TARGET_NAME
-}
-
 makeinstall_target() {
   mkdir -p $SYSROOT_PREFIX/usr/lib/
-    cp libattr/.libs/libattr.a $SYSROOT_PREFIX/usr/lib/
+    cp .libs/libattr.a $SYSROOT_PREFIX/usr/lib/
 
   mkdir -p $SYSROOT_PREFIX/usr/include/attr
     cp include/*.h $SYSROOT_PREFIX/usr/include/attr
