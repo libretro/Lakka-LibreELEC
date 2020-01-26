a45850d5f797fea4c7dfec769c411b717e520776
Lukas Rusak <lorusak@gmail.com>
2020-01-26 09:24:29 -0800

    gmmlib: update to 19.4.1

--- packages/multimedia/gmmlib/package.mk
+++ packages/multimedia/gmmlib/package.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="gmmlib"
-PKG_VERSION="50707fb182bc8fc8f9711d2c0da96f6ded1b8ef4"
-PKG_SHA256="73c8895e3d5c54c8a44682c69db5c1a40da5500dacc07c89cb520d2b21626b05"
+PKG_VERSION="19.4.1"
+PKG_SHA256="bb874b41c499abb8f6253b1834e93a02ed9744de71f2503ee9cd4100af7c1860"
 PKG_ARCH="x86_64"
 PKG_LICENSE="MIT"
 PKG_SITE="https://01.org/linuxmedia"
-PKG_URL="https://github.com/intel/gmmlib/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/intel/gmmlib/archive/intel-gmmlib-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="gmmlib: The Intel(R) Graphics Memory Management Library provides device specific and buffer management for the Intel(R) Graphics Compute Runtime for OpenCL(TM) and the Intel(R) Media Driver for VAAPI."
 
