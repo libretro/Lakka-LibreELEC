25004b1bd93e9e61e802b80dba4b6e60a0d40ed2
MilhouseVH <milhouseVH.github@nmacleod.com>
2020-01-27 08:42:13 +0000

    strace: update to strace-5.4

--- packages/addons/addon-depends/system-tools-depends/strace/package.mk
+++ packages/addons/addon-depends/system-tools-depends/strace/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="strace"
-PKG_VERSION="4.26"
-PKG_SHA256="7c4d2ffeef4f7d1cdc71062ca78d1130eb52f947c2fca82f59f6a1183bfa1e1c"
+PKG_VERSION="5.4"
+PKG_SHA256="f7d00514d51290b6db78ad7a9de709baf93caa5981498924cbc9a744cfd2a741"
 PKG_LICENSE="BSD"
 PKG_SITE="https://strace.io/"
 PKG_URL="https://strace.io/files/$PKG_VERSION/strace-$PKG_VERSION.tar.xz"
