213acf34f5790c2f5a241a0876be7a4bc9d486b2
natinusala <natinusala@gmail.com>
2020-02-06 23:20:52 +0100

    OdroidGoAdvance: fix bootloader and kernel

--- packages/tools/rkbin/package.mk
+++ packages/tools/rkbin/package.mk
@@ -2,9 +2,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="rkbin"
-# Version is: Kwiboo/tag:libreelec-ba436b9
-PKG_VERSION="ba436b9d616318a9437895457f6bbef1cc873e2b"
-PKG_SHA256="72c2ef6ec1fe79da7c701056662343b9e1df1cf20e5df10bafc4ec0619ef4578"
+PKG_VERSION="7c1d75f1da57884dcee0586ea2930e436a6c7ffb"
+PKG_SHA256="f384c119a7418dcc504097e8d47a4906f6082af74df50604c2406da9be667ed8"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="nonfree"
 PKG_SITE="https://github.com/rockchip-linux/rkbin"
