6c1c6604baeea79997a657e10333ffd066389551
MilhouseVH <milhouseVH.github@nmacleod.com>
2020-02-10 10:57:18 +0000

    RTL8812AU: update to RTL8812AU-45ea063

--- packages/linux-drivers/RTL8812AU/package.mk
+++ packages/linux-drivers/RTL8812AU/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="RTL8812AU"
-PKG_VERSION="4aaf486f7f758a1b5132552420773780efdf946e"
-PKG_SHA256="fa8f55a1998f860c4629c6e5ccc79e01164fbcfd59fb511fd22027962dc2d36a"
+PKG_VERSION="45ea063b03846e8a8c9fc04b2f9c2090bbe9b46b"
+PKG_SHA256="99e407dac59bab34262f2f2f10db314a733c0644e940d7ed7759f65858db1dd9"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/MilhouseVH/RTL8812AU"
 PKG_URL="https://github.com/MilhouseVH/RTL8812AU/archive/$PKG_VERSION.tar.gz"
