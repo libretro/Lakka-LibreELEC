db102f6e80939263ad91a7c647fe4687d331d6c3
Nathan S <natinusala@gmail.com>
2020-02-12 20:45:27 +0100

    libretro-cores: new package, add master switch (#898)

---
+++ packages/libretro/libretro-cores/package.mk
@@ -0,0 +1,19 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2020 Team Lakka
+
+PKG_NAME="libretro-cores"
+PKG_VERSION="1"
+PKG_ARCH="any"
+PKG_SECTION="libretro"
+PKG_SHORTDESC="libretro cores metapackage"
+PKG_SITE="https://www.libretro.com/"
+PKG_STAMP="$DISABLE_LIBRETRO"
+
+PKG_TOOLCHAIN="manual"
+
+# $LIBRETRO_CORES defined in distribution options
+if [ "$DISABLE_LIBRETRO" != "yes" ]; then
+  PKG_DEPENDS_TARGET="$LIBRETRO_CORES"
+fi
+
