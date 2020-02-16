fcee09860c0ce72dbeb251ef14c4423b8eac9274
MilhouseVH <milhouseVH.github@nmacleod.com>
2020-02-16 07:20:09 +0000

    nano: update to nano-4.8

--- packages/tools/nano/package.mk
+++ packages/tools/nano/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="nano"
-PKG_VERSION="4.4"
-PKG_SHA256="2af222e0354848ffaa3af31b5cd0a77917e9cb7742cd073d762f3c32f0f582c7"
+PKG_VERSION="4.8"
+PKG_SHA256="c348f61c68ab1d573b308398212a09cd68c60fbee20f01a5bd4b50071a258e63"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.nano-editor.org/"
 PKG_URL="http://ftpmirror.gnu.org/nano/${PKG_NAME}-${PKG_VERSION}.tar.xz"
