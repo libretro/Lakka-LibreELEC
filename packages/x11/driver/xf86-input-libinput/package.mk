31a91ccaad43d74087fc0558412b8b8ca939a5ba
MilhouseVH <milhouseVH.github@nmacleod.com>
2020-02-16 02:20:29 +0000

    xf86-input-libinput: update to xf86-input-libinput-0.29.0

--- packages/x11/driver/xf86-input-libinput/package.mk
+++ packages/x11/driver/xf86-input-libinput/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="xf86-input-libinput"
-PKG_VERSION="0.28.2"
-PKG_SHA256="b8b346962c6b62b8069928c29c0db83b6f544863bf2fc6830f324de841de2820"
+PKG_VERSION="0.29.0"
+PKG_SHA256="c28b56a21754b972db31798e6a4cf4dc9d69208d08f8fe41701a94def5e94bee"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.freedesktop.org/wiki/Software/libinput/"
 PKG_URL="http://xorg.freedesktop.org/archive/individual/driver/$PKG_NAME-$PKG_VERSION.tar.bz2"
