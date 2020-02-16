0dd23bcde4ec4c8407582d848e7ef54903934c82
MilhouseVH <milhouseVH.github@nmacleod.com>
2020-02-16 03:23:21 +0000

    tiff: update to tiff-4.1.0

--- packages/graphics/tiff/package.mk
+++ packages/graphics/tiff/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="tiff"
-PKG_VERSION="4.0.10"
-PKG_SHA256="2c52d11ccaf767457db0c46795d9c7d1a8d8f76f68b0b800a3dfe45786b996e4"
+PKG_VERSION="4.1.0"
+PKG_SHA256="5d29f32517dadb6dbcd1255ea5bbc93a2b54b94fbf83653b4d65c7d6775b8634"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.remotesensing.org/libtiff/"
 PKG_URL="http://download.osgeo.org/libtiff/$PKG_NAME-$PKG_VERSION.tar.gz"
