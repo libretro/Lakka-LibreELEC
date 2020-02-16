034b72e2a621cbb9a5c4a87cd67d643d1f364492
MilhouseVH <milhouseVH.github@nmacleod.com>
2020-02-16 02:01:20 +0000

    openssh: update to openssh-8.2p1

--- packages/network/openssh/package.mk
+++ packages/network/openssh/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="openssh"
-PKG_VERSION="8.0p1"
-PKG_SHA256="bd943879e69498e8031eb6b7f44d08cdc37d59a7ab689aa0b437320c3481fd68"
+PKG_VERSION="8.2p1"
+PKG_SHA256="43925151e6cf6cee1450190c0e9af4dc36b41c12737619edff8bcebdff64e671"
 PKG_LICENSE="OSS"
 PKG_SITE="https://www.openssh.com/"
 PKG_URL="https://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/$PKG_NAME-$PKG_VERSION.tar.gz"
