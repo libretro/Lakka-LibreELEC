094ba6516e2b131c4793400e17b6f0744648c2b9
MilhouseVH <milhouseVH.github@nmacleod.com>
2020-02-16 04:12:32 +0000

    Mako: update to Mako-1.1.1

--- packages/python/devel/Mako/package.mk
+++ packages/python/devel/Mako/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="Mako"
-PKG_VERSION="1.0.12"
-PKG_SHA256="0cfa65de3a835e87eeca6ac856b3013aade55f49e32515f65d999f91a2324162"
+PKG_VERSION="1.1.1"
+PKG_SHA256="2984a6733e1d472796ceef37ad48c26f4a984bb18119bb2dbc37a44d8f6e75a4"
 PKG_LICENSE="GPL"
 PKG_SITE="https://pypi.org/project/Mako"
 PKG_URL="https://files.pythonhosted.org/packages/source/${PKG_NAME:0:1}/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz"
