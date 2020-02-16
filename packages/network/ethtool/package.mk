1753bcd8e1df0d33978fa266d934fce13dcbf20e
MilhouseVH <milhouseVH.github@nmacleod.com>
2020-02-16 01:57:06 +0000

    ethtool: update to ethtool-5.4

--- packages/network/ethtool/package.mk
+++ packages/network/ethtool/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="ethtool"
-PKG_VERSION="5.1"
-PKG_SHA256="65feac1fec6565290b7784b2efc925dee900a9b11202ce7c6c30a967c3da3387"
+PKG_VERSION="5.4"
+PKG_SHA256="22f81e4927d6eb6a13a259d705248a96005fce2005e15257f639f5a1c8c52b6b"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kernel.org/pub/software/network/ethtool/"
 PKG_URL="http://www.kernel.org/pub/software/network/ethtool/$PKG_NAME-$PKG_VERSION.tar.xz"
