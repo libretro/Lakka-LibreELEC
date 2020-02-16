4a314d31236242c6572e19d4695109462787aa2b
MilhouseVH <milhouseVH.github@nmacleod.com>
2020-02-16 03:45:01 +0000

    cpio: update to cpio-2.13

--- packages/compress/cpio/package.mk
+++ packages/compress/cpio/package.mk
@@ -1,9 +1,10 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="cpio"
-PKG_VERSION="2.12"
-PKG_SHA256="08a35e92deb3c85d269a0059a27d4140a9667a6369459299d08c17f713a92e73"
+PKG_VERSION="2.13"
+PKG_SHA256="e87470d9c984317f658567c03bfefb6b0c829ff17dbf6b0de48d71a4c8f3db88"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/software/cpio/"
 PKG_URL="http://ftpmirror.gnu.org/cpio/$PKG_NAME-$PKG_VERSION.tar.gz"
