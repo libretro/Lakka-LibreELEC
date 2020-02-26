80a277dfd7fd4824f40203b3b4dff27e98f7b17f
MilhouseVH <milhouseVH.github@nmacleod.com>
2020-02-26 00:04:08 +0000

    netbase: fix site url; update to netbase-6.1

--- packages/network/netbase/package.mk
+++ packages/network/netbase/package.mk
@@ -2,10 +2,10 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="netbase"
-PKG_VERSION="6.0"
-PKG_SHA256="692baeb7b76eba5580c7edbc97ce1784a06b5aa4b367c5ed0b39e0ce7a97d594"
+PKG_VERSION="6.1"
+PKG_SHA256="084d743bd84d4d9380bac4c71c51e57406dce44f5a69289bb823c903e9b035d8"
 PKG_LICENSE="GPL"
-PKG_SITE="https://anonscm.debian.org/cgit/users/md/netbase.git/"
+PKG_SITE="https://salsa.debian.org/md/netbase"
 PKG_URL="http://ftp.debian.org/debian/pool/main/n/netbase/netbase_$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="The netbase package provides data for network services and protocols from the iana db."
