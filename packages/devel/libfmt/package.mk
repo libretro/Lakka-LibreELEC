2dac31c7b170684f06608b6557527e92cabc6ec4
Matthias Reichl <hias@horus.com>
2020-02-28 14:03:12 +0100

    libfmt: bring cmake options in line with kodi
    see https://github.com/xbmc/xbmc/pull/17362
    
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/devel/libfmt/package.mk
+++ packages/devel/libfmt/package.mk
@@ -10,4 +10,4 @@ PKG_URL="https://github.com/fmtlib/fmt/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="fmt is an open-source formatting library for C++. It can be used as a safe alternative to printf or as a fast alternative to IOStreams."
 
-PKG_CMAKE_OPTS_TARGET="-DFMT_DOC=OFF -DFMT_INSTALL=ON -DFMT_TEST=OFF -DFMT_USE_CPP11=ON"
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_CXX_STANDARD=14 -DCMAKE_CXX_EXTENSIONS:BOOL=OFF -DFMT_DOC=OFF -DFMT_INSTALL=ON -DFMT_TEST=OFF"
