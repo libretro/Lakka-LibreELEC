97feb83c15715677637773b4b2c6ca42315d49b0
MilhouseVH <milhouseVH.github@nmacleod.com>
2020-03-04 21:29:55 +0000

    dav1d: update to dav1d-0.5.2

--- packages/multimedia/dav1d/package.mk
+++ packages/multimedia/dav1d/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="dav1d"
-PKG_VERSION="0.5.1"
-PKG_SHA256="0214d201a338e8418f805b68f9ad277e33d79c18594dee6eaf6dcd74db2674a9"
+PKG_VERSION="0.5.2"
+PKG_SHA256="1780199d71e62a82ca1bfca2d71da5c599c7e1edc68c72f6fc3c0c25ce1880b7"
 PKG_LICENSE="BSD"
 PKG_SITE="http://www.jbkempf.com/blog/post/2018/Introducing-dav1d"
 PKG_URL="https://code.videolan.org/videolan/dav1d/-/archive/${PKG_VERSION}/dav1d-${PKG_VERSION}.tar.bz2"
