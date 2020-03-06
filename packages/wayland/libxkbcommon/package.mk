8f0d6b7d7d89ef2e1074108551b87ca82968cdd1
natinusala <natinusala@gmail.com>
2020-03-06 12:39:13 +0100

    odroidgo2: cleanup x11 and wayland

--- packages/wayland/libxkbcommon/package.mk
+++ packages/wayland/libxkbcommon/package.mk
@@ -1,27 +1,38 @@
-# SPDX-License-Identifier: GPL-2.0-or-later
-# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+#
+#  OpenELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  OpenELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
 
 PKG_NAME="libxkbcommon"
-PKG_VERSION="0.8.4"
-PKG_SHA256="60ddcff932b7fd352752d51a5c4f04f3d0403230a584df9a2e0d5ed87c486c8b"
-PKG_LICENSE="MIT"
+PKG_VERSION="0.7.1"
+PKG_SHA256="ba59305d2e19e47c27ea065c2e0df96ebac6a3c6e97e28ae5620073b6084e68b"
+PKG_ARCH="any"
+PKG_LICENSE="OSS"
 PKG_SITE="http://xkbcommon.org"
 PKG_URL="http://xkbcommon.org/download/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain xkeyboard-config"
-PKG_LONGDESC="xkbcommon is a library to handle keyboard descriptions."
+PKG_SECTION="wayland"
+PKG_SHORTDESC="xkbcommon: a library to handle keyboard descriptions"
+PKG_LONGDESC="xkbcommon is a library to handle keyboard descriptions, including loading them from disk, parsing them and handling their state. It's mainly meant for client toolkits, window systems, and other system applications; currently that includes Wayland, kmscon, GTK+, Qt, Clutter, and more. It is also used by some XCB applications for proper keyboard support."
 
-PKG_MESON_OPTS_TARGET="-Denable-docs=false"
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
 
-if [ "${DISPLAYSERVER}" = "x11" ]; then
-  PKG_DEPENDS_TARGET+=" libXau"
-  PKG_MESON_OPTS_TARGET+=" -Denable-x11=true \
-                           -Denable-wayland=false"
-elif [ "${DISPLAYSERVER}" = "weston" ]; then
-  PKG_DEPENDS_TARGET+=" wayland"
-  PKG_MESON_OPTS_TARGET+=" -Denable-x11=false \
-                           -Denable-wayland=true"
+if [ "$DISPLAYSERVER" = "x11" ]; then
+  PKG_CONFIGURE_OPTS_TARGET="--enable-x11"
 else
-  PKG_MESON_OPTS_TARGET+=" -Denable-x11=false \
-                           -Denable-wayland=false"
+  PKG_CONFIGURE_OPTS_TARGET="--disable-x11"
 fi
--- packages/wayland/wayland/package.mk
+++ packages/wayland/wayland/package.mk
@@ -1,23 +1,37 @@
-# SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
+#
+#  OpenELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  OpenELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
 
 PKG_NAME="wayland"
-PKG_VERSION="1.17.93"
+PKG_VERSION="1.14.0"
+PKG_REV="1"
+PKG_ARCH="any"
 PKG_LICENSE="OSS"
-PKG_SITE="https://wayland.freedesktop.org/"
-PKG_URL="https://wayland.freedesktop.org/releases/${PKG_NAME}-${PKG_VERSION}.tar.xz"
+PKG_SITE="http://wayland.freedesktop.org/"
+PKG_URL="http://wayland.freedesktop.org/releases/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain wayland:host libxml2"
 PKG_DEPENDS_HOST="libffi:host expat:host libxml2:host"
-PKG_LONGDESC="a display server protocol"
+PKG_PRIORITY="optional"
+PKG_SECTION="wayland"
+PKG_SHORTDESC=""
+PKG_LONGDESC=""
 
-PKG_TOOLCHAIN="configure"
-
-# Odroid Go Advance uses libmali's wayland-egl
-# so we need to make sure that libmali is installed
-# to serve the wayland-egl package correctly
-if [ "$DEVICE" == "OdroidGoAdvance" ]; then
-  PKG_DEPENDS_TARGET+=" libmali"
-fi
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="yes"
 
 PKG_CONFIGURE_OPTS_HOST="--enable-shared \
                          --disable-static \
@@ -32,6 +46,10 @@ PKG_CONFIGURE_OPTS_TARGET="--with-sysroot=$SYSROOT_PREFIX \
                            --enable-libraries \
                            --disable-documentation \
                            --with-gnu-ld"
+
+pre_configure_target() {
+  strip_lto
+}
 
 post_makeinstall_target() {
   rm -rf $INSTALL/usr/bin
