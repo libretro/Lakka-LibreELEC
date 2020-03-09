31087c5f4cee1d2f11771cfa6aa727d1125ac13e
natinusala <natinusala@gmail.com>
2020-03-09 19:37:00 +0100

    retroarch: update

--- packages/libretro/retroarch/package.mk
+++ packages/libretro/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="a10db7c"
+PKG_VERSION="c007b68"
 PKG_REV="11"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
