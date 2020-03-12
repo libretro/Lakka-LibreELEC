b5418dedf9f27901b0277f887a2db0b60ff8de08
MilhouseVH <milhouseVH.github@nmacleod.com>
2020-03-12 23:03:01 +0000

    linux (Generic/Allwinner): update to linux-5.5.9

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -29,8 +29,8 @@ case "$LINUX" in
     PKG_SOURCE_NAME="linux-$LINUX-$PKG_VERSION.tar.gz"
     ;;
   *)
-    PKG_VERSION="5.5.8"
-    PKG_SHA256="e5b6f6938e99a39fb359618e92349ca4e2cd9db31e1b81b7d14d2fa7dc03fc06"
+    PKG_VERSION="5.5.9"
+    PKG_SHA256="a435e16950bbe80362495383c2b5e8b78a4b3879c894e2b3c38ecba6fe7ca878"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v5.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;
