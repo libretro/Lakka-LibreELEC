a88074841043605d36b9b48401789acf57c477be
Matthias Reichl <hias@horus.com>
2020-03-22 14:33:57 +0100

    libprojectM: update to 3.1.2
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/graphics/libprojectM/package.mk
+++ packages/graphics/libprojectM/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libprojectM"
-PKG_VERSION="3.1.1-rc4"
-PKG_SHA256="a0a3cf3a24e372ddfaab43c509c679ed8f5f0e962093228d2982c58e59eb2034"
+PKG_VERSION="v3.1.2"
+PKG_SHA256="d59f61806f4e0e89bb42c494514ee295b3a7c4893432a1d38607777040a90823"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/projectM-visualizer/projectm"
 PKG_URL="https://github.com/projectM-visualizer/projectm/archive/$PKG_VERSION.tar.gz"
