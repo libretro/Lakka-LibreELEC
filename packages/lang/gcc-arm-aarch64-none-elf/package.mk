7ec5ec3f93ba2d47f0db7c6505887fe147c96e89
Catalin Toda <catalinii@yahoo.com>
2020-03-27 20:16:27 -0700

    Add linaro gcc for compiling on host on aarch64

---
+++ packages/lang/gcc-arm-aarch64-none-elf/package.mk
@@ -0,0 +1,36 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="gcc-arm-aarch64-none-elf"
+PKG_VERSION="9.2-2019.12"
+PKG_SHA256="041ca53bdd434b1a48f32161e957da1b84543c373d1881e2fd84a1579f87f243"
+PKG_LICENSE="GPL"
+PKG_SITE="https://developer.arm.com/tools-and-software/open-source-software/developer-tools/gnu-toolchain/gnu-a"
+PKG_URL="https://developer.arm.com/-/media/Files/downloads/gnu-a/${PKG_VERSION}/binrel/gcc-arm-${PKG_VERSION}-aarch64-aarch64-none-elf.tar.xz"
+PKG_DEPENDS_HOST="ccache:host"
+PKG_LONGDESC="ARM Aarch64 GNU Linux Binary Toolchain"
+PKG_TOOLCHAIN="manual"
+
+makeinstall_host() {
+  mkdir -p $TOOLCHAIN/lib/gcc-arm-aarch64-none-elf/
+    cp -a * $TOOLCHAIN/lib/gcc-arm-aarch64-none-elf
+
+  # wrap gcc and g++ with ccache like in gcc package.mk
+  PKG_GCC_PREFIX="$TOOLCHAIN/lib/gcc-arm-aarch64-none-elf/bin/aarch64-none-elf-"
+
+  cp "${PKG_GCC_PREFIX}gcc" "${PKG_GCC_PREFIX}gcc.real"
+cat > "${PKG_GCC_PREFIX}gcc" << EOF
+#!/bin/sh
+$TOOLCHAIN/bin/ccache ${PKG_GCC_PREFIX}gcc.real "\$@"
+EOF
+
+  chmod +x "${PKG_GCC_PREFIX}gcc"
+
+  cp "${PKG_GCC_PREFIX}g++" "${PKG_GCC_PREFIX}g++.real"
+cat > "${PKG_GCC_PREFIX}g++" << EOF
+#!/bin/sh
+$TOOLCHAIN/bin/ccache ${PKG_GCC_PREFIX}g++.real "\$@"
+EOF
+
+  chmod +x "${PKG_GCC_PREFIX}g++"
+}
