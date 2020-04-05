107293854b33c1e7eab7c9e4057d9b5893e63fa2
David Lawson <dmrlawson@gmail.com>
2020-04-05 13:01:42 +0100

    sameboy: update PKG_VERSION to a commit that exists
    Not sure what happened but it appears to be pinned to a commit that no longer exists.
    
    Updated with `./libretro_update.sh -p sameboy`

--- packages/libretro/sameboy/package.mk
+++ packages/libretro/sameboy/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="sameboy"
-PKG_VERSION="da29506"
+PKG_VERSION="c9e547c"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
