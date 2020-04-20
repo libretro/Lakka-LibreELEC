965b3d31f602ae0d03341972cee8c6fa680790d0
Matthias Reichl <hias@horus.com>
2020-04-20 22:07:04 +0200

    kodi binary addons: add visualization.starburst
    Signed-off-by: Matthias Reichl <hias@horus.com>

---
+++ packages/mediacenter/kodi-binary-addons/visualization.starburst/package.mk
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="visualization.starburst"
+PKG_VERSION="2.1.1-Matrix"
+PKG_SHA256="5f77920509941bfe82f29521a5a8ec437f7aefdc34a4c42fa3c3ba0b92bc8b5a"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/xbmc/visualization.starburst"
+PKG_URL="https://github.com/xbmc/visualization.starburst/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform glm"
+PKG_SECTION=""
+PKG_SHORTDESC="visualization.starburst"
+PKG_LONGDESC="visualization.starburst"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="xbmc.player.musicviz"
