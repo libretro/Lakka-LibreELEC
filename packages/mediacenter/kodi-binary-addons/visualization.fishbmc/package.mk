840e10c47490b6282ab74ee038c98d383e57ca35
Matthias Reichl <hias@horus.com>
2020-04-20 22:06:17 +0200

    kodi binary addons: add visualization.fishbmc
    Signed-off-by: Matthias Reichl <hias@horus.com>

---
+++ packages/mediacenter/kodi-binary-addons/visualization.fishbmc/package.mk
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="visualization.fishbmc"
+PKG_VERSION="6.0.1-Matrix"
+PKG_SHA256="ce38c70ee0727d3a8f4037fae40a509bd148128cdb6fe6756cef39e59ef80bd8"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/xbmc/visualization.fishbmc"
+PKG_URL="https://github.com/xbmc/visualization.fishbmc/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform glm"
+PKG_SECTION=""
+PKG_SHORTDESC="visualization.fishbmc"
+PKG_LONGDESC="visualization.fishbmc"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="xbmc.player.musicviz"
