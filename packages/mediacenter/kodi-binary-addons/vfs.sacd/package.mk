66e3c809fe41183835d028a0a98c9ac58373f2ce
Matthias Reichl <hias@horus.com>
2020-04-20 12:56:40 +0200

    kodi binary addons: add vfs.sacd
    Signed-off-by: Matthias Reichl <hias@horus.com>

---
+++ packages/mediacenter/kodi-binary-addons/vfs.sacd/package.mk
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="vfs.sacd"
+PKG_VERSION="1.2.0-Matrix"
+PKG_SHA256="a633a848f7e4e3c72f378edca5bda71ee6fe6e73416ad16db8480a8096f48f31"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/xbmc/vfs.sacd"
+PKG_URL="https://github.com/xbmc/vfs.sacd/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform"
+PKG_SECTION=""
+PKG_SHORTDESC="vfs.sacd"
+PKG_LONGDESC="vfs.sacd"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="kodi.vfs"
