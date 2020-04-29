d9643acfdfc4aa567a4a343e76eab4a0dbf33ba1
MilhouseVH <milhouseVH.github@nmacleod.com>
2020-04-29 22:15:49 +0100

    linux (RPi/RPi2/RPi4): update to linux-bcd2408 (5.4.35 with CONFIG_DRM_VC4_HDMI_CEC)

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -23,8 +23,8 @@ case "$LINUX" in
     PKG_PATCH_DIRS="amlogic"
     ;;
   raspberrypi)
-    PKG_VERSION="cda6aaf08ad0f88926611ec50e7de4c4b4f88832" # 5.4.35 with ISP
-    PKG_SHA256="b14f6aab5a4c32d71e203bb745bd78bc24c4daf04c5b29114fab30b177276bf2"
+    PKG_VERSION="bcd240863396bdb0c127261409d4fae5a67d8000" # 5.4.35 with CONFIG_DRM_VC4_HDMI_CEC
+    PKG_SHA256="531ecd8b0c3c86a5b43386ea0e594341c860b43a351b8a2162272ea45698abac"
     PKG_URL="https://github.com/raspberrypi/linux/archive/$PKG_VERSION.tar.gz"
     PKG_SOURCE_NAME="linux-$LINUX-$PKG_VERSION.tar.gz"
     ;;
