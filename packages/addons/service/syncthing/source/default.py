da51883d871bfee8d4896219045b34a44e52c35f
awiouy <awiouy@gmail.com>
2020-05-01 13:43:18 +0200

    syncthing: update to 1.4.2

--- packages/addons/service/syncthing/source/default.py
+++ packages/addons/service/syncthing/source/default.py
@@ -8,13 +8,13 @@ import xbmcaddon
 
 class Monitor(xbmc.Monitor):
 
-   def __init__(self, *args, **kwargs):
-      xbmc.Monitor.__init__(self)
-      self.id = xbmcaddon.Addon().getAddonInfo('id')
+    def __init__(self, *args, **kwargs):
+        xbmc.Monitor.__init__(self)
+        self.id = xbmcaddon.Addon().getAddonInfo('id')
 
-   def onSettingsChanged(self):
-      subprocess.call(['systemctl', 'restart', self.id])
+    def onSettingsChanged(self):
+        subprocess.call(['systemctl', 'restart', self.id])
 
 
-if __name__ == "__main__":
-   Monitor().waitForAbort()
+if __name__ == '__main__':
+    Monitor().waitForAbort()
