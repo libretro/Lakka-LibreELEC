7eef75bce3afbcfa573662e2d5bb0577cdc6b6bb
MilhouseVH <milhouseVH.github@nmacleod.com>
2020-05-11 19:46:05 +0100

    rpi-eeprom: update to rpi-eeprom-cbb061e

--- packages/tools/rpi-eeprom/package.mk
+++ packages/tools/rpi-eeprom/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="rpi-eeprom"
-PKG_VERSION="a70a647233a1103dc12ec006b46e8451f4b93401"
-PKG_SHA256="ce5410222b07dd493b66a87f12babd97bc2ecaea2593ca710b03d65947580cfa"
+PKG_VERSION="cbb061e0c3ccff823d866733970addc206821415"
+PKG_SHA256="b5fc4061ffc795b59854c5118e88fbe7329dd1551c8a054968f57128ed479e9b"
 PKG_ARCH="arm"
 PKG_LICENSE="BSD-3/custom"
 PKG_SITE="https://github.com/raspberrypi/rpi-eeprom"
