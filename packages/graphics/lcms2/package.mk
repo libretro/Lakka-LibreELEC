d7b25b0e99086448ae49bad6dc16d6dfa72f2490
CvH <namerp@googlemail.com>
2020-06-14 19:53:16 +0200

    lcms2: initial package

---
+++ packages/graphics/lcms2/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="lcms2"
+PKG_VERSION="2.9"
+PKG_SHA256="8e23a09dc81af856db37941a4ea26acdf6a45b0281ec5b7ee94b5a4e9f7afbf7"
+PKG_LICENSE="MIT"
+PKG_SITE="http://www.littlecms.com"
+PKG_URL="https://github.com/mm2/Little-CMS/archive/lcms${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain tiff"
+PKG_LONGDESC="An small-footprint color management engine, with special focus on accuracy and performance."
