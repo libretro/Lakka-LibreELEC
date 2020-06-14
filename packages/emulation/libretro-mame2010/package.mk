0a49476a93ba0e9222f603e732cd9fe3681dcb95
CvH <namerp@googlemail.com>
2020-06-14 12:14:43 +0200

    libretro-mame2010: add case cortex-a8

--- packages/emulation/libretro-mame2010/package.mk
+++ packages/emulation/libretro-mame2010/package.mk
@@ -23,7 +23,7 @@ pre_configure_target() {
     arm1176jzf-s)
       PKG_MAKE_OPTS_TARGET="platform=armv6-hardfloat-$TARGET_CPU"
       ;;
-    cortex-a7|cortex-a9)
+    cortex-a7|cortex-a8|cortex-a9)
       PKG_MAKE_OPTS_TARGET="platform=armv7-neon-hardfloat-$TARGET_CPU"
       ;;
     *cortex-a53|cortex-a17)
