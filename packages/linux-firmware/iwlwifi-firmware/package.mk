ebac9df49edacdca3147cd49771f0d9207dd76aa
MilhouseVH <milhouseVH.github@nmacleod.com>
2020-06-14 16:40:50 +0100

    iwlwifi-firmware: update to iwlwifi-firmware-422508e

--- packages/linux-firmware/iwlwifi-firmware/package.mk
+++ packages/linux-firmware/iwlwifi-firmware/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="iwlwifi-firmware"
-PKG_VERSION="83cdbfd68b3e3f864afbcd79de3cb274350c888c"
-PKG_SHA256="beaea52c65d940acedfbfb40a65427ed2de30c3092c12df938036d6e4fd7b404"
+PKG_VERSION="422508ec618285ef41a6a4baae731cb84b2a53be"
+PKG_SHA256="67f6097c62feb82bd51bd94b8b5607a37bf5cb34ae079649d35e505c2db2b107"
 PKG_LICENSE="Free-to-use"
 PKG_SITE="https://github.com/LibreELEC/iwlwifi-firmware"
 PKG_URL="https://github.com/LibreELEC/iwlwifi-firmware/archive/$PKG_VERSION.tar.gz"
