04a6b86c37b1abdd4413ccf486b7c15c3de84094
Matthias Reichl <hias@horus.com>
2020-06-17 13:14:54 +0200

    bcm2835-bootloader: revert to pre-common firmware on RPi0-3
    Apr 16 2020 and later firmwares cause boot issues on some RPi3s,
    see eg:
    https://forum.libreelec.tv/thread/22153-rpi3-updade-to-9-2-3-ending-in-black-screen/
    
    Switch back to the Apr 15 firmware, which is also what Raspbian /
    Raspberry Pi OS is shipping via apt for RPi0-3, until these issues
    have been resolved.
    
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/tools/bcm2835-bootloader/package.mk
+++ packages/tools/bcm2835-bootloader/package.mk
@@ -3,8 +3,17 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="bcm2835-bootloader"
-PKG_VERSION="cb2b95d73e9f0b1ebf05e03bb1959603d982feeb"
-PKG_SHA256="e223022bf48ba55aecbcb0013f98cb50ef449daffcfb96cf9af48c5b61e85869"
+
+# use latest master firmware on RPi4 and latest pre-common
+# firmware on RPi0-3
+if [ "$DEVICE" = "RPi4" ]; then
+  PKG_VERSION="cb2b95d73e9f0b1ebf05e03bb1959603d982feeb"
+  PKG_SHA256="e223022bf48ba55aecbcb0013f98cb50ef449daffcfb96cf9af48c5b61e85869"
+else
+  PKG_VERSION="9e3c23ce779e8cf44c33d6a25bba249319207f68"
+  PKG_SHA256="7ab85b6d7082be87556bc02353b97f97bb1d4af304e4004a3d7ad2a17bb8a696"
+fi
+
 PKG_ARCH="arm"
 PKG_LICENSE="nonfree"
 PKG_SITE="http://www.broadcom.com"
