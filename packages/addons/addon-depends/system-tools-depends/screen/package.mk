ecddcc97da2893c345a13f6a0985db2ac28b114e
CvH <namerp@googlemail.com>
2020-06-21 22:50:17 +0200

    screen: update to 4.8.0

--- packages/addons/addon-depends/system-tools-depends/screen/package.mk
+++ packages/addons/addon-depends/system-tools-depends/screen/package.mk
@@ -2,14 +2,14 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="screen"
-PKG_VERSION="4.6.2"
-PKG_SHA256="1b6922520e6a0ce5e28768d620b0f640a6631397f95ccb043b70b91bb503fa3a"
+PKG_VERSION="4.8.0"
+PKG_SHA256="6e11b13d8489925fde25dfb0935bf6ed71f9eb47eff233a181e078fde5655aa1"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/software/screen/"
 PKG_URL="http://ftpmirror.gnu.org/screen/$PKG_NAME-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain ncurses"
 PKG_LONGDESC="Screen is a window manager that multiplexes a physical terminal between several processes"
-PKG_BUILD_FLAGS="-parallel -sysroot"
+PKG_BUILD_FLAGS="-sysroot"
 
 PKG_CONFIGURE_OPTS_TARGET="ac_cv_header_utempter_h=no \
                            --enable-colors256 \
@@ -18,11 +18,3 @@ PKG_CONFIGURE_OPTS_TARGET="ac_cv_header_utempter_h=no \
                            --disable-telnet \
                            --disable-socket-dir"
 
-pre_configure_target() {
-  CFLAGS="$CFLAGS -DTERMINFO"
-  export LDFLAGS=`echo $LDFLAGS | sed -e "s|-Wl,--as-needed||"`
-
-# screen fails to build in subdirs
-  cd $PKG_BUILD
-    rm -rf .$TARGET_NAME
-}
