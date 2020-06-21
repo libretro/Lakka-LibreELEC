36acaf38bc3fb6efe5f37dfaa04d2002aac26255
CvH <namerp@googlemail.com>
2020-06-21 23:09:16 +0200

    system-tools: cleanup package

--- packages/addons/tools/system-tools/package.mk
+++ packages/addons/tools/system-tools/package.mk
@@ -56,17 +56,13 @@ if [ "$TARGET_ARCH" = "x86_64" ]; then
 fi
 
 addon() {
-  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/lib/
-  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/data/
-  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin/
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/{bin,data,lib}
+
     # autossh
     cp -P $(get_install_dir autossh)/usr/bin/autossh $ADDON_BUILD/$PKG_ADDON_ID/bin
 
     # diffutils
-    cp -P $(get_install_dir diffutils)/usr/bin/cmp $ADDON_BUILD/$PKG_ADDON_ID/bin
-    cp -P $(get_install_dir diffutils)/usr/bin/diff $ADDON_BUILD/$PKG_ADDON_ID/bin
-    cp -P $(get_install_dir diffutils)/usr/bin/diff3 $ADDON_BUILD/$PKG_ADDON_ID/bin
-    cp -P $(get_install_dir diffutils)/usr/bin/sdiff $ADDON_BUILD/$PKG_ADDON_ID/bin
+    cp -P $(get_install_dir diffutils)/usr/bin/{cmp,diff,diff3,sdiff} $ADDON_BUILD/$PKG_ADDON_ID/bin
 
     # dstat
     cp -P $(get_install_dir dstat)/usr/bin/dstat $ADDON_BUILD/$PKG_ADDON_ID/bin
@@ -78,8 +74,7 @@ addon() {
     cp -P $(get_install_dir efibootmgr)/usr/bin/efibootmgr $ADDON_BUILD/$PKG_ADDON_ID/bin 2>/dev/null || :
 
     # encfs
-    cp -P $(get_install_dir encfs)/usr/bin/encfs $ADDON_BUILD/$PKG_ADDON_ID/bin
-    cp -P $(get_install_dir encfs)/usr/bin/encfsctl $ADDON_BUILD/$PKG_ADDON_ID/bin
+    cp -P $(get_install_dir encfs)/usr/bin/{encfs,encfsctl} $ADDON_BUILD/$PKG_ADDON_ID/bin
 
     # evtest
     cp -P $(get_install_dir evtest)/usr/bin/evtest $ADDON_BUILD/$PKG_ADDON_ID/bin
@@ -108,15 +103,11 @@ addon() {
     cp -P $(get_install_dir htop)/usr/bin/htop $ADDON_BUILD/$PKG_ADDON_ID/bin
 
     # i2c-tools
-    cp -P $(get_install_dir i2c-tools)/usr/sbin/i2cdetect $ADDON_BUILD/$PKG_ADDON_ID/bin
-    cp -P $(get_install_dir i2c-tools)/usr/sbin/i2cdump $ADDON_BUILD/$PKG_ADDON_ID/bin
-    cp -P $(get_install_dir i2c-tools)/usr/sbin/i2cget $ADDON_BUILD/$PKG_ADDON_ID/bin
-    cp -P $(get_install_dir i2c-tools)/usr/sbin/i2cset $ADDON_BUILD/$PKG_ADDON_ID/bin
+    cp -P $(get_install_dir i2c-tools)/usr/sbin/{i2cdetect,i2cdump,i2cget,i2cset} $ADDON_BUILD/$PKG_ADDON_ID/bin
     cp -P $(get_install_dir i2c-tools)/usr/lib/$PKG_PYTHON_VERSION/site-packages/smbus.so $ADDON_BUILD/$PKG_ADDON_ID/lib
 
     # inotify-tools
-    cp -P $(get_install_dir inotify-tools)/usr/bin/inotifywait $ADDON_BUILD/$PKG_ADDON_ID/bin
-    cp -P $(get_install_dir inotify-tools)/usr/bin/inotifywatch $ADDON_BUILD/$PKG_ADDON_ID/bin
+    cp -P $(get_install_dir inotify-tools)/usr/bin/{inotifywait,inotifywatch} $ADDON_BUILD/$PKG_ADDON_ID/bin
 
     # jq
     cp -P $(get_install_dir jq)/usr/bin/jq $ADDON_BUILD/$PKG_ADDON_ID/bin
@@ -142,10 +133,8 @@ addon() {
     cp -P $(get_install_dir nmon)/usr/bin/nmon $ADDON_BUILD/$PKG_ADDON_ID/bin/
 
     # p7zip
-    cp -P $(get_install_dir p7zip)/usr/bin/7z.so $ADDON_BUILD/$PKG_ADDON_ID/bin
+    cp -P $(get_install_dir p7zip)/usr/bin/{7z,7za,7z.so} $ADDON_BUILD/$PKG_ADDON_ID/bin
     cp -PR $(get_install_dir p7zip)/usr/bin/Codecs $ADDON_BUILD/$PKG_ADDON_ID/bin
-    cp -P $(get_install_dir p7zip)/usr/bin/7z $ADDON_BUILD/$PKG_ADDON_ID/bin
-    cp -P $(get_install_dir p7zip)/usr/bin/7za $ADDON_BUILD/$PKG_ADDON_ID/bin
 
     # patch
     cp -P $(get_install_dir patch)/usr/bin/patch $ADDON_BUILD/$PKG_ADDON_ID/bin
