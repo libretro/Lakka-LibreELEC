2131a790ea513e67ad0d1993afb8ce452a80e352
CvH <namerp@googlemail.com>
2020-06-21 22:50:17 +0200

    stress-ng: update to 0.11.14

--- packages/addons/addon-depends/system-tools-depends/stress-ng/package.mk
+++ packages/addons/addon-depends/system-tools-depends/stress-ng/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="stress-ng"
-PKG_VERSION="0.09.50"
-PKG_SHA256="b07290495794092cdf2f8d51ac631f28b8cec9455a16552d8ca48bc1f376c634"
+PKG_VERSION="0.11.14"
+PKG_SHA256="b21436fdbd9dc482a3fd95ae27cccf0097d0f226361ea3785215f7a4ad50136b"
 PKG_LICENSE="GPLv2"
 PKG_SITE="http://kernel.ubuntu.com/~cking/stress-ng/"
 PKG_URL="http://kernel.ubuntu.com/~cking/tarballs/stress-ng/stress-ng-$PKG_VERSION.tar.xz"
