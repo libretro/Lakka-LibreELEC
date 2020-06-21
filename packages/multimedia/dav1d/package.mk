35460e69c71beafb71d54e0f6a545e9e172efdce
CvH <namerp@googlemail.com>
2020-06-21 17:31:12 +0200

    dav1d: update to 0.7.1

--- packages/multimedia/dav1d/package.mk
+++ packages/multimedia/dav1d/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="dav1d"
-PKG_VERSION="0.7.0"
-PKG_SHA256="8057149f5f08c5ca47e1344fba9046ff84ac85ca409d7adbec8268c707ec5c19"
+PKG_VERSION="0.7.1"
+PKG_SHA256="9eac4f50089f54a9f562827bda4a21187d68c01d8b20055eef1d7efca9f84cf8"
 PKG_LICENSE="BSD"
 PKG_SITE="http://www.jbkempf.com/blog/post/2018/Introducing-dav1d"
 PKG_URL="https://code.videolan.org/videolan/dav1d/-/archive/${PKG_VERSION}/dav1d-${PKG_VERSION}.tar.bz2"
