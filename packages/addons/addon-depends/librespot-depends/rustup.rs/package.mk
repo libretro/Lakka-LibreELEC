461c744012891090f539fde5402730ccd8e4cf95
CvH <namerp@googlemail.com>
2020-06-21 22:50:17 +0200

    rustup.rs: update to 1.21.1

--- packages/addons/addon-depends/librespot-depends/rustup.rs/package.mk
+++ packages/addons/addon-depends/librespot-depends/rustup.rs/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="rustup.rs"
-PKG_VERSION="1.20.2"
-PKG_SHA256="28207ee4c2d66840ca903df152b23b916326a5d3eeb643a1de0f24a16afa4209"
+PKG_VERSION="1.21.1"
+PKG_SHA256="3dd54cb15313ff01c930ad4e36326f7d60caadd2d6707790d83bea26fbb8bbe1"
 PKG_LICENSE="MIT"
 PKG_SITE="https://www.rust-lang.org"
 PKG_URL="https://github.com/rust-lang-nursery/rustup.rs/archive/$PKG_VERSION.tar.gz"
