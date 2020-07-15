24fc13636211c185e87db5dd321560249ca473d2
Ian Leonard <antonlacon@gmail.com>
2020-07-15 00:23:05 +0000

    libfmt: update to 7.0.1
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- packages/devel/libfmt/package.mk
+++ packages/devel/libfmt/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libfmt"
-PKG_VERSION="6.2.1"
-PKG_SHA256="5edf8b0f32135ad5fafb3064de26d063571e95e8ae46829c2f4f4b52696bbff0"
+PKG_VERSION="7.0.1"
+PKG_SHA256="ac335a4ca6beaebec4ddb2bc35b9ae960b576f3b64a410ff2c379780f0cd4948"
 PKG_LICENSE="BSD"
 PKG_SITE="https://github.com/fmtlib/fmt"
 PKG_URL="https://github.com/fmtlib/fmt/archive/$PKG_VERSION.tar.gz"
