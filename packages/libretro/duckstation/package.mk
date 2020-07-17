efe01a02ce5ea1d75ae4af1ccdf1ad2d8c456cf1
gouchi <gouchi@free.fr>
2020-07-17 16:31:49 +0200

    duckstation : Add duckstation libretro core (#978)

---
+++ packages/libretro/duckstation/package.mk
@@ -0,0 +1,44 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.tv; see the file COPYING.  If not, write to
+#  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
+#  http://www.gnu.org/copyleft/gpl.html
+################################################################################
+
+PKG_NAME="duckstation"
+PKG_VERSION="9cd7512"
+PKG_REV="1"
+PKG_ARCH="x86_64 aarch64"
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://github.com/stenzek/duckstation"
+PKG_URL="$PKG_SITE.git"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="libretro"
+PKG_SHORTDESC="DuckStation is an simulator/emulator of the Sony PlayStation(TM) console, focusing on playability, speed, and long-term maintainability."
+PKG_LONGDESC="DuckStation is an simulator/emulator of the Sony PlayStation(TM) console, focusing on playability, speed, and long-term maintainability."
+PKG_TOOLCHAIN="cmake-make"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release \
+		       -DBUILD_LIBRETRO_CORE=ON"
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/libretro
+  cp duckstation_libretro.so $INSTALL/usr/lib/libretro/
+}
