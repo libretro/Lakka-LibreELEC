704b5d2d48b37f35b033990dcb255fa7e6034a6e
Matthias Reichl <hias@horus.com>
2020-07-30 09:48:16 +0200

    dispmanx_vnc: remove addon
    RPi graphic stack no longer use dispmanx
    
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/addons/service/dispmanx_vnc/package.mk
+++ packages/addons/service/dispmanx_vnc/package.mk
@@ -1,35 +1,26 @@
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="dispmanx_vnc"
-PKG_VERSION="5bde54b5a43a2e6f65994f3b9c665db90c45923f"
-PKG_SHA256="1f096cd70d86620596db947757486064fe37dc2766ad33530318c35b279472aa"
-PKG_VERSION_DATE="2018-08-18"
-PKG_REV="106"
+PKG_VERSION="1.0"
+PKG_REV="100"
 PKG_ARCH="arm"
 PKG_ADDON_PROJECTS="RPi"
 PKG_LICENSE="OSS"
-PKG_SITE="https://github.com/patrikolausson/dispmanx_vnc"
-PKG_URL="https://github.com/patrikolausson/dispmanx_vnc/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain libvncserver bcm2835-bootloader bcm2835-driver libconfig openssl"
+PKG_SITE=""
+PKG_URL=""
+PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="service/system"
-PKG_SHORTDESC="Raspberry Pi VNC: a Virtual Network Computing server for Raspberry Pi"
-PKG_LONGDESC="Raspberry Pi VNC ($PKG_VERSION_DATE) is a Virtual Network Computing (VNC) server for Raspberry Pi using dispmanx"
+PKG_SHORTDESC="Addon removed"
+PKG_LONGDESC="Addon removed"
+PKG_TOOLCHAIN="manual"
+
+PKG_ADDON_BROKEN="Raspberry Pi VNC was removed."
 
 PKG_IS_ADDON="yes"
 PKG_ADDON_NAME="Raspberry Pi VNC"
-PKG_ADDON_TYPE="xbmc.service"
-PKG_MAINTAINER="Anton Voyl (awiouy)"
-
-pre_make_target() {
-  export SYSROOT_PREFIX
-}
-
-makeinstall_target() {
-  :
-}
+PKG_ADDON_TYPE="xbmc.broken"
 
 addon() {
-  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
-    cp -p $PKG_BUILD/dispmanx_vncserver $ADDON_BUILD/$PKG_ADDON_ID/bin
+  :
 }
