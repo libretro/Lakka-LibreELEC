86268c038bb694a698222cfa78c840eb3bf7a0fb
Ntemis <ierokipides.dem@gmail.com>
2020-08-07 20:08:10 +0200

    GCC-9.3:Fix remaining issues after upgrade

--- packages/graphics/glu/package.mk
+++ packages/graphics/glu/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="glu"
-PKG_VERSION="9.0.0"
-PKG_SHA256="3d19cca9b26ec4048dd22e3d294acd43e080a3205a29ff47765bd514571ea8f9"
+PKG_VERSION="9.0.1"
+PKG_SHA256="f98dff2045e0c557da88553bf4cf9958f49cd5bd40d33d2f8b5e13c8f904d126"
 PKG_LICENSE="OSS"
 PKG_SITE="http://cgit.freedesktop.org/mesa/glu/"
 PKG_URL="http://cgit.freedesktop.org/mesa/glu/snapshot/$PKG_NAME-$PKG_VERSION.tar.gz"
