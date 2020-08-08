8682b99e89143b615b8ae3da390e3b87a95202ca
Ian Leonard <antonlacon@gmail.com>
2020-08-08 00:02:10 +0000

    e2fsprogs: don't create ext4dev symlinks
    ext4dev was promoted to ext4 in 2012.
    
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- packages/sysutils/e2fsprogs/package.mk
+++ packages/sysutils/e2fsprogs/package.mk
@@ -94,14 +94,12 @@ makeinstall_init() {
     ln -sf e2fsck $INSTALL/usr/sbin/fsck.ext2
     ln -sf e2fsck $INSTALL/usr/sbin/fsck.ext3
     ln -sf e2fsck $INSTALL/usr/sbin/fsck.ext4
-    ln -sf e2fsck $INSTALL/usr/sbin/fsck.ext4dev
 
   if [ $INITRAMFS_PARTED_SUPPORT = "yes" ]; then
     cp misc/mke2fs $INSTALL/usr/sbin
     ln -sf mke2fs $INSTALL/usr/sbin/mkfs.ext2
     ln -sf mke2fs $INSTALL/usr/sbin/mkfs.ext3
     ln -sf mke2fs $INSTALL/usr/sbin/mkfs.ext4
-    ln -sf mke2fs $INSTALL/usr/sbin/mkfs.ext4dev
   fi
 }
 
