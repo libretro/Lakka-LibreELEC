34206a057c13a9cca166ae7bebcfcae3880bc06b
Matthias Reichl <hias@horus.com>
2020-08-23 11:27:44 +0200

    libmicrohttpd: update to 0.9.71
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/web/libmicrohttpd/package.mk
+++ packages/web/libmicrohttpd/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libmicrohttpd"
-PKG_VERSION="0.9.70"
-PKG_SHA256="90d0a3d396f96f9bc41eb0f7e8187796049285fabef82604acd4879590977307"
+PKG_VERSION="0.9.71"
+PKG_SHA256="e8f445e85faf727b89e9f9590daea4473ae00ead38b237cf1eda55172b89b182"
 PKG_LICENSE="LGPLv2.1"
 PKG_SITE="http://www.gnu.org/software/libmicrohttpd/"
 PKG_URL="http://ftpmirror.gnu.org/libmicrohttpd/$PKG_NAME-$PKG_VERSION.tar.gz"
