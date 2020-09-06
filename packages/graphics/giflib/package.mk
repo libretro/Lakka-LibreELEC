4bc79b38f1e8279e0ce75c46e24190bcf4d0669b
Matthias Reichl <hias@horus.com>
2020-09-06 15:11:53 +0200

    giflib: update to 5.2.1
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/graphics/giflib/package.mk
+++ packages/graphics/giflib/package.mk
@@ -2,18 +2,21 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="giflib"
-PKG_VERSION="5.1.4"
-PKG_SHA256="df27ec3ff24671f80b29e6ab1c4971059c14ac3db95406884fc26574631ba8d5"
+PKG_VERSION="5.2.1"
+PKG_SHA256="31da5562f44c5f15d63340a09a4fd62b48c45620cd302f77a6d9acf0077879bd"
 PKG_LICENSE="OSS"
 PKG_SITE="http://giflib.sourceforge.net/"
-PKG_URL="$SOURCEFORGE_SRC/giflib/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_URL="$SOURCEFORGE_SRC/giflib/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_HOST="zlib:host"
 PKG_DEPENDS_TARGET="toolchain zlib"
 PKG_LONGDESC="giflib: giflib service library"
+PKG_TOOLCHAIN="manual"
 
-PKG_CONFIGURE_OPTS_HOST="--disable-shared --enable-static"
-PKG_CONFIGURE_OPTS_TARGET="--with-sysroot=$SYSROOT_PREFIX"
+make_host() {
+  make libgif.a CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}"
+}
 
-post_makeinstall_target() {
-  rm -rf $INSTALL/usr/bin
+makeinstall_host() {
+  make install-include PREFIX="${TOOLCHAIN}"
+  make install-lib PREFIX="${TOOLCHAIN}"
 }
