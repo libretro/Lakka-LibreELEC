d08600d07af893933b6d8542a65edff25ed25b83
Matthias Reichl <hias@horus.com>
2020-09-07 00:10:35 +0200

    libplist: update to 2.2.0
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/devel/libplist/package.mk
+++ packages/devel/libplist/package.mk
@@ -2,16 +2,21 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="libplist"
-PKG_VERSION="2.0.0"
-PKG_SHA256="3a7e9694c2d9a85174ba1fa92417cfabaea7f6d19631e544948dc7e17e82f602"
+PKG_VERSION="2.2.0"
+PKG_SHA256="4a5517e5377ec421df84c586ba85bb4e1d26f11ad203d7d450a907c0156fbd9a"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.libimobiledevice.org/"
-PKG_URL="http://www.libimobiledevice.org/downloads/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_URL="https://github.com/libimobiledevice/libplist/releases/download/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain glib"
 PKG_LONGDESC="libplist is a library for manipulating Apple Binary and XML Property Lists"
 PKG_TOOLCHAIN="autotools"
 
 PKG_CONFIGURE_OPTS_TARGET="--without-cython"
+
+pre_configure_target() {
+  # work around bashism in configure script
+  export CONFIG_SHELL="/bin/bash"
+}
 
 post_makeinstall_target() {
   rm -rf $INSTALL/usr/bin
