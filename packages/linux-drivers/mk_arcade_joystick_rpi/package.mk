996b121d02768efb52a09ca86054d3992c095e12
Valerio Turrini <Turro75@users.noreply.github.com>
2020-09-15 22:57:15 +0200

    add mk_arcade_joystick_rpi package
    small modification to compile on RPi4

---
+++ packages/linux-drivers/mk_arcade_joystick_rpi/package.mk
@@ -0,0 +1,70 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+#
+#  OpenELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  OpenELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="mk_arcade_joystick_rpi"
+PKG_VERSION="6a9fb02"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/Turro75/mk_arcade_joystick_rpi"
+PKG_URL="$PKG_SITE.git"
+PKG_SOURCE_DIR="${PKG_NAME}-${PKG_VERSION}"
+PKG_DEPENDS_TARGET="toolchain linux"
+PKG_NEED_UNPACK="$LINUX_DEPENDS"
+PKG_SECTION="driver"
+PKG_SHORTDESC="test building"
+PKG_TOOLCHAIN="manual"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+if [ "$TARGET_KERNEL_ARCH" = "arm64" -a "$TARGET_ARCH" = "arm" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET gcc-linaro-aarch64-elf:host"
+  export PATH=$TOOLCHAIN/lib/gcc-linaro-aarch64-elf/bin/:$PATH
+  TARGET_PREFIX=aarch64-elf-
+fi
+
+MYFLAG=""
+case $DEVICE in
+    RPi2) MYFLAG="-DRPI2"
+    ;;
+    RPi4) MYFLAG="-DRPI4"
+    ;;
+esac
+
+pre_make_target() {
+  unset LDFLAGS
+}
+
+make_target() {
+  cd $PKG_BUILD
+  make V=1 \
+       ARCH=$TARGET_KERNEL_ARCH \
+       KERNELDIR=$(kernel_path) \
+       CROSS_COMPILE=$TARGET_PREFIX \
+       CONFIG_POWER_SAVING=n \
+       ARCH="$ARCH" \
+       CFLAGS_mk_arcade_joystick_rpi.o="$MYFLAG" \
+       -f Makefile.cross
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/$(get_full_module_dir)/$PKG_NAME
+  cp $PKG_BUILD/*.ko $INSTALL/$(get_full_module_dir)/$PKG_NAME
+
+}
+
