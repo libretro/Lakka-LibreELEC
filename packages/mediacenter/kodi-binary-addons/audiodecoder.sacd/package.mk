81a64b4dcbf2b9cae33f7e35a4262a701b81c8bb
CvH <namerp@googlemail.com>
2020-09-22 18:09:11 +0200

    audiodecoder.sacd: initial package

---
+++ packages/mediacenter/kodi-binary-addons/audiodecoder.sacd/package.mk
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="audiodecoder.sacd"
+PKG_VERSION="0.1.1-Matrix"
+PKG_SHA256="8ef2faddcfa404625b474f4e5efbf260bb90e3aeed0158f6103b0e5b6c6579c6"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/xbmc/audiodecoder.sacd"
+PKG_URL="https://github.com/xbmc/audiodecoder.sacd/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform wavpack"
+PKG_SECTION=""
+PKG_SHORTDESC="audiodecoder.sacd"
+PKG_LONGDESC="audiodecoder.sacd"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="kodi.audiodecoder"
