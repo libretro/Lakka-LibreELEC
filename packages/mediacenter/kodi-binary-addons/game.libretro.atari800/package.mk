654a7d491f471ed4dc6af9b31bc1f831a066ea87
Matthias Reichl <hias@horus.com>
2020-10-10 10:52:23 +0200

    game.libretro.atari800: add package
    Signed-off-by: Matthias Reichl <hias@horus.com>

---
+++ packages/mediacenter/kodi-binary-addons/game.libretro.atari800/package.mk
@@ -0,0 +1,17 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="game.libretro.atari800"
+PKG_VERSION="3.1.0.4-Matrix"
+PKG_SHA256="15d3dfa8a59c8ea3e1be564c31f09d6dd6394f660d61219f43d8b8adac0a046a"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/kodi-game/game.libretro.atari800"
+PKG_URL="https://github.com/kodi-game/game.libretro.atari800/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform libretro-atari800"
+PKG_SECTION=""
+PKG_LONGDESC="game.libretro.atari800: atari800 for Kodi"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="kodi.gameclient"
