34b41b588d057c5eaf2b9a575793916fced39830
David Lawson <dmrlawson@gmail.com>
2020-10-20 20:44:18 +0100

    meson: update to 0.55.3
    Fixes systemd build

--- packages/python/devel/meson/package.mk
+++ packages/python/devel/meson/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="meson"
-PKG_VERSION="0.53.1"
-PKG_SHA256="ec1ba33eea701baca2c1607dac458152dc8323364a51fdef6babda2623413b04"
+PKG_VERSION="0.55.3"
+PKG_SHA256="6bed2a25a128bbabe97cf40f63165ebe800e4fcb46db8ab7ef5c2b5789f092a5"
 PKG_LICENSE="Apache"
 PKG_SITE="http://mesonbuild.com"
 PKG_URL="https://github.com/mesonbuild/meson/releases/download/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.gz"
