cc515dab96cc9a0d77dc37b1c154436f7f035c96
David G. F <david@davidgf.net>
2020-10-25 00:11:18 +0200

    Bump squashfs-tools version (#1070)
    Seems it has some GCC 10 compilation issues. This cleans up the patches.
    This also points to the actual repo (the kernel one seems drepecated).
    
    See https://github.com/plougher/squashfs-tools/pull/83

--- packages/sysutils/squashfs/package.mk
+++ packages/sysutils/squashfs/package.mk
@@ -3,11 +3,10 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="squashfs"
-PKG_VERSION="e38956b"
-PKG_SHA256="d49241e238076ee56920c6aec31f0de7b41fe770d1b2c03d1714bbffb833a98f"
+PKG_VERSION="c570c61"
 PKG_LICENSE="GPL"
-PKG_SITE="https://git.kernel.org/pub/scm/fs/squashfs/squashfs-tools.git"
-PKG_URL="https://git.kernel.org/pub/scm/fs/squashfs/squashfs-tools.git/snapshot/$PKG_VERSION.tar.gz"
+PKG_SITE="https://github.com/plougher/squashfs-tools"
+PKG_URL="$PKG_SITE.git"
 PKG_DEPENDS_HOST="ccache:host zlib:host lzo:host xz:host zstd:host"
 PKG_NEED_UNPACK="$(get_pkg_directory zlib) $(get_pkg_directory lzo) $(get_pkg_directory xz) $(get_pkg_directory zstd)"
 PKG_LONGDESC="A compressed read-only filesystem for Linux."
