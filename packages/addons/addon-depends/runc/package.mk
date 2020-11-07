9bad11600e3fb4e0998f67e5f93bfafbccc40881
heitbaum <6086324+heitbaum@users.noreply.github.com>
2020-11-07 22:27:20 +0000

    runc: Update to runc 1.0-rc92

--- packages/addons/addon-depends/runc/package.mk
+++ packages/addons/addon-depends/runc/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="runc"
-PKG_VERSION="1.0.0-rc8"
-PKG_SHA256="efe4ff9bbe49b19074346d65c914d809c0a3e90d062ea9619fe240f931f0b700"
+PKG_VERSION="1.0.0-rc92"
+PKG_SHA256="28378df983a3c586ed3ec8c76a774a9b10f36a0c323590a284b801cce95cc61f"
 PKG_LICENSE="APL"
 PKG_SITE="https://github.com/opencontainers/runc"
 PKG_URL="https://github.com/opencontainers/runc/archive/v${PKG_VERSION}.tar.gz"
@@ -13,7 +13,7 @@ PKG_LONGDESC="A CLI tool for spawning and running containers according to the OC
 PKG_TOOLCHAIN="manual"
 
 # Git commit of the matching release https://github.com/opencontainers/runc/releases
-PKG_GIT_COMMIT="425e105d5a03fabd737a126ad93d62a9eeede87f"
+PKG_GIT_COMMIT="ff819c7e9184c13b7c2607fe6c30ae19403a7aff"
 
 pre_make_target() {
   go_configure
