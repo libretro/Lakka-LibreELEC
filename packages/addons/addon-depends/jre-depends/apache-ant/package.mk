79c983d09c74a438d4464cfc78a0306518d7bfc9
Portisch <hugo.portisch@yahoo.de>
2020-11-09 11:43:57 +0100

    apache-ant: bump package to 1.10.9

--- packages/addons/addon-depends/jre-depends/apache-ant/package.mk
+++ packages/addons/addon-depends/jre-depends/apache-ant/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="apache-ant"
-PKG_VERSION="1.10.8"
-PKG_SHA256="8be685aacf2bfe8515a1249fbebb0ccd861dfe05ee2c027c89fd7912c1ce2c2a"
+PKG_VERSION="1.10.9"
+PKG_SHA256="42f12ea7dc854b69aac0bd05c5767252dddd297888503e69b8aae0a1dc791cf5"
 PKG_LICENSE="Apache License 2.0"
 PKG_SITE="https://ant.apache.org/"
 PKG_URL="https://downloads.apache.org/ant/binaries/${PKG_NAME}-${PKG_VERSION}-bin.tar.xz"
