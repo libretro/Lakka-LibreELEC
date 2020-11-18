31ef8f9723fda749d5e887abdcd3df7a960792cb
Ntemis <ierokipides.dem@gmail.com>
2020-11-18 20:05:54 +0200

    Update genesis-plus-gx to possibly fix a sega-cd issue

--- packages/libretro/genesis-plus-gx/package.mk
+++ packages/libretro/genesis-plus-gx/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="genesis-plus-gx"
-PKG_VERSION="f8a8046"
+PKG_VERSION="8c43029"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Non-commercial"
