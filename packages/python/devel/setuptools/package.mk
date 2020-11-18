444f1ca4db8aeec4cdd26f374413116c5258454b
heitbaum <6086324+heitbaum@users.noreply.github.com>
2020-11-18 11:49:04 +0000

    setuptools: update to 51.0.0

--- packages/python/devel/setuptools/package.mk
+++ packages/python/devel/setuptools/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="setuptools"
-PKG_VERSION="45.2.0"
-PKG_SHA256="1cee9d515859e83a409056189bb9f512881bf0fb3334609beed0cf8d384948fc"
+PKG_VERSION="51.0.0"
+PKG_SHA256="64aab833ffcdd05e386e3f18f5a430082b14a4b74522724907f6a9a43ba6ec5e"
 PKG_LICENSE="OSS"
 PKG_SITE="https://pypi.org/project/setuptools"
 PKG_URL="https://github.com/pypa/setuptools/archive/v$PKG_VERSION.tar.gz"
