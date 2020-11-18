d0d13868fd3803f42605de3461659dba9432073e
CvH <namerp@googlemail.com>
2020-11-18 23:54:49 +0100

    libwebsockets: build with libuv

--- packages/addons/addon-depends/ttyd-depends/libwebsockets/package.mk
+++ packages/addons/addon-depends/ttyd-depends/libwebsockets/package.mk
@@ -7,7 +7,8 @@ PKG_SHA256="531e8f54fb9df64e790a3a62ace103dfbd67d2e3994745623422f89fbb7abcaf"
 PKG_LICENSE="LGPL2+"
 PKG_SITE="https://libwebsockets.org"
 PKG_URL="https://github.com/warmcat/libwebsockets/archive/v$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain openssl json-c"
+PKG_DEPENDS_TARGET="toolchain openssl json-c libuv"
 PKG_LONGDESC="Library for implementing network protocols with a tiny footprint."
 
-PKG_CMAKE_OPTS_TARGET="-DLWS_WITHOUT_TESTAPPS=ON"
+PKG_CMAKE_OPTS_TARGET="-DLWS_WITH_LIBUV=ON \
+                       -DLWS_WITHOUT_TESTAPPS=ON"
