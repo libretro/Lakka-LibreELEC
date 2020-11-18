e283d5384aecff64a5a15677b2feeef542db977d
heitbaum <6086324+heitbaum@users.noreply.github.com>
2020-11-18 11:48:34 +0000

    Mako: update to 1.1.3

--- packages/python/devel/Mako/package.mk
+++ packages/python/devel/Mako/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="Mako"
-PKG_VERSION="1.1.1"
-PKG_SHA256="2984a6733e1d472796ceef37ad48c26f4a984bb18119bb2dbc37a44d8f6e75a4"
+PKG_VERSION="1.1.3"
+PKG_SHA256="8195c8c1400ceb53496064314c6736719c6f25e7479cd24c77be3d9361cddc27"
 PKG_LICENSE="GPL"
 PKG_SITE="https://pypi.org/project/Mako"
 PKG_URL="https://files.pythonhosted.org/packages/source/${PKG_NAME:0:1}/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz"
