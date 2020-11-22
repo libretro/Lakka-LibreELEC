7b577cc6a69f9c1b31ac58cfbb4a8b2e6b3f0757
CvH <namerp@googlemail.com>
2020-11-22 23:02:22 +0100

    chrome-libxkbcommon: initial package

---
+++ packages/addons/addon-depends/chrome-depends/chrome-libxkbcommon/package.mk
@@ -0,0 +1,19 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
+
+. $(get_pkg_directory libxkbcommon)/package.mk
+
+PKG_NAME="chrome-libxkbcommon"
+PKG_LONGDESC="libxkbcommon for chrome"
+PKG_URL=""
+PKG_DEPENDS_UNPACK+=" libxkbcommon"
+PKG_BUILD_FLAGS="-sysroot"
+
+PKG_MESON_OPTS_TARGET="$PKG_MESON_OPTS_TARGET \
+                           -Denable-static=false \
+                           -Denable-shared=true"
+
+unpack() {
+  mkdir -p $PKG_BUILD
+  tar --strip-components=1 -xf $SOURCES/${PKG_NAME:7}/${PKG_NAME:7}-$PKG_VERSION.tar.xz -C $PKG_BUILD
+}
