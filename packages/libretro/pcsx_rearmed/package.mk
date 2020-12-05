4c5d3c93b61cea18ce4ef8cb016f83ddf1a87b8d
Seth Barberee <seth.barberee@gmail.com>
2020-12-05 17:01:30 -0600

    disable threaded renderer on arm

--- packages/libretro/pcsx_rearmed/package.mk
+++ packages/libretro/pcsx_rearmed/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="pcsx_rearmed"
-PKG_VERSION="d56340b"
+PKG_VERSION="4bc5e35"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
