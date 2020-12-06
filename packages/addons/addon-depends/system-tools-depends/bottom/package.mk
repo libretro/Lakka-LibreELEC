ed9b628edaf78166029eac3c472b491bd0635c15
heitbaum <rudi@heitbaum.com>
2020-12-06 01:58:22 +0000

    bottom: initial commit

---
+++ packages/addons/addon-depends/system-tools-depends/bottom/package.mk
@@ -0,0 +1,25 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="bottom"
+PKG_VERSION="0.6.1"
+PKG_SHA256="aa9c26e7e8e1300e9827c098317efa28cc28ae8acabfdb12f14e4ba65f42a57c"
+PKG_LICENSE="MIT"
+PKG_SITE="https://github.com/ClementTsang/bottom"
+PKG_URL="https://github.com/ClementTsang/bottom/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain rust"
+PKG_LONGDESC="A TUI system monitor written in Rust."
+PKG_TOOLCHAIN="manual"
+
+make_target() {
+  . "$(get_build_dir rust)/cargo/env"
+  cargo build \
+    --release \
+    --locked \
+    --all-features
+}
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}
+  cp ${PKG_BUILD}/.${TARGET_NAME}/*/release/btm ${INSTALL}
+}
