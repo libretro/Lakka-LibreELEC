c7aa301e5cbcbcf329e6b41a59a90bfe230c9109
Portisch <hugo.portisch@yahoo.de>
2020-12-11 07:27:37 +0100

    rsync: explicit enable and add missing dependency openssl for target

--- packages/network/rsync/package.mk
+++ packages/network/rsync/package.mk
@@ -8,18 +8,20 @@ PKG_LICENSE="GPLv3"
 PKG_SITE="http://www.samba.org/ftp/rsync/rsync.html"
 PKG_URL="https://download.samba.org/pub/rsync/src/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_HOST="autotools:host zlib:host zstd:host"
-PKG_DEPENDS_TARGET="toolchain zlib"
+PKG_DEPENDS_TARGET="toolchain zlib openssl"
 PKG_LONGDESC="A very fast method for bringing remote files into sync."
 PKG_BUILD_FLAGS="-sysroot"
 
 PKG_CONFIGURE_OPTS_HOST="--with-included-popt \
                          --without-included-zlib \
+                         --disable-openssl \
                          --disable-lz4 \
                          --enable-zstd \
                          --disable-xxhash"
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-acl-support \
                            --disable-asm \
+                           --enable-openssl \
                            --disable-lz4 \
                            --disable-md2man \
                            --disable-simd \
