a7b57d43bfc89bc1c551f69a790db48cc0ca793f
heitbaum <rudi@heitbaum.com>
2020-12-13 06:55:46 +0000

    minizip: updated to 2.10.4 and fixed failed compile

--- packages/compress/minizip/package.mk
+++ packages/compress/minizip/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="minizip"
-PKG_VERSION="2.9.2"
-PKG_SHA256="8425399277d9d5e39454e655cfd3eb004607960c8358a3e732f5e741a6b5df0a"
+PKG_VERSION="2.10.4"
+PKG_SHA256="6ef3d2e0c15352fe87a4a658b2e8f665fb0c21ddfb57a2e0a515658389d2e850"
 PKG_LICENSE="zlib"
 PKG_SITE="https://github.com/nmoinvaz/minizip"
 PKG_URL="https://github.com/nmoinvaz/minizip/archive/$PKG_VERSION.tar.gz"
@@ -11,9 +11,4 @@ PKG_DEPENDS_TARGET="zlib"
 PKG_LONGDESC="Minizip zlib contribution fork with latest bug fixes"
 
 PKG_CMAKE_OPTS_TARGET="-DUSE_AES=OFF \
-                       -DBUILD_TEST=ON"
-
-makeinstall_target() {
-  cp -v miniunz_exec $SYSROOT_PREFIX/usr/bin/miniunz
-  cp -v minizip_exec $SYSROOT_PREFIX/usr/bin/minizip
-}
+                       -DMZ_BUILD_TEST=ON"
