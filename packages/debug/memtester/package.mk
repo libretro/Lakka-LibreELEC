4f32ec3ba803a7aa71213c732494cf5eb62cfcce
heitbaum <rudi@heitbaum.com>
2020-12-13 06:09:14 +0000

    memtester: update to 4.5.0

--- packages/debug/memtester/package.mk
+++ packages/debug/memtester/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="memtester"
-PKG_VERSION="4.3.0"
-PKG_SHA256="f9dfe2fd737c38fad6535bbab327da9a21f7ce4ea6f18c7b3339adef6bf5fd88"
+PKG_VERSION="4.5.0"
+PKG_SHA256="8ed52b0d06d4aeb61954994146e2a5b2d20448a8f3ce3ee995120e6dbde2ae37"
 PKG_LICENSE="GPL"
 PKG_SITE="http://pyropus.ca/software/memtester/"
 PKG_URL="http://pyropus.ca/software/memtester/old-versions/memtester-${PKG_VERSION}.tar.gz"
