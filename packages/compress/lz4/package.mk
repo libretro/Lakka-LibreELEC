ff061734ebe3a6b993e240feacaa723a75e6c8c6
heitbaum <rudi@heitbaum.com>
2020-12-13 06:55:07 +0000

    lz4: updated to 1.9.3

--- packages/compress/lz4/package.mk
+++ packages/compress/lz4/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="lz4"
-PKG_VERSION="1.9.2"
-PKG_SHA256="658ba6191fa44c92280d4aa2c271b0f4fbc0e34d249578dd05e50e76d0e5efcc"
+PKG_VERSION="1.9.3"
+PKG_SHA256="030644df4611007ff7dc962d981f390361e6c97a34e5cbc393ddfbe019ffe2c1"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/lz4/lz4"
 PKG_URL="https://github.com/lz4/lz4/archive/v$PKG_VERSION.tar.gz"
