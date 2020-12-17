f4bed0f54adba71d4425c9a74340eb4c4b23277d
Ntemis <ierokipides.dem@gmail.com>
2020-12-17 05:54:25 +0200

    Add rsync package

---
+++ packages/network/rsync/package.mk
@@ -0,0 +1,31 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="rsync"
+PKG_VERSION="3.2.3"
+PKG_SHA256="becc3c504ceea499f4167a260040ccf4d9f2ef9499ad5683c179a697146ce50e"
+PKG_LICENSE="GPLv3"
+PKG_SITE="http://www.samba.org/ftp/rsync/rsync.html"
+PKG_URL="https://download.samba.org/pub/rsync/src/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_HOST="autotools:host zlib:host zstd:host"
+PKG_DEPENDS_TARGET="toolchain zlib openssl"
+PKG_LONGDESC="A very fast method for bringing remote files into sync."
+PKG_BUILD_FLAGS="-sysroot"
+
+PKG_CONFIGURE_OPTS_HOST="--with-included-popt \
+                         --without-included-zlib \
+                         --disable-lz4 \
+                         --enable-zstd \
+                         --disable-xxhash"
+
+PKG_CONFIGURE_OPTS_TARGET="--disable-acl-support \
+                           --disable-asm \
+                           --disable-lz4 \
+                           --disable-md2man \
+                           --disable-simd \
+                           --disable-xattr-support \
+                           --disable-xxhash \
+                           --disable-zstd \
+                           --with-included-popt \
+                           --without-included-zlib"
+
