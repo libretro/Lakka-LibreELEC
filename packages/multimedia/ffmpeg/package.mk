e2ca4f5e9d4ff765e81c025df168bc4216570be9
Matthias Reichl <hias@horus.com>
2020-12-19 10:40:44 +0100

    ffmpeg: update rpi patch
    Patch created using revisions 922f5ee..fbcd774
    from branch dev/4.3.1/drm_prime_1 of https://github.com/jc-kynesim/rpi-ffmpeg

--- packages/multimedia/ffmpeg/package.mk
+++ packages/multimedia/ffmpeg/package.mk
@@ -20,7 +20,7 @@ PKG_FFMPEG_HWACCEL="--enable-hwaccels"
 PKG_FFMPEG_RPI="--disable-mmal"
 
 if [ "${PROJECT}" = "RPi" -a "${DEVICE}" = "RPi4" ]; then
-  PKG_PATCH_DIRS="rpi v4l2-drmprime"
+  PKG_PATCH_DIRS="rpi"
   PKG_FFMPEG_RPI+=" --disable-rpi --enable-sand"
 else
   PKG_PATCH_DIRS="v4l2-request v4l2-drmprime"
