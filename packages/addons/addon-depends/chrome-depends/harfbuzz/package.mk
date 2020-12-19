886ee669d20c45453282ae30f6bc99df77407a17
heitbaum <rudi@heitbaum.com>
2020-12-19 14:33:34 +0000

    harfbuzz: updated to 2.7.4 and meson build

--- packages/addons/addon-depends/chrome-depends/harfbuzz/package.mk
+++ packages/addons/addon-depends/chrome-depends/harfbuzz/package.mk
@@ -3,20 +3,21 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="harfbuzz"
-PKG_VERSION="1.8.1"
-PKG_SHA256="fbed6392ddb085e45e6090a9f389f72926d0e355f4b0a2ef51d35cf21686df45"
+PKG_VERSION="2.7.4"
+PKG_SHA256="6ad11d653347bd25d8317589df4e431a2de372c0cf9be3543368e07ec23bb8e7"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.freedesktop.org/wiki/Software/HarfBuzz"
-PKG_URL="https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-$PKG_VERSION.tar.bz2"
+PKG_URL="https://github.com/harfbuzz/harfbuzz/releases/download/$PKG_VERSION/harfbuzz-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain cairo freetype glib icu"
 PKG_LONGDESC="HarfBuzz is an OpenType text shaping engine."
-PKG_TOOLCHAIN="configure"
+PKG_TOOLCHAIN="meson"
+PKG_DEPENDS_CONFIG="icu"
 
-PKG_CONFIGURE_OPTS_TARGET="--with-icu \
-                           --disable-gtk-doc \
-                           --disable-gtk-doc-html \
-                           --disable-gtk-doc-pdf"
-
-pre_configure_target() {
-  export LDFLAGS="$LDFLAGS -ldl"
-}
+PKG_MESON_OPTS_TARGET="-Dcairo=enabled \
+                       -Ddocs=disabled \
+                       -Dfontconfig=enabled \
+                       -Dfreetype=enabled \
+                       -Dglib=enabled \
+                       -Dgobject=disabled \
+                       -Dgraphite=disabled \
+                       -Dicu=enabled"
