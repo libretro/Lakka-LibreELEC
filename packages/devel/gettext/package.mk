2294589cb341877abf2b42ef9fe1aab0d4ec2ff2
heitbaum <rudi@heitbaum.com>
2020-12-25 08:16:29 +0000

    gettext: update to 0.21

--- packages/devel/gettext/package.mk
+++ packages/devel/gettext/package.mk
@@ -3,12 +3,13 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="gettext"
-PKG_VERSION="0.20.2"
-PKG_SHA256="ecb9d0908ca41d5ca5fef974323b3bba6bec19eebba0b44f396de98cfcc089f1"
+PKG_VERSION="0.21"
+PKG_SHA256="d20fcbb537e02dcf1383197ba05bd0734ef7bf5db06bdb241eb69b7d16b73192"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/s/gettext/"
-PKG_URL="http://ftp.gnu.org/pub/gnu/gettext/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_URL="http://ftp.gnu.org/pub/gnu/gettext/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_HOST="ccache:host"
+PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A program internationalization library and tools."
 
 PKG_CONFIGURE_OPTS_HOST="--disable-static --enable-shared \
