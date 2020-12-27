1150ed2d24356b78775c62344467b1faa0a8b145
heitbaum <rudi@heitbaum.com>
2020-12-27 09:05:30 +0000

    oniguruma: update to 6.9.6

--- packages/addons/addon-depends/system-tools-depends/depends/oniguruma/package.mk
+++ packages/addons/addon-depends/system-tools-depends/depends/oniguruma/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="oniguruma"
-PKG_VERSION="6.9.1"
-PKG_SHA256="c7c3feb7be45a5cc9f2dec239b4a317a422e6ffea299cf91ffab1b926633ea12"
+PKG_VERSION="6.9.6"
+PKG_SHA256="bd0faeb887f748193282848d01ec2dad8943b5dfcb8dc03ed52dcc963549e819"
 PKG_LICENSE="BSD"
 PKG_SITE="https://github.com/kkos/oniguruma"
 PKG_URL="https://github.com/kkos/oniguruma/releases/download/v$PKG_VERSION/onig-$PKG_VERSION.tar.gz"
