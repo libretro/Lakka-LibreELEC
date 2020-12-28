683d0303a09d0df28b310a8696476344bd7bff33
heitbaum <rudi@heitbaum.com>
2020-12-28 09:44:50 +0000

    libgpg-error: update to 1.41

--- packages/security/libgpg-error/package.mk
+++ packages/security/libgpg-error/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libgpg-error"
-PKG_VERSION="1.39"
-PKG_SHA256="4a836edcae592094ef1c5a4834908f44986ab2b82e0824a0344b49df8cdb298f"
+PKG_VERSION="1.41"
+PKG_SHA256="64b078b45ac3c3003d7e352a5e05318880a5778c42331ce1ef33d1a0d9922742"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://www.gnupg.org"
 PKG_URL="https://www.gnupg.org/ftp/gcrypt/libgpg-error/$PKG_NAME-$PKG_VERSION.tar.bz2"
