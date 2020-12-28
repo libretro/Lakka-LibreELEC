e4ec478aefaa08e3462117f9a5e008e657ca9ce3
heitbaum <rudi@heitbaum.com>
2020-12-28 04:27:32 +0000

    at-spi2-core: update to 2.38.0

--- packages/addons/addon-depends/chrome-depends/at-spi2-core/package.mk
+++ packages/addons/addon-depends/chrome-depends/at-spi2-core/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="at-spi2-core"
-PKG_VERSION="2.36.0"
-PKG_SHA256="88da57de0a7e3c60bc341a974a80fdba091612db3547c410d6deab039ca5c05a"
+PKG_VERSION="2.38.0"
+PKG_SHA256="84e36c3fe66862133f5fe229772b76aa2526e10de5014a3778f2fa46ce550da5"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.gnome.org/"
 PKG_URL="https://ftp.gnome.org/pub/gnome/sources/at-spi2-core/${PKG_VERSION:0:4}/at-spi2-core-$PKG_VERSION.tar.xz"
