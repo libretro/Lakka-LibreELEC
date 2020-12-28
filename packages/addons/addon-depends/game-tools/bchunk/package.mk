3d8f6a13b74a0edd82077c83cedbd9e28c8e30a8
heitbaum <rudi@heitbaum.com>
2020-12-28 04:22:08 +0000

    bchunk: update to 1.2.2

--- packages/addons/addon-depends/game-tools/bchunk/package.mk
+++ packages/addons/addon-depends/game-tools/bchunk/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="bchunk"
-PKG_VERSION="1.2.0"
-PKG_SHA256="afdc9d5e38bdd16f0b8b9d9d382b0faee0b1e0494446d686a08b256446f78b5d"
+PKG_VERSION="1.2.2"
+PKG_SHA256="e7d99b5b60ff0b94c540379f6396a670210400124544fb1af985dd3551eabd89"
 PKG_LICENSE="GPL"
 PKG_SITE="http://he.fi/bchunk/"
 PKG_URL="http://he.fi/bchunk/$PKG_NAME-$PKG_VERSION.tar.gz"
