128225d376859e4e8062ef80b34b1232b0163982
heitbaum <rudi@heitbaum.com>
2020-12-29 04:32:13 +0000

    nlohmann-json: update to 3.9.1

--- packages/textproc/nlohmann-json/package.mk
+++ packages/textproc/nlohmann-json/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="nlohmann-json"
-PKG_VERSION="3.7.3"
-PKG_SHA256="249548f4867417d66ae46b338dfe0a2805f3323e81c9e9b83c89f3adbfde6f31"
+PKG_VERSION="3.9.1"
+PKG_SHA256="4cf0df69731494668bdd6460ed8cb269b68de9c19ad8c27abc24cd72605b2d5b"
 PKG_LICENSE="MIT"
 PKG_SITE="https://nlohmann.github.io/json/"
 PKG_URL="https://github.com/nlohmann/json/archive/v$PKG_VERSION.tar.gz"
