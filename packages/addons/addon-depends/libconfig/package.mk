100e97f2a0bfafbe2439bb0772c6f9649cf62122
heitbaum <rudi@heitbaum.com>
2020-12-30 11:38:20 +0000

    libconfig: update to 1.7.2 and PKG_SITE

--- packages/addons/addon-depends/libconfig/package.mk
+++ packages/addons/addon-depends/libconfig/package.mk
@@ -2,10 +2,10 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libconfig"
-PKG_VERSION="1.5"
-PKG_SHA256="cae5c02361d8a9b2bb26946c64f089d2e5e599972f386203fbc48975c0d885c8"
+PKG_VERSION="1.7.2"
+PKG_SHA256="f67ac44099916ae260a6c9e290a90809e7d782d96cdd462cac656ebc5b685726"
 PKG_LICENSE="LGPL"
-PKG_SITE="http://hyperrealm.com/libconfig/libconfig.html"
+PKG_SITE="https://github.com/hyperrealm/libconfig"
 PKG_URL="https://github.com/hyperrealm/libconfig/archive/v$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A C/C++ configuration file library."
