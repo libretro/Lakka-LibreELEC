5a19e5403de099221685101a9d0f46ac71bd596b
heitbaum <rudi@heitbaum.com>
2020-12-30 12:08:00 +0000

    pugixml: update to 1.11.4

--- packages/textproc/pugixml/package.mk
+++ packages/textproc/pugixml/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pugixml"
-PKG_VERSION="1.10"
-PKG_SHA256="10f1f0a32b559ca8435d95855928d990cfbb9796e339efb638080c897728174c"
+PKG_VERSION="1.11.4"
+PKG_SHA256="017139251c122dbff400a507cddc4cb74120a431a50c6c524f30edcc5b331ade"
 PKG_LICENSE="MIT"
 PKG_SITE="https://pugixml.org/"
 PKG_URL="https://github.com/zeux/pugixml/archive/v${PKG_VERSION}.tar.gz"
