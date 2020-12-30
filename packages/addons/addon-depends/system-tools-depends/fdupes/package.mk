72a3dcebaec91ad58d376320d407935659b49ba2
heitbaum <rudi@heitbaum.com>
2020-12-30 03:45:16 +0000

    fdupes: update to 2.1.2

--- packages/addons/addon-depends/system-tools-depends/fdupes/package.mk
+++ packages/addons/addon-depends/system-tools-depends/fdupes/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="fdupes"
-PKG_VERSION="2.0.0"
-PKG_SHA256="eb9e3bd3e722ebb2a272e45a1073f78c60f8989b151c3661421b86b14b203410"
+PKG_VERSION="2.1.2"
+PKG_SHA256="cd5cb53b6d898cf20f19b57b81114a5b263cc1149cd0da3104578b083b2837bd"
 PKG_LICENSE="GPL"
 PKG_SITE="http://premium.caribe.net/~adrian2/fdupes.html"
-PKG_URL="https://github.com/adrianlopezroche/fdupes/releases/download/${PKG_VERSION}/fdupes-${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/adrianlopezroche/fdupes/releases/download/v${PKG_VERSION}/fdupes-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain ncurses"
 PKG_LONGDESC="A program for identifying or deleting duplicate files residing within specified directories."
 PKG_BUILD_FLAGS="-sysroot"
