8e704399336d4bb1e5014b3cdd954520ee148b9f
CvH <namerp@googlemail.com>
2020-12-30 17:32:23 +0100

    json-c: version cleanup

--- packages/addons/addon-depends/ttyd-depends/json-c/package.mk
+++ packages/addons/addon-depends/ttyd-depends/json-c/package.mk
@@ -4,10 +4,10 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="json-c"
-PKG_VERSION="9021cdcdd01fc9dbcbe1f06391848c2ac915212f"
-PKG_SHA256="a102249b3f7f11c526d1af6e428f351398620ec3d679458b9ad3cdcfb14ceaaf"
+PKG_VERSION="0.15"
+PKG_SHA256="74985882e39467b34722e584ab836ed2abd47061888f318125fd4b167002afd5"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/json-c/json-c"
-PKG_URL="https://github.com/json-c/json-c/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/json-c/json-c/archive/json-c-${PKG_VERSION%-*}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Implements a reference counting object model that allows you to easily construct JSON objects in C."
