70b896c259e3b55fa8965349649a5e3e91b25b9a
heitbaum <rudi@heitbaum.com>
2020-12-30 04:31:40 +0000

    htop: update to 3.0.4

--- packages/addons/addon-depends/system-tools-depends/htop/package.mk
+++ packages/addons/addon-depends/system-tools-depends/htop/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="htop"
-PKG_VERSION="3.0.0beta5"
-PKG_SHA256="c439add8d6a463699629fc3f0103f55b045d519c31611fa4a68629063238985a"
+PKG_VERSION="3.0.4"
+PKG_SHA256="d8a0536ce95e3d59f8e292e73ee037033a74a8cc118fd10d22048bd4aeb61324"
 PKG_LICENSE="GPL"
 PKG_SITE="https://hisham.hm/htop"
-PKG_URL="https://github.com/hishamhm/htop/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/htop-dev/htop/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain ncurses"
 PKG_LONGDESC="An interactive process viewer for Unix."
 PKG_TOOLCHAIN="autotools"
