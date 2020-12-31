37d8b1dbe0c50d457ae068eb1f58ce35b4cfc882
heitbaum <rudi@heitbaum.com>
2020-12-31 13:18:20 +0000

    harfbuzz: rename harfbuzz to harfbuzz-icu

--- packages/addons/addon-depends/chrome-depends/harfbuzz-icu/package.mk
+++ packages/addons/addon-depends/chrome-depends/harfbuzz-icu/package.mk
@@ -2,16 +2,15 @@
 # Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
-PKG_NAME="harfbuzz"
-PKG_VERSION="2.7.4"
-PKG_SHA256="6ad11d653347bd25d8317589df4e431a2de372c0cf9be3543368e07ec23bb8e7"
-PKG_LICENSE="GPL"
-PKG_SITE="http://www.freedesktop.org/wiki/Software/HarfBuzz"
-PKG_URL="https://github.com/harfbuzz/harfbuzz/releases/download/$PKG_VERSION/harfbuzz-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain cairo freetype glib icu"
-PKG_LONGDESC="HarfBuzz is an OpenType text shaping engine."
-PKG_TOOLCHAIN="meson"
+. $(get_pkg_directory harfbuzz)/package.mk
+
+PKG_NAME="harfbuzz-icu"
+PKG_URL=""
+PKG_DEPENDS_TARGET+=" icu"
+PKG_LONGDESC="HarfBuzz with icu"
 PKG_DEPENDS_CONFIG="icu"
+PKG_DEPENDS_UNPACK+=" harfbuzz"
+PKG_BUILD_FLAGS="-sysroot"
 
 PKG_MESON_OPTS_TARGET="-Dcairo=enabled \
                        -Ddocs=disabled \
@@ -21,3 +20,8 @@ PKG_MESON_OPTS_TARGET="-Dcairo=enabled \
                        -Dgobject=disabled \
                        -Dgraphite=disabled \
                        -Dicu=enabled"
+
+unpack() {
+  mkdir -p $PKG_BUILD
+  tar --strip-components=1 -xf $SOURCES/${PKG_NAME:0:8}/${PKG_NAME:0:8}-$PKG_VERSION.tar.xz -C $PKG_BUILD
+}
