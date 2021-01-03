2e82bda1a6dad87c5c53addc869562c1517c6a4a
heitbaum <rudi@heitbaum.com>
2021-01-03 11:39:07 +0000

    configtools: added timestamps to PKG_VERSION

--- packages/devel/configtools/package.mk
+++ packages/devel/configtools/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="configtools"
-PKG_VERSION="5256817ace8493502ec88501a19e4051c2e220b0"
+PKG_VERSION="5256817ace8493502ec88501a19e4051c2e220b0" # 2020-01-01
 PKG_SHA256="3856ff9a2a9382a3396549c047c28e0f05b7e4822239ffe91ce2e59b0a0284db"
 PKG_LICENSE="GPL"
 PKG_SITE="http://git.savannah.gnu.org/cgit/config.git"
