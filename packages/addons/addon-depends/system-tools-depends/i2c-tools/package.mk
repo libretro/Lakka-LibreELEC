4e4309350c39316068b523ff178f248aded3e7d0
heitbaum <rudi@heitbaum.com>
2021-01-08 09:38:56 +0000

    i2c-tools: update to 4.2
    update the build variables to use AR not TARGET_AR,
    TARGET_AR is not exposed to PKG_TOOLCHAIN=make
    
    update from 3.1.2 (2015-06-17) to 4.2 (2020-09-22)
    changelog:
    https://git.kernel.org/pub/scm/utils/i2c-tools/i2c-tools.git/tree/CHANGES

--- packages/addons/addon-depends/system-tools-depends/i2c-tools/package.mk
+++ packages/addons/addon-depends/system-tools-depends/i2c-tools/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="i2c-tools"
-PKG_VERSION="3.1.2"
-PKG_SHA256="6d6079153cd49a62d4addacef4c092db1a46ba60b2807070a3fbe050262aef87"
+PKG_VERSION="4.2"
+PKG_SHA256="37f2dabc7082d185903ff21d1f584b5dcb4dd2eb2c879bbd8d7c50ae900dacd6"
 PKG_LICENSE="GPL"
 PKG_SITE="https://i2c.wiki.kernel.org/index.php/I2C_Tools"
 PKG_URL="https://www.kernel.org/pub/software/utils/i2c-tools/$PKG_NAME-$PKG_VERSION.tar.xz"
@@ -19,7 +19,7 @@ pre_make_target() {
 make_target() {
   make  EXTRA="py-smbus" \
         CC="$CC" \
-        AR="$TARGET_AR" \
+        AR="$AR" \
         CFLAGS="$TARGET_CFLAGS" \
         CPPFLAGS="$TARGET_CPPFLAGS -I${SYSROOT_PREFIX}/usr/include/$PKG_PYTHON_VERSION" \
         PYTHON=${TOOLCHAIN}/bin/python3
