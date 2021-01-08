d992d90d7707f524cb64bd02e4f2d94f4bfe993a
heitbaum <rudi@heitbaum.com>
2021-01-08 10:03:37 +0000

    lm_sensors: update to 3.6.0
    update from guthash for 3.5.0 (2018-11-23) to 3.6.0 (2019-10-18)
    changelog: https://github.com/lm-sensors/lm-sensors/blob/master/CHANGES

--- packages/addons/addon-depends/system-tools-depends/lm_sensors/package.mk
+++ packages/addons/addon-depends/system-tools-depends/lm_sensors/package.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="lm_sensors"
-PKG_VERSION="e8afbda10fba571c816abddcb5c8180afc435bba"
-PKG_SHA256="255b9a9b30c7969b3df0460392a807239c18b15baac1ff33ff5fef3b1cc1169d"
+PKG_VERSION="3.6.0"
+PKG_SHA256="0591f9fa0339f0d15e75326d0365871c2d4e2ed8aa1ff759b3a55d3734b7d197"
 PKG_ARCH="arm x86_64"
 PKG_LICENSE="GPL"
-PKG_SITE="http://secure.netroedge.com/~lm78/"
-PKG_URL="https://github.com/groeck/lm-sensors/archive/${PKG_VERSION}.tar.gz"
+PKG_SITE="https://hwmon.wiki.kernel.org"
+PKG_URL="https://github.com/groeck/lm-sensors/archive/V${PKG_VERSION//./-}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Provides user-space support for the hardware monitoring drivers."
 PKG_BUILD_FLAGS="-sysroot"
