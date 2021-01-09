f55a328f63ca7c52afd88f3917b3e91e1eab7083
heitbaum <rudi@heitbaum.com>
2021-01-09 07:38:20 +0000

    efivar: update to 2020-10-16
    update from 272b216 (20 Jun 2019) to cff88dd (16 Oct 2020)
    changelog: https://github.com/rhboot/efivar/commits/master

--- packages/addons/addon-depends/system-tools-depends/depends/efivar/package.mk
+++ packages/addons/addon-depends/system-tools-depends/depends/efivar/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="efivar"
-PKG_VERSION="272b216e197b2b3d05da68ef51861545a36dc6d8"
-PKG_SHA256="ab3b517c7cfc5bafc45a9b30c859d552b77c0b7c35883afd1c734e13013ffb39"
+PKG_VERSION="cff88dd96b9d43e2c5875a24ba6180b196890ded" # 16 Oct 2020
+PKG_SHA256="016dfae596b691c8d38e488f8bfac3ba437befc260a6f32e60dd390595c9f3e9"
 PKG_ARCH="x86_64"
 PKG_LICENSE="LGPL"
 PKG_SITE="https://github.com/rhboot/efivar"
@@ -12,12 +12,17 @@ PKG_DEPENDS_HOST="toolchain:host"
 PKG_DEPENDS_TARGET="toolchain efivar:host"
 PKG_LONGDESC="Tools and library to manipulate EFI variables."
 
+pre_make_host() {
+  export TOPDIR=$PKG_BUILD
+}
+
 make_host() {
   make -C src/ include/efivar/efivar-guids.h
 }
 
 pre_make_target() {
-  sed -e 's/-Werror//' -i gcc.specs
+  sed -e 's/-Werror//' -i src/include/gcc.specs
+  export TOPDIR=$PKG_BUILD
 }
 
 make_target() {
