1facad16f580719a654cb2b6af4d4d901e6ab294
heitbaum <rudi@heitbaum.com>
2021-01-10 03:02:58 +0000

    x265: fix download link

--- packages/addons/addon-depends/ffmpegx-depends/x265/package.mk
+++ packages/addons/addon-depends/ffmpegx-depends/x265/package.mk
@@ -3,11 +3,11 @@
 
 PKG_NAME="x265"
 PKG_VERSION="3.4"
-PKG_SHA256="c2047f23a6b729e5c70280d23223cb61b57bfe4ad4e8f1471eeee2a61d148672"
+PKG_SHA256="1a430f3a793982d4e0762d67dc2d49f308bf28a8bba4f2d42fea3340e33e9e31"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.videolan.org/developers/x265.html"
-PKG_URL="https://bitbucket.org/multicoreware/x265/downloads/x265_${PKG_VERSION}.tar.gz"
+PKG_URL="https://bitbucket.org/multicoreware/x265_git/get/${PKG_VERSION}.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="x265 is a H.265/HEVC video encoder application library"
 PKG_TOOLCHAIN="make"
