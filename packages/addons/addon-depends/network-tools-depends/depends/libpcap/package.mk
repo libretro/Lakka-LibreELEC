d0d64e061632f941b762a39e0b0804e40193024f
heitbaum <rudi@heitbaum.com>
2021-01-13 23:58:55 +0000

    libpcap: use configure, not cmake. review cmake in future release

--- packages/addons/addon-depends/network-tools-depends/depends/libpcap/package.mk
+++ packages/addons/addon-depends/network-tools-depends/depends/libpcap/package.mk
@@ -9,6 +9,8 @@ PKG_SITE="http://www.tcpdump.org/"
 PKG_URL="http://www.tcpdump.org/release/libpcap-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A portable framework for low-level network monitoring."
+# use configure, not cmake. review cmake in future release.
+PKG_TOOLCHAIN="configure"
 
 PKG_CONFIGURE_OPTS_TARGET="LIBS=-lpthread \
                            ac_cv_header_libusb_1_0_libusb_h=no \
