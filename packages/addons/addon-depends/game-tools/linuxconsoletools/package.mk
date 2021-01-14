e5af99b271a18b9871036fd4713bd8d746acbbdd
heitbaum <rudi@heitbaum.com>
2021-01-14 09:26:09 +0000

    linuxconsoletools: update to 1.7.1
    update 1.7.0 to 1.7.1
    changelog:- Version 1.7.1
    -------------
    * inputattach supports the Gunze protocol (thanks to Tillmann Karras).
    * Three missing dependencies were added in the Makefile (thanks to
      Stefanos Chaliasos).
    * An overflow in ffcfstress was fixed (thanks to Bernat Arlandis).

--- packages/addons/addon-depends/game-tools/linuxconsoletools/package.mk
+++ packages/addons/addon-depends/game-tools/linuxconsoletools/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="linuxconsoletools"
-PKG_VERSION="1.7.0"
-PKG_SHA256="95d112f06393806116341d593bda002c8bc44119c1538407623268fed90d8c34"
+PKG_VERSION="1.7.1"
+PKG_SHA256="bd4d4b7e37da02fc67e47ddf20b6f1243c0a7af7b02b918d5e72138ea8727547"
 PKG_LICENSE="GPL"
 PKG_SITE="http://sourceforge.net/projects/linuxconsole/"
 PKG_URL="http://prdownloads.sourceforge.net/linuxconsole/$PKG_NAME-$PKG_VERSION.tar.bz2"
