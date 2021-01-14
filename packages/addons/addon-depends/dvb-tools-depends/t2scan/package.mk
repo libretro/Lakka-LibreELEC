41a8ef24e2f42e08a38c76202217787b7983e98b
heitbaum <rudi@heitbaum.com>
2021-01-14 09:06:41 +0000

    t2scan: update to 0.7

--- packages/addons/addon-depends/dvb-tools-depends/t2scan/package.mk
+++ packages/addons/addon-depends/dvb-tools-depends/t2scan/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="t2scan"
-PKG_VERSION="7d35e17d19baeda627ec9e5fd2144f07e7c5f922"
-PKG_SHA256="20dd68dccd63b18317256b3d58f15031a820380c4cdad4d5f5e81c11daf9e658"
+PKG_VERSION="0.7"
+PKG_SHA256="44e4b738a2beed8eb964be3d90b6da48c2d1c672d81fd8db8bbda87bcc433fcb"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/mighty-p/t2scan"
-PKG_URL="https://github.com/mighty-p/t2scan/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/mighty-p/t2scan/archive/v$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A small channel scan tool which generates DVB-T/T2 channels.conf files."
 PKG_BUILD_FLAGS="-sysroot"
