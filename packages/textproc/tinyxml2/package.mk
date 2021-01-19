b9821a561d65dc1bcaab3e8c20db4e808868cd31
Ian Leonard <antonlacon@gmail.com>
2021-01-19 19:34:17 +0000

    textproc: automated code cleanup
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- packages/textproc/tinyxml2/package.mk
+++ packages/textproc/tinyxml2/package.mk
@@ -7,7 +7,7 @@ PKG_VERSION="8.0.0"
 PKG_SHA256="6ce574fbb46751842d23089485ae73d3db12c1b6639cda7721bf3a7ee862012c"
 PKG_LICENSE="zlib"
 PKG_SITE="http://www.grinninglizard.com/tinyxml2/index.html"
-PKG_URL="https://github.com/leethomason/tinyxml2/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/leethomason/tinyxml2/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="TinyXML2 is a simple, small, C++ XML parser."
 
