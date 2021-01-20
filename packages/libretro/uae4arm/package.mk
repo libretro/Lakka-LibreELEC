c8733ead58cd1ac4219915df61559823f55cd4e4
Chips-fr <darcelf@gmail.com>
2021-01-20 10:06:36 +0100

    Add CD32 support in uae4arm

--- packages/libretro/uae4arm/package.mk
+++ packages/libretro/uae4arm/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="uae4arm"
-PKG_VERSION="920f779"
+PKG_VERSION="f278f62"
 PKG_REV="1"
 PKG_ARCH="arm"
 PKG_LICENSE="GPL"
