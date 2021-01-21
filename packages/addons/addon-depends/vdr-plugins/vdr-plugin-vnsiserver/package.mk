4a5b73c3b4a441fd30201d87c4f453d06ebf7a5b
smp79 <sergey79@gmail.com>
2021-01-21 22:32:54 +0300

    vdr-plugin-vnsiserver: update to 47a90dd

--- packages/addons/addon-depends/vdr-plugins/vdr-plugin-vnsiserver/package.mk
+++ packages/addons/addon-depends/vdr-plugins/vdr-plugin-vnsiserver/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="vdr-plugin-vnsiserver"
-PKG_VERSION="908684b58a3ebd4a447bc4b0a82b0bd8059bf605"
-PKG_SHA256="6c39a2e9854632274cec2b11e4eaaea9edc4095856941672f40d0633ece506df"
+PKG_VERSION="47a90dd9298753083a9a6482bb9990ea9a88aa7a"
+PKG_SHA256="cd8087306dc5d77b150ca9f77bba91460507dc9a2336b9f61ce13aeefecf23e3"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/mdre77/vdr-plugin-vnsiserver"
 PKG_URL="https://github.com/mdre77/vdr-plugin-vnsiserver/archive/${PKG_VERSION}.tar.gz"
