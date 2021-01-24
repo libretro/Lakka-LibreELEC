3f3733272a7625ada6e8818585c2e0f5ecf77d37
heitbaum <rudi@heitbaum.com>
2021-01-24 12:51:29 +0000

    bison: update to 3.8.1
    update 3.7.4 (2020-11-14) to 3.8.1 (2021-09-11)
    news: https://git.savannah.gnu.org/cgit/bison.git/plain/NEWS
    log: https://git.savannah.gnu.org/cgit/bison.git/log/

--- packages/devel/bison/package.mk
+++ packages/devel/bison/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="bison"
-PKG_VERSION="3.7.4"
-PKG_SHA256="a3b5813f48a11e540ef26f46e4d288c0c25c7907d9879ae50e430ec49f63c010"
+PKG_VERSION="3.8.1"
+PKG_SHA256="31fc602488aad6bdecf0ccc556e0fc72fc57cdc595cf92398f020e0cf4980f15"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/software/bison/"
 PKG_URL="http://ftpmirror.gnu.org/bison/${PKG_NAME}-${PKG_VERSION}.tar.xz"
