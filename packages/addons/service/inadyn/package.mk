9cbf2b76292a3e9e96c2ebe1c305077f065dd54d
heitbaum <rudi@heitbaum.com>
2021-02-01 07:29:02 +0000

    inadyn: update to 2.8.1
    update 2.7 (2020-03-22) to 2.8.1 (2021-01-31)
    changelog: https://github.com/troglobit/inadyn/blob/master/ChangeLog.md

--- packages/addons/service/inadyn/package.mk
+++ packages/addons/service/inadyn/package.mk
@@ -2,9 +2,9 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="inadyn"
-PKG_VERSION="2.7"
-PKG_SHA256="b27ad8ac6718d167495ed8d2ce91fd90cf896916ab2eb28a6bf5ed1a54fe74e4"
-PKG_REV="106"
+PKG_VERSION="2.8.1"
+PKG_SHA256="bf575d2c4330821f834f947ff7a30afb83538f0f5802924e8e645a03922a8036"
+PKG_REV="107"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="http://troglobit.com/inadyn.html"
