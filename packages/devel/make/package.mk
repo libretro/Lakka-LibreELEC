5152c4ac9a3dbc7c9400c76565906467e2f55f49
Seth Barberee <seth.barberee@gmail.com>
2021-02-11 13:58:44 -0600

    make: fix compile issues and update package from LE

--- packages/devel/make/package.mk
+++ packages/devel/make/package.mk
@@ -3,18 +3,18 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="make"
-PKG_VERSION="4.2.1"
-PKG_SHA256="d6e262bf3601b42d2b1e4ef8310029e1dcf20083c5446b4b7aa67081fdffc589"
+PKG_VERSION="4.3"
+PKG_SHA256="e05fdde47c5f7ca45cb697e973894ff4f5d79e13b750ed57d7b66d8defc78e19"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://www.gnu.org/software/make/"
-PKG_URL="http://ftpmirror.gnu.org/make/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_URL="http://ftpmirror.gnu.org/make/${PKG_NAME}-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST=""
 PKG_LONGDESC="Utility to maintain groups of programs."
 
 pre_configure_host() {
-  export CC=$LOCAL_CC
+  export CC=${LOCAL_CC}
 }
 
 post_makeinstall_host() {
-  ln -sf make $TOOLCHAIN/bin/gmake
+  ln -sf make ${TOOLCHAIN}/bin/gmake
 }
