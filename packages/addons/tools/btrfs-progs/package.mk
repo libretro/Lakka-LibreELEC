0734ad63ed79ee7f8a289350531efecd869ac826
heitbaum <rudi@heitbaum.com>
2021-02-14 11:50:07 +0000

    btrfs-progs: update addon to (103) with disable-zstd

--- packages/addons/tools/btrfs-progs/package.mk
+++ packages/addons/tools/btrfs-progs/package.mk
@@ -4,12 +4,12 @@
 PKG_NAME="btrfs-progs"
 PKG_VERSION="4.15.1"
 PKG_SHA256="9cb985b3466e2e0ca712ef8570d7eb2f94b56592221baf0fc76622f413852445"
-PKG_REV="102"
+PKG_REV="103"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://btrfs.wiki.kernel.org/index.php/Main_Page"
 PKG_URL="https://github.com/kdave/btrfs-progs/archive/v${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain util-linux zlib lzo zstd"
+PKG_DEPENDS_TARGET="toolchain util-linux zlib lzo"
 PKG_SECTION="tools"
 PKG_SHORTDESC="tools for the btrfs filesystem"
 PKG_LONGDESC="tools for the btrfs filesystem"
@@ -22,8 +22,9 @@ PKG_ADDON_NAME="BTRFS Tools"
 PKG_ADDON_TYPE="xbmc.python.script"
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-backtrace \
+                           --disable-convert \
                            --disable-documentation \
-                           --disable-convert"
+                           --disable-zstd"
 
 pre_configure_target() {
   ./autogen.sh
