c62f1a7ccdb3e1695c6e4d5513933bcfda527078
Jernej Skrabec <jernej.skrabec@siol.net>
2021-02-15 20:39:35 +0100

    dt-overlays: Update to 4352d87db6e85d285673359e80cce87d5e025c64

--- packages/tools/dt-overlays/package.mk
+++ packages/tools/dt-overlays/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="dt-overlays"
-PKG_VERSION="e57a9b1eadd420a4e5d50c0669184828ff12abc5"
-PKG_SHA256="b63ee4f54723349ce405c9bd3545b5bb0814e4d1959a6a1db0bb2517318b1cc5"
+PKG_VERSION="4352d87db6e85d285673359e80cce87d5e025c64"
+PKG_SHA256="754a0c37a41f8e5d9d179eb1df40b84eb48c0a505a37e39fed5f1ceb35e8e120"
 PKG_LICENSE="GPLv2+ or MIT"
 PKG_SITE="https://github.com/LibreELEC/dt-overlays"
 PKG_URL="https://github.com/LibreELEC/dt-overlays/archive/${PKG_VERSION}.tar.gz"
