16df7fdfb567c878c089ff57d85c0607f4caa306
heitbaum <rudi@heitbaum.com>
2021-02-17 07:46:50 +0000

    intel-ucode: update to 20210608
    update 20201118 to 20210608
    skipped 20210216 as it was only relevant to Server chips.
    
    release notes:
    - https://github.com/intel/Intel-Linux-Processor-Microcode-Data-Files/releases/tag/microcode-20210608
    - https://github.com/intel/Intel-Linux-Processor-Microcode-Data-Files/releases/tag/microcode-20210216

--- packages/linux-firmware/intel-ucode/package.mk
+++ packages/linux-firmware/intel-ucode/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="intel-ucode"
-PKG_VERSION="20201118"
-PKG_SHA256="e42a264b7b86e80d013d6d00062467352c1f37e0aaea10fe5b51e4d8687921ab"
+PKG_VERSION="20210608"
+PKG_SHA256="fd85b6b769efd029dec6a2c07106fd18fb4dcb548b7bc4cde09295a8344ef6d7"
 PKG_ARCH="x86_64"
 PKG_LICENSE="other"
 PKG_SITE="https://downloadcenter.intel.com/search?keyword=linux+microcode"
