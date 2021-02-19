0d599fa98d34587114072e27ed63ceca0cd9b282
SupervisedThinking <supervisedthinking@gmail.com>
2021-02-19 01:14:35 +0100

    game.libretro.fbneo: initial package 1.0.0.32-Matrix"

---
+++ packages/mediacenter/kodi-binary-addons/game.libretro.fbneo/package.mk
@@ -0,0 +1,17 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="game.libretro.fbneo"
+PKG_VERSION="1.0.0.32-Matrix"
+PKG_SHA256="6bc8ac3c4147301245d3d54962f71c561959275f41c2a0d415b26b6bfd8578b9"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/kodi-game/game.libretro.fbneo"
+PKG_URL="https://github.com/kodi-game/game.libretro.fbneo/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform libretro-fbneo"
+PKG_SECTION=""
+PKG_LONGDESC="game.libretro.fbneo: FinalBurn Neo GameClient for Kodi"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="kodi.gameclient"
