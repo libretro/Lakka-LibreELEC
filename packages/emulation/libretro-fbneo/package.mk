1b9787576d539a824fef332092060ec2852e0cf4
SupervisedThinking <supervisedthinking@gmail.com>
2021-02-19 01:14:18 +0100

    libretro-fbneo: initial package git 67f87d5

---
+++ packages/emulation/libretro-fbneo/package.mk
@@ -0,0 +1,36 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="libretro-fbneo"
+PKG_VERSION="67f87d5e58ac4bbc88c61d263ecb2af7faea49f3"
+PKG_SHA256="cc3b588d18dcf207e0e6bba258e9dfc602a3e1c7602f7db752ec34d5d36414ed"
+PKG_LICENSE="OSS"
+PKG_SITE="https://github.com/libretro/FBNeo"
+PKG_URL="https://github.com/libretro/FBNeo/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform"
+PKG_LONGDESC="game.libretro.fbneo: FinalBurn Neo GameClient for Kodi"
+PKG_TOOLCHAIN="make"
+PKG_BUILD_FLAGS="+lto"
+
+PKG_LIBNAME="fbneo_libretro.so"
+PKG_LIBPATH="src/burner/libretro/${PKG_LIBNAME}"
+PKG_LIBVAR="FBNEO_LIB"
+
+PKG_MAKE_OPTS_TARGET="-C src/burner/libretro/ GIT_VERSION=${PKG_VERSION:0:7}"
+
+pre_configure_target() {
+  if [ "${ARCH}" = "arm" ]; then
+    PKG_MAKE_OPTS_TARGET+=" platform=armv"
+
+    # NEON Support
+    if target_has_feature neon; then
+      PKG_MAKE_OPTS_TARGET+="-neon"
+    fi
+  fi
+}
+
+makeinstall_target() {
+  mkdir -p ${SYSROOT_PREFIX}/usr/lib/cmake/${PKG_NAME}
+  cp ${PKG_LIBPATH} ${SYSROOT_PREFIX}/usr/lib/${PKG_LIBNAME}
+  echo "set(${PKG_LIBVAR} ${SYSROOT_PREFIX}/usr/lib/${PKG_LIBNAME})" > ${SYSROOT_PREFIX}/usr/lib/cmake/${PKG_NAME}/${PKG_NAME}-config.cmake
+}
