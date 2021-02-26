a5099794ece53fa67bfe369217048ad9eb03982f
Lukas Rusak <lorusak@gmail.com>
2021-02-26 11:53:43 -0800

    systemd: use default hwdb service

--- packages/sysutils/systemd/package.mk
+++ packages/sysutils/systemd/package.mk
@@ -121,14 +121,6 @@ post_makeinstall_target() {
   safe_remove ${INSTALL}/usr/lib/tmpfiles.d/home.conf
   safe_remove ${INSTALL}/usr/share/factory
 
-  # clean up hwdb
-  safe_remove ${INSTALL}/usr/lib/udev/hwdb.d/20-OUI.hwdb
-  safe_remove ${INSTALL}/usr/lib/udev/hwdb.d/20-acpi-vendor.hwdb
-  safe_remove ${INSTALL}/usr/lib/udev/hwdb.d/20-bluetooth-vendor-product.hwdb
-  safe_remove ${INSTALL}/usr/lib/udev/hwdb.d/20-net-ifname.hwdb
-  safe_remove ${INSTALL}/usr/lib/udev/hwdb.d/20-sdio-classes.hwdb
-  safe_remove ${INSTALL}/usr/lib/udev/hwdb.d/20-sdio-vendor-model.hwdb
-
   # remove Network adaper renaming rule, this is confusing
   safe_remove ${INSTALL}/usr/lib/udev/rules.d/80-net-setup-link.rules
 
@@ -149,8 +141,8 @@ post_makeinstall_target() {
   safe_remove ${INSTALL}/usr/lib/systemd/system/systemd-update-done.service
   safe_remove ${INSTALL}/usr/lib/systemd/system/*.target.wants/systemd-update-done.service
 
-  # remove systemd-hwdb-update. we have own hwdb.service
-  safe_remove ${INSTALL}/usr/lib/systemd/system/systemd-hwdb-update.service
+  # adjust systemd-hwdb-update (we have read-only /etc).
+  sed '/^ConditionNeedsUpdate=.*$/d' -i ${INSTALL}/usr/lib/systemd/system/systemd-hwdb-update.service
 
   # remove nspawn
   safe_remove ${INSTALL}/usr/bin/systemd-nspawn
@@ -281,7 +273,6 @@ post_install() {
   enable_service userconfig.service
   enable_service usercache.service
   enable_service envconfig.service
-  enable_service hwdb.service
   enable_service cpufreq.service
   enable_service network-base.service
   enable_service systemd-timesyncd.service
