f2cddd7a5c73c3629a45987d7fd6d59fb1530c6a
heitbaum <rudi@heitbaum.com>
2021-03-06 05:08:51 +0000

    sundtek-mediatv: update xbmc.translatePath (deprecated) to xbmcvfs.translatePath

--- packages/addons/driver/sundtek-mediatv/source/resources/actions.py
+++ packages/addons/driver/sundtek-mediatv/source/resources/actions.py
@@ -1,19 +1,21 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 import os
 import sys
 import subprocess
 import xbmcaddon
+import xbmcvfs
 
 __settings__      = xbmcaddon.Addon(id = 'driver.dvb.sundtek-mediatv')
 __cwd__           = __settings__.getAddonInfo('path')
-__resources_lib__ = xbmc.translatePath(os.path.join(__cwd__, 'resources', 'lib'))
-__settings_xml__  = xbmc.translatePath(os.path.join(__cwd__, 'resources', 'settings.xml'))
+__resources_lib__ = xbmcvfs.translatePath(os.path.join(__cwd__, 'resources', 'lib'))
+__settings_xml__  = xbmcvfs.translatePath(os.path.join(__cwd__, 'resources', 'settings.xml'))
 
-__mediaclient__   = xbmc.translatePath(os.path.join(__cwd__, 'bin', 'mediaclient'))
+__mediaclient__   = xbmcvfs.translatePath(os.path.join(__cwd__, 'bin', 'mediaclient'))
 __mediaclient_e__ = __mediaclient__ + ' -e'
-__update_sh__     = xbmc.translatePath(os.path.join(__cwd__, 'bin', 'sundtek-update-driver.sh'))
+__update_sh__     = xbmcvfs.translatePath(os.path.join(__cwd__, 'bin', 'sundtek-update-driver.sh'))
 
 if len(sys.argv) == 2:
   if sys.argv[1] == 'refresh_tuners':
