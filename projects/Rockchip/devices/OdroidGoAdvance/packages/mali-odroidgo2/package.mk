6c07e387f9b70287d4f90ae53b55486c68a4985a
David G. F <david@davidgf.net>
2021-03-09 21:52:25 +0100

    Fix OGA builds (#1196)
    * Fix OGA builds
    
    No need for the u-boot patch, update the INI files instead.
    Added the new userspace mali G31 blob and update the build script (we
    pull headers still from Rockchip repo).
    Add the RTL8821 patch to fix linux build with newer GCCs too.
    
    This makes OGAs bootable again (tested it on OGA Super)
    
    * Adding aarch64 support

--- projects/Rockchip/devices/OdroidGoAdvance/packages/mali-odroidgo2/package.mk
+++ projects/Rockchip/devices/OdroidGoAdvance/packages/mali-odroidgo2/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2020-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="mali-odroidgo2"
-PKG_VERSION="f226e982386287a4df669e2832d9ddd613d4153b"
-PKG_SHA256="cec41b7383b64fbb9fee891c9bcfaa979cb8d8b943d131a471c7fac7e16b393e"
+PKG_VERSION="c3b4a820e1cfd8e049c5321808c29713c25e2cd0"
+PKG_SHA256="0594dbcd730ba04cd64b87a1e66c21adcebcf98629a81c2267ddb718fc7fe875"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="nonfree"
 PKG_SITE="https://github.com/rockchip-linux/libmali"
@@ -14,33 +14,44 @@ PKG_LONGDESC="The Mali GPU library used in Rockchip Platform for Odroidgo Advanc
 post_makeinstall_target() {
 	# remove all the extra blobs, we only need one
 	rm -rf $INSTALL/usr
-	
-	mkdir -p $INSTALL/usr/lib/
-	cp $PKG_BUILD/lib/arm-linux-gnueabihf/libmali-bifrost-g31-rxp0-gbm.so $INSTALL/usr/lib/libmali.so
 
-	ln -sf /usr/lib/libmali.so $INSTALL/usr/lib/libEGL.so
-	ln -sf /usr/lib/libmali.so $INSTALL/usr/lib/libEGL.so.1
-	ln -sf /usr/lib/libmali.so $INSTALL/usr/lib/libgbm.so
-	ln -sf /usr/lib/libmali.so $INSTALL/usr/lib/libGLESv2.so
-	ln -sf /usr/lib/libmali.so $INSTALL/usr/lib/libGLESv2.so.2
-	ln -sf /usr/lib/libmali.so $INSTALL/usr/lib/libGLESv3.so
-	ln -sf /usr/lib/libmali.so $INSTALL/usr/lib/libGLESv3.so.3
-	ln -sf /usr/lib/libmali.so $INSTALL/usr/lib/libGLESv1_CM.so.1
-	ln -sf /usr/lib/libmali.so $INSTALL/usr/lib/libGLES_CM.so.1
+	if [ "$ARCH" == "arm" ]; then
+		MALI_BLOB="libmali-rk3326-gbm_arm32_r13p0_with_vulkan_and_cl.so"
+	else
+		MALI_BLOB="libmali-rk3326-gbm_arm64_r13p0_with_vulkan_and_cl.so"
+	fi
+
+	mkdir -p $INSTALL/usr/lib/
+	cp $PKG_BUILD/$MALI_BLOB $INSTALL/usr/lib/libmali.so.1.9.0
+
+	ln -sf libmali.so.1.9.0 $INSTALL/usr/lib/libmali.so.1
+	ln -sf libmali.so.1.9.0 $INSTALL/usr/lib/libmali.so
+
+	ln -sf libmali.so $INSTALL/usr/lib/libEGL.so
+	ln -sf libmali.so $INSTALL/usr/lib/libEGL.so.1
+	ln -sf libmali.so $INSTALL/usr/lib/libgbm.so
+	ln -sf libmali.so $INSTALL/usr/lib/libGLESv2.so
+	ln -sf libmali.so $INSTALL/usr/lib/libGLESv2.so.2
+	ln -sf libmali.so $INSTALL/usr/lib/libGLESv3.so
+	ln -sf libmali.so $INSTALL/usr/lib/libGLESv3.so.3
+	ln -sf libmali.so $INSTALL/usr/lib/libGLESv1_CM.so.1
+	ln -sf libmali.so $INSTALL/usr/lib/libGLES_CM.so.1
 
 	cp -pr $PKG_BUILD/include $SYSROOT_PREFIX/usr
-	cp $PKG_BUILD/include/gbm.h $SYSROOT_PREFIX/usr/include/gbm.h
+	cp $PKG_BUILD/include/GBM/gbm.h $SYSROOT_PREFIX/usr/include/gbm.h
+	# This can be removed depending on the Mesa version at use
+	cp $PKG_BUILD/include/KHR/mali_khrplatform.h $SYSROOT_PREFIX/usr/include/KHR/khrplatform.h
 	
 	mkdir -p $SYSROOT_PREFIX/usr/lib
-	cp -PR $PKG_BUILD/lib/arm-linux-gnueabihf/libmali-bifrost-g31-rxp0-gbm.so $SYSROOT_PREFIX/usr/lib/libmali.so
+	cp -PR $PKG_BUILD/$MALI_BLOB $SYSROOT_PREFIX/usr/lib/libmali.so
 
-	ln -sf $SYSROOT_PREFIX/usr/lib/libmali.so $SYSROOT_PREFIX/usr/lib/libEGL.so
-	ln -sf $SYSROOT_PREFIX/usr/lib/libmali.so $SYSROOT_PREFIX/usr/lib/libEGL.so.1
-	ln -sf $SYSROOT_PREFIX/usr/lib/libmali.so $SYSROOT_PREFIX/usr/lib/libgbm.so
-	ln -sf $SYSROOT_PREFIX/usr/lib/libmali.so $SYSROOT_PREFIX/usr/lib/libGLESv2.so
-	ln -sf $SYSROOT_PREFIX/usr/lib/libmali.so $SYSROOT_PREFIX/usr/lib/libGLESv2.so.2
-	ln -sf $SYSROOT_PREFIX/usr/lib/libmali.so $SYSROOT_PREFIX/usr/lib/libGLESv3.so
-	ln -sf $SYSROOT_PREFIX/usr/lib/libmali.so $SYSROOT_PREFIX/usr/lib/libGLESv3.so.3
-	ln -sf $SYSROOT_PREFIX/usr/lib/libmali.so $SYSROOT_PREFIX/usr/lib/libGLESv1_CM.so.1
-	ln -sf $SYSROOT_PREFIX/usr/lib/libmali.so $SYSROOT_PREFIX/usr/lib/libGLES_CM.so.1
+	ln -sf libmali.so $SYSROOT_PREFIX/usr/lib/libEGL.so
+	ln -sf libmali.so $SYSROOT_PREFIX/usr/lib/libEGL.so.1
+	ln -sf libmali.so $SYSROOT_PREFIX/usr/lib/libgbm.so
+	ln -sf libmali.so $SYSROOT_PREFIX/usr/lib/libGLESv2.so
+	ln -sf libmali.so $SYSROOT_PREFIX/usr/lib/libGLESv2.so.2
+	ln -sf libmali.so $SYSROOT_PREFIX/usr/lib/libGLESv3.so
+	ln -sf libmali.so $SYSROOT_PREFIX/usr/lib/libGLESv3.so.3
+	ln -sf libmali.so $SYSROOT_PREFIX/usr/lib/libGLESv1_CM.so.1
+	ln -sf libmali.so $SYSROOT_PREFIX/usr/lib/libGLES_CM.so.1
 }
