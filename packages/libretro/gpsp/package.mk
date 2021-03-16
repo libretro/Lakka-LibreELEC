2d8c6792d9ada83550563c28d035a15a73e91e47
Seth Barberee <seth.barberee@gmail.com>
2021-03-16 13:25:38 -0500

    gpsp: update for ARM fixes

--- packages/libretro/gpsp/package.mk
+++ packages/libretro/gpsp/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="gpsp"
-PKG_VERSION="a78ef12"
+PKG_VERSION="b9ac453"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
