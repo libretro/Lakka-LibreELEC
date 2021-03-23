b3930d74e37d47894066de745f65b2b2810dc528
Matthias Reichl <hias@horus.com>
2021-03-23 19:56:54 +0100

    linux (RPi): update to 5.10.25-e1499ba
    zoom fixes are now in RPi kernel, dropped local patch
    
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -22,8 +22,8 @@ case "${LINUX}" in
     PKG_SOURCE_NAME="linux-${LINUX}-${PKG_VERSION}.tar.gz"
     ;;
   raspberrypi)
-    PKG_VERSION="13c43880f2ee6db7d26949cb6a3e8db1a4b76736" # 5.10.23
-    PKG_SHA256="cfc1e54102b66719ad8f3e6f97baa4ddc46873ba82a130c481619a705901b1da"
+    PKG_VERSION="e1499baa0b0c4b91f0f149421a212746739b93c2" # 5.10.25
+    PKG_SHA256="b605c7d493fa8b17439c325a93d9da1823ff48bcfa7a69d87fcf0fc161a0375a"
     PKG_URL="https://github.com/raspberrypi/linux/archive/${PKG_VERSION}.tar.gz"
     PKG_SOURCE_NAME="linux-${LINUX}-${PKG_VERSION}.tar.gz"
     ;;
