2d5c8d88fc4703119409d908220e264b67758268
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2021-03-24 21:41:21 +0100

    joyutils: add package

---
+++ packages/tools/joyutils/package.mk
@@ -0,0 +1,22 @@
+PKG_NAME="joyutils"
+PKG_VERSION="b6703eb"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/datrh/joyutils"
+PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="tools"
+PKG_SHORTDESC="jscal, jstest, and jsattach utilities for the Linux joystick driver"
+PKG_TOOLCHAIN="manual"
+
+make_target() {
+  ${CC} -lm -o jscal jscal.c ${CFLAGS}
+  ${CC} -lm -o jstest jstest.c ${CFLAGS}
+  ${CC} -lm -o jsattach jsattach.c ${CFLAGS}
+}
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/bin
+    cp jscal ${INSTALL}/usr/bin/
+    cp jstest ${INSTALL}/usr/bin/
+    cp jsattach ${INSTALL}/usr/bin/
+}
