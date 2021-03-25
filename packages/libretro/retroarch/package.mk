a313a7efc218e9fa856ee558ba09bbe803ca1d40
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2021-03-25 09:49:56 +0100

    retroarch: bump to same commit as public release
    and remove patch included in upstream

--- packages/libretro/retroarch/package.mk
+++ packages/libretro/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="5e551dd"
+PKG_VERSION="6388872"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/RetroArch"
