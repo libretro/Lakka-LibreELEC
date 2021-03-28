b54e66096e6de9eee9dae97722ef447116119a76
by-gnome <40670278+by-gnome@users.noreply.github.com>
2021-03-28 18:55:33 +0300

    linux: update to 5.10.26 (#1229)

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -44,8 +44,8 @@ case "$LINUX" in
     PKG_URL="https://github.com/hardkernel/linux/archive/$PKG_VERSION.tar.gz"
     ;;
   mainline-5.10)
-    PKG_VERSION="5.10.22"
-    PKG_SHA256="245b16b532d67ca34805bd66790e3697d19269499d9b27056d73bfd0823163df"
+    PKG_VERSION="5.10.26"
+    PKG_SHA256="fc532833f1ac167f363f1b9de85db39d2d635ab516f66dc381bdd70804601482"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v5.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;
