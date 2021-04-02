83fc1809d952c95d5c18c52f2e2bb4afaef80eaf
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2021-04-02 19:58:21 +0200

    rpi-eeprom: drop deprecated flashrom, use pciutils

--- packages/tools/rpi-eeprom/package.mk
+++ packages/tools/rpi-eeprom/package.mk
@@ -8,7 +8,7 @@ PKG_ARCH="arm aarch64"
 PKG_LICENSE="BSD-3/custom"
 PKG_SITE="https://github.com/raspberrypi/rpi-eeprom"
 PKG_URL="https://github.com/raspberrypi/rpi-eeprom/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="flashrom"
+PKG_DEPENDS_TARGET="pciutils"
 PKG_LONGDESC="rpi-eeprom: firmware, config and scripts to update RPi4 SPI bootloader"
 PKG_TOOLCHAIN="manual"
 
@@ -38,11 +38,13 @@ makeinstall_target() {
       done
     done
 
+    # also copy default and latest symlinks
+    cp -Prv ${PKG_BUILD}/firmware/{default,latest} ${DESTDIR}
+
   mkdir -p ${INSTALL}/usr/bin
     cp -PRv ${PKG_DIR}/source/rpi-eeprom-update ${INSTALL}/usr/bin
     cp -PRv ${PKG_BUILD}/rpi-eeprom-update ${INSTALL}/usr/bin/.rpi-eeprom-update.real
     cp -PRv ${PKG_BUILD}/rpi-eeprom-config ${INSTALL}/usr/bin
-    cp -PRv ${PKG_BUILD}/firmware/vl805 ${INSTALL}/usr/bin
 
   mkdir -p ${INSTALL}/etc/default
     cp -PRv ${PKG_DIR}/config/* ${INSTALL}/etc/default
