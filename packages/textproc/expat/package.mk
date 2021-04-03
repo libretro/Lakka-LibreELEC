235d54b6b2b046b27a81e789e25a5d6c2578394d
heitbaum <rudi@heitbaum.com>
2021-04-03 09:21:12 +0000

    expat: update to 2.4.1

--- packages/textproc/expat/package.mk
+++ packages/textproc/expat/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="expat"
-PKG_VERSION="2.2.10"
-PKG_SHA256="b2c160f1b60e92da69de8e12333096aeb0c3bf692d41c60794de278af72135a5"
+PKG_VERSION="2.4.1"
+PKG_SHA256="2f9b6a580b94577b150a7d5617ad4643a4301a6616ff459307df3e225bcfbf40"
 PKG_LICENSE="OSS"
 PKG_SITE="http://expat.sourceforge.net/"
 PKG_URL="https://github.com/libexpat/libexpat/releases/download/R_${PKG_VERSION//./_}/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
