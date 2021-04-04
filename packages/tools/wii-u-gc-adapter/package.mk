444fc8f46a8112d5d7322eab8970006e1e8ac54d
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2021-04-04 13:31:03 +0200

    wii-u-gc-adapter: add package

---
+++ packages/tools/wii-u-gc-adapter/package.mk
@@ -0,0 +1,12 @@
+PKG_NAME="wii-u-gc-adapter"
+PKG_VERSION="1030fee"
+PKG_LICENSE="Non-commercial"
+PKG_SITE="https://github.com/ToadKing/wii-u-gc-adapter"
+PKG_URL="https://github.com/ToadKing/wii-u-gc-adapter/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain libusb systemd"
+PKG_SHORTDESC="Tool for using the Wii U GameCube Adapter on Linux"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/bin
+  cp wii-u-gc-adapter ${INSTALL}/usr/bin/
+}
