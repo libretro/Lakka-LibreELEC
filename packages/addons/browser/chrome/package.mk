3f11a26d56a05fd28d1974fa3b193eaca5ac9526
heitbaum <rudi@heitbaum.com>
2021-04-04 00:43:33 +0000

    chrome: update to 89.0.4389.114 and fix gtk3 compile issues and addon (105)

--- packages/addons/browser/chrome/package.mk
+++ packages/addons/browser/chrome/package.mk
@@ -4,8 +4,8 @@
 PKG_NAME="chrome"
 PKG_VERSION="1.0"
 # curl -s http://dl.google.com/linux/chrome/deb/dists/stable/main/binary-amd64/Packages | grep -B 1 Version
-PKG_VERSION_NUMBER="87.0.4280.66"
-PKG_REV="104"
+PKG_VERSION_NUMBER="89.0.4389.114"
+PKG_REV="105"
 PKG_ARCH="x86_64"
 PKG_LICENSE="Custom"
 PKG_SITE="http://www.google.com/chrome"
