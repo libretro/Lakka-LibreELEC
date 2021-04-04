b3c835fa89949604027b6fd388b874020ee575a7
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2021-04-04 13:27:55 +0200

    sixpair: add package

---
+++ packages/network/sixpair/package.mk
@@ -0,0 +1,20 @@
+PKG_NAME="sixpair"
+PKG_VERSION="23e6e08"
+PKG_LICENSE="GPL"
+PKG_SITE="http://www.pabr.org/sixlinux/"
+PKG_URL="https://github.com/lakkatv/sixpair/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain libusb libusb-compat"
+PKG_SHORTDESC="Associate PS3 Sixaxis controller to system bluetoothd via USB"
+PKG_TOOLCHAIN="manual"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+make_target() {
+  make sixpair LDLIBS=-lusb
+}
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/bin
+    cp sixpair ${INSTALL}/usr/bin
+}
