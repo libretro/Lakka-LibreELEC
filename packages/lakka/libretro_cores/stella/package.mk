a38ef3bbb19f8c0cd48049fab9724f38a8ee4f6d
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2021-04-27 09:19:31 +0200

    add stella libretro core

---
+++ packages/lakka/libretro_cores/stella/package.mk
@@ -0,0 +1,16 @@
+PKG_NAME="stella"
+PKG_VERSION="66e6334"
+PKG_LICENSE="GPL2"
+PKG_SITE="https://github.com/stella-emu/stella"
+PKG_URL="${PKG_SITE}.git"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SHORTDESC="Stella is a multi-platform Atari 2600 VCS emulator released under the GNU General Public License (GPL)."
+PKG_TOOLCHAIN="make"
+
+PKG_MAKE_OPTS_TARGET="-C ../src/libretro -f Makefile"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+    cp -v ../src/libretro/stella_libretro.so ${INSTALL}/usr/lib/libretro/
+}
+
