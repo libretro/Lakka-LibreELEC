a26800cc6f08cd68cc55b7b007c4f4a9e42bbae6
Demetris Ierokipides <ierokipides.dem@gmail.com>
2021-05-11 19:10:33 +0300

    Linux: fix miscalculated sha256 issue

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -46,7 +46,7 @@ case "$LINUX" in
     ;;
   mainline-5.10)
     PKG_VERSION="5.10.35"
-    PKG_SHA256="5afc03c992bc735c16825f1d157a1fa180d57ad9f8b730c0550d542f6f61f79c"
+    PKG_SHA256="ac37a19d45b77a87e58e3aae8b127a6e7eb85ed7467fc8e58474b387bfd498fd"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v5.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;
