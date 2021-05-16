fef213e06da4037e7645762014e01c03001cdbec
Ian Leonard <antonlacon@gmail.com>
2021-05-16 23:35:58 +0000

    dav1d: update to 0.9.0
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- packages/multimedia/dav1d/package.mk
+++ packages/multimedia/dav1d/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="dav1d"
-PKG_VERSION="0.8.2"
-PKG_SHA256="78ec7a1714d98a8f4ecbc4255e83e6c4c944cdd881871ea234ce40153fd3df04"
+PKG_VERSION="0.9.0"
+PKG_SHA256="e0cb645f170e7a087bc76e501324177be51a8db21df22ad37b43d289d7d1f7b5"
 PKG_LICENSE="BSD"
 PKG_SITE="http://www.jbkempf.com/blog/post/2018/Introducing-dav1d"
 PKG_URL="https://code.videolan.org/videolan/dav1d/-/archive/${PKG_VERSION}/dav1d-${PKG_VERSION}.tar.bz2"
