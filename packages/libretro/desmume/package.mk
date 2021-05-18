b198b11ccf02e96ad7c24b3f24c8b516d4a6c549
Demetris Ierokipides <ierokipides.dem@gmail.com>
2021-05-19 02:16:33 +0300

    Desmume: restore working commit(only haiku is affected)

--- packages/libretro/desmume/package.mk
+++ packages/libretro/desmume/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="desmume"
-PKG_VERSION="0bc055a"
+PKG_VERSION="7300ed8"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
