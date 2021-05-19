adbd61821b41a1332724aa2d4c9378d693f13091
Matthias Reichl <hias@horus.com>
2021-05-19 20:10:43 +0200

    kodi-binary-addons: update to latest versions
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/mediacenter/kodi-binary-addons/peripheral.joystick/package.mk
+++ packages/mediacenter/kodi-binary-addons/peripheral.joystick/package.mk
@@ -3,9 +3,9 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="peripheral.joystick"
-PKG_VERSION="1.7.1-Matrix"
-PKG_SHA256="4dc63c6c5bdad25881eeba800765d97c53b2583addf28e71bbcd67775452ecdb"
-PKG_REV="4"
+PKG_VERSION="1.7.2-Matrix"
+PKG_SHA256="405acfbcc510f96a48244168362213955357f95cd8b1b92fe1ce483213045bfb"
+PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/xbmc/peripheral.joystick"
