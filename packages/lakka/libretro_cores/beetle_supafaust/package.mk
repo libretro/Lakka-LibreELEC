16cb4259897bb1e3e50cc5051cc17079d7b60f60
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2021-05-22 20:34:43 +0200

    beetle_supafaust: new libretro core

---
+++ packages/lakka/libretro_cores/beetle_supafaust/package.mk
@@ -0,0 +1,14 @@
+PKG_NAME="beetle_supafaust"
+PKG_VERSION="927b3a1"
+PKG_ARCH="arm"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/libretro/supafaust"
+PKG_URL="${PKG_SITE}.git"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="SNES emulator for multicore ARM Cortex A7,A9,A15,A53 Linux platforms"
+PKG_TOOLCHAIN="make"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+    cp -v mednafen_supafaust_libretro.so ${INSTALL}/usr/lib/libretro/
+}
