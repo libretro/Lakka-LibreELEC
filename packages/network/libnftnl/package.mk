8fabb8533dfa920c7fd3bf3d80632c5c073d9cca
heitbaum <rudi@heitbaum.com>
2021-05-25 21:58:17 +0000

    libnftnl: update to 1.2.1

--- packages/network/libnftnl/package.mk
+++ packages/network/libnftnl/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libnftnl"
-PKG_VERSION="1.1.9"
-PKG_SHA256="e9b21a6f9a41f9e72aff696cc842e4a9a78ec8d281aec188f3e2b7a21db94b9a"
+PKG_VERSION="1.2.1"
+PKG_SHA256="7508a5c414fab13e3cb3ce8262d0ce4f02c1590a8e4f8628ab497b5b4585937c"
 PKG_LICENSE="GPL"
 PKG_SITE="http://netfilter.org/projects/libnftnl"
 PKG_URL="http://netfilter.org/projects/libnftnl/files/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
