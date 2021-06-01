7da2f7197535a8cbc6ec11ccd540b9a1f12b6255
Demetris Ierokipides <ierokipides.dem@gmail.com>
2021-06-01 03:03:37 +0300

    Retroarch: Clean xu4 libmali clutter

--- packages/libretro/retroarch/package.mk
+++ packages/libretro/retroarch/package.mk
@@ -84,8 +84,6 @@ elif [ "$OPENGLES" == "libmali" ] || [ "$OPENGLES" == "bcm2835-driver" ]; then
   RETROARCH_GL="--enable-opengles --enable-kms --disable-x11 --disable-wayland"
 elif [ "$OPENGLES" == "allwinner-mali" ]; then
   RETROARCH_GL="--enable-opengles --enable-kms --disable-x11"
-elif [ "$OPENGLES" == "odroidxu3-mali" ]; then
-  RETROARCH_GL="--enable-opengles --disable-kms --disable-x11 --enable-egl"
 elif [ "$OPENGLES" == "mesa" ]; then
   if [ "$PROJECT" == "RPi" ]; then
     RETROARCH_GL="--disable-x11 --enable-opengles --disable-videocore --enable-kms --enable-egl --disable-wayland"
