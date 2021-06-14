2a94faeac54844626e47f4b6c1e30aaf78520729
Demetris Ierokipides <ierokipides.dem@gmail.com>
2021-06-14 21:52:27 +0300

    Update mesa3d to 21.1.2

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa"
-PKG_VERSION="21.1.1"
-PKG_SHA256="f0bc7e16da77dd6ac02c43260a26d89696683f5a9f5e799492227a3c1183e3ee"
+PKG_VERSION="21.1.2"
+# PKG_SHA256=""
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
 PKG_URL="https://github.com/mesa3d/mesa/archive/mesa-$PKG_VERSION.tar.gz"
