a11b69b7cb66e609b014e4602a9ced8bb562ae94
heitbaum <rudi@heitbaum.com>
2021-06-26 00:57:35 +0000

    containerd: update to 1.5.6

--- packages/addons/addon-depends/containerd/package.mk
+++ packages/addons/addon-depends/containerd/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="containerd"
-PKG_VERSION="1.3.9"
-PKG_SHA256="9244212589c84b12262769dca6fb985c0c680cb5259c8904b29c511d81fd62d0"
+PKG_VERSION="1.5.6"
+PKG_SHA256="144612f9400c2fc52b2abc7b1ef33bf708d322dad9b3afdc1ae9d02fa034753b"
 PKG_LICENSE="APL"
 PKG_SITE="https://containerd.tools/"
 PKG_URL="https://github.com/containerd/containerd/archive/v${PKG_VERSION}.tar.gz"
@@ -13,7 +13,7 @@ PKG_LONGDESC="A daemon to control runC, built for performance and density."
 PKG_TOOLCHAIN="manual"
 
 # Git commit of the matching release https://github.com/containerd/containerd/releases
-PKG_GIT_COMMIT="8fba4e9a7d01810a393d5d25a3621dc101981175"
+PKG_GIT_COMMIT="1a1b383ad5b520349f13f9715e0cd1e2f132c087"
 
 pre_make_target() {
 
