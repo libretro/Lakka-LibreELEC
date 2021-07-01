c03380ca04a9264b6e49f7e2219bdcbba213d627
mglae <mglmail@arcor.de>
2021-07-01 23:43:15 +0200

    vdr-plugin-eepg: update to 6be7f2e

--- packages/addons/addon-depends/vdr-plugins/vdr-plugin-eepg/package.mk
+++ packages/addons/addon-depends/vdr-plugins/vdr-plugin-eepg/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="vdr-plugin-eepg"
-PKG_VERSION="c46be443988489780f993a47d23a42936b49d6c0"
-PKG_SHA256="08b343caa392bddefeb6e8cfe6f341b4e11e120046dce05961a335a06e9bee6c"
+PKG_VERSION="6be7f2ee644aa33bd6e6e038548be8a85514272e"
+PKG_SHA256="f8698d8e72c4e1239a57ba4a0b2f54020270506d5dce7298f3080fcc8d59f56f"
 PKG_LICENSE="GPL"
 PKG_SITE="http://projects.vdr-developer.org/projects/plg-eepg"
 PKG_URL="https://github.com/vdr-projects/vdr-plugin-eepg/archive/${PKG_VERSION}.tar.gz"
