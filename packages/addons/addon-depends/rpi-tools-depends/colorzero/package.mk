a4e1ebd9e7f3f400de0bad5673a7d85ba82533e4
Matthias Reichl <hias@horus.com>
2021-07-08 20:19:17 +0200

    colorzero: update to 2.0
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/addons/addon-depends/rpi-tools-depends/colorzero/package.mk
+++ packages/addons/addon-depends/rpi-tools-depends/colorzero/package.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="colorzero"
-PKG_VERSION="1.1"
-PKG_SHA256="24d30b55793150f9ae7478f8aa2126fe5598a31f928907eadbf9d5a19aa9964c"
+PKG_VERSION="2.0"
+PKG_SHA256="86c9933b004aec8ce1c476d1d1129e00325c7724df3c09aa353d5f8e883ed08d"
 PKG_ARCH="arm"
 PKG_LICENSE="BSD"
 PKG_SITE="https://github.com/waveform80/colorzero"
-PKG_URL="https://github.com/waveform80/colorzero/archive/release-$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/waveform80/colorzero/archive/release-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Another color manipulation library for Python (originally from picamera)."
 PKG_TOOLCHAIN="manual"
