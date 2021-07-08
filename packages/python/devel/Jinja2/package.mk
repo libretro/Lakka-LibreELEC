4e2e4df290a134dd0f5f1568fa5272302bf1f3dd
heitbaum <rudi@heitbaum.com>
2021-07-08 11:44:57 +0000

    Jinja2: initial package
    systemd >= v249 requires Jinja2 for templating
    - refer: systemd/systemd - PR19630: Use jinja2 for templating
    - m4 use is dropped, and the two parallel substitution systems in meson are unified.
    
    Co-authored-by: CvH <1355173+CvH@users.noreply.github.com>

---
+++ packages/python/devel/Jinja2/package.mk
@@ -0,0 +1,16 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="Jinja2"
+PKG_VERSION="3.0.3"
+PKG_SHA256="611bb273cd68f3b993fabdc4064fc858c5b47a973cb5aa7999ec1ba405c87cd7"
+PKG_LICENSE="BSD"
+PKG_SITE="https://pypi.org/project/Jinja2/"
+PKG_URL="https://files.pythonhosted.org/packages/source/${PKG_NAME:0:1}/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_HOST="Python3:host setuptools:host MarkupSafe:host"
+PKG_LONGDESC="Jinja is a fast, expressive, extensible templating engine."
+PKG_TOOLCHAIN="manual"
+
+makeinstall_host() {
+  exec_thread_safe python3 setup.py install --prefix=${TOOLCHAIN}
+}
