fdbc211372df33f4706b73ec475dfc7f07834320
heitbaum <rudi@heitbaum.com>
2021-07-09 10:02:34 +0000

    stress-ng: update to 0.13.05 and PKG_URL

--- packages/addons/addon-depends/system-tools-depends/stress-ng/package.mk
+++ packages/addons/addon-depends/system-tools-depends/stress-ng/package.mk
@@ -2,10 +2,10 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="stress-ng"
-PKG_VERSION="0.12.11"
-PKG_SHA256="971393075321c24c3d5769acfabb705911d1f411ced5937b7cfea58528c1b4e6"
+PKG_VERSION="0.13.05"
+PKG_SHA256="f058c8fba37596ab32c3a4b2aedbdbf5f2b8a8ba1d312059e733290543ad00ac"
 PKG_LICENSE="GPLv2"
-PKG_SITE="https://kernel.ubuntu.com/~cking/stress-ng/"
+PKG_SITE="https://github.com/ColinIanKing/stress-ng"
 PKG_URL="https://kernel.ubuntu.com/~cking/tarballs/stress-ng/stress-ng-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain attr keyutils libaio libcap zlib"
 PKG_LONGDESC="stress-ng will stress test a computer system in various selectable ways"
