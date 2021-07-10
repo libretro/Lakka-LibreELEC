af7283bdb0bb17e9f57c0c549561aa4d9ae7a9fa
heitbaum <rudi@heitbaum.com>
2021-07-10 09:07:47 +0000

    dotnet-runtime: enables both ASP.NET 3.x and ASP.NET 5.x and addon (114)
    .NET 3.1.16 which includes both .NET and ASP.NET Core Runtimes have been
    relocated to the dotnet3-runtime package to enable both 3.x, 5.x to be
    available to applications.
    
    Also added .NET Runtime 5.0.7 and ASP.NET Core Runtime 5.0.7
    
    The following additional packages have been added as dependancies of
    this package.
    
    - add aspnet5-runtime (used by Jellyfin)
    - add dotnet3-runtime (used by Emby4)
    - add dotnet5-runtime (used by Jellyfin)

--- packages/addons/tools/dotnet-runtime/package.mk
+++ packages/addons/tools/dotnet-runtime/package.mk
@@ -2,42 +2,29 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="dotnet-runtime"
-PKG_VERSION="3.1.16"
-PKG_REV="113"
+PKG_REV="114"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
 PKG_SITE="https://dotnet.microsoft.com/"
-PKG_DEPENDS_TARGET="toolchain icu"
+PKG_DEPENDS_TARGET="toolchain icu aspnet5-runtime dotnet3-runtime dotnet5-runtime icu"
 PKG_SECTION="tools"
-PKG_SHORTDESC=".NET Core Runtime"
-PKG_LONGDESC=".NET Core Runtime (${PKG_VERSION}) runs applications built with .NET Core, a cross-platform .NET implementation."
+PKG_SHORTDESC="ASP.NET Core Runtime"
+PKG_LONGDESC="ASP.NET Core Runtimes ($(get_pkg_version dotnet3-runtime)) and ($(get_pkg_version dotnet5-runtime)) enables you to run existing console/web/server applications."
 PKG_TOOLCHAIN="manual"
 
 PKG_IS_ADDON="yes"
-PKG_ADDON_NAME=".Net Core Runtime"
+PKG_ADDON_NAME="ASP.Net Core Runtimes"
 PKG_ADDON_PROJECTS="any !RPi1"
 PKG_ADDON_TYPE="xbmc.python.script"
 PKG_MAINTAINER="Anton Voyl (awiouy)"
 
-case "${ARCH}" in
-  "aarch64")
-    PKG_SHA256="b76c049484efd86466d2e1cd88994521633c399d090adb1c6804128603816abe"
-    PKG_URL="https://download.visualstudio.microsoft.com/download/pr/64353333-3080-45f7-a3d5-33e391e4596c/e9d5d53cb318628485e8d1fbd26ec30d/aspnetcore-runtime-3.1.16-linux-arm64.tar.gz"
-    ;;
-  "arm")
-    PKG_SHA256="a0163cd5c5ceae228bfffb40053f3509e155a110c23e81c38705757a870e24cc"
-    PKG_URL="https://download.visualstudio.microsoft.com/download/pr/bd734390-3b5f-402a-826f-e0eae538b8ba/5914dd937ede96cb9297e6e7a80f46f3/aspnetcore-runtime-3.1.16-linux-arm.tar.gz"
-    ;;
-  "x86_64")
-    PKG_SHA256="b1a2f61d8a49e2a3ca5eb9daa103b83eb49ea1bcf14914560e601222e94a3022"
-    PKG_URL="https://download.visualstudio.microsoft.com/download/pr/c20a5ac5-5174-46b8-a875-b916a416050d/b2ddd212a183260569178d880899bd94/aspnetcore-runtime-3.1.16-linux-x64.tar.gz"
-    ;;
-esac
-PKG_SOURCE_NAME="aspnetcore-runtime_${PKG_VERSION}_${ARCH}.tar.gz"
-
 addon() {
   mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
-    cp -r ${PKG_BUILD}/* \
+    cp -r $(get_build_dir dotnet3-runtime)/* \
+          ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
+    cp -r $(get_build_dir dotnet5-runtime)/* \
+          ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
+    cp -r $(get_build_dir aspnet5-runtime)/* \
           ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
 
   mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/lib
