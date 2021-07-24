1aabf7b6d98f797d1cfa3a0403682aeb436d175d
heitbaum <rudi@heitbaum.com>
2021-07-24 13:17:17 +0000

    nghttp2: update to 1.45.1

--- packages/web/nghttp2/package.mk
+++ packages/web/nghttp2/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="nghttp2"
-PKG_VERSION="1.43.0"
-PKG_SHA256="f7d54fa6f8aed29f695ca44612136fa2359013547394d5dffeffca9e01a26b0f"
+PKG_VERSION="1.45.1"
+PKG_SHA256="abdc4addccadbc7d89abe27c4d6427d78e57d139f69c1f45749227393c68bf79"
 PKG_LICENSE="MIT"
 PKG_SITE="http://www.linuxfromscratch.org/blfs/view/cvs/basicnet/nghttp2.html"
 PKG_URL="https://github.com/nghttp2/nghttp2/releases/download/v${PKG_VERSION}/nghttp2-${PKG_VERSION}.tar.xz"
