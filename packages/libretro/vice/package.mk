4114e2d78c36de72a633aaf22aa1fde7803fd821
Demetris Ierokipides <ierokipides.dem@gmail.com>
2021-08-08 02:50:32 +0300

    Vice: Bump package and switch x64 to x64sc In readme states: x64, the old and less accurate C64 emulator, is no more built by default Also states that x64sc, a C64 emulator is focused on accuracy!

--- packages/libretro/vice/package.mk
+++ packages/libretro/vice/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="vice"
-PKG_VERSION="2977793"
+PKG_VERSION="74515a9"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/vice-libretro"
@@ -34,7 +34,7 @@ PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
 make_target() {
-  make EMUTYPE=x64
+  make EMUTYPE=x64sc
   make EMUTYPE=x128
   make EMUTYPE=xplus4
   make EMUTYPE=xvic
