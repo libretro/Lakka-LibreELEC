aec67855afb779d2148f0439a909def0f6d373a3
David Lawson <dmrlawson@gmail.com>
2021-08-14 11:56:42 +0100

    duckstation: add for arm, x86_64 (#1459)

--- packages/libretro/duckstation/package.mk
+++ packages/libretro/duckstation/package.mk
@@ -1,9 +1,8 @@
 PKG_NAME="duckstation"
-PKG_ARCH="aarch64"
+PKG_ARCH="x86_64 arm aarch64"
 PKG_LICENSE="GPL"
 PKG_VERSION="Lakka33"
 PKG_SITE="https://www.duckstation.org"
-PKG_URL="https://www.duckstation.org/libretro/duckstation_libretro_linux_aarch64.zip"
 PKG_SECTION="libretro"
 PKG_SHORTDESC="DuckStation is an simulator/emulator of the Sony PlayStation(TM) console, focusing on playability, speed, and long-term maintainability."
 PKG_TOOLCHAIN="manual"
@@ -11,10 +10,23 @@ PKG_TOOLCHAIN="manual"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
+case ${ARCH} in
+aarch64)
+  binary_filename="duckstation_libretro_linux_aarch64.zip"
+  ;;
+arm)
+  binary_filename="duckstation_libretro_linux_armv7.zip"
+  ;;
+x86_64)
+  binary_filename="duckstation_libretro_linux_x64.zip"
+  ;;
+esac
+PKG_URL="https://www.duckstation.org/libretro/${binary_filename}"
+
 make_target() {
   cd $PKG_BUILD
   wget $PKG_URL
-  unzip duckstation_libretro_linux_aarch64.zip
+  unzip $binary_filename
 }
 
 makeinstall_target() {
