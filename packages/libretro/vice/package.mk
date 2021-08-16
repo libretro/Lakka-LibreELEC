64852c042f240fc7656668b1a2e29baa2a435601
Ntemis <ierokipides.dem@gmail.com>
2021-08-16 23:39:36 +0200

    Vice: Fix linking and restore x64 target

--- packages/libretro/vice/package.mk
+++ packages/libretro/vice/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="vice"
-PKG_VERSION="74515a9"
+PKG_VERSION="4157fc5"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/vice-libretro"
@@ -34,9 +34,12 @@ PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
 make_target() {
-  make EMUTYPE=x64sc
+  make EMUTYPE=x64
+  make objectclean
   make EMUTYPE=x128
+  make objectclean
   make EMUTYPE=xplus4
+  make objectclean
   make EMUTYPE=xvic
 }
 
