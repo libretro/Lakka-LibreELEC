a9b29218991b4f35242aedae56caddf61e61da98
heitbaum <rudi@heitbaum.com>
2021-08-22 18:13:05 +1000

    Mako: update to 1.1.5
    update 1.1.4 (2021-01-15) to 1.1.5 (2021-08-21)
    
    changelog:
    - https://docs.makotemplates.org/en/latest/changelog.html
    
    bug:tests:
    - Fixed some issues with running the test suite which would be revealed by running tests in random order.

--- packages/python/devel/Mako/package.mk
+++ packages/python/devel/Mako/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="Mako"
-PKG_VERSION="1.1.4"
-PKG_SHA256="17831f0b7087c313c0ffae2bcbbd3c1d5ba9eeac9c38f2eb7b50e8c99fe9d5ab"
+PKG_VERSION="1.1.5"
+PKG_SHA256="169fa52af22a91900d852e937400e79f535496191c63712e3b9fda5a9bed6fc3"
 PKG_LICENSE="GPL"
 PKG_SITE="https://pypi.org/project/Mako"
 PKG_URL="https://files.pythonhosted.org/packages/source/${PKG_NAME:0:1}/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
