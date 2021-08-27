497be543ed9fcdf2831b3177f5962486703da942
Ntemis <ierokipides.dem@gmail.com>
2021-08-27 22:20:59 +0200

    Add dosbox-pure

---
+++ packages/libretro/dosbox-pure/package.mk
@@ -0,0 +1,23 @@
+PKG_NAME="dosbox-pure"
+PKG_VERSION="abd96ad"
+PKG_ARCH="any"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/libretro/dosbox-pure"
+PKG_URL="$PKG_SITE.git"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="libretro"
+PKG_LONGDESC="DOSBox Pure is a fork of DOSBox, an emulator for DOS games, built for RetroArch/Libretro aiming for simplicity and ease of use."
+PKG_BUILD_FLAGS="-lto"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+make_target() {
+  make -f Makefile
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/libretro
+  cp dosbox_pure_libretro.so $INSTALL/usr/lib/libretro/
+}
