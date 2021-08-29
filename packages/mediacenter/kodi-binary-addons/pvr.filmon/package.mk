6d4ceaf38136d0372672921b7c3876fab5aa3e3e
heitbaum <rudi@heitbaum.com>
2021-08-29 15:18:41 +1000

    kodi-binary-addons: update to latest versions
    - audiodecoder.organya: update 3.0.0-Matrix to 19.0.0-Matrix
    - imagedecoder.heif: update 1.4.0-Matrix to 19.0.0-Matrix
    - imagedecoder.mpo: update 2.0.1-Matrix to 19.0.0-Matrix
    - pvr.filmon: update 6.1.2-Matrix to 6.1.3-Matrix
    - pvr.iptvsimple: update 7.6.9-Matrix to 7.6.10-Matrix
    - pvr.vuplus: update 7.4.9-Matrix to 7.4.11-Matrix

--- packages/mediacenter/kodi-binary-addons/pvr.filmon/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.filmon/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pvr.filmon"
-PKG_VERSION="6.1.2-Matrix"
-PKG_SHA256="3ede4930bce246f15cdc4a063086327c28c412b923d0c1030e2fd3ba8a63e138"
+PKG_VERSION="6.1.3-Matrix"
+PKG_SHA256="e8cf22398e2ab2da9873dc84a3fa5fcc27c7726adfedd813ebdd9b8d41066fec"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
