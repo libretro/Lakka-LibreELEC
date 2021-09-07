a95fde183ea7d246569d0fc72199bc117f76b095
Ian Leonard <antonlacon@gmail.com>
2021-09-07 04:49:17 +0000

    openssl: update to 1.1.1l
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- packages/security/openssl/package.mk
+++ packages/security/openssl/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="openssl"
-PKG_VERSION="1.1.1k"
-PKG_SHA256="892a0875b9872acd04a9fde79b1f943075d5ea162415de3047c327df33fbaee5"
+PKG_VERSION="1.1.1l"
+PKG_SHA256="0b7a3e5e59c34827fe0c3a74b7ec8baef302b98fa80088d7f9153aa16fa76bd1"
 PKG_LICENSE="BSD"
 PKG_SITE="https://www.openssl.org"
 PKG_URL="https://www.openssl.org/source/${PKG_NAME}-${PKG_VERSION}.tar.gz"
