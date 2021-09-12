b2881a1cb8328399302518802d3ecc8facdf30b2
heitbaum <rudi@heitbaum.com>
2021-09-12 11:56:35 +1000

    libsndfile: enable external-libs (FLAC, Ogg, Opus and Vorbis)
    create the pkg-config sndfile.pc used by fluidsynth and pulseaudio

--- packages/audio/libsndfile/package.mk
+++ packages/audio/libsndfile/package.mk
@@ -8,13 +8,20 @@ PKG_SHA256="8cdee0acb06bb0a3c1a6ca524575643df8b1f3a55a0893b4dd9f829d08263785"
 PKG_LICENSE="LGPL-2.1-or-later"
 PKG_SITE="https://libsndfile.github.io/libsndfile/"
 PKG_URL="https://github.com/libsndfile/libsndfile/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain alsa-lib"
+PKG_DEPENDS_TARGET="toolchain alsa-lib flac libogg libvorbis opus"
 PKG_LONGDESC="A C library for reading and writing sound files containing sampled audio data."
 PKG_BUILD_FLAGS="+pic"
 
+# As per notes in configure.ac:
+#  One or more of the external libraries (ie libflac, libogg, libvorbis and libopus)
+#  is either missing ... Unfortunately, for ease of maintenance, the external libs
+#  are an all or nothing affair.
+# So all of flac, libogg, libvorbis, opus are required.
+
 PKG_CMAKE_OPTS_TARGET="-DBUILD_PROGRAMS=OFF \
                        -DBUILD_EXAMPLES=OFF \
-                       -DBUILD_TESTING=OFF \
                        -DBUILD_REGTEST=OFF \
-                       -DENABLE_EXTERNAL_LIBS=OFF \
-                       -DINSTALL_MANPAGES=OFF"
+                       -DBUILD_TESTING=OFF \
+                       -DENABLE_EXTERNAL_LIBS=ON \
+                       -DINSTALL_MANPAGES=OFF \
+                       -DINSTALL_PKGCONFIG_MODULE=ON"
