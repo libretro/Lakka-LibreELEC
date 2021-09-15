a06ac723785adbccccae2c237a80998843b28805
heitbaum <rudi@heitbaum.com>
2021-09-15 19:54:30 +1000

    kodi-binary-addons: update to latest versions
    - inputstream.ffmpegdirect: update 1.21.8-Matrix to 19.0.0-Matrix
    - inputstream.rtmp: update 3.4.0-Matrix to 19.0.0-Matrix
    - peripheral.xarcade: update 1.2.0-Matrix to 19.0.0-Matrix
    - pvr.demo: update 7.1.5-Matrix to 19.0.0-Matrix
    - pvr.mythtv: update 7.3.1-Matrix to 7.3.4-Matrix
    - visualization.projectm: update 3.3.0-Matrix to 19.0.0-Matrix
    - visualization.shadertoy: update 19.0.1-Matrix to 19.1.1-Matrix

--- packages/mediacenter/kodi-binary-addons/pvr.demo/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.demo/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pvr.demo"
-PKG_VERSION="7.1.5-Matrix"
-PKG_SHA256="a8b810dc8186b4b014eca2970a35e31e46ef38fbb77525fe6c013d8b65ab8d0a"
+PKG_VERSION="19.0.0-Matrix"
+PKG_SHA256="a95f81d83da6cd0720270736424fc99ed07fef1ecdd5822a16c64b4097cc8f71"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
