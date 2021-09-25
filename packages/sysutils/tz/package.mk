e5685c5d4dde599680f935b847af644285601b07
heitbaum <rudi@heitbaum.com>
2021-09-26 07:33:15 +1000

    tz: update to 2021c
    ann:
    - http://mm.icann.org/pipermail/tz-announce/2021-September/000066.html
    - http://mm.icann.org/pipermail/tz-announce/2021-October/000067.html

--- packages/sysutils/tz/package.mk
+++ packages/sysutils/tz/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="tz"
-PKG_VERSION="2021a"
-PKG_SHA256="bd7ecd99cbb0a2f15bb7e38be2cbc04dced89922fce4ac0ffcd1ca844ba9362f"
+PKG_VERSION="2021c"
+PKG_SHA256="43e657d877c73bd130dc3ddba83f64d654e8269d38f731b13d5cb23b79befeb1"
 PKG_LICENSE="Public Domain"
 PKG_SITE="http://www.iana.org/time-zones"
 PKG_URL="https://github.com/eggert/tz/archive/${PKG_VERSION}.tar.gz"
