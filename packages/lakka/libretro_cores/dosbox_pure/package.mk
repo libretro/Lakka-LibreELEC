aad4dc8d4a19dbe582b70edd6c091d9489b6b306
Ntemis <ierokipides.dem@gmail.com>
2021-09-29 21:27:45 +0200

    dosbox_pure: new libretro core
    port of
    https://github.com/libretro/Lakka-LibreELEC/commit/497be543ed9fcdf2831b3177f5962486703da942
    part 1/2

---
+++ packages/lakka/libretro_cores/dosbox_pure/package.mk
@@ -0,0 +1,14 @@
+PKG_NAME="dosbox_pure"
+PKG_VERSION="4d70760"
+PKG_ARCH="any"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/libretro/dosbox-pure"
+PKG_URL="${PKG_SITE}.git"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="DOSBox Pure is a fork of DOSBox, an emulator for DOS games, built for RetroArch/Libretro aiming for simplicity and ease of use."
+PKG_TOOLCHAIN="make"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+    cp -v dosbox_pure_libretro.so ${INSTALL}/usr/lib/libretro/
+}
