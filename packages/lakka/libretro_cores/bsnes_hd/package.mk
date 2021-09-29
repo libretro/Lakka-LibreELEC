949918bd00ae0729c9dcce5c0bfcc4933f292534
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2021-09-29 08:56:30 +0200

    bsnes_hd: add new libretro core

---
+++ packages/lakka/libretro_cores/bsnes_hd/package.mk
@@ -0,0 +1,19 @@
+PKG_NAME="bsnes_hd"
+PKG_VERSION="0fd18e0"
+PKG_ARCH="any"
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://github.com/DerKoun/bsnes-hd"
+PKG_URL="${PKG_SITE}.git"
+PKG_BRANCH="master"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Fork of bsnes that adds HD video features."
+PKG_TOOLCHAIN="make"
+
+make_target() {
+  make -C bsnes -f GNUmakefile target="libretro" compiler="${CXX}" CXXFLAGS="${CXXFLAGS}" platform=linux local=false binary=library
+}
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+    cp bsnes/out/bsnes_hd_beta_libretro.so ${INSTALL}/usr/lib/libretro/
+}
