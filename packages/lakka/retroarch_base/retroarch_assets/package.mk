c9f17af4a2820f09f942db14941707480f89669d
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2021-09-29 09:57:23 +0200

    retroarch_assets: add RetroArch fonts to path
    Co-authored-by: gouchi <gouchi@free.fr>
    Co-authored-by: Tomas Kelemen <vudiq@vudiq.sk>

--- packages/lakka/retroarch_base/retroarch_assets/package.mk
+++ packages/lakka/retroarch_base/retroarch_assets/package.mk
@@ -3,10 +3,15 @@ PKG_VERSION="8d114dd"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/libretro/retroarch-assets"
 PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_DEPENDS_TARGET="toolchain fontconfig"
 PKG_SHORTDESC="RetroArch assets. Background and icon themes for the menu drivers."
 PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   make -C ${PKG_BUILD} install INSTALLDIR="${INSTALL}/usr/share/retroarch-assets"
+}
+
+post_makeinstall_target() {
+  mkdir -p ${INSTALL}/etc/fonts/conf.d
+    cp -v ${PKG_DIR}/conf.d/*.conf ${INSTALL}/etc/fonts/conf.d
 }
