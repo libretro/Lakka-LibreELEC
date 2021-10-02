78c6e415b41fd47207712932c4c848a1a762270a
Ian Leonard <antonlacon@gmail.com>
2021-10-02 18:52:18 +0000

    libfmt: update to 8.0.1
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- packages/devel/libfmt/package.mk
+++ packages/devel/libfmt/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libfmt"
-PKG_VERSION="7.1.3"
-PKG_SHA256="5cae7072042b3043e12d53d50ef404bbb76949dad1de368d7f993a15c8c05ecc"
+PKG_VERSION="8.0.1"
+PKG_SHA256="b06ca3130158c625848f3fb7418f235155a4d389b2abc3a6245fb01cb0eb1e01"
 PKG_LICENSE="BSD"
 PKG_SITE="https://github.com/fmtlib/fmt"
 PKG_URL="https://github.com/fmtlib/fmt/archive/${PKG_VERSION}.tar.gz"
