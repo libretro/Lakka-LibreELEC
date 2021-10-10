6713cb97a92fe38840264207c4faa352e3bca509
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2021-10-10 14:42:19 +0200

    RPi bootloader: remove additions to distroconfig
    these lines are already included in the project's file

--- packages/tools/bcm2835-bootloader/package.mk
+++ packages/tools/bcm2835-bootloader/package.mk
@@ -32,10 +32,6 @@ makeinstall_target() {
     find_file_path config/config.txt ${PKG_DIR}/files/config.txt && cp -PRv ${FOUND_PATH} ${INSTALL}/usr/share/bootloader
 
     if [ "${DISTRO}" = "Lakka" ]; then
-      if [ "${ARCH}" = "aarch64" ]; then
-	echo "arm_64bit=1" >> ${INSTALL}/usr/share/bootloader/distroconfig.txt
-	echo "kernel=${KERNEL_NAME}" >> ${INSTALL}/usr/share/bootloader/distroconfig.txt
-      fi
       echo "disable_splash=1" >> ${INSTALL}/usr/share/bootloader/distroconfig.txt
       echo "dtparam=audio=on" >> ${INSTALL}/usr/share/bootloader/distroconfig.txt
       echo "hdmi_max_pixel_freq:0=200000000" >> ${INSTALL}/usr/share/bootloader/distroconfig.txt
