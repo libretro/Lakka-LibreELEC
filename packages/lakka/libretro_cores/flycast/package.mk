5e6c12784e60307f1b8b9c5c4f8e826774a18418
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2021-10-12 22:32:03 +0200

    flycast: fix compilation for i386, arm and aarch64

--- packages/lakka/libretro_cores/flycast/package.mk
+++ packages/lakka/libretro_cores/flycast/package.mk
@@ -5,16 +5,13 @@ PKG_SITE="https://github.com/libretro/flycast"
 PKG_URL="${PKG_SITE}.git"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Flycast is a multiplatform Sega Dreamcast emulator"
-PKG_LONGDESC="Flycast is a multiplatform Sega Dreamcast emulator"
 PKG_TOOLCHAIN="make"
 
 PKG_MAKE_OPTS_TARGET="HAVE_OPENMP=0 LDFLAGS=-lrt"
 
 if [ "${OPENGL_SUPPORT}" = "yes" ]; then
   PKG_DEPENDS_TARGET+=" ${OPENGL}"
-  if [ "${PROJECT}" = "Generic" ]; then
-    PKG_MAKE_OPTS_TARGET+=" HAVE_OIT=1"
-  fi
+  PKG_MAKE_OPTS_TARGET+=" HAVE_OIT=1"
 fi
 
 if [ "${OPENGLES_SUPPORT}" = "yes" ]; then
@@ -27,22 +24,37 @@ if [ "${VULKAN_SUPPORT}" = "yes" ]; then
   PKG_MAKE_OPTS_TARGET+=" HAVE_VULKAN=1"
 fi
 
-if [ "${ARCH}" = "arm" ]; then
-  if [ "${DEVICE:0:4}" = "RPi4" ]; then
-    PKG_MAKE_OPTS_TARGET+=" platform=rpi4-gles-neon"
-  elif [ "${DEVICE}" = "RPi2" ]; then
-    PKG_MAKE_OPTS_TARGET+=" platform=rpi"
-  elif [ "${DEVICE}" = "OdroidGoAdvance" ]; then
-    PKG_MAKE_OPTS_TARGET+=" platform=classic_armv8_a35"
-  else
-    PKG_MAKE_OPTS_TARGET+=" platform=armv-gles-neon"
-  fi
-elif [ "${DEVICE:0:4}" = "RPi4" -o "${DEVICE}" = "RPi3" ] && [ "${ARCH}" = "aarch64" ]; then
-  PKG_MAKE_OPTS_TARGET+=" platform=rpi4_64-gles-neon"
-fi
+case ${DEVICE:-${PROJECT}} in
+  RPi2)
+    PKG_MAKE_OPTS_TARGET+=" platform=rpi2"
+    ;;
+  RPi3)
+    PKG_MAKE_OPTS_TARGET+=" platform=rpi3_64"
+    ;;
+  RPi4*)
+    PKG_MAKE_OPTS_TARGET+=" platform=rpi4_64"
+    ;;
+  RK3288)
+    PKG_MAKE_OPTS_TARGET+=" platform=RK3288"
+    ;;
+  Switch)
+    PKG_MAKE_OPTS_TARGET+=" platform=jetson-nano"
+    ;;
+  *)
+    if [ "${ARCH}" = "aarch64" ]; then
+      PKG_MAKE_OPTS_TARGET+=" platform=arm64"
+    elif [ "${ARCH}" = "arm" ]; then
+      PKG_MAKE_OPTS_TARGET+=" platform=armv-gles-neon"
+    fi
+    ;;
+esac
 
 pre_make_target() {
-  PKG_MAKE_OPTS_TARGET+=" AS=${AS} CC_AS=${CC} ARCH=${ARCH}"
+  if [ "${ARCH}" = "arm" -o "${ARCH}" = "aarch64" ]; then
+    PKG_MAKE_OPTS_TARGET+=" AS=${AS} CC_AS=${CC}"
+  else
+    PKG_MAKE_OPTS_TARGET+=" AS=${AS} CC_AS=${AS}"
+  fi
 }
 
 makeinstall_target() {
