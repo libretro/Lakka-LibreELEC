b9029645e74ec73c72abd2fd282f6d22199e8ff4
Matthias Reichl <hias@horus.com>
2021-10-19 16:09:37 +0200

    RPi: update to firmware d2ab264
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/graphics/bcm2835-driver/package.mk
+++ packages/graphics/bcm2835-driver/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="bcm2835-driver"
-PKG_VERSION="b5257da58c0e652633592c6fce09a7f3b100a2eb"
-PKG_SHA256="a1a754b0a6b0b5985c414a1b9a4940e7b372e5aaea79e1a32cf875e80e343932"
+PKG_VERSION="d2ab264b688a223e23029ffbba86820c8c6cb329"
+PKG_SHA256="d9b0fde734f4c22a960aea3c65f5b474a59c145d3160fee29b6e3fabe5b205de"
 PKG_LICENSE="nonfree"
 PKG_SITE="http://www.broadcom.com"
 PKG_URL="${DISTRO_SRC}/${PKG_NAME}-${PKG_VERSION}.tar.xz"
