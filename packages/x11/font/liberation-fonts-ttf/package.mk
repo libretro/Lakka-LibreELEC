9b9bb84559f4f9a5d7690df276816095d6229621
SupervisedThinking <supervisedthinking@gmail.com>
2021-10-19 18:30:04 +0200

    liberation-fonts-ttf: update pkg license identifier according to SPDX

--- packages/x11/font/liberation-fonts-ttf/package.mk
+++ packages/x11/font/liberation-fonts-ttf/package.mk
@@ -5,7 +5,7 @@
 PKG_NAME="liberation-fonts-ttf"
 PKG_VERSION="2.1.5"
 PKG_SHA256="7191c669bf38899f73a2094ed00f7b800553364f90e2637010a69c0e268f25d0"
-PKG_LICENSE="OFL1_1"
+PKG_LICENSE="OFL-1.1"
 PKG_SITE="https://github.com/liberationfonts/liberation-fonts"
 PKG_URL="https://github.com/liberationfonts/liberation-fonts/files/7261482/${PKG_NAME}-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain util-macros"
