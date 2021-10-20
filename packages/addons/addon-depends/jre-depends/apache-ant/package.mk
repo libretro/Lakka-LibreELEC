ba1e003bc2fa0898e7ff616b817e1cc47072e9ad
heitbaum <rudi@heitbaum.com>
2021-10-20 19:53:03 +1100

    apache-ant: update to 1.10.12

--- packages/addons/addon-depends/jre-depends/apache-ant/package.mk
+++ packages/addons/addon-depends/jre-depends/apache-ant/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="apache-ant"
-PKG_VERSION="1.10.11"
-PKG_SHA256="baa049855cdecbefa62539555824058e52412e5ebe8f102e1db944cb762e06d9"
+PKG_VERSION="1.10.12"
+PKG_SHA256="6115c940367e50755cf806de5816f20a1db1321b1cb734e2c34ab20ef6682b9b"
 PKG_LICENSE="Apache License 2.0"
 PKG_SITE="https://ant.apache.org/"
 PKG_URL="https://downloads.apache.org/ant/binaries/${PKG_NAME}-${PKG_VERSION}-bin.tar.xz"
