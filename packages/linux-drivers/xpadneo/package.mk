188e9700f7b38583372232cc7a3437825fa2cb2e
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2021-10-24 08:44:51 +0200

    xpadneo: remove disable_ertm
    as it causes connection issues with different gamepads, it is safer to
    remove it and the user can add this on need basis via
    
    ```
    echo options bluetooth disable_ertm=1 > /storage/.config/modprobe.d/disable_ertm.conf
    ```
    
    fixes #1528

--- packages/linux-drivers/xpadneo/package.mk
+++ packages/linux-drivers/xpadneo/package.mk
@@ -28,5 +28,4 @@ makeinstall_target() {
   mkdir -p $INSTALL/usr/lib/modprobe.d
     cp -v $PKG_BUILD/hid-xpadneo/etc-modprobe.d/*.conf $INSTALL/usr/lib/modprobe.d/
     echo "options hid_xpadneo trigger_rumble_mode=2" >> $INSTALL/usr/lib/modprobe.d/xpadneo.conf
-    echo "options bluetooth disable_ertm=1" >> $INSTALL/usr/lib/modprobe.d/xpadneo.conf
 }
