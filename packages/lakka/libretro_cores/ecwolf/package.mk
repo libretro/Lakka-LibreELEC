82badd695c2cce978bb419882c218af79361aa84
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2021-10-25 09:25:30 +0200

    ecwolf: add libretro core

---
+++ packages/lakka/libretro_cores/ecwolf/package.mk
@@ -0,0 +1,19 @@
+PKG_NAME="ecwolf"
+PKG_VERSION="30c4192"
+PKG_LICENSE="Unknown"
+PKG_SITE="https://github.com/libretro/ecwolf"
+PKG_URL="${PKG_SITE}.git"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SHORTDESC="ECWolf is a port of the Wolfenstein 3D engine based of Wolf4SDL."
+PKG_TOOLCHAIN="make"
+
+PKG_MAKE_OPTS_TARGET="-C src/libretro"
+
+pre_make_target() {
+  cd ${PKG_BUILD}
+}
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+    cp src/libretro/ecwolf_libretro.so ${INSTALL}/usr/lib/libretro/
+}
