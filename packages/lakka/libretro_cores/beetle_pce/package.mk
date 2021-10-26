351d7926b1fd1f6b363d34f4de4cacd358190ec9
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2021-10-26 12:48:44 +0200

    beetle_pce: add new libretro core (non-fast version of beetle_pce)
    original package `beetle_pce` renamed to `beetle_pce_fast`

--- packages/lakka/libretro_cores/beetle_pce/package.mk
+++ packages/lakka/libretro_cores/beetle_pce/package.mk
@@ -1,15 +1,15 @@
 PKG_NAME="beetle_pce"
-PKG_VERSION="81d4c9d"
+PKG_VERSION="b04ae2f"
 PKG_LICENSE="GPLv2"
-PKG_SITE="https://github.com/libretro/beetle-pce-fast-libretro"
+PKG_SITE="https://github.com/libretro/beetle-pce-libretro"
 PKG_URL="${PKG_SITE}.git"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="Standalone port of Mednafen PCE Fast to libretro."
+PKG_LONGDESC="Standalone port of Mednafen PCE to libretro."
 PKG_TOOLCHAIN="make"
 
 PKG_MAKE_OPTS_TARGET="HAVE_CDROM=1"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v mednafen_pce_fast_libretro.so ${INSTALL}/usr/lib/libretro/
+    cp -v mednafen_pce_libretro.so ${INSTALL}/usr/lib/libretro/
 }
---
+++ packages/lakka/libretro_cores/beetle_pce_fast/package.mk
@@ -0,0 +1,15 @@
+PKG_NAME="beetle_pce_fast"
+PKG_VERSION="81d4c9d"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/libretro/beetle-pce-fast-libretro"
+PKG_URL="${PKG_SITE}.git"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Standalone port of Mednafen PCE Fast to libretro."
+PKG_TOOLCHAIN="make"
+
+PKG_MAKE_OPTS_TARGET="HAVE_CDROM=1"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+    cp -v mednafen_pce_fast_libretro.so ${INSTALL}/usr/lib/libretro/
+}
