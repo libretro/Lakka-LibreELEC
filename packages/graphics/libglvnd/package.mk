21fb7ccc64ca36e691b52875667a2684ef2e2c83
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2021-10-28 13:07:59 +0200

    L4T: use upstream libglvnd package

--- packages/graphics/libglvnd/package.mk
+++ packages/graphics/libglvnd/package.mk
@@ -10,20 +10,31 @@ PKG_URL="https://github.com/NVIDIA/libglvnd/archive/v${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain libX11 libXext xorgproto"
 PKG_LONGDESC="libglvnd is a vendor-neutral dispatch layer for arbitrating OpenGL API calls between multiple vendors."
 
-if [ "${OPENGLES_SUPPORT}" = "no" ]; then
-  PKG_MESON_OPTS_TARGET="-Dgles1=false \
-                         -Dgles2=false"
+if [ ! "${PROJECT}" = "L4T" ]; then
+  if [ "${OPENGLES_SUPPORT}" = "no" ]; then
+    PKG_MESON_OPTS_TARGET="-Dgles1=false \
+                           -Dgles2=false"
+  fi
+else
+  PKG_MESON_OPTS_TARGET="-Dheaders=true \
+                         -Dx11=enabled \
+                         -Degl=true \
+                         -Dglx=enabled \
+                         -Dgles1=true \
+                         -Dgles2=true"
 fi
 
 post_makeinstall_target() {
-  if [ "${DISPLAYSERVER}" = "x11" ]; then
-    # Remove old symlinks to GLVND libGL.so.1.7.0
-    safe_remove              ${INSTALL}/usr/lib/libGL.so
-    safe_remove              ${INSTALL}/usr/lib/libGL.so.1
-    # Create new symlinks to /var/lib/libGL.so
-    ln -sf libGL.so.1        ${INSTALL}/usr/lib/libGL.so
-    ln -sf /var/lib/libGL.so ${INSTALL}/usr/lib/libGL.so.1
-    # Create new symlink to GLVND libGL.so.1.7.0
-    ln -sf libGL.so.1.7.0    ${INSTALL}/usr/lib/libGL_glvnd.so.1
+  if [ ! "${PROJECT}" = "L4T" ]; then
+    if [ "${DISPLAYSERVER}" = "x11" ]; then
+      # Remove old symlinks to GLVND libGL.so.1.7.0
+      safe_remove              ${INSTALL}/usr/lib/libGL.so
+      safe_remove              ${INSTALL}/usr/lib/libGL.so.1
+      # Create new symlinks to /var/lib/libGL.so
+      ln -sf libGL.so.1        ${INSTALL}/usr/lib/libGL.so
+      ln -sf /var/lib/libGL.so ${INSTALL}/usr/lib/libGL.so.1
+      # Create new symlink to GLVND libGL.so.1.7.0
+      ln -sf libGL.so.1.7.0    ${INSTALL}/usr/lib/libGL_glvnd.so.1
+    fi
   fi
 }
