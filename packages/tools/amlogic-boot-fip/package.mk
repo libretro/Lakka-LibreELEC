7811237198115f21a28cd7fc65c30a198516d6ab
Christian Hewitt <christianshewitt@gmail.com>
2021-10-29 12:06:34 +0000

    amlogic-boot-fip: bump to use Makefile changes

--- packages/tools/amlogic-boot-fip/package.mk
+++ packages/tools/amlogic-boot-fip/package.mk
@@ -3,8 +3,8 @@
 
 PKG_NAME="amlogic-boot-fip"
 PKG_LICENSE="nonfree"
-PKG_VERSION="ac20772f44b2b74c8f55331b5c91a277d0bfbc37"
-PKG_SHA256="4bc92f046e960b5760ace3d93536eb26422484225b34238631de9b8d819a92aa"
+PKG_VERSION="7ff0004e0e4d261ba81334a2f46302bd06704aca"
+PKG_SHA256="bd8012388cca6a01e819c3fcce2522ca6a32f7287ac2b901efbbe90978dfe74d"
 PKG_SITE="https://github.com/LibreELEC/amlogic-boot-fip"
 PKG_URL="https://github.com/LibreELEC/amlogic-boot-fip/archive/${PKG_VERSION}.tar.gz"
 PKG_LONGDESC="Firmware Image Package (FIP) sources used to sign Amlogic u-boot binaries in LibreELEC images"
