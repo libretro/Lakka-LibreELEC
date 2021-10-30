41b720b7628a80db40c14006d80853366df13199
Matthias Reichl <hias@horus.com>
2021-10-30 17:06:50 +0200

    u-boot-tools: add missing deps and host-cc options
    u-boot requires openssl includes and libs on host, add the missing
    dependencies and set HOSTCC, HOSTCFLAGS and HOSTLDFLAGS so the build
    can pick up openssl from the LE toolchain
    
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/tools/u-boot-tools/package.mk
+++ packages/tools/u-boot-tools/package.mk
@@ -7,12 +7,12 @@ PKG_SHA256="cde723e19262e646f2670d25e5ec4b1b368490de950d4e26275a988c36df0bd4"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.denx.de/wiki/U-Boot"
 PKG_URL="http://ftp.denx.de/pub/u-boot/u-boot-${PKG_VERSION}.tar.bz2"
-PKG_DEPENDS_HOST="ccache:host bison:host flex:host"
+PKG_DEPENDS_HOST="ccache:host bison:host flex:host openssl:host pkg-config:host"
 PKG_LONGDESC="Das U-Boot is a cross-platform bootloader for embedded systems."
 
 make_host() {
   make qemu-x86_64_defconfig
-  make tools-only
+  make tools-only HOSTCC="${HOST_CC}" HOSTCFLAGS="-I${TOOLCHAIN}/include" HOSTLDFLAGS="${HOST_LDFLAGS}"
 }
 
 make_target() {
