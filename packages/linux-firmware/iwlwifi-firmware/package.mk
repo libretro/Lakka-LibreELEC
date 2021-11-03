613c18c69a6f306a02074ae4c1bcd51f31e7290d
heitbaum <rudi@heitbaum.com>
2021-11-03 19:45:35 +1100

    iwlwifi-firmware: update to 97df2ba

--- packages/linux-firmware/iwlwifi-firmware/package.mk
+++ packages/linux-firmware/iwlwifi-firmware/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="iwlwifi-firmware"
-PKG_VERSION="be9ab3481819d49126a973a5e8ae732b754e9ef4"
-PKG_SHA256="5437f92a26afd2c996800f0e8e7acf2cf288839fc57a8afe73c0717ff1f4841c"
+PKG_VERSION="97df2badf8a4308f5b55ae0cf4d6b3ebe2fa564d"
+PKG_SHA256="a14dad55a00aff022d4792d03dc904b929c4d32d07860f4db69d400c70ab5d46"
 PKG_LICENSE="Free-to-use"
 PKG_SITE="https://github.com/LibreELEC/iwlwifi-firmware"
 PKG_URL="https://github.com/LibreELEC/iwlwifi-firmware/archive/${PKG_VERSION}.tar.gz"
