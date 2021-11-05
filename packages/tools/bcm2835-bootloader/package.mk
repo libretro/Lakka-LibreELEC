64a17bd3fc908a60925b521aa09007033110ebd2
Matthias Reichl <hias@horus.com>
2021-11-05 20:15:18 +0100

    RPi: update to firmware c8c985a
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/tools/bcm2835-bootloader/package.mk
+++ packages/tools/bcm2835-bootloader/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="bcm2835-bootloader"
-PKG_VERSION="7aba6111f600010390f09b2e97d5076486266079"
-PKG_SHA256="a741385fad4060cdd28d40da31eae0d910ee98af91621a598ffbcfe0a16be3b6"
+PKG_VERSION="c8c985aed7b849deefc77236d9fc550ee7bec15d"
+PKG_SHA256="8cb12872c814fa242957e59a20c31e07d9a7f84099659efbcd59a5ef6d10db9b"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="nonfree"
 PKG_SITE="http://www.broadcom.com"
