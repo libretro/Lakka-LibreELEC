65d67b859351e30e436c4eef16f98ec44b3d259b
heitbaum <rudi@heitbaum.com>
2021-11-06 16:51:37 +1100

    go: update to 1.17.3

--- packages/addons/addon-depends/go/package.mk
+++ packages/addons/addon-depends/go/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="go"
-PKG_VERSION="1.17.2"
-PKG_SHA256="022fe1f533432a305db705448f50f9a3ea369586dc32d07219b1bc7db1e01577"
+PKG_VERSION="1.17.3"
+PKG_SHA256="173005ea42cc068287265320375750de0401c243c906c962d993d69ff24d989d"
 PKG_LICENSE="BSD"
 PKG_SITE="https://golang.org"
 PKG_URL="https://github.com/golang/go/archive/${PKG_NAME}${PKG_VERSION}.tar.gz"
