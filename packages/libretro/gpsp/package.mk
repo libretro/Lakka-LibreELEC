77c9a9577ea2c20f43bc0d83091fa394c21d0758
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2021-11-08 14:55:57 +0100

    gpsp: fix if -> elif

--- packages/libretro/gpsp/package.mk
+++ packages/libretro/gpsp/package.mk
@@ -36,7 +36,7 @@ PKG_AUTORECONF="no"
 make_target() {
   if [ "$ARCH" = "arm" ]; then
     make CC=$CC platform=armv
-  if [ "$ARCH" = "aarch64" ]; then
+  elif [ "$ARCH" = "aarch64" ]; then
     make CC=$CC HAVE_DYNAREC=0
   else
     make CC=$CC
