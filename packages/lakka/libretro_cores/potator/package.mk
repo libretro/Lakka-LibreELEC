a5d7a3637d203a841c4ecd1cde251725a9d9ed82
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2021-11-08 18:48:11 +0100

    potator: add libretro core (Watara Supervision)

---
+++ packages/lakka/libretro_cores/potator/package.mk
@@ -0,0 +1,15 @@
+PKG_NAME="potator"
+PKG_VERSION="3e3bfd7"
+PKG_LICENSE="GPL3"
+PKG_SITE="https://github.com/libretro/potator"
+PKG_URL="${PKG_SITE}.git"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="A Watara Supervision Emulator based on Normmatt version"
+PKG_TOOLCHAIN="make"
+
+PKG_MAKE_OPTS_TARGET="-C platform/libretro"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+    cp -v platform/libretro/potator_libretro.so ${INSTALL}/usr/lib/libretro/
+}
