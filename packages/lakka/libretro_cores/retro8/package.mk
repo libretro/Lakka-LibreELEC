48f7dc4f5048c5498b17dc8de72456efb14638be
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2021-11-08 18:52:49 +0100

    retro8: add libretro core (PICO-8)

---
+++ packages/lakka/libretro_cores/retro8/package.mk
@@ -0,0 +1,15 @@
+PKG_NAME="retro8"
+PKG_VERSION="e5eb738"
+PKG_LICENSE="GPL3"
+PKG_SITE="https://github.com/libretro/retro8"
+PKG_URL="${PKG_SITE}.git"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="PICO-8 implementation"
+PKG_TOOLCHAIN="make"
+
+PKG_MAKE_OPTS_TARGET="-C ../"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+    cp -v ../retro8_libretro.so ${INSTALL}/usr/lib/libretro/
+}
