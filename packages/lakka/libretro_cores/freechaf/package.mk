d0762e1f5428b75553db9def7cde59e08b0cb38e
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2021-11-08 18:45:52 +0100

    freechaf: add libretro core (Fairchild ChannelF)

---
+++ packages/lakka/libretro_cores/freechaf/package.mk
@@ -0,0 +1,13 @@
+PKG_NAME="freechaf"
+PKG_VERSION="7275a3e"
+PKG_LICENSE="GPL3"
+PKG_SITE="https://github.com/libretro/FreeChaF"
+PKG_URL="${PKG_SITE}.git"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Fairchild ChannelF Libretro core"
+PKG_TOOLCHAIN="make"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+    cp -v freechaf_libretro.so ${INSTALL}/usr/lib/libretro/
+}
