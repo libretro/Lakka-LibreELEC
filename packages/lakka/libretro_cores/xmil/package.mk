311a65721dff993238ca164f547f0a41b9255164
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2021-11-08 18:54:35 +0100

    xmil: add libretro core (Sharp X1)

---
+++ packages/lakka/libretro_cores/xmil/package.mk
@@ -0,0 +1,15 @@
+PKG_NAME="xmil"
+PKG_VERSION="4b4227b"
+PKG_LICENSE="GPL3"
+PKG_SITE="https://github.com/libretro/xmil-libretro"
+PKG_URL="${PKG_SITE}.git"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Libretro port of X Millennium Sharp X1 emulator"
+PKG_TOOLCHAIN="make"
+
+PKG_MAKE_OPTS_TARGET="-C libretro/ -f Makefile.libretro"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+    cp -v libretro/x1_libretro.so ${INSTALL}/usr/lib/libretro/
+}
