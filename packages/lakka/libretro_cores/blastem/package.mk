7d6789fca08a2f80d0a08d6d82192a6146d585f3
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2021-11-08 18:43:39 +0100

    blastem: add libretro core (Sega Genesis / Mega-Drive)

---
+++ packages/lakka/libretro_cores/blastem/package.mk
@@ -0,0 +1,18 @@
+PKG_NAME="blastem"
+PKG_VERSION="0786858"
+PKG_ARCH="x86_64 i386"
+PKG_LICENSE="GPL3"
+PKG_SITE="https://github.com/libretro/blastem"
+PKG_URL="${PKG_SITE}.git"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="A github mirror for BlastEm - The fast and accurate Genesis emulator"
+PKG_TOOLCHAIN="make"
+
+PKG_MAKE_OPTS_TARGET="-f Makefile.libretro"
+
+[ "${ARCH}" = "i386" ] && PKG_MAKE_OPTS_TARGET+=" ARCH=x86" || true
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+   cp -v blastem_libretro.so ${INSTALL}/usr/lib/libretro/
+}
