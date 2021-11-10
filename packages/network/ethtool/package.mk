7104aa63d7869191aed3cfb7c9bec22ba5a46947
heitbaum <rudi@heitbaum.com>
2021-11-10 19:00:53 +1100

    ethtool: update to 5.15

--- packages/network/ethtool/package.mk
+++ packages/network/ethtool/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="ethtool"
-PKG_VERSION="5.14"
-PKG_SHA256="bb13db91915cacd7a492b65b65df07a67e4b974ddbeaf76205b1945a23d27686"
+PKG_VERSION="5.15"
+PKG_SHA256="686fd6110389d49c2a120f00c3cd5dfe43debada8e021e4270d74bbe452a116d"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.kernel.org/pub/software/network/ethtool/"
 PKG_URL="https://www.kernel.org/pub/software/network/ethtool/${PKG_NAME}-${PKG_VERSION}.tar.xz"
