50ea6ab1e55252fa2c7624a23be34469a6f40dce
Ronald Brown <rbrown4014@yahoo.com>
2021-11-15 22:54:20 +0100

    Fix uboot merge issues

--- projects/L4T/devices/Switch/packages/switch-u-boot/package.mk
+++ projects/L4T/devices/Switch/packages/switch-u-boot/package.mk
@@ -1,18 +1,26 @@
-PKG_NAME="switch-u-boot"
-PKG_VERSION="6cdb578a09d6a55a45b01ca9f333e0d13314cd48"
-PKG_GIT_CLONE_BRANCH="linux-norebase"
-<<<<<<< HEAD
-PKG_DEPENDS_TARGET="toolchain gcc-linaro-aarch64-linux-gnu:host gcc-linaro-arm-linux-gnueabi:host Python3 Python3:host swig:host"
-PKG_SITE="https://gitlab.com/switchroot/switch-uboot"
-PKG_URL="${PKG_SITE}.git"
-PKG_CLEAN="switch-coreboot"
-PKG_TOOLCHAIN="make"
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.tv; see the file COPYING.  If not, write to
+#  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
+#  http://www.gnu.org/copyleft/gpl.html
+################################################################################
 
-make_target() {
-  export PATH=${TOOLCHAIN}/lib/gcc-linaro-aarch64-linux-gnu/bin/:${PATH}
-  export PATH=${TOOLCHAIN}/lib/gcc-linaro-arm-linux-gnueabi/bin/:${PATH}
-  OLD_CROSS_COMPILE=${CROSS_COMPILE}
-=======
+PKG_NAME="switch-u-boot"
+PKG_VERSION="6cdb578a"
+PKG_GIT_CLONE_BRANCH="linux-norebase"
 PKG_ARCH="any"
 PKG_DEPENDS_HOST="toolchain Python3:host gcc-linaro-aarch64-linux-gnu:host gcc-linaro-arm-linux-gnueabi:host swig:host"
 PKG_SITE="https://gitlab.com/switchroot/switch-uboot.git"
@@ -28,25 +36,15 @@ make_host() {
   export PATH=$TOOLCHAIN/lib/gcc-linaro-aarch64-linux-gnu/bin/:$PATH
   export PATH=$TOOLCHAIN/lib/gcc-linaro-arm-linux-gnueabi/bin/:$PATH
   OLD_CROSS_COMPILE=$CROSS_COMPILE
->>>>>>> aaaa8166b0... Bootloader cleanups, seperate bootloader stuff for libreelec builds
   export CROSS_COMPILE=aarch64-linux-gnu-
   
   make nintendo-switch_defconfig
   make tools-only
   
-  export CROSS_COMPILE=${OLD_CROSS_COMPILE}
+  export CROSS_COMPILE=$OLD_CROSS_COMPILE
 }
 
-<<<<<<< HEAD
-makeinstall_target() {
-  #mkdir -p ${BUILD}/switch-boot
-    #cp -v ${PKG_BUILD}/u-boot.elf ${BUILD}/switch-boot
-  
-  mkdir -p ${TOOLCHAIN}/bin
-    cp -v tools/mkimage ${TOOLCHAIN}/bin
-=======
 makeinstall_host() {
   mkdir -p ${TOOLCHAIN}/bin
   cp tools/mkimage ${TOOLCHAIN}/bin
->>>>>>> aaaa8166b0... Bootloader cleanups, seperate bootloader stuff for libreelec builds
 }
