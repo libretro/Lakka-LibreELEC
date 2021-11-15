1e0c39e02284c9a953bb15d8da40b28afd69420a
Ronald Brown <rbrown4014@yahoo.com>
2021-11-15 22:51:42 +0100

    switch-bootloader-merge-fixes

--- projects/L4T/devices/Switch/packages/switch-bootloader/package.mk
+++ projects/L4T/devices/Switch/packages/switch-bootloader/package.mk
@@ -1,17 +1,32 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.tv; see the file COPYING.  If not, write to
+#  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
+#  http://www.gnu.org/copyleft/gpl.html
+################################################################################
+
 PKG_NAME="switch-bootloader"
 PKG_VERSION="1.0"
-<<<<<<< HEAD
-PKG_DEPENDS_TARGET="switch-u-boot linux"
-=======
 PKG_ARCH="any"
 PKG_DEPENDS_TARGET="switch-u-boot:host linux cbfstool:host switch-u-boot:host"
->>>>>>> aaaa8166b0... Bootloader cleanups, seperate bootloader stuff for libreelec builds
 #PKG_DEPENDS_TARGET="switch-coreboot linux"
 PKG_TOOLCHAIN="manual"
 
-make_target() {
-  :
-}
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
 
 if [ "${DISTRO}" = "Lakka" ]; then
   DISTRO_PATH="lakka"
