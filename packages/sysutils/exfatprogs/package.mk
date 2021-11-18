6e3df912bd752fc234811e25208810f400daba83
heitbaum <rudi@heitbaum.com>
2021-11-18 20:21:12 +1100

    exfatprogs: update to 1.1.3
    update 1.1.2 to 1.1.3
    
    CHANGES :
    - mkfs.exfat: ensure that the cluster size is greater than or equal than the sector size.
    - mkfs.exfat: replace lseek() + write() with pwrite().
    
    BUG FIXES :
    - mkfs.exfat: prevent an integer overflow when computing the FAT length.
    - fsck.exfat: fix a double free memory error.

--- packages/sysutils/exfatprogs/package.mk
+++ packages/sysutils/exfatprogs/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="exfatprogs"
-PKG_VERSION="1.1.2"
-PKG_SHA256="b95dd447b657eaab05edb444c181b1ef10601fc98bcf5645a4b91f5b4a77f83c"
+PKG_VERSION="1.1.3"
+PKG_SHA256="3dbd74b60eec18d2c68d57be4a872d7baa4f7d848e669e31159452ef5245b809"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/exfatprogs/exfatprogs"
 PKG_URL="https://github.com/exfatprogs/exfatprogs/releases/download/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.xz"
