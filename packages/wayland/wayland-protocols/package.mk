0301040b59d447d6215315f7dcad106638246f79
heitbaum <rudi@heitbaum.com>
2021-11-18 23:52:41 +1100

    wayland-protocols: add dependancy

--- packages/wayland/wayland-protocols/package.mk
+++ packages/wayland/wayland-protocols/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="6c0af1915f96f615927a6270d025bd973ff1c58e521e4ca1fc9abfc914633f76"
 PKG_LICENSE="OSS"
 PKG_SITE="https://wayland.freedesktop.org/"
 PKG_URL="https://wayland.freedesktop.org/releases/${PKG_NAME}-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_DEPENDS_TARGET="toolchain wayland:host"
 PKG_LONGDESC="Specifications of extended Wayland protocols"
 
 PKG_MESON_OPTS_TARGET="-Dtests=false"
