d7e40973417ee069c3b3c7c9bf9e9c7afcc5b546
heitbaum <rudi@heitbaum.com>
2021-11-22 22:23:38 +1100

    fluidsynth: update to 2.2.7

--- packages/audio/fluidsynth/package.mk
+++ packages/audio/fluidsynth/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="fluidsynth"
-PKG_VERSION="2.2.3"
-PKG_SHA256="b31807cb0f88e97f3096e2b378c9815a6acfdc20b0b14f97936d905b536965c4"
+PKG_VERSION="2.2.7"
+PKG_SHA256="460d86d8d687f567dc4780890b72538c7ff6b2082080ef2f9359d41670a309cf"
 PKG_LICENSE="GPL"
 PKG_SITE="http://fluidsynth.org/"
 PKG_URL="https://github.com/FluidSynth/fluidsynth/archive/v${PKG_VERSION}.tar.gz"
