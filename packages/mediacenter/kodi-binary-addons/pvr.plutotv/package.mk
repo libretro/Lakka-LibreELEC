8be56f1d4957e285c15c4f3c4d8f4f53d290430f
flubshi <flubshi@web.de>
2021-11-30 21:58:01 +0100

    kodi-binary-addons: add pvr.plutotv

---
+++ packages/mediacenter/kodi-binary-addons/pvr.plutotv/package.mk
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="pvr.plutotv"
+PKG_VERSION="20.0.0-Nexus"
+PKG_SHA256="743d214faa202d69c2397168ae6e6e03d3a108dd89c3eacb9b82957743fcb25e"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/kodi-pvr/pvr.plutotv"
+PKG_URL="https://github.com/kodi-pvr/pvr.plutotv/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform rapidjson"
+PKG_SECTION=""
+PKG_SHORTDESC="pvr.plutotv"
+PKG_LONGDESC="pvr.plutotv"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="xbmc.pvrclient"
