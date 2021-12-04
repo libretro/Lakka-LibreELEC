0e03848a8ab1adec68627b57a109fdb7cdcf2802
heitbaum <rudi@heitbaum.com>
2021-12-04 09:53:37 +0000

    pycryptodome: update to 3.12.0

--- packages/python/security/pycryptodome/package.mk
+++ packages/python/security/pycryptodome/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pycryptodome"
-PKG_VERSION="3.11.0"
-PKG_SHA256="401b147d44eb9cfe075883ea8748bbb5e9cdfcb936a03f4dcbdef6d095f17073"
+PKG_VERSION="3.12.0"
+PKG_SHA256="d08b9799b6c5b9c99219b775066234d73f3007f5dab4189e33a7c3581507ee57"
 PKG_LICENSE="BSD"
 PKG_SITE="https://pypi.org/project/pycryptodome"
 PKG_URL="https://github.com/Legrandin/${PKG_NAME}/archive/v${PKG_VERSION}.tar.gz"
