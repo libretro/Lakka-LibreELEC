353ca78b629e01276cd17a3764b7797c7426a627
heitbaum <rudi@heitbaum.com>
2021-12-13 11:55:04 +0000

    libinput: update to 1.20.0
    ann:
    1.19.3 - https://lists.freedesktop.org/archives/wayland-devel/2021-December/042068.html
    1.20.0 - https://lists.freedesktop.org/archives/wayland-devel/2022-February/042111.html

--- packages/wayland/libinput/package.mk
+++ packages/wayland/libinput/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="libinput"
-PKG_VERSION="1.19.2"
-PKG_SHA256="0fc39f0af3ee1a77c60c34bc45391a4d0879169f7c0f7bbbeb5eef590b98b883"
+PKG_VERSION="1.20.0"
+PKG_SHA256="6c1f97892a7d599f97349e5e7c1239901fe00edcd4f6289f410034d5dc06cc85"
 PKG_LICENSE="GPL"
-PKG_SITE="http://www.freedesktop.org/wiki/Software/libinput/"
-PKG_URL="http://www.freedesktop.org/software/libinput/${PKG_NAME}-${PKG_VERSION}.tar.xz"
+PKG_SITE="https://www.freedesktop.org/wiki/Software/libinput/"
+PKG_URL="https://gitlab.freedesktop.org/libinput/libinput/-/archive/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain systemd libevdev mtdev"
 PKG_LONGDESC="libinput is a library to handle input devices in Wayland compositors and to provide a generic X.Org input driver."
 
