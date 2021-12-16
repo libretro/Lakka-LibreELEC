3a3718221541b2a7ada7518c0e958e3da560df0c
Ian Leonard <antonlacon@gmail.com>
2021-12-16 02:39:43 +0000

    sqlite: update to 3.37.0
    Signed-off-by: Ian Leonard <antonlacon@gmail.com>

--- packages/databases/sqlite/package.mk
+++ packages/databases/sqlite/package.mk
@@ -3,9 +3,9 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="sqlite"
-PKG_VERSION="3.35.5"
+PKG_VERSION="3.37.0"
 PKG_VERSION_SQLITE="${PKG_VERSION/./}00"
-PKG_SHA256="f52b72a5c319c3e516ed7a92e123139a6e87af08a2dc43d7757724f6132e6db0"
+PKG_SHA256="731a4651d4d4b36fc7d21db586b2de4dd00af31fd54fb5a9a4b7f492057479f7"
 PKG_LICENSE="PublicDomain"
 PKG_SITE="https://www.sqlite.org/"
 PKG_URL="https://www.sqlite.org/2021/${PKG_NAME}-autoconf-${PKG_VERSION_SQLITE/./0}.tar.gz"
