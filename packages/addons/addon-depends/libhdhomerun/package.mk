d2d9fa3d0e43cdcaa561d2d5441e99437672183b
CvH <namerp@googlemail.com>
2021-12-27 13:13:32 +0100

    libhdhomerun: update to 20210624

--- packages/addons/addon-depends/libhdhomerun/package.mk
+++ packages/addons/addon-depends/libhdhomerun/package.mk
@@ -3,11 +3,11 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libhdhomerun"
-PKG_VERSION="7c54382fb681d03888b469033e50bebaf4ce6bce" # Release 20201023
-PKG_SHA256="d686f88b8dc6089e2cb3829958170d6ba02ecc4f2c5f2cfe3fd37e0dda76ce42"
+PKG_VERSION="20210624"
+PKG_SHA256="deaf463bbcc3eefa72f97199efb6213f7b0e2c8e91f1b3d2cbf52056a8715d15"
 PKG_LICENSE="LGPL"
 PKG_SITE="http://www.silicondust.com"
-PKG_URL="https://github.com/Silicondust/libhdhomerun/archive/${PKG_VERSION}.tar.gz"
+PKG_URL="https://download.silicondust.com/hdhomerun/libhdhomerun_${PKG_VERSION}.tgz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="The library provides functionality to setup the HDHomeRun."
 
