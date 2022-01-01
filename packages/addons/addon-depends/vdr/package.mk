bc5a4bf71a22ebe0f3d164a14d58644822c31300
heitbaum <rudi@heitbaum.com>
2022-01-01 04:46:29 +0000

    vdr: update to 2.4.8

--- packages/addons/addon-depends/vdr/package.mk
+++ packages/addons/addon-depends/vdr/package.mk
@@ -4,8 +4,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="vdr"
-PKG_VERSION="2.4.7"
-PKG_SHA256="8d2b34ea3b1139d1d92826477c4afe8db7c7bf4a0cc67ab34062480aaca443cf"
+PKG_VERSION="2.4.8"
+PKG_SHA256="4dce19d99c0ab04c032ce33e7b27a84c73b004d520c28baef572466f43b066d5"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.tvdr.de"
 PKG_URL="http://git.tvdr.de/?p=vdr.git;a=snapshot;h=refs/tags/${PKG_VERSION};sf=tbz2"
