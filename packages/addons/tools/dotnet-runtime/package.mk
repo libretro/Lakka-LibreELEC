95e1b5e4353df6bf759ac7a1d3fa7a4ceb2ea172
heitbaum <rudi@heitbaum.com>
2022-01-02 02:40:56 +0000

    dotnet-runtime: Drop ASP.NET 5.x and Add ASP.NET 6.x and addon (115)

--- packages/addons/tools/dotnet-runtime/package.mk
+++ packages/addons/tools/dotnet-runtime/package.mk
@@ -2,14 +2,14 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="dotnet-runtime"
-PKG_REV="114"
+PKG_REV="115"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
 PKG_SITE="https://dotnet.microsoft.com/"
-PKG_DEPENDS_TARGET="toolchain icu aspnet5-runtime dotnet3-runtime dotnet5-runtime icu"
+PKG_DEPENDS_TARGET="toolchain icu aspnet6-runtime dotnet3-runtime dotnet6-runtime icu"
 PKG_SECTION="tools"
 PKG_SHORTDESC="ASP.NET Core Runtime"
-PKG_LONGDESC="ASP.NET Core Runtimes ($(get_pkg_version dotnet3-runtime)) and ($(get_pkg_version dotnet5-runtime)) enables you to run existing console/web/server applications."
+PKG_LONGDESC="ASP.NET Core Runtimes ($(get_pkg_version dotnet3-runtime)) and ($(get_pkg_version dotnet6-runtime)) enables you to run existing console/web/server applications."
 PKG_TOOLCHAIN="manual"
 
 PKG_IS_ADDON="yes"
@@ -22,9 +22,9 @@ addon() {
   mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
     cp -r $(get_build_dir dotnet3-runtime)/* \
           ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
-    cp -r $(get_build_dir dotnet5-runtime)/* \
+    cp -r $(get_build_dir dotnet6-runtime)/* \
           ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
-    cp -r $(get_build_dir aspnet5-runtime)/* \
+    cp -r $(get_build_dir aspnet6-runtime)/* \
           ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
 
   mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/lib
