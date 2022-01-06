22947e8dfecd97c921486385720fc812b2174411
Giovanni Cascione <43221199+spleen1981@users.noreply.github.com>
2022-01-06 18:25:59 +0100

    flycast: bump to e61951a (#1573)

--- packages/libretro/flycast/package.mk
+++ packages/libretro/flycast/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="flycast"
-PKG_VERSION="bf87f7b"
+PKG_VERSION="e61951a"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/flyinghead/flycast"
@@ -44,7 +44,7 @@ if [ "$OPENGLES_SUPPORT" = yes ]; then
 fi
 
 PKG_CMAKE_OPTS_TARGET="-DLIBRETRO=ON \
-                        -DUSE_OPENMP=OFF \ 
+                        -DUSE_OPENMP=OFF \
                         -DCMAKE_BUILD_TYPE=Release \
                         --target flycast_libretro"
 
