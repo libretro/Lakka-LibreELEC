48bfca4bebcf45c78648f01af98a8acaceb8d3fe
heitbaum <rudi@heitbaum.com>
2022-01-08 12:22:27 +0000

    libdrm: remove RPATH
    Remove all test programs installed by install-test-programs=true except modetest
    
    Do not "not use" the ninja install and replace this with a simple "cp modetest"
    as ninja strips the unnecessary build rpath during the install.

--- packages/graphics/libdrm/package.mk
+++ packages/graphics/libdrm/package.mk
@@ -23,7 +23,7 @@ PKG_MESON_OPTS_TARGET="-Dlibkms=false \
                        -Dman-pages=false \
                        -Dvalgrind=false \
                        -Dfreedreno-kgsl=false \
-                       -Dinstall-test-programs=false \
+                       -Dinstall-test-programs=true \
                        -Dudev=false"
 
 listcontains "${GRAPHIC_DRIVERS}" "(crocus|i915|iris)" &&
@@ -48,6 +48,14 @@ listcontains "${GRAPHIC_DRIVERS}" "etnaviv" &&
   PKG_MESON_OPTS_TARGET+=" -Detnaviv=true" || PKG_MESON_OPTS_TARGET+=" -Detnaviv=false"
 
 post_makeinstall_target() {
-  mkdir -p ${INSTALL}/usr/bin
-    cp -a ${PKG_BUILD}/.${TARGET_NAME}/tests/modetest/modetest ${INSTALL}/usr/bin/
+  # Remove all test programs installed by install-test-programs=true except modetest
+  # Do not "not use" the ninja install and replace this with a simple "cp modetest"
+  # as ninja strips the unnecessary build rpath during the install.
+  safe_remove ${INSTALL}/usr/bin/amdgpu_stress
+  safe_remove ${INSTALL}/usr/bin/drmdevice
+  safe_remove ${INSTALL}/usr/bin/kms-steal-crtc
+  safe_remove ${INSTALL}/usr/bin/kms-universal-planes
+  safe_remove ${INSTALL}/usr/bin/modeprint
+  safe_remove ${INSTALL}/usr/bin/proptest
+  safe_remove ${INSTALL}/usr/bin/vbltest
 }
