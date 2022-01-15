3189105bf93b0e8f77466d3cc30f53bc8561af93
Giovanni Cascione <ing.cascione@gmail.com>
2022-01-15 12:26:23 +0100

    mame2015: fix aarch64 build error

--- packages/libretro/mame2015/package.mk
+++ packages/libretro/mame2015/package.mk
@@ -58,7 +58,7 @@ make_target() {
       make
       ;;
     *)
-      if [ ${PROJECT} = "RPi" -a ${ARCH} = "aarch64" ]; then
+      if [ ${ARCH} = "aarch64" ]; then
         make
       else
         make platform=armv
