e11ef2509ddf147a457dcef60ce2f7eea11f995f
SupervisedThinking <supervisedthinking@gmail.com>
2022-01-16 00:27:53 +0100

    pango: don't depend on X11 - updated dependencies & build opts

--- packages/addons/addon-depends/chrome-depends/pango/package.mk
+++ packages/addons/addon-depends/chrome-depends/pango/package.mk
@@ -8,11 +8,20 @@ PKG_SHA256="4add05edf51c1fb375a1ccde7498914120e23cb280dd7395b1aeb441f1838a4c"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.pango.org/"
 PKG_URL="https://download.gnome.org/sources/pango/${PKG_VERSION:0:4}/pango-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_TARGET="toolchain cairo freetype fontconfig fribidi glib harfbuzz libX11 libXft"
-PKG_DEPENDS_CONFIG="libXft cairo"
+PKG_DEPENDS_TARGET="toolchain cairo freetype fontconfig fribidi glib json-glib harfbuzz"
+PKG_DEPENDS_CONFIG="cairo"
 PKG_LONGDESC="The Pango library for layout and rendering of internationalized text."
-PKG_TOOLCHAIN="meson"
-PKG_BUILD_FLAGS="-sysroot"
 
-PKG_MESON_OPTS_TARGET="-Dgtk_doc=false \
-                       -Dintrospection=disabled"
+configure_package() {
+  # Build with X11 support
+  if [ ${DISPLAYSERVER} = "x11" ]; then
+    PKG_DEPENDS_TARGET+=" libX11 libXft"
+    PKG_DEPENDS_CONFIG+=" libXft"
+    PKG_BUILD_FLAGS="-sysroot"
+  fi
+}
+
+pre_configure_target() {
+  PKG_MESON_OPTS_TARGET="-Dgtk_doc=false \
+                         -Dintrospection=disabled"
+}
