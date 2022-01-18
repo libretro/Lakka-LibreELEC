839bb334077589858d9e376631cb4697856c0ba8
heitbaum <rudi@heitbaum.com>
2022-01-18 09:40:14 +0000

    expat: update to 2.4.4
    update 2.4.1 (2021-05-23) to 2.4.4 (2022-01-30)
    
    changelog:
    - https://github.com/libexpat/libexpat/blob/R_2_4_4/expat/Changes
    release dates:
    - https://libexpat.github.io/

--- packages/textproc/expat/package.mk
+++ packages/textproc/expat/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="expat"
-PKG_VERSION="2.4.1"
-PKG_SHA256="2f9b6a580b94577b150a7d5617ad4643a4301a6616ff459307df3e225bcfbf40"
+PKG_VERSION="2.4.4"
+PKG_SHA256="14c58c2a0b5b8b31836514dfab41bd191836db7aa7b84ae5c47bc0327a20d64a"
 PKG_LICENSE="OSS"
 PKG_SITE="http://expat.sourceforge.net/"
 PKG_URL="https://github.com/libexpat/libexpat/releases/download/R_${PKG_VERSION//./_}/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
