4fc6973a20008e8fba397fcc7b959a55133fcba2
SupervisedThinking <supervisedthinking@gmail.com>
2022-02-01 12:33:07 +0100

    wayland-protocols: update to 1.25 - https://lists.freedesktop.org/archives/wayland-devel/2022-January/042102.html

--- packages/wayland/wayland-protocols/package.mk
+++ packages/wayland/wayland-protocols/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="wayland-protocols"
-PKG_VERSION="1.24"
-PKG_SHA256="bff0d8cffeeceb35159d6f4aa6bab18c807b80642c9d50f66cba52ecf7338bc2"
+PKG_VERSION="1.25"
+PKG_SHA256="f1ff0f7199d0a0da337217dd8c99979967808dc37731a1e759e822b75b571460"
 PKG_LICENSE="OSS"
 PKG_SITE="https://wayland.freedesktop.org/"
 PKG_URL="https://wayland.freedesktop.org/releases/${PKG_NAME}-${PKG_VERSION}.tar.xz"
