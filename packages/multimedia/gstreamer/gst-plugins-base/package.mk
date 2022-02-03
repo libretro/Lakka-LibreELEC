9bf3ef36ce0782746e8e6aeca986c737657e5063
SupervisedThinking <supervisedthinking@gmail.com>
2022-02-03 11:12:11 +0100

    gst-plugins-base: initial package 1.18.5

---
+++ packages/multimedia/gstreamer/gst-plugins-base/package.mk
@@ -0,0 +1,68 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="gst-plugins-base"
+PKG_VERSION="1.18.5"
+PKG_SHA256="960b7af4585700db0fdd5b843554e11e2564fed9e061f591fae88a7be6446fa3"
+PKG_LICENSE="GPL-2.1-or-later"
+PKG_SITE="https://gstreamer.freedesktop.org/modules/gst-plugins-base.html"
+PKG_URL="https://gstreamer.freedesktop.org/src/gst-plugins-base/${PKG_NAME}-${PKG_VERSION}.tar.xz"
+PKG_DEPENDS_TARGET="toolchain gstreamer"
+PKG_LONGDESC="Base GStreamer plugins and helper libraries"
+PKG_BUILD_FLAGS="-gold"
+
+pre_configure_target() {
+  PKG_MESON_OPTS_TARGET="-Dgl=disabled \
+                         -Dadder=disabled \
+                         -Dapp=disabled \
+                         -Daudioconvert=disabled \
+                         -Daudiomixer=disabled \
+                         -Daudiorate=disabled \
+                         -Daudioresample=disabled \
+                         -Daudiotestsrc=disabled \
+                         -Dcompositor=disabled \
+                         -Dencoding=disabled \
+                         -Dgio=disabled \
+                         -Dgio-typefinder=disabled \
+                         -Doverlaycomposition=disabled \
+                         -Dpbtypes=disabled \
+                         -Dplayback=disabled \
+                         -Drawparse=enabled \
+                         -Dsubparse=enabled \
+                         -Dtcp=disabled \
+                         -Dtypefind=disabled \
+                         -Dvideoconvert=disabled \
+                         -Dvideorate=disabled \
+                         -Dvideoscale=disabled \
+                         -Dvideotestsrc=disabled \
+                         -Dvolume=disabled \
+                         -Dalsa=disabled \
+                         -Dcdparanoia=disabled \
+                         -Dlibvisual=disabled \
+                         -Dogg=disabled \
+                         -Dopus=disabled \
+                         -Dpango=disabled \
+                         -Dtheora=disabled \
+                         -Dtremor=disabled \
+                         -Dvorbis=disabled \
+                         -Dx11=disabled \
+                         -Dxshm=disabled \
+                         -Dxvideo=disabled \
+                         -Dexamples=disabled \
+                         -Dtests=disabled \
+                         -Dtools=disabled \
+                         -Dintrospection=disabled \
+                         -Dnls=disabled \
+                         -Dorc=disabled \
+                         -Dgobject-cast-checks=disabled \
+                         -Dglib-asserts=disabled \
+                         -Dglib-checks=disabled \
+                         -Dpackage-name="gst-plugins-base"
+                         -Dpackage-origin="LibreELEC.tv"
+                         -Ddoc=disabled"
+}
+
+post_makeinstall_target(){
+  # clean up
+  safe_remove ${INSTALL}
+}
