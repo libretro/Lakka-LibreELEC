6dc64ae84a2cbb71b9332c65f7eff103060482d3
smp79 <sergey79@gmail.com>
2022-02-03 19:08:44 +0300

    vdr-addon: update to 119

--- packages/addons/service/vdr-addon/package.mk
+++ packages/addons/service/vdr-addon/package.mk
@@ -4,8 +4,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="vdr-addon"
-PKG_VERSION="2.6.0"
-PKG_REV="118"
+PKG_VERSION="2.6.1"
+PKG_REV="119"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://libreelec.tv"
