aa698919c8a8ebe60837e4c173da92ce310a00b2
heitbaum <rudi@heitbaum.com>
2022-02-04 01:35:34 +0000

    iperf: update to 3.11

--- packages/addons/addon-depends/network-tools-depends/iperf/package.mk
+++ packages/addons/addon-depends/network-tools-depends/iperf/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="iperf"
-PKG_VERSION="3.10.1"
-PKG_SHA256="6a4bb4d5c124b3fa64dfbda469ab16857ad6565310bcaa3dd8cd32f96c2fc473"
+PKG_VERSION="3.11"
+PKG_SHA256="96e909c0d3ab6034c52328c2954fb3934aaff349395c4bc2611dcd50e6b89875"
 PKG_LICENSE="BSD"
 PKG_SITE="http://software.es.net/iperf/"
 PKG_URL="https://github.com/esnet/iperf/archive/${PKG_VERSION}.tar.gz"
