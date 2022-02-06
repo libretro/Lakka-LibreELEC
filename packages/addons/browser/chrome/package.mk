f64ae4f2233eda1d124a5be4173e1f10f60ebcfa
heitbaum <rudi@heitbaum.com>
2022-02-06 11:19:13 +0000

    chrome: update to 103.0.5060.134 and addon (113)
    Signed-off-by: Rudi Heitbaum <rudi@heitbaum.com>

--- packages/addons/browser/chrome/package.mk
+++ packages/addons/browser/chrome/package.mk
@@ -4,8 +4,8 @@
 PKG_NAME="chrome"
 PKG_VERSION="1.0"
 # curl -s http://dl.google.com/linux/chrome/deb/dists/stable/main/binary-amd64/Packages | grep -B 1 Version
-PKG_VERSION_NUMBER="97.0.4692.71"
-PKG_REV="111"
+PKG_VERSION_NUMBER="103.0.5060.134"
+PKG_REV="113"
 PKG_ARCH="x86_64"
 PKG_LICENSE="Custom"
 PKG_SITE="http://www.google.com/chrome"
