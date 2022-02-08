fca77a82fe89bc7f9b325a2885c77ec0204b925c
SupervisedThinking <supervisedthinking@gmail.com>
2022-02-08 23:17:32 +0100

    wlroots: update to 0.15.1

--- packages/wayland/lib/wlroots/package.mk
+++ packages/wayland/lib/wlroots/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="wlroots"
-PKG_VERSION="0.15.0"
-PKG_SHA256="fcb9e9dbbb112f5c527c1c4155994fd97b6b1eb3950316e186e3c293e7a07fc4"
+PKG_VERSION="0.15.1"
+PKG_SHA256="8bb791aed9405abc20253c570de1a3b7af91ad65bee2b60293fbbab27ea62c8d"
 PKG_LICENSE="MIT"
 PKG_SITE="https://gitlab.freedesktop.org/wlroots/wlroots/"
 PKG_URL="https://gitlab.freedesktop.org/wlroots/wlroots/-/archive/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
