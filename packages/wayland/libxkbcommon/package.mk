b3f9ff57199a2590f6086e2bd5698a6e03f30e4e
heitbaum <rudi@heitbaum.com>
2022-02-11 09:45:34 +0000

    libxkbcommon: update to 1.4.0 and HSTS

--- packages/wayland/libxkbcommon/package.mk
+++ packages/wayland/libxkbcommon/package.mk
@@ -3,11 +3,11 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libxkbcommon"
-PKG_VERSION="1.3.1"
-PKG_SHA256="b3c710d27a2630054e1e1399c85b7f330ef03359b460f0c1b3b587fd01fe9234"
+PKG_VERSION="1.4.0"
+PKG_SHA256="106cec5263f9100a7e79b5f7220f889bc78e7d7ffc55d2b6fdb1efefb8024031"
 PKG_LICENSE="MIT"
-PKG_SITE="http://xkbcommon.org"
-PKG_URL="http://xkbcommon.org/download/${PKG_NAME}-${PKG_VERSION}.tar.xz"
+PKG_SITE="https://xkbcommon.org"
+PKG_URL="https://xkbcommon.org/download/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain xkeyboard-config libxml2"
 PKG_LONGDESC="xkbcommon is a library to handle keyboard descriptions."
 
