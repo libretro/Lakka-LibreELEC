21a32e41380f8985682ac14d630d873fe0309cde
Matthias Reichl <hias@horus.com>
2022-02-12 00:59:17 +0100

    libdvdnav: use tag instead of githash
    This fixes kodi hash check error on build
    
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/multimedia/libdvdnav/package.mk
+++ packages/multimedia/libdvdnav/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libdvdnav"
-PKG_VERSION="9277007ce2263b908e9ce3091cc31b3dd87c351c"
-PKG_SHA256="e50db40a823ddc795d1fe5f18db2517fb3e05fe0c4a88abf1578d95d7a1cce63"
+PKG_VERSION="6.0.0-Leia-Alpha-3"
+PKG_SHA256="071e414e61b795f2ff9015b21a85fc009dde967f27780d23092643916538a57a"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/xbmc/libdvdnav"
 PKG_URL="https://github.com/xbmc/libdvdnav/archive/${PKG_VERSION}.tar.gz"
