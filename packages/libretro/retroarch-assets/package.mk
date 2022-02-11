8a997d4bbaffc346938a22fc4f5799e834af9a83
Philippe Simons <simons.philippe@gmail.com>
2022-02-11 13:07:35 +0100

    retroarch-assets: bump git ref to latest

--- packages/libretro/retroarch-assets/package.mk
+++ packages/libretro/retroarch-assets/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch-assets"
-PKG_VERSION="6320719"
+PKG_VERSION="6a6508b"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
