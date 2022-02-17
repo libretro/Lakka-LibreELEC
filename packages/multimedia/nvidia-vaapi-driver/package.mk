518f70531ce3d915f1ab25562095b9d69ce5864e
SupervisedThinking <supervisedthinking@gmail.com>
2022-02-17 15:59:20 +0100

    nvidia-vaapi-driver: update to 0.0.5

--- packages/multimedia/nvidia-vaapi-driver/package.mk
+++ packages/multimedia/nvidia-vaapi-driver/package.mk
@@ -2,10 +2,10 @@
 # Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="nvidia-vaapi-driver"
-PKG_VERSION="0.0.4"
-PKG_SHA256="8b99c618f530ea388964cdb3e8454de2d89f4354de8d0a0ea7941e88576d7e2a"
+PKG_VERSION="0.0.5"
+PKG_SHA256="20b424f41c32e2f49729f85c70d7dcfd28e59a62d845024d9823b7f32faf089d"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/elFarto/nvidia-vaapi-driver"
 PKG_URL="https://github.com/elFarto/nvidia-vaapi-driver/archive/v${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain nv-codec-headers gst-plugins-bad"
+PKG_DEPENDS_TARGET="toolchain libva nv-codec-headers gst-plugins-bad"
 PKG_LONGDESC="A VA-API implemention using NVIDIA's NVDEC"
