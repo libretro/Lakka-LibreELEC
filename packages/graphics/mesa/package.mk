f9d5da2330f6938ae494b9d37130de6cd29efe4f
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2022-02-21 13:38:50 +0100

    update mesa to 21.3.6
    https://docs.mesa3d.org/relnotes/21.3.6.html

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa"
-PKG_VERSION="21.2.5"
-PKG_SHA256="8e49585fb760d973723dab6435d0c86f7849b8305b1e6d99f475138d896bacbb"
+PKG_VERSION="21.3.6"
+PKG_SHA256="96bb761fd546e9aa41d025fcc025225c5668443839dae21e3731959beb096736"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
 PKG_URL="https://mesa.freedesktop.org/archive/mesa-${PKG_VERSION}.tar.xz"
