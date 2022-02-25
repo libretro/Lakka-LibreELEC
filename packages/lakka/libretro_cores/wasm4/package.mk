193c2b6b77158c0787c752e2a0bfd746c0d70468
gouchi <gouchi@free.fr>
2022-02-25 12:46:43 +0100

    wasm4 : Add wasm4 to Libretro cores
    port of #1606

---
+++ packages/lakka/libretro_cores/wasm4/package.mk
@@ -0,0 +1,25 @@
+PKG_NAME="wasm4"
+PKG_VERSION="27aadc4afc4b60c976e59df61f90a0cca1797ecf"
+PKG_LICENSE="ISC"
+PKG_SITE="https://github.com/aduros/wasm4"
+PKG_URL="${PKG_SITE}.git"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="WASM-4 is a low-level fantasy game console for building small games with WebAssembly."
+PKG_TOOLCHAIN="cmake"
+PKG_CMAKE_OPTS_TARGET="-DLIBRETRO=on \
+                       -DCMAKE_BUILD_TYPE=Release"
+
+pre_configure_target() {
+  PKG_CMAKE_SCRIPT="${PKG_BUILD}/runtimes/native/CMakeLists.txt"
+}
+
+pre_make_target() {
+  find ${PKG_BUILD} -name flags.make -exec sed -i "s:isystem :I:g" \{} \;
+  find ${PKG_BUILD} -name build.ninja -exec sed -i "s:isystem :I:g" \{} \;
+}
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+    cp -v wasm4_libretro.so ${INSTALL}/usr/lib/libretro/
+}
+
