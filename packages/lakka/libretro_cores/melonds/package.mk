47918beb10d4f6c15da5e33ca50767c798cf1e59
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2022-03-02 10:57:51 +0100

    melonds: enable for more platforms

--- packages/lakka/libretro_cores/melonds/package.mk
+++ packages/lakka/libretro_cores/melonds/package.mk
@@ -1,6 +1,5 @@
 PKG_NAME="melonds"
 PKG_VERSION="e6e16ed5ab792bd2fe99daae7e13b97ef769ba50"
-PKG_ARCH="x86_64 aarch64"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/melonds"
 PKG_URL="${PKG_SITE}.git"
@@ -13,24 +12,29 @@ PKG_MAKE_OPTS_TARGET="-C ../"
 if [ "${OPENGL_SUPPORT}" = "yes" ]; then
   PKG_DEPENDS_TARGET+=" ${OPENGL}"
   PKG_MAKE_OPTS_TARGET+=" HAVE_OPENGL=1"
-else
-  PKG_MAKE_OPTS_TARGET+=" HAVE_OPENGL=0"
 fi
 
 if [ "${OPENGLES_SUPPORT}" = "yes" ]; then
   PKG_DEPENDS_TARGET+=" ${OPENGLES}"
+  PKG_MAKE_OPTS_TARGET+=" HAVE_OPENGL=1"
 fi
 
 if [ "${VULKAN_SUPPORT}" = "yes" ]; then
   PKG_DEPENDS_TARGET+=" ${VULKAN}"
 fi
 
-if [ "${DEVICE:0:4}" = "RPi4" -a "${ARCH}" = "aarch64" ]; then
-  PKG_MAKE_OPTS_TARGET+=" platform=rpi4_64"
-fi
-
-if [ "${PROJECT}" = "L4T" -a "${L4T_DEVICE_TYPE}" = "t210" ]; then
-  PKG_MAKE_OPTS_TARGET+=" platform=tegra210"
+if [ "${ARCH}" = "aarch64" ]; then
+  PKG_MAKE_OPTS_TARGET+=" ARCH=arm64"
+  if [ "${PROJECT}" = "L4T" -a "${L4T_DEVICE_TYPE}" = "t210" ]; then
+    PKG_MAKE_OPTS_TARGET+=" platform=tegra210"
+  else
+    PKG_MAKE_OPTS_TARGET+=" platform=unix"
+  fi
+elif [ "${ARCH}" = "arm" ]; then
+  PKG_MAKE_OPTS_TARGET+=" platfrom=unix"
+  if target_has_feature neon ; then
+    PKG_MAKE_OPTS_TARGET+=" HAVE_NEON=1"
+  fi
 fi
 
 makeinstall_target() {
