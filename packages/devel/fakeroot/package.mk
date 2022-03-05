7dc00ab0b4a734425eff8186de6543c25ea11219
Rudi Heitbaum <rudi@heitbaum.com>
2022-03-05 11:58:46 +0000

    fakeroot: update to 1.28

--- packages/devel/fakeroot/package.mk
+++ packages/devel/fakeroot/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="fakeroot"
-PKG_VERSION="1.27"
-PKG_SHA256="3c45eb2d1802a2762069e2e9d21bdd6fb533592bc0cda74c9aff066ab01caddc"
+PKG_VERSION="1.28"
+PKG_SHA256="56d405e36ff685f83879be08fdd654255ab9aa38632b4605a98e896ad63990c2"
 PKG_LICENSE="GPL3"
 PKG_SITE="https://tracker.debian.org/pkg/fakeroot"
 PKG_URL="http://ftp.debian.org/debian/pool/main/f/fakeroot/${PKG_NAME}_${PKG_VERSION}.orig.tar.gz"
