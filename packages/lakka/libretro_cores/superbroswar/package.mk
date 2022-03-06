dae2a57ab60847d46268b059d17a1204c313b9bc
Philippe Simons <simons.philippe@gmail.com>
2022-03-06 18:57:16 +0100

    add PKG_TOOLCHAIN

--- packages/lakka/libretro_cores/superbroswar/package.mk
+++ packages/lakka/libretro_cores/superbroswar/package.mk
@@ -6,6 +6,7 @@ PKG_URL="${PKG_SITE}.git"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Super Bros War deathmatch game."
 PKG_MAKE_OPTS_TARGET="-f Makefile.libretro"
+PKG_TOOLCHAIN="make"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
