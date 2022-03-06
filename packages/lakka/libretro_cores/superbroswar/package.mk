719a5a9e2ea822f39861609e975492bd287edd17
Philippe Simons <simons.philippe@gmail.com>
2022-03-06 22:22:04 +0100

    fix compilation (hopefully)

--- packages/lakka/libretro_cores/superbroswar/package.mk
+++ packages/lakka/libretro_cores/superbroswar/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="superbroswar"
-PKG_VERSION="91a029839868d8c4aec925ffc1b7bfc641ae1a8d"
+PKG_VERSION="39a2e7724070589286632309718c5251c43bc679"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/superbroswar-libretro"
 PKG_URL="${PKG_SITE}.git"
