04b76d441f4acde5d6d79ff43888e8311d687262
Philippe Simons <simons.philippe@gmail.com>
2022-03-07 23:08:21 +0100

    core: add Super Bros War (#1625)

---
+++ packages/lakka/libretro_cores/superbroswar/package.mk
@@ -0,0 +1,14 @@
+PKG_NAME="superbroswar"
+PKG_VERSION="39a2e7724070589286632309718c5251c43bc679"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/libretro/superbroswar-libretro"
+PKG_URL="${PKG_SITE}.git"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Super Bros War deathmatch game."
+PKG_MAKE_OPTS_TARGET="-f Makefile.libretro"
+PKG_TOOLCHAIN="make"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+    cp -v superbroswar_libretro.so ${INSTALL}/usr/lib/libretro/
+}
