ed1a003f5082fb0724514f45268e613e3839a84f
Rudi Heitbaum <rudi@heitbaum.com>
2022-03-18 20:25:07 +0000

    at-spi2-core: update to 2.45.1

--- packages/addons/addon-depends/chrome-depends/at-spi2-core/package.mk
+++ packages/addons/addon-depends/chrome-depends/at-spi2-core/package.mk
@@ -3,11 +3,11 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="at-spi2-core"
-PKG_VERSION="2.42.0"
-PKG_SHA256="4b5da10e94fa3c6195f95222438f63a0234b99ef9df772c7640e82baeaa6e386"
+PKG_VERSION="2.45.1"
+PKG_SHA256="ba95f346e93108fbb3462c62437081d582154db279b4052dedc52a706828b192"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.gnome.org/"
-PKG_URL="https://ftp.gnome.org/pub/gnome/sources/at-spi2-core/${PKG_VERSION:0:4}/at-spi2-core-${PKG_VERSION}.tar.xz"
+PKG_URL="https://download.gnome.org/sources/at-spi2-core/${PKG_VERSION:0:4}/at-spi2-core-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain atk dbus glib libXtst"
 PKG_LONGDESC="Protocol definitions and daemon for D-Bus at-spi."
 
