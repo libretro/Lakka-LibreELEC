0847e35ccfbc760c454680a599ed377fad75da9c
Matthias Reichl <hias@horus.com>
2022-03-19 15:14:45 +0100

    rpi-eeprom: update to dd0e086
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/tools/rpi-eeprom/package.mk
+++ packages/tools/rpi-eeprom/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="rpi-eeprom"
-PKG_VERSION="e866f258d97a06f8b6ca8253b9e0e831494e5c86"
-PKG_SHA256="3638a7e22ecb338c5cfeed3dafd7744255eaecce2dd00197c2585f89d03db2a7"
+PKG_VERSION="dd0e086280124e0522111a7d2bf56420d1167844"
+PKG_SHA256="dd0e66f3f608664521756e25f3dbe686dee7776e817f912e600d53b91ccc3bc3"
 PKG_ARCH="arm"
 PKG_LICENSE="BSD-3/custom"
 PKG_SITE="https://github.com/raspberrypi/rpi-eeprom"
