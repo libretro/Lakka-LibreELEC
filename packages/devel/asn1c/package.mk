3303bb456a6fc80ea234d6c3080a8aecf7b4bedb
Rudi Heitbaum <rudi@heitbaum.com>
2022-03-22 02:05:01 +0000

    asn1c: initial package
    asn1c is required for the build of heimdal

---
+++ packages/devel/asn1c/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="asn1c"
+PKG_VERSION="0.9.28"
+PKG_SHA256="8007440b647ef2dd9fb73d931c33ac11764e6afb2437dbe638bb4e5fc82386b9"
+PKG_LICENSE="BSD"
+PKG_SITE="http://lionet.info/asn1c/blog/"
+PKG_URL="https://github.com/vlm/asn1c/releases/download/v${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_HOST="ccache:host"
+PKG_LONGDESC="The ASN.1 Compiler"
