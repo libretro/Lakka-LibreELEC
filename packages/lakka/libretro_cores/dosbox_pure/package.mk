7de9d7787e7d9fd983dad9bfb9ee384057c11fac
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2022-03-22 22:07:56 +0100

    dosbox_pure: buildfix
    - use correct g++
    - add CPUFLAGS
    - optimize with 3 via Makefile

--- packages/lakka/libretro_cores/dosbox_pure/package.mk
+++ packages/lakka/libretro_cores/dosbox_pure/package.mk
@@ -7,6 +7,13 @@ PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="DOSBox Pure is a fork of DOSBox, an emulator for DOS games, built for RetroArch/Libretro aiming for simplicity and ease of use."
 PKG_TOOLCHAIN="make"
 
+make_target() {
+  # remove optimization from CFLAGS, set via Makefile
+  CFLAGS="${CFLAGS//-O3/}"
+  CFLAGS="${CFLAGS//-O2/}"
+  make CXX=${CXX} CPUFLAGS="${CFLAGS}"
+}
+
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
     cp -v dosbox_pure_libretro.so ${INSTALL}/usr/lib/libretro/
