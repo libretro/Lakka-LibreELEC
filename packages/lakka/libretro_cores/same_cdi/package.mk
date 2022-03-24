0378d3befffeb361cfa53e1642c8769758dbc934
Tomáš Kelemen <vudiq@vudiq.sk>
2022-03-24 16:28:57 +0100

    same_cdi: add new libretro core (#1654)
    as it is based on mame current, not including for Pi and Pi Zero

---
+++ packages/lakka/libretro_cores/same_cdi/package.mk
@@ -0,0 +1,42 @@
+PKG_NAME="same_cdi"
+PKG_VERSION="dcc76a5b6d3fb5da9ca29209e1a66bbc1396f83a"
+PKG_LICENSE="MAME"
+PKG_SITE="https://github.com/libretro/same_cdi"
+PKG_URL="${PKG_SITE}.git"
+PKG_DEPENDS_TARGET="toolchain expat zlib flac sqlite"
+PKG_LONGDESC="SAME_CDI is a libretro core to play CD-i games. This is a fork and modification of the MAME libretro core"
+PKG_TOOLCHAIN="make"
+
+PKG_MAKE_OPTS_TARGET="-f Makefile.libretro REGENIE=1 VERBOSE=1 NOWERROR=1 OPENMP=0 CROSS_BUILD=1 TOOLS=0 RETRO=1 PYTHON_EXECUTABLE=python3 CONFIG=libretro LIBRETRO_OS=unix TARGET=mame OSD=retro USE_SYSTEM_LIB_EXPAT=1 USE_SYSTEM_LIB_ZLIB=1 USE_SYSTEM_LIB_FLAC=1 USE_SYSTEM_LIB_SQLITE3=1 LIBRETRO_CPU= ARCH= PROJECT="
+
+case ${ARCH} in
+  x86_64)
+    PKG_MAKE_OPTS_TARGET+=" NOASM=0 PTR64=1 PLATFORM=x86_64"
+    ;;
+  i386)
+    PKG_MAKE_OPTS_TARGET+=" NOASM=0 PTR64=0 PLATFORM=x86"
+    ;;
+  aarch64)
+    PKG_MAKE_OPTS_TARGET+=" NOASM=0 PTR64=0 PLATFORM=arm64"
+    ;;
+  arm)
+    PKG_MAKE_OPTS_TARGET+=" NOASM=1 PTR64=0 PLATFORM=arm"
+    ;;
+esac
+
+pre_make_target() {
+  PKG_MAKE_OPTS_TARGET+=" OVERRIDE_CC=${CC} OVERRIDE_CXX=${CXX} OVERRIDE_LD=${LD}"
+  sed -i scripts/genie.lua \
+      -e 's|-static-libstdc++||g'
+}
+
+make_target() {
+  unset DISTRO
+  [ "${ARCH}" = "aarch64" ] && export ARCHOPTS="-D__aarch64__ -DASMJIT_BUILD_X86"
+  make ${PKG_MAKE_OPTS_TARGET}
+}
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+    cp -v same_cdi_libretro.so ${INSTALL}/usr/lib/libretro/
+}
