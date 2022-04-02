2bd5b8f71b6ea81f93e7a165546cca939ab3d64a
Philippe Simons <simons.philippe@gmail.com>
2022-04-02 20:47:55 +0200

    superbroswar: add libretro core
    backported from Lakka-v4.x branch

---
+++ packages/libretro/superbroswar/package.mk
@@ -0,0 +1,14 @@
+PKG_NAME="superbroswar"
+PKG_VERSION="a8dda0c"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/libretro/superbroswar-libretro"
+PKG_URL="${PKG_SITE}.git"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Super Bros War deathmatch game."
+PKG_MAKE_OPTS_TARGET="-f Makefile.libretro"
+PKG_TOOLCHAIN="make"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+    cp -v superbroswar_libretro.so ${INSTALL}/usr/lib/libretro/
+}
