bc005a1cfb222cd81f88ae96cf6c1e02d6d6161a
Rudi Heitbaum <rudi@heitbaum.com>
2022-04-04 10:17:36 +0000

    libXcursor: update to 1.2.1

--- packages/addons/addon-depends/chrome-depends/libXcursor/package.mk
+++ packages/addons/addon-depends/chrome-depends/libXcursor/package.mk
@@ -3,11 +3,11 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libXcursor"
-PKG_VERSION="1.2.0"
-PKG_SHA256="3ad3e9f8251094af6fe8cb4afcf63e28df504d46bfa5a5529db74a505d628782"
+PKG_VERSION="1.2.1"
+PKG_SHA256="46c143731610bafd2070159a844571b287ac26192537d047a39df06155492104"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.X.org"
-PKG_URL="https://xorg.freedesktop.org/archive/individual/lib/libXcursor-${PKG_VERSION}.tar.bz2"
+PKG_URL="https://xorg.freedesktop.org/archive/individual/lib/libXcursor-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain libX11 libXfixes libXrender"
 PKG_LONGDESC="X11 Cursor management library.s"
 PKG_BUILD_FLAGS="+pic -sysroot"
