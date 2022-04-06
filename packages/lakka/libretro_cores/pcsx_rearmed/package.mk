b3b9a80dd65ba3e578e2463372c6547562625a45
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2022-04-06 08:41:36 +0200

    pcsx_rearmed: build with correct dynarec, disable dynarec on x86_64/i386
    see: #1651

--- packages/lakka/libretro_cores/pcsx_rearmed/package.mk
+++ packages/lakka/libretro_cores/pcsx_rearmed/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="pcsx_rearmed"
-PKG_VERSION="7e3ce9afd1ddd5c16f20957cc7a46f6e681bff50"
+PKG_VERSION="46a38bdab1a4d9f578a368705a9e3e144fd81189"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/pcsx_rearmed"
 PKG_URL="${PKG_SITE}.git"
@@ -23,21 +23,24 @@ if [ "${VULKAN_SUPPORT}" = "yes" ]; then
   PKG_DEPENDS_TARGET+=" ${VULKAN}"
 fi
 
-if [ "${ARCH}" = "aarch64" ]; then
-  PKG_MAKE_OPTS_TARGET+=" platform=unix"
-elif [ "${ARCH}" = "arm" ]; then
+if [ "${ARCH}" = "arm" ]; then
   PKG_MAKE_OPTS_TARGET+=" DYNAREC=ari64"
   if target_has_feature neon ; then
     PKG_MAKE_OPTS_TARGET+=" HAVE_NEON=1 BUILTIN_GPU=neon"
   else
     PKG_MAKE_OPTS_TARGET+=" HAVE_NEON=0 BUILTIN_GPU=unai"
   fi
-fi
-
-if [ "${DEVICE}" = "OdroidGoAdvance" ]; then
-  sed -e "s|armv8-a|armv8-a+crc|" \
-      -i ../Makefile.libretro
-  PKG_MAKE_OPTS_TARGET+=" platfrom=classic_armv8_a35"
+  if [ "${DEVICE}" = "OdroidGoAdvance" ]; then
+    sed -e "s|armv8-a|armv8-a+crc|" \
+        -i ../Makefile.libretro
+    PKG_MAKE_OPTS_TARGET+=" platfrom=classic_armv8_a35"
+  else
+    PKG_MAKE_OPTS_TARGET+=" platform=unix"
+  fi
+elif [ "${ARCH}" = "aarch64" ]; then
+  PKG_MAKE_OPTS_TARGET+=" platform=unix DYNAREC=ari64"
+else
+  PKG_MAKE_OPTS_TARGET+=" platform=unix DYNAREC=none"
 fi
 
 makeinstall_target() {
