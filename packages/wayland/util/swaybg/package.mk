06a916ec1652e58d9eba514f78e292a8e44746fc
Rudi Heitbaum <rudi@heitbaum.com>
2022-04-15 06:07:35 +0000

    swaybg: update to 1.1.1

--- packages/wayland/util/swaybg/package.mk
+++ packages/wayland/util/swaybg/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="swaybg"
-PKG_VERSION="1.1"
-PKG_SHA256="958f425310514ae2aae064595c3e7efba2187b0d2947136c49188558fdb367b1"
+PKG_VERSION="1.1.1"
+PKG_SHA256="71cc8fc2cb7ae5ad3af772ab286b0b42f1c7cb17bea131e78c2d40a2fb8e6c59"
 PKG_LICENSE="MIT"
 PKG_SITE="https://swaywm.org/"
 PKG_URL="https://github.com/swaywm/swaybg/archive/v${PKG_VERSION}.tar.gz"
