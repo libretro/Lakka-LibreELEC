c1015664de79a7d43a6e011023d38fc579398bb1
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2022-04-15 15:09:32 +0200

    mame2010: buildfix

--- packages/lakka/libretro_cores/mame2010/package.mk
+++ packages/lakka/libretro_cores/mame2010/package.mk
@@ -7,16 +7,22 @@ PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Late 2010 version of MAME (0.139) for libretro. Compatible with MAME 0.139 romsets."
 PKG_TOOLCHAIN="manual"
 
-make_target() {
+pre_make_target() {
+  PKG_MAKE_OPTS_TARGET="CC=${CC} LD=${CC}"
   if [ "${ARCH}" = "arm" ]; then
-    make CC=${CC} LD=${CC} PLATCFLAGS="${CFLAGS}" PTR64=0 ARM_ENABLED=1 LCPU=arm
+    PKG_MAKE_OPTS_TARGET+=" PTR64=0 ARM_ENABLED=1 LCPU=arm"
   elif [ "${ARCH}" = "i386" ]; then
-    make CC=${CC} LD=${CC} PLATCFLAGS="${CFLAGS}" PTR64=0 ARM_ENABLED=0 LCPU=x86
+    PKG_MAKE_OPTS_TARGET+=" PTR64=0 ARM_ENABLED=0 LCPU=x86"
   elif [ "${ARCH}" = "x86_64" ]; then
-    make CC=${CC} LD=${CC} PLATCFLAGS="${CFLAGS}" PTR64=1 ARM_ENABLED=0 LCPU=x86_64
+    PKG_MAKE_OPTS_TARGET+=" PTR64=1 ARM_ENABLED=0 LCPU=x86_64"
   elif [ "${ARCH}" = "aarch64" ]; then
-    make CC=${CC} LD=${CC} PLATCFLAGS="${CFLAGS}" PTR64=1 ARM_ENABLED=1 LCPU=arm64
+    PKG_MAKE_OPTS_TARGET+=" PTR64=1 ARM_ENABLED=1 LCPU=arm64"
   fi
+  make ${PKG_MAKE_OPTS_TARGET} PLATCFLAGS="${CFLAGS}" maketree
+}
+
+make_target() {
+  make ${PKG_MAKE_OPTS_TARGET} PLATCFLAGS="${CFLAGS}"
 }
 
 makeinstall_target() {
