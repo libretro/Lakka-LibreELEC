d93eeeb6fcd9d2d377613335f66cf2a4c0bc9120
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2022-04-19 18:38:19 +0200

    flycast: update to include Naomi 2 support

--- packages/lakka/libretro_cores/flycast/package.mk
+++ packages/lakka/libretro_cores/flycast/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="flycast"
-PKG_VERSION="fd50529d864c0af7007f2f283a84bf67851fe0e4"
+PKG_VERSION="aa6c9e21063c929ccf651328547e5c6a9afd1f62"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/flyinghead/flycast"
 PKG_URL="${PKG_SITE}.git"
