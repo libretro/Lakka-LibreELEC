2a8f8c921063992bf077d91cee9c2abca30e6fce
Rudi Heitbaum <rudi@heitbaum.com>
2022-04-20 12:14:01 +0000

    media-driver: update to 22.4.0 and fix Release build warning
    Fix the following build warning by defining BUILD_TYPE and matching
    CMAKE_BUILD_TYPE to BUILD_TYPE. The build has been building as Release,
    as the CMAKE_BUILD_TYPE does not override BUILD_TYPE.
    
      *BUILD_TYPE not defined, default to: release
      CMake Warning at Tools/bldsys/include/bs_base_utils.cmake:81 (message):
        Inconsistent: BUILD_TYPE="release" vs CMAKE_BUILD_TYPE="MinSizeRel"
      Call Stack (most recent call first):
        Tools/bldsys/bs_init.cmake:35 (bs_check_build_type)
        media_driver/CMakeLists.txt:69 (include)
      ...
      -- media -- BUILD_TYPE/UFO_BUILD_TYPE/CMAKE_BUILD_TYPE = release/release/Release
    
    The full eror will not disappear until a future PR to change the LE build from
    CMAKE_BUILD_TYPE="MinSizeRel" to CMAKE_BUILD_TYPE="Release"

--- packages/multimedia/media-driver/package.mk
+++ packages/multimedia/media-driver/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="media-driver"
-PKG_VERSION="22.3.1"
-PKG_SHA256="0fdccad95a561178bd19fba69ab94be23bd4a3072e68aa18c3304c990d87d7d8"
+PKG_VERSION="22.4.0"
+PKG_SHA256="f070527b141174970a17195d0225ed43693c39fec83cd5e6d0effaa88e2a5553"
 PKG_ARCH="x86_64"
 PKG_LICENSE="MIT"
 PKG_SITE="https://01.org/linuxmedia"
@@ -11,4 +11,8 @@ PKG_URL="https://github.com/intel/media-driver/archive/intel-media-${PKG_VERSION
 PKG_DEPENDS_TARGET="toolchain libva libdrm gmmlib"
 PKG_LONGDESC="media-driver: The Intel(R) Media Driver for VAAPI is a new VA-API (Video Acceleration API) user mode driver supporting hardware accelerated decoding, encoding, and video post processing for GEN based graphics hardware."
 
-PKG_CMAKE_OPTS_TARGET="-DBUILD_CMRTLIB=OFF -DENABLE_NONFREE_KERNELS=ON -DBUILD_KERNELS=ON -DBYPASS_MEDIA_ULT=yes"
+PKG_CMAKE_OPTS_TARGET="-DBUILD_CMRTLIB=OFF \
+                       -DBUILD_KERNELS=ON \
+                       -DBUILD_TYPE=release \
+                       -DENABLE_NONFREE_KERNELS=ON \
+                       -DMEDIA_RUN_TEST_SUITE=OFF"
