bbdcbea4ace46e7fdc25913a3ccc6d77b8695a1f
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2022-04-21 07:39:45 +0200

    race: remove DRZ80 from make options
    does not improve performance, creates issues with save states
    closes #1664

--- packages/lakka/libretro_cores/race/package.mk
+++ packages/lakka/libretro_cores/race/package.mk
@@ -7,10 +7,6 @@ PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="NeoGeo Pocket emulator"
 PKG_TOOLCHAIN="make"
 
-if [ "${ARCH}" = "arm" ]; then
-  PKG_MAKE_OPTS_TARGET="DRZ80=1"
-fi
-
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
     cp -v race_libretro.so ${INSTALL}/usr/lib/libretro/
