53780b7d56ff6be3360365a075615a965493f6e7
Rudi Heitbaum <rudi@heitbaum.com>
2022-04-21 10:28:09 +0000

    fribidi: update to 1.0.12

--- packages/devel/fribidi/package.mk
+++ packages/devel/fribidi/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="fribidi"
-PKG_VERSION="1.0.11"
-PKG_SHA256="30f93e9c63ee627d1a2cedcf59ac34d45bf30240982f99e44c6e015466b4e73d"
+PKG_VERSION="1.0.12"
+PKG_SHA256="0cd233f97fc8c67bb3ac27ce8440def5d3ffacf516765b91c2cc654498293495"
 PKG_LICENSE="LGPL"
 PKG_SITE="http://fribidi.freedesktop.org/"
 PKG_URL="https://github.com/fribidi/fribidi/releases/download/v${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.xz"
