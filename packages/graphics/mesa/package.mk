98196c03865fb1289080578cccf05c6d19ff8271
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2022-04-23 20:01:24 +0200

    mesa: update to 22.0.2

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa"
-PKG_VERSION="22.0.1"
-PKG_SHA256="c05f9682c54560b36e0afa70896233fc73f1ed715e10d1a028b0eb84fd04426f"
+PKG_VERSION="22.0.2"
+PKG_SHA256="df4fa560dcce6680133067cd15b0505fc424ca703244ce9ab247c74d2fab6885"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
 PKG_URL="https://mesa.freedesktop.org/archive/mesa-${PKG_VERSION}.tar.xz"
