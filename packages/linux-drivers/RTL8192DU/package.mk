0b30cdfc0ef9602b29ed85ab8c13e54003a1481f
Rudi Heitbaum <rudi@heitbaum.com>
2022-04-25 12:49:08 +0000

    RTL8192DU: fix kernel 5.18 compilation

--- packages/linux-drivers/RTL8192DU/package.mk
+++ packages/linux-drivers/RTL8192DU/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="RTL8192DU"
-PKG_VERSION="eb09a4e6e96a5a033afe60c71022bfa42eea148a"
-PKG_SHA256="e78c4fc28a66c1f15154a26c93bee9696965fe81a3246a751c2b4d1fa10e3454"
+PKG_VERSION="a4e32040416d2dba2611a8f410a4f7663a425eb3"
+PKG_SHA256="1b3db84d541d12ac898aed23f4c45cd20382546cc1804a0bbd93fb3d80361b5a"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/lwfinger/rtl8192du"
 PKG_URL="https://github.com/lwfinger/rtl8192du/archive/${PKG_VERSION}.tar.gz"
