7bd520b57d4a91062b99bc114e147085b6cc8db9
Rudi Heitbaum <rudi@heitbaum.com>
2022-04-30 07:24:39 +0000

    minisatip: drop dvb-apps dependancy in v1.2.7

--- packages/addons/service/minisatip/package.mk
+++ packages/addons/service/minisatip/package.mk
@@ -9,7 +9,7 @@ PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/catalinii/minisatip"
 PKG_URL="https://github.com/catalinii/minisatip/archive/refs/tags/v${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain dvb-apps libdvbcsa libxml2 openssl"
+PKG_DEPENDS_TARGET="toolchain libdvbcsa libxml2 openssl"
 PKG_SECTION="service"
 PKG_SHORTDESC="minisatip: a Sat>IP streaming server for Linux"
 PKG_LONGDESC="minisatip(${PKG_VERSION_NUMBER}): is a Sat>IP streaming server for Linux supporting DVB-C, DVB-S/S2, DVB-T/T2, ATSC and ISDB-T"
@@ -29,10 +29,6 @@ PKG_CONFIGURE_OPTS_TARGET="--enable-static \
 pre_configure_target() {
   cd ${PKG_BUILD}
     rm -rf .${TARGET_NAME}
-
-  # enables Common Interface (DVBEN50221)
-  CFLAGS+=" -I$(get_install_dir dvb-apps)/usr/include"
-  LDFLAGS+=" -L$(get_install_dir dvb-apps)/usr/lib"
 }
 
 makeinstall_target() {
