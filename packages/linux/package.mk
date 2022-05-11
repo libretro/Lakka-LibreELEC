664ec2788c9796a69cdd21198b15a7a2fdd16320
Matthias Reichl <hias@horus.com>
2022-05-11 16:32:05 +0200

    linux (RPi): update to 5.15.38-2cca267
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -22,8 +22,8 @@ case "${LINUX}" in
     PKG_SOURCE_NAME="linux-${LINUX}-${PKG_VERSION}.tar.gz"
     ;;
   raspberrypi)
-    PKG_VERSION="9af1cc301e4dffb830025207a54d0bc63bec16c7" # 5.15.36
-    PKG_SHA256="b624a2c8f2a6db60623c4194a5e4ff4e8c21864334101765aff545bc45a0ca23"
+    PKG_VERSION="2cca267016006654836c46bdd8efa8f9b6402e5d" # 5.15.38
+    PKG_SHA256="bb00314f592f482ba8a1eaf3eed53fa835fc56ed25eeef16c44ecc2abb83f0cf"
     PKG_URL="https://github.com/raspberrypi/linux/archive/${PKG_VERSION}.tar.gz"
     PKG_SOURCE_NAME="linux-${LINUX}-${PKG_VERSION}.tar.gz"
     ;;
