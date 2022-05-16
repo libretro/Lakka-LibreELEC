587f6407e4c037f17ce6d7ca0c044e7ab0adff97
Lukas Rusak <lorusak@gmail.com>
2022-05-16 08:35:54 -0700

    rust-std-snapshot: add package

---
+++ packages/rust/rust-std-snapshot/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="rust-std-snapshot"
+PKG_VERSION="$(get_pkg_version rust)"
+PKG_SHA256="6fb8ee3650beb10836ae48a9aaa535473e64eaca20695b88113267aea3c7557f"
+PKG_LICENSE="MIT"
+PKG_SITE="https://www.rust-lang.org"
+PKG_URL="https://static.rust-lang.org/dist/rust-std-${PKG_VERSION}-${MACHINE_HARDWARE_CPU}-unknown-linux-gnu.tar.xz"
+PKG_LONGDESC="rust std library bootstrap package"
+PKG_TOOLCHAIN="manual"
