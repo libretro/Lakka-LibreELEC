59b247ee666b5c74dd09af0726d2ef2f1da671d1
Lukas Rusak <lorusak@gmail.com>
2022-05-17 11:23:38 -0700

    image: add PIPEWIRE_SUPPORT option

--- packages/virtual/image/package.mk
+++ packages/virtual/image/package.mk
@@ -19,6 +19,12 @@ PKG_LONGDESC="Root package used to build and create complete image"
 
 [ "${PULSEAUDIO_SUPPORT}" = "yes" ] && PKG_DEPENDS_TARGET+=" pulseaudio"
 
+[ "${PIPEWIRE_SUPPORT}" = "yes" ] && PKG_DEPENDS_TARGET+=" pipewire wireplumber"
+
+if [ "${PULSEAUDIO_SUPPORT}" = "yes" -a "${PIPEWIRE_SUPPORT}" = "yes" ]; then
+  die "PULSEAUDIO_SUPPORT and PIPEWIRE_SUPPORT cannot be enabled together"
+fi
+
 # Automounter support
 [ "${UDEVIL}" = "yes" ] && PKG_DEPENDS_TARGET+=" udevil"
 
