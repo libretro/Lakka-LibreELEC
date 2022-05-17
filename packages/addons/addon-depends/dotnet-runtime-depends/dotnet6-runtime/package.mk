3f3fcb53deedb6a591a60096931619be695fc8ae
Rudi Heitbaum <rudi@heitbaum.com>
2022-05-17 13:52:48 +0000

    dotnet6-runtime: update to 6.0.5

--- packages/addons/addon-depends/dotnet-runtime-depends/dotnet6-runtime/package.mk
+++ packages/addons/addon-depends/dotnet-runtime-depends/dotnet6-runtime/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="dotnet6-runtime"
-PKG_VERSION="6.0.4"
+PKG_VERSION="6.0.5"
 PKG_LICENSE="MIT"
 PKG_SITE="https://dotnet.microsoft.com/"
 PKG_DEPENDS_TARGET="toolchain"
@@ -11,16 +11,16 @@ PKG_TOOLCHAIN="manual"
 
 case "${ARCH}" in
   "aarch64")
-    PKG_SHA256="672f8e8ffcf012059aef6c2f7edfe34ea307559fa2d8821bb5a4d07a4fbbbed6"
-    PKG_URL="https://download.visualstudio.microsoft.com/download/pr/3641affa-8bb0-486f-93d9-68adff4f4af7/1e3df9fb86cba7299b9e575233975734/dotnet-runtime-6.0.4-linux-arm64.tar.gz"
+    PKG_SHA256="0e61cffb7ef9ceed54ad759f253287ef09523825cd87ed9e84f608701abba325"
+    PKG_URL="https://download.visualstudio.microsoft.com/download/pr/b7bfeef6-3df9-46a1-8cc9-5b2a3121a1d7/44287ecada25d3f0bd8610550e08246d/dotnet-runtime-6.0.5-linux-arm64.tar.gz"
     ;;
   "arm")
-    PKG_SHA256="11b8c2952210e226ec9c2a996d7a6b24f4b8ddd8ae0590080bfea5362fb7e0af"
-    PKG_URL="https://download.visualstudio.microsoft.com/download/pr/f8e1ab66-58f7-4ebb-a9bb-9decfa03501f/88e1fb49af6f75dc54c23383162409c5/dotnet-runtime-6.0.4-linux-arm.tar.gz"
+    PKG_SHA256="48aef75040b082f7c7d4222c5726f9dbf4da0d1dd8511a236fa112d2e8eac77c"
+    PKG_URL="https://download.visualstudio.microsoft.com/download/pr/36a5510d-e454-4f46-aeaa-ed2c9521e12e/1d60cf7759fd938f2e6c9730d0792b9d/dotnet-runtime-6.0.5-linux-arm.tar.gz"
     ;;
   "x86_64")
-    PKG_SHA256="02e2c5b5950e3fc1fe4dd146175758f198c711e495883f7f286d575437fd82c4"
-    PKG_URL="https://download.visualstudio.microsoft.com/download/pr/5b08d331-15ac-4a53-82a5-522fa45b1b99/65ae300dd160ae0b88b91dd78834ce3e/dotnet-runtime-6.0.4-linux-x64.tar.gz"
+    PKG_SHA256="688694c604bbd810d28446752131e20468390a071aa2a5157a4e2d87a43dfa3c"
+    PKG_URL="https://download.visualstudio.microsoft.com/download/pr/56d9250f-97df-4786-b33e-a8e34b349e86/dcf054ca00899a70a80aa1a7d3072b52/dotnet-runtime-6.0.5-linux-x64.tar.gz"
     ;;
 esac
 PKG_SOURCE_NAME="dotnet-runtime_${PKG_VERSION}_${ARCH}.tar.gz"
