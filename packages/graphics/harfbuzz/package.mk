ac35bdf77ea4c83e2bf0c845e2e5bf9c291a25b1
Rudi Heitbaum <rudi@heitbaum.com>
2022-05-21 10:43:29 +0000

    harfbuzz: update to 4.3.0

--- packages/graphics/harfbuzz/package.mk
+++ packages/graphics/harfbuzz/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="harfbuzz"
-PKG_VERSION="4.2.1"
-PKG_SHA256="bd17916513829aeff961359a5ccebba6de2f4bf37a91faee3ac29c120e3d7ee1"
+PKG_VERSION="4.3.0"
+PKG_SHA256="a49628f4c4c8e6d8df95ef44935a93446cf2e46366915b0e3ca30df21fffb530"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.freedesktop.org/wiki/Software/HarfBuzz"
 PKG_URL="https://github.com/harfbuzz/harfbuzz/releases/download/${PKG_VERSION}/harfbuzz-${PKG_VERSION}.tar.xz"
