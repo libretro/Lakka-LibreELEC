cc614f2f968a8c0806064c5b7fa61b45a3f61f28
Giovanni Cascione <43221199+spleen1981@users.noreply.github.com>
2022-05-30 14:35:23 +0200

    backport puae2021 to lakka_3x

---
+++ packages/libretro/puae2021/package.mk
@@ -0,0 +1,16 @@
+PKG_NAME="puae2021"
+PKG_VERSION="7a2f6f1da8b4c949c26806a3fa23f7455f44cd39"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/libretro/libretro-uae"
+PKG_URL="${PKG_SITE}.git"
+PKG_GIT_CLONE_BRANCH="2.6.1"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Portable Commodore Amiga Emulator. Branch frozen at older version for better performance."
+PKG_TOOLCHAIN="make"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+    cp -v puae2021_libretro.so ${INSTALL}/usr/lib/libretro/
+  mkdir -p ${INSTALL}/usr/share/retroarch-system/uae_data
+    cp -vR ${PKG_BUILD}/sources/uae_data/* ${INSTALL}/usr/share/retroarch-system/uae_data/
+}
