f8016b08c6d56d0b4a3fc5c0c5759bca7e9e99ce
Rudi Heitbaum <rudi@heitbaum.com>
2022-06-05 05:22:13 +0000

    systemd: update to 251.2

--- packages/sysutils/systemd/package.mk
+++ packages/sysutils/systemd/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="systemd"
-PKG_VERSION="251.1"
-PKG_SHA256="0d986657082ce72246a12093933ea4c3c8e2a50e3000f1a4192d14dcaa20cd9a"
+PKG_VERSION="251.2"
+PKG_SHA256="5b371e824fe3e3128f2338b8db8cd37171f8e249eea88077eea468f73d833545"
 PKG_LICENSE="LGPL2.1+"
 PKG_SITE="http://www.freedesktop.org/wiki/Software/systemd"
 PKG_URL="https://github.com/systemd/systemd-stable/archive/v${PKG_VERSION}.tar.gz"
