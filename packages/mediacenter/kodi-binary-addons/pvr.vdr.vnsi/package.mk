962fe30bb52f333098caf5666cc5e7fda93d1d84
Rudi Heitbaum <rudi@heitbaum.com>
2022-06-13 03:48:04 +0000

    pvr.vdr.vnsi: update 20.2.3-Nexus to 20.3.0-Nexus

--- packages/mediacenter/kodi-binary-addons/pvr.vdr.vnsi/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.vdr.vnsi/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pvr.vdr.vnsi"
-PKG_VERSION="20.2.3-Nexus"
-PKG_SHA256="5d0af19321124a4121cb9400a71d0ed4dbbe4916017f9abfb57769a934789bdc"
+PKG_VERSION="20.3.0-Nexus"
+PKG_SHA256="d06f5b3c50b5cda2bb63387317505be4a82dfe9a27188021808c63960961cef4"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
