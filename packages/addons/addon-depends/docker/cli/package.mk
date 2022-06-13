6cadb044899fc309e0262e8b08ef6a67fd892944
CvH <namerp@googlemail.com>
2022-06-13 21:39:36 +0200

    docker: align copyright to disto default

--- packages/addons/addon-depends/docker/cli/package.mk
+++ packages/addons/addon-depends/docker/cli/package.mk
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 2022 Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="cli"
 PKG_VERSION="$(get_pkg_version moby)"
