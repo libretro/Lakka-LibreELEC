9f8b01782681855e2bc152fccfc0b7276cc016d6
Rudi Heitbaum <rudi@heitbaum.com>
2022-06-15 12:14:31 +0000

    aspnet6-runtime: update to 6.0.6

--- packages/addons/addon-depends/dotnet-runtime-depends/aspnet6-runtime/package.mk
+++ packages/addons/addon-depends/dotnet-runtime-depends/aspnet6-runtime/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="aspnet6-runtime"
-PKG_VERSION="6.0.5"
+PKG_VERSION="6.0.6"
 PKG_LICENSE="MIT"
 PKG_SITE="https://dotnet.microsoft.com/"
 PKG_DEPENDS_TARGET="toolchain"
@@ -11,16 +11,16 @@ PKG_TOOLCHAIN="manual"
 
 case "${ARCH}" in
   "aarch64")
-    PKG_SHA256="70dc0a73b71761a2f717bf8917f7ea4a1be4c41d2cffbe29df68f38d26e8061e"
-    PKG_URL="https://download.visualstudio.microsoft.com/download/pr/8ba7087e-4513-41e5-8359-a4bcd2a3661f/e6828f0d8cf1ecc63074c9ff57685e27/aspnetcore-runtime-6.0.5-linux-arm64.tar.gz"
+    PKG_SHA256="e80fbf9feed47e2214c848ac66bdf10ce19ddcd5a06a05ec9b255a7915bd43fd"
+    PKG_URL="https://download.visualstudio.microsoft.com/download/pr/94553ccb-ce1a-401c-8840-bdffb4e9d0cb/ab8a0024df90506d953904ac38b5a978/aspnetcore-runtime-6.0.6-linux-arm64.tar.gz"
     ;;
   "arm")
-    PKG_SHA256="cfc2046f516d89cec3b0052029044691448f8dcd0a3e8779776a234124846308"
-    PKG_URL="https://download.visualstudio.microsoft.com/download/pr/eda01ff6-fb9f-49ce-bdc1-67c688f9f1fa/75b195f97f4b219fccbac4432a6afaf0/aspnetcore-runtime-6.0.5-linux-arm.tar.gz"
+    PKG_SHA256="037a8add2f4c03bb23842097c5aa25fe52b87f6cd5d977237a6e579b6919f73b"
+    PKG_URL="https://download.visualstudio.microsoft.com/download/pr/084bfc2b-f28d-4995-87f0-d82519245825/7f5398fc2caf95355b154856868ef560/aspnetcore-runtime-6.0.6-linux-arm.tar.gz"
     ;;
   "x86_64")
-    PKG_SHA256="95a3cc7c4e7de792e39e40ffda72127ba49a49604b61fee18d50f970c9c1e903"
-    PKG_URL="https://download.visualstudio.microsoft.com/download/pr/a0e9ceb8-04eb-4510-876c-795a6a123dda/6141e57558eddc2d4629c7c14c2c6fa1/aspnetcore-runtime-6.0.5-linux-x64.tar.gz"
+    PKG_SHA256="38ad01d083f0f493116bd06dd80210589abc8b3d502cf81c5dc40ba0fc19d3d5"
+    PKG_URL="https://download.visualstudio.microsoft.com/download/pr/afd5344f-a9e9-45f9-85b5-de4551c53736/c30996daa407f9bb540ebc5edfcf16fc/aspnetcore-runtime-6.0.6-linux-x64.tar.gz"
     ;;
 esac
 PKG_SOURCE_NAME="aspnetcore-runtime_${PKG_VERSION}_${ARCH}.tar.gz"
