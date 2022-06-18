072f439fedeeb3b54521479df41e365b37a695aa
Rudi Heitbaum <rudi@heitbaum.com>
2022-06-18 23:00:22 +0000

    strace: update to 5.18

--- packages/debug/strace/package.mk
+++ packages/debug/strace/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="strace"
-PKG_VERSION="5.17"
-PKG_SHA256="5fb298dbd1331fd1e1bc94c5c32395860d376101b87c6cd3d1ba9f9aa15c161f"
+PKG_VERSION="5.18"
+PKG_SHA256="60293ea79ac9253d600cdc9be077ad2988ca22284a439c9e66be5150db3d1187"
 PKG_LICENSE="BSD"
 PKG_SITE="https://strace.io/"
 PKG_URL="https://strace.io/files/${PKG_VERSION}/strace-${PKG_VERSION}.tar.xz"
