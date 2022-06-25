f3038ef9bddfe1ed06f3e315dad9e8959a174bfe
Rudi Heitbaum <rudi@heitbaum.com>
2022-06-25 14:08:03 +0000

    llvm: update to 14.0.6
    ann:
    - https://discourse.llvm.org/t/llvm-14-0-6-release/63431

--- packages/lang/llvm/package.mk
+++ packages/lang/llvm/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="llvm"
-PKG_VERSION="14.0.5"
-PKG_SHA256="c9d27903ba3883c476a83cd515e36e1e07b0585db55692835de11385d9e3c8fa"
+PKG_VERSION="14.0.6"
+PKG_SHA256="8b3cfd7bc695bd6cea0f37f53f0981f34f87496e79e2529874fd03a2f9dd3a8a"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="http://llvm.org/"
 PKG_URL="https://github.com/llvm/llvm-project/releases/download/llvmorg-${PKG_VERSION}/llvm-project-${PKG_VERSION}.src.tar.xz"
