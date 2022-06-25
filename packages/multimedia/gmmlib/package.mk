7889296a251c4d884110644c727d1b242ab32b5c
Rudi Heitbaum <rudi@heitbaum.com>
2022-06-25 05:50:24 +0000

    gmmlib: update to 22.1.4

--- packages/multimedia/gmmlib/package.mk
+++ packages/multimedia/gmmlib/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="gmmlib"
-PKG_VERSION="22.1.3"
-PKG_SHA256="81dbb4ddec98bb18c3a038cd40222046ae7f5b24b2d5acbfb2400f39f02f2aaf"
+PKG_VERSION="22.1.4"
+PKG_SHA256="18f291b6d5c9a170468e050e301f23760bb5b20b79d28a49a791ace2f22880c9"
 PKG_ARCH="x86_64"
 PKG_LICENSE="MIT"
 PKG_SITE="https://01.org/linuxmedia"
@@ -12,4 +12,5 @@ PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="gmmlib: The Intel(R) Graphics Memory Management Library provides device specific and buffer management for the Intel(R) Graphics Compute Runtime for OpenCL(TM) and the Intel(R) Media Driver for VAAPI."
 
 PKG_CMAKE_OPTS_TARGET="-DBUILD_TYPE=release \
-                       -DRUN_TEST_SUITE=OFF"
+                       -DRUN_TEST_SUITE=OFF \
+                       -DCMAKE_BUILD_TYPE=Release"
