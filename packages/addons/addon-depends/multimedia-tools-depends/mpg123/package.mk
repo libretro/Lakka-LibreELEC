e04fada0ea2c740f91b7b95eed1eb3abe5c405bd
Rudi Heitbaum <rudi@heitbaum.com>
2022-06-26 16:49:37 +0000

    mpg123: update to 1.30.2 and https

--- packages/addons/addon-depends/multimedia-tools-depends/mpg123/package.mk
+++ packages/addons/addon-depends/multimedia-tools-depends/mpg123/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mpg123"
-PKG_VERSION="1.29.3"
-PKG_SHA256="963885d8cc77262f28b77187c7d189e32195e64244de2530b798ddf32183e847"
+PKG_VERSION="1.30.2"
+PKG_SHA256="c7ea863756bb79daed7cba2942ad3b267a410f26d2dfbd9aaf84451ff28a05d7"
 PKG_LICENSE="LGPLv2"
 PKG_SITE="http://www.mpg123.org/"
-PKG_URL="http://downloads.sourceforge.net/sourceforge/mpg123/mpg123-${PKG_VERSION}.tar.bz2"
+PKG_URL="https://downloads.sourceforge.net/sourceforge/mpg123/mpg123-${PKG_VERSION}.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain alsa-lib"
 PKG_LONGDESC="A console based real time MPEG Audio Player for Layer 1, 2 and 3."
 PKG_BUILD_FLAGS="-sysroot"
