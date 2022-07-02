c77146f83a63819f720c6aaf52d4628a0c4ba4c5
Rudi Heitbaum <rudi@heitbaum.com>
2022-07-02 17:46:50 +0000

    libmtp: update to 1.1.20
    release notes:
    - https://github.com/libmtp/libmtp/releases/tag/v1.1.20

--- packages/addons/addon-depends/system-tools-depends/depends/libmtp/package.mk
+++ packages/addons/addon-depends/system-tools-depends/depends/libmtp/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libmtp"
-PKG_VERSION="1.1.19"
-PKG_SHA256="deb4af6f63f5e71215cfa7fb961795262920b4ec6cb4b627f55b30b18aa33228"
+PKG_VERSION="1.1.20"
+PKG_SHA256="c9191dac2f5744cf402e08641610b271f73ac21a3c802734ec2cedb2c6bc56d0"
 PKG_LICENSE="GPL"
 PKG_SITE="http://libmtp.sourceforge.net/"
 PKG_URL="${SOURCEFORGE_SRC}/project/${PKG_NAME}/${PKG_NAME}/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
