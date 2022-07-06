ebcb98b678bbce32a3ff4db3c63d51d8830b8a41
Christian Hewitt <christianshewitt@gmail.com>
2022-07-06 18:14:28 +0000

    brcmfmac_sdio-firmware: bump to include latest symlinks

--- packages/linux-firmware/brcmfmac_sdio-firmware/package.mk
+++ packages/linux-firmware/brcmfmac_sdio-firmware/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="brcmfmac_sdio-firmware"
-PKG_VERSION="1da9185bddb6ec3c975f5739335e7e23b7e2b6d0"
-PKG_SHA256="2638014742cf49a412417a138b3cd831b8e1387ab7dd100cc1f2e04f22539a75"
+PKG_VERSION="afc477e807c407736cfaff6a6188d09197dfbceb"
+PKG_SHA256="377558e84df5f04d76674b1fccddd565b6a97b8aca31533548746509d76a03e0"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/LibreELEC/brcmfmac_sdio-firmware"
 PKG_URL="https://github.com/LibreELEC/brcmfmac_sdio-firmware/archive/${PKG_VERSION}.tar.gz"
