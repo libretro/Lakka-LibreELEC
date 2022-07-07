e8b0149f7cc1f31bae647ca85baf6db4c935d130
Rudi Heitbaum <rudi@heitbaum.com>
2022-07-07 22:55:14 +0000

    efibootmgr: update to 18

--- packages/addons/addon-depends/system-tools-depends/efibootmgr/package.mk
+++ packages/addons/addon-depends/system-tools-depends/efibootmgr/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="efibootmgr"
-PKG_VERSION="b9fedd6b6f57055164bc361bc5cf16a602843c6e" # Nov 5, 2021
-PKG_SHA256="06061ebf96d28522f5a20b592305e71b91d3fb479ddcce41dadb5180da16d8d8"
+PKG_VERSION="18"
+PKG_SHA256="442867d12f8525034a404fc8af3036dba8e1fc970998af2486c3b940dfad0874"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/rhboot/efibootmgr"
