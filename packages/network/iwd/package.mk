afc191d908f3d469cdaa172adde1c516d37c8b4f
Rudi Heitbaum <rudi@heitbaum.com>
2022-07-15 23:40:36 +0000

    iwd: update to 1.29

--- packages/network/iwd/package.mk
+++ packages/network/iwd/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="iwd"
-PKG_VERSION="1.28"
-PKG_SHA256="ee538c720ad15335ece81a52a4b8a11820236df9ed9a5a72a2e64d74c652f0b0"
+PKG_VERSION="1.29"
+PKG_SHA256="71533fe3b8f6381f24832198ba11d00f04a361454770c173b3b66bc3cdf272bd"
 PKG_LICENSE="GPL"
 PKG_SITE="https://git.kernel.org/cgit/network/wireless/iwd.git/about/"
 PKG_URL="https://www.kernel.org/pub/linux/network/wireless/iwd-${PKG_VERSION}.tar.xz"
