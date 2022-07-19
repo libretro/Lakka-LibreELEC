696a5ac2ad66677c789c49872547716bed712e33
Rudi Heitbaum <rudi@heitbaum.com>
2022-07-19 10:04:10 +0000

    stress-ng: update to 0.14.03 and enable compile with glibc 2.36

--- packages/addons/addon-depends/system-tools-depends/stress-ng/package.mk
+++ packages/addons/addon-depends/system-tools-depends/stress-ng/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="stress-ng"
-PKG_VERSION="0.13.05"
-PKG_SHA256="3de49e1100866634f549e99c1644283d0cde817b844a69dcf7f80afa2227d350"
+PKG_VERSION="0.14.03"
+PKG_SHA256="95012c62883ab5826e6157557a075df98cce3cbce2a48bb40851bcc968a8441a"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/ColinIanKing/stress-ng"
 PKG_URL="https://github.com/ColinIanKing/stress-ng/archive/refs/tags/V${PKG_VERSION}.tar.gz"
