1cc8a2dabd6350ee84cf5c7de75f9a164a894d29
Rudi Heitbaum <rudi@heitbaum.com>
2022-07-20 08:11:20 +0000

    pipewire: update to 0.3.58
    changelog:
    - https://gitlab.freedesktop.org/pipewire/pipewire/-/blob/master/NEWS

--- packages/audio/pipewire/package.mk
+++ packages/audio/pipewire/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pipewire"
-PKG_VERSION="0.3.55"
-PKG_SHA256="666260961a242239a940b7f6130e5e9cb51ad76f9ebf6b5403198ee5efa69344"
+PKG_VERSION="0.3.58"
+PKG_SHA256="5cb73bf27b8b80e37804d7829c5ad8fa5510835f449bbb7e4773f188584771ad"
 PKG_LICENSE="LGPL"
 PKG_SITE="https://pipewire.org"
 PKG_URL="https://github.com/PipeWire/pipewire/archive/${PKG_VERSION}.tar.gz"
