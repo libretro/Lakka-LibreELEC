433e007aab6d3db003b94d1719d14207fc4fe095
Rudi Heitbaum <rudi@heitbaum.com>
2022-07-23 00:13:45 +0000

    kodi-binary-addons: update to latest versions
    - inputstream.ffmpegdirect: update 20.2.3-Nexus to 20.3.0-Nexus
    - pvr.iptvsimple: update 20.3.1-Nexus to 20.4.0-Nexus

--- packages/mediacenter/kodi-binary-addons/inputstream.ffmpegdirect/package.mk
+++ packages/mediacenter/kodi-binary-addons/inputstream.ffmpegdirect/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="inputstream.ffmpegdirect"
-PKG_VERSION="20.2.3-Nexus"
-PKG_SHA256="967ed228d7e4cc1926ca356a016ed7b17180b18b676b6b1a60dbc057fa1f69fc"
+PKG_VERSION="20.3.0-Nexus"
+PKG_SHA256="684d2afeb70f36c327495499fc382b8a6da7903115284aff3ece5d3e369d6273"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL2+"
