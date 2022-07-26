54099db87321e80ae0b3e02517b62b75fef2f6df
Rudi Heitbaum <rudi@heitbaum.com>
2022-07-26 22:19:26 +0000

    json-glib: build depends on glib-mkenums from glib:host

--- packages/addons/addon-depends/chrome-depends/json-glib/package.mk
+++ packages/addons/addon-depends/chrome-depends/json-glib/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="bf4d1cd6c343ce13b9258e6703a0411a3b659887b65877e85a2aa488ae18b865"
 PKG_LICENSE="LGPL-2.1"
 PKG_SITE="https://github.com/GNOME/json-glib"
 PKG_URL="https://github.com/GNOME/json-glib/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain glib"
+PKG_DEPENDS_TARGET="toolchain glib glib:host"
 PKG_LONGDESC="JSON-GLib implements a full suite of JSON-related tools using GLib and GObject."
 
 PKG_MESON_OPTS_TARGET="-Dintrospection=disabled \
