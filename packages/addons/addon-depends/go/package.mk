596aaff80d1b9f55515435c0123b1203649d4c28
Rudi Heitbaum <rudi@heitbaum.com>
2022-08-03 10:03:00 +0000

    go: update to 1.19.1

--- packages/addons/addon-depends/go/package.mk
+++ packages/addons/addon-depends/go/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="go"
-PKG_VERSION="1.18.5"
-PKG_SHA256="30c07c08c9b9a79d652272ac3298c249a810a4c908e8937cf5e58d2414af1cc3"
+PKG_VERSION="1.19.1"
+PKG_SHA256="884439c3de751c705e47a120af6b92b65770a952a7b8788244bc6d73b2548c3e"
 PKG_LICENSE="BSD"
 PKG_SITE="https://golang.org"
 PKG_URL="https://github.com/golang/go/archive/${PKG_NAME}${PKG_VERSION}.tar.gz"
