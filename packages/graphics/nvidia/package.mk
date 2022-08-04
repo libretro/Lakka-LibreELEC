9976432c3fb2859ee56e53e29dae553a20cfd7b9
SupervisedThinking <supervisedthinking@gmail.com>
2022-08-04 23:12:54 +0200

    nvidia: update to 515.65.01

--- packages/graphics/nvidia/package.mk
+++ packages/graphics/nvidia/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="nvidia"
-PKG_VERSION="515.57"
-PKG_SHA256="f05d782baca1ba681f8e417768338176875fb8bc0b687ce576daa4dc1f437f64"
+PKG_VERSION="515.65.01"
+PKG_SHA256="89e9c22b3903a2360bb18a7f1220dc52234b2cb928bb9dd6690236cf2b024695"
 PKG_ARCH="x86_64"
 PKG_LICENSE="nonfree"
 PKG_SITE="https://www.nvidia.com/en-us/drivers/unix/"
