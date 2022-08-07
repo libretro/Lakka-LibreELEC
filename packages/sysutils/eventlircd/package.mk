4495fc9eee555142499fb89a397fe57f1f4f28eb
Rudi Heitbaum <rudi@heitbaum.com>
2022-08-07 09:48:43 +0000

    eventlircd: use upstream to support -D_TIME_BITS=64 for 64-bit time

--- packages/sysutils/eventlircd/package.mk
+++ packages/sysutils/eventlircd/package.mk
@@ -1,9 +1,10 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="eventlircd"
-PKG_VERSION="3b753e91ae8c28dc34dd017a354d72b3c0715309"
-PKG_SHA256="4eca52d0570fa568b3296a2c9bc2af252423e25c1a67654bd79680fc5a93092a"
+PKG_VERSION="fd511a9cc7de6af4517604fdc2b3f0f5b64c2a81"
+PKG_SHA256="4c7a93d31845085272e497611159910af69e37a0f70925f36880ba40f4693e23"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/LibreELEC/eventlircd"
 PKG_URL="https://github.com/LibreELEC/eventlircd/archive/${PKG_VERSION}.tar.gz"
