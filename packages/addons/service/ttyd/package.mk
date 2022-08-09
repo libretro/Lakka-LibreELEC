0ff275706ac20268b4ae8399912fe7d915a49fa6
Rudi Heitbaum <rudi@heitbaum.com>
2022-08-09 09:37:17 +0000

    ttyd: update to 1.7.1 and addon (106)

--- packages/addons/service/ttyd/package.mk
+++ packages/addons/service/ttyd/package.mk
@@ -2,9 +2,9 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="ttyd"
-PKG_VERSION="1.6.3"
-PKG_SHA256="1116419527edfe73717b71407fb6e06f46098fc8a8e6b0bb778c4c75dc9f64b9"
-PKG_REV="105"
+PKG_VERSION="1.7.1"
+PKG_SHA256="e1e9993b1320c8623447304ae27031502569a1e37227ec48d4e21dae7db6eb66"
+PKG_REV="106"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/tsl0922/ttyd"
