247789d98693518e53d5de86d6899bf51a36aea2
Rudi Heitbaum <rudi@heitbaum.com>
2022-08-11 09:41:34 +0000

    tz: update to 2022b

--- packages/sysutils/tz/package.mk
+++ packages/sysutils/tz/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="tz"
-PKG_VERSION="2022a"
-PKG_SHA256="e9d82a851a15bb5db2cbaae2c3fc633743ad9edc069e3738c5e8908978064ed8"
+PKG_VERSION="2022b"
+PKG_SHA256="5da3fd3eb51d51d2aa4c599db8a1dd8dff453d79f4881131e35d40707ae1f838"
 PKG_LICENSE="Public Domain"
 PKG_SITE="http://www.iana.org/time-zones"
 PKG_URL="https://github.com/eggert/tz/archive/${PKG_VERSION}.tar.gz"
