c7e59fb4ccb57506fb277a8121444368df2d0917
Rudi Heitbaum <rudi@heitbaum.com>
2022-08-19 21:12:54 +0000

    SDL2: update to 2.24.0 and clean up build options
    release notes:
    - https://github.com/libsdl-org/SDL/releases/tag/release-2.24.0

--- packages/multimedia/SDL2/package.mk
+++ packages/multimedia/SDL2/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="SDL2"
-PKG_VERSION="2.0.22"
-PKG_SHA256="fe7cbf3127882e3fc7259a75a0cb585620272c51745d3852ab9dd87960697f2e"
+PKG_VERSION="2.24.0"
+PKG_SHA256="91e4c34b1768f92d399b078e171448c6af18cafda743987ed2064a28954d6d97"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.libsdl.org/"
 PKG_URL="https://www.libsdl.org/release/${PKG_NAME}-${PKG_VERSION}.tar.gz"
@@ -14,91 +14,88 @@ PKG_BUILD_FLAGS="+pic"
 
 if [ "${TARGET_ARCH}" = "x86_64" ]; then
   PKG_DEPENDS_TARGET+=" nasm:host"
-  PKG_SDL2_X86ASM="-DASSEMBLY=ON"
+  PKG_SDL2_X86ASM="-DSDL_ASSEMBLY=ON"
 else
   # Only x86(-64) and ppc assembly present as of 2.0.8
-  PKG_SDL2_X86ASM="-DASSEMBLY=OFF"
+  PKG_SDL2_X86ASM="-DSDL_ASSEMBLY=OFF"
 fi
 
 PKG_CMAKE_OPTS_TARGET="-DSDL_STATIC=ON \
                        -DSDL_SHARED=OFF \
-                       -DLIBC=ON \
-                       -DGCC_ATOMICS=ON \
+                       -DSDL_LIBC=ON \
+                       -DSDL_GCC_ATOMICS=ON \
                        ${PKG_SDL2_X86ASM} \
-                       -DALTIVEC=OFF \
-                       -DOSS=OFF \
-                       -DALSA=ON \
-                       -DALSA_SHARED=ON \
-                       -DESD=OFF \
-                       -DESD_SHARED=OFF \
-                       -DARTS=OFF \
-                       -DARTS_SHARED=OFF \
-                       -DNAS=OFF \
-                       -DNAS_SHARED=ON \
-                       -DSNDIO=OFF \
-                       -DDISKAUDIO=OFF \
-                       -DDUMMYAUDIO=OFF \
-                       -DVIDEO_WAYLAND=OFF \
-                       -DVIDEO_WAYLAND_QT_TOUCH=ON \
-                       -DWAYLAND_SHARED=OFF \
-                       -DVIDEO_MIR=OFF \
-                       -DMIR_SHARED=OFF \
-                       -DVIDEO_COCOA=OFF \
-                       -DVIDEO_DIRECTFB=OFF \
-                       -DDIRECTFB_SHARED=OFF \
-                       -DFUSIONSOUND=OFF \
-                       -DFUSIONSOUND_SHARED=OFF \
-                       -DVIDEO_DUMMY=OFF \
-                       -DINPUT_TSLIB=OFF \
-                       -DPTHREADS=ON \
-                       -DPTHREADS_SEM=ON \
-                       -DDIRECTX=OFF \
-                       -DSDL_DLOPEN=ON \
-                       -DCLOCK_GETTIME=OFF \
-                       -DRPATH=OFF \
-		       -DVIDEO_KMSDRM=OFF \
-                       -DRENDER_D3D=OFF"
+                       -DSDL_ALTIVEC=OFF \
+                       -DSDL_OSS=OFF \
+                       -DSDL_ALSA=ON \
+                       -DSDL_ALSA_SHARED=ON \
+                       -DSDL_ESD=OFF \
+                       -DSDL_ESD_SHARED=OFF \
+                       -DSDL_ARTS=OFF \
+                       -DSDL_ARTS_SHARED=OFF \
+                       -DSDL_NAS=OFF \
+                       -DSDL_NAS_SHARED=ON \
+                       -DSDL_SNDIO=OFF \
+                       -DSDL_DISKAUDIO=OFF \
+                       -DSDL_DUMMYAUDIO=OFF \
+                       -DSDL_WAYLAND=OFF \
+                       -DSDL_WAYLAND_QT_TOUCH=ON \
+                       -DSDL_WAYLAND_SHARED=OFF \
+                       -DSDL_COCOA=OFF \
+                       -DSDL_DIRECTFB=OFF \
+                       -DSDL_DIRECTFB_SHARED=OFF \
+                       -DSDL_FUSIONSOUND=OFF \
+                       -DSDL_FUSIONSOUND_SHARED=OFF \
+                       -DSDL_DUMMYVIDEO=OFF \
+                       -DSDL_PTHREADS=ON \
+                       -DSDL_PTHREADS_SEM=ON \
+                       -DSDL_DIRECTX=OFF \
+                       -DSDL_LOADSO=ON \
+                       -DSDL_CLOCK_GETTIME=OFF \
+                       -DSDL_RPATH=OFF \
+                       -DSDL_KMSDRM=OFF \
+                       -DSDL_RENDER_D3D=OFF"
 
 if [ "${DISPLAYSERVER}" = "x11" ]; then
   PKG_DEPENDS_TARGET+=" libX11 libXrandr"
 
   PKG_CMAKE_OPTS_TARGET="${PKG_CMAKE_OPTS_TARGET} \
-                         -DVIDEO_X11=ON \
-                         -DX11_SHARED=ON \
-                         -DVIDEO_X11_XCURSOR=OFF \
-                         -DVIDEO_X11_XINERAMA=OFF \
-                         -DVIDEO_X11_XINPUT=OFF \
-                         -DVIDEO_X11_XRANDR=ON \
-                         -DVIDEO_X11_XSCRNSAVER=OFF \
-                         -DVIDEO_X11_XSHAPE=OFF \
-                         -DVIDEO_X11_XVM=OFF"
+                         -DSDL_X11=ON \
+                         -DSDL_X11_SHARED=ON \
+                         -DSDL_X11_XCURSOR=OFF \
+                         -DSDL_X11_XINERAMA=OFF \
+                         -DSDL_X11_XINPUT=OFF \
+                         -DSDL_X11_XRANDR=ON \
+                         -DSDL_X11_XSCRNSAVER=OFF \
+                         -DSDL_X11_XSHAPE=OFF \
+                         -DSDL_X11_XVM=OFF"
 else
   PKG_CMAKE_OPTS_TARGET="${PKG_CMAKE_OPTS_TARGET} \
-                         -DVIDEO_X11=OFF"
+                         -DSDL_X11=OFF"
 fi
 
 if [ ! "${OPENGL}" = "no" ]; then
   PKG_DEPENDS_TARGET+=" ${OPENGL}"
 
   PKG_CMAKE_OPTS_TARGET="${PKG_CMAKE_OPTS_TARGET} \
-                         -DVIDEO_OPENGL=ON \
-                         -DVIDEO_OPENGLES=OFF"
+                         -DSDL_OPENGL=ON \
+                         -DSDL_OPENGLES=OFF"
 else
   PKG_CMAKE_OPTS_TARGET="${PKG_CMAKE_OPTS_TARGET} \
-                         -DVIDEO_OPENGL=OFF \
-                         -DVIDEO_OPENGLES=ON"
+                         -DSDL_OPENGL=OFF \
+                         -DSDL_OPENGLES=ON"
 fi
 
 if [ "${PULSEAUDIO_SUPPORT}" = yes ]; then
   PKG_DEPENDS_TARGET+=" pulseaudio"
 
   PKG_CMAKE_OPTS_TARGET="${PKG_CMAKE_OPTS_TARGET} \
-                         -DPULSEAUDIO=ON \
-                         -DPULSEAUDIO_SHARED=ON"
+                         -DSDL_PULSEAUDIO=ON \
+                         -DSDL_PULSEAUDIO_SHARED=ON"
 else
   PKG_CMAKE_OPTS_TARGET="${PKG_CMAKE_OPTS_TARGET} \
-                         -DPULSEAUDIO=OFF \
-                         -DPULSEAUDIO_SHARED=OFF"
+                         -DSDL_PULSEAUDIO=OFF \
+                         -DSDL_PULSEAUDIO_SHARED=OFF"
 fi
 
 post_makeinstall_target() {
