d82f878f7d71b2dbd0cadc9c4dc1a3f95ef2ea4b
Rudi Heitbaum <rudi@heitbaum.com>
2022-08-23 14:27:00 +0000

    libbpf: update to 1.0.1
    release notes:
    - https://github.com/libbpf/libbpf/releases/tag/v1.0.0
    - https://github.com/libbpf/libbpf/releases/tag/v1.0.1

--- packages/devel/libbpf/package.mk
+++ packages/devel/libbpf/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libbpf"
-PKG_VERSION="0.8.1"
-PKG_SHA256="7bda8187efc619d1eb20a1ba5ab949dd68d40dd44945310c91ac0f915fa4a42b"
+PKG_VERSION="1.0.1"
+PKG_SHA256="3d6afde67682c909e341bf194678a8969f17628705af25f900d5f68bd299cb03"
 PKG_LICENSE="LGPL-2.1"
 PKG_SITE="https://github.com/libbpf/libbpf"
 PKG_URL="https://github.com/libbpf/libbpf/archive/refs/tags/v${PKG_VERSION}.tar.gz"
