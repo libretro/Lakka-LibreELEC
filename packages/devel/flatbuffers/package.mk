c6b9f4c2b487aad4e854dbaf3dbde1fa54bcf18f
Rudi Heitbaum <rudi@heitbaum.com>
2022-08-24 12:58:21 +0000

    flatbuffers: update to 2.0.7

--- packages/devel/flatbuffers/package.mk
+++ packages/devel/flatbuffers/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="flatbuffers"
-PKG_VERSION="2.0.6"
-PKG_SHA256="e2dc24985a85b278dd06313481a9ca051d048f9474e0f199e372fea3ea4248c9"
+PKG_VERSION="2.0.7"
+PKG_SHA256="4c7986174dc3941220bf14feaacaad409c3e1526d9ad7f490366fede9a6f43fa"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://github.com/google/flatbuffers"
 PKG_URL="https://github.com/google/flatbuffers/archive/v${PKG_VERSION}.tar.gz"
