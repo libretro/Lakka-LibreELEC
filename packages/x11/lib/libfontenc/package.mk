c73637159a32d73e30a92a6ae751a4b48e5ad957
Rudi Heitbaum <rudi@heitbaum.com>
2022-08-27 09:31:35 +0000

    libfontenc: update to 1.1.5 and https

--- packages/x11/lib/libfontenc/package.mk
+++ packages/x11/lib/libfontenc/package.mk
@@ -3,11 +3,11 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libfontenc"
-PKG_VERSION="1.1.4"
-PKG_SHA256="2cfcce810ddd48f2e5dc658d28c1808e86dcf303eaff16728b9aa3dbc0092079"
+PKG_VERSION="1.1.5"
+PKG_SHA256="43ef0387090ea526faf8d3b0907ee0e4be0fb433c17e0b1bdb8433aa1de2b12c"
 PKG_LICENSE="OSS"
-PKG_SITE="http://www.X.org"
-PKG_URL="http://xorg.freedesktop.org/archive/individual/lib/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
+PKG_SITE="https://www.X.org"
+PKG_URL="https://xorg.freedesktop.org/archive/individual/lib/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain util-macros zlib font-util xorgproto"
 PKG_LONGDESC="Libfontenc is a library which helps font libraries portably determine and deal with different encodings of fonts."
 PKG_BUILD_FLAGS="+pic"
