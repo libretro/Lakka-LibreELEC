d23269f79d3b870c615464c5e5c1b028b389259c
Rudi Heitbaum <rudi@heitbaum.com>
2022-08-28 02:12:41 +0000

    xorgproto: drop PKG_TOOLCHAIN hardcoding - was already meson and https

--- packages/x11/proto/xorgproto/package.mk
+++ packages/x11/proto/xorgproto/package.mk
@@ -5,11 +5,10 @@ PKG_NAME="xorgproto"
 PKG_VERSION="2022.2"
 PKG_SHA256="5d13dbf2be08f95323985de53352c4f352713860457b95ccaf894a647ac06b9e"
 PKG_LICENSE="OSS"
-PKG_SITE="http://www.X.org"
+PKG_SITE="https://www.X.org"
 PKG_URL="https://xorg.freedesktop.org/archive/individual/proto/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain util-macros"
 PKG_LONGDESC="combined X.Org X11 Protocol headers"
-PKG_TOOLCHAIN="meson"
 
 PKG_MESON_OPTS_TARGET="-Dlegacy=false \
                        -Dpkgconfigdatadir=/usr/lib/pkgconfig"
