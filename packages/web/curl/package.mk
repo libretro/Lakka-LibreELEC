c950d033813ce6cfe985b8cae978dd3ac64a61ac
Rudi Heitbaum <rudi@heitbaum.com>
2022-08-31 10:20:03 +0000

    curl: update to 7.85.0
    https://curl.se/changes.html#7_85_0
    https://daniel.haxx.se/blog/2022/08/31/curl-7-85-0-for-you/
    https://youtu.be/I4vYSKjvHGE

--- packages/web/curl/package.mk
+++ packages/web/curl/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="curl"
-PKG_VERSION="7.84.0"
-PKG_SHA256="2d118b43f547bfe5bae806d8d47b4e596ea5b25a6c1f080aef49fbcd817c5db8"
+PKG_VERSION="7.85.0"
+PKG_SHA256="88b54a6d4b9a48cb4d873c7056dcba997ddd5b7be5a2d537a4acb55c20b04be6"
 PKG_LICENSE="MIT"
 PKG_SITE="https://curl.haxx.se"
 PKG_URL="https://curl.haxx.se/download/${PKG_NAME}-${PKG_VERSION}.tar.xz"
