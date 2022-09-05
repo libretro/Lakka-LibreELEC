c7fe617cd28b423ac5664aee50324c5da3a2f506
Rudi Heitbaum <rudi@heitbaum.com>
2022-09-05 09:42:11 +0000

    u-boot-tools: use u-boot download and version

--- packages/tools/u-boot-tools/package.mk
+++ packages/tools/u-boot-tools/package.mk
@@ -2,13 +2,18 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="u-boot-tools"
-PKG_VERSION="2022.07"
-PKG_SHA256="92b08eb49c24da14c1adbf70a71ae8f37cc53eeb4230e859ad8b6733d13dcf5e"
+PKG_VERSION="$(get_pkg_version u-boot)"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.denx.de/wiki/U-Boot"
-PKG_URL="https://ftp.denx.de/pub/u-boot/u-boot-${PKG_VERSION}.tar.bz2"
+PKG_URL=""
 PKG_DEPENDS_HOST="ccache:host bison:host flex:host openssl:host pkg-config:host"
 PKG_LONGDESC="Das U-Boot is a cross-platform bootloader for embedded systems."
+PKG_DEPENDS_UNPACK+=" u-boot"
+
+unpack() {
+  mkdir -p ${PKG_BUILD}
+  tar --strip-components=1 -xf ${SOURCES}/u-boot/u-boot-${PKG_VERSION}.tar.bz2 -C ${PKG_BUILD}
+}
 
 make_host() {
   make qemu-x86_64_defconfig HOSTCC="${HOST_CC}" HOSTCFLAGS="-I${TOOLCHAIN}/include" HOSTLDFLAGS="${HOST_LDFLAGS}"
