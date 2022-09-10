93efc4dab70c7acf4127c1c2669c2926373e7737
Alex Bee <knaerzche@gmail.com>
2022-09-11 00:30:07 +0200

    atf: add support for Rockchip platform

--- packages/tools/atf/package.mk
+++ packages/tools/atf/package.mk
@@ -14,6 +14,17 @@ PKG_TOOLCHAIN="manual"
 
 [ -n "${KERNEL_TOOLCHAIN}" ] && PKG_DEPENDS_TARGET+=" gcc-${KERNEL_TOOLCHAIN}:host"
 
+if [ "${PROJECT}" = "Rockchip" -a "${DEVICE}" = "RK3399" ]; then
+  PKG_DEPENDS_TARGET+=" gcc-arm-none-eabi:host"
+  export M0_CROSS_COMPILE="${TOOLCHAIN}/bin/arm-none-eabi-"
+fi
+
+if [ "${PROJECT}" = "Rockchip" ]; then
+  BL31="bl31/bl31.elf"
+else
+  BL31="bl31.bin"
+fi
+
 make_target() {
   if [ "${DEVICE}" = "iMX8" ]; then
     CROSS_COMPILE="${TARGET_KERNEL_PREFIX}" LDFLAGS="--no-warn-rwx-segments" CFLAGS="--param=min-pagesize=0" make PLAT=${ATF_PLATFORM} bl31
@@ -24,5 +35,5 @@ make_target() {
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/share/bootloader
-  cp -a build/${ATF_PLATFORM}/release/bl31.bin ${INSTALL}/usr/share/bootloader
+  cp -a build/${ATF_PLATFORM}/release/${BL31} ${INSTALL}/usr/share/bootloader
 }
