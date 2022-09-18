9c68239d833f2e760ab0fb578816ed65f41807cc
ntemis <ierokipides.dem@gmail.com>
2022-09-19 02:03:50 +0300

    Reverting vulkan updated packages
    This reverts commit ee8318ff492b06cbd03785459548c8d3b3d83d20.

---
+++ packages/lakka/lakka_depends/vulkan-headers/package.mk
@@ -0,0 +1,8 @@
+PKG_NAME="vulkan-headers"
+PKG_VERSION="1.2.187"
+PKG_SHA256="37f399aef2dbeb0eb979139f41a0fe411b3dff77e70be85e93318a7b833557de"
+PKG_LICENSE="Apache 2.0"
+PKG_SITE="https://www.khronos.org"
+PKG_URL="https://github.com/KhronosGroup/Vulkan-Headers/archive/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain cmake:host Python3"
+PKG_LONGDESC="Vulkan Installable Client Driver (ICD) Loader."
