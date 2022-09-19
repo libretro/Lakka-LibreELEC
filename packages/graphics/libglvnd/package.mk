3ffa75164e28c5b33a66eed80fd4aafe01dc6a80
David Lawson <dmrlawson@gmail.com>
2022-09-19 21:50:04 +0200

    libglvnd: add back exception for L4T and Ayn

--- packages/graphics/libglvnd/package.mk
+++ packages/graphics/libglvnd/package.mk
@@ -22,29 +22,40 @@ pre_configure_target(){
   if [ "${OPENGLES_SUPPORT}" = "no" ]; then
     PKG_MESON_OPTS_TARGET+=" -Dgles2=false"
   fi
+
+  if [ "${DEVICE}" = "Odin" ]; then
+    PKG_MESON_OPTS_TARGET="-Dheaders=true \
+                         -Dx11=enabled \
+                         -Degl=true \
+                         -Dglx=enabled \
+                         -Dgles1=true \
+                         -Dgles2=true"
+  fi
 }
 
 post_makeinstall_target() {
+if [ ! "${PROJECT}" = "L4T" -a ! "${PROJECT}" = "Ayn" ]; then
   if [ "${DISPLAYSERVER}" = "x11" ]; then
-    # Remove old symlinks to libGL.so.1.7.0 (GLVND)
-    safe_remove              ${INSTALL}/usr/lib/libGL.so
-    safe_remove              ${INSTALL}/usr/lib/libGL.so.1
-    # Create new symlinks to /var/lib/libGL.so
-    ln -sf libGL.so.1        ${INSTALL}/usr/lib/libGL.so
-    ln -sf /var/lib/libGL.so ${INSTALL}/usr/lib/libGL.so.1
-    # Create new symlink to libGL.so.1.7.0
-    ln -sf libGL.so.1.7.0    ${INSTALL}/usr/lib/libGL_glvnd.so.1
+      # Remove old symlinks to libGL.so.1.7.0 (GLVND)
+      safe_remove              ${INSTALL}/usr/lib/libGL.so
+      safe_remove              ${INSTALL}/usr/lib/libGL.so.1
+      # Create new symlinks to /var/lib/libGL.so
+      ln -sf libGL.so.1        ${INSTALL}/usr/lib/libGL.so
+      ln -sf /var/lib/libGL.so ${INSTALL}/usr/lib/libGL.so.1
+      # Create new symlink to libGL.so.1.7.0
+      ln -sf libGL.so.1.7.0    ${INSTALL}/usr/lib/libGL_glvnd.so.1
 
-    # Remove old symlinks to libGLX.so.0.0.0 (GLVND)
-    safe_remove               ${INSTALL}/usr/lib/libGLX.so
-    safe_remove               ${INSTALL}/usr/lib/libGLX.so.0
-    # Create new symlinks to /var/lib/libGLX.so
-    ln -sf libGLX.so.0        ${INSTALL}/usr/lib/libGLX.so
-    ln -sf /var/lib/libGLX.so ${INSTALL}/usr/lib/libGLX.so.0
-    # Create new symlink to libGLX.so.0.0.0
-    ln -sf libGLX.so.0.0.0    ${INSTALL}/usr/lib/libGLX_glvnd.so.0
+      # Remove old symlinks to libGLX.so.0.0.0 (GLVND)
+      safe_remove               ${INSTALL}/usr/lib/libGLX.so
+      safe_remove               ${INSTALL}/usr/lib/libGLX.so.0
+      # Create new symlinks to /var/lib/libGLX.so
+      ln -sf libGLX.so.0        ${INSTALL}/usr/lib/libGLX.so
+      ln -sf /var/lib/libGLX.so ${INSTALL}/usr/lib/libGLX.so.0
+      # Create new symlink to libGLX.so.0.0.0
+      ln -sf libGLX.so.0.0.0    ${INSTALL}/usr/lib/libGLX_glvnd.so.0
 
-    # indirect rendering
-    ln -sf /var/lib/libGLX_indirect.so.0 ${INSTALL}/usr/lib/libGLX_indirect.so.0
+      # indirect rendering
+      ln -sf /var/lib/libGLX_indirect.so.0 ${INSTALL}/usr/lib/libGLX_indirect.so.0
+    fi
   fi
 }
