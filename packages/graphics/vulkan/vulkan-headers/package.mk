22528351a066bfbfd3b88d6dcc7a693d9a423be8
Rudi Heitbaum <rudi@heitbaum.com>
2022-09-23 06:14:00 +0000

    vulkan-headers: update to 1.3.229

--- packages/graphics/vulkan/vulkan-headers/package.mk
+++ packages/graphics/vulkan/vulkan-headers/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="vulkan-headers"
-PKG_VERSION="1.3.228"
-PKG_SHA256="e5a4dcdfb9361ee8d1b9401ed337b65e7c34a54224b99a9cde303a650a9cb350"
+PKG_VERSION="1.3.229"
+PKG_SHA256="fe620275ca1e29501dcb3f54c69cc011b6d9c3296408fac4e18dc491a1be754f"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://github.com/KhronosGroup/Vulkan-Headers"
 PKG_URL="https://github.com/KhronosGroup/Vulkan-Headers/archive/v${PKG_VERSION}.tar.gz"
