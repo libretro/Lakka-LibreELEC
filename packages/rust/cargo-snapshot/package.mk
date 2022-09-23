4f525d5ff47f1a855e5f3a1f37955bfd015da7c4
Rudi Heitbaum <rudi@heitbaum.com>
2022-09-23 08:23:49 +0000

    cargo-snapshot update to 1.64.0

--- packages/rust/cargo-snapshot/package.mk
+++ packages/rust/cargo-snapshot/package.mk
@@ -3,7 +3,7 @@
 
 PKG_NAME="cargo-snapshot"
 PKG_VERSION="$(get_pkg_version rust)"
-PKG_SHA256="f370d12e4c11f0c835becb738bcf00d363f29b76f8b424b4dcb005abcf15fc9a"
+PKG_SHA256="21434d83a30ad3fa4e4831487c5574a20b07dd57d213b26e1246290c57d4ec41"
 PKG_LICENSE="MIT"
 PKG_SITE="https://www.rust-lang.org"
 PKG_URL="https://static.rust-lang.org/dist/cargo-${PKG_VERSION}-${MACHINE_HARDWARE_NAME}-unknown-linux-gnu.tar.xz"
