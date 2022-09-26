7c4f7969e71cb99b116d41f58d0cfc93bdab5b5a
Rudi Heitbaum <rudi@heitbaum.com>
2022-09-26 04:47:39 +0000

    libopenmpt: update to 0.6.6

--- packages/audio/libopenmpt/package.mk
+++ packages/audio/libopenmpt/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libopenmpt"
-PKG_VERSION="0.6.5"
-PKG_SHA256="f22abe977cdae405f685b75150e7fb155b2c7896b4700fd54abe68840f66e9c0"
+PKG_VERSION="0.6.6"
+PKG_SHA256="6ddb9e26a430620944891796fefb1bbb38bd9148f6cfc558810c0d3f269876c7"
 PKG_LICENSE="BSD"
 PKG_SITE="https://lib.openmpt.org/libopenmpt/"
 PKG_URL="https://lib.openmpt.org/files/libopenmpt/src/${PKG_NAME}-${PKG_VERSION}+release.autotools.tar.gz"
