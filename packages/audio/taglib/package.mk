0c7c05a60e583e91006289e6da1b42d5283edd7a
Rudi Heitbaum <rudi@heitbaum.com>
2022-09-30 10:51:05 +0000

    taglib: update to 1.13 and Urls and cmake options
    release notes:
    - https://taglib.org

--- packages/audio/taglib/package.mk
+++ packages/audio/taglib/package.mk
@@ -1,18 +1,21 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="taglib"
-PKG_VERSION="1.12"
-PKG_SHA256="7fccd07669a523b07a15bd24c8da1bbb92206cb19e9366c3692af3d79253b703"
+PKG_VERSION="1.13"
+PKG_SHA256="58f08b4db3dc31ed152c04896ee9172d22052bc7ef12888028c01d8b1d60ade0"
 PKG_LICENSE="LGPL"
-PKG_SITE="http://taglib.github.com/"
-PKG_URL="http://taglib.github.io/releases/${PKG_NAME}-${PKG_VERSION}.tar.gz"
+PKG_SITE="https://taglib.org"
+PKG_URL="https://taglib.org/releases/${PKG_NAME}-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain cmake:host zlib"
 PKG_LONGDESC="TagLib is a library for reading and editing the meta-data of several popular audio formats."
 
-PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=OFF \
-                       -DWITH_MP4=ON \
-                       -DWITH_ASF=ON"
+PKG_CMAKE_OPTS_TARGET="-DBUILD_EXAMPLES=OFF \
+                       -DBUILD_SHARED_LIBS=OFF \
+                       -DBUILD_TESTING=OFF \
+                       -DENABLE_CCACHE=ON \
+                       -DWITH_ZLIB=ON"
 
 post_makeinstall_target() {
   rm -rf ${INSTALL}/usr/bin
