36fbabbb31f72c3a67743b6dc39931fd569e4e89
Rudi Heitbaum <rudi@heitbaum.com>
2022-10-03 11:04:23 +0000

    expat: update to 2.4.9 and URIs
    https://github.com/libexpat/libexpat/blob/R_2_4_9/expat/Changes

--- packages/textproc/expat/package.mk
+++ packages/textproc/expat/package.mk
@@ -3,11 +3,11 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="expat"
-PKG_VERSION="2.4.8"
-PKG_SHA256="a247a7f6bbb21cf2ca81ea4cbb916bfb9717ca523631675f99b3d4a5678dcd16"
+PKG_VERSION="2.4.9"
+PKG_SHA256="6e8c0728fe5c7cd3f93a6acce43046c5e4736c7b4b68e032e9350daa0efc0354"
 PKG_LICENSE="OSS"
-PKG_SITE="http://expat.sourceforge.net/"
-PKG_URL="https://github.com/libexpat/libexpat/releases/download/R_${PKG_VERSION//./_}/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
+PKG_SITE="https://libexpat.github.io"
+PKG_URL="https://github.com/libexpat/libexpat/releases/download/R_${PKG_VERSION//./_}/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_HOST="toolchain:host"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Expat is an XML parser library written in C."
