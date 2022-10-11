edd569c1a7966f28731fd4434b04431fa772afd4
SupervisedThinking <supervisedthinking@gmail.com>
2022-10-11 11:10:03 +0200

    nvidia: update to 515.76
    - fix build with Linux 6.0

--- packages/graphics/nvidia/package.mk
+++ packages/graphics/nvidia/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="nvidia"
-PKG_VERSION="515.65.01"
-PKG_SHA256="89e9c22b3903a2360bb18a7f1220dc52234b2cb928bb9dd6690236cf2b024695"
+PKG_VERSION="515.76"
+PKG_SHA256="5059943d57f922a7a873356dcdbb596ba034c24431eb5ca88d1e0064bf3fd98a"
 PKG_ARCH="x86_64"
 PKG_LICENSE="nonfree"
 PKG_SITE="https://www.nvidia.com/en-us/drivers/unix/"
