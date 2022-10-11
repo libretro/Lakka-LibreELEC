ac250de91140ddabce2d13838d8ad8389e1cb916
Rudi Heitbaum <rudi@heitbaum.com>
2022-10-11 08:16:41 +0000

    ethtool: update to 6.0
    log:
    - https://git.kernel.org/pub/scm/network/ethtool/ethtool.git

--- packages/network/ethtool/package.mk
+++ packages/network/ethtool/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="ethtool"
-PKG_VERSION="5.19"
-PKG_SHA256="3b752a3329827907ac3812f2831dfecf51c8c41c55d2d69cfb9c53ca06449fc6"
+PKG_VERSION="6.0"
+PKG_SHA256="d5446c93de570ce68f3b1ea69dbfa12fcfd67fc19897f655d3f18231e2b818d6"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.kernel.org/pub/software/network/ethtool/"
 PKG_URL="https://www.kernel.org/pub/software/network/ethtool/${PKG_NAME}-${PKG_VERSION}.tar.xz"
