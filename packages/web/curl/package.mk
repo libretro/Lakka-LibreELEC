c853bf67c810d026ddb97dd496407166d020ed1c
Rudi Heitbaum <rudi@heitbaum.com>
2022-10-27 11:16:59 +0000

    curl: update to 7.86.0
    release notes:
    - https://curl.se/changes.html

--- packages/web/curl/package.mk
+++ packages/web/curl/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="curl"
-PKG_VERSION="7.85.0"
-PKG_SHA256="88b54a6d4b9a48cb4d873c7056dcba997ddd5b7be5a2d537a4acb55c20b04be6"
+PKG_VERSION="7.86.0"
+PKG_SHA256="2d61116e5f485581f6d59865377df4463f2e788677ac43222b496d4e49fb627b"
 PKG_LICENSE="MIT"
 PKG_SITE="https://curl.haxx.se"
 PKG_URL="https://curl.haxx.se/download/${PKG_NAME}-${PKG_VERSION}.tar.xz"
