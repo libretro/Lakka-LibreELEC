05d019dd5c0e3a37a871ad51c0e654259ec2c6fa
Rudi Heitbaum <rudi@heitbaum.com>
2022-11-01 23:53:33 +0000

    nv-codec-headers: update to 11.1.5.2

--- packages/multimedia/nv-codec-headers/package.mk
+++ packages/multimedia/nv-codec-headers/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="nv-codec-headers"
-PKG_VERSION="11.1.5.1"
-PKG_SHA256="d095fbd56aa93772471a323be0ebe65504a0f43f06c76a30b6d25da77b06ae9c"
+PKG_VERSION="11.1.5.2"
+PKG_SHA256="576df78bad704e2854991100bea99e974759304ac1411c02707ebc95a425191b"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/FFmpeg/nv-codec-headers"
 PKG_URL="https://github.com/FFmpeg/nv-codec-headers/archive/n${PKG_VERSION}.tar.gz"
