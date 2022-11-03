bb2fe2a6bce5a20b2a1b038ce97d205c04c0e672
Rudi Heitbaum <rudi@heitbaum.com>
2022-11-03 20:46:24 +0000

    rust: update to 1.65.0

--- packages/rust/rust/package.mk
+++ packages/rust/rust/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="rust"
-PKG_VERSION="1.64.0"
-PKG_SHA256="b3cd9f481e1a2901bf6f3808d30c69cc4ea80d93c4cc4e2ed52258b180381205"
+PKG_VERSION="1.65.0"
+PKG_SHA256="5828bb67f677eabf8c384020582b0ce7af884e1c84389484f7f8d00dd82c0038"
 PKG_LICENSE="MIT"
 PKG_SITE="https://www.rust-lang.org"
 PKG_URL="https://static.rust-lang.org/dist/rustc-${PKG_VERSION}-src.tar.gz"
