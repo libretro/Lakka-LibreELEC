5687afe19bf002f40034c9f298c979331520a108
Rudi Heitbaum <rudi@heitbaum.com>
2022-11-04 08:15:34 +0000

    fakeroot: update to 1.30.1

--- packages/devel/fakeroot/package.mk
+++ packages/devel/fakeroot/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="fakeroot"
-PKG_VERSION="1.30"
-PKG_SHA256="d7cdcf32c3451b577d22250eb0ebd5c02282b5d8de01a6fe1656d192c4539579"
+PKG_VERSION="1.30.1"
+PKG_SHA256="32ebb1f421aca0db7141c32a8c104eb95d2b45c393058b9435fbf903dd2b6a75"
 PKG_LICENSE="GPL3"
 PKG_SITE="https://tracker.debian.org/pkg/fakeroot"
 PKG_URL="http://ftp.debian.org/debian/pool/main/f/fakeroot/${PKG_NAME}_${PKG_VERSION}.orig.tar.gz"
