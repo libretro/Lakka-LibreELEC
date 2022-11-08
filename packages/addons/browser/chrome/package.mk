d1acad1e5792c1a9595a51d1db8a6062c6b1d308
Rudi Heitbaum <rudi@heitbaum.com>
2022-11-08 10:04:24 +0000

    chrome: update to 109.0.5414.74 and addon (1)

--- packages/addons/browser/chrome/package.mk
+++ packages/addons/browser/chrome/package.mk
@@ -4,8 +4,8 @@
 PKG_NAME="chrome"
 PKG_VERSION="1.0"
 # curl -s http://dl.google.com/linux/chrome/deb/dists/stable/main/binary-amd64/Packages | grep -B 1 Version
-PKG_VERSION_NUMBER="103.0.5060.134"
-PKG_REV="0"
+PKG_VERSION_NUMBER="109.0.5414.74"
+PKG_REV="1"
 PKG_ARCH="x86_64"
 PKG_LICENSE="Custom"
 PKG_SITE="http://www.google.com/chrome"
