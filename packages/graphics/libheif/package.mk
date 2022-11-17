38895e8ebbda89fae9fcde1b9bba4ce0701c03cd
Rudi Heitbaum <rudi@heitbaum.com>
2022-11-17 10:23:34 +0000

    libheif: update to 1.14.0

--- packages/graphics/libheif/package.mk
+++ packages/graphics/libheif/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libheif"
-PKG_VERSION="1.13.0"
-PKG_SHA256="c20ae01bace39e89298f6352f1ff4a54b415b33b9743902da798e8a1e51d7ca1"
+PKG_VERSION="1.14.0"
+PKG_SHA256="9a2b969d827e162fa9eba582ebd0c9f6891f16e426ef608d089b1f24962295b5"
 PKG_LICENSE="LGPLv3"
 PKG_SITE="https://www.libde265.org"
 PKG_URL="https://github.com/strukturag/libheif/releases/download/v${PKG_VERSION}/libheif-${PKG_VERSION}.tar.gz"
