c8cc75db390990bd446a2bb13dcbf4950e5e57ed
mglae <mglmail@arcor.de>
2022-11-28 19:53:47 +0100

    unix_ar: initial package. Python module for ar archives
    Co-authored-by: CvH <1355173+CvH@users.noreply.github.com>

---
+++ packages/addons/addon-depends/chrome-depends/unix_ar/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="unix_ar"
+PKG_VERSION="0.2.1"
+PKG_SHA256="4e1c6e8fe5255b7babe93b9af51532ba09c6946eee413c5db00cab3878d7ed2f"
+PKG_LICENSE="BSD 3-Clause"
+PKG_SITE="https://github.com/getninjas/unix_ar"
+PKG_URL="https://github.com/getninjas/unix_ar/archive/refs/tags/${PKG_VERSION}.tar.gz"
+PKG_TOOLCHAIN="manual"
+PKG_LONGDESC="Python: This packages allows the reading and writing of AR archive files."
