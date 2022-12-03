918844a388a831f5c9ce1966fb41ae19c6e1725b
Rudi Heitbaum <rudi@heitbaum.com>
2022-12-03 01:10:25 +0000

    linux: update to 6.0.11

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -28,8 +28,8 @@ case "${LINUX}" in
     PKG_SOURCE_NAME="linux-${LINUX}-${PKG_VERSION}.tar.gz"
     ;;
   *)
-    PKG_VERSION="6.0.10"
-    PKG_SHA256="39e57fcd84cd70bfa3e1a4185d3aa0ed7f1432f24c6548d16326b0c3c9541dd0"
+    PKG_VERSION="6.0.11"
+    PKG_SHA256="2bae6131e64971e1e34ff395fa542971134c857bdb0b29069ab847c7c9a9c762"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v${PKG_VERSION/.*/}.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;
