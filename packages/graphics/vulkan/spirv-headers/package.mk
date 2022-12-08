5874984a20d2537bcc4a77afa955ad19ccb9ff78
Rudi Heitbaum <rudi@heitbaum.com>
2022-12-08 11:56:38 +0000

    spirv-headers: update to githash 1d31a10

--- packages/graphics/vulkan/spirv-headers/package.mk
+++ packages/graphics/vulkan/spirv-headers/package.mk
@@ -6,8 +6,8 @@ PKG_NAME="spirv-headers"
 # The SPIRV-Headers pkg_version needs to match the compatible (known_good) glslang pkg_version.
 # https://raw.githubusercontent.com/KhronosGroup/glslang/${PKG_VERSION}/known_good.json
 # When updating glslang pkg_version please update to the known_good spirv-headers pkg_version.
-PKG_VERSION="85a1ed200d50660786c1a88d9166e871123cce39"
-PKG_SHA256="9729304d0915e758c5ea2a1c60b55a123d976f172c0a8dae3162ad23c77ef33b"
+PKG_VERSION="1d31a100405cf8783ca7a31e31cdd727c9fc54c3"
+PKG_SHA256="ec09b682c93bffc6e3d58459840a0306b6b9360e56e797c4243d2d7b785dea74"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://github.com/KhronosGroup/SPIRV-headers"
 PKG_URL="https://github.com/KhronosGroup/SPIRV-headers/archive/${PKG_VERSION}.tar.gz"
