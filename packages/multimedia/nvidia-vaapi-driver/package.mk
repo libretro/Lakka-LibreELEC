63972821f4d247b3f07363ffbce20d07c24877cc
Rudi Heitbaum <rudi@heitbaum.com>
2022-12-17 04:01:54 +0000

    nvidia-vaapi-driver: update to 0.0.8

--- packages/multimedia/nvidia-vaapi-driver/package.mk
+++ packages/multimedia/nvidia-vaapi-driver/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="nvidia-vaapi-driver"
-PKG_VERSION="0.0.7"
-PKG_SHA256="c19e35d41cd39a93c2a1af99065d51719a994fdd89f8b0716914cba7e716718e"
+PKG_VERSION="0.0.8"
+PKG_SHA256="a396e8b48ec9bc58cec50d5b049e401ddc59083195edf2b8669e9788f4d44293"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/elFarto/nvidia-vaapi-driver"
 PKG_URL="https://github.com/elFarto/nvidia-vaapi-driver/archive/v${PKG_VERSION}.tar.gz"
