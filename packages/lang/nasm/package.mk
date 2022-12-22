a70986bce7ee98c26c017374a6ff00ab580e63b7
Rudi Heitbaum <rudi@heitbaum.com>
2022-12-22 11:12:06 +0000

    nasm: update to 2.16.01 and HSTS

--- packages/lang/nasm/package.mk
+++ packages/lang/nasm/package.mk
@@ -3,11 +3,15 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="nasm"
-PKG_VERSION="2.15.05"
-PKG_SHA256="3caf6729c1073bf96629b57cee31eeb54f4f8129b01902c73428836550b30a3f"
+PKG_VERSION="2.16.01"
+PKG_SHA256="c77745f4802375efeee2ec5c0ad6b7f037ea9c87c92b149a9637ff099f162558"
 PKG_ARCH="x86_64"
 PKG_LICENSE="BSD"
 PKG_SITE="https://www.nasm.us/"
-PKG_URL="http://www.nasm.us/pub/nasm/releasebuilds/${PKG_VERSION}/nasm-${PKG_VERSION}.tar.xz"
+PKG_URL="https://www.nasm.us/pub/nasm/releasebuilds/${PKG_VERSION}/nasm-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_HOST="ccache:host"
 PKG_LONGDESC="The Netwide Assembler, NASM, is an 80x86 and x86-64 assembler designed for portability and modularity."
+
+pre_configure_host() {
+  HOST_CONFIGURE_OPTS=$(echo ${HOST_CONFIGURE_OPTS} | sed -e "s|--disable-static||" -e "s|--enable-shared||")
+}
