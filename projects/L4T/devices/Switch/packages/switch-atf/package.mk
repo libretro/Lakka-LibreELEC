e03eeaa8d3af3af763af0434c14183401a0a928c
Ronald Brown <rbrown4014@yahoo.com>
2022-12-22 04:08:21 +0100

    Switch: Switch to new Bootloader.

---
+++ projects/L4T/devices/Switch/packages/switch-atf/package.mk
@@ -0,0 +1,32 @@
+PKG_NAME="switch-atf"
+PKG_VERSION="45110f9628ea9a178d5adbec15f9a7eb2f9bc26c"
+PKG_GIT_CLONE_BRANCH="switch-v3-atf2.6"
+PKG_DEPENDS_TARGET="toolchain gcc:host dtc:host"
+PKG_SITE="https://gitlab.com/switchroot/bootstack/switch-atf"
+PKG_URL="${PKG_SITE}.git"
+PKG_TOOLCHAIN="make"
+
+make_target() {
+  ARCH=arm64 \
+   CROSS_COMPILE="${TARGET_KERNEL_PREFIX}" \
+   LDFLAGS="--emit-relocs" \
+   CFLAGS="-fno-pic -fno-stack-protector -Wno-deprecated-declarations -Wno-unused-function" \
+   make bl31 \
+   PLAT=tegra \
+   TARGET_SOC=t210 \
+   TZDRAM_BASE=0xFFF00000 \
+   RESET_TO_BL31=1 \
+   COLD_BOOT_SINGLE_CPU=1 \
+   PROGRAMMABLE_RESET_ADDRESS=1 \
+   ENABLE_STACK_PROTECTOR=none \
+   SDEI_SUPPORT=0 \
+   CRASH_REPORTING=1 \
+   ENABLE_ASSERTIONS=1 \
+   LOG_LEVEL=0 \
+   PLAT_LOG_LEVEL_ASSERT=0
+}
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/share/bootloader/boot
+    cp build/tegra/t210/release/bl31.bin ${INSTALL}/usr/share/bootloader/boot/bl31.bin
+}
