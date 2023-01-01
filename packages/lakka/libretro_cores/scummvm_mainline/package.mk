7409c4db99750bd70dadc0bef45765fc1e27c673
Giovanni Cascione <43221199+spleen1981@users.noreply.github.com>
2023-01-01 22:02:54 +0100

    Update scummvm_mainline and fix system path (#1803)

--- packages/lakka/libretro_cores/scummvm_mainline/package.mk
+++ packages/lakka/libretro_cores/scummvm_mainline/package.mk
@@ -1,6 +1,6 @@
 
 PKG_NAME="scummvm_mainline"
-PKG_VERSION="98c2b107daebbf3fef8dadf3f32b74b2608e0a4e"
+PKG_VERSION="8d29673b74aae64b3878176bdaa7e701214a0d4d" # v2.6.1.2
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/spleen1981/scummvm-mainline-libretro"
 PKG_URL="$PKG_SITE.git"
@@ -8,12 +8,14 @@ PKG_DEPENDS_TARGET="toolchain"
 PKG_SHORTDESC="Script Creation Utility for Maniac Mansion Virtual Machine"
 PKG_LONGDESC="ScummVM is a program which allows you to run certain classic graphical point-and-click adventure games, provided you already have their data files."
 PKG_TOOLCHAIN="make"
+PKG_LR_UPDATE_TAG="yes"
 
 pre_make_target() {
-  CXXFLAGS+=" -DHAVE_POSIX_MEMALIGN=1"
-  CFLAGS+=" -fPIC"
+
   if [ "${DEVICE}" = "OdroidGoAdvance" ]; then
     PKG_MAKE_OPTS_TARGET+=" platform=oga_a35_neon_hardfloat"
+  else
+    PKG_MAKE_OPTS_TARGET+=" platform=$TARGET_NAME"
   fi
 }
 
@@ -23,10 +25,10 @@ makeinstall_target() {
   cp -v ${PKG_BUILD}/scummvm_mainline_libretro.info ${INSTALL}/usr/lib/libretro/
 
   # unpack files to retroarch-system folder and create basic ini file
-  mkdir -p ${INSTALL}/usr/share/retroarch-system
-  unzip ${PKG_BUILD}/scummvm.zip -d ${INSTALL}/usr/share/retroarch-system
+  mkdir -p ${INSTALL}/usr/share/retroarch/system
+  unzip ${PKG_BUILD}/scummvm.zip -d ${INSTALL}/usr/share/retroarch/system
 
-  cat << EOF > ${INSTALL}/usr/share/retroarch-system/scummvm.ini
+  cat << EOF > ${INSTALL}/usr/share/retroarch/system/scummvm.ini
 [scummvm]
 extrapath=/tmp/system/scummvm/extra
 browser_lastpath=/tmp/system/scummvm/extra
