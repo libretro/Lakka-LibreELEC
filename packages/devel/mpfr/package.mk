9fca4625be02e525c78c167ce640c332be61bc61
Rudi Heitbaum <rudi@heitbaum.com>
2023-01-06 21:04:50 +0000

    mpfr: update to 4.2.0

--- packages/devel/mpfr/package.mk
+++ packages/devel/mpfr/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mpfr"
-PKG_VERSION="4.1.1"
-PKG_SHA256="ffd195bd567dbaffc3b98b23fd00aad0537680c9896171e44fe3ff79e28ac33d"
+PKG_VERSION="4.2.0"
+PKG_SHA256="06a378df13501248c1b2db5aa977a2c8126ae849a9d9b7be2546fb4a9c26d993"
 PKG_LICENSE="LGPL"
 PKG_SITE="http://www.mpfr.org/"
 PKG_URL="http://ftpmirror.gnu.org/mpfr/${PKG_NAME}-${PKG_VERSION}.tar.xz"
