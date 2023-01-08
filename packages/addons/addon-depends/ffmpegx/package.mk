94eb3465fc3703a08b4989045500a3790dd8a9c3
Rudi Heitbaum <rudi@heitbaum.com>
2023-01-08 18:10:09 +0000

    ffmpegx: update to 4.4.2

--- packages/addons/addon-depends/ffmpegx/package.mk
+++ packages/addons/addon-depends/ffmpegx/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="ffmpegx"
-PKG_VERSION="4.4"
-PKG_SHA256="06b10a183ce5371f915c6bb15b7b1fffbe046e8275099c96affc29e17645d909"
+PKG_VERSION="4.4.2"
+PKG_SHA256="af419a7f88adbc56c758ab19b4c708afbcae15ef09606b82b855291f6a6faa93"
 PKG_LICENSE="LGPLv2.1+"
 PKG_SITE="https://ffmpeg.org"
 PKG_URL="https://ffmpeg.org/releases/ffmpeg-${PKG_VERSION}.tar.xz"
