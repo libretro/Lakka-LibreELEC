ec1053faa268167ee03281b697593a9c9700cbe7
Rudi Heitbaum <rudi@heitbaum.com>
2023-01-09 23:31:16 +0000

    libva-utils: update to 2.17.1

--- packages/debug/libva-utils/package.mk
+++ packages/debug/libva-utils/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libva-utils"
-PKG_VERSION="2.17.0"
-PKG_SHA256="607447143f08c6221f3bd5ab7aad874a4e220b6e04254b67433a7b2e97ff98ba"
+PKG_VERSION="2.17.1"
+PKG_SHA256="6ea5993c3eba230a979fa9d35b4cad8df06d4474a773dc0918033bf50353f966"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/01org/libva-utils"
 PKG_URL="https://github.com/intel/libva-utils/archive/${PKG_VERSION}.tar.gz"
