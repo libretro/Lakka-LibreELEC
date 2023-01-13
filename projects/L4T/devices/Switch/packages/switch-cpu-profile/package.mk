1ad43743cc782b7558018df628362dacdda0967d
GavinDarkglider <31815002+GavinDarkglider@users.noreply.github.com>
2023-01-13 09:54:08 -0800

    Switch: Fix Clock speed menus on mariko. (#1822)

--- projects/L4T/devices/Switch/packages/switch-cpu-profile/package.mk
+++ projects/L4T/devices/Switch/packages/switch-cpu-profile/package.mk
@@ -1,12 +1,11 @@
 PKG_NAME="switch-cpu-profile"
-PKG_VERSION="1.1"
+PKG_VERSION="2.0"
 PKG_DEPENDS_TARGET="Python3"
 PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/bin
-    #cp -v ${PKG_DIR}/scripts/cpu-profile ${INSTALL}/usr/bin
-    touch ${INSTALL}/usr/bin/cpu-profile
+    cp -v ${PKG_DIR}/scripts/cpu-profile ${INSTALL}/usr/bin
     chmod +x ${INSTALL}/usr/bin/cpu-profile
 }
 
