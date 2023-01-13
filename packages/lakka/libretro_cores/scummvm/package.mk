35384ad6694cde7d1f0bb6ca7396f43462e3f24f
Giovanni Cascione <43221199+spleen1981@users.noreply.github.com>
2023-01-13 18:50:06 +0100

    move spleen1981/scummvm_mainline to libretro/scummvm (#1819)
    and remove legacy core

--- packages/lakka/libretro_cores/scummvm/package.mk
+++ packages/lakka/libretro_cores/scummvm/package.mk
@@ -1,37 +1,41 @@
-PKG_NAME="scummvm"
-PKG_VERSION="2fb2e4c551c9c1510c56f6e890ee0300b7b3fca3"
-PKG_LICENSE="GPLv2"
-PKG_SITE="https://github.com/libretro/scummvm"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_LONGDESC="ScummVM with libretro backend."
-PKG_TOOLCHAIN="make"
 
-PKG_MAKE_OPTS_TARGET="-C ../backends/platform/libretro/build/"
+PKG_NAME="scummvm"
+PKG_VERSION="a0554745e87361643f1ca3aa820a5073214de935"
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://github.com/libretro/scummvm"
+PKG_URL="$PKG_SITE.git"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SHORTDESC="The ScummVM adventure game engine ported to libretro."
+PKG_LONGDESC="ScummVM is a program which allows you to run certain classic graphical point-and-click adventure games, provided you already have their data files."
+PKG_TOOLCHAIN="make"
+PKG_LR_UPDATE_TAG="yes"
 
 pre_make_target() {
-  CXXFLAGS+=" -DHAVE_POSIX_MEMALIGN=1"
   if [ "${DEVICE}" = "OdroidGoAdvance" ]; then
     PKG_MAKE_OPTS_TARGET+=" platform=oga_a35_neon_hardfloat"
+  else
+    PKG_MAKE_OPTS_TARGET+=" platform=$TARGET_NAME"
   fi
+}
+
+make_target() {
+  make all ${PKG_MAKE_OPTS_TARGET}
 }
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v ../backends/platform/libretro/build/scummvm_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_BUILD}/scummvm_libretro.so ${INSTALL}/usr/lib/libretro/
+  cp -v ${PKG_BUILD}/scummvm_libretro.info ${INSTALL}/usr/lib/libretro/
 
-  # unpack files to retroarch/system folder and create basic ini file
-  if [ -f ${PKG_BUILD}/backends/platform/libretro/aux-data/scummvm.zip ]; then
-    mkdir -p ${INSTALL}/usr/share/retroarch/system
-      unzip ${PKG_BUILD}/backends/platform/libretro/aux-data/scummvm.zip \
-            -d ${INSTALL}/usr/share/retroarch/system
+  # unpack files to retroarch-system folder and create basic ini file
+  mkdir -p ${INSTALL}/usr/share/retroarch/system
+  unzip ${PKG_BUILD}/scummvm.zip -d ${INSTALL}/usr/share/retroarch/system
 
-      cat << EOF > ${INSTALL}/usr/share/retroarch/system/scummvm.ini
+  cat << EOF > ${INSTALL}/usr/share/retroarch/system/scummvm.ini
 [scummvm]
 extrapath=/tmp/system/scummvm/extra
 browser_lastpath=/tmp/system/scummvm/extra
 themepath=/tmp/system/scummvm/theme
 guitheme=scummmodern
 EOF
-  fi
 }
