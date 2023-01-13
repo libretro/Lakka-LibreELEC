24be7d85000597cea7596d21d42331231a31eb41
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2023-01-13 23:07:37 +0100

    dosbox: add core back

---
+++ packages/lakka/libretro_cores/dosbox/package.mk
@@ -0,0 +1,16 @@
+PKG_NAME="dosbox"
+PKG_VERSION="b7b24262c282c0caef2368c87323ff8c381b3102"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/libretro/dosbox-libretro"
+PKG_URL="${PKG_SITE}.git"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="libretro wrapper for the DOSBox emulator"
+PKG_TOOLCHAIN="make"
+
+PKG_MAKE_OPTS_TARGET="-f Makefile.libretro"
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+    cp -v dosbox_libretro.so ${INSTALL}/usr/lib/libretro/
+}
+
