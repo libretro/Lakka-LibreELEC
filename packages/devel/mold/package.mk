668c918a45ae16edcdea3f33337a9ffc51f7a5ee
Rudi Heitbaum <rudi@heitbaum.com>
2023-01-20 19:52:58 +0000

    mold: update to 1.10.1

--- packages/devel/mold/package.mk
+++ packages/devel/mold/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mold"
-PKG_VERSION="1.9.0"
-PKG_SHA256="faf381ba268e714bec7f872de0dd6ea9187ae20b4e12c434a67ac92854701280"
+PKG_VERSION="1.10.1"
+PKG_SHA256="19e4aa16b249b7e6d2e0897aa1843a048a0780f5c76d8d7e643ab3a4be1e4787"
 PKG_LICENSE="AGPL-3.0-or-later"
 PKG_SITE="https://github.com/rui314/mold"
 PKG_URL="https://github.com/rui314/mold/archive/refs/tags/v${PKG_VERSION}.tar.gz"
