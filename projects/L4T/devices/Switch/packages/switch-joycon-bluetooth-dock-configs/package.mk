15583e38de9a163691bcf5f87b216a2053e8a58a
Ronald Brown <rbrown4014@yahoo.com>
2023-01-23 07:54:59 +0100

    Switch: Bump switch specific package versions, and add changes needed to make built in lakka clock speed menu work properly. Also hide gamemode.

--- projects/L4T/devices/Switch/packages/switch-joycon-bluetooth-dock-configs/package.mk
+++ projects/L4T/devices/Switch/packages/switch-joycon-bluetooth-dock-configs/package.mk
@@ -1,4 +1,5 @@
 PKG_NAME="switch-joycon-bluetooth-dock-configs"
+PKG_VERSION=2.0
 PKG_LICENSE="GPL"
 PKG_DEPENDS_TARGET="joycond rewritefs xdotool"
 PKG_SECTION="virtual"
@@ -8,11 +9,13 @@ post_install() {
   enable_service xorg-configure-switch.service
   enable_service var-bluetoothconfig.mount
   enable_service pair-joycon.service
+  enable_service fix-permissions.service
 
   mkdir -p ${INSTALL}/usr/bin
   cp -Pv ${PKG_DIR}/scripts/pair-joycon.sh ${INSTALL}/usr/bin
   cp -Pv ${PKG_DIR}/scripts/dock-hotplug ${INSTALL}/usr/bin
+  cp -pv ${PKG_DIR}/scripts/fix-freq-permissions.sh ${INSTALL}/usr/bin
   chmod +x ${INSTALL}/usr/bin/pair-joycon.sh
   chmod +x ${INSTALL}/usr/bin/dock-hotplug
-
+  chmod +x ${INSTALL}/usr/bin/fix-freq-permissions.sh
 }
