b995ef726d80bada3181b9aa490265e9d2ca69b3
Rudi Heitbaum <rudi@heitbaum.com>
2023-01-24 03:21:06 +0000

    xf86-video-vmware: update to 13.4.0

--- packages/x11/driver/xf86-video-vmware/package.mk
+++ packages/x11/driver/xf86-video-vmware/package.mk
@@ -3,12 +3,12 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="xf86-video-vmware"
-PKG_VERSION="13.3.0"
-PKG_SHA256="47971924659e51666a757269ad941a059ef5afe7a47b5101c174a6022ac4066c"
+PKG_VERSION="13.4.0"
+PKG_SHA256="aed31ee5ed5ecc6e2226705383e7ad06f7602c1376a295305f376b17af3eb81a"
 PKG_ARCH="x86_64"
 PKG_LICENSE="OSS"
-PKG_SITE="http://www.vmware.com"
-PKG_URL="http://xorg.freedesktop.org/releases/individual/driver/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
+PKG_SITE="https://www.vmware.com"
+PKG_URL="https://xorg.freedesktop.org/releases/individual/driver/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain mesa libX11 xorg-server"
 PKG_LONGDESC="xf86-video-vmware: The Xorg driver for vmware video"
 PKG_TOOLCHAIN="autotools"
