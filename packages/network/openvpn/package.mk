d57d7636dfa4cc5df3d952fee5307f025d0747b3
Rudi Heitbaum <rudi@heitbaum.com>
2023-01-26 02:15:19 +0000

    openvpn: update to 2.6.0
    needs dependent package libcap-ng
    
    release notes:
    - https://community.openvpn.net/openvpn/wiki/Downloads
    - https://github.com/OpenVPN/openvpn/blob/v2.6.0/Changes.rst

--- packages/network/openvpn/package.mk
+++ packages/network/openvpn/package.mk
@@ -3,12 +3,12 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="openvpn"
-PKG_VERSION="2.5.8"
-PKG_SHA256="a6f315b7231d44527e65901ff646f87d7f07862c87f33531daa109fb48c53db2"
+PKG_VERSION="2.6.0"
+PKG_SHA256="ebec933263c9850ef6f7ce125e2f22214be60b1cbb8ccff18892643fe083ae8f"
 PKG_LICENSE="GPL"
 PKG_SITE="https://openvpn.net"
 PKG_URL="https://swupdate.openvpn.org/community/releases/${PKG_NAME}-${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain lzo openssl"
+PKG_DEPENDS_TARGET="toolchain libcap-ng lz4 lzo openssl"
 PKG_LONGDESC="A full featured SSL VPN software solution that integrates OpenVPN server capabilities."
 
 PKG_CONFIGURE_OPTS_TARGET="ac_cv_have_decl_TUNSETPERSIST=no \
