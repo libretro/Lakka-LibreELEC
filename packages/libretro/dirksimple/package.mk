40b83d240c1eef9e046a2f3c1a4985ec8d9842b8
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2023-01-28 02:05:04 +0100

    dirksimple: new libretro core

---
+++ packages/libretro/dirksimple/package.mk
@@ -0,0 +1,17 @@
+PKG_NAME="dirksimple"
+PKG_VERSION="ce2581b"
+PKG_ARCH="any"
+PKG_LICENSE="Zlib"
+PKG_SITE="https://github.com/icculus/DirkSimple"
+PKG_URL="$PKG_SITE.git"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Libretro core of dirt-simple Dragon's Lair player"
+PKG_TOOLCHAIN="cmake"
+
+PKG_CMAKE_OPTS_TARGET="-DLIBRETRO=ON"
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/libretro
+  cp dirksimple_libretro.so $INSTALL/usr/lib/libretro/
+  cp ../dirksimple_libretro.info $INSTALL/usr/lib/libretro/
+}
