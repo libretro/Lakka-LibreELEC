cd7f4dad8eb1380948f72adb46f9b677e0029fc9
Rudi Heitbaum <rudi@heitbaum.com>
2023-01-30 11:44:46 +0000

    pvr.nextpvr: update 20.3.1-Nexus to 20.4.0-Nexus

--- packages/mediacenter/kodi-binary-addons/pvr.nextpvr/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.nextpvr/package.mk
@@ -3,9 +3,9 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pvr.nextpvr"
-PKG_VERSION="20.3.1-Nexus"
-PKG_SHA256="661edd0d564ada8c70ae641ecb500cae84e85b09983836106c43cd4da86d738b"
-PKG_REV="2"
+PKG_VERSION="20.4.0-Nexus"
+PKG_SHA256="9b9ae55b126ee0ca42a94d2a447167c0d61be36de336a4b236c0879b9b0832e3"
+PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/kodi-pvr/pvr.nextpvr"
