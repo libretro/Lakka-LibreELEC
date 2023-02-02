fd8a9e021c7e525b9e6da2da4632ec22216ad939
Rudi Heitbaum <rudi@heitbaum.com>
2023-02-02 23:07:30 +0000

    openssh: update to 9.2p1
    release notes:
    - https://www.openssh.com/releasenotes.html#9.2p1
    
    log:
    - https://github.com/openssh/openssh-portable/compare/V_9_1_P1...V_9_2_P1

--- packages/network/openssh/package.mk
+++ packages/network/openssh/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="openssh"
-PKG_VERSION="9.1p1"
-PKG_SHA256="19f85009c7e3e23787f0236fbb1578392ab4d4bf9f8ec5fe6bc1cd7e8bfdd288"
+PKG_VERSION="9.2p1"
+PKG_SHA256="3f66dbf1655fb45f50e1c56da62ab01218c228807b21338d634ebcdf9d71cf46"
 PKG_LICENSE="OSS"
 PKG_SITE="https://www.openssh.com/"
 PKG_URL="https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/${PKG_NAME}-${PKG_VERSION}.tar.gz"
