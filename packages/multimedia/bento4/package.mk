0984ccc77f695278ee854f85beab67f22fdeb49b
Rudi Heitbaum <rudi@heitbaum.com>
2023-02-08 10:15:08 +0000

    bento4: update to 1.6.0-639-6-Nexus

--- packages/multimedia/bento4/package.mk
+++ packages/multimedia/bento4/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="bento4"
-PKG_VERSION="1.6.0-639-5-Nexus"
-PKG_SHA256="3efc43c8146d6a89c4f80208f0e24b1c26ecfc84a2887135b10891612366c373"
+PKG_VERSION="1.6.0-639-6-Nexus"
+PKG_SHA256="8afa4ae07a7629a65e0d5014750960ced33a8771d363652f3913261fb5d0c84f"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.bento4.com"
 PKG_URL="https://github.com/xbmc/Bento4/archive/refs/tags/${PKG_VERSION}.tar.gz"
