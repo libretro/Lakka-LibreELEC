f3188c601b0dd09249402a146a263b03d901c36c
Rudi Heitbaum <rudi@heitbaum.com>
2023-02-10 07:08:32 +0000

    rust-std-snapshot: update to 1.67.1

--- packages/rust/rust-std-snapshot/package.mk
+++ packages/rust/rust-std-snapshot/package.mk
@@ -3,7 +3,7 @@
 
 PKG_NAME="rust-std-snapshot"
 PKG_VERSION="$(get_pkg_version rust)"
-PKG_SHA256="8f03b271bba56b0245833f2cb08044865068ce8721d6a736d3ef7056aa109daa"
+PKG_SHA256="f4dc8468dfc1dbd86f865b10f06e0e4b4e76f5a3a1cc27317a520ab1660844e9"
 PKG_LICENSE="MIT"
 PKG_SITE="https://www.rust-lang.org"
 PKG_URL="https://static.rust-lang.org/dist/rust-std-${PKG_VERSION}-${MACHINE_HARDWARE_NAME}-unknown-linux-gnu.tar.xz"
