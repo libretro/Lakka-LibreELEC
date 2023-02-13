9c4eef77db849a379ce5c0b218b5bd6238427daf
Matthias Reichl <hias@horus.com>
2023-02-14 00:11:30 +0100

    inputstream.adaptive: add nss dependency for all archs
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/mediacenter/kodi-binary-addons/inputstream.adaptive/package.mk
+++ packages/mediacenter/kodi-binary-addons/inputstream.adaptive/package.mk
@@ -9,16 +9,12 @@ PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/xbmc/inputstream.adaptive"
 PKG_URL="https://github.com/xbmc/inputstream.adaptive/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain kodi-platform bento4 expat"
+PKG_DEPENDS_TARGET="toolchain kodi-platform bento4 expat nss"
 PKG_SECTION=""
 PKG_SHORTDESC="inputstream.adaptive"
 PKG_LONGDESC="inputstream.adaptive"
 
 PKG_IS_ADDON="yes"
-
-if [ "${TARGET_ARCH}" = "x86_64" ] || [ "${TARGET_ARCH}" = "arm" ]; then
-  PKG_DEPENDS_TARGET+=" nss"
-fi
 
 addon() {
   install_binary_addon ${PKG_ADDON_ID}
