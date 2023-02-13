27f3671ae7b449369013c707385f6865016c4b76
Rudi Heitbaum <rudi@heitbaum.com>
2023-02-13 07:58:46 +0000

    sway: update to 1.8.1

--- packages/wayland/compositor/sway/package.mk
+++ packages/wayland/compositor/sway/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="sway"
-PKG_VERSION="1.8"
-PKG_SHA256="fae0422bca8f528027f77e3a7bbea2be0498bba2ad3f0d22554ff8827e37f04e"
+PKG_VERSION="1.8.1"
+PKG_SHA256="e9f575761342fc8fe0cfeea80c90f32ddfa8c543572fec179f40922346f47dff"
 PKG_LICENSE="MIT"
 PKG_SITE="https://swaywm.org/"
 PKG_URL="https://github.com/swaywm/sway/archive/${PKG_VERSION}.tar.gz"
