8a65e1a2b76d08bf13de90be83950b8b96f11c6a
Rudi Heitbaum <rudi@heitbaum.com>
2023-02-14 10:15:56 +0000

    glib: update to 2.75.3
    changelig:
    - https://gitlab.gnome.org/GNOME/glib/-/commit/b65044c52b78fab65a0415e2148f4b98a1680da2

--- packages/devel/glib/package.mk
+++ packages/devel/glib/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="glib"
-PKG_VERSION="2.75.2"
-PKG_SHA256="360d6fb75202c0eb0d07f0ab812b19b526f1c05ccc0a8ed7e5d2c988616d343a"
+PKG_VERSION="2.75.3"
+PKG_SHA256="7c517d0aff456c35a039bce8a8df7a08ce95a8285b09d1849f8865f633f7f871"
 PKG_LICENSE="LGPL"
 PKG_SITE="https://www.gtk.org/"
 PKG_URL="https://download.gnome.org/sources/glib/$(get_pkg_version_maj_min)/${PKG_NAME}-${PKG_VERSION}.tar.xz"
