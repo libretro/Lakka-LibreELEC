7e809528e98b0b74b253275bcd67e08f2a21edc4
Matthias Reichl <hias@horus.com>
2023-02-24 15:20:07 +0100

    virtual/network: include nss in image for all archs
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/virtual/network/package.mk
+++ packages/virtual/network/package.mk
@@ -6,7 +6,7 @@ PKG_VERSION=""
 PKG_LICENSE="various"
 PKG_SITE="https://libreelec.tv"
 PKG_URL=""
-PKG_DEPENDS_TARGET="toolchain connman netbase ethtool openssh iw wireless-regdb"
+PKG_DEPENDS_TARGET="toolchain connman netbase ethtool openssh iw wireless-regdb nss"
 PKG_SECTION="virtual"
 PKG_LONGDESC="Metapackage for various packages to install network support"
 
@@ -28,9 +28,4 @@ fi
 
 if [ "${ISCSI_SUPPORT}" = "yes" ]; then
   PKG_DEPENDS_TARGET+=" open-iscsi"
-fi
-
-# nss needed by inputstream.adaptive, chromium etc.
-if [ "${TARGET_ARCH}" = "x86_64" ] || [ "${TARGET_ARCH}" = "arm" ]; then
-  PKG_DEPENDS_TARGET+=" nss"
 fi
