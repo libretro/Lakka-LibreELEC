855e0d9c5bead83549b4c186b1f1c1e945cdea62
Rudi Heitbaum <rudi@heitbaum.com>
2023-03-01 11:45:15 +0000

    btrfs-progs: update to 6.2.2 and addon (1)
    changes:
    - https://btrfs.readthedocs.io/en/latest/CHANGES.html

--- packages/addons/tools/btrfs-progs/package.mk
+++ packages/addons/tools/btrfs-progs/package.mk
@@ -2,9 +2,9 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="btrfs-progs"
-PKG_VERSION="6.1.3"
-PKG_SHA256="e6512ff305963bc68f11803fa759fecbead778a3a951aeb4f7f3f76dabb31db4"
-PKG_REV="0"
+PKG_VERSION="6.2.2"
+PKG_SHA256="140d3d98f2cba4c7f05c16aec3038f044e11555a40c27a5006185c99a10c7ca2"
+PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://btrfs.wiki.kernel.org/index.php/Main_Page"
