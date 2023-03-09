eba2f0e780c0624276965339dd2c0d3f864bbb19
Rudi Heitbaum <rudi@heitbaum.com>
2023-03-09 08:51:08 +0000

    openvpn: update to 2.6.1

--- packages/network/openvpn/package.mk
+++ packages/network/openvpn/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="openvpn"
-PKG_VERSION="2.6.0"
-PKG_SHA256="ebec933263c9850ef6f7ce125e2f22214be60b1cbb8ccff18892643fe083ae8f"
+PKG_VERSION="2.6.1"
+PKG_SHA256="8cbc4fd8ce27b85107b449833c3b30fb05f1ca3c81b46a0ba8658036944266bc"
 PKG_LICENSE="GPL"
 PKG_SITE="https://openvpn.net"
 PKG_URL="https://swupdate.openvpn.org/community/releases/${PKG_NAME}-${PKG_VERSION}.tar.gz"
