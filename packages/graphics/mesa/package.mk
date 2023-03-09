a4c61232eccacf77ace896e4a42e6bed4bbb7a3a
Rudi Heitbaum <rudi@heitbaum.com>
2023-03-09 07:55:00 +0000

    mesa: update to 22.3.7

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa"
-PKG_VERSION="22.3.6"
-PKG_SHA256="4ec8ec65dbdb1ee9444dba72970890128a19543a58cf05931bd6f54f124e117f"
+PKG_VERSION="22.3.7"
+PKG_SHA256="894ce2f4a1c2e76177cdd2284620192d0da3066b243eec2fbb1d7cf37f13042c"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
 PKG_URL="https://mesa.freedesktop.org/archive/mesa-${PKG_VERSION}.tar.xz"
