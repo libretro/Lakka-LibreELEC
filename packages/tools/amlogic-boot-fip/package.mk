b91fd79e4a71dfb31176dd5d21aa7dfa4547abfb
Christian Hewitt <christianshewitt@gmail.com>
2023-03-09 15:51:14 +0000

    amlogic-boot-fip: bump to include bananapi-cm4io

--- packages/tools/amlogic-boot-fip/package.mk
+++ packages/tools/amlogic-boot-fip/package.mk
@@ -3,8 +3,8 @@
 
 PKG_NAME="amlogic-boot-fip"
 PKG_LICENSE="nonfree"
-PKG_VERSION="955a8ae91630038d50342814bae1b055c75707db"
-PKG_SHA256="32dbcf80440b406f00783bfc1ffacb721ac829c7c12624c594474ecf48a77db8"
+PKG_VERSION="e96b6a694380ff07d5a9e4be644ffe254bd18512"
+PKG_SHA256="2cc06bc7d5647fd8c0025181fa42c4a8ef0ed16b918a1fa2060ea83c22e47b20"
 PKG_SITE="https://github.com/LibreELEC/amlogic-boot-fip"
 PKG_URL="https://github.com/LibreELEC/amlogic-boot-fip/archive/${PKG_VERSION}.tar.gz"
 PKG_LONGDESC="Firmware Image Package (FIP) sources used to sign Amlogic u-boot binaries in LibreELEC images"
