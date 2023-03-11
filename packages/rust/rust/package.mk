1532f63e793f1c924e229d3ff34aed334b6f158c
Rudi Heitbaum <rudi@heitbaum.com>
2023-03-11 05:45:33 +0000

    rust: update to 1.68.0

--- packages/rust/rust/package.mk
+++ packages/rust/rust/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="rust"
-PKG_VERSION="1.67.1"
-PKG_SHA256="46483d3e5de85a3bd46f8e7a3ae1837496391067dbe713a25d3cf051b3d9ff6e"
+PKG_VERSION="1.68.0"
+PKG_SHA256="eaf4d8b19f23a232a4770fb53ab5e7acdedec11da1d02b0e5d491ca92ca96d62"
 PKG_LICENSE="MIT"
 PKG_SITE="https://www.rust-lang.org"
 PKG_URL="https://static.rust-lang.org/dist/rustc-${PKG_VERSION}-src.tar.gz"
