e16d9e8e44300ea8e463266d0b82e6b3c4a9b51b
Rudi Heitbaum <rudi@heitbaum.com>
2023-03-14 09:16:36 +0000

    pvr.vbox: update 20.4.1-Nexus to 20.4.2-Nexus

--- packages/mediacenter/kodi-binary-addons/pvr.vbox/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.vbox/package.mk
@@ -3,9 +3,9 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pvr.vbox"
-PKG_VERSION="20.4.1-Nexus"
-PKG_SHA256="c8ee26a903491fa8a2c321322a5ae3184139e892508f30faadbd9bd0df6287bc"
-PKG_REV="2"
+PKG_VERSION="20.4.2-Nexus"
+PKG_SHA256="04c50eba71426f062339491e7567545ec8c910b17c7055b28285eab14f26ca6f"
+PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/kodi-pvr/pvr.vbox"
