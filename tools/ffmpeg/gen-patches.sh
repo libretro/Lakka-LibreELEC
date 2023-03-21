0fec58c1c72590f6d0ce7b0cca759a8574fa7d94
Matthias Reichl <hias@horus.com>
2023-03-21 19:12:20 +0100

    tools/ffmpeg/gen-patches.sh: switch to ffmpeg 6.0
    v4l2 patches still need to be updated
    
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- tools/ffmpeg/gen-patches.sh
+++ tools/ffmpeg/gen-patches.sh
@@ -2,7 +2,7 @@
 
 # base ffmpeg version
 FFMPEG_REPO="git://source.ffmpeg.org/ffmpeg.git"
-FFMPEG_VERSION="n5.1.2"
+FFMPEG_VERSION="n6.0"
 
 KODI_FFMPEG_REPO="https://github.com/xbmc/FFmpeg"
 KODI_FFMPEG_VERSION="5.1.2-Nexus-Alpha3"
@@ -34,11 +34,11 @@ create_patch() {
       ;;
     libreelec)
       REPO="https://github.com/LibreELEC/FFmpeg"
-      REFSPEC="5.1.2-libreelec-misc"
+      REFSPEC="6.0-libreelec-misc"
       ;;
     rpi)
       REPO="https://github.com/jc-kynesim/rpi-ffmpeg"
-      REFSPEC="test/5.1.2/main"
+      REFSPEC="dev/6.0/rpi_import_1"
       ;;
     *)
       echo "illegal feature set ${FEATURE_SET}"
