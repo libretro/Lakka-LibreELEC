e98a9cfd4c3254c5671d892b92cb0d274e00a97a
Rudi Heitbaum <rudi@heitbaum.com>
2023-04-01 23:03:39 +0000

    vulkan-loader: update to 1.3.246

--- packages/graphics/vulkan/vulkan-loader/package.mk
+++ packages/graphics/vulkan/vulkan-loader/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="vulkan-loader"
-PKG_VERSION="1.3.245"
-PKG_SHA256="93ee2885dc8ead7b654112c8c1ebfa8e10fa486e8c71c909846f855f43bdf046"
+PKG_VERSION="1.3.246"
+PKG_SHA256="2531b6ef63b18040e5b0a3aa56df51e6e3cc42e7314c67a88a363be4c6975b2f"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://github.com/KhronosGroup/Vulkan-Loader"
 PKG_URL="https://github.com/KhronosGroup/Vulkan-Loader/archive/v${PKG_VERSION}.tar.gz"
