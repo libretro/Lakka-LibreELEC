ab94b90125ccd624b88fdb35a141523b3a2503f8
Rudi Heitbaum <rudi@heitbaum.com>
2023-04-04 09:46:55 +0000

    foot: update to 1.14.0

--- packages/wayland/util/foot/package.mk
+++ packages/wayland/util/foot/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="foot"
-PKG_VERSION="1.13.1"
-PKG_SHA256="604f777fdaccfc1ee2d20376cc5688a819dcd6b7113725880cc973194fd6c737"
+PKG_VERSION="1.14.0"
+PKG_SHA256="9a306951bc6bdce150364bccb0fb4b67720f50e98e9ac1de89792c1c1aa30690"
 PKG_LICENSE="MIT"
 PKG_SITE="https://codeberg.org/dnkl/foot/"
 PKG_URL="https://codeberg.org/dnkl/foot/archive/${PKG_VERSION}.tar.gz"
