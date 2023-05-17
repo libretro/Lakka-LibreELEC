7c1f88db24dcdd307cf1755f8eb7754ba338a916
Rudi Heitbaum <rudi@heitbaum.com>
2023-05-17 12:34:38 +0000

    intel-ucode: update to 20230516

--- packages/linux-firmware/intel-ucode/package.mk
+++ packages/linux-firmware/intel-ucode/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="intel-ucode"
-PKG_VERSION="20230512"
-PKG_SHA256="58f3321dcf900175d87d5b39455138c2a24e69df4ba997fb44e3e0d19e531ad1"
+PKG_VERSION="20230516"
+PKG_SHA256="ad8cbc5a273f4640003f6a3f1b129ac6a046f3498ce037921ebac1adf267e55d"
 PKG_ARCH="x86_64"
 PKG_LICENSE="other"
 PKG_SITE="https://downloadcenter.intel.com/search?keyword=linux+microcode"
