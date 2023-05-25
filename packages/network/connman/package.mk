3eeaaeb269e57ccbcf7f532b15cc9760142a265b
Rudi Heitbaum <rudi@heitbaum.com>
2023-05-25 10:39:19 +0000

    connman: update to 24180d1 (1.41+/2023-04-11)
    log:
    - https://git.kernel.org/pub/scm/network/connman/connman.git/log/

--- packages/network/connman/package.mk
+++ packages/network/connman/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="connman"
-PKG_VERSION="9971144ae862e83f1f5d8cb84c0b62f2542dcdec" # 1.41 + 2023-01-16
-PKG_SHA256="57ec9d2f4b007f90c1eecdae8ae69d051457f2e408b4d93f5490735797376d9e"
+PKG_VERSION="24180d1243bf005c721be6f9d8d6da67bb669c55"
+PKG_SHA256="13299f1dd4b41ef90acfdeb215b2d773b75347694ef0cd5b12cc06bc0ef694e6"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.connman.net"
 PKG_URL="https://git.kernel.org/pub/scm/network/connman/connman.git/snapshot/connman-${PKG_VERSION}.tar.gz"
