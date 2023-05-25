cffcac215725095c3019990566a98975c79bf4a7
Rudi Heitbaum <rudi@heitbaum.com>
2023-05-25 22:31:46 +0000

    nqptp: update to 1.2.1

--- packages/addons/addon-depends/snapcast-depends/nqptp/package.mk
+++ packages/addons/addon-depends/snapcast-depends/nqptp/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="nqptp"
-PKG_VERSION="c71b49a3556ba8547ee28482cb31a97fe99298aa"
-PKG_SHA256="02ed710ed37269adbede06fcd4e12892cc0f9d14d5c68b7f45d67b8694bff1e4"
+PKG_VERSION="1.2.1"
+PKG_SHA256="fab700572961ca81addb405e8bd4bd57c47259f91e7e8e0f5f82240c38c63ce5"
 PKG_LICENSE="GPL-2.0"
 PKG_SITE="https://github.com/mikebrady/nqptp"
 PKG_URL="https://github.com/mikebrady/nqptp/archive/${PKG_VERSION}.tar.gz"
