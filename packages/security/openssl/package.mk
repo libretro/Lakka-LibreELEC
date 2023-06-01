06b81cccdc517e0f0389dbaf67c6b59ef9d541ca
Rudi Heitbaum <rudi@heitbaum.com>
2023-06-01 14:42:20 +0000

    openssl: update to 3.0.9
    release annoucement:
        - https://www.openssl.org/news/secadv/20230530.txt

--- packages/security/openssl/package.mk
+++ packages/security/openssl/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="openssl"
-PKG_VERSION="3.0.8"
-PKG_SHA256="6c13d2bf38fdf31eac3ce2a347073673f5d63263398f1f69d0df4a41253e4b3e"
+PKG_VERSION="3.0.9"
+PKG_SHA256="eb1ab04781474360f77c318ab89d8c5a03abc38e63d65a603cabbf1b00a1dc90"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://www.openssl.org"
 PKG_URL="https://www.openssl.org/source/${PKG_NAME}-${PKG_VERSION}.tar.gz"
