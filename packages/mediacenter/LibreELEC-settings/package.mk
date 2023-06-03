1bded022694ee1bd15784ad96ead8e9d3cb30f9a
mglae <mglmail@arcor.de>
2023-06-04 00:15:40 +0200

    LibreELEC-settings: update to b920d5d

--- packages/mediacenter/LibreELEC-settings/package.mk
+++ packages/mediacenter/LibreELEC-settings/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="LibreELEC-settings"
-PKG_VERSION="9a334c0857fe5ccf84af272f42dc6f6cd5c72e4b"
-PKG_SHA256="71be076033ae4bcb9012a12c2fc47b0805b0e40db2e812e19613643bbcba978c"
+PKG_VERSION="b920d5d83a8a7445d121d2f920169444111bf93c"
+PKG_SHA256="d8147068b6172250d98d41fafd7d6dbaa286074932b537214bf0dab95fe9e99a"
 PKG_LICENSE="GPL"
 PKG_SITE="https://libreelec.tv"
 PKG_URL="https://github.com/LibreELEC/service.libreelec.settings/archive/${PKG_VERSION}.tar.gz"
