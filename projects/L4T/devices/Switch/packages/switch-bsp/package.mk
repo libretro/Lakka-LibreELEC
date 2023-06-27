a177acc59ddddb307d68aab1461f568ec31ecfd1
wings110 <283982871@qq.com>
2023-06-28 02:34:00 +0800

    add lose quotation
    add lose quotation

--- projects/L4T/devices/Switch/packages/switch-bsp/package.mk
+++ projects/L4T/devices/Switch/packages/switch-bsp/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="switch-bsp"
-PKG_VERSION=1.0"
+PKG_VERSION="1.0"
 PKG_LICENSE="GPL"
 PKG_DEPENDS_TARGET="joycond rewritefs xdotool alsa-lib alsa-ucm-conf usb-gadget-scripts"
 PKG_SECTION="virtual"
