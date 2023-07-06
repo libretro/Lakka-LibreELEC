38c0b60feae90e57b2a7a799e009b8655ebd667e
Rudi Heitbaum <rudi@heitbaum.com>
2023-07-06 12:37:52 +0000

    linux: update to 6.1.38

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -29,8 +29,8 @@ case "${LINUX}" in
     PKG_SOURCE_NAME="linux-${LINUX}-${PKG_VERSION}.tar.gz"
     ;;
   *)
-    PKG_VERSION="6.1.36"
-    PKG_SHA256="d8ca0e300f30b9ff70c6e1497c638a1dac1407f45d3655e9c62c6e45a08afe6b"
+    PKG_VERSION="6.1.38"
+    PKG_SHA256="f9a4f91b609f7d332a5f2be01ab86336fa00149fae6bdc19f16fa19f78802d43"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v${PKG_VERSION/.*/}.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;
