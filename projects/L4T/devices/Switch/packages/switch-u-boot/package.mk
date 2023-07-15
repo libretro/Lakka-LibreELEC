509ce2f4454a0b63058833c7d3fcefe28a0afe4f
Ronald Brown <rbrown4014@yahoo.com>
2023-07-15 18:40:59 +0200

    L4T:Switch: Update Switch Uboot

--- projects/L4T/devices/Switch/packages/switch-u-boot/package.mk
+++ projects/L4T/devices/Switch/packages/switch-u-boot/package.mk
@@ -1,9 +1,9 @@
 PKG_NAME="switch-u-boot"
-PKG_VERSION="dd5432fa2ad727b3af159a704367a6e1fd6f246d"
+PKG_VERSION="2edfd36a6fd3987f2bc19df5e4c3c3ef58797424"
 PKG_GIT_CLONE_BRANCH="linux-hekatf"
 PKG_DEPENDS_HOST="toolchain Python3:host gcc:host swig:host"
 PKG_DEPENDS_TARGET="toolchain Python3 gcc:target swig:host"
-PKG_SITE="https://gitlab.com/switchroot/switch-uboot"
+PKG_SITE="https://github.com/CTCaer/switch-l4t-uboot"
 PKG_URL="${PKG_SITE}.git"
 PKG_TOOLCHAIN="make"
 
