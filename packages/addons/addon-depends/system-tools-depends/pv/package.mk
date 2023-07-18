5d97cb4d800cc4f48e48ca74adaba0ff8c453d5b
Rudi Heitbaum <rudi@heitbaum.com>
2023-07-18 08:38:38 +0000

    pv: update to 1.7.24

--- packages/addons/addon-depends/system-tools-depends/pv/package.mk
+++ packages/addons/addon-depends/system-tools-depends/pv/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pv"
-PKG_VERSION="1.6.20"
-PKG_SHA256="e831951eff0718fba9b1ef286128773b9d0e723e1fbfae88d5a3188814fdc603"
+PKG_VERSION="1.7.24"
+PKG_SHA256="3bf43c5809c8d50066eaeaea5a115f6503c57a38c151975b710aa2bee857b65e"
 PKG_LICENSE="GNU"
 PKG_SITE="http://www.ivarch.com/programs/pv.shtml"
-PKG_URL="http://www.ivarch.com/programs/sources/pv-${PKG_VERSION}.tar.bz2"
+PKG_URL="http://www.ivarch.com/programs/sources/pv-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Pipe Viewer can be inserted into any normal pipeline between two processes."
 PKG_BUILD_FLAGS="-sysroot"
