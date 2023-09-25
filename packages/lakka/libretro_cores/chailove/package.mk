639a3074e6b6a1c7070c2e04117a1edc8ae22c15
gouchi <gouchi@free.fr>
2023-09-25 18:45:16 +0000

    chailove : update (#1883)
    Fix #1881

--- packages/lakka/libretro_cores/chailove/package.mk
+++ packages/lakka/libretro_cores/chailove/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="chailove"
-PKG_VERSION="9677b142ab9562cb278e87d7475c55fe0b9a4333"
+PKG_VERSION="778370bb10013632272b882beee45ea1b030ab8c"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/libretro/libretro-chailove"
 PKG_URL="${PKG_SITE}.git"
