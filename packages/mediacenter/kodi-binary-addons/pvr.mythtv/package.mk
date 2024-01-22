b7c3393412ba274fc22b91681084f02daf342645
Rudi Heitbaum <rudi@heitbaum.com>
2024-01-22 12:25:11 +0000

    pvr.mythtv: update 20.5.8-Nexus to 20.5.10-Nexus

--- packages/mediacenter/kodi-binary-addons/pvr.mythtv/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.mythtv/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pvr.mythtv"
-PKG_VERSION="20.5.8-Nexus"
-PKG_SHA256="c3eefdb9e12c4ca517709378bfe0b985b808d3dbd2ce75f234c78a88c3a1e411"
+PKG_VERSION="20.5.10-Nexus"
+PKG_SHA256="eb21e185d1333a8696565138bc58df3d37970c9a3615193396e9f296a56a24c4"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
