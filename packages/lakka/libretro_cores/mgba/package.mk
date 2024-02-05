a6c3f69926fc248089ae659fa46ce9cbfe292af7
Demetris Ierokipides <ierokipides.dem@gmail.com>
2024-02-05 20:14:24 +0200

    mgba: upgrade to upstream 0.10.3 stable

--- packages/lakka/libretro_cores/mgba/package.mk
+++ packages/lakka/libretro_cores/mgba/package.mk
@@ -1,23 +1,37 @@
 PKG_NAME="mgba"
-PKG_VERSION="314bf7b676f5b820f396209eb0c7d6fbe8103486"
+PKG_VERSION="1c61b54208ca6266129d0f2394c04bd8c44f98c5"
 PKG_LICENSE="MPLv2.0"
-PKG_SITE="https://github.com/libretro/mgba"
+PKG_SITE="https://github.com/mgba-emu/mgba"
 PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_DEPENDS_TARGET="toolchain libzip libpng zlib"
 PKG_LONGDESC="mGBA Game Boy Advance Emulator"
-PKG_TOOLCHAIN="make"
+PKG_TOOLCHAIN="cmake"
 
-PKG_MAKE_OPTS_TARGET="-C ../ -f Makefile.libretro"
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_BUILD_TYPE=Release \
+                       -DBUILD_LIBRETRO=ON \
+                       -DSKIP_LIBRARY=ON \
+                       -DBUILD_QT=OFF \
+                       -DBUILD_SDL=OFF \
+                       -DUSE_DISCORD_RPC=OFF \
+                       -DUSE_GDB_STUB=OFF \
+                       -DUSE_DEBUGGERS=OFF \
+                       -DUSE_EDITLINE=OFF \
+                       -DUSE_EPOXY=OFF"
 
-if [ "${ARCH}" = "arm" ]; then
-  PKG_MAKE_OPTS_TARGET+=" platform=unix-armv"
+if [ "${OPENGL_SUPPORT}" = "yes" ]; then
+  PKG_DEPENDS_TARGET+=" ${OPENGL}"
+  PKG_CMAKE_OPTS_TARGET+=" -DBUILD_GL=ON"
+elif [ "${OPENGL_SUPPORT}" = "no" ]; then
+  PKG_CMAKE_OPTS_TARGET+=" -DBUILD_GL=OFF"
 fi
 
-if target_has_feature neon ; then
-  PKG_MAKE_OPTS_TARGET+=" HAVE_NEON=1"
+if [ "${OPENGLES_SUPPORT}" = "yes" ]; then
+  PKG_DEPENDS_TARGET+=" ${OPENGLES}"
+  PKG_CMAKE_OPTS_TARGET+=" -DUSING_GLES2=ON"
 fi
+
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v ../mgba_libretro.so ${INSTALL}/usr/lib/libretro/
+    cp -v mgba_libretro.so ${INSTALL}/usr/lib/libretro/
 }
