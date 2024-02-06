0d1035111bf79921b4b95eccbe072247f82971e4
Demetris Ierokipides <ierokipides.dem@gmail.com>
2024-02-06 19:01:39 +0200

    Update Visualboyadvance to v2.1.9 [2.1.9] - 2024-03-02 ======================= - Remove SDL sound driver [rkitover] - Fix wav audio recording [40356555+Aikku93] - Dialog appearance improvements + link warning [zachbacon] - build: don't fseeko64()/ftello64() on musl libc [rkitover] - visualboyadvance-m.metainfo.xml: add screenshots to the metainfo [zachbacon]

--- packages/lakka/libretro_cores/vbam/package.mk
+++ packages/lakka/libretro_cores/vbam/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="vbam"
-PKG_VERSION="0501f985a3d46fdb84e00c42ec2c2a4cf8c2aa92"
+PKG_VERSION="32bc9c9dc790be1f0e87512503b79c1017e55b11"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/visualboyadvance-m/visualboyadvance-m"
 PKG_URL="${PKG_SITE}.git"
