0b51c7cc5c64f37ee76d5f5d8a3beb37a8a02069
Alex Abbatiello <50689733+synthic@users.noreply.github.com>
2024-03-30 13:44:02 -0500

    Fix issue schellingb/dosbox-pure#488

--- packages/lakka/libretro_cores/dosbox_pure/package.mk
+++ packages/lakka/libretro_cores/dosbox_pure/package.mk
@@ -7,6 +7,10 @@ PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="DOSBox Pure is a fork of DOSBox, an emulator for DOS games, built for RetroArch/Libretro aiming for simplicity and ease of use."
 PKG_TOOLCHAIN="make"
 
+if [ "${DEVICE}" = "RPi5" ]; then
+  CFLAGS+=" -DPAGESIZE=16384"
+fi
+
 make_target() {
   # remove optimization from CFLAGS, set via Makefile
   CFLAGS="${CFLAGS//-O3/}"
