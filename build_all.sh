bcfc48874798d8139c244bff933123e9ab64ca83
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2024-04-02 22:08:43 +0200

    build_all: add 32bit PC

--- build_all.sh
+++ build_all.sh
@@ -84,10 +84,11 @@ targets="\
 	Allwinner|R40|arm|image \
 	Amlogic|AMLGX|aarch64|image \
 	Ayn|Odin|aarch64|image \
+	Generic|Generic|i386|image \
 	Generic|Generic|x86_64|image \
-	L4T|Switch|aarch64|image \
 	Generic|wayland|x86_64|image \
 	Generic|x11|x86_64|image \
+	L4T|Switch|aarch64|image \
 	NXP|iMX6|arm|image \
 	NXP|iMX8|aarch64|image \
 	Rockchip|RK3288|arm|image \
