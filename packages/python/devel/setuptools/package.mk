50d02f16fe90b5835affa5ed78287871d59f4edf
Jernej Skrabec <jernej.skrabec@gmail.com>
2024-08-21 21:47:29 +0200

    setuptools: update to 73.0.1

--- packages/python/devel/setuptools/package.mk
+++ packages/python/devel/setuptools/package.mk
@@ -3,17 +3,17 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="setuptools"
-PKG_VERSION="52.0.0"
-PKG_SHA256="ff0c74d1b905a224d647f99c6135eacbec2620219992186b81aa20012bc7f882"
+PKG_VERSION="73.0.1"
+PKG_SHA256="d59a3e788ab7e012ab2c4baed1b376da6366883ee20d7a5fc426816e3d7b1193"
 PKG_LICENSE="OSS"
 PKG_SITE="https://pypi.org/project/setuptools"
-PKG_URL="https://github.com/pypa/setuptools/archive/v${PKG_VERSION}.tar.gz"
+PKG_URL="https://files.pythonhosted.org/packages/source/${PKG_NAME:0:1}/${PKG_NAME}/${PKG_NAME,,}-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST="Python3:host"
 PKG_LONGDESC="Replaces Setuptools as the standard method for working with Python module distributions."
 PKG_TOOLCHAIN="manual"
 
 make_host() {
-  python3 bootstrap.py
+  python3 setup.py build
 }
 
 makeinstall_host() {
