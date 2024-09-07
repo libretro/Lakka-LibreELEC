b53d8fa597089d11719bdc470cc42367459d4f9b
Rudi Heitbaum <rudi@heitbaum.com>
2024-09-07 05:06:15 +0000

    pybuild: update to 1.2.2

--- packages/python/devel/pybuild/package.mk
+++ packages/python/devel/pybuild/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2024-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pybuild"
-PKG_VERSION="1.2.1"
-PKG_SHA256="526263f4870c26f26c433545579475377b2b7588b6f1eac76a001e873ae3e19d"
+PKG_VERSION="1.2.2"
+PKG_SHA256="119b2fb462adef986483438377a13b2f42064a2a3a4161f24a0cca698a07ac8c"
 PKG_LICENSE="BSD"
 PKG_SITE="https://pypi.org/project/build/"
 PKG_URL="https://files.pythonhosted.org/packages/source/b/build/build-${PKG_VERSION}.tar.gz"
