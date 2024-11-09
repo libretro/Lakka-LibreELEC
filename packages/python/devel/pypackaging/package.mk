45f78b78fc7e360233bea65f98026e730daf9e1e
Rudi Heitbaum <rudi@heitbaum.com>
2024-11-09 03:07:24 +0000

    pypackaging: update to 24.2

--- packages/python/devel/pypackaging/package.mk
+++ packages/python/devel/pypackaging/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2024-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pypackaging"
-PKG_VERSION="24.1"
-PKG_SHA256="026ed72c8ed3fcce5bf8950572258698927fd1dbda10a5e981cdf0ac37f4f002"
+PKG_VERSION="24.2"
+PKG_SHA256="c228a6dc5e932d346bc5739379109d49e8853dd8223571c7c5b55260edc0b97f"
 PKG_LICENSE="BSD"
 PKG_SITE="https://pypi.org/project/build/"
 PKG_URL="https://files.pythonhosted.org/packages/source/p/packaging/packaging-${PKG_VERSION}.tar.gz"
