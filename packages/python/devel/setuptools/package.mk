fb67b6a2f80a6affd55a13d4e080f3cf125426fc
Rudi Heitbaum <rudi@heitbaum.com>
2024-11-12 05:10:08 +0000

    setuptools: update to 75.4.0

--- packages/python/devel/setuptools/package.mk
+++ packages/python/devel/setuptools/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="setuptools"
-PKG_VERSION="75.3.0"
-PKG_SHA256="fba5dd4d766e97be1b1681d98712680ae8f2f26d7881245f2ce9e40714f1a686"
+PKG_VERSION="75.4.0"
+PKG_SHA256="1dc484f5cf56fd3fe7216d7b8df820802e7246cfb534a1db2aa64f14fcb9cdcb"
 PKG_LICENSE="OSS"
 PKG_SITE="https://pypi.org/project/setuptools"
 PKG_URL="https://files.pythonhosted.org/packages/source/${PKG_NAME:0:1}/${PKG_NAME}/${PKG_NAME,,}-${PKG_VERSION}.tar.gz"
