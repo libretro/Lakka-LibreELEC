dee0300aff4ba7f8e8022d622f6ad5e7aaaa2d5b
ShigeakiAsai <asai@cocoa.ocn.ne.jp>
2025-03-06 19:15:42 +0900

    retroflag_picase_safeshutdown: Add support RETROFLAG Pi CASE safe shutdown Just only copy related files. Does not enable this function.

--- packages/lakka/package.mk
+++ packages/lakka/package.mk
@@ -34,6 +34,10 @@ if [ "${PROJECT}" = "RPi" ]; then
   if [ "${DEVICE}" = "RPiZero-GPiCase" -o "${DEVICE}" = "RPiZero2-GPiCase" -o "${DEVICE}" = "RPi4-GPiCase2" -o "${DEVICE}" = "RPiZero2-GPiCase2W" ]; then
     PKG_DEPENDS_TARGET+=" gpicase_safeshutdown"
   fi
+  
+  if [ "${DEVICE}" = "RPi3" -o "${DEVICE}" = "RPi4" -o "${DEVICE}" = "RPi5" ]; then
+    PKG_DEPENDS_TARGET+=" retroflag_picase_safeshutdown"
+  fi
 fi
 
 if [ "${DEVICE}" != "Switch" -a "${DEVICE}" != "RPiZero-GPiCase" -a "${DEVICE}" != "RPiZero2-GPiCase" -a "${DEVICE}" != "RPiZero2-GPiCase2W" ]; then
