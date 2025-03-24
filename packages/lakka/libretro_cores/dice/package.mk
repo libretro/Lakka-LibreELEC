9eb17a942843cc5108ccfeadbfd1f542860c2c67
Ken Mitton <mittonk@alum.mit.edu>
2025-03-23 22:24:50 -0400

    dice: Bump to pick up dice-libretro#24 fix for game.zip#inner.rom parsing.

--- packages/lakka/libretro_cores/dice/package.mk
+++ packages/lakka/libretro_cores/dice/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="dice"
-PKG_VERSION="0b49c6956b6507a44c4798a1de2106f06c00cf92"
+PKG_VERSION="f834ce8e118cd1562adfd76fdd8f17c2710ad227"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/mittonk/dice-libretro"
 PKG_URL="${PKG_SITE}.git"
