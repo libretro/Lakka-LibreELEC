420166f42c4903ce35fcb6eaa62a7130e593278a
Andreas T Jonsson <mail@andreasjonsson.se>
2025-06-17 07:29:04 +0200

    VirtualXT: Updated core

--- packages/lakka/libretro_cores/virtualxt/package.mk
+++ packages/lakka/libretro_cores/virtualxt/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="virtualxt"
-PKG_VERSION="4fd0a7a9774e673b3ee4a39126c67accee9c56ad"
+PKG_VERSION="50519a28e43ca558526f81f68793506c5094c9af"
 PKG_LICENSE="zlib"
 PKG_SITE="https://github.com/virtualxt/virtualxt"
 PKG_URL="${PKG_SITE}.git"
