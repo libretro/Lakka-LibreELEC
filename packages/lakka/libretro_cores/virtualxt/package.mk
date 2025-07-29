26641848b1a00ee0ebccbd3948094ee5cf09ea77
Yasai-san <83913986+ShigeakiAsai@users.noreply.github.com>
2025-07-30 06:04:16 +0900

    virtualxt: temporarily disable on arm because build failure is occurred. (#2090)
    https://github.com/virtualxt/virtualxt/issues/97

--- packages/lakka/libretro_cores/virtualxt/package.mk
+++ packages/lakka/libretro_cores/virtualxt/package.mk
@@ -1,6 +1,10 @@
 PKG_NAME="virtualxt"
 PKG_VERSION="50519a28e43ca558526f81f68793506c5094c9af"
 PKG_LICENSE="zlib"
+# temporarily disable on arm because build failure is occurred.
+# https://github.com/virtualxt/virtualxt/issues/97
+# please enable (remove "!arm" in PKG_ARCH ) when build failure is fixed.
+PKG_ARCH="any !arm"
 PKG_SITE="https://github.com/virtualxt/virtualxt"
 PKG_URL="${PKG_SITE}.git"
 PKG_DEPENDS_TARGET="toolchain"
