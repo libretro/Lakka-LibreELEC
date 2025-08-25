37fc0cb6cb84b68cb17ebded47b011580f552d03
Shigeaki Asai <y-asai.0816@docomonet.jp>
2025-08-25 13:28:51 +0900

    Revert "linux: add older raspberrypi kernel for PiBoyDmg"
    This reverts commit b386902506c2a8817d75b8b2c21c84c98c81281e.

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -29,12 +29,6 @@ case "${LINUX}" in
     PKG_SOURCE_NAME="linux-${LINUX}-${PKG_VERSION}.tar.gz"
     PKG_PATCH_DIRS="raspberrypi rtlwifi/6.13 rtlwifi/6.14 rtlwifi/after-6.14"
     ;;
-  raspberrypi-6.6.y)
-    PKG_VERSION="6d16e47ca139ba64c5daedf06e72f2774adbdc48" # 6.6.74
-    PKG_SHA256="2625c01652ed76de6a69062c286bda0c256b0106bf29352766e8232c1690033f"
-    PKG_URL="https://github.com/raspberrypi/linux/archive/${PKG_VERSION}.tar.gz"
-    PKG_SOURCE_NAME="linux-${LINUX}-${PKG_VERSION}.tar.gz"
-    ;;
   raspberrypi-5.10.y)
     PKG_VERSION="8e1110a580887f4b82303b9354c25d7e2ff5860e" # 5.10.110
     PKG_SHA256="e3244061e44426eafe97541633b87a71b8be1828d1bc48a18ec4c83fddb2f4c5"
@@ -232,7 +226,7 @@ pre_make_target() {
   fi
 
   # enable Dualsense on default and raspberrypi kernels for Lakka
-  if [ "${DISTRO}" = "Lakka" ] && [ "${LINUX}" = "default" -o "${LINUX:0:11}" = "raspberrypi" ]; then
+  if [ "${DISTRO}" = "Lakka" ] && [ "${LINUX}" = "default" -o "${LINUX}" = "raspberrypi" ]; then
     ${PKG_BUILD}/scripts/config \
                                 --enable CONFIG_HID_PLAYSTATION \
                                 --enable CONFIG_PLAYSTATION_FF
