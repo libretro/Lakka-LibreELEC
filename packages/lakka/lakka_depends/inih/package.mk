d2da3923df6b5b6ab8f4853e2b70c0b814faaca6
Shigeaki Asai <y-asai.0816@docomonet.jp>
2025-10-04 17:29:15 +0900

    inih: add inih package that is used from liblcf(easyrpg)

---
+++ packages/lakka/lakka_depends/inih/package.mk
@@ -0,0 +1,15 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2025-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="inih"
+PKG_VERSION="r62"
+PKG_SHA256="9c15fa751bb8093d042dae1b9f125eb45198c32c6704cd5481ccde460d4f8151"
+PKG_ARCH="any"
+PKG_LICENSE="BSD"
+PKG_SITE="https://github.com/benhoyt/inih"
+PKG_URL="${PKG_SITE}/archive/refs/tags/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="devel"
+PKG_LONGDESC="inih (INI Not Invented Here) is a simple .INI file parser written in C."
+
+PKG_TOOLCHAIN="auto"
