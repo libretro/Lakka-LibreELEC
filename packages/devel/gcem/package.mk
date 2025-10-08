bd7f848928a770cd147db7ef010fe3cdb743fe05
Rudi Heitbaum <rudi@heitbaum.com>
2025-10-08 09:39:34 +0000

    gcem: initial package
    - required by: fluidsynth: update to 2.5.0

---
+++ packages/devel/gcem/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2025-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="gcem"
+PKG_VERSION="1.18.0"
+PKG_SHA256="8e71a9f5b62956da6c409dda44b483f98c4a98ae72184f3aa4659ae5b3462e61"
+PKG_LICENSE="Apache-2.0"
+PKG_SITE="https://github.com/kthohr/gcem"
+PKG_URL="https://github.com/kthohr/gcem/archive/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="C++ compile-time math library using generalized constant expressions"
