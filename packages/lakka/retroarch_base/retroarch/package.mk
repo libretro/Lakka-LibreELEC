0f50f30d9ac02d43596f0902390f14b489b29254
Shigeaki Asai <y-asai.0816@docomonet.jp>
2025-11-17 12:30:04 +0900

    retroarch: bump version to v1.22.1

--- packages/lakka/retroarch_base/retroarch/package.mk
+++ packages/lakka/retroarch_base/retroarch/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="retroarch"
-PKG_VERSION="703321e80784e3fe3ba0dd648e482cd1ee47cdc0"
+PKG_VERSION="a15217dbaf2c0794f62c64511839d4f72d71cfd9"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/RetroArch"
 PKG_URL="${PKG_SITE}.git"
