b4fdd542287d7e4a3de5e0d4fe2ad8cfb28b587a
Shigeaki Asai <y-asai.0816@docomonet.jp>
2025-11-17 12:31:48 +0900

    slang_shaders: bump version to 3d57e977b892eee945f3347be506c37ddc1b11b5

--- packages/lakka/retroarch_base/slang_shaders/package.mk
+++ packages/lakka/retroarch_base/slang_shaders/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="slang_shaders"
-PKG_VERSION="724d3ec5f67f8fd1eb0962bf712fcf812290ae1a"
+PKG_VERSION="3d57e977b892eee945f3347be506c37ddc1b11b5"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/libretro/slang-shaders"
 PKG_URL="${PKG_SITE}.git"
