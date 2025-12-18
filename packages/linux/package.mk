ba06f8c2c659820ab2de4aea5622beb963c6a8b1
Rudi Heitbaum <rudi@heitbaum.com>
2025-12-18 21:17:25 +0000

    linux: update to 6.18.2

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -61,8 +61,8 @@ case "${LINUX}" in
     PKG_GIT_CLONE_BRANCH="sdm845-5.19.16"
     ;;
   *)
-    PKG_VERSION="6.18"
-    PKG_SHA256="9106a4605da9e31ff17659d958782b815f9591ab308d03b0ee21aad6c7dced4b"
+    PKG_VERSION="6.18.2"
+    PKG_SHA256="558c6bbab749492b34f99827fe807b0039a744693c21d3a7e03b3a48edaab96a"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v${PKG_VERSION/.*/}.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
     PKG_PATCH_DIRS="default rtlwifi/6.18"
     ;;
