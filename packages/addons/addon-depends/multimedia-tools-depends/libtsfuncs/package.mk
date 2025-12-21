bfcf2be7c8f5a08f094ca030f0183fe43e938c43
Rudi Heitbaum <rudi@heitbaum.com>
2025-12-21 13:33:40 +0000

    libtsfuncs: initial package
    used by tsdecrypt

---
+++ packages/addons/addon-depends/multimedia-tools-depends/libtsfuncs/package.mk
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2025-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="libtsfuncs"
+PKG_VERSION="1482ed310186c4fecec2e409e1125316c51cf9c5"
+PKG_SHA256="1abe4f1e33595d23d7a8d9a9f0122ce44aefe141af914d232e7f97cc9f44a2c2"
+PKG_LICENSE="MIT"
+PKG_SITE="https://georgi.unixsol.org/programs/tsdecrypt/"
+PKG_URL="https://github.com/gfto/libtsfuncs/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET=""
+PKG_LONGDESC="libtsfuncs is a library for mpeg PSI parsing and generation"
+PKG_TOOLCHAIN="manual"
