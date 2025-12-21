1eba60708607ed9deaf4888b8e2cbb8f3f6c64e8
Rudi Heitbaum <rudi@heitbaum.com>
2025-12-21 13:02:12 +0000

    tsdecrypt: update to githash f4876e8 and PKG_URL
    needs:
    - libfuncs
    - libtsfuncs

--- packages/addons/addon-depends/multimedia-tools-depends/tsdecrypt/package.mk
+++ packages/addons/addon-depends/multimedia-tools-depends/tsdecrypt/package.mk
@@ -3,19 +3,29 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="tsdecrypt"
-PKG_VERSION="10.0"
-PKG_SHA256="a337a7d60cc8f78b9dffbd7d675390497763bcb8f878ec9f1bec3eb80f32b1f1"
+PKG_VERSION="f4876e84cf1866645f84b93f830af67193c85f69"
+PKG_SHA256="084704d2b121f0fbbe5e5960bb50fbc11695b1cfcd65ebab24b1bf58cfebb38f"
 PKG_LICENSE="GPL"
-PKG_SITE="http://georgi.unixsol.org/programs/tsdecrypt"
-PKG_URL="http://georgi.unixsol.org/programs/tsdecrypt/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
+PKG_SITE="https://georgi.unixsol.org/programs/tsdecrypt/"
+PKG_URL="https://github.com/gfto/tsdecrypt/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain libdvbcsa openssl"
+PKG_DEPENDS_UNPACK="libfuncs libtsfuncs"
 PKG_LONGDESC="A tool that reads incoming mpeg transport stream over UDP/RTP and then decrypts it using libdvbcsa/ffdecsa."
 PKG_BUILD_FLAGS="-sysroot"
 
 PKG_MAKEINSTALL_OPTS_TARGET="PREFIX=/usr"
 
+post_unpack() {
+  tar --strip-components=1 \
+    -xf "${SOURCES}/libfuncs/libfuncs-$(get_pkg_version libfuncs).tar.gz" \
+    -C "${PKG_BUILD}/libfuncs"
+  tar --strip-components=1 \
+    -xf "${SOURCES}/libtsfuncs/libtsfuncs-$(get_pkg_version libtsfuncs).tar.gz" \
+    -C "${PKG_BUILD}/libtsfuncs"
+}
+
 make_target() {
-  make CC=${CC} LINK="${LD} -o"
+  make TARGET=${TARGET_PREFIX} CC=gcc LINK="ld -o"
 }
 
 post_make_target() {
