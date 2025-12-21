73041dca9d2a96d8045f02016406ce884a4bea88
Rudi Heitbaum <rudi@heitbaum.com>
2025-12-21 13:34:39 +0000

    libfuncs: initial package
    used by tsdecrypt

---
+++ packages/addons/addon-depends/multimedia-tools-depends/libfuncs/package.mk
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2025-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="libfuncs"
+PKG_VERSION="55d6236c87e8920563b89af6cd1bf276b423a9a7"
+PKG_SHA256="f3362a41da8b1846ecc9380dd40c0f9e9de638285efe6cc3a6caf9ba66a1728f"
+PKG_LICENSE="MIT"
+PKG_SITE="https://georgi.unixsol.org/programs/tsdecrypt/"
+PKG_URL="https://github.com/gfto/libfuncs/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET=""
+PKG_LONGDESC="libfuncs is collection of code (list, queue, circular buffer, io, logging, etc.)"
+PKG_TOOLCHAIN="manual"
