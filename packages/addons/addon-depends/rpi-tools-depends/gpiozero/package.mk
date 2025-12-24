71d0640c4dce5df2ec41af65fb992b2855487714
Shigeaki Asai <y-asai.0816@docomonet.jp>
2025-12-24 18:53:35 +0900

    gpiozero: recovery for "${DISTRO}" = "Lakka"

--- packages/addons/addon-depends/rpi-tools-depends/gpiozero/package.mk
+++ packages/addons/addon-depends/rpi-tools-depends/gpiozero/package.mk
@@ -11,3 +11,33 @@ PKG_URL="https://files.pythonhosted.org/packages/source/${PKG_NAME:0:1}/${PKG_NA
 PKG_DEPENDS_TARGET="toolchain colorzero"
 PKG_LONGDESC="A simple interface to everyday GPIO components used with Raspberry Pi."
 PKG_TOOLCHAIN="manual"
+
+if [ "${DISTRO}" = "Lakka" ]; then
+  PKG_ARCH+=" aarch64"
+fi
+
+pre_make_target() {
+  if [ "${DISTRO}" = "Lakka" ]; then
+    export PYTHONXCPREFIX="${SYSROOT_PREFIX}/usr"
+    export LDSHARED="${CC} -shared"
+  fi
+}
+
+make_target() {
+  if [ "${DISTRO}" = "Lakka" ]; then
+    python setup.py build --cross-compile
+  fi
+}
+
+makeinstall_target() {
+  if [ "${DISTRO}" = "Lakka" ]; then
+    python setup.py install --root=${INSTALL} --prefix=/usr
+  fi
+}
+
+post_makeinstall_target() {
+  if [ "${DISTRO}" = "Lakka" ]; then
+    find ${INSTALL}/usr/lib -name "*.py" -exec rm -rf "{}" ";"
+    rm -rf ${INSTALL}/usr/bin
+  fi
+}
