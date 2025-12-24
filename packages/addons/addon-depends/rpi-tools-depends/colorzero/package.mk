8ee619db0c77288c20253fc7b5d59ef5df57e645
Shigeaki Asai <y-asai.0816@docomonet.jp>
2025-12-24 18:50:49 +0900

    colorzero: recovery for "${DISTRO}" = "Lakka"

--- packages/addons/addon-depends/rpi-tools-depends/colorzero/package.mk
+++ packages/addons/addon-depends/rpi-tools-depends/colorzero/package.mk
@@ -11,3 +11,34 @@ PKG_URL="https://github.com/waveform80/colorzero/archive/release-${PKG_VERSION}.
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Another color manipulation library for Python (originally from picamera)."
 PKG_TOOLCHAIN="manual"
+
+[ "${DISTRO}" = "Lakka" ] && PKG_DEPENDS_TARGET+=" Python3 distutilscross:host" || true
+
+if [ "${DISTRO}" = "Lakka" ]; then
+  PKG_ARCH+=" aarch64"
+fi
+
+pre_make_target() {
+  if [ "${DISTRO}" = "Lakka" ]; then
+    export PYTHONXCPREFIX="${SYSROOT_PREFIX}/usr"
+    export LDSHARED="${CC} -shared"
+  fi
+}
+
+make_target() {
+  if [ "${DISTRO}" = "Lakka" ]; then
+    python setup.py build --cross-compile
+  fi
+}
+
+makeinstall_target() {
+  if [ "${DISTRO}" = "Lakka" ]; then
+    python setup.py install --root=${INSTALL} --prefix=/usr
+  fi
+}
+
+post_makeinstall_target() {
+  if [ "${DISTRO}" = "Lakka" ]; then
+    find ${INSTALL}/usr/lib -name "*.py" -exec rm -rf "{}" ";"
+  fi
+}
