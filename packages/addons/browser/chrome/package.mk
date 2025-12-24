f21542b21b85573d1729744dd61fbb0996777c99
Rudi Heitbaum <rudi@heitbaum.com>
2025-12-24 08:58:18 +0000

    chrome: update to 143.0.7499.169 and addon (3)
    - cups: update to 2.4.16
    - at-spi2-core: update to 2.58.2

--- packages/addons/browser/chrome/package.mk
+++ packages/addons/browser/chrome/package.mk
@@ -4,8 +4,8 @@
 PKG_NAME="chrome"
 PKG_VERSION="1.0"
 # curl -s http://dl.google.com/linux/chrome/deb/dists/stable/main/binary-amd64/Packages | grep -B 1 Version
-PKG_VERSION_NUMBER="141.0.7390.107"
-PKG_REV="2"
+PKG_VERSION_NUMBER="143.0.7499.169"
+PKG_REV="3"
 PKG_ARCH="x86_64"
 PKG_LICENSE="Custom"
 PKG_SITE="http://www.google.com/chrome"
