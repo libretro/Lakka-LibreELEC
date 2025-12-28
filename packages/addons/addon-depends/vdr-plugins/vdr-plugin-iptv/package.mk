826773922549a1e7b6a8ebbbef2289007e1b2253
Rudi Heitbaum <rudi@heitbaum.com>
2025-12-28 03:07:44 +0000

    vdr-plugin-iptv: update to 2.6.11

--- packages/addons/addon-depends/vdr-plugins/vdr-plugin-iptv/package.mk
+++ packages/addons/addon-depends/vdr-plugins/vdr-plugin-iptv/package.mk
@@ -3,11 +3,11 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="vdr-plugin-iptv"
-PKG_VERSION="f80cd74389576abea552311bade45363247a1c32"
-PKG_SHA256="b26ac5b96f573cf405ad8a6f2fe81cced67c261fcb941f74a588f99fe2fef464"
+PKG_VERSION="2.6.11"
+PKG_SHA256="7c98f8d5c416a45477183e5d3783338bb367f6c27e70a12fad13820b7650c751"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.saunalahti.fi/~rahrenbe/vdr/iptv/"
-PKG_URL="https://github.com/Zabrimus/vdr-plugin-iptv/archive/${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/Zabrimus/vdr-plugin-iptv/archive/v${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain vdr curl"
 PKG_NEED_UNPACK="$(get_pkg_directory vdr)"
 PKG_LONGDESC="vdr-iptv is an IPTV plugin for the Video Disk Recorder (VDR)"
