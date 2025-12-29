48e0208d0df62c0f7cc3b0a46e5dd3824bc840f0
Shigeaki Asai <y-asai.0816@docomonet.jp>
2025-12-29 15:02:41 +0900

    Update glibc to fix upstream libreelec addons (0010-Update-glibc-to-fix-upstream-libreelec-addons.patch)

--- packages/devel/glibc/package.mk
+++ packages/devel/glibc/package.mk
@@ -40,6 +40,7 @@ PKG_CONFIGURE_OPTS_TARGET="BASH_SHELL=/bin/sh \
 
 if [ "${PROJECT}" = "L4T" ]; then
   PKG_CONFIGURE_OPTS_TARGET="${PKG_CONFIGURE_OPTS_TARGET//--enable-kernel=6.6.0/--enable-kernel=3.0.0/}"
+  PKG_CONFIGURE_OPTS_TARGET+=" --disable-mathvec"
 elif [ "${PROJECT}" = "Ayn" ]; then
   PKG_CONFIGURE_OPTS_TARGET="${PKG_CONFIGURE_OPTS_TARGET//--enable-kernel=6.6.0/--enable-kernel=5.19.0/}"
 fi
