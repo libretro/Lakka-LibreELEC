f8060bc4fc2b11acbef4d3d20fe7ae4a014ffc80
Rudi Heitbaum <rudi@heitbaum.com>
2025-12-31 07:22:09 +0000

    tcpdump: update to 4.99.6

--- packages/addons/addon-depends/network-tools-depends/tcpdump/package.mk
+++ packages/addons/addon-depends/network-tools-depends/tcpdump/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="tcpdump"
-PKG_VERSION="4.99.5"
-PKG_SHA256="8c75856e00addeeadf70dad67c9ff3dd368536b2b8563abf6854d7c764cd3adb"
+PKG_VERSION="4.99.6"
+PKG_SHA256="5839921a0f67d7d8fa3dacd9cd41e44c89ccb867e8a6db216d62628c7fd14b09"
 PKG_SITE="https://www.tcpdump.org/"
 PKG_URL="https://www.tcpdump.org/release/tcpdump-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain libpcap libtirpc"
