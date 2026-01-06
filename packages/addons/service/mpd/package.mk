5aaef6b23cd6d858e105f310b581a3add21686ba
Rudi Heitbaum <rudi@heitbaum.com>
2026-01-06 12:11:41 +0000

    mpd: migrate addons settings to new format and addon (3)

--- packages/addons/service/mpd/package.mk
+++ packages/addons/service/mpd/package.mk
@@ -5,7 +5,7 @@
 PKG_NAME="mpd"
 PKG_VERSION="0.24.6"
 PKG_SHA256="8ce34a010577feb42999a96d867325c6d38bf7ae1b7a57f878d7e548c1fd1fa9"
-PKG_REV="2"
+PKG_REV="3"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.musicpd.org"
