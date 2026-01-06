078804040f1dd417a3d0a2e4b9a65a510766ca2d
Rudi Heitbaum <rudi@heitbaum.com>
2026-01-06 12:14:59 +0000

    minidlna: migrate addons settings to new format and addon (1)

--- packages/addons/service/minidlna/package.mk
+++ packages/addons/service/minidlna/package.mk
@@ -4,7 +4,7 @@
 PKG_NAME="minidlna"
 PKG_VERSION="1.3.3"
 PKG_SHA256="39026c6d4a139b9180192d1c37225aa3376fdf4f1a74d7debbdbb693d996afa4"
-PKG_REV="0"
+PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="BSD-3c/GPLv2"
 PKG_SITE="https://sourceforge.net/projects/minidlna/"
