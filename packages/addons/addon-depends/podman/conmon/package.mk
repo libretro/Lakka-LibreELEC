e71fcd5a77ff8f11a3678d6788b656ecadc33610
Rudi Heitbaum <rudi@heitbaum.com>
2026-01-12 12:05:44 +0000

    conmon: update to 2.2.0

--- packages/addons/addon-depends/podman/conmon/package.mk
+++ packages/addons/addon-depends/podman/conmon/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2023-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="conmon"
-PKG_VERSION="2.1.13"
-PKG_SHA256="350992cb2fe4a69c0caddcade67be20462b21b4078dae00750e8da1774926d60"
+PKG_VERSION="2.2.0"
+PKG_SHA256="300d21c2244e1b5e90cc62d796da3e94812bec281bae6868d6e738432155319d"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://github.com/containers/conmon"
 PKG_URL="https://github.com/containers/conmon/archive/v${PKG_VERSION}.tar.gz"
@@ -11,7 +11,7 @@ PKG_DEPENDS_TARGET="toolchain glib libseccomp systemd"
 PKG_LONGDESC="An OCI container runtime monitor"
 
 # Git commit of the matching release https://github.com/containers/conmon
-export PKG_GIT_COMMIT="82de887596ed8ee6d9b2ee85e4f167f307bb569b"
+export PKG_GIT_COMMIT="ff908cce92cf89167b6b97ed240e91a6b147acc1"
 
 pre_configure_target() {
   export PKG_CONFIG_PATH="$(get_install_dir libseccomp)/usr/lib/pkgconfig:${PKG_CONFIG_PATH}"
