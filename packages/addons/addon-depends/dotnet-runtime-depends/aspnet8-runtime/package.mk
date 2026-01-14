ef0b068edef3e0ce1fa13e9d52381dfacd9ab54a
Rudi Heitbaum <rudi@heitbaum.com>
2026-01-14 01:30:32 +0000

    aspnet8-runtime: update to 8.0.23

--- packages/addons/addon-depends/dotnet-runtime-depends/aspnet8-runtime/package.mk
+++ packages/addons/addon-depends/dotnet-runtime-depends/aspnet8-runtime/package.mk
@@ -1,8 +1,8 @@
 # SPDX-License-Identifier: GPL-2.0-only
-# Copyright (C) 2023-present Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="aspnet8-runtime"
-PKG_VERSION="8.0.22"
+PKG_VERSION="8.0.23"
 PKG_LICENSE="MIT"
 PKG_SITE="https://dotnet.microsoft.com/"
 PKG_DEPENDS_TARGET="toolchain"
@@ -11,16 +11,16 @@ PKG_TOOLCHAIN="manual"
 
 case "${ARCH}" in
   "aarch64")
-    PKG_SHA256="110a531808e0b9814ea59cc9b205542dec1ea9d00f2586457c586bc5d28aa461"
-    PKG_URL="https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/8.0.22/aspnetcore-runtime-8.0.22-linux-arm64.tar.gz"
+    PKG_SHA256="b7b0ce6b3cb802aee04ac03ccc978b12a1b1d22a1b66a40d701ed43d35724a81"
+    PKG_URL="https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/8.0.23/aspnetcore-runtime-8.0.23-linux-arm64.tar.gz"
     ;;
   "arm")
-    PKG_SHA256="c84f3e66b59f025c51155ea8bc59aca3134efa4eb534bf9a6bc09a2afc817c45"
-    PKG_URL="https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/8.0.22/aspnetcore-runtime-8.0.22-linux-arm.tar.gz"
+    PKG_SHA256="c4b3b74abafc2e2e0a41fbc46ca4673dca1cd1ceea4c786e8fb14c13d9bd9d5a"
+    PKG_URL="https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/8.0.23/aspnetcore-runtime-8.0.23-linux-arm.tar.gz"
     ;;
   "x86_64")
-    PKG_SHA256="8850a0b40a91d72f3d68b3a3a1f2faf9fb086573177e66aed8dedcf6fd295aec"
-    PKG_URL="https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/8.0.22/aspnetcore-runtime-8.0.22-linux-x64.tar.gz"
+    PKG_SHA256="c456d2166defbf3c13372c54b0f2a6934da98279474ea7410115d7a5090f2204"
+    PKG_URL="https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/8.0.23/aspnetcore-runtime-8.0.23-linux-x64.tar.gz"
     ;;
 esac
 PKG_SOURCE_NAME="aspnetcore-runtime_${PKG_VERSION}_${ARCH}.tar.gz"
