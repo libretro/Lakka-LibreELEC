45889dbf7f33812cd529538fe5441fae56361523
Rudi Heitbaum <rudi@heitbaum.com>
2026-01-18 04:38:24 +0000

    pvr.sledovanitv.cz: update 21.6.0-Omega to 21.6.1-Omega

--- packages/mediacenter/kodi-binary-addons/pvr.sledovanitv.cz/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.sledovanitv.cz/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pvr.sledovanitv.cz"
-PKG_VERSION="21.6.0-Omega"
-PKG_SHA256="92587d655307f16c1d31e660e9ee73b3f7c21319bd6d65180464fd2abeeb22a4"
+PKG_VERSION="21.6.1-Omega"
+PKG_SHA256="b2b85a1c35db1b8c1eb270fc9d2b4ba4fa90e3af99b3c622b45ce2db0353514a"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
