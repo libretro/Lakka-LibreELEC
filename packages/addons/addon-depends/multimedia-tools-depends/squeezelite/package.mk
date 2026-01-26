215a281ca20f1a69a523b81cc9623767e43b6ae8
Rudi Heitbaum <rudi@heitbaum.com>
2026-01-26 11:44:58 +0000

    squeezelite: update to githash 6d571de (2.0.0.1556)

--- packages/addons/addon-depends/multimedia-tools-depends/squeezelite/package.mk
+++ packages/addons/addon-depends/multimedia-tools-depends/squeezelite/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="squeezelite"
-PKG_VERSION="0e5514efcb6366b74d7144626f1e9b4da7e81bf8"
-PKG_SHA256="9d17b46aa3eee53141549758e874d259d8b20a0120d57a395b6a3ad8da5a1dae"
+PKG_VERSION="6d571de8fa6dfff23a5a0cbb2c81b402d2c30c31"
+PKG_SHA256="aaac7a4b2106166b6443b97ced537f45c6893da5683583f4518e14bddf9f40cc"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/ralph-irving/squeezelite"
 PKG_URL="https://github.com/ralph-irving/squeezelite/archive/${PKG_VERSION}.tar.gz"
