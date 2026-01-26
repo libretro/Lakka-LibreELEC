a8cace4e89322fe08b4cc517434acba9fe675e0a
Matthias Reichl <hias@horus.com>
2026-01-26 23:27:30 +0100

    linux (RPi): update to 6.18.7-d477259
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -23,8 +23,8 @@ case "${LINUX}" in
 	PKG_PATCH_DIRS="default rtlwifi/6.18"
     ;;
   raspberrypi)
-    PKG_VERSION="0b059dc32f6db80e2877fbe1de73eb1c7e905dce" # 6.18.7
-    PKG_SHA256="2bb0f4af603c6dd8bcd593a71774759501e65e32093ac2ce2bf96ed8749642a2"
+    PKG_VERSION="d477259acbe9fe42de4aa605c0c3952a2b970b37" # 6.18.7
+    PKG_SHA256="d12318c2afbb7299a294a811d6571cdcf114b1878ac1b86d1f7d98292bb65742"
     PKG_URL="https://github.com/raspberrypi/linux/archive/${PKG_VERSION}.tar.gz"
     PKG_SOURCE_NAME="linux-${LINUX}-${PKG_VERSION}.tar.gz"
     PKG_PATCH_DIRS="raspberrypi"
