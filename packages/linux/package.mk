db785ed1a90adfce85fe3ce1a328b5661fb5c5ab
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-01-27 19:40:14 +0900

    linux: update to 6.18.7

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -61,8 +61,8 @@ case "${LINUX}" in
     PKG_GIT_CLONE_BRANCH="sdm845-5.19.16"
     ;;
   *)
-    PKG_VERSION="6.18.4"
-    PKG_SHA256="f850139ca5f79c1bf6bb8b32f92e212aadca97bdaef8a83a7cf4ac4d6a525fab"
+    PKG_VERSION="6.18.7"
+    PKG_SHA256="b726a4d15cf9ae06219b56d87820776e34d89fbc137e55fb54a9b9c3015b8f1e"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v${PKG_VERSION/.*/}.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;
