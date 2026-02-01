d35b76bd18eb06609b045cfe81a0b2ff5ce55b12
Rudi Heitbaum <rudi@heitbaum.com>
2026-02-01 00:33:47 +0000

    at-spi2-core: update to 2.59.1

--- packages/addons/addon-depends/chrome-depends/at-spi2-core/package.mk
+++ packages/addons/addon-depends/chrome-depends/at-spi2-core/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="at-spi2-core"
-PKG_VERSION="2.59.0"
-PKG_SHA256="5e88eed802474c3f181e4a70e7a7c4a4b1b873ac52bd766e6fd269049b52616f"
+PKG_VERSION="2.59.1"
+PKG_SHA256="e791ccf1d0305ad1709e370a0a83034f36a5a248f6b2ae35dbdddb1cdff72c3f"
 PKG_LICENSE="OSS"
 PKG_SITE="https://www.gnome.org/"
 PKG_URL="https://download.gnome.org/sources/at-spi2-core/${PKG_VERSION:0:4}/at-spi2-core-${PKG_VERSION}.tar.xz"
