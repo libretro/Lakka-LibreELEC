12b2318ec80cf35223c3330d86891345fa23415a
Rudi Heitbaum <rudi@heitbaum.com>
2026-02-05 03:44:29 +0000

    mediainfo: update to 26.01

--- packages/addons/addon-depends/multimedia-tools-depends/mediainfo/package.mk
+++ packages/addons/addon-depends/multimedia-tools-depends/mediainfo/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mediainfo"
-PKG_VERSION="25.10"
-PKG_SHA256="366b325101ab1a9a4ee79fbdb8e3dd9e78bcc0a21c0f9b1966313aaed3d33502"
+PKG_VERSION="26.01"
+PKG_SHA256="150656cad88ef4efa4e8299cfc24d9b7a72354576a69349920588acee2cc3c76"
 PKG_LICENSE="GPL"
 PKG_SITE="https://mediaarea.net/en/MediaInfo/Download/Source"
 PKG_URL="https://mediaarea.net/download/source/mediainfo/${PKG_VERSION}/mediainfo_${PKG_VERSION}.tar.xz"
