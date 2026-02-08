7994d1723cb16a747e009e5cc6c22e4e14bbc079
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-02-08 23:02:50 +0900

    mesa: Add libglvnd package on RK356X/RK3576/RK3588 wayland for melonds, vircon32 and vitaquake2 cores building

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -55,6 +55,11 @@ elif [ "${DISPLAYSERVER}" = "wl" ]; then
   PKG_DEPENDS_TARGET+=" wayland wayland-protocols"
   PKG_MESON_OPTS_TARGET+=" -Dplatforms=wayland \
                            -Dglx=disabled"
+  # melonds, vircon32 and vitaquake2 cores on wayland need opengl headers that is contained in libglvnd
+  if [ "${DISTRO}" = "Lakka" -a "${DEVICE:0:4}" = "RK35" ]; then
+    PKG_DEPENDS_TARGET+=" libglvnd"
+    PKG_MESON_OPTS_TARGET+=" -Dglvnd=true"
+  fi
 elif [ "${DISTRO}" = "Lakka" -o "${PROJECT}" = "L4T" ]; then
   PKG_DEPENDS_TARGET+=" libglvnd"
   PKG_MESON_OPTS_TARGET+=" -Dplatforms="" -Dglx=disabled -Dglvnd=enabled"
