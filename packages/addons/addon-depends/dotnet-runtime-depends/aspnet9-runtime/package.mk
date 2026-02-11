419a412487c749f629a74619a3c111d4ae594c6a
Rudi Heitbaum <rudi@heitbaum.com>
2026-02-11 12:24:52 +0000

    aspnet9-runtime: update to 9.0.13

--- packages/addons/addon-depends/dotnet-runtime-depends/aspnet9-runtime/package.mk
+++ packages/addons/addon-depends/dotnet-runtime-depends/aspnet9-runtime/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="aspnet9-runtime"
-PKG_VERSION="9.0.12"
+PKG_VERSION="9.0.13"
 PKG_LICENSE="MIT"
 PKG_SITE="https://dotnet.microsoft.com/"
 PKG_DEPENDS_TARGET="toolchain"
@@ -11,16 +11,16 @@ PKG_TOOLCHAIN="manual"
 
 case "${ARCH}" in
   "aarch64")
-    PKG_SHA256="b0ce7bc0ab002c8f4852991ec3d29ca55f46142168325a4767405a709c3363bd"
-    PKG_URL="https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/9.0.12/aspnetcore-runtime-9.0.12-linux-arm64.tar.gz"
+    PKG_SHA256="636cbf7e66dbcc3d58e855c1816731c8fbe5e130869352fb81b5ad224f5d1905"
+    PKG_URL="https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/9.0.13/aspnetcore-runtime-9.0.13-linux-arm64.tar.gz"
     ;;
   "arm")
-    PKG_SHA256="75b83e5245713470e581b238299c3f6125f16b5c5c625f7c04c7d9ce85e068be"
-    PKG_URL="https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/9.0.12/aspnetcore-runtime-9.0.12-linux-arm.tar.gz"
+    PKG_SHA256="d0b9b5cd8b16dd405fcb3a853d90e17fc81955800899ec51f1cca9e26c172ae0"
+    PKG_URL="https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/9.0.13/aspnetcore-runtime-9.0.13-linux-arm.tar.gz"
     ;;
   "x86_64")
-    PKG_SHA256="e691ad6b028fc8f4c2128ecd6a38bf13b217cff9c034d97cdc3df8ee30e688ec"
-    PKG_URL="https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/9.0.12/aspnetcore-runtime-9.0.12-linux-x64.tar.gz"
+    PKG_SHA256="947e248fd0745b89e5708378bcd67aa20caae2ba11c3fd9bd76e3e4114966ccd"
+    PKG_URL="https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/9.0.13/aspnetcore-runtime-9.0.13-linux-x64.tar.gz"
     ;;
 esac
 PKG_SOURCE_NAME="aspnetcore-runtime_${PKG_VERSION}_${ARCH}.tar.gz"
