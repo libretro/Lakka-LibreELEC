b3acf8e8e4a3a7d51610e86ee7f711535395c409
Rudi Heitbaum <rudi@heitbaum.com>
2026-02-11 12:23:53 +0000

    chrome: update to 145.0.7632.45 and addon (5)
    - at-spi2-core: update to 2.59.1

--- packages/addons/browser/chrome/package.mk
+++ packages/addons/browser/chrome/package.mk
@@ -4,8 +4,8 @@
 PKG_NAME="chrome"
 PKG_VERSION="1.0"
 # curl -s http://dl.google.com/linux/chrome/deb/dists/stable/main/binary-amd64/Packages | grep -B 1 Version
-PKG_VERSION_NUMBER="143.0.7499.192"
-PKG_REV="4"
+PKG_VERSION_NUMBER="145.0.7632.45"
+PKG_REV="5"
 PKG_ARCH="x86_64"
 PKG_LICENSE="Custom"
 PKG_SITE="http://www.google.com/chrome"
