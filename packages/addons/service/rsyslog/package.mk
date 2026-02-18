902b23bcaf8a6d3e752efa6f2c65e2bf2dceede2
Rudi Heitbaum <rudi@heitbaum.com>
2026-02-18 09:26:14 +0000

    rsyslog: update to 8.2602.0 and addon (6)
    requires:
    - libyaml: initial package
    - protobuf-c: initial package
    - snappy: initial package

--- packages/addons/service/rsyslog/package.mk
+++ packages/addons/service/rsyslog/package.mk
@@ -2,14 +2,14 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="rsyslog"
-PKG_VERSION="8.2512.0"
-PKG_SHA256="93c50025d90b6c795fa350d56a3d832bfce45043ea9bd68240d9c2a9394bc629"
-PKG_REV="5"
+PKG_VERSION="8.2602.0"
+PKG_SHA256="4fe5256cea046d77546d36042d090e384184bc24041ecda5d03c03d35d1eabbb"
+PKG_REV="6"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/rsyslog"
 PKG_URL="https://www.rsyslog.com/files/download/rsyslog/${PKG_NAME}-${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain curl libestr libfastjson libgcrypt liblogging liblognorm librelp util-linux zlib"
+PKG_DEPENDS_TARGET="toolchain protobuf-c:host curl libestr libfastjson libgcrypt liblogging liblognorm librelp protobuf-c snappy util-linux zlib"
 PKG_SECTION="service"
 PKG_SHORTDESC="Rsyslog: a rocket-fast system for log processing."
 PKG_LONGDESC="Rsyslog (${PKG_VERSION}) offers high-performance, great security features and a modular design."
@@ -31,6 +31,7 @@ export LIBGCRYPT_CONFIG="${SYSROOT_PREFIX}/usr/bin/libgcrypt-config"
 
 pre_configure_target() {
   CFLAGS+=" -fcommon"
+  export PKG_CONFIG_PATH="$(get_install_dir protobuf-c)/usr/lib/pkgconfig:$(get_install_dir snappy)/usr/lib/pkgconfig:$(get_install_dir libyaml)/usr/lib/pkgconfig:${PKG_CONFIG_PATH}"
 }
 
 post_configure_target() {
