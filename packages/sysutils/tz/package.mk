97d10068b52fdf7aa8eccc03bc70be2b30bdc416
Rudi Heitbaum <rudi@heitbaum.com>
2026-03-02 20:30:39 +0000

    tz: update to 2026a

--- packages/sysutils/tz/package.mk
+++ packages/sysutils/tz/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="tz"
-PKG_VERSION="2025c"
-PKG_SHA256="d970fb6753529583226fb1bb9df6237e5e968ea7d70a8bd0df2f3394c86f7ac4"
+PKG_VERSION="2026a"
+PKG_SHA256="686cdc7b5b1dbe8396f5e244782f1e3972ed3afae6c653c756949da803ab8129"
 PKG_LICENSE="Public Domain"
 PKG_SITE="http://www.iana.org/time-zones"
 PKG_URL="https://github.com/eggert/tz/archive/${PKG_VERSION}.tar.gz"
