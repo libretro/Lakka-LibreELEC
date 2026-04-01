ef64db7d210725c77fc266a507168432018e2d12
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-04-01 20:13:43 +0900

    mame2000: bump version to cee538f9b28f00039f298cb3c2b588203f07d0be

--- packages/lakka/libretro_cores/mame2000/package.mk
+++ packages/lakka/libretro_cores/mame2000/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="mame2000"
-PKG_VERSION="2ec60f6e1078cf9ba173e80432cc28fd4eea200f"
+PKG_VERSION="cee538f9b28f00039f298cb3c2b588203f07d0be"
 PKG_LICENSE="MAME"
 PKG_SITE="https://github.com/libretro/mame2000-libretro"
 PKG_URL="${PKG_SITE}.git"
