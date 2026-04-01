c8ad9c0bd73aefb0c413c5e4ebb3fb8b3f9cf9b7
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-04-01 19:05:59 +0900

    fake_08: bump version to f6bab5a7ba521ce440e45d1aeef6122674be6ee9

--- packages/lakka/libretro_cores/fake_08/package.mk
+++ packages/lakka/libretro_cores/fake_08/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="fake_08"
-PKG_VERSION="130814eccd388a1e456c17b5ef3b9771ce8f7701"
+PKG_VERSION="f6bab5a7ba521ce440e45d1aeef6122674be6ee9"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/jtothebell/fake-08"
 PKG_URL="${PKG_SITE}.git"
