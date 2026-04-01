cfc3956d5e6a8a821c0b4179b0197f9010b43a10
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-04-01 20:23:21 +0900

    mame2010: bump version to 29095383c0281100fee2ee09f1438d8ae990c510

--- packages/lakka/libretro_cores/mame2010/package.mk
+++ packages/lakka/libretro_cores/mame2010/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="mame2010"
-PKG_VERSION="c5b413b71e0a290c57fc351562cd47ba75bac105"
+PKG_VERSION="29095383c0281100fee2ee09f1438d8ae990c510"
 PKG_LICENSE="MAME"
 PKG_SITE="https://github.com/libretro/mame2010-libretro"
 PKG_URL="${PKG_SITE}.git"
