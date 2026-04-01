422d052bc5a1b9a06d36e78baefe2c7134224cac
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-04-01 18:48:45 +0900

    crocods: bump version to 89210e76ec6079660976068fa6b1cef3ac2b293b

--- packages/lakka/libretro_cores/crocods/package.mk
+++ packages/lakka/libretro_cores/crocods/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="crocods"
-PKG_VERSION="a320f6e38af49af84a63f81329a1bdb9322022b4"
+PKG_VERSION="89210e76ec6079660976068fa6b1cef3ac2b293b"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/libretro/libretro-crocods"
 PKG_URL="${PKG_SITE}.git"
