7c4a6b4c6ae40796f4371bef5b18192ab61248e0
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-04-01 13:14:04 +0900

    azahar: bump version to 3066887ff4cf9f89e626047d0f142ad231053cf1

--- packages/lakka/libretro_cores/azahar/package.mk
+++ packages/lakka/libretro_cores/azahar/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="azahar"
-PKG_VERSION="9b045bf8370d035cacd99e5ad19a082df9e3b262"
+PKG_VERSION="3066887ff4cf9f89e626047d0f142ad231053cf1"
 PKG_ARCH="x86_64 aarch64"
 PKG_LICENSE="GPL-2.0"
 PKG_SITE="https://github.com/azahar-emu/azahar"
