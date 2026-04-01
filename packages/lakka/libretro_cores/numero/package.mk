de800bedde413876895ab743aa793b3f44019360
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-04-01 20:40:23 +0900

    numero: bump version to 28d25413b47e289ed7211fa39c886e223eaed407

--- packages/lakka/libretro_cores/numero/package.mk
+++ packages/lakka/libretro_cores/numero/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="numero"
-PKG_VERSION="867c40ad9cdfe5295e0be81e0a17b16964a4a33e"
+PKG_VERSION="28d25413b47e289ed7211fa39c886e223eaed407"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/nbarkhina/numero"
 PKG_URL="${PKG_SITE}.git"
