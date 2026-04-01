93582a1e1a1b691c3791b892abc80c8832b7ff35
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-04-01 18:51:07 +0900

    dice: bump version to 72d66a67a296b004fa0bb7a57dbab9cfcec462b9

--- packages/lakka/libretro_cores/dice/package.mk
+++ packages/lakka/libretro_cores/dice/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="dice"
-PKG_VERSION="beb422a68dbb7f78d73b1fd6cd0807288a6613c6"
+PKG_VERSION="72d66a67a296b004fa0bb7a57dbab9cfcec462b9"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/mittonk/dice-libretro"
 PKG_URL="${PKG_SITE}.git"
