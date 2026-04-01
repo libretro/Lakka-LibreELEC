b1b5a6c6deee68dc26037deb5f6b78f63f4ab917
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-04-01 18:38:04 +0900

    boytacean: bump version to 440213b507cfaefcc3adf6a62460097407379bcc

--- packages/lakka/libretro_cores/boytacean/package.mk
+++ packages/lakka/libretro_cores/boytacean/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="boytacean"
-PKG_VERSION="2a0d2beb34f6cec016c227c16f57963612e5669e"
+PKG_VERSION="440213b507cfaefcc3adf6a62460097407379bcc"
 PKG_LICENSE="Apache-2.0"
 PKG_ARCH="any !i386"
 PKG_SITE="https://github.com/joamag/boytacean"
