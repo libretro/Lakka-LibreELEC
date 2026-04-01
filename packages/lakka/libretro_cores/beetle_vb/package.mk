3ea5d87ee1ad73189b946f8a5a8c5ffc0c0c144d
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-04-01 18:29:29 +0900

    beetle_vb: bump version to 734205c5ead87a89cd1d53fe086f8f8fe660cf1d

--- packages/lakka/libretro_cores/beetle_vb/package.mk
+++ packages/lakka/libretro_cores/beetle_vb/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="beetle_vb"
-PKG_VERSION="8f837ebc077afdd6652efb2827fd8308a07113ca"
+PKG_VERSION="734205c5ead87a89cd1d53fe086f8f8fe660cf1d"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/beetle-vb-libretro"
 PKG_URL="${PKG_SITE}.git"
