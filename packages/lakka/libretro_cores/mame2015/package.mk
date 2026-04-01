b02c19ad4bcedecb9ca845b8f828ebecf5aa4bc8
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-04-01 20:24:07 +0900

    mame2015: bump version to 48fdf6532f31e3bb8b58b9ae10198b047cd8de42

--- packages/lakka/libretro_cores/mame2015/package.mk
+++ packages/lakka/libretro_cores/mame2015/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="mame2015"
-PKG_VERSION="316cd06349f2b34b4719f04f7c0d07569a74c764"
+PKG_VERSION="48fdf6532f31e3bb8b58b9ae10198b047cd8de42"
 PKG_ARCH="x86_64 aarch64 arm"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/mame2015-libretro"
