fe4376eceb9f6aad1d81a54dc9218de936310ce8
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-04-23 19:52:28 +0900

    sameboy: uses upstream repo and libretro branch and adds workaround patch for rgbasm not found in build time

--- packages/lakka/libretro_cores/sameboy/package.mk
+++ packages/lakka/libretro_cores/sameboy/package.mk
@@ -1,15 +1,21 @@
 PKG_NAME="sameboy"
-PKG_VERSION="51433012a871a44555492273fd22f29867d12655"
+PKG_VERSION="8230189896a8bb6598574d302ba0ad3658f98ab4"
 PKG_LICENSE="MIT"
-PKG_SITE="https://github.com/libretro/sameboy"
+PKG_SITE="https://github.com/LIJI32/SameBoy"
 PKG_URL="${PKG_SITE}.git"
+PKG_GIT_CLONE_BRANCH="libretro"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Gameboy and Gameboy Color emulator written in C"
 PKG_TOOLCHAIN="make"
 
-PKG_MAKE_OPTS_TARGET="-C libretro/"
+PKG_MAKE_OPTS_TARGET="libretro"
+PKG_BUILD_FLAGS="-parallel"
+
+pre_make_target() {
+  export BOOTROMS_DIR="${PKG_BUILD}/BootROMs/prebuilt"
+}
 
 makeinstall_target() {
-  mkdir -p ${INSTALL}/usr/lib/libretro
-    cp -v libretro/sameboy_libretro.so ${INSTALL}/usr/lib/libretro/
+  mkdir -p "${INSTALL}/usr/lib/libretro"
+    cp -v "${PKG_BUILD}/build/bin/sameboy_libretro.so" "${INSTALL}/usr/lib/libretro/"
 }
